9986853be590e7f63c7ed87aef670cea33f63b02
James Campbell <jpcampb2@ncsu.edu>
2017-07-20 16:01:43 -0400

    Add initial unittests for distributional expectations.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
-from .test_dataset import *
-from .test_pandas_dataset import *
 from .test_great_expectations import *
 from .test_util import *
+from .test_dataset import *
+from .test_pandas_dataset import *
+from .test_pandas_dataset_distributional_expectations import *
