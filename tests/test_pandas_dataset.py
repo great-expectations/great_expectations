bddddad3683fcf9f7d8c46b2041651191328cd85
Abe Gong <abegong@gmail.com>
2017-10-23 16:28:05 -0700

    Implement tests for expect_column_most_common_value_to_be and expect_column_most_common_value_to_be_in_set

--- tests/test_pandas_dataset.py
+++ tests/test_pandas_dataset.py
@@ -1066,6 +1066,84 @@ class TestPandasDataset(unittest.TestCase):
             out = D.expect_column_proportion_of_unique_values_to_be_between(**t['in'])
             self.assertEqual(out, t['out'])
 
+    def test_expect_column_most_common_value_to_be(self):
+
+        D = ge.dataset.PandasDataSet({
+            'x' : [1,1,2,2,3,None, None, None, None, None],
+            'y' : ['hello', 'jello', 'mello', 'hello', 'jello', 'mello', 'hello', 'jello', 'mello', 'jello'],
+            'z' : [1,2,2,3,3,3,4,4,4,4],
+        })
+        D.set_default_expectation_argument("output_format", "COMPLETE")
+
+        T = [
+                {
+                    'in':{"column":"x","value":1},
+                    'out':{"success":False, "true_value":[1,2]},
+                },{
+                    'in':{"column":"x", "value":1, "ties_okay":True},
+                    'out':{"success":True, "true_value":[1,2]},
+                },{
+                    'in':{"column":"x","value":3},
+                    'out':{"success":False, "true_value":[1,2]},
+                },{
+                    'in':{"column":"x","value":3, "ties_okay":True},
+                    'out':{"success":False, "true_value":[1,2]},
+                },{
+                    'in':{"column":"y","value":"jello"},
+                    'out':{'success':True, "true_value":["jello"]},
+                },{
+                    'in':{"column":"y","value":"hello"},
+                    'out':{'success':False, "true_value":["jello"]},
+                },{
+                    'in':{"column":"z","value":4},
+                    'out':{'success':True, "true_value":[4]},
+                }
+        ]
+
+        for t in T:
+            out = D.expect_column_most_common_value_to_be(**t['in'])
+            self.assertEqual(out, t['out'])
+
+
+    def test_expect_column_most_common_value_to_be_in_set(self):
+
+        D = ge.dataset.PandasDataSet({
+            'x' : [1,1,2,2,3,None, None, None, None, None],
+            'y' : ['hello', 'jello', 'mello', 'hello', 'jello', 'mello', 'hello', 'jello', 'mello', 'jello'],
+            'z' : [1,2,2,3,3,3,4,4,4,4],
+        })
+        D.set_default_expectation_argument("output_format", "COMPLETE")
+
+        T = [
+                {
+                    'in':{"column":"x","value_set":[1]},
+                    'out':{"success":True, "true_value":[1,2]},
+                },{
+                    'in':{"column":"x", "value_set":[1]},
+                    'out':{"success":True, "true_value":[1,2]},
+                },{
+                    'in':{"column":"x","value_set":3},
+                    'out':{"success":False, "true_value":[1,2]},
+                },{
+                    'in':{"column":"y","value_set":["jello", "hello"]},
+                    'out':{'success':True, "true_value":["jello"]},
+                },{
+                    'in':{"column":"y","value_set":["hello", "mello"]},
+                    'out':{'success':False, "true_value":["jello"]},
+                },{
+                    'in':{"column":"z","value_set":[4]},
+                    'out':{'success':True, "true_value":[4]},
+                }
+        ]
+
+
+        for t in T:
+            out = D.expect_column_most_common_value_to_be_in_set(**t['in'])
+            self.assertEqual(out, t['out'])
+
+
+    #####
+
     def test_expectation_decorator_summary_mode(self):
 
         df = ge.dataset.PandasDataSet({
