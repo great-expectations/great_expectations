2c6ced20ea30b5b4e4555fed669966c4763f27d4
James Campbell <jpcampb2@ncsu.edu>
2017-11-24 14:40:17 -0500

    Build new test datasets programmatically.

--- tests/test_fixtures/distributional_expectations_partition_fixtures.py
+++ tests/test_fixtures/distributional_expectations_partition_fixtures.py
@@ -3,64 +3,72 @@ import numpy as np
 import scipy.stats as stats
 import great_expectations as ge
 import json
+import sys
 
-generate_new_data = False
-generate_new_partitions = True
+"""
+Use this file to generate random datasets for testing distributional expectations.
 
-if generate_new_data == True:
+Tests expect two datasets: "distributional_expectations_data_base.csv" and "distributional_expectations_data_test.csv" 
+They also expect a set of partitions: "test_partitions.json"
+
+The partitions should be built from distributional_expectations_data_base.csv. The tests will use distributional_expectations_data_test.csv
+
+"""
+
+def generate_new_data():
     norm_0_1 = stats.norm.rvs(0, 1, 100)
     norm_1_1 = stats.norm.rvs(1, 1, 100)
     norm_10_1 = stats.norm.rvs(10, 1, 100)
     bimodal = np.concatenate((norm_0_1[:50], norm_10_1[50:]))
     categorical_fixed = (['A'] * 54) + (['B'] * 32) + (['C'] * 14)
 
-    df = pd.DataFrame( {
+    return pd.DataFrame( {
         'norm_0_1': norm_0_1,
         'norm_1_1': norm_1_1,
+        'norm_10_1': norm_10_1,
         'bimodal': bimodal,
         'categorical_fixed': categorical_fixed
     })
 
-    df.to_csv('../test_sets/distributional_expectations_data_test.csv')
-else:
-    df = pd.read_csv('../test_sets/distributional_expectations_data_base.csv')
 
-D = ge.dataset.PandasDataSet(df)
 
-if generate_new_partitions == True:
+def generate_new_partitions(df):
     test_partitions = {}
     for column in ['norm_0_1', 'norm_1_1', 'bimodal']:
-        for partition_type in ['no_holdout', 'tail_holdout', 'both_holdout']:
-            if partition_type == 'no_holdout':
-                internal_weight_holdout = 0.
-                tail_weight_holdout = 0.
-            elif partition_type == 'tail_holdout':
-                internal_weight_holdout = 0.
-                tail_weight_holdout = 1e-5
-            elif partition_type == 'both_holdout':
-                internal_weight_holdout = 1e-5
-                tail_weight_holdout = 1e-5
+        partition_object = ge.dataset.util.kde_partition_data(df[column])
+        # Print how close sum of weights is to one for a quick visual consistency check when data are generated
+        #print(column + '_kde: '+ str(abs(1-np.sum(partition_object['weights']))))
+        test_partitions[column + '_kde'] = partition_object
 
-            partition_object = ge.dataset.util.kde_partition_data(df[column],
-                                                                  internal_weight_holdout=internal_weight_holdout,
-                                                                  tail_weight_holdout=tail_weight_holdout)
-            print(column + '_kde_' + partition_type + ': '+ str(abs(1-np.sum(partition_object['weights']))))
-            test_partitions[column + '_kde_' + partition_type] = partition_object
-
-            for bin_type in ['uniform', 'ntile', 'auto']:
-                partition_object = ge.dataset.util.continuous_partition_data(df[column], bin_type,
-                                                                             internal_weight_holdout=internal_weight_holdout,
-                                                                             tail_weight_holdout=tail_weight_holdout)
-                print(column + '_' + bin_type + '_' + partition_type + ': ' + str(abs(1 - np.sum(partition_object['weights']))))
-                test_partitions[column + '_' + bin_type + '_' + partition_type] = partition_object
+        for bin_type in ['uniform', 'ntile', 'auto']:
+            partition_object = ge.dataset.util.continuous_partition_data(df[column], bin_type)
+            # Print how close sum of weights is to one for a quick visual consistency check when data are generated
+            #print(column + '_' + bin_type + ': ' + str(abs(1 - np.sum(partition_object['weights']))))
+            test_partitions[column + '_' + bin_type] = partition_object
 
     partition_object = ge.dataset.util.categorical_partition_data(df['categorical_fixed'])
     test_partitions['categorical_fixed'] = partition_object
     alt_partition = ge.dataset.util.categorical_partition_data(df['categorical_fixed'])
+    # overwrite weights with uniform weights to give a testing dataset
     alt_partition['weights'] = [1./len(alt_partition['partition'])] * len(alt_partition['partition'])
     test_partitions['categorical_fixed_alternate'] = alt_partition
+
+    return test_partitions
+
+
+if __name__ == "__main__":
+    # Set precision we'll use:
+    precision = sys.float_info.dig
+    print("Setting pandas float_format to use " + str(precision) + " digits of precision.")
+
+    df = generate_new_data()
+    df.to_csv('../test_sets/distributional_expectations_data_base.csv', float_format='%.' + str(precision) + 'g')
+    test_partitions = generate_new_partitions(df)
+
+    ge.dataset.util.ensure_json_serializable(test_partitions)
     with open('../test_sets/test_partitions.json', 'w') as file:
         file.write(json.dumps(test_partitions))
-else:
-    with open('../test_sets/test_partitions.json', 'r') as file:
-        test_partitions = json.loads(file.read())
+
+    df = generate_new_data()
+    df.to_csv('../test_sets/distributional_expectations_data_test.csv', float_format='%.' + str(precision) + 'g')
+    print("Done generating new base data, partitions, and test data.")
