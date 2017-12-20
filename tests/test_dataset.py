f5b8f45e2fd1f0348b31b3c41265a097caf22b88
James Campbell <jpcampb2@ncsu.edu>
2017-12-20 09:31:39 -0500

    Remove meta_notes hidden parameter (#157)

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -90,28 +90,6 @@ class TestDataset(unittest.TestCase):
             df.expect_column_values_to_be_increasing("x", meta={"unserializable_array":np.array(range(10))})
 
 
-    def test_expectation_meta_notes(self):
-        df = ge.dataset.PandasDataSet({
-            'x': [1, 2, 4],
-            'y': [1, 2, 5],
-            'z': ['hello', 'jello', 'mello'],
-        })
-
-        result = df.expect_column_median_to_be_between('x', 2, 2, meta_notes="This expectation is also for lolz.")
-        k = 0
-        self.assertEqual(result['success'], True)
-        config = df.get_expectations_config()
-        for expectation_config in config['expectations']:
-            if expectation_config['expectation_type'] == 'expect_column_median_to_be_between':
-                k += 1
-                self.assertEqual(
-                    expectation_config['meta'],
-                    {"notes": "This expectation is also for lolz."}
-                )
-
-        self.assertEqual(k, 1)
-
-
 
     #TODO: !!! Add tests for expectation and column_expectation
     #TODO: !!! Add tests for save_expectation
