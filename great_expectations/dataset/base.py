76d5f706075873229afcdb5260a01ca14926cf85
Max Gasner <max.gasner@gmail.com>
2018-01-29 15:21:12 -0800

    return a list for partial_exception_counts

--- great_expectations/dataset/base.py
+++ great_expectations/dataset/base.py
@@ -8,8 +8,10 @@ import traceback
 import warnings
 
 import numpy as np
-from collections import Counter
-from collections import defaultdict
+from collections import (
+    Counter,
+    defaultdict
+)
 
 from ..version import __version__
 from .util import DotDict, recursively_convert_to_json_serializable, DocInherit
@@ -697,16 +699,12 @@ If you wish to change this behavior, please set discard_failed_expectations, dis
             missing_count = element_count-int(len(nonnull_values))#int(null_indexes.sum())
             exception_count = len(exception_list)
 
-            #FIXME This shouldn't use pandas.
-            # exception_value_series = pd.Series(exception_list).value_counts().iloc[:20]
-            # partial_exception_counts = dict(zip(
-            #     list(exception_value_series.index),
-            #     list(exception_value_series.values),
-            # ))
-            partial_exception_counts = [{
-                    "value" : k,
-                    "count" : v
-                } for k,v in Counter(exception_list).most_common(20)
+            partial_exception_counts = [
+                {'value': key, 'count': value}
+                for key, value
+                in sorted(
+                    Counter(exception_list).most_common(20),
+                    key=lambda x: (-x[1], x[0]))
             ]
 
             if element_count > 0:
