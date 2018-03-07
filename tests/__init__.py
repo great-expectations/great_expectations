4e0a27f527eab6f759a80d38f4d86a6c58a9c10b
James Campbell <jpcampb2@ncsu.edu>
2018-03-06 16:14:05 -0800

    Move pandas distributional expectations to new submodule

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ from .test_great_expectations import *
 from .test_util import *
 from .test_dataset import *
 from .test_pandas_dataset import *
-from .test_pandas_dataset_distributional_expectations import *
+from tests.pandas.test_pandas_dataset_distributional_expectations import *
 from .test_expectation_decorators import *
 from .test_cli import *
