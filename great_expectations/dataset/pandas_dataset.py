d9b843c05a5ece6f778fc1cb1f8ef063ef1b11c1
Abe <abegong@gmail.com>
2018-03-07 16:49:16 -0800

    Implement expect_columns_to_be

--- great_expectations/dataset/pandas_dataset.py
+++ great_expectations/dataset/pandas_dataset.py
@@ -185,6 +185,24 @@ class PandasDataSet(MetaPandasDataSet, pd.DataFrame):
             }
 
     @DocInherit
+    @DataSet.expectation(['column_list'])
+    def expect_columns_to_be(self, column_list,
+                               result_format=None, include_config=False, catch_exceptions=None, meta=None):
+        if len(list(self.columns)) != len(column_list):
+            return {
+                "success" : False
+            }
+
+        if list(self.columns) == column_list:
+            return {
+                "success" : True
+            }
+        else:
+            return {
+                "success": False
+            }
+
+    @DocInherit
     @DataSet.expectation(['min_value', 'max_value'])
     def expect_table_row_count_to_be_between(self,
         min_value=0,
