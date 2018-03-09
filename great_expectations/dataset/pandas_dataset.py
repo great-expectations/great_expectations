e538a6018b044d8d84de2adb7f65aeb2c3253ffe
Abe <abegong@gmail.com>
2018-03-09 06:02:05 -0800

    Change name to expect_table_columns_to_match_ordered_list

--- great_expectations/dataset/pandas_dataset.py
+++ great_expectations/dataset/pandas_dataset.py
@@ -220,8 +220,9 @@ class PandasDataset(MetaPandasDataset, pd.DataFrame):
 
     @DocInherit
     @DataSet.expectation(['column_list'])
-    def expect_columns_to_be(self, column_list,
+    def expect_table_columns_to_match_ordered_list(self, column_list,
                                result_format=None, include_config=False, catch_exceptions=None, meta=None):
+
         if len(list(self.columns)) != len(column_list):
             return {
                 "success" : False
