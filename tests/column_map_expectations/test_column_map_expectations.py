bb02aedba3b3ceddc5a446bc989ed35d915e131e
Abe <abegong@gmail.com>
2018-03-09 20:30:17 -0800

    Decruft

--- tests/column_map_expectations/test_column_map_expectations.py
+++ tests/column_map_expectations/test_column_map_expectations.py
@@ -15,24 +15,13 @@ import glob
 
 from tests.util import get_dataset, evaluate_json_test
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-test_configuration_files = glob.glob(dir_path+'/*.json')
-# print test_configuration_files
-
-# test_configurations_dict = {}
-# for filename in test_configuration_files:
-#   file = open(filename)
-#   test_configurations_dict['filename'] = json.load(file)
-
-# dataset = test_configurations['dataset']
-# expectation_name = test_configurations['expectation_type']
-# test_cases = test_configurations['tests']
-# test_case_ids = [test['title'] for test in test_cases]
-
-# @pytest.fixture(scope="module")
 contexts = ['PandasDataSet', 'SqlAlchemyDataSet']
 
 def pytest_generate_tests(metafunc):
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    test_configuration_files = glob.glob(dir_path+'/*.json')
+
     parametrized_tests = []
     ids = []
     for c in contexts:
@@ -44,18 +33,16 @@ def pytest_generate_tests(metafunc):
                 my_dataset = get_dataset(c, d["data"])
 
                 for test in d["tests"]:
-                    #FIXME:
-                    # my_data.remove_all_expectations()
                     parametrized_tests.append({
                         "expectation_type": test_configuration["expectation_type"],
                         "dataset": my_dataset,
                         "test": test,
                     })
+                    #FIXME: title should be required.
+                    #If it's not present, then what?
                     if not "title" in test:
                         test["title"] = "BLANK"
                     ids.append(c+":"+test_configuration["expectation_type"]+":"+test["title"])
-
-    print len(parametrized_tests)
 
     metafunc.parametrize(
         "test_case",
@@ -64,8 +51,8 @@ def pytest_generate_tests(metafunc):
     )
 
 def test_case_runner(test_case):
-    # print test_case["test"], test_case["expectation_type"]
-    # print test_case["dataset"].get_expectations_config()
+    #FIXME:
+    # test_case["dataset"].remove_all_expectations()
     evaluate_json_test(
         test_case["dataset"],
         test_case["expectation_type"],
