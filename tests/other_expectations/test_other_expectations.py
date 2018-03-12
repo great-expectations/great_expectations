c46a6ee626257792dc7b1bc9d6cd61aa638151f3
James Campbell <jpcampb2@ncsu.edu>
2018-03-12 17:38:40 -0400

    Update all test runners to not generate tests on notimplemented list

--- tests/other_expectations/test_other_expectations.py
+++ tests/other_expectations/test_other_expectations.py
@@ -12,16 +12,17 @@ import pytest
 import os
 import json
 import glob
+import warnings
 
-from tests.test_utils import get_dataset, evaluate_json_test
+from tests.test_utils import get_dataset, candidate_test_is_on_temporary_notimplemented_list, evaluate_json_test
 
 contexts = ['PandasDataSet', 'SqlAlchemyDataSet']
 
-def pytest_generate_tests(metafunc):
 
-    #Load all the JSON files in the directory
+def pytest_generate_tests(metafunc):
+    # Load all the JSON files in the directory
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    test_configuration_files = glob.glob(dir_path+'/*.json')
+    test_configuration_files = glob.glob(dir_path + '/*.json')
 
     parametrized_tests = []
     ids = []
@@ -30,26 +31,31 @@ def pytest_generate_tests(metafunc):
             file = open(filename)
             test_configuration = json.load(file)
 
-            for d in test_configuration['datasets']:
-                my_dataset = get_dataset(c, d["data"])
+            if candidate_test_is_on_temporary_notimplemented_list(c, test_configuration["expectation_type"]):
+                warnings.warn("Skipping generation of tests for expectation " + test_configuration["expectation_type"] +
+                              " and context " + c)
+            else:
+                for d in test_configuration['datasets']:
+                    my_dataset = get_dataset(c, d["data"])
 
-                for test in d["tests"]:
-                    parametrized_tests.append({
-                        "expectation_type": test_configuration["expectation_type"],
-                        "dataset": my_dataset,
-                        "test": test,
-                    })
+                    for test in d["tests"]:
+                        parametrized_tests.append({
+                            "expectation_type": test_configuration["expectation_type"],
+                            "dataset": my_dataset,
+                            "test": test,
+                        })
 
-                    ids.append(c+":"+test_configuration["expectation_type"]+":"+test["title"])
-            
+                        ids.append(c + ":" + test_configuration["expectation_type"] + ":" + test["title"])
+
     metafunc.parametrize(
         "test_case",
         parametrized_tests,
         ids=ids
     )
 
+
 def test_case_runner(test_case):
-    #FIXME:
+    # FIXME:
     # test_case["dataset"].remove_all_expectations()
     evaluate_json_test(
         test_case["dataset"],
