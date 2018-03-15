3cea99d98d24a28316dab5a16d6b9cc67bfd8cfc
James Campbell <jpcampb2@ncsu.edu>
2018-03-15 19:18:24 -0400

    Add basic docstrings for the Data Contexts.

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -8,11 +8,18 @@ from great_expectations.data_context import SqlAlchemyDataContext, PandasCSVData
 from .version import __version__
 
 
-def get_data_context(context_type, *args, **kwargs):
-    if context_type == "sqlalchemy":
-        return SqlAlchemyDataContext(args, kwargs)
-    elif context_type == "pandas_directory":
-        return PandasCSVDataContext(args, kwargs)
+def get_data_context(context_type, options):
+    """Return a data_context object which exposes options to list datasets and get a dataset from
+    that context. This is a new API in Great Expectations 0.4, and is subject to rapid change.
+
+    :param context_type: (string) one of "SqlAlchemy" or "PandasCSV"
+    :param options: options to be passed to the data context's connect method.
+    :return: a new DataContext object
+    """
+    if context_type == "SqlAlchemy":
+        return SqlAlchemyDataContext(options)
+    elif context_type == "PandasCSV":
+        return PandasCSVDataContext(options)
     else:
         raise ValueError("Unknown data context.")
 
--- great_expectations/data_context/SqlAlchemyDataContext.py
+++ great_expectations/data_context/SqlAlchemyDataContext.py
@@ -5,6 +5,12 @@ from sqlalchemy import create_engine, MetaData
 
 
 class SqlAlchemyDataContext(DataContext):
+    """
+    A SqlAlchemyDataContext creates a SQLAlchemy engine and provides a list of tables available in the list_datasets
+    method. Its get_dataset method returns a new SqlAlchemy dataset with the provided name.
+
+    Warning: this feature is new in v0.4 and may change based on community feedback.
+    """
 
     def __init__(self, *args, **kwargs):
         super(SqlAlchemyDataContext, self).__init__(*args, **kwargs)
