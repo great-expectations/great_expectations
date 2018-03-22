e01ae89f774342364df1522e3b8d08e6ffb23e6a
Christopher Nobblitt <ccnobbli@ncsu.edu>
2018-03-22 09:58:47 -0400

    #186 removed extra data columns that we aren't using anymore

--- tests/test_fixtures/fixed_distribution_data.py
+++ tests/test_fixtures/fixed_distribution_data.py
@@ -27,17 +27,17 @@ def generate_data():
         'norm': stats.norm.rvs(loc=norm_mean, scale=norm_std, size=500),
         'norm_std': stats.norm.rvs(size=500),
         'beta': stats.beta.rvs(a=beta_a, b=beta_b, loc=beta_loc, scale=beta_scale, size=500),
-        'beta_std': stats.beta.rvs(a=beta_a, b=beta_b, size=500),
+        #'beta_std': stats.beta.rvs(a=beta_a, b=beta_b, size=500),
         'gamma': stats.gamma.rvs(a=gamma_a, loc=gamma_loc, scale=gamma_scale, size=500),
-        'gamma_std': stats.gamma.rvs(a=gamma_a, size=500),
-        'poisson': stats.poisson.rvs(mu=poisson_lambda, loc=poisson_loc, size=500),
-        'poisson_std': stats.poisson.rvs(mu=poisson_lambda, size=500),
+        #'gamma_std': stats.gamma.rvs(a=gamma_a, size=500),
+        #'poisson': stats.poisson.rvs(mu=poisson_lambda, loc=poisson_loc, size=500),
+        #'poisson_std': stats.poisson.rvs(mu=poisson_lambda, size=500),
         'uniform': stats.uniform.rvs(loc=uniform_loc, scale=uniform_scale, size=500),
-        'uniform_std': stats.uniform.rvs(size=500),
+        #'uniform_std': stats.uniform.rvs(size=500),
         'chi2': stats.chi2.rvs(df=chi2_df, loc=chi2_loc, scale=chi2_scale, size=500),
-        'chi2_std': stats.chi2.rvs(df=chi2_df, size=500),
+        #'chi2_std': stats.chi2.rvs(df=chi2_df, size=500),
         'exponential': stats.expon.rvs(loc=expon_loc, scale=expon_scale, size=500),
-        'exponential_std': stats.expon.rvs(size=500)
+        #'exponential_std': stats.expon.rvs(size=500)
     })
 
     # different seed for chi2
