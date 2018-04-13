6692ef01fdb0ca3da3812db5698a2d4fa95bc81a
Bouke Nederstigt <bouke.nederstigt@gmail.com>
2018-04-13 14:27:51 +0200

    Operator & hooks improvements
    - Cleanup connection management in operator so it becomes easier to switch between different types of hooks
    - Add MySql hook so we can use datasets from database
    - Skip unit test because we can't run airflow on travis
    - Enable results dumping to s3

--- great_expectations_airflow/__init__.py
+++ great_expectations_airflow/__init__.py
@@ -1,8 +1,7 @@
 from airflow.plugins_manager import AirflowPlugin
-
-from great_expectations_airflow.hooks.s3_csv_hook import ExpectationCsvS3Hook
 from great_expectations_airflow.operators.expectation_operator import ExpectationOperator
-
+from great_expectations_airflow.hooks.s3_csv_hook import ExpectationS3CsvHook
+from great_expectations_airflow.hooks.db_hook import ExpectationMySQLHook
 
 class GreatExpectationsPlugin(AirflowPlugin):
     name = 'great_expectations_plugin'
@@ -10,7 +9,7 @@ class GreatExpectationsPlugin(AirflowPlugin):
     # A list of class(es) derived from BaseOperator
     operators = [ExpectationOperator]
     # A list of class(es) derived from BaseHook
-    hooks = [ExpectationCsvS3Hook]
+    hooks = [ExpectationS3CsvHook, ExpectationMySQLHook]
     # A list of class(es) derived from BaseExecutor
     executors = []
     # A list of references to inject into the macros namespace
