1a486ce70293f4c2ed4621a2b6b3604869470894
James Campbell <jpcampb2@ncsu.edu>
2018-06-01 21:24:39 -0400

    More cleanup of legacy test-cli code.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,7 @@ import subprocess
 
 from .test_utils import assertDeepAlmostEqual
 
+
 def get_system_command_result(command_str):
     p = subprocess.Popen(
         command_str.split(' '),
@@ -22,39 +23,34 @@ def get_system_command_result(command_str):
 
 class TestCLI(unittest.TestCase):
 
-    def test_cli_arguments(self):
+    def test_cli_help_message(self):
         filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
         #!!! These tests require you to uninstall and re-install great_expectations.
         #!!! Otherwise, they test the CLI, but not great_expectations itself.
 
-        #print(get_system_command_result('python '+filepath+'/../bin/great_expectations '))
+        # print(get_system_command_result('python '+filepath+'/../bin/great_expectations '))
         self.assertEqual(
             get_system_command_result('python '+filepath+'/../bin/great_expectations '),
             {'output': '', 'errors': "usage: great_expectations [-h] {initialize,validate} ...\ngreat_expectations: error: invalid choice: '' (choose from 'initialize', 'validate')\n"}
         )
 
-        # print get_system_command_result('python '+filepath+'/../bin/great_expectations validate')
-        # self.assertEqual(
-        #     get_system_command_result('python '+filepath+'/../bin/great_expectations validate'),
-        #     {'output': '', 'errors': 'usage: great_expectations validate [-h] [--result_format result_format]\n                                   [--catch_exceptions]\n                                   [--include_config INCLUDE_CONFIG]\n                                   [--only_return_failures]\n                                   [--custom_dataset_module CUSTOM_DATASET_MODULE]\n                                   [--custom_dataset_class CUSTOM_DATASET_CLASS]\n                                   data_set expectations_config_file\ngreat_expectations validate: error: too few arguments\n'}
-        # )
+    def test_cli_basic_usage(self):
+        filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
         command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/test_sets/Titanic.csv '+filepath+'/test_sets/titanic_expectations.json'
-        print(command_str)
+        # print(command_str)
 
         try:
-          result = get_system_command_result(command_str)
-          json_result = json.loads(result["output"])
+            result = get_system_command_result(command_str)
+            json_result = json.loads(result["output"])
         except ValueError as ve:
-          print ("=== Result ==================================================")
-          print (result)
-          print ("=== Error ===================================================")
-          print(ve)
-          json_result = {}
+            print ("=== Result ==================================================")
+            print (result)
+            print ("=== Error ===================================================")
+            print(ve)
+            json_result = {}
 
-        # print("^"*80)
-        # print(json.dumps(json_result, indent=2))
         self.maxDiff = None
 
         with open(filepath + '/test_sets/expected_cli_results_default.json', 'r') as f:
@@ -65,39 +61,8 @@ class TestCLI(unittest.TestCase):
             expected_cli_results
         )
 
-        #
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f'
-        # self.assertEqual(
-        #     get_system_command_result(command_str),
-        #     {'output': '{\n  "results": [\n    {\n      "exception_traceback": null, \n      "expectation_type": "expect_column_values_to_be_in_set", \n      "success": false, \n      "raised_exception": false, \n      "kwargs": {\n        "column": "PClass", \n        "result_format": "BASIC", \n        "value_set": [\n          "1st", \n          "2nd", \n          "3rd"\n        ]\n      }, \n      "summary_obj": {\n        "unexpected_percent": 0.0007616146230007616, \n        "partial_unexpected_list": [\n          "*"\n        ], \n        "unexpected_percent_nonmissing": 0.0007616146230007616, \n        "unexpected_count": 1\n      }\n    }\n  ]\n}\n', 'errors': ''}
-        # )
-        #
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f -o=COMPLETE'
-        # self.assertEqual(
-        #     get_system_command_result(command_str),
-        #     {'output': '{\n  "results": [\n    {\n      "exception_traceback": null, \n      "expectation_type": "expect_column_values_to_be_in_set", \n      "success": False, \n      "unexpected_list": [\n        "*"\n      ], \n      "raised_exception": false, \n      "kwargs": {\n        "column": "PClass", \n        "result_format": "COMPLETE", \n        "value_set": [\n          "1st", \n          "2nd", \n          "3rd"\n        ]\n      }, \n      "unexpected_index_list": [\n        456\n      ]\n    }\n  ]\n}\n', 'errors': ''}
-        # )
-        #
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f -o=BOOLEAN_ONLY'
-        # self.assertEqual(
-        #     get_system_command_result(command_str),
-        #     {'output': '{\n  "results": [\n    {\n      "expectation_type": "expect_column_values_to_be_in_set", \n      "success": false, \n      "kwargs": {\n        "column": "PClass", \n        "result_format": "BOOLEAN_ONLY", \n        "value_set": [\n          "1st", \n          "2nd", \n          "3rd"\n        ]\n      }\n    }\n  ]\n}\n', 'errors': ''}
-        # )
-        #
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f -e'
-        # # print get_system_command_result(command_str)
-        # self.assertEqual(
-        #     get_system_command_result(command_str),
-        #     {'output': '{\n  "results": [\n    {\n      "summary_obj": {\n        "unexpected_percent": 0.0007616146230007616, \n        "partial_unexpected_list": [\n          "*"\n        ], \n        "unexpected_percent_nonmissing": 0.0007616146230007616, \n        "unexpected_count": 1\n      }, \n      "expectation_type": "expect_column_values_to_be_in_set", \n      "success": false, \n      "kwargs": {\n        "column": "PClass", \n        "result_format": "BASIC", \n        "value_set": [\n          "1st", \n          "2nd", \n          "3rd"\n        ]\n      }\n    }\n  ]\n}\n', 'errors': ''}
-        # )
-        #
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f -e'
-        # # print get_system_command_result(command_str)
-        # self.assertEqual(
-        #     get_system_command_result(command_str)["output"],
-        #     "{\n  \"results\": [\n    {\n      \"summary_obj\": {\n        \"unexpected_percent\": 0.0007616146230007616, \n        \"partial_unexpected_list\": [\n          \"*\"\n        ], \n        \"unexpected_percent_nonmissing\": 0.0007616146230007616, \n        \"unexpected_count\": 1\n      }, \n      \"expectation_type\": \"expect_column_values_to_be_in_set\", \n      \"success\": false, \n      \"kwargs\": {\n        \"column\": \"PClass\", \n        \"result_format\": \"BASIC\", \n        \"value_set\": [\n          \"1st\", \n          \"2nd\", \n          \"3rd\"\n        ]\n      }\n    }\n  ]\n}\n"
-        # )
-        #print(filepath)
+    def test_cli_custom_dataset(self):
+        filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
         command_str = 'python ' + filepath + '/../bin/great_expectations validate ' \
                       + filepath + '/test_sets/Titanic.csv '\
@@ -114,7 +79,6 @@ class TestCLI(unittest.TestCase):
           json_result = {}
 
         self.maxDiff = None
-        # print(json.dumps(json_result, indent=2))
 
         #Remove partial unexpected counts, because we can't guarantee that they'll be the same every time.
         del json_result["results"][0]["result"]['partial_unexpected_counts']
@@ -126,39 +90,6 @@ class TestCLI(unittest.TestCase):
             json_result,
             expected_cli_results
         )
-        # command_str = 'python '+filepath+'/../bin/great_expectations validate '+filepath+'/examples/Titanic.csv '+filepath+'/examples/titanic_expectations.json -f'
-        # # print get_system_command_result(command_str)
-        # self.assertEqual(
-        #     get_system_command_result(command_str)["output"],
-        #     {
-        #       "results": [
-        #         {
-        #           "exception_traceback": None, 
-        #           "expectation_type": "expect_column_values_to_be_in_set", 
-        #           "success": False,
-        #           "raised_exception": False, 
-        #           "kwargs": {
-        #             "column": "PClass", 
-        #             "result_format": "BASIC", 
-        #             "include_config": False, 
-        #             "value_set": [
-        #               "1st", 
-        #               "2nd", 
-        #               "3rd"
-        #             ]
-        #           }, 
-        #           "summary_obj": {
-        #             "unexpected_percent": 0.0007616146230007616,
-        #             "partial_unexpected_list": [
-        #               "*"
-        #             ], 
-        #             "unexpected_percent_nonmissing": 0.0007616146230007616,
-        #             "unexpected_count": 1
-        #           }
-        #         }
-        #       ]
-        #     }
-        # )
 
     def test_cli_evaluation_parameters(self):
         filepath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
