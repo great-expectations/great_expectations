5e89b4487d7fc8470fafcb1dfe2d014dd5f16cca
anhollis <anhollis@ncsu.edu>
2018-09-17 17:04:00 -0400

    test_from_pandas made compatible with pytest

--- tests/test_pandas_dataset.py
+++ tests/test_pandas_dataset.py
@@ -387,19 +387,19 @@ class TestPandasDataset(unittest.TestCase):
         with self.assertRaises(ValueError):
             df.expect_column_mean_to_be_between('x',4,6, result_format="QUACK")
 
-    def test_from_pandas(self):
-        pd_df = pd.DataFrame({
-            'x':[1,3,5,7,9],
-            'y':[2,4,6,8,10],
-            'z':[None,'a','b','c','abc']
-        })
+def test_from_pandas():
+    pd_df = pd.DataFrame({
+        'x':[1,3,5,7,9],
+        'y':[2,4,6,8,10],
+        'z':[None,'a','b','c','abc']
+    })
 
-        ge_df = ge.from_pandas(pd_df)
-        self.assertIsInstance(ge_df, ge.dataset.Dataset)
-        self.assertEquals(list(ge_df.columns), ['x', 'y', 'z'])
-        self.assertEquals(list(ge_df['x']), list(pd_df['x']))
-        self.assertEquals(list(ge_df['y']), list(pd_df['y']))
-        self.assertEquals(list(ge_df['z']), list(pd_df['z']))
+    ge_df = ge.from_pandas(pd_df)
+    assert isinstance(ge_df, ge.dataset.Dataset)
+    assert list(ge_df.columns)==['x', 'y', 'z']
+    assert list(ge_df['x'])==list(pd_df['x'])
+    assert list(ge_df['y'])==list(pd_df['y'])
+    assert list(ge_df['z'])==list(pd_df['z'])
 
 
 def test_from_pandas_expectations_config():
