7cc2dee15be987ad12be14eecf0894dca7f2a98e
anhollis <anhollis@ncsu.edu>
2018-10-23 10:41:28 -0400

    Import MetaFileDataSet and FileDataset

--- great_expectations/dataset/__init__.py
+++ great_expectations/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .base import Dataset
 from .pandas_dataset import MetaPandasDataset, PandasDataset
 from .sqlalchemy_dataset import MetaSqlAlchemyDataset, SqlAlchemyDataset
+from .file_dataset import  MetaFileDataset, FileDataset
