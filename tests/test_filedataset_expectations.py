2dc75e12254ff7c6963a0338f993718e88b45536
anhollis <anhollis@ncsu.edu>
2018-10-31 11:07:38 -0400

    Added tests for invalid skip and regex values

--- tests/test_filedataset_expectations.py
+++ tests/test_filedataset_expectations.py
@@ -17,7 +17,22 @@ def expect_file_line_regex_match_count_to_be_between_test():
     complete_data=open('./tests/test_sets/Titanic.csv',"r")
     
     file_dat=ge.dataset.FileDataset(complete_data)
+    
+    
+    #Invalid Skip Parameter
+    with pytest.raises(ValueError):
+        file_dat.expect_file_line_regex_match_count_to_be_between(regex=",\S",
+                                                            expected_min_count=1.3, 
+                                                            expected_max_count=8,
+                                                            skip=2.4) 
 
+    #Invalid Regex
+    with pytest.raises(ValueError):
+        file_dat.expect_file_line_regex_match_count_to_be_between(regex=2,
+                                                            expected_min_count=1.3, 
+                                                            expected_max_count=8,
+                                                            skip=2.4) 
+    
         
     
     #Non-integer min value
@@ -93,6 +108,12 @@ def expect_file_line_regex_match_count_to_equal_test():
     
     file_dat=ge.dataset.FileDataset(complete_data)
     file_incomplete_dat=ge.dataset.FileDataset(incomplete_data)
+    
+    #Invalid Regex Value
+    with pytest.raises(ValueError):
+        file_dat.expect_file_line_regex_match_count_to_equal(regex=True,
+                                                            expected_count=6.3,
+                                                            skip=1) 
     
     
     #Non-integer expected_count
