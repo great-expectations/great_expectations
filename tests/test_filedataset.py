8c2be83eace3abb489ad606d7c5c43dab16834f6
anhollis <anhollis@ncsu.edu>
2018-11-05 19:08:44 -0500

    Added tests to raise NotImplementedError when using Dataset method equivalents of FileDataset expectations.

--- tests/test_filedataset.py
+++ tests/test_filedataset.py
@@ -106,7 +106,34 @@ def test_expectation_config_filedataset():
 
 
     
+#Raise NotImplementedError when using FileDataset expectations from Dataset object
+def test_filedataset_expectations_NotImplementedError():
+   
+    #Create Dataset objects
     
+    f_dat=ge.dataset.Dataset() 
+    
+    #Test expect_file_line_regex+match_count_to_equal
+    
+    with pytest.raises(NotImplementedError):
+        
+        try:
+            f_dat.expect_file_line_regex_match_count_to_equal(regex=',\S',
+                                                            expected_count=3,
+                                                            skip=1,result_format="BASIC")
+        except:
+            raise
+   
+    #Test expect_file_line_regex+match_count_to_between
+    with pytest.raises(NotImplementedError):
+        
+        try:
+            f_dat.expect_file_line_regex_match_count_to_be_between(regex=',\S',
+                                                            max_expected_count=3,
+                                                            skip=1,result_format="BASIC")
+        except:
+            raise
+                
     
     
 
