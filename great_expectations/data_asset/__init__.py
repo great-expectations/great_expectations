98b686ad169eb607186300bb11c8015723e3f71e
anhollis <anhollis@ncsu.edu>
2019-03-06 16:49:57 -0500

    Created File_DataAsset object

--- great_expectations/data_asset/__init__.py
+++ great_expectations/data_asset/__init__.py
@@ -2,3 +2,4 @@ from .base import DataAsset
 from .dataset import Dataset
 from .pandas_dataset import MetaPandasDataset, PandasDataset
 from .sqlalchemy_dataset import MetaSqlAlchemyDataset, SqlAlchemyDataset
+from .file_data_asset import  MetaFileDataAsset, FileDataAsset
