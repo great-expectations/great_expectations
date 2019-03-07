1126d8b78413aca8d7ad1c35fda6ceb8b247e2fb
anhollis <anhollis@ncsu.edu>
2019-03-06 22:14:50 -0500

    Deleted old file_expectations and accompanying tests as these are superseded by new FileDataAsset expectations

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -1,6 +1,5 @@
 from .util import *
 from great_expectations import data_asset
 from great_expectations.data_context import get_data_context
-from .file_expectations import *
 
 from .version import __version__
