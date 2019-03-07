da4a73b2907bc045262349e4f514e07c4762ec38
anhollis <anhollis@ncsu.edu>
2019-03-06 21:30:00 -0500

    Created expect_file_to_have_valid_table_header

--- great_expectations/data_asset/file_data_asset.py
+++ great_expectations/data_asset/file_data_asset.py
@@ -475,3 +475,77 @@ class FileDataAsset(MetaFileDataAsset):
             success = True
 
         return {"success":success}
+
+    @DataAsset.expectation([])
+    def expect_file_to_have_valid_table_header(self, regex, skip=None,
+                                               result_format=None,
+                                               include_config=False,
+                                               catch_exceptions=None, meta=None):
+        """
+        Checks to see if a file has a line with unique delimited values,
+        such a line may be used as a table header.
+
+        Keyword Args:
+            skip (nonnegative integer): \
+                Integer specifying the first lines in the file the method
+                should skip before assessing expectations
+
+            regex (string):
+                A string that can be compiled as valid regular expression.
+                Used to specify the elements of the table header (the column headers)
+
+            result_format (str or None):
+                Which output mode to use: `BOOLEAN_ONLY`, `BASIC`, `COMPLETE`, or `SUMMARY`.
+                For more detail, see :ref:`result_format <result_format>`.
+
+            include_config (boolean): \
+                If True, then include the expectation config as part of the result object. \
+                For more detail, see :ref:`include_config`.
+
+            catch_exceptions (boolean or None): \
+                If True, then catch exceptions and include them as part of the result object. \
+                For more detail, see :ref:`catch_exceptions`.
+
+            meta (dict or None): \
+                A JSON-serializable dictionary (nesting allowed) that will be
+                included in the output without modification. For more detail,
+                see :ref:`meta`.
+
+        Returns:
+            A JSON-serializable expectation result object.
+
+        Exact fields vary depending on the values passed to :ref:`result_format <result_format>` and
+        :ref:`include_config`, :ref:`catch_exceptions`, and :ref:`meta`.
+        """
+
+        try:
+            comp_regex = re.compile(regex)
+        except:
+            raise ValueError("Must enter valid regular expression for regex")
+
+        success = False
+
+        try:
+            with open(self.path, 'r') as f:
+                lines = f.readlines() #Read in file lines
+
+        except IOError:
+            raise
+
+            #Skip k initial lines designated by the user
+        if skip is not None and skip <= len(lines):
+            try:
+                assert float(skip).is_integer()
+                assert float(skip) >= 0
+            except:
+                raise ValueError("skip must be a positive integer")
+
+            for i in range(1, skip+1):
+                lines.pop(0)
+
+        header_line = lines[0].strip()
+        header_names = comp_regex.split(header_line)
+        if len(set(header_names)) == len(header_names):
+            success = True
+
+        return {"success":success}
