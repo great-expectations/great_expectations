ef916ac704e77ae1e078aebbfd8b315bd78174fc
Christian Selig <seligchristian@gmail.com>
2019-03-13 21:50:36 -0700

    c/p some stuff from feature/spark-and-docker

--- great_expectations/dataset/__init__.py
+++ great_expectations/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .base import Dataset
 from .pandas_dataset import MetaPandasDataset, PandasDataset
 from .sqlalchemy_dataset import MetaSqlAlchemyDataset, SqlAlchemyDataset
+from .sparkdf_dataset import MetaSparkDFDataset, SparkDFDataset
