23d79551e5f65dac4220c42b2148e7a4af70628f
Christian Selig <cselig@uw.edu>
2019-04-18 18:55:57 -0700

    add chisquare test expectation for spark

--- great_expectations/dataset/sparkdf_dataset.py
+++ great_expectations/dataset/sparkdf_dataset.py
@@ -242,6 +242,20 @@ class SparkDFDataset(MetaSparkDFDataset):
     def _get_column_mean(self, column):
         return self.spark_df.select(mean_(col(column))).collect()[0][0]
 
+    def _get_column_value_counts(self, column):
+        value_counts = self.spark_df.select(column)\
+            .filter('{} is not null'.format(column))\
+            .groupBy(column)\
+            .count()\
+            .orderBy(column)\
+            .collect()
+        return pd.Series(
+            [row['count'] for row in value_counts],
+            index=[row[column] for row in value_counts],
+            # don't know about including name here
+            name=column,
+        )
+
     @DocInherit
     @MetaSparkDFDataset.column_map_expectation
     def expect_column_values_to_be_in_set(
@@ -473,12 +487,7 @@ class SparkDFDataset(MetaSparkDFDataset):
 
             # Data are expected to be discrete, use value_counts
             col_name = column.columns[0]
-            value_counts = column.groupBy(col_name).count().orderBy(col_name).collect()
-            observed_counts = pd.Series(
-                [row['count'] for row in value_counts],
-                index=[row[col_name] for row in value_counts],
-                name='observed',
-            )
+            observed_counts = self.column_value_counts[col_name]
             observed_weights = observed_counts / self.row_count
             expected_weights = pd.Series(
                 partition_object['weights'], index=partition_object['values'], name='expected')
@@ -488,7 +497,7 @@ class SparkDFDataset(MetaSparkDFDataset):
             na_counts = test_df.isnull().sum()
 
             # Handle NaN: if we expected something that's not there, it's just not there.
-            pk = test_df['observed'].fillna(0)
+            pk = test_df[col_name].fillna(0)
             # Handle NaN: if something's there that was not expected, substitute the relevant value for tail_weight_holdout
             if na_counts['expected'] > 0:
                 # Scale existing expected values
@@ -526,5 +535,65 @@ class SparkDFDataset(MetaSparkDFDataset):
         else:
             # TODO
             raise NotImplementedError('Not implemented for continuous distributions')
+
+        return return_obj
+
+    @DocInherit
+    @MetaSparkDFDataset.column_aggregate_expectation
+    def expect_column_chisquare_test_p_value_to_be_greater_than(
+        self,
+        column,
+        partition_object=None,
+        p=0.05,
+        tail_weight_holdout=0,
+        result_format=None,
+        include_config=False,
+        catch_exceptions=None,
+        meta=None,
+    ):
+        if not is_valid_categorical_partition_object(partition_object):
+            raise ValueError("Invalid partition object.")
+        col_name = column.columns[0]
+        observed_frequencies = self.column_value_counts[col_name]
+        # Convert to Series object to allow joining on index values
+        expected_column = pd.Series(
+            partition_object['weights'], index=partition_object['values'], name='expected'
+        ) * self.column_nonnull_counts[col_name]
+        # Join along the indices to allow proper comparison of both types of possible missing values
+        # test_df = pd.concat([expected_column, observed_frequencies], axis=1, sort=True) # Sort parameter not available before pandas 0.23.0
+        test_df = pd.concat([expected_column, observed_frequencies], axis=1)
+
+        na_counts = test_df.isnull().sum()
+
+        # Handle NaN: if we expected something that's not there, it's just not there.
+        test_df[col_name] = test_df[col_name].fillna(0)
+        # Handle NaN: if something's there that was not expected, substitute the relevant value for tail_weight_holdout
+        if na_counts['expected'] > 0:
+            # Scale existing expected values
+            test_df['expected'] = test_df['expected'] * \
+                (1 - tail_weight_holdout)
+            # Fill NAs with holdout.
+            test_df['expected'] = test_df['expected'].fillna(
+                len(column) * (tail_weight_holdout / na_counts['expected']))
+
+        test_result = stats.chisquare(
+            test_df[col_name], test_df['expected'])[1]
+
+        return_obj = {
+            "success": test_result > p,
+            "result": {
+                "observed_value": test_result,
+                "details": {
+                    "observed_partition": {
+                        "values": test_df.index.tolist(),
+                        "weights": test_df[col_name].tolist()
+                    },
+                    "expected_partition": {
+                        "values": test_df.index.tolist(),
+                        "weights": test_df['expected'].tolist()
+                    }
+                }
+            }
+        }
 
         return return_obj
