a9253631d4d47949e1a0a8d660c5266e6e3692f1
Abe <abegong@gmail.com>
2019-04-20 13:18:26 -0700

    Add basic jinja rendering

--- great_expectations/render/full_page.py
+++ great_expectations/render/full_page.py
@@ -1,8 +1,19 @@
+import os
+from jinja2 import Template
+
 from .base import Renderer
 from .single_expectation import SingleExpectationRenderer
 
 #FullPageExpectationHtmlRenderer
 #FullPageValidationResultHtmlRenderer
+
+t = Template(open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'fixtures/single_page.j2'
+    )
+).read())
+print(t.render())
 
 class FullPageHtmlRenderer(Renderer):
     def __init__(self, expectations, inspectable):
