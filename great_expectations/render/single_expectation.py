df6c2abd73a564d89f6581742381a6465a439c7c
Abe <abegong@gmail.com>
2019-04-20 11:10:16 -0700

    Start to flesh our directory structure. WIP WIP WIP

--- great_expectations/render/single_expectation.py
+++ great_expectations/render/single_expectation.py
@@ -1,20 +1,3 @@
-"""
-jtbd:
-* render many Expectation Suites as a full static HTML site
-    navigation
-
-* render a single Expectation Suite as a standalone HTML file
-    sections
-
-* render a single Expectation Suite as a (potentially nested) list of elements (e.g. div, p, span, JSON, jinja, markdown)
-    grouping?
-
-* render a single Expectation as a single element (e.g. div, p, span, JSON, jinja, markdown)
-"""
-
-
-
-
 class SingleExpectationRenderer(Renderer):
     def __init__(self, expectation):
         self.expectation = expectation
