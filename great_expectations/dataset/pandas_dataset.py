0fbd476c7e43aae7b5901ada2bf7e0775f2c9b3f
Tobias Schmidt <royalts@gmail.com>
2019-04-25 08:49:01 +0200

    parse value_set everywhere

--- great_expectations/dataset/pandas_dataset.py
+++ great_expectations/dataset/pandas_dataset.py
@@ -551,6 +551,11 @@ class PandasDataset(MetaPandasDataset, pd.DataFrame):
 
         return column.map(lambda x: isinstance(x, tuple(target_type_list)))
 
+    @staticmethod
+    def _parse_value_set(value_set):
+        parsed_value_set = [parse(value) if isinstance(value, string_types) else value for value in value_set]
+        return parsed_value_set
+
     @DocInherit
     @MetaPandasDataset.column_map_expectation
     def expect_column_values_to_be_in_set(self, column, value_set,
@@ -558,7 +563,7 @@ class PandasDataset(MetaPandasDataset, pd.DataFrame):
                                           parse_strings_as_datetimes=None,
                                           result_format=None, include_config=False, catch_exceptions=None, meta=None):
         if parse_strings_as_datetimes:
-            parsed_value_set = [parse(value) if isinstance(value, string_types) else value for value in value_set]
+            parsed_value_set = self._parse_value_set(value_set)
         else:
             parsed_value_set = value_set
         return column.map(lambda x: x in parsed_value_set)
@@ -567,8 +572,13 @@ class PandasDataset(MetaPandasDataset, pd.DataFrame):
     @MetaPandasDataset.column_map_expectation
     def expect_column_values_to_not_be_in_set(self, column, value_set,
                                               mostly=None,
+                                              parse_strings_as_datetimes=None,
                                               result_format=None, include_config=False, catch_exceptions=None, meta=None):
-        return column.map(lambda x: x not in value_set)
+        if parse_strings_as_datetimes:
+            parsed_value_set = self._parse_value_set(value_set)
+        else:
+            parsed_value_set = value_set
+        return column.map(lambda x: x not in parsed_value_set)
 
     @DocInherit
     @MetaPandasDataset.column_map_expectation
