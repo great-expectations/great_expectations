af4cfcbd6da323c5129f6be44e3fc1f3b78a8b55
James Campbell <james.p.campbell@gmail.com>
2019-05-06 11:53:10 -0400

    Add additional utility methods to data_context

--- great_expectations/data_context/base.py
+++ great_expectations/data_context/base.py
@@ -1,6 +1,9 @@
 import os
 import json
 from great_expectations.version import __version__
+from great_expectations.dataset import PandasDataset
+from great_expectations import read_csv
+from urllib.parse import urlparse
 
 class DataContext(object):
     """A generic DataContext, exposing the base API including constructor with `options` parameter, list_datasets,
@@ -60,8 +63,63 @@ class DataContext(object):
             if result['expectation_config']['expectation_type'] == 'expect_column_unique_value_count_to_be_between'\
                 and result['expectation_config']['kwargs']['column'] == 'patient_nbr':
                 self.validation_params = {
-                    "urn:great_expectations:validations:datasets:diabetes_data:expectations:expect_column_unique_value_count_to_be_between:columns:patient_nbr:result:observed_value": result['result']['observed_value']
+                    "urn:great_expectations:validations:datasets:source_diabetes_data:expectations:expect_column_unique_value_count_to_be_between:columns:patient_nbr:result:observed_value": result['result']['observed_value']
                 }
 
     def _compile(self):
         return NotImplementedError
+
+    def review_validation_result(self, url, failed_only=False):
+        url = url.strip()
+        if url.startswith("s3://"):
+            try:
+                import boto3
+                s3 = boto3.client('s3')
+            except ImportError:
+                raise ImportError("boto3 is required for retrieving a dataset from s3")
+        
+            parsed_url = urlparse(url)
+            bucket = parsed_url.netloc
+            key = parsed_url.path[1:]
+            
+            s3_response_object = s3.get_object(Bucket=bucket, Key=key)
+            object_content = s3_response_object['Body'].read()
+            
+            results_dict = json.loads(object_content)
+
+            if failed_only:
+                failed_results_list = [result for result in results_dict["results"] if not result["success"]]
+                results_dict["results"] = failed_results_list
+                return results_dict
+            else:
+                return results_dict
+        else:
+            raise ValueError("Only s3 urls are supported.")
+
+    def get_failed_dataset(self, validation_result, **kwargs):
+        try:
+            reference_url = validation_result["meta"]["dataset_reference"]
+        except KeyError:
+            raise ValueError("Validation result must have a dataset_reference in the meta object to fetch")
+        
+        if reference_url.startswith("s3://"):
+            try:
+                import boto3
+                s3 = boto3.client('s3')
+            except ImportError:
+                raise ImportError("boto3 is required for retrieving a dataset from s3")
+        
+            parsed_url = urlparse(reference_url)
+            bucket = parsed_url.netloc
+            key = parsed_url.path[1:]
+            
+            s3_response_object = s3.get_object(Bucket=bucket, Key=key)
+            if key.endswith(".csv"):
+                # Materialize as dataset
+                # TODO: check the associated config for the correct data_asset_type to use
+                return read_csv(s3_response_object['Body'], **kwargs)
+            else:
+                return s3_response_object['Body']
+
+        else:
+            raise ValueError("Only s3 urls are supported.")
