87da8d0be95fa1507be383a64e16f0e5786f540c
Abe <abegong@gmail.com>
2019-05-07 09:29:29 -0700

    Refactor view_models.page into a directory

---
+++ great_expectations/render/view_models/default/page/descriptive.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+from .base import FullPageHtmlRenderer
+from ..section import (
+    DescriptiveEvrColumnSectionRenderer,
+)
+
+
+class DescriptiveEvrPageRenderer(FullPageHtmlRenderer):
+    """Renders an EVR set as a standalone HTML file."""
+    @classmethod
+    def _validate_input(cls, evrs):
+        assert type(evrs) == list
+
+        return True
+
+    @classmethod
+    def render(cls, evrs):
+        cls._validate_input(evrs)
+        t = cls._get_template()
+
+        grouped_evrs = cls._group_evrs_by_columns(evrs)
+
+        sections = []
+        for group, evrs in grouped_evrs.items():
+            section_renderer = DescriptiveEvrColumnSectionRenderer(group, evrs)
+            sections.append(
+                section_renderer.render()
+            )
+
+        rendered_page = t.render(
+            **{
+                "sections": sections
+            })
+
+        return rendered_page
+
+    @classmethod
+    def _group_evrs_by_columns(cls, evrs_list):
+        column_evrs_dict = defaultdict(list)
+
+        for evr in evrs_list:
+            exp = evr["expectation_config"]
+            if "column" in exp["kwargs"]:
+                column_evrs_dict[exp["kwargs"]["column"]].append(evr)
+            else:
+                column_evrs_dict["NO_COLUMN"].append(evr)
+
+        return column_evrs_dict
