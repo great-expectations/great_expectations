c16c69a21baaee05fee56ba5c8978b955ca6dc34
Abe <abegong@gmail.com>
2019-05-07 06:46:48 -0700

    Refactor snippets. WIP

--- great_expectations/render/base.py
+++ great_expectations/render/base.py
@@ -1,23 +1,22 @@
 class Renderer(object):
-    def __init__(self, *args, **kwargs):
-        """
-        Note: This shouldn't get used much other than input validation. We expect most renderers to be stateless, single-use classes.
-        """
-        self.validate_input(*args, **kwargs)
 
-    def validate_input(self, *args, **kwargs):
+    @classmethod
+    def validate_input(cls, *args, **kwargs):
         raise NotImplementedError
 
-    def render(self):
-        pass
-
-class SingleExpectationRenderer(Renderer):
-    def __init__(self, expectation):
-        self.expectation = expectation
-
-    def validate_input(self, expectation):
-        return True
-
-    def render(self):
+    @classmethod
+    def _render_to_json(cls, *args, **kwargs):
         raise NotImplementedError
 
+    @classmethod
+    def render(cls, *args, **kwargs):
+        raise NotImplementedError
+
+
+class ViewModelRenderer(Renderer):
+
+    @classmethod
+    def render(cls, *args, **kwargs):
+        class.validate_input(*args, **kwargs)
+        class._render_to_json(*args, **kwargs)
+        raise NotImplementedError
---
+++ great_expectations/render/snippets/evr_graph.py
@@ -0,0 +1,3 @@
+import json
+
+from ..base import SnippetRenderer
