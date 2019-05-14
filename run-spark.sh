1151441b9cc23ec898ec1eb15a1e207af19f0492
James Campbell <james.p.campbell@gmail.com>
2019-05-14 11:36:21 -0400

    More test-runner consolidation

--- run-spark.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget http://apache.cs.utah.edu/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
-tar -xzvf spark-2.4.3-bin-hadoop2.7.tgz
-cd spark-2.4.3-bin-hadoop2.7 && ./sbin/start-master.sh &
