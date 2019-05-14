5a9000ce1b8574b0bd1960aadf0409945898464b
James Campbell <james.p.campbell@gmail.com>
2019-05-14 08:36:49 -0400

    Additional fix for positional args

--- great_expectations/data_context/spark_context.py
+++ great_expectations/data_context/spark_context.py
@@ -20,7 +20,7 @@ class SparkCSVDataContext(DataContext):
     def list_datasets(self):
         return os.listdir(self.directory)
 
-    def get_dataset(self, dataset_name, *args, caching=False, **kwargs):
+    def get_dataset(self, dataset_name, caching=False, **kwargs):
         # TODO: deal with passing kwargs to spark.read and don't hard code options
         df = self.spark.read.option('header', True).csv(os.path.join(self.directory, dataset_name))
         return SparkDFDataset(df, caching=caching)
