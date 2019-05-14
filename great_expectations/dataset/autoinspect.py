024a9db546ac023499a79b5298e41f724edd7f77
James Campbell <james.p.campbell@gmail.com>
2019-05-14 11:00:28 -0400

    Avoid hasattrs for betters python2/3 compatibility

--- great_expectations/dataset/autoinspect.py
+++ great_expectations/dataset/autoinspect.py
@@ -27,10 +27,10 @@ def columns_exist(inspect_dataset):
     Args:
         inspect_dataset (great_expectations.dataset): The dataset to inspect and to which to add expectations.
     """
-    if not hasattr(inspect_dataset, 'table_columns'):
+    table_columns = getattr(inspect_dataset, 'table_columns')
+    if table_columns is None:
         warnings.warn(
             "No columns list found in dataset; no autoinspection performed.")
-        return
     if not isinstance(inspect_dataset.table_columns[0], string_types):
         raise AutoInspectError("Unable to determine column names for this dataset.")
     create_multiple_expectations(inspect_dataset, inspect_dataset.table_columns, "expect_column_to_exist")
