6c797ff2cc60b197ae93aa6000c181282f325c1a
James Campbell <james.p.campbell@gmail.com>
2019-05-22 15:32:42 -0400

    Update new set expectations to refactored dataset design

--- great_expectations/dataset/sqlalchemy_dataset.py
+++ great_expectations/dataset/sqlalchemy_dataset.py
@@ -9,12 +9,7 @@ import warnings
 
 logger = logging.getLogger(__name__)
 
-try:
-    import sqlalchemy as sa
-    from sqlalchemy.engine import reflection
-except ImportError:
-    logger.error("Unable to load SqlAlchemy context; install optional sqlalchemy dependency for support")
-    raise
+import pandas as pd
 
 from dateutil.parser import parse
 from datetime import datetime
@@ -174,6 +169,14 @@ class SqlAlchemyDataset(MetaSqlAlchemyDataset):
 
     def __init__(self, table_name=None, engine=None, connection_string=None,
                  custom_sql=None, schema=None, *args, **kwargs):
+
+        try:
+            import sqlalchemy as sa
+            from sqlalchemy.engine import reflection
+        except ImportError:
+            logger.error("Unable to load SqlAlchemy context; install optional sqlalchemy dependency for support")
+            raise
+
         if table_name is None:
             raise ValueError("No table_name provided.")
 
@@ -236,7 +239,7 @@ class SqlAlchemyDataset(MetaSqlAlchemyDataset):
         element_count = count_results['element_count']
         null_count = count_results['null_count'] or 0
         return element_count - null_count
-
+ 
     def _get_column_sum(self, column):
         return self.engine.execute(
             sa.select([sa.func.sum(sa.column(column))]).select_from(
@@ -258,6 +261,20 @@ class SqlAlchemyDataset(MetaSqlAlchemyDataset):
             sa.select([sa.func.min(sa.column(column))]).select_from(
                 self._table)
         ).scalar()
+    
+    def _get_column_value_counts(self, column):
+        results = self.engine.execute(
+            sa.select([
+                sa.column(column).label("value"),
+                sa.func.count(sa.column(column)).label("count"),
+            ]).where(sa.column(column) != None) \
+              .group_by(sa.column(column)) \
+              .select_from(self._table)).fetchall()
+        return pd.Series(
+            [row[1] for row in results],
+            index=[row[0] for row in results],
+            name=column
+        )
 
     def _get_column_mean(self, column):
         return self.engine.execute(
@@ -293,6 +310,60 @@ class SqlAlchemyDataset(MetaSqlAlchemyDataset):
             # An odd number of column values, we can just take the center value
             column_median = column_values[1][0]  # True center value
         return column_median
+
+    def _get_column_hist(self, column, bins):
+        # TODO: this is **terribly** inefficient; consider refactor
+        """return a list of counts corresponding to bins"""
+        hist = []
+        for i in range(0, len(bins) - 1):
+            # all bins except last are half-open
+            if i == len(bins) - 2:
+                max_strictly = False
+            else:
+                max_strictly = True
+            hist.append(
+                self._get_column_count_in_range(column, min_val=bins[i], max_val=bins[i + 1], max_strictly=max_strictly)
+            )
+        return hist
+
+    def _get_column_count_in_range(self, column, min_val=None, max_val=None, min_strictly=False, max_strictly=True):
+        if min_val is None and max_val is None:
+            raise ValueError('Must specify either min or max value')
+        if min_val is not None and max_val is not None and min_val > max_val:
+            raise ValueError('Min value must be <= to max value')
+
+        min_condition = None
+        max_condition = None
+        if min_val is not None:
+            if min_strictly:
+                min_condition = sa.column(column) > min_val
+            else:
+                min_condition = sa.column(column) >= min_val
+        if max_val is not None:
+            if max_strictly:
+                max_condition = sa.column(column) < max_val
+            else:
+                max_condition = sa.column(column) <= max_val
+        
+        if min_condition is not None and max_condition is not None:
+            condition = sa.and_(min_condition, max_condition)
+        elif min_condition is not None:
+            condition = min_condition
+        else:
+            condition = max_condition
+
+        query = query = sa.select([
+                    sa.func.count((sa.column(column)))
+                ]) \
+                .where(
+                    sa.and_(
+                        sa.column(column) != None,
+                        condition
+                    )
+                ) \
+                .select_from(self._table)
+        
+        return self.engine.execute(query).scalar()
 
     def create_temporary_table(self, table_name, custom_sql):
         """
@@ -371,54 +442,6 @@ class SqlAlchemyDataset(MetaSqlAlchemyDataset):
         else:
             parsed_value_set = value_set
         return sa.column(column).notin_(tuple(parsed_value_set))
-
-    def get_distinct_values(self, column):
-        sql_results = self.engine.execute(
-            sa.select(
-                [sa.func.distinct(sa.column(column))]) \
-                    .where(sa.column(column) != None) \
-                    .select_from(self._table)).fetchall()
-        return set([val[0] for val in sql_results])
-
-    @DocInherit
-    @MetaSqlAlchemyDataset.column_aggregate_expectation
-    def expect_column_distinct_values_to_contain_set(self, column, value_set,
-                                                     parse_strings_as_datetimes=None,
-                                                     result_format=None, include_config=False, catch_exceptions=None, meta=None):
-        if parse_strings_as_datetimes:
-            parsed_value_set = self._parse_value_set(value_set)
-        else:
-            parsed_value_set = value_set
-
-        expected_value_set = set(parsed_value_set)
-        observed_value_set = self.get_distinct_values(column)
-
-        return {
-            "success": observed_value_set.issuperset(expected_value_set),
-            "result": {
-                "observed_value": sorted(list(observed_value_set))
-            }
-        }
-
-    @DocInherit
-    @MetaSqlAlchemyDataset.column_aggregate_expectation
-    def expect_column_distinct_values_to_equal_set(self, column, value_set,
-                                                   parse_strings_as_datetimes=None,
-                                                   result_format=None, include_config=False, catch_exceptions=None, meta=None):
-        if parse_strings_as_datetimes:
-            parsed_value_set = self._parse_value_set(value_set)
-        else:
-            parsed_value_set = value_set
-
-        expected_value_set = set(parsed_value_set)
-        observed_value_set = self.get_distinct_values(column)
-
-        return {
-            "success": observed_value_set == expected_value_set,
-            "result": {
-                "observed_value": sorted(list(observed_value_set))
-            }
-        }
 
     @DocInherit
     @MetaSqlAlchemyDataset.column_map_expectation
