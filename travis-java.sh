9d6dbf79fa09cbbbf7ddffae64e930a776da2b07
James Campbell <james.p.campbell@gmail.com>
2019-05-25 15:35:13 -0400

    Another travis script tweak

--- travis-java.sh
+++ travis-java.sh
@@ -15,7 +15,10 @@ java -version
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -qq update
 sudo apt-get install -y openjdk-8-jdk --no-install-recommends
+sudp update-java-alternatives -l
 sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+ls -l /usr/lib/jvm/
+ls -l /usr/lib/jvm/java-8-openjdk-amd64
 
 # change JAVA_HOME to Java 8
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
