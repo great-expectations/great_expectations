ab477f97e389ab7b678c8c976c20340facb792cc
James Campbell <james.p.campbell@gmail.com>
2019-05-28 17:20:37 -0400

    More travis tweaks

--- travis-java.sh
+++ travis-java.sh
@@ -15,15 +15,16 @@ java -version
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -qq update
 sudo apt-get install -y openjdk-8-jdk --no-install-recommends
-# sudp update-java-alternatives -l
-# sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+sudo update-java-alternatives -l
+sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
 ls -l /usr/lib/jvm/
 ls -l /usr/lib/jvm/java-8-openjdk-amd64
-source /opt/jdk_switcher/jdk_switcher
-jdk_switcher use openjdk8
+# source /opt/jdk_switcher/jdk_switcher
+/opt/jdk_switcher/jdk_switcher use openjdk8
 
 # change JAVA_HOME to Java 8
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo "Current JAVA_HOME: $JAVA_HOME"
 echo "Current java -version:"
+source ~/.bash_profile.rc
 java -version
