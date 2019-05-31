e2908c2368a1b3eaf06b84e28e56f8908a439c11
James Campbell <james.p.campbell@gmail.com>
2019-05-31 13:55:22 -0400

    Update SparkContext to SparkSource

--- great_expectations/data_context/datasource/databricks_generator.py
+++ great_expectations/data_context/datasource/databricks_generator.py
@@ -1,7 +1,8 @@
+import datetime
 import logging
 
-from .base import DataContext
-from ..dataset.sparkdf_dataset import SparkDFDataset
+from .batch_generator import BatchGenerator
+from ...dataset.sparkdf_dataset import SparkDFDataset
 
 logger = logging.getLogger(__name__)
 
@@ -11,30 +12,33 @@ except ImportError:
     logger.error("Unable to load spark context; install optional spark dependency for support.")
     raise
 
-class DatabricksTableContext(DataContext):
+class DatabricksTableGenerator(BatchGenerator):
     """Meant to be used in a Databricks notebook
 
     Unsure of if we want to keep this and other new, more esoteric data contexts in the
     main library or provide an easy way for people to write their own.
     """
 
-    def __init__(self, options, *args, **kwargs):
-        super(DatabricksTableContext, self).__init__(options, *args, **kwargs)
+    def __init__(self, name, type_, datasource, database):
+        super(DatabricksTableGenerator, self).__init__(name, type_, datasource)
         # this should grab the already instantiated SparkSession available on Databricks notebooks
-        self.spark = SparkSession.builder.getOrCreate()
+        self.spark = datasource.spark
+        self.database = database
 
-    def connect(self, options):
-        self.database = options
-
-    def list_datasets(self):
+    def list_data_asset_names(self):
         tables = self.spark.sql('show tables in {}'.format(self.database))
         return [row.tableName for row in tables.collect()]
 
-    def get_dataset(self, dataset_name, caching=False, **kwargs):
-        query = 'select * from {}.{}'.format(self.database, dataset_name)
+
+    def _get_iterator(self, data_asset_name, **kwargs):
+        query = 'select * from {}.{}'.format(self.database, data_asset_name)
         if kwargs.get('partition'):
             if not kwargs.get('date_field'):
                 raise Exception('Must specify date_field when using partition.')
             query += ' where {} = "{}"'.format(kwargs.get('date_field'), kwargs.get('partition'))
-        df = self.spark.sql(query)
-        return SparkDFDataset(df, caching=caching)
+        return iter(
+            {
+                "query": query,
+                "timestamp": datetime.datetime.now().timestamp()
+            }
+        )
