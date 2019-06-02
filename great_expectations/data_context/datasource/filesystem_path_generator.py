6946e13669efbfb13a28d551c4347fe3bf3b0544
Eugene Mandel <eugene@superconductivehealth.com>
2019-06-02 11:45:21 -0700

    The pandas data source (and its filesys_path_generator) must be ready for the base directory specified by the user to be a relative path. In this case we will store the original relative path in the config. In runtime filesys_path_generator will interpret the path as relative to the data context's root directory (parent of great_expectations dir). Added a note to the CLI to inform the user that both absolute and relative paths are OK to enter.

--- great_expectations/data_context/datasource/filesystem_path_generator.py
+++ great_expectations/data_context/datasource/filesystem_path_generator.py
@@ -14,7 +14,7 @@ class FilesystemPathGenerator(BatchGenerator):
 
     def list_data_asset_names(self):
         known_assets = []
-        file_options = os.listdir(self._base_directory)
+        file_options = os.listdir(self._get_current_base_directory())
         for file_option in file_options:
             if file_option.endswith(".csv"):
                 known_assets.append(file_option[:-4])
@@ -25,18 +25,18 @@ class FilesystemPathGenerator(BatchGenerator):
     def _get_iterator(self, data_asset_name):
         # If the data_asset_name is a file, then return the path.
         # Otherwise, use files in a subdir as batches
-        if os.path.isdir(os.path.join(self._base_directory, data_asset_name)):
-            return self._build_batch_kwargs_path_iter(os.scandir(os.path.join(self._base_directory, data_asset_name)))
-        elif os.path.isfile(os.path.join(self._base_directory, data_asset_name)):
+        if os.path.isdir(os.path.join(self._get_current_base_directory(), data_asset_name)):
+            return self._build_batch_kwargs_path_iter(os.scandir(os.path.join(self._get_current_base_directory(), data_asset_name)))
+        elif os.path.isfile(os.path.join(self._get_current_base_directory(), data_asset_name)):
             return iter([
                 {
-                    "path": os.path.join(self._base_directory, data_asset_name)
+                    "path": os.path.join(self._get_current_base_directory(), data_asset_name)
                 }
                 ])
-        elif os.path.isfile(os.path.join(self._base_directory, data_asset_name + ".csv")):
+        elif os.path.isfile(os.path.join(self._get_current_base_directory(), data_asset_name + ".csv")):
             return iter([
                 {
-                    "path": os.path.join(self._base_directory, data_asset_name + ".csv")
+                    "path": os.path.join(self._get_current_base_directory(), data_asset_name + ".csv")
                 }
                 ])
         else:
@@ -50,3 +50,11 @@ class FilesystemPathGenerator(BatchGenerator):
                 }
         except StopIteration:
             return
+
+    # If base directory is a relative path, interpret it as relative to the data context's
+    # context root directory (parent directory of great_expectation dir)
+    def _get_current_base_directory(self):
+        if os.path.isabs(self._base_directory):
+            return self._base_directory
+        else:
+            return os.path.join(self._datasource.get_data_context().get_context_root_directory(), self._base_directory)
