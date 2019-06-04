e5522d5fe885e93d5c56793e0679e90156757c73
James Campbell <james.p.campbell@gmail.com>
2019-06-04 13:28:12 -0400

    Add support for reader_options to spark_source

--- great_expectations/data_context/datasource/spark_source.py
+++ great_expectations/data_context/datasource/spark_source.py
@@ -18,7 +18,7 @@ class SparkDFDatasource(Datasource):
     """For now, functions like PandasCSVDataContext
     """
 
-    def __init__(self, name="default", data_context=None, generators=None, **kwargs):
+    def __init__(self, name="default", data_context=None, generators=None, reader_options=None, **kwargs):
         if generators is None:
             # Provide a gentle way to build a datasource with a sane default, including ability to specify the base_directory
             base_directory = kwargs.pop("base_directory", "/data")
@@ -26,6 +26,11 @@ class SparkDFDatasource(Datasource):
                 "default": {"type": "filesystem", "base_directory": base_directory}
         }
         super(SparkDFDatasource, self).__init__(name, type_="spark", data_context=data_context, generators=generators)
+        self._datasource_config.update(
+            {
+                "reader_options": reader_options or {}
+            }
+        )
         try:
             self.spark = SparkSession.builder.getOrCreate()
         except Exception:
@@ -50,7 +55,10 @@ class SparkDFDatasource(Datasource):
 
         if "path" in batch_kwargs:
             reader = self.spark.read
-            for option in kwargs.items():
+            all_reader_options = dict(**self._datasource_config["reader_options"])
+            all_reader_options.update(**kwargs)
+
+            for option in all_reader_options.items():
                 reader = reader.option(*option)
             df = reader.csv(os.path.join(batch_kwargs["path"]))
 
