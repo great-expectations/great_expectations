d92db7553646600a2b251d9872554779c4c92971
Christian Selig <cselig@uw.edu>
2019-06-03 21:43:22 -0700

    fix ordering issue when reading json test config

--- tests/test_dataset_implementations/test_dataset_implementations.py
+++ tests/test_dataset_implementations/test_dataset_implementations.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import OrderedDict
 
 from ..test_utils import CONTEXTS, get_dataset, candidate_getter_is_on_temporary_notimplemented_list
 
@@ -9,7 +10,7 @@ import numpy as np
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 test_config_path = os.path.join(dir_path, 'test_dataset_implementations.json')
-test_config = json.load(open(test_config_path))
+test_config = json.load(open(test_config_path), object_pairs_hook=OrderedDict)
 test_datasets = test_config['test_datasets']
 
 def generate_ids(test):
