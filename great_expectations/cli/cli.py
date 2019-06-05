31c9b1f31d0df47daffb0f7057c1994e95126dda
James Campbell <james.p.campbell@gmail.com>
2019-06-04 21:50:46 -0400

    PEP8 Fixes

--- great_expectations/cli/cli.py
+++ great_expectations/cli/cli.py
@@ -6,7 +6,6 @@ import os
 import json
 import logging
 import sys
-import glob
 
 from pyfiglet import figlet_format
 
@@ -15,14 +14,14 @@ try:
 except ImportError:
     colored = None
 
-from .supporting_methods import (_scaffold_directories_and_notebooks, script_relative_path)
-from ..data_context import DataContext
+from .supporting_methods import _scaffold_directories_and_notebooks
 from great_expectations import __version__, read_csv
 from great_expectations.dataset import Dataset, PandasDataset
 from great_expectations.data_asset import FileDataAsset
 from great_expectations.data_context import DataContext
 
 logger = logging.getLogger(__name__)
+
 
 def cli_message(string, color, font="big", figlet=False):
     if colored:
@@ -41,28 +40,31 @@ def cli():
     """great_expectations command-line interface"""
     pass
 
+
 @cli.command()
 @click.argument('dataset')
 @click.argument('expectations_config_file')
 @click.option('--evaluation_parameters', '-p', default=None,
-    help='Path to a file containing JSON object used to evaluate parameters in expectations config.')
+              help='Path to a file containing JSON object used to evaluate parameters in expectations config.')
 @click.option('--result_format', '-o', default="SUMMARY",
-    help='Result format to use when building evaluation responses.')
+              help='Result format to use when building evaluation responses.')
 @click.option('--catch_exceptions', '-e', default=True, type=bool,
-    help='Specify whether to catch exceptions raised during evaluation of expectations (defaults to True).')
+              help='Specify whether to catch exceptions raised during evaluation of expectations (defaults to True).')
 @click.option('--only_return_failures', '-f', default=False, type=bool,
-    help='Specify whether to only return expectations that are not met during evaluation (defaults to False).')
+              help='Specify whether to only return expectations that are not met during evaluation \
+              (defaults to False).')
 @click.option('--custom_dataset_module', '-m', default=None,
-    help='Path to a python module containing a custom dataset class.')
+              help='Path to a python module containing a custom dataset class.')
 @click.option('--custom_dataset_class', '-c', default=None,
-    help='Name of the custom dataset class to use during evaluation.')
+              help='Name of the custom dataset class to use during evaluation.')
 def validate(dataset, expectations_config_file, evaluation_parameters, result_format, 
              catch_exceptions, only_return_failures, custom_dataset_module, custom_dataset_class):
     """Validate a CSV file against an expectations configuration.
 
     DATASET: Path to a file containing a CSV file to validate using the provided expectations_config_file.
 
-    EXPECTATIONS_CONFIG_FILE: Path to a file containing a valid great_expectations expectations config to use to validate the data.
+    EXPECTATIONS_CONFIG_FILE: Path to a file containing a valid great_expectations expectations config to use to \
+validate the data.
     """
 
     """
@@ -86,11 +88,12 @@ def validate(dataset, expectations_config_file, evaluation_parameters, result_fo
             custom_dataset_module))
         module_name = os.path.basename(
             custom_dataset_module).split('.')[0]
-        custom_module = __import__(module_name)
+        custom_module = __import__(str(module_name))
         dataset_class = getattr(
             custom_module, custom_dataset_class)
     elif "data_asset_type" in expectations_config:
-        if expectations_config["data_asset_type"] == "Dataset" or expectations_config["data_asset_type"] == "PandasDataset":
+        if (expectations_config["data_asset_type"] == "Dataset" or
+                expectations_config["data_asset_type"] == "PandasDataset"):
             dataset_class = PandasDataset
         elif expectations_config["data_asset_type"].endswith("Dataset"):
             logger.info("Using PandasDataset to validate dataset of type %s." %
@@ -99,8 +102,8 @@ def validate(dataset, expectations_config_file, evaluation_parameters, result_fo
         elif expectations_config["data_asset_type"] == "FileDataAsset":
             dataset_class = FileDataAsset
         else:
-            logger.critical("Unrecognized data_asset_type %s. You may need to specifcy custom_dataset_module and custom_dataset_class." %
-                            expectations_config["data_asset_type"])
+            logger.critical("Unrecognized data_asset_type %s. You may need to specifcy custom_dataset_module and \
+                custom_dataset_class." % expectations_config["data_asset_type"])
             return -1
     else:
         dataset_class = PandasDataset
@@ -124,7 +127,7 @@ def validate(dataset, expectations_config_file, evaluation_parameters, result_fo
 
 @cli.command()
 @click.option('--target_directory', '-d', default="./",
-    help='The root of the project directory where you want to initialize Great Expectations.')
+              help='The root of the project directory where you want to initialize Great Expectations.')
 def init(target_directory):
     """Initialze a new Great Expectations project.
     
@@ -189,15 +192,16 @@ Before we point you to the right notebook, what data does your project work with
 #     """
 
 
-    msg_prompt_dbt_choose_profile = """
-Please specify the name of the dbt profile (from your ~/.dbt/profiles.yml file Great Expectations should use to connect to the database    
-    """
+#     msg_prompt_dbt_choose_profile = """
+# Please specify the name of the dbt profile (from your ~/.dbt/profiles.yml file Great Expectations \
+# should use to connect to the database
+#     """
 
-    msg_dbt_go_to_notebook = """
-To create expectations for your dbt models start Jupyter and open notebook
-great_expectations/notebooks/using_great_expectations_with_dbt.ipynb - 
-it will walk you through next steps. 
-    """
+#     msg_dbt_go_to_notebook = """
+# To create expectations for your dbt models start Jupyter and open notebook
+# great_expectations/notebooks/using_great_expectations_with_dbt.ipynb -
+# it will walk you through next steps.
+#     """
 
     msg_prompt_filesys_enter_base_path = """
 Enter the path of the root directory where the data files are stored 
@@ -244,11 +248,7 @@ great_expectations/notebooks/using_great_expectations_with_pandas.ipynb -
 it will walk you through configuring the database connection and next steps. 
      """
 
-    project_yml_filename = os.path.join(target_directory,
-                                        "great_expectations/great_expectations.yml")
     base_dir = os.path.join(target_directory, "great_expectations")
-    sql_alchemy_profile = None
-    dbt_profile = None
 
     cli_message("Great Expectations", color="cyan", figlet=True)
 
@@ -261,13 +261,15 @@ it will walk you through configuring the database connection and next steps.
 
     _scaffold_directories_and_notebooks(base_dir)
     cli_message(
-        "\nDone. Later you can check out great_expectations/great_expectations.yml config file for useful options.", color="blue")
+        "\nDone. Later you can check out great_expectations/great_expectations.yml config file for useful options.",
+        color="blue")
 
     context = DataContext('.')
 
     # Shows a list of options to select from
 
-    data_source_selection = click.prompt(msg_prompt_choose_data_source, type=click.Choice(["1", "2", "3", "4"]), show_choices=False)
+    data_source_selection = click.prompt(msg_prompt_choose_data_source, type=click.Choice(["1", "2", "3", "4"]),
+                                         show_choices=False)
 
     print(data_source_selection)
 
@@ -275,8 +277,12 @@ it will walk you through configuring the database connection and next steps.
     #     dbt_profile = click.prompt(msg_prompt_dbt_choose_profile)
     #     log_message(msg_dbt_go_to_notebook, color="blue")
     #     context.add_datasource("dbt", "dbt", profile=dbt_profile)
-    if data_source_selection == "3": # Spark
-        path = click.prompt(msg_prompt_filesys_enter_base_path, default='/data/', type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True), show_default=True)
+    if data_source_selection == "3":  # Spark
+        path = click.prompt(msg_prompt_filesys_enter_base_path, default='/data/', type=click.Path(exists=True,
+                                                                                                  file_okay=False,
+                                                                                                  dir_okay=True,
+                                                                                                  readable=True),
+                            show_default=True)
 
         default_data_source_name = os.path.basename(path)
         data_source_name = click.prompt(msg_prompt_datasource_name, default=default_data_source_name, show_default=True)
@@ -284,18 +290,23 @@ it will walk you through configuring the database connection and next steps.
         cli_message(msg_filesys_go_to_notebook, color="blue")
         context.add_datasource(data_source_name, "spark", base_directory=path)
 
-
-    elif data_source_selection == "2": #sqlalchemy
+    elif data_source_selection == "2":  # sqlalchemy
         data_source_name = click.prompt(msg_prompt_datasource_name, default="mydb", show_default=True)
 
         cli_message(msg_sqlalchemy_config_connection.format(data_source_name), color="blue")
 
-        drivername = click.prompt("What is the driver for the sqlalchemy connection?", default="postgres", show_default=True)
-        host = click.prompt("What is the host for the sqlalchemy connection?", default="localhost", show_default=True)
-        port = click.prompt("What is the port for the sqlalchemy connection?", default="5432", show_default=True)
-        username = click.prompt("What is the username for the sqlalchemy connection?", default="postgres", show_default=True)
-        password = click.prompt("What is the password for the sqlalchemy connection?", default="", show_default=False, hide_input=True)
-        database = click.prompt("What is the database name for the sqlalchemy connection?", default="postgres", show_default=True)
+        drivername = click.prompt("What is the driver for the sqlalchemy connection?", default="postgres",
+                                  show_default=True)
+        host = click.prompt("What is the host for the sqlalchemy connection?", default="localhost",
+                            show_default=True)
+        port = click.prompt("What is the port for the sqlalchemy connection?", default="5432",
+                            show_default=True)
+        username = click.prompt("What is the username for the sqlalchemy connection?", default="postgres",
+                                show_default=True)
+        password = click.prompt("What is the password for the sqlalchemy connection?", default="",
+                                show_default=False, hide_input=True)
+        database = click.prompt("What is the database name for the sqlalchemy connection?", default="postgres",
+                                show_default=True)
 
         credentials = {
             "drivername": drivername,
@@ -311,9 +322,12 @@ it will walk you through configuring the database connection and next steps.
 
         context.add_datasource(data_source_name, "sqlalchemy", profile=data_source_name)
 
-
-    elif data_source_selection == "1": # csv
-        path = click.prompt(msg_prompt_filesys_enter_base_path, default='/data/', type=click.Path(exists=False, file_okay=False, dir_okay=True, readable=True), show_default=True)
+    elif data_source_selection == "1":  # csv
+        path = click.prompt(msg_prompt_filesys_enter_base_path, default='/data/', type=click.Path(exists=False,
+                                                                                                  file_okay=False,
+                                                                                                  dir_okay=True,
+                                                                                                  readable=True),
+                            show_default=True)
 
         default_data_source_name = os.path.basename(path)
         data_source_name = click.prompt(msg_prompt_datasource_name, default=default_data_source_name, show_default=True)
@@ -324,6 +338,7 @@ it will walk you through configuring the database connection and next steps.
     else:
         cli_message(msg_unknown_data_source, color="blue")
 
+
 def main():
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
@@ -332,6 +347,7 @@ def main():
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     cli()
+
 
 if __name__ == '__main__':
     main()
