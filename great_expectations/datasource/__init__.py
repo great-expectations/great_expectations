426c11fa09d37a7a883f8dbc6bcdd82bc4134713
James Campbell <james.p.campbell@gmail.com>
2019-06-04 21:55:06 -0400

    Refactor PandasCSVDatasource to PandasDatasource

--- great_expectations/datasource/__init__.py
+++ great_expectations/datasource/__init__.py
@@ -1,2 +1,2 @@
 from .spark_source import SparkDFDatasource
-from .pandas_source import PandasCSVDatasource, FilesystemPathGenerator
+from .pandas_source import PandasDatasource, FilesystemPathGenerator
