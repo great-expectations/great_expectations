e79d9628146d91229ba973a45e239d8dd108f6ea
James Campbell <james.p.campbell@gmail.com>
2019-06-06 22:45:17 -0700

    Bullet_list renderer implementation

--- great_expectations/render/renderer/content_block.py
+++ great_expectations/render/renderer/content_block.py
@@ -2,26 +2,40 @@ from .renderer import Renderer
 
 class ContentBlock(Renderer):
     
+    _content_block_type = "text"
+
     @classmethod
-    def validate_input(cls, ge_object):
+    def validate_input(cls, render_object):
         pass
 
     @classmethod
-    def render(cls, ge_object, result_key):
-        cls.validate_input(ge_object)
-        expectation_type = cls._get_expectation_type(ge_object)
+    def render(cls, render_object, **kwargs):
+        cls.validate_input(render_object)
+        object_type, data_asset_name = cls._find_ge_object_type(render_object)
 
-        content_block_fn = getattr(cls, expectation_type, None)
-        if content_block_fn is not None:
-            return content_block_fn(ge_object, result_key)
+        if object_type.startswith("list"):
+            blocks = []
+            for obj_ in render_object:
+                expectation_type = cls._get_expectation_type(obj_)
+
+                content_block_fn = getattr(cls, expectation_type, None)
+                if content_block_fn is not None:
+                    blocks += content_block_fn(render_object, **kwargs)
+            return {
+                "content_block_type": cls._content_block_type,
+                cls._content_block_type: blocks
+            }
         else:
-            return None
+            expectation_type = cls._get_expectation_type(render_object)
+
+            content_block_fn = getattr(cls, expectation_type, None)
+            if content_block_fn is not None:
+                return content_block_fn(render_object, **kwargs)
+            else:
+                return None
 
 class HeaderContentBlock(ContentBlock):
     pass
 
 class ColumnTypeContentBlock(ContentBlock):
-    pass
-
-class BulletListContentBlock(ContentBlock):
     pass
