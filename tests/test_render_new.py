155b2183301d6f06015674275cc3c95a8d2c83fb
Abe <abegong@gmail.com>
2019-06-08 07:26:12 -0700

    Re-up basic OOBE flow

--- tests/test_render_new.py
+++ tests/test_render_new.py
@@ -2,30 +2,36 @@ import pytest
 
 import json
 
+import great_expectations as ge
 from great_expectations.render.renderer import DescriptivePageRenderer, DescriptiveColumnSectionRenderer, PrescriptiveColumnSectionRenderer
 from great_expectations.render.view import DescriptivePageView
 from great_expectations.render.renderer.content_block import ValueListContentBlock
+
 
 @pytest.fixture()
 def validation_results():
     with open("./tests/test_sets/expected_cli_results_default.json", "r") as infile:
         return json.load(infile)
 
+
 @pytest.fixture()
 def expectations():
     with open("./tests/test_sets/titanic_expectations.json", "r") as infile:
         return json.load(infile)
+
 
 def test_render_descriptive_page_renderer(validation_results):
     print(json.dumps(DescriptivePageRenderer.render(validation_results), indent=2))
     # TODO: Use above print to set up snapshot test once we like the result
     assert True
 
+
 def test_render_descriptive_page_view(validation_results):
     renderer = DescriptivePageRenderer.render(validation_results)
     print(DescriptivePageView.render(renderer))
     # TODO: Use above print to set up snapshot test once we like the result
     assert True
+
 
 def test_render_descriptive_column_section_renderer(validation_results):
     # Group EVRs by column
@@ -40,7 +46,8 @@ def test_render_descriptive_column_section_renderer(validation_results):
             pass
 
     for column in evrs.keys():
-        print(json.dumps(DescriptiveColumnSectionRenderer.render(evrs[column]), indent=2))
+        print(json.dumps(DescriptiveColumnSectionRenderer.render(
+            evrs[column]), indent=2))
     # TODO: Use above print to set up snapshot test once we like the result
     assert True
 
@@ -60,7 +67,8 @@ def test_render_prescriptive_column_section_renderer(expectations):
 
     for column in exp_groups.keys():
         print(column)
-        print(json.dumps(PrescriptiveColumnSectionRenderer.render(exp_groups[column]), indent=2))
+        print(json.dumps(PrescriptiveColumnSectionRenderer.render(
+            exp_groups[column]), indent=2))
     # TODO: Use above print to set up snapshot test once we like the result
     assert True
 
@@ -68,3 +76,19 @@ def test_render_prescriptive_column_section_renderer(expectations):
 def test_content_block_list_available_expectations(expectations):
     available_expectations = ValueListContentBlock.list_available_expectations()
     assert available_expectations == ['expect_column_values_to_be_in_set']
+
+
+def test_full_oobe_flow():
+    df = ge.read_csv("examples/data/Titanic.csv")
+    # df = ge.read_csv("examples/data/Meteorite_Landings.csv")
+    df.autoinspect(ge.dataset.autoinspect.pseudo_pandas_profiling)
+    # df.autoinspect(ge.dataset.autoinspect.columns_exist)
+    evrs = df.validate()  # ["results"]
+    # print(json.dumps(evrs, indent=2))
+
+    rendered_json = DescriptivePageRenderer.render(evrs)
+    rendered_page = DescriptivePageView.render(rendered_json)
+    assert rendered_page != None
+
+    with open('./test.html', 'w') as f:
+        f.write(rendered_page)
