51aed8231c7a9984ff86e720e186ababafa09839
Abe <abegong@gmail.com>
2019-06-12 14:24:37 -0700

    Put first info into meta objs

--- great_expectations/profile/base.py
+++ great_expectations/profile/base.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 from ..dataset import Dataset
 
 
@@ -8,9 +11,25 @@ class DataSetProfiler(object):
         return issubclass(type(dataset), Dataset)
 
     @classmethod
+    def add_meta(cls, expectations_config):
+        if not "meta" in expectations_config:
+            expectations_config["meta"] = {}
+
+        class_name = str(cls.__name__)
+        expectations_config["meta"][class_name] = {
+            "created_by": class_name,
+            "created_at": time.time(),
+            # "batch_kwargs": {},
+        }
+
+        print(expectations_config["meta"])
+        return expectations_config
+
+    @classmethod
     def profile(cls, dataset):
         assert cls.validate_dataset(dataset)
         expectations_config = cls._profile(dataset)
+        expectations_config = cls.add_meta(expectations_config)
         validation_results = None  # dataset.validate(expectations_config)
         return expectations_config, validation_results
 
