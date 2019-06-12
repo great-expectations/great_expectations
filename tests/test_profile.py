5cd2b421999ba35beff88c170b1431df19cdb580
Abe <abegong@gmail.com>
2019-06-11 21:45:26 -0700

    Implement ColumnsExistProfiler with basic tests

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -4,6 +4,7 @@ import json
 
 from great_expectations.profile.base import DataSetProfiler
 from great_expectations.profile.pseudo_pandas_profiling import PseudoPandasProfiler
+from great_expectations.profile.columns_exist import ColumnsExistProfiler
 from great_expectations.dataset.pandas_dataset import PandasDataset
 
 
@@ -15,6 +16,29 @@ def test_DataSetProfiler_methods():
 
     with pytest.raises(NotImplementedError) as e_info:
         DataSetProfiler.profile(toy_dataset)
+
+
+def test_ColumnsExistProfiler():
+    toy_dataset = PandasDataset({"x": [1, 2, 3]})
+    results = ColumnsExistProfiler.profile(toy_dataset)
+
+    print(json.dumps(results, indent=2))
+
+    assert results == {
+        "data_asset_name": None,
+        "data_asset_type": "Dataset",
+        "meta": {
+            "great_expectations.__version__": "0.7.0-beta"
+        },
+        "expectations": [
+            {
+                "expectation_type": "expect_column_to_exist",
+                "kwargs": {
+                    "column": "x"
+                }
+            }
+        ]
+    }
 
 
 def test_PseudoPandasProfiler():
