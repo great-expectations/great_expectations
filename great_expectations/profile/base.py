9a45abedf9b1dd94a499ed1991d92e32a91b6282
Abe <abegong@gmail.com>
2019-06-12 17:12:08 -0700

    Get expectation-level meta configs in place

--- great_expectations/profile/base.py
+++ great_expectations/profile/base.py
@@ -11,6 +11,16 @@ class DataSetProfiler(object):
         return issubclass(type(dataset), Dataset)
 
     @classmethod
+    def add_expectation_meta(cls, expectation):
+        if not "meta" in expectation:
+            expectation["meta"] = {}
+
+        expectation["meta"][str(cls.__name__)] = {
+            "confidence": "very low"
+        }
+        return expectation
+
+    @classmethod
     def add_meta(cls, expectations_config, batch_kwargs=None):
         if not "meta" in expectations_config:
             expectations_config["meta"] = {}
@@ -24,7 +34,10 @@ class DataSetProfiler(object):
         if batch_kwargs != None:
             expectations_config["meta"][class_name]["batch_kwargs"] = batch_kwargs
 
-        print(expectations_config["meta"])
+        new_expectations = [cls.add_expectation_meta(
+            exp) for exp in expectations_config["expectations"]]
+        expectations_config["expectations"] = new_expectations
+
         return expectations_config
 
     @classmethod
