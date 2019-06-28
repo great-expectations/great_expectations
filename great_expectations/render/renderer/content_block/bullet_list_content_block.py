348c4a68aaa92855564fc02a33e45ef5f1c19050
Abe <abegong@gmail.com>
2019-06-27 21:45:49 -0400

    Standardize on a single return with template_str identified in the middle part of the function

--- great_expectations/render/renderer/content_block/bullet_list_content_block.py
+++ great_expectations/render/renderer/content_block/bullet_list_content_block.py
@@ -139,31 +139,19 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         )
 
         if (params["min_value"] is None) and (params["max_value"] is None):
-            return [{
-                "template": "$column has a bogus `expect_column_unique_value_count_to_be_between` expectation.",
-                "params": params,
-                "styling": styling,
-            }]
-
+            template_str = "$column has a bogus `expect_column_unique_value_count_to_be_between` expectation."
         elif params["min_value"] is None:
-            return [{
-                "template": "$column must have fewer than $max_value unique values.",
-                "params": params,
-                "styling": styling,
-            }]
-
+            template_str = "$column must have fewer than $max_value unique values."
         elif params["max_value"] is None:
-            return [{
-                "template": "$column must have at least $min_value unique values.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "$column must have at least $min_value unique values."
         else:
-            return [{
-                "template": "$column must have between $min_value and $max_value unique values.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "$column must have between $min_value and $max_value unique values."
+
+        return [{
+            "template": template_str,
+            "params": params,
+            "styling": styling,
+        }]
 
     # NOTE: This method is a pretty good example of good usage of `params`.
     @classmethod
@@ -175,55 +163,33 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
 
         if (params["min_value"] is None) and (params["max_value"] is None):
             params["expectation_name"] = "expect_column_values_to_be_between"
-            return [{
-                "template": "$column has a bogus $expectation_name expectation.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "$column has a bogus $expectation_name expectation."
 
         if "mostly" in params:
             if params["min_value"] is not None and params["max_value"] is not None:
-                return [{
-                    "template": "$column must be between $min_value and $max_value at least $mostly% of the time.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must be between $min_value and $max_value at least $mostly% of the time."
 
             elif params["min_value"] is None:
-                return [{
-                    "template": "$column must be less than $max_value at least $mostly% of the time.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must be less than $max_value at least $mostly% of the time."
 
             elif params["max_value"] is None:
-                return [{
-                    "template": "$column must be more than $min_value at least $mostly% of the time.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must be more than $min_value at least $mostly% of the time."
 
         else:
             if params["min_value"] is not None and params["max_value"] is not None:
-                return [{
-                    "template": "$column must always be between $min_value and $max_value.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must always be between $min_value and $max_value."
 
             elif params["min_value"] is None:
-                return [{
-                    "template": "$column must always be less than $max_value.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must always be less than $max_value."
 
             elif params["max_value"] is None:
-                return [{
-                    "template": "$column must always be more than $min_value.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "$column must always be more than $min_value."
+
+        return [{
+            "template": template_str,
+            "params": params,
+            "styling": styling,
+        }]
 
     @classmethod
     def expect_column_pair_values_A_to_be_greater_than_B(cls, expectation, styling=None, include_column_name=True):
@@ -234,39 +200,25 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         )
 
         if (params["column_A"] is None) or (params["column_B"] is None):
-            return [{
-                "template": "$column has a bogus `expect_column_pair_values_A_to_be_greater_than_B` expectation.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "$column has a bogus `expect_column_pair_values_A_to_be_greater_than_B` expectation."
 
         if params["mostly"] == None:
             if params["or_equal"] in [None, False]:
-                return [{
-                    "template": "Values in $column_A must always be greater than those in $column_B.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "Values in $column_A must always be greater than those in $column_B."
             else:
-                return [{
-                    "template": "Values in $column_A must always be greater than or equal to those in $column_B.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "Values in $column_A must always be greater than or equal to those in $column_B."
 
         else:
             if params["or_equal"] in [None, False]:
-                return [{
-                    "template": "Values in $column_A must be greater than those in $column_B at least $mostly % of the time.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "Values in $column_A must be greater than those in $column_B at least $mostly % of the time."
             else:
-                return [{
-                    "template": "Values in $column_A must be greater than or equal to those in $column_B at least $mostly % of the time.",
-                    "params": params,
-                    "styling": styling,
-                }]
+                template_str = "Values in $column_A must be greater than or equal to those in $column_B at least $mostly % of the time."
+
+        return [{
+            "template": template_str,
+            "params": params,
+            "styling": styling,
+        }]
 
     @classmethod
     def expect_column_pair_values_to_be_equal(cls, expectation, styling=None, include_column_name=True):
@@ -280,30 +232,22 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
 
         if (params["column_A"] is None) or (params["column_B"] is None):
             # FIXME: this string is wrong
-            return [{
-                "template": " has a bogus $expectation_name expectation.",
-                "params": {
-                    "expectation_name": "expect_column_pair_values_to_be_equal"
-                },
-                "styling": styling,
-            }]
+            template_str = " has a bogus $expectation_name expectation."
 
         if params["mostly"] == None:
-            return [{
-                "template": "Values in $column_A and $column_B must always be equal.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "Values in $column_A and $column_B must always be equal."
 
         else:
             # Note: this pattern for type conversion seems to work reasonably well.
             # Note: I'm not 100% sure that this is the right place to encode details like how many decimals to show.
             params["mostly_pct"] = "%.1f" % (params["mostly"]*100,)
-            return [{
-                "template": "Values in $column_A and $column_B must be equal at least $mostly_pct % of the time.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "Values in $column_A and $column_B must be equal at least $mostly_pct % of the time."
+
+        return [{
+            "template": template_str,
+            "params": params,
+            "styling": styling,
+        }]
 
     @classmethod
     def expect_table_columns_to_match_ordered_list(cls, expectation, styling=None, include_column_name=True):
@@ -315,8 +259,10 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         # FIXME: This is slightly wrong, since the whole string (including commas) will get syntax highlighting.
         # It would be better to have each element highlighted separately, but I need to research methods to do this elegantly.
         params["column_list_str"] = ", ".join(params["column_list"])
+        template_str = "This table should have these columns in this order: $column_list_str"
+
         return [{
-            "template": "This table should have these columns in this order: $column_list_str",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -331,8 +277,10 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         # FIXME: This is slightly wrong, since the whole string (including commas) will get syntax highlighting.
         # It would be better to have each element highlighted separately, but I need to research methods to do this elegantly.
         params["column_list_str"] = ", ".join(params["column_list"])
+        template_str = "Values must always be unique across columns: $column_list_str"
+
         return [{
-            "template": "Values must always be unique across columns: $column_list_str",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -345,25 +293,19 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         )
 
         if params["min_value"] is not None and params["max_value"] is not None:
-            return [{
-                "template": "Must have between $min_value and $max_value rows.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "Must have between $min_value and $max_value rows."
 
         elif params["min_value"] is None:
-            return [{
-                "template": "Must have less than than $max_value rows.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "Must have less than than $max_value rows."
 
         elif params["max_value"] is None:
-            return [{
-                "template": "Must have more than $min_value rows.",
-                "params": params,
-                "styling": styling,
-            }]
+            template_str = "Must have more than $min_value rows."
+
+        return [{
+            "template": template_str,
+            "params": params,
+            "styling": styling,
+        }]
 
     @classmethod
     def expect_table_row_count_to_equal(cls, expectation, styling=None, include_column_name=True):
@@ -372,9 +314,12 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
             ["value"]
         )
 
+        template_str = "Must have exactly $value rows."
+
         return [{
-            "template": "Must have exactly $value rows.",
-            "params": params
+            "template": template_str,
+            "params": params,
+            "styling": styling,
         }]
 
     @classmethod
@@ -390,8 +335,11 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         values_string = " ".join(
             ["$v__"+str(i) for i, v in enumerate(params["value_set"])]
         )
+
+        template_str = "$column values must belong to this set: "+values_string+"."
+
         return [{
-            "template": "$column values must belong to this set: "+values_string+".",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -423,8 +371,10 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
             ["column", "mostly"],
         )
 
+        template_str = "$column values must never be null."
+
         return [{
-            "template": "$column values must never be null.",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -437,8 +387,10 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
             ["column", "min_value", "max_value"],
         )
 
+        template_str = "$column must have between $min_value and $max_value% unique values."
+
         return [{
-            "template": "$column must have between $min_value and $max_value% unique values.",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -451,8 +403,10 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
             ["column", ],
         )
 
+        template_str = "$column values must be unique."
+
         return [{
-            "template": "$column values must be unique.",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
@@ -470,8 +424,11 @@ class PrescriptiveBulletListContentBlockRenderer(ContentBlockRenderer):
         values_string = " ".join(
             ["$v__"+str(i) for i, v in enumerate(params["type_list"])]
         )
+
+        template_str = "$column values must belong to this set: "+values_string+"."
+
         return [{
-            "template": "$column values must belong to this set: "+values_string+".",
+            "template": template_str,
             "params": params,
             "styling": styling,
         }]
