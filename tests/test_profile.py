12bc4cc1766e949565fad7e6797a89c27aef8dd8
James Campbell <james.p.campbell@gmail.com>
2019-07-03 10:47:46 -0400

    Update profile tests

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -16,6 +16,7 @@ from .test_utils import assertDeepAlmostEqual
 # test class-based profile methods
 
 
+# noinspection PyPep8Naming
 def test_DataSetProfiler_methods():
     toy_dataset = PandasDataset({"x": [1, 2, 3]})
 
@@ -26,37 +27,20 @@ def test_DataSetProfiler_methods():
         DatasetProfiler.profile(toy_dataset)
 
 
+# noinspection PyPep8Naming
 def test_ColumnsExistProfiler():
     toy_dataset = PandasDataset({"x": [1, 2, 3]})
+
     expectations_config, evr_config = ColumnsExistProfiler.profile(toy_dataset)
 
-    print(json.dumps(expectations_config, indent=2))
-    # FIXME: REVISIT THIS TEST FOR CONTENT
-    # TODO: REVISIT THIS TEST FOR CONTENT
-
-    # assert expectations_config == {
-    #     "data_asset_name": None,
-    #     "data_asset_type": "Dataset",
-    #     "meta": {
-    #         "great_expectations.__version__": "0.7.0-beta",
-    #         "ColumnsExistProfiler": {
-    #             "created_by": "BasicDatasetProfiler",
-    #             "created_at": 0,
-    #         },
-    #     },
-    #     "expectations": [
-    #         {
-    #             "expectation_type": "expect_column_to_exist",
-    #             "kwargs": {
-    #                 "column": "x"
-    #             }
-    #         }
-    #     ]
-    # }
+    assert len(expectations_config["expectations"]) == 1
+    assert expectations_config["expectations"][0]["expectation_type"] == "expect_column_to_exist"
+    assert expectations_config["expectations"][0]["kwargs"]["column"] == "x"
 
 
+# noinspection PyPep8Naming
 def test_BasicDatasetProfiler():
-    toy_dataset = PandasDataset({"x": [1, 2, 3]})
+    toy_dataset = PandasDataset({"x": [1, 2, 3]}, data_asset_name="toy_dataset")
     assert len(toy_dataset.get_expectation_suite(
         suppress_warnings=True)["expectations"]) == 0
 
@@ -67,46 +51,33 @@ def test_BasicDatasetProfiler():
     assert len(toy_dataset.get_expectation_suite(
         suppress_warnings=True)["expectations"]) > 0
 
-    # We should add an additional test that instantiates the batch via context, so the data_asset_name will be populated.
-    assert expectations_config["data_asset_name"] == None
+    assert expectations_config["data_asset_name"] == "toy_dataset"
     assert "BasicDatasetProfiler" in expectations_config["meta"]
-    # We should add an additional test that instantiates the batch via context, so that batch_kwargs will be populated.
+
     assert set(expectations_config["meta"]["BasicDatasetProfiler"].keys()) == {
         "created_by", "created_at"
     }
+
+    added_expectations = set()
     for exp in expectations_config["expectations"]:
+        added_expectations.add(exp["expectation_type"])
         assert "BasicDatasetProfiler" in exp["meta"]
-        assert exp["meta"]["BasicDatasetProfiler"] == {
-            "confidence": "very low"
-        }
+        assert "confidence" in exp["meta"]["BasicDatasetProfiler"]
 
-    # FIXME: REVISIT THIS TEST FOR CONTENT
-    # TODO: REVISIT THIS TEST FOR CONTENT
+    expected_expectations = {
+        'expect_table_row_count_to_be_between',
+        'expect_table_columns_to_match_ordered_list',
+        'expect_column_values_to_be_in_set',
+        'expect_column_unique_value_count_to_be_between',
+        'expect_column_proportion_of_unique_values_to_be_between',
+        'expect_column_values_to_not_be_null',
+        'expect_column_values_to_be_in_type_list',
+        'expect_column_values_to_be_unique'}
 
-
-    # Example:
-    # {
-    #     "data_asset_name": "notable_works_by_charles_dickens",
-    #     "meta": {
-    #         "great_expectations.__version__": "0.7.0-beta",
-    #         "BasicDatasetProfiler": {
-    #             "created_by": "BasicDatasetProfiler",
-    #             "created_at": 0,
-    #             "batch_kwargs": {},
-    #         },
-    #     },
-    #     "expectations": [
-    #         {
-    #             "expectation_type": "expect_column_to_exist",
-    #             "meta": {
-    #                 "BasicDatasetProfiler": {
-    #                     "confidence": "very low"
-    #                 }
-    #             }
-    #         }]
-    # }
+    assert expected_expectations.issubset(added_expectations)
 
 
+# noinspection PyPep8Naming
 def test_BasicDatasetProfiler_with_context(empty_data_context, filesystem_csv_2):
     empty_data_context.add_datasource(
         "my_datasource", "pandas", base_directory=str(filesystem_csv_2))
@@ -128,9 +99,7 @@ def test_BasicDatasetProfiler_with_context(empty_data_context, filesystem_csv_2)
 
     for exp in expectations_config["expectations"]:
         assert "BasicDatasetProfiler" in exp["meta"]
-        assert exp["meta"]["BasicDatasetProfiler"] == {
-            "confidence": "very low"
-        }
+        assert "confidence" in exp["meta"]["BasicDatasetProfiler"]
 
     assert validation_results["meta"]["data_asset_name"] == "my_datasource/default/f1"
     assert set(validation_results["meta"].keys()) == {
@@ -138,29 +107,31 @@ def test_BasicDatasetProfiler_with_context(empty_data_context, filesystem_csv_2)
     }
 
 
+# noinspection PyPep8Naming
 def test_context_profiler(empty_data_context, filesystem_csv_2):
+    """This just validates that it's possible to profile using the datasource hook, and have
+    validation results available in the DataContext"""
     empty_data_context.add_datasource(
         "my_datasource", "pandas", base_directory=str(filesystem_csv_2))
     not_so_empty_data_context = empty_data_context
 
     assert not_so_empty_data_context.list_expectation_suites() == {}
-    not_so_empty_data_context.profile_datasource("my_datasource")
+    not_so_empty_data_context.profile_datasource("my_datasource", profiler=BasicDatasetProfiler)
 
     assert "my_datasource" in not_so_empty_data_context.list_expectation_suites()
 
     profiled_expectations = not_so_empty_data_context.get_expectation_suite('f1', "BasicDatasetProfiler")
 
-    # FIXME: REVISIT THIS TEST FOR CONTENT
-    # TODO: REVISIT THIS TEST FOR CONTENT
+    print(json.dumps(profiled_expectations, indent=2))
+    for exp in profiled_expectations["expectations"]:
+        assert "BasicDatasetProfiler" in exp["meta"]
+        assert "confidence" in exp["meta"]["BasicDatasetProfiler"]
+
+    assert profiled_expectations["data_asset_name"] == "my_datasource/default/f1"
+    assert profiled_expectations["expectation_suite_name"] == "BasicDatasetProfiler"
+    assert "batch_kwargs" in profiled_expectations["meta"]["BasicDatasetProfiler"]
+
     assert len(profiled_expectations["expectations"]) > 0
-
-    # print(json.dumps(validation_results, indent=2))
-
-    # # Note: deliberately not testing context file storage in this test.
-    # context_expectations_config = not_so_empty_data_context.get_expectation_suite(
-    #     "my_datasource", "f1")
-
-    # assert context_expectations_config == profiled_expectations
 
 
 # noinspection PyPep8Naming
