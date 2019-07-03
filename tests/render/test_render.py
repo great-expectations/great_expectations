63379832060b72beb0b4b12dc22f6ef876a73ea5
James Campbell <james.p.campbell@gmail.com>
2019-07-03 14:14:14 -0400

    Clarify behavior in test_render

--- tests/render/test_render.py
+++ tests/render/test_render.py
@@ -39,19 +39,26 @@ def tetanus_varicella_basic_dataset_profiler_expectations():
         return json.load(infile, object_pairs_hook=OrderedDict)
 
 
-def test_render_descriptive_page_renderer(titanic_validation_results):
-    print(json.dumps(DescriptivePageRenderer.render(titanic_validation_results), indent=2))
-    # TODO: Use above print to set up snapshot test once we like the result
-    assert True
+@pytest.mark.smoketest
+def test_smoke_render_descriptive_page_renderer(titanic_validation_results):
+    rendered = DescriptivePageRenderer.render(titanic_validation_results)
+    with open('./tests/render/output/test_render_descriptive_page_renderer.json', 'w') as outfile:
+        json.dump(rendered, outfile, indent=2)
+
+    assert len(rendered["sections"]) > 5
 
 
+@pytest.mark.smoketest
 def test_render_descriptive_page_view(titanic_validation_results):
-    renderer = DescriptivePageRenderer.render(titanic_validation_results)
-    print(DefaultJinjaPageView.render(renderer))
-    # TODO: Use above print to set up snapshot test once we like the result
-    assert True
+    document = DescriptivePageRenderer.render(titanic_validation_results)
+    rendered = DefaultJinjaPageView.render(document)
+    with open('./tests/render/output/test_render_descriptive_page_view.html', 'w') as outfile:
+        outfile.write(rendered)
+
+    assert len(rendered) > 1000
 
 
+@pytest.mark.smoketest
 def test_render_descriptive_column_section_renderer(titanic_validation_results):
     # Group EVRs by column
     evrs = {}
@@ -65,12 +72,12 @@ def test_render_descriptive_column_section_renderer(titanic_validation_results):
             pass
 
     for column in evrs.keys():
-        print(json.dumps(DescriptiveColumnSectionRenderer.render(
-            evrs[column]), indent=2))
-    # TODO: Use above print to set up snapshot test once we like the result
-    assert True
+        with open('./tests/render/output/test_render_descriptive_column_section_renderer__' + column + '.json', 'w') \
+                as outfile:
+            json.dump(DescriptiveColumnSectionRenderer.render(evrs[column]), outfile, indent=2)
 
 
+@pytest.mark.smoketest
 def test_render_prescriptive_column_section_renderer(titanic_expectations):
     # Group expectations by column
     exp_groups = {}
@@ -85,67 +92,52 @@ def test_render_prescriptive_column_section_renderer(titanic_expectations):
             pass
 
     for column in exp_groups.keys():
-        print(column)
-        print(json.dumps(PrescriptiveColumnSectionRenderer.render(
-            exp_groups[column]), indent=2))
-    # TODO: Use above print to set up snapshot test once we like the result
-    assert True
+        with open('./tests/render/output/test_render_prescriptive_column_section_renderer' + column + '.json', 'w') \
+                as outfile:
+            json.dump(PrescriptiveColumnSectionRenderer.render(exp_groups[column]), outfile, indent=2)
 
 
-def test_content_block_list_available_expectations(titanic_expectations):
+def test_content_block_list_available_expectations():
     available_expectations = ValueListContentBlockRenderer.list_available_expectations()
     assert available_expectations == ['expect_column_values_to_be_in_set']
 
 
+@pytest.mark.smoketest
 def test_render_profiled_fixture_expectations(tetanus_varicella_basic_dataset_profiler_expectations):
-
     rendered_json = PrescriptivePageRenderer.render(tetanus_varicella_basic_dataset_profiler_expectations)
-
-    # print(json.dumps(rendered_json, indent=2))
-    # with open('./test.json', 'w') as f:
-    #     f.write(json.dumps(rendered_json, indent=2))
-
     rendered_page = DefaultJinjaPageView.render(rendered_json)
-    assert rendered_page != None
 
-    # print(rendered_page)
     with open('./tests/render/output/test_render_profiled_fixture_expectations.html', 'w') as f:
         f.write(rendered_page)
 
+    assert rendered_page[:15] == "<!DOCTYPE html>"
+    assert rendered_page[-7:] == "</html>"
 
+
+@pytest.mark.smoketest
 def test_render_profiled_fixture_evrs(tetanus_varicella_basic_dataset_profiler_evrs):
     rendered_json = DescriptivePageRenderer.render(tetanus_varicella_basic_dataset_profiler_evrs)
-
-    print(json.dumps(rendered_json, indent=2))
-    # with open('./test.json', 'w') as f:
-    #     f.write(json.dumps(rendered_json, indent=2))
-
     rendered_page = DefaultJinjaPageView.render(rendered_json)
-    assert rendered_page != None
 
-    # print(rendered_page)
     with open('./tests/render/output/test_render_profiled_fixture_evrs.html', 'w') as f:
         f.write(rendered_page)
 
-    # assert False
+    assert rendered_page[:15] == "<!DOCTYPE html>"
+    assert rendered_page[-7:] == "</html>"
 
 
+@pytest.mark.smoketest
 def test_full_oobe_flow():
     df = ge.read_csv("examples/data/Titanic.csv")
-    # df = ge.read_csv("examples/data/Meteorite_Landings.csv")
-    # df = ge.read_csv("examples/data/adult.data")
     df.profile(BasicDatasetProfiler)
-    # df.autoinspect(ge.dataset.autoinspect.columns_exist)
     evrs = df.validate()  # ["results"]
-    # print(json.dumps(evrs, indent=2))
 
     rendered_json = DescriptivePageRenderer.render(evrs)
-    # print(json.dumps(rendered_json, indent=2))
     rendered_page = DefaultJinjaPageView.render(rendered_json)
-    assert rendered_page != None
-    # print(rendered_page)
 
     with open('./tests/render/output/test_full_oobe_flow.html', 'w') as f:
         f.write(rendered_page)
 
-    # assert False
+    assert rendered_page[:15] == "<!DOCTYPE html>"
+    assert rendered_page[-7:] == "</html>"
+
