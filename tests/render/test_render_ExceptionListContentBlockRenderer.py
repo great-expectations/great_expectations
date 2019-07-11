b4dbfe3ca1057ecc2e5153ec5773ca636cabc159
James Campbell <james.p.campbell@gmail.com>
2019-07-11 12:19:25 -0400

    Update styling for rendering exceptions

--- tests/render/test_render_ExceptionListContentBlockRenderer.py
+++ tests/render/test_render_ExceptionListContentBlockRenderer.py
@@ -1,5 +1,5 @@
 from great_expectations.render.renderer.content_block import ExceptionListContentBlockRenderer
-
+import json
 
 def test_exception_list_content_block_renderer():
     # We should grab the exception message and add default formatting
@@ -17,34 +17,68 @@ def test_exception_list_content_block_renderer():
     )
 
     assert result == {
-        'content_block_type': 'bullet_list',
-        'bullet_list': [
+        "content_block_type": "bullet_list",
+        "bullet_list": [
             {
-                'template': '$column: $expectation_type raised an exception: $exception_message',
-                'params': {
-                    'column': 'answer',
-                    'expectation_type': 'expect_column_kl_divergence_to_be_less_than',
-                    'exception_message': 'Invalid partition object.'
+                "template": "$column: $expectation_type raised an exception: $exception_message",
+                "params": {
+                    "column": "answer",
+                    "expectation_type": "expect_column_kl_divergence_to_be_less_than",
+                    "exception_message": "Invalid partition object."
                 },
-                'styling': {
-                    'default': {
-                        'classes': ['badge', 'badge-secondary']
-                    },
-                    'params': {
-                        'column': {
-                            'classes': ['badge', 'badge-primary']
+                "styling": {
+                    "classes": [
+                        "list-group-item"
+                    ],
+                    "params": {
+                        "column": {
+                            "classes": [
+                                "badge",
+                                "badge-primary"
+                            ]
                         },
-                        'expectation_type': {
-                            'classes': ['text-monospace']
+                        "expectation_type": {
+                            "classes": [
+                                "text-monospace"
+                            ]
                         },
-                        'exception_message': {
-                            'classes': ['alert', 'alert-secondary', 'text-monospace']
+                        "exception_message": {
+                            "classes": [
+                                "text-monospace"
+                            ]
                         }
                     }
                 }
             }
         ],
-        'styling': {
-            'classes': ['col-12']
-        }
+        "styling": {
+            "classes": [
+                "col-12"
+            ],
+            "styles": {
+                "margin-top": "20px"
+            },
+            "header": {
+                "classes": [
+                    "collapsed"
+                ],
+                "attributes": {
+                    "data-toggle": "collapse",
+                    "href": "#{{content_block_id}}-body",
+                    "role": "button",
+                    "aria-expanded": "true",
+                    "aria-controls": "collapseExample"
+                },
+                "styles": {
+                    "cursor": "pointer"
+                }
+            },
+            "body": {
+                "classes": [
+                    "list-group",
+                    "collapse"
+                ]
+            }
+        },
+        "header": "Failed expectations <span class=\"mr-3 triangle\"></span>"
     }
