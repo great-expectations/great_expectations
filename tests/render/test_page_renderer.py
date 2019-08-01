405305fc75831d53bbbb8aa58b79369cc7763159
Abe <abegong@gmail.com>
2019-07-31 21:03:04 -0600

    Add test_page_renderer.py

---
+++ tests/render/test_page_renderer.py
@@ -0,0 +1,57 @@
+from great_expectations.render.renderer import (
+    PrescriptivePageRenderer,
+    DescriptivePageRenderer,
+)
+
+def test_render_asset_notes():
+    import pypandoc
+    # print(pypandoc.convert_text("*hi*", to='html', format="md"))
+
+    result = PrescriptivePageRenderer._render_asset_notes({
+        "meta" : {
+            "notes" : "*hi*"
+        }
+    })
+    print(result)
+    assert result["content"] == ["*hi*"]
+
+    result = PrescriptivePageRenderer._render_asset_notes({
+        "meta" : {
+            "notes" : ["*alpha*", "_bravo_", "charlie"]
+        }
+    })
+    print(result)
+    assert result["content"] == ["*alpha*", "_bravo_", "charlie"]
+
+    result = PrescriptivePageRenderer._render_asset_notes({
+        "meta" : {
+            "notes" : {
+                "format": "string",
+                "content": ["*alpha*", "_bravo_", "charlie"]
+            }
+        }
+    })
+    print(result)
+    assert result["content"] == ["*alpha*", "_bravo_", "charlie"]
+
+    result = PrescriptivePageRenderer._render_asset_notes({
+        "meta" : {
+            "notes" : {
+                "format": "markdown",
+                "content": "*alpha*"
+            }
+        }
+    })
+    print(result)
+    assert result["content"] == ["<p><em>alpha</em></p>\n"]
+
+    result = PrescriptivePageRenderer._render_asset_notes({
+        "meta" : {
+            "notes" : {
+                "format": "markdown",
+                "content": ["*alpha*", "_bravo_", "charlie"]
+            }
+        }
+    })
+    print(result)
+    assert result["content"] == ["<p><em>alpha</em></p>\n", "<p><em>bravo</em></p>\n", "<p>charlie</p>\n"]
