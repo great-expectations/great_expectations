646a9262ba004ea87272ad4b949c4b710809f00a
Rob Lim <robert.m.lim@gmail.com>
2019-08-06 11:13:30 -0700

    Update tests

--- tests/render/test_column_section_renderer.py
+++ tests/render/test_column_section_renderer.py
@@ -4,8 +4,8 @@ import json
 from collections import OrderedDict
 
 from great_expectations.render.renderer import (
-    PrescriptiveColumnSectionRenderer,
-    DescriptiveColumnSectionRenderer,
+    ExpectationSuiteColumnSectionRenderer,
+    ProfilingResultsColumnSectionRenderer,
 )
 from great_expectations.render.view import (
     DefaultJinjaPageView,
@@ -18,7 +18,7 @@ def titanic_expectations():
 
 
 @pytest.mark.smoketest
-def test_render_descriptive_column_section_renderer(titanic_validation_results):
+def test_render_profiling_results_column_section_renderer(titanic_validation_results):
     # Group EVRs by column
     evrs = {}
     for evr in titanic_validation_results["results"]:
@@ -31,13 +31,13 @@ def test_render_descriptive_column_section_renderer(titanic_validation_results):
             pass
 
     for column in evrs.keys():
-        with open('./tests/render/output/test_render_descriptive_column_section_renderer__' + column + '.json', 'w') \
+        with open('./tests/render/output/test_render_profiling_results_column_section_renderer__' + column + '.json', 'w') \
                 as outfile:
-            json.dump(DescriptiveColumnSectionRenderer.render(evrs[column]), outfile, indent=2)
+            json.dump(ProfilingResultsColumnSectionRenderer.render(evrs[column]), outfile, indent=2)
 
 
 @pytest.mark.smoketest
-def test_render_prescriptive_column_section_renderer(titanic_expectations):
+def test_render_expectation_suite_column_section_renderer(titanic_expectations):
     # Group expectations by column
     exp_groups = {}
     # print(json.dumps(titanic_expectations, indent=2))
@@ -51,19 +51,19 @@ def test_render_prescriptive_column_section_renderer(titanic_expectations):
             pass
 
     for column in exp_groups.keys():
-        with open('./tests/render/output/test_render_prescriptive_column_section_renderer' + column + '.json', 'w') \
+        with open('./tests/render/output/test_render_expectation_suite_column_section_renderer' + column + '.json', 'w') \
                 as outfile:
-            json.dump(PrescriptiveColumnSectionRenderer.render(exp_groups[column]), outfile, indent=2)
+            json.dump(ExpectationSuiteColumnSectionRenderer.render(exp_groups[column]), outfile, indent=2)
 
     # # This can be used for regression testing
     # for column in exp_groups.keys():
-    #     with open('./tests/render/output/test_render_prescriptive_column_section_renderer' + column + '.json') as infile:
-    #         assert json.dumps(PrescriptiveColumnSectionRenderer.render(exp_groups[column]), indent=2) == infile
+    #     with open('./tests/render/output/test_render_expectation_suite_column_section_renderer' + column + '.json') as infile:
+    #         assert json.dumps(ExpectationSuiteColumnSectionRenderer.render(exp_groups[column]), indent=2) == infile
 
 
-def test_DescriptiveColumnSectionRenderer_render(titanic_profiled_evrs_1, titanic_profiled_name_column_evrs):
+def test_ProfilingResultsColumnSectionRenderer_render(titanic_profiled_evrs_1, titanic_profiled_name_column_evrs):
     #Smoke test for titanic names
-    document = DescriptiveColumnSectionRenderer().render(titanic_profiled_name_column_evrs)
+    document = ProfilingResultsColumnSectionRenderer().render(titanic_profiled_name_column_evrs)
     print(document)
     assert document != {}
 
@@ -81,7 +81,7 @@ def test_DescriptiveColumnSectionRenderer_render(titanic_profiled_evrs_1, titani
     age_column_evrs = evrs_by_column["Age"]
     print(json.dumps(age_column_evrs, indent=2))
 
-    document = DescriptiveColumnSectionRenderer().render(age_column_evrs)
+    document = ProfilingResultsColumnSectionRenderer().render(age_column_evrs)
     print(document)
 
     # Save output to view
@@ -90,9 +90,9 @@ def test_DescriptiveColumnSectionRenderer_render(titanic_profiled_evrs_1, titani
     # open('./tests/render/output/titanic.html', 'w').write(html)
 
 
-def test_DescriptiveColumnSectionRenderer_render_header(titanic_profiled_name_column_evrs):
+def test_ProfilingResultsColumnSectionRenderer_render_header(titanic_profiled_name_column_evrs):
     content_blocks = []
-    DescriptiveColumnSectionRenderer()._render_header(
+    ProfilingResultsColumnSectionRenderer()._render_header(
         titanic_profiled_name_column_evrs,
         content_blocks,
         column_type = None
@@ -109,7 +109,7 @@ def test_DescriptiveColumnSectionRenderer_render_header(titanic_profiled_name_co
             "placement": "top"
         }
     }
-    assert content_block["sub_header"] == {
+    assert content_block["subheader"] == {
         "template": "Type: None",
         "tooltip": {
             "content": "expect_column_values_to_be_of_type <br>expect_column_values_to_be_in_type_list"
@@ -117,27 +117,27 @@ def test_DescriptiveColumnSectionRenderer_render_header(titanic_profiled_name_co
     }
 
 
-# def test_DescriptiveColumnSectionRenderer_render_overview_table():
+# def test_ProfilingResultsColumnSectionRenderer_render_overview_table():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_overview_table(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_overview_table(evrs, content_blocks)
 
-# def test_DescriptiveColumnSectionRenderer_render_quantile_table():
+# def test_ProfilingResultsColumnSectionRenderer_render_quantile_table():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_quantile_table(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_quantile_table(evrs, content_blocks)
 
-# def test_DescriptiveColumnSectionRenderer_render_stats_table():
+# def test_ProfilingResultsColumnSectionRenderer_render_stats_table():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_stats_table(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_stats_table(evrs, content_blocks)
 
-# def test_DescriptiveColumnSectionRenderer_render_histogram(titanic_profiled_evrs_1):
+# def test_ProfilingResultsColumnSectionRenderer_render_histogram(titanic_profiled_evrs_1):
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_histogram(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_histogram(evrs, content_blocks)
 
-# def test_DescriptiveColumnSectionRenderer_render_values_set():
+# def test_ProfilingResultsColumnSectionRenderer_render_values_set():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_values_set(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_values_set(evrs, content_blocks)
 
-def test_DescriptiveColumnSectionRenderer_render_bar_chart_table(titanic_profiled_evrs_1):
+def test_ProfilingResultsColumnSectionRenderer_render_bar_chart_table(titanic_profiled_evrs_1):
 
     print(titanic_profiled_evrs_1["results"][0])
     distinct_values_evrs = [evr for evr in titanic_profiled_evrs_1["results"] if evr["expectation_config"]["expectation_type"] == "expect_column_distinct_values_to_be_in_set"]
@@ -146,7 +146,7 @@ def test_DescriptiveColumnSectionRenderer_render_bar_chart_table(titanic_profile
 
     content_blocks = []
     for evr in distinct_values_evrs:
-        DescriptiveColumnSectionRenderer()._render_bar_chart_table(
+        ProfilingResultsColumnSectionRenderer()._render_bar_chart_table(
             distinct_values_evrs,
             content_blocks,
         )
@@ -172,7 +172,7 @@ def test_DescriptiveColumnSectionRenderer_render_bar_chart_table(titanic_profile
     # age_column_evrs = evrs_by_column["Survived"]
     # # print(json.dumps(age_column_evrs, indent=2))
 
-    # document = DescriptiveColumnSectionRenderer().render(age_column_evrs)
+    # document = ProfilingResultsColumnSectionRenderer().render(age_column_evrs)
     # # print(document)
 
     # html = DefaultJinjaPageView.render({"sections":[document]})
@@ -180,17 +180,17 @@ def test_DescriptiveColumnSectionRenderer_render_bar_chart_table(titanic_profile
     # open('./tests/render/output/titanic_age.html', 'w').write(html)
 
 
-# def test_DescriptiveColumnSectionRenderer_render_expectation_types():
+# def test_ProfilingResultsColumnSectionRenderer_render_expectation_types():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_expectation_types(evrs, content_blocks)
+#     ProfilingResultsColumnSectionRenderer()._render_expectation_types(evrs, content_blocks)
 
-# def test_DescriptiveColumnSectionRenderer_render_failed():
+# def test_ProfilingResultsColumnSectionRenderer_render_failed():
 #     evrs = {}
-#     DescriptiveColumnSectionRenderer()._render_failed(evrs, content_blocks)
+#     ExpectationSuiteColumnSectionRenderer()._render_failed(evrs, content_blocks)
 
 
-def test_PrescriptiveColumnSectionRenderer_render_header(titanic_profiled_name_column_expectations):
-    remaining_expectations, content_blocks = PrescriptiveColumnSectionRenderer._render_header(
+def test_ExpectationSuiteColumnSectionRenderer_render_header(titanic_profiled_name_column_expectations):
+    remaining_expectations, content_blocks = ExpectationSuiteColumnSectionRenderer._render_header(
         titanic_profiled_name_column_expectations,#["expectations"],
         [],
     )
@@ -215,8 +215,8 @@ def test_PrescriptiveColumnSectionRenderer_render_header(titanic_profiled_name_c
     ]
 
 
-def test_PrescriptiveColumnSectionRenderer_render_bullet_list(titanic_profiled_name_column_expectations):
-    remaining_expectations, content_blocks = PrescriptiveColumnSectionRenderer._render_bullet_list(
+def test_ExpectationSuiteColumnSectionRenderer_render_bullet_list(titanic_profiled_name_column_expectations):
+    remaining_expectations, content_blocks = ExpectationSuiteColumnSectionRenderer._render_bullet_list(
         titanic_profiled_name_column_expectations,#["expectations"],
         [],
     )
