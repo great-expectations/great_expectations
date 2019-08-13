f3870345f1ecf051f8357cb296f09a24fe9bc424
Abe <abegong@gmail.com>
2019-08-13 07:40:38 -0700

    Scaffold files

---
+++ great_expectations/data_context/store/store.py
@@ -0,0 +1,9 @@
+class Store(object):
+    """
+    """
+
+    def get(self, key, value):
+        raise NotImplementedError
+
+    def set(self, key, value):
+        raise NotImplementedError
---
+++ tests/data_context/test_store.py
@@ -0,0 +1,9 @@
+import pytest
+import json
+
+from great_expectations.data_context.store import (
+    Store,
+)
+
+def test_nothing():
+    pass
