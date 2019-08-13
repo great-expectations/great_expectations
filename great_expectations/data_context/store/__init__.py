28368f988295191569006c666e9acf7be7c16999
Abe <abegong@gmail.com>
2019-08-13 14:48:03 -0700

    Decruft

--- great_expectations/data_context/store/__init__.py
+++ great_expectations/data_context/store/__init__.py
@@ -3,17 +3,3 @@ from .store import (
     InMemoryStore,
     FilesystemStore,
 )
-# from .byte_store import (
-#     ByteStore
-# )
-# from .json_serializable_store import (
-#     JsonSerializableStore,
-#     JsonSerializableInMemoryStore,
-#     JsonSerializableFileStore,
-#     JsonSerializableS3Store,
-# )
-
-
-
-
-
