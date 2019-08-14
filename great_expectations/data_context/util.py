183ff05c133867d5ee04a0577ec154f26620c5a8
Abe <abegong@gmail.com>
2019-08-14 12:40:14 -0700

    Add msg

--- great_expectations/data_context/util.py
+++ great_expectations/data_context/util.py
@@ -121,6 +121,11 @@ def get_slack_callback(webhook):
 
 def safe_mmkdir(directory, exist_ok=True):
     """Simple wrapper since exist_ok is not available in python 2"""
+    if not isinstance(directory, str):
+        raise TypeError("directory must be of type str, not {0}".format({
+            "directory_type": str(type(directory))
+        }))
+
     if not exist_ok:
         raise ValueError(
             "This wrapper should only be used for exist_ok=True; it is designed to make porting easier later")
