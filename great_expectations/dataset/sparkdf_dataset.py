2ca386c4940deedf5b25434428b7a9f2ca3bb663
James Campbell <james.p.campbell@gmail.com>
2019-08-15 17:58:17 -0400

    Refactor set membership to not use UDF

--- great_expectations/dataset/sparkdf_dataset.py
+++ great_expectations/dataset/sparkdf_dataset.py
@@ -407,8 +407,10 @@ class SparkDFDataset(MetaSparkDFDataset):
         if parse_strings_as_datetimes:
             column = self._apply_dateutil_parse(column)
             value_set = [parse(value) if isinstance(value, string_types) else value for value in value_set]
-        success_udf = udf(lambda x: x in value_set)
-        return column.withColumn('__success', success_udf(column[0]))
+        # success_udf = udf(lambda x: x in value_set)
+        # return column.withColumn('__success', success_udf(column[0]))
+        return column.withColumn('__success', column[0].isin(value_set))
+
 
     @DocInherit
     @MetaSparkDFDataset.column_map_expectation
@@ -422,8 +424,12 @@ class SparkDFDataset(MetaSparkDFDataset):
             catch_exceptions=None,
             meta=None,
     ):
-        success_udf = udf(lambda x: x not in value_set)
-        return column.withColumn('__success', success_udf(column[0]))
+        # success_udf = udf(lambda x: x not in value_set)
+        # return column.withColumn('__success', success_udf(column[0]))
+        if None in value_set:
+            logger.error("expect_column_values_to_not_be_in_set cannot support a None in the value_set in spark")
+            raise ValueError("expect_column_values_to_not_be_in_set cannot support a None in the value_set in spark")
+        return column.withColumn('__success', ~column[0].isin(value_set))
 
     @DocInherit
     @MetaSparkDFDataset.column_map_expectation
