8ec46ceb88ac55c8820f852475ea7f6e42acf164
Abe <abegong@gmail.com>
2019-08-20 07:09:09 -0700

    Add first pass typing and tests for configs

--- great_expectations/actions/actions.py
+++ great_expectations/actions/actions.py
@@ -1,5 +1,5 @@
 from .types import (
-    BasicValidationActionConfig
+    ActionConfig
 )
 
 class BasicValidationAction():
--- tests/actions/test_core_actions.py
+++ tests/actions/test_core_actions.py
@@ -4,8 +4,37 @@ from great_expectations.actions import (
     BasicValidationAction,
 )
 from great_expectations.actions.types import (
-    BasicValidationActionConfig
+    ActionConfig,
+    ActionSetConfig,
 )
 
-def test_nothing():
-    pass
+
+def test_action_config():
+
+    ActionConfig(**{
+        "module_name" : "great_expectations.actions",
+        "class_name": "SummarizeAndSendToWebhookAction",
+        "kwargs" : {
+            "webhook": "http://myslackwebhook.com/",
+            "summarization_module_name": "great_expectations.render",
+            "summarization_class_name": "SummarizeValidationResultsForSlack",
+        }
+    })
+
+def test_action_set_config():
+
+    ActionSetConfig(
+        # coerce_types=True, #Need to merge in fixes to LooselyTypedDataDcit before this will work.
+        #TODO: We'll also need to modify LLTD to take a DictOf argument
+        **{
+            "action_list" : [ActionConfig(**{
+                "module_name" : "great_expectations.actions",
+                "class_name": "SummarizeAndSendToWebhookAction",
+                "kwargs" : {
+                    "webhook": "http://myslackwebhook.com/",
+                    "summarization_module_name": "great_expectations.render",
+                    "summarization_class_name": "SummarizeValidationResultsForSlack",
+                }
+            })]
+        }
+    )
