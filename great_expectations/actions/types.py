a9217795e8d976b3fdc589e4d841c227835c2b23
Abe <abegong@gmail.com>
2019-08-20 07:09:09 -0700

    Add first pass typing and tests for configs

--- great_expectations/actions/types.py
+++ great_expectations/actions/types.py
@@ -1,8 +1,31 @@
+from six import string_types
+
 from ..types import (
-    LooselyTypedDotDict
+    LooselyTypedDotDict,
+    ListOf,
 )
 
-class BasicValidationActionConfig(LooselyTypedDotDict):
+class ActionConfig(LooselyTypedDotDict):
     _allowed_keys = set([
-        
+        "module_name",
+        "class_name",
+        "kwargs"
     ])
+    _key_types = {
+        "module_name" : str, #This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
+        "class_name" : str, #This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
+        "kwargs" : dict,
+    }
+
+class ActionSetConfig(LooselyTypedDotDict):
+    _allowed_keys = set([
+        "module_name",
+        "class_name",
+        "action_list",
+    ])
+
+    _key_types = {
+        "module_name" : string_types,
+        "class_name" : string_types,
+        "action_list" : ListOf(ActionConfig),
+    }
