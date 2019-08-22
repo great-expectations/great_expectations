e38772892cf4c85cd2b8223d9099e95a61a78563
Abe <abegong@gmail.com>
2019-08-22 11:06:31 -0700

    Use new bse types to improve typing

--- great_expectations/actions/types.py
+++ great_expectations/actions/types.py
@@ -2,26 +2,31 @@ from six import string_types
 
 from ..types import (
     DotDict,
-    LooselyTypedDotDict,
+    RequiredKeysDotDict,
+    AllowedKeysDotDict,
     ListOf,
 )
 
-class ActionInternalConfig(DotDict):
+# NOTE : Some day it might make sense to implement 
+class ActionInternalConfig(RequiredKeysDotDict):
+    """A typed object containing the kwargs for a specific subclass of ValidationAction.
+    """
     pass
 
-class ActionConfig(LooselyTypedDotDict):
+class ActionConfig(AllowedKeysDotDict):
     _allowed_keys = set([
         "module_name",
         "class_name",
         "kwargs"
     ])
     _key_types = {
-        "module_name" : str, #This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
-        "class_name" : str, #This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
-        "kwargs" : ActionInternalConfig,
+        "module_name" : str, #TODO: This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
+        "class_name" : str, #TODO: This should be string_types. Need to merge in fixes to LooselyTypedDataDcit before that will work, though...
+        #TODO: Add this back in.
+        # "kwargs" : ActionInternalConfig,
     }
 
-class ActionSetConfig(LooselyTypedDotDict):
+class ActionSetConfig(AllowedKeysDotDict):
     _allowed_keys = set([
         "module_name",
         "class_name",
