726f8d3f1908d23c65bc21e61c3ace1e26cd80ae
Abe <abegong@gmail.com>
2019-08-24 22:21:11 -0700

    Pass test_validate_saves_result_inserts_run_id

--- tests/data_context/test_data_context.py
+++ tests/data_context/test_data_context.py
@@ -68,7 +68,6 @@ def test_validate_saves_result_inserts_run_id(empty_data_context, filesystem_csv
 
     print(gen_directory_tree_str(validations_dir))
 
-
     with open(os.path.join(not_so_empty_data_context.root_directory, 
               "uncommitted/validations/1955-11-05T000000Z/my_datasource/default/f1/default.json")) as infile:
         saved_validation_result = json.load(infile)
