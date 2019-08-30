87fb212447a033956e23c6051a820dae08455140
Abe <abegong@gmail.com>
2019-08-30 07:37:50 -0700

    Review and remove deprecated store files

--- great_expectations/data_context/store/__init__.py
+++ great_expectations/data_context/store/__init__.py
@@ -1,25 +1,10 @@
-# from .store import (
-#     Store,
-# )
-
-# from .basic import (
-#     FilesystemStore,
-#     # InMemoryStore,
-# )
-
-# from .namespaced import (
-#     # NamespacedStore,
-#     # NamespacedInMemoryStore,
-#     # NamespacedFilesystemStore,
-# )
-
 from .store_backend import (
     StoreBackend,
     InMemoryStoreBackend,
     FilesystemStoreBackend,
 )
 
-from .new_store import (
+from .store import (
     WriteOnlyStore,
     ReadWriteStore,
     BasicInMemoryStore,
