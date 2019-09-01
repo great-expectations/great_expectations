ccb864316379ed4a52a03a3db7b6d633e6b0a0de
Abe <abegong@gmail.com>
2019-08-31 19:17:16 -0700

    Remove prints and add a note

--- great_expectations/data_context/store/store.py
+++ great_expectations/data_context/store/store.py
@@ -46,7 +46,6 @@ class WriteOnlyStore(object):
 
     def set(self, key, value, serialization_type=None):
         self._validate_key(key)
-        print(key)
         
         if serialization_type:
             serialization_method = self._get_serialization_method(
@@ -262,7 +261,6 @@ class NamespacedReadWriteStore(ReadWriteStore):
 
     def _set(self, key, serialized_value):
         key_tuple = self._convert_resource_identifier_to_tuple(key)
-        print(key_tuple)
         return self.store_backend.set(key_tuple, serialized_value)
 
     def list_keys(self):
@@ -283,6 +281,7 @@ class NamespacedReadWriteStore(ReadWriteStore):
         # The logic in this function is recursive, so it can't return a tuple
 
         list_ = []
+        #Fetch keys in _key_order to guarantee tuple ordering in both python 2 and 3
         for key_name in key._key_order:
             key_element = key[key_name]
             if isinstance( key_element, DataContextResourceIdentifier ):
