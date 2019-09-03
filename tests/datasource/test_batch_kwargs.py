07c00e339802a2ae9157f2a69bff2986fffb9fbc
James Campbell <james.p.campbell@gmail.com>
2019-09-03 14:04:00 -0400

    Update batch_kwargs handling and tests

--- tests/datasource/test_batch_kwargs.py
+++ tests/datasource/test_batch_kwargs.py
@@ -14,6 +14,7 @@ from great_expectations.datasource.types import *
 from great_expectations.datasource.generator import SubdirReaderGenerator, GlobReaderGenerator
 
 
+
 @freeze_time("1955-11-05")
 def test_batch_kwargs_fingerprint():
     test_batch_kwargs = PathBatchKwargs(
@@ -25,7 +26,9 @@ def test_batch_kwargs_fingerprint():
     #demonstrate *output* kwargs post-datasource/generator
 
     # When there is only a single "important" key used in batch_kwargs, the ID can prominently include it
-    assert test_batch_kwargs.batch_fingerprint == ("19551105T000000.000000Z", "path:/data/test.csv")
+    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
+        partition_id="19551105T000000.000000Z",
+        fingerprint="path:/data/test.csv")
 
     test_batch_kwargs = PathBatchKwargs(
         {
@@ -35,7 +38,9 @@ def test_batch_kwargs_fingerprint():
     )
 
     # When partition_id is explicitly included, we can extract it and potentially still have a human readable id
-    assert test_batch_kwargs.batch_fingerprint == ("20190101", "path:/data/test.csv")
+    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
+        partition_id="20190101",
+        fingerprint="path:/data/test.csv")
 
     test_batch_kwargs = PathBatchKwargs(
         {
@@ -48,7 +53,9 @@ def test_batch_kwargs_fingerprint():
         }
     )
     # When there are multiple relevant keys we use the hash of the batch_kwargs dictionary
-    assert test_batch_kwargs.batch_fingerprint == ("3", "a5d67721928ee13317a81459818a556b")
+    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
+        partition_id="3",
+        fingerprint="a5d67721928ee13317a81459818a556b")
 
 
 def test_batch_kwargs_from_dict():
@@ -59,7 +66,9 @@ def test_batch_kwargs_from_dict():
 
     # The build_batch_fingerprint convenience method makes it possible to build a batch_fingerprint from a dict.
     # HOWEVER, using it can be difficult since the default-ignored keys may depend on a specific batch_kwargs type
-    assert BatchKwargs.build_batch_fingerprint(test_batch_kwargs) == ("1", "path:/data/test.csv")
+    assert BatchKwargs.build_batch_fingerprint(test_batch_kwargs) == BatchFingerprint(
+        partition_id="1",
+        fingerprint="path:/data/test.csv")
 
 
 def test_glob_reader_path_partitioning():
