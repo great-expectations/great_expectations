7864684594b5fc4aedec1150137c9e63d7b98528
Abe <abegong@gmail.com>
2019-09-09 12:19:15 -0700

    Add more FIXMEs

--- great_expectations/data_context/data_context.py
+++ great_expectations/data_context/data_context.py
@@ -23,6 +23,8 @@ from ..types.base import DotDict
 
 from great_expectations.exceptions import DataContextError, ConfigNotFoundError, ProfilerError
 
+# FIXME : fully deprecate site_builder, by replacing it with new_site_builder.
+# FIXME : Consolidate all builder files and classes in great_expectations/render/builder, to make it clear that they aren't renderers. 
 # from great_expectations.render.renderer.site_builder import SiteBuilder
 from great_expectations.render.renderer.new_site_builder import SiteBuilder
 
