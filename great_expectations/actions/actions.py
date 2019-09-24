c10d86565f5af74c679f1fe5b7eb5bb0cdf71590
Robert Moses Lim <robert.m.lim@gmail.com>
2019-09-24 12:15:58 -0700

    Add SlackNotifcationAction

--- great_expectations/actions/actions.py
+++ great_expectations/actions/actions.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 logger = logging.getLogger(__name__)
 
 from ..util import (
@@ -52,6 +53,77 @@ class NoOpAction(NamespacedValidationAction):
     
     def take_action(self, validation_result_id, validation_result_suite):
         print("Happily doing nothing")
+
+
+class SlackNotificationAction(NamespacedValidationAction):
+    
+    def __init__(
+            self,
+            data_context,
+            summarizer,
+            webhook=None,
+            notify_on="all", # "failure", "success"
+            # result_key="failure",
+            # name,
+            # result_key,
+            # target_store_name,
+            # stores,
+            # services
+    ):
+        self.data_context = data_context
+        self.summarizer = instantiate_class_from_config(
+            config=summarizer,
+            runtime_config={},
+            config_defaults={},
+        )
+        if webhook:
+            self.webhook = webhook
+        else:
+            webhook = data_context.get_project_config().get("slack_webhook")
+        assert webhook, "No Slack webhook found in action or project configs."
+        self.notify_on = notify_on
+        
+    def take_action(self, validation_result_id, validation_result_suite):
+        logger.debug("SummarizeAndNotifySlackAction.take_action")
+    
+        if validation_result_suite is None:
+            return
+        
+        if not isinstance(validation_result_id, ValidationResultIdentifier):
+            raise TypeError("validation_result_id must be of type ValidationResultIdentifier, not {0}".format(
+                type(validation_result_id)
+            ))
+
+        validation_success = validation_result_suite["success"]
+        
+        if self.notify_on == "all" or \
+                self.notify_on == "success" and validation_success or \
+                self.notify_on == "failure" and not validation_success:
+            query = self.summarizer.render(validation_result_suite)
+            self._send_slack_notification(query)
+        else:
+            return
+                
+    def _send_slack_notification(self, query):
+        session = requests.Session()
+    
+        try:
+            response = session.post(url=self.webhook, json=query)
+        except requests.ConnectionError:
+            logger.warning(
+                'Failed to connect to Slack webhook at {url} '
+                'after {max_retries} retries.'.format(
+                    url=self.webhook, max_retries=10))
+        except Exception as e:
+            logger.error(str(e))
+        else:
+            if response.status_code != 200:
+                logger.warning(
+                    'Request to Slack webhook at {url} '
+                    'returned error {status_code}: {text}'.format(
+                        url=self.webhook,
+                        status_code=response.status_code,
+                        text=response.text))
 
 
 class SummarizeAndStoreAction(NamespacedValidationAction):
