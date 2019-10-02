9fd6d134c9597c2e37776cdd709d11267afcb592
James Campbell <james.p.campbell@gmail.com>
2019-10-02 15:15:34 -0400

    Add rtd_ge_version

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -1,5 +1,6 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
+rtd_url_ge_version = __version__.replace(".", "_")
 del get_versions
 
 from .util import from_pandas, read_csv, read_excel, read_json, read_parquet, read_table, validate
