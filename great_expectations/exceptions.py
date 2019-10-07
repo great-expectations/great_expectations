de61d1246a9460143cfc9c3771e72a726d6e0cf8
Aylr <Aylr@users.noreply.github.com>
2019-10-07 10:51:49 -0600

    * fixed critical bug on missing library requirement * improved error messages around building documentation from dynamically loaded classes

--- great_expectations/exceptions.py
+++ great_expectations/exceptions.py
@@ -56,15 +56,52 @@ class ConfigNotFoundError(DataContextError):
 """
 
 
+class PluginModuleNotFoundError(GreatExpectationsError):
+    """A module import failed."""
+    def __init__(self, module_name):
+        template = """Error: No module named `{}` could be found in your plugins directory.
+    - Please verify your plugins directory is configured correctly.
+    - Please verify you have a module named `{}` in your plugins directory.
+"""
+        self.message = template.format(module_name, module_name)
+
+        colored_template = "<red>" + template + "</red>"
+        module_snippet = "</red><yellow>" + module_name + "</yellow><red>"
+        self.cli_colored_message = colored_template.format(
+            module_snippet,
+            module_snippet
+        )
+
+
+class PluginClassNotFoundError(GreatExpectationsError, AttributeError):
+    """A module import failed."""
+    def __init__(self, module_name, class_name):
+        template = """Error: The module: `{}` does not contain the class: `{}`.
+    - Please verify this class name `{}`.
+"""
+        self.message = template.format(module_name, class_name, class_name)
+
+        colored_template = "<red>" + template + "</red>"
+        module_snippet = "</red><yellow>" + module_name + "</yellow><red>"
+        class_snippet = "</red><yellow>" + class_name + "</yellow><red>"
+        self.cli_colored_message = colored_template.format(
+            module_snippet,
+            class_snippet,
+            class_snippet,
+        )
+
+
 class ExpectationSuiteNotFoundError(GreatExpectationsError):
     def __init__(self, data_asset_name):
         self.data_asset_name = data_asset_name
         self.message = "No expectation suite found for data_asset_name %s" % data_asset_name
 
+
 class BatchKwargsError(DataContextError):
     def __init__(self, message, batch_kwargs):
         self.message = message
         self.batch_kwargs = batch_kwargs
+
 
 class DatasourceInitializationError(GreatExpectationsError):
     def __init__(self, datasource_name, message):
