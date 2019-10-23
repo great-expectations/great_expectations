0346f65a9581d65ebd8fe3089f82454f086ee770
James Campbell <james.p.campbell@gmail.com>
2019-10-22 20:49:09 -0400

    Update numeric data formatting logic in data-docs
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- great_expectations/render/renderer/content_block/validation_results_table_content_block.py
+++ great_expectations/render/renderer/content_block/validation_results_table_content_block.py
@@ -1,7 +1,6 @@
 from great_expectations.render.renderer.content_block.expectation_string import ExpectationStringRenderer
-from great_expectations.render.types import (
-    RenderedComponentContent
-)
+from great_expectations.render.types import RenderedComponentContent
+from great_expectations.render.util import float_to_str
 
 import pandas as pd
 import altair as alt
@@ -160,9 +159,9 @@ class ValidationResultsTableContentBlockRenderer(ExpectationStringRenderer):
         if success or not result.get("unexpected_count"):
             return None
         else:
-            unexpected_count = result["unexpected_count"]
-            unexpected_percent = "%.2f%%" % (result["unexpected_percent"])
-            element_count = result["element_count"]
+            unexpected_count = float_to_str(result["unexpected_count"], use_locale=True)
+            unexpected_percent = float_to_str(result["unexpected_percent"], precision=4) + "%"
+            element_count = float_to_str(result["element_count"], use_locale=True)
             
             template_str = "\n\n$unexpected_count unexpected values found. " \
                            "$unexpected_percent of $element_count total rows."
@@ -253,16 +252,19 @@ class ValidationResultsTableContentBlockRenderer(ExpectationStringRenderer):
         if result.get("observed_value"):
             return str(result.get("observed_value"))
         elif expectation_type == "expect_column_values_to_be_null":
-            notnull_percent = result["unexpected_percent"]
-            return ("{null_percent:.8f}".format(null_percent=(100-notnull_percent)).rstrip('0').rstrip('.') +
-                    "% null")
+            try:
+                notnull_percent = result["unexpected_percent"]
+                return float_to_str(100-notnull_percent, precision=5) + "% null"
+            except KeyError:
+                return "unknown % null"
         elif expectation_type == "expect_column_values_to_not_be_null":
-            null_percent = result["unexpected_percent"]
-            return ("{filled_percent:.8f}".format(filled_percent=(100-null_percent)).rstrip('0').rstrip('.')
-                    + "% not null")
+            try:
+                null_percent = result["unexpected_percent"]
+                return float_to_str(100-null_percent, precision=5) + "% not null"
+            except KeyError:
+                return "unknown % not null"
         elif result.get("unexpected_percent") is not None:
-            return ("{:.8f}".format(round(result.get("unexpected_percent"), 4)).rstrip('0').rstrip('.')
-                    + "% unexpected")
+            return float_to_str(result.get("unexpected_percent"), precision=5) + "% unexpected"
         else:
             return "--"
 
