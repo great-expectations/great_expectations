3fc71ab42cf0047207d5537f9b963b9acd43f3c0
Robert Moses Lim <robert.m.lim@gmail.com>
2019-11-12 01:53:27 -0800

    Adjsut min chart pixel width

--- great_expectations/render/renderer/column_section_renderer.py
+++ great_expectations/render/renderer/column_section_renderer.py
@@ -448,8 +448,8 @@ class ProfilingResultsColumnSectionRenderer(ColumnSectionRenderer):
             return None
         else:
             chart_pixel_width = (len(weights) / 60.0) * 1000
-            if chart_pixel_width < 300:
-                chart_pixel_width = 300
+            if chart_pixel_width < 250:
+                chart_pixel_width = 250
             chart_container_col_width = round((len(weights) / 60.0) * 12)
             if chart_container_col_width < 4:
                 chart_container_col_width = 4
@@ -510,8 +510,8 @@ class ProfilingResultsColumnSectionRenderer(ColumnSectionRenderer):
             return None
         else:
             chart_pixel_width = (len(values) / 60.0) * 1000
-            if chart_pixel_width < 300:
-                chart_pixel_width = 300
+            if chart_pixel_width < 250:
+                chart_pixel_width = 250
             chart_container_col_width = round((len(values) / 60.0) * 12)
             if chart_container_col_width < 4:
                 chart_container_col_width = 4
