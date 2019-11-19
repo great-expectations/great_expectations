159ed2c0d8a308640eac5358fb33ad3e96bd9af2
James Campbell <james.p.campbell@gmail.com>
2019-11-18 21:35:52 -0500

    Update build_index_page and runtime for netlify

--- tests/build_index_page.py
+++ tests/build_index_page.py
@@ -6,14 +6,14 @@ html_files = glob.glob("tests/**/output/**/*.html", recursive=True)
 html_list = ""
 for f_ in html_files:
     html_list += '\t<li><a href="{}">{}</li>\n'.format(
-        f_,
+        f_[6:],
         f_.split(".")[-2],
     )
 
 json_list = ""
 for f_ in json_files:
     json_list += '\t<li><a href="{}">{}</li>\n'.format(
-        f_,
+        f_[6:],
         f_.split(".")[-2],
     )
 
