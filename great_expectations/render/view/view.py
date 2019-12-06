9f56e1eef340005abfd36eeffc8c907deff9c26b
Robert Moses Lim <robert.m.lim@gmail.com>
2019-12-06 10:02:43 -0800

    Add todos for tests

--- great_expectations/render/view/view.py
+++ great_expectations/render/view/view.py
@@ -201,6 +201,7 @@ class DefaultJinjaView(object):
         else:
             return ""
 
+    # TODO: Add test
     def render_markdown(self, markdown):
         try:
             return pypandoc.convert_text(markdown, format="md", to="html")
