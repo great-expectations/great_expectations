606d208136b61e8a53daa04150592006ba77e342
Eugene Mandel <eugene@superconductivehealth.com>
2020-01-16 15:26:05 -0800

    In expect_column_min_to_be_between and expect_column_max_to_be_between: cast min_value and max_value from string into datetime in order to assure that the operands of the comparison operators are of consistent types.

--- great_expectations/dataset/dataset.py
+++ great_expectations/dataset/dataset.py
@@ -3219,7 +3219,14 @@ class Dataset(MetaDataset):
         if column_min is None:
             success = False
         else:
+
             if min_value is not None:
+                if isinstance(column_min, datetime):
+                    try:
+                        min_value = parse(min_value)
+                    except ValueError as e:
+                        pass
+
                 if strict_min:
                     above_min = column_min > min_value
                 else:
@@ -3228,6 +3235,12 @@ class Dataset(MetaDataset):
                 above_min = True
 
             if max_value is not None:
+                if isinstance(column_min, datetime):
+                    try:
+                        max_value = parse(max_value)
+                    except ValueError as e:
+                        pass
+
                 if strict_max:
                     below_max = column_min < max_value
                 else:
@@ -3342,6 +3355,12 @@ class Dataset(MetaDataset):
             success = False
         else:
             if min_value is not None:
+                if isinstance(column_max, datetime):
+                    try:
+                        min_value = parse(min_value)
+                    except ValueError as e:
+                        pass
+
                 if strict_min:
                     above_min = column_max > min_value
                 else:
@@ -3350,6 +3369,12 @@ class Dataset(MetaDataset):
                 above_min = True
 
             if max_value is not None:
+                if isinstance(column_max, datetime):
+                    try:
+                        max_value = parse(max_value)
+                    except ValueError as e:
+                        pass
+
                 if strict_max:
                     below_max = column_max < max_value
                 else:
