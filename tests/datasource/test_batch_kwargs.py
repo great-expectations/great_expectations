e579c8d2d9e501d7464a4c81cd79898c4907f4c2
James Campbell <james.p.campbell@gmail.com>
2020-01-30 20:22:31 -0500

    Update BatchKwargs to_id tests
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- tests/datasource/test_batch_kwargs.py
+++ tests/datasource/test_batch_kwargs.py
@@ -12,60 +12,31 @@ except ImportError:
 from great_expectations.datasource.types import *
 
 
-@freeze_time("1955-11-05")
 def test_batch_kwargs_fingerprint():
     test_batch_kwargs = PathBatchKwargs(
         {
             "path": "/data/test.csv"
         }
     )
-
-    #demonstrate *output* kwargs post-datasource/generator
-
     # When there is only a single "important" key used in batch_kwargs, the ID can prominently include it
-    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
-        partition_id="19551105T000000.000000Z",
-        fingerprint="path:/data/test.csv")
+    assert test_batch_kwargs.to_id() == "path:/data/test.csv"
+
 
     test_batch_kwargs = PathBatchKwargs(
         {
             "path": "/data/test.csv",
-            "partition_id": "20190101"
-        }
-    )
-
-    # When partition_id is explicitly included, we can extract it and potentially still have a human readable id
-    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
-        partition_id="20190101",
-        fingerprint="path:/data/test.csv")
-
-    test_batch_kwargs = PathBatchKwargs(
-        {
-            "path": "/data/test.csv",
-            "iterator": True,
-            "partition_id": "3",
-            "chunksize": 2e7,
-            "parse_dates": [0, 3],
-            "names": ["start", "type", "quantity", "end"]
+            "reader_method": "read_csv",
+            "reader_options": {
+                "iterator": True,
+                "chunksize": 2e7,
+                "parse_dates": [0, 3],
+                "names": ["start", "type", "quantity", "end"]
+            }
         }
     )
     # When there are multiple relevant keys we use the hash of the batch_kwargs dictionary
-    assert test_batch_kwargs.batch_fingerprint == BatchFingerprint(
-        partition_id="3",
-        fingerprint="a5d67721928ee13317a81459818a556b")
-
-
-def test_batch_kwargs_from_dict():
-    test_batch_kwargs = {
-            "path": "/data/test.csv",
-            "partition_id": "1"
-        }
-
-    # The build_batch_fingerprint convenience method makes it possible to build a batch_fingerprint from a dict.
-    # HOWEVER, using it can be difficult since the default-ignored keys may depend on a specific batch_kwargs type
-    assert BatchKwargs.build_batch_fingerprint(test_batch_kwargs) == BatchFingerprint(
-        partition_id="1",
-        fingerprint="path:/data/test.csv")
+    print(test_batch_kwargs.to_id())
+    assert test_batch_kwargs.to_id() == "8607e071c6383509c8cd8f4c1ea65518"
 
 
 def test_batch_kwargs_attributes_and_keys():
@@ -73,11 +44,13 @@ def test_batch_kwargs_attributes_and_keys():
     test_batch_kwargs = PathBatchKwargs(
         {
             "path": "/data/test.csv",
-            "iterator": True,
-            "partition_id": "3",
-            "chunksize": 2e7,
-            "parse_dates": [0, 3],
-            "names": ["start", "type", "quantity", "end"]
+            "reader_method": "read_csv",
+            "reader_options": {
+                "iterator": True,
+                "chunksize": 2e7,
+                "parse_dates": [0, 3],
+                "names": ["start", "type", "quantity", "end"]
+            }
         }
     )
     assert test_batch_kwargs.path == "/data/test.csv"
@@ -92,6 +65,6 @@ def test_batch_kwargs_attributes_and_keys():
         assert test_batch_kwargs.names == ["start", "type", "quantity", "end"]
 
     # But we can access and set even protected names using dictionary notation
-    assert test_batch_kwargs["names"] == ["start", "type", "quantity", "end"]
+    assert test_batch_kwargs["reader_options"]["names"] == ["start", "type", "quantity", "end"]
     test_batch_kwargs["path"] = "/a/new/path.csv"
     assert test_batch_kwargs.path == "/a/new/path.csv"
