15fb9a18810a50df0e5265905af54f71cddeda0a
James Campbell <james.p.campbell@gmail.com>
2020-02-05 10:56:35 -0500

    linting
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -1,7 +1,8 @@
-from .util import from_pandas, read_csv, read_excel, read_json, read_parquet, read_pickle, read_table, validate
-from great_expectations.data_context import DataContext
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 rtd_url_ge_version = __version__.replace(".", "_")
 del get_versions
+
+from .util import from_pandas, read_csv, read_excel, read_json, read_parquet, read_pickle, read_table, validate
+from great_expectations.data_context import DataContext
