548ab989e8da53841b2cc09cf8d1603d17ad56db
Aylr <Aylr@users.noreply.github.com>
2020-02-05 14:29:10 -0700

    * new and tested DataContext.does_project_have_a_datasource_in_config_file to support more reliable init * augmented DataContext.is_project_initialized * clarified test names in test_project.py

--- tests/cli/test_project.py
+++ tests/cli/test_project.py
@@ -6,7 +6,7 @@ from great_expectations.cli import cli
 from tests.cli.utils import assert_no_logging_messages_or_tracebacks
 
 
-def test_project_check_on_missing_ge_dir(caplog, tmp_path_factory):
+def test_project_check_on_missing_ge_dir_guides_user_to_fix(caplog, tmp_path_factory):
     project_dir = str(tmp_path_factory.mktemp("empty_dir"))
     runner = CliRunner()
     result = runner.invoke(cli, ["project", "check-config", "-d", project_dir])
@@ -18,7 +18,7 @@ def test_project_check_on_missing_ge_dir(caplog, tmp_path_factory):
     assert_no_logging_messages_or_tracebacks(caplog, result)
 
 
-def test_project_check_on_valid_project(caplog, titanic_data_context):
+def test_project_check_on_valid_project_says_so(caplog, titanic_data_context):
     project_dir = titanic_data_context.root_directory
     runner = CliRunner()
     result = runner.invoke(cli, ["project", "check-config", "-d", project_dir])
@@ -28,7 +28,7 @@ def test_project_check_on_valid_project(caplog, titanic_data_context):
     assert_no_logging_messages_or_tracebacks(caplog, result)
 
 
-def test_project_check_on_project_with_missing_config_file(
+def test_project_check_on_project_with_missing_config_file_guides_user(
     caplog, titanic_data_context
 ):
     project_dir = titanic_data_context.root_directory
