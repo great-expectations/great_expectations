eef52d2547d8307b6e31bd14486a507d519eafa1
Aylr <Aylr@users.noreply.github.com>
2020-02-05 15:47:18 -0700

    * DataContext now checks for suite existence with additional tests * major behavior success w/ passing test_init_on_existing_project_with_no_datasources_should_continue_init_flow_and_add_one

--- great_expectations/cli/init_messages.py
+++ great_expectations/cli/init_messages.py
@@ -74,3 +74,5 @@ NO_DATASOURCES_FOUND = """<red>Error: No datasources were found.</red> Please ad
   - running `<green>great_expectations datasource new</green>` or
   - by editing the {} file
 """.format(DataContext.GE_YML)
+
+SETUP_SUCCESS = "\n<cyan>Great Expectations is now set up.</cyan>"
