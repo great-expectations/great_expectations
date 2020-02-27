a4860c0cf78bce3fc1375d4ac337509e2aeb28d0
James Campbell <james.p.campbell@gmail.com>
2020-02-26 23:38:12 -0800

    Simplify cli_logging configuration
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- great_expectations/cli/cli_logging.py
+++ great_expectations/cli/cli_logging.py
@@ -3,23 +3,17 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-logging.getLogger(
-    "great_expectations.datasource.generator.in_memory_generator"
-).setLevel(logging.CRITICAL)
-logging.getLogger(
-    "great_expectations.dataset.sqlalchemy_dataset"
-).setLevel(logging.CRITICAL)
-logging.getLogger(
-    "great_expectations.profile.sample_expectations_dataset_profiler"
-).setLevel(logging.CRITICAL)
 
-# Take over the entire GE module logging namespace when running CLI
-logger = logging.getLogger("great_expectations")
-
+###
+# REVIEWER NOTE: THE ORIGINAL IMPLEMENTATION WAS HEAVY HANDED AND I BELIEVE WAS A TEMPORARY WORKAROUND.
+# PLEASE CAREFULLY REVIEW TO ENSURE REMOVING THIS DOES NOT AFFECT DESIRED BEHAVIOR
+###
 
 def _set_up_logger():
-    handler = logging.StreamHandler()
+    # Log to console with a simple formatter; used by CLI
     formatter = logging.Formatter("%(message)s")
+    handler = logging.StreamHandler()
+    handler.setLevel(level=logging.WARNING)
     handler.setFormatter(formatter)
+    logger = logging.getLogger("great_expectations")
     logger.addHandler(handler)
-    logger.setLevel(logging.WARNING)
