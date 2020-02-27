4ffe91fc21796d259986660c04e0dd3707b8a7b5
James Campbell <james.p.campbell@gmail.com>
2020-02-27 13:54:36 -0800

    Allow one backend testing
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- tests/data_asset/test_data_asset_citations.py
+++ tests/data_asset/test_data_asset_citations.py
@@ -1,14 +1,12 @@
 
-
-def test_data_asset_citations(dataset):
-    # TODO: We don't need to run this test against all backends...
+def test_data_asset_citations(pandas_dataset):
     citation_date = "2020-02-27T12:34:56.1234Z"
-    dataset.add_citation("test citation", citation_date=citation_date)
-    suite = dataset.get_expectation_suite()
+    pandas_dataset.add_citation("test citation", citation_date=citation_date)
+    suite = pandas_dataset.get_expectation_suite()
     assert suite.meta["citations"][0] == {
         "comment": "test citation",
-        "batch_kwargs": dataset.batch_kwargs,
-        "batch_parameters": dataset.batch_parameters,
-        "batch_markers": dataset.batch_markers,
+        "batch_kwargs": pandas_dataset.batch_kwargs,
+        "batch_parameters": pandas_dataset.batch_parameters,
+        "batch_markers": pandas_dataset.batch_markers,
         "citation_date": citation_date
     }
