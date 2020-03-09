44e137ef913c00e907451475c69350283ee2c81a
Robert Moses Lim <robert.m.lim@gmail.com>
2020-03-09 00:56:51 -0700

    Add sqlalchemy init test that includes extra whitespace in user input

--- tests/cli/test_init_sqlite.py
+++ tests/cli/test_init_sqlite.py
@@ -178,6 +178,147 @@ great_expectations/
     assert "{}/great_expectations/uncommitted/data_docs/local_site/validations/warning/".format(project_dir) in mock_webbrowser.call_args[0][0]
 
 
+@mock.patch("webbrowser.open", return_value=True, side_effect=None)
+def test_cli_init_on_new_project_extra_whitespace_in_url(
+    mock_webbrowser, caplog, tmp_path_factory, titanic_sqlite_db_file
+):
+    project_dir = str(tmp_path_factory.mktemp("test_cli_init_diff"))
+    ge_dir = os.path.join(project_dir, "great_expectations")
+
+    database_path = os.path.join(project_dir, "titanic.db")
+    shutil.copy(titanic_sqlite_db_file, database_path)
+    engine = create_engine("sqlite:///{}".format(database_path))
+    engine_url_with_added_whitespace = "    " + str(engine.url) + "  "
+
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(
+        cli,
+        ["init", "-d", project_dir],
+        input="Y\n2\n5\ntitanic\n{}\n1\nwarning\n\n".format(
+            engine_url_with_added_whitespace, catch_exceptions=False
+        ),
+    )
+    stdout = result.output
+    assert len(stdout) < 3000, "CLI output is unreasonably long."
+
+    assert "Always know what to expect from your data" in stdout
+    assert "What data would you like Great Expectations to connect to" in stdout
+    assert "Which database backend are you using" in stdout
+    assert "Give your new data source a short name" in stdout
+    assert "What is the url/connection string for the sqlalchemy connection" in stdout
+    assert "Attempting to connect to your database." in stdout
+    assert "Great Expectations connected to your database" in stdout
+    assert "Which table would you like to use?" in stdout
+    assert "Name the new expectation suite [main.titanic.warning]" in stdout
+    assert (
+        "Great Expectations will choose a couple of columns and generate expectations about them"
+        in stdout
+    )
+    assert "Generating example Expectation Suite..." in stdout
+    assert "Building" in stdout
+    assert "Data Docs" in stdout
+    assert "A new Expectation suite 'warning' was added to your project" in stdout
+    assert "Great Expectations is now set up" in stdout
+
+    context = DataContext(ge_dir)
+    assert len(context.list_datasources()) == 1
+    assert context.list_datasources() == [
+        {"class_name": "SqlAlchemyDatasource", "name": "titanic"}
+    ]
+
+    first_suite = context.list_expectation_suites()[0]
+    suite = context.get_expectation_suite(first_suite.expectation_suite_name)
+    assert len(suite.expectations) == 13
+
+    assert os.path.isdir(ge_dir)
+    config_path = os.path.join(project_dir, "great_expectations/great_expectations.yml")
+    assert os.path.isfile(config_path)
+
+    config = yaml.load(open(config_path, "r"))
+    data_source_class = config["datasources"]["titanic"]["data_asset_type"][
+        "class_name"
+    ]
+    assert data_source_class == "SqlAlchemyDataset"
+
+    obs_tree = gen_directory_tree_str(ge_dir)
+
+    # Instead of monkey patching datetime, just regex out the time directories
+    date_safe_obs_tree = re.sub(r"\d*T\d*\.\d*Z", "9999.9999", obs_tree)
+    # Instead of monkey patching guids, just regex out the guids
+    guid_safe_obs_tree = re.sub(
+        r"[a-z0-9]{32}(?=\.(json|html))", "foobarbazguid", date_safe_obs_tree
+    )
+    assert (
+        guid_safe_obs_tree
+        == """\
+great_expectations/
+    .gitignore
+    great_expectations.yml
+    expectations/
+        warning.json
+    notebooks/
+        pandas/
+            validation_playground.ipynb
+        spark/
+            validation_playground.ipynb
+        sql/
+            validation_playground.ipynb
+    plugins/
+        custom_data_docs/
+            renderers/
+            styles/
+                data_docs_custom_styles.css
+            views/
+    uncommitted/
+        config_variables.yml
+        data_docs/
+            local_site/
+                index.html
+                expectations/
+                    warning.html
+                static/
+                    fonts/
+                        HKGrotesk/
+                            HKGrotesk-Bold.otf
+                            HKGrotesk-BoldItalic.otf
+                            HKGrotesk-Italic.otf
+                            HKGrotesk-Light.otf
+                            HKGrotesk-LightItalic.otf
+                            HKGrotesk-Medium.otf
+                            HKGrotesk-MediumItalic.otf
+                            HKGrotesk-Regular.otf
+                            HKGrotesk-SemiBold.otf
+                            HKGrotesk-SemiBoldItalic.otf
+                    images/
+                        favicon.ico
+                        glossary_scroller.gif
+                        iterative-dev-loop.png
+                        logo-long-vector.svg
+                        logo-long.png
+                        short-logo-vector.svg
+                        short-logo.png
+                        validation_failed_unexpected_values.gif
+                    styles/
+                        data_docs_custom_styles_template.css
+                        data_docs_default_styles.css
+                validations/
+                    warning/
+                        9999.9999/
+                            foobarbazguid.html
+        samples/
+        validations/
+            warning/
+                9999.9999/
+                    foobarbazguid.json
+"""
+    )
+
+    assert_no_logging_messages_or_tracebacks(caplog, result)
+
+    assert result.exit_code == 0
+    assert mock_webbrowser.call_count == 1
+    assert "{}/great_expectations/uncommitted/data_docs/local_site/validations/warning/".format(project_dir) in mock_webbrowser.call_args[0][0]
+
 
 @mock.patch("webbrowser.open", return_value=True, side_effect=None)
 def test_init_on_existing_project_with_no_datasources_should_continue_init_flow_and_add_one(
