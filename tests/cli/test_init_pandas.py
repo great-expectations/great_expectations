a06cd5ee50cade070de14246992074f68a95eb21
Rob Lim <robert.m.lim@gmail.com>
2020-03-10 16:57:14 -0700

    Bugfix/excel profiling (#1128)
    * Handle case where user tries using broken file in init
    
    - give user chance to fix file and try again or choose different file
    
    * Add cli init test for broken file
    
    * Add cli test
    
    - broken file and trying again with different file
    
    * Add entry to changelog
    
    * Trim down file
    
    * * fix test from upstream merge
    
    Co-authored-by: Taylor Miller <Aylr@users.noreply.github.com>

--- tests/cli/test_init_pandas.py
+++ tests/cli/test_init_pandas.py
@@ -371,6 +371,175 @@ def test_init_on_existing_project_with_datasource_with_no_suite_create_one(
     assert_no_logging_messages_or_tracebacks(caplog, result)
 
 
+@pytest.mark.xfail(condition=PY2, reason="Py2")
+def test_cli_init_on_new_project_with_broken_excel_file_without_trying_again(caplog, tmp_path_factory):
+    project_dir = str(tmp_path_factory.mktemp("test_cli_init_diff"))
+    os.makedirs(os.path.join(project_dir, "data"))
+    data_path = os.path.join(project_dir, "data", "broken_excel_file.xls")
+    fixture_path = file_relative_path(__file__, "../test_sets/broken_excel_file.xls")
+    shutil.copy(fixture_path, data_path)
+
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(
+        cli,
+        ["init", "-d", project_dir],
+        input="Y\n1\n1\n{}\n\n\nn\n".format(data_path, catch_exceptions=False),
+    )
+    stdout = result.output
+
+    assert len(stdout) < 3000, "CLI output is unreasonably long."
+    assert "Always know what to expect from your data" in stdout
+    assert "What data would you like Great Expectations to connect to" in stdout
+    assert "What are you processing your files with" in stdout
+    assert "Enter the path (relative or absolute) of a data file" in stdout
+    assert "Cannot load file." in stdout
+    assert "- Please check the file and try again or select a different data file." in stdout
+    assert "- Error: Unsupported format, or corrupt file: Expected BOF record; found b'PRODUCTI'" in stdout
+    assert "Try again? [Y/n]:" in stdout
+    assert "[{}]:".format(data_path) in stdout
+    assert "We have saved your setup progress. When you are ready, run great_expectations init to continue." in stdout
+
+    assert os.path.isdir(os.path.join(project_dir, "great_expectations"))
+    config_path = os.path.join(project_dir, "great_expectations/great_expectations.yml")
+    assert os.path.isfile(config_path)
+
+    config = yaml.load(open(config_path, "r"))
+    data_source_class = config["datasources"]["files_datasource"]["data_asset_type"][
+        "class_name"
+    ]
+    assert data_source_class == "PandasDataset"
+
+    assert_no_logging_messages_or_tracebacks(caplog, result)
+
+
+@pytest.mark.xfail(condition=PY2, reason="Py2")
+@mock.patch("webbrowser.open", return_value=True, side_effect=None)
+def test_cli_init_on_new_project_with_broken_excel_file_try_again_with_different_file(mock_webbrowser, caplog, tmp_path_factory):
+    project_dir = str(tmp_path_factory.mktemp("test_cli_init_diff"))
+    os.makedirs(os.path.join(project_dir, "data"))
+    data_path = os.path.join(project_dir, "data", "broken_excel_file.xls")
+    fixture_path = file_relative_path(__file__, "../test_sets/broken_excel_file.xls")
+    data_path_2 = os.path.join(project_dir, "data", "Titanic.csv")
+    fixture_path_2 = file_relative_path(__file__, "../test_sets/Titanic.csv")
+    shutil.copy(fixture_path, data_path)
+    shutil.copy(fixture_path_2, data_path_2)
+
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(
+        cli,
+        ["init", "-d", project_dir],
+        input="Y\n1\n1\n{}\n\n{}\n".format(data_path, data_path_2, catch_exceptions=False),
+    )
+    stdout = result.output
+    assert mock_webbrowser.call_count == 1
+    assert "{}/great_expectations/uncommitted/data_docs/local_site/validations/warning/".format(project_dir) in \
+           mock_webbrowser.call_args[0][0]
+
+    assert len(stdout) < 3000, "CLI output is unreasonably long."
+    assert "Always know what to expect from your data" in stdout
+    assert "What data would you like Great Expectations to connect to" in stdout
+    assert "What are you processing your files with" in stdout
+    assert "Enter the path (relative or absolute) of a data file" in stdout
+    assert "Cannot load file." in stdout
+    assert "- Please check the file and try again or select a different data file." in stdout
+    assert "- Error: Unsupported format, or corrupt file: Expected BOF record; found b'PRODUCTI'" in stdout
+    assert "Try again? [Y/n]:" in stdout
+    assert "[{}]:".format(data_path) in stdout
+
+    assert "Name the new expectation suite [warning]" in stdout
+    assert (
+            "Great Expectations will choose a couple of columns and generate expectations about them"
+            in stdout
+    )
+    assert "Generating example Expectation Suite..." in stdout
+    assert "Building" in stdout
+    assert "Data Docs" in stdout
+    assert "A new Expectation suite 'warning' was added to your project" in stdout
+    assert "Great Expectations is now set up" in stdout
+
+    assert os.path.isdir(os.path.join(project_dir, "great_expectations"))
+    config_path = os.path.join(project_dir, "great_expectations/great_expectations.yml")
+    assert os.path.isfile(config_path)
+
+    config = yaml.load(open(config_path, "r"))
+    data_source_class = config["datasources"]["files_datasource"]["data_asset_type"][
+        "class_name"
+    ]
+    assert data_source_class == "PandasDataset"
+
+    obs_tree = gen_directory_tree_str(os.path.join(project_dir, "great_expectations"))
+
+    # Instead of monkey patching datetime, just regex out the time directories
+    date_safe_obs_tree = re.sub(r"\d*T\d*\.\d*Z", "9999.9999", obs_tree)
+    # Instead of monkey patching guids, just regex out the guids
+    guid_safe_obs_tree = re.sub(
+        r"[a-z0-9]{32}(?=\.(json|html))", "foobarbazguid", date_safe_obs_tree
+    )
+    assert (
+        guid_safe_obs_tree
+        == """great_expectations/
+    .gitignore
+    great_expectations.yml
+    expectations/
+        warning.json
+    notebooks/
+        pandas/
+            validation_playground.ipynb
+        spark/
+            validation_playground.ipynb
+        sql/
+            validation_playground.ipynb
+    plugins/
+        custom_data_docs/
+            renderers/
+            styles/
+                data_docs_custom_styles.css
+            views/
+    uncommitted/
+        config_variables.yml
+        data_docs/
+            local_site/
+                index.html
+                expectations/
+                    warning.html
+                static/
+                    fonts/
+                        HKGrotesk/
+                            HKGrotesk-Bold.otf
+                            HKGrotesk-BoldItalic.otf
+                            HKGrotesk-Italic.otf
+                            HKGrotesk-Light.otf
+                            HKGrotesk-LightItalic.otf
+                            HKGrotesk-Medium.otf
+                            HKGrotesk-MediumItalic.otf
+                            HKGrotesk-Regular.otf
+                            HKGrotesk-SemiBold.otf
+                            HKGrotesk-SemiBoldItalic.otf
+                    images/
+                        favicon.ico
+                        glossary_scroller.gif
+                        iterative-dev-loop.png
+                        logo-long-vector.svg
+                        logo-long.png
+                        short-logo-vector.svg
+                        short-logo.png
+                        validation_failed_unexpected_values.gif
+                    styles/
+                        data_docs_custom_styles_template.css
+                        data_docs_default_styles.css
+                validations/
+                    warning/
+                        9999.9999/
+                            foobarbazguid.html
+        validations/
+            warning/
+                9999.9999/
+                    foobarbazguid.json
+""")
+
+    assert_no_logging_messages_or_tracebacks(caplog, result)
+
+
 def _delete_and_recreate_dir(directory):
     shutil.rmtree(directory)
     assert not os.path.isdir(directory)
