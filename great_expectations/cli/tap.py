56e4e03c60c3e2e4524ff2a5ea6a3e860651bbc5
Aylr <Aylr@users.noreply.github.com>
2020-03-16 16:18:55 -0600

    * move a suite loader to utils * 3 basic tests for tap new

--- great_expectations/cli/tap.py
+++ great_expectations/cli/tap.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+import sys
 
 import click
 
 from great_expectations import DataContext
 from great_expectations import exceptions as ge_exceptions
-from great_expectations.cli.suite import _load_suite
-from great_expectations.cli.util import cli_message
+from great_expectations.cli.datasource import select_datasource
+from great_expectations.cli.util import cli_message, load_expectation_suite
 from great_expectations.data_context.util import file_relative_path
 
 
@@ -19,6 +20,7 @@ def tap():
 @tap.command(name="new")
 @click.argument("suite")
 @click.argument("tap_filename")
+@click.option("--datasource", default=None)
 @click.option("--csv", default=None)
 @click.option(
     "--directory",
@@ -26,8 +28,8 @@ def tap():
     default=None,
     help="The project's great_expectations directory.",
 )
-def tap_new(suite, tap_filename, directory, csv=None):
-    """Create a new tap."""
+def tap_new(suite, tap_filename, directory, csv=None, datasource=None):
+    """BETA! Create a new tap file."""
     try:
         context = DataContext(directory)
     except ge_exceptions.ConfigNotFoundError as err:
@@ -35,13 +37,21 @@ def tap_new(suite, tap_filename, directory, csv=None):
         return
 
     directory = context.root_directory
+    datasource = select_datasource(context, datasource_name=datasource)
 
-    suite = _load_suite(context, suite)
+    if not datasource:
+        cli_message("<red>No datasources found in the context.</red>")
+        sys.exit(1)
+
+    # Note this can exit if no suite is found.
+    suite = load_expectation_suite(context, suite)
+
     n_expectations = len(suite.expectations)
     cli_message(f"Loaded suite {suite.expectation_suite_name} that has {n_expectations} expectations.")
 
+    batch_kwargs = {"datasource": datasource.name, "path": csv,
+            "reader_method": "read_csv"}
     template = _load_template()
-    batch_kwargs = {"datasource": "files_datasource", "path": os.path.abspath(csv), "reader_method": "read_csv"}
     template = template.format(tap_filename, directory, suite.expectation_suite_name, batch_kwargs)
 
     cli_message(f"<yellow>{template}</yellow>")
@@ -51,6 +61,11 @@ def tap_new(suite, tap_filename, directory, csv=None):
     cli_message(f"""<green>A new tap has been made! Open {tap_filename} in an editor to tweak it</green>""")
 
     # _debugging_stuff(tap_filename)
+
+
+def _file_batch_kwargs(datasource, csv):
+    return {"datasource": datasource, "path": os.path.abspath(csv),
+            "reader_method": "read_csv"}
 
 
 def _debugging_stuff(tap_filename):
