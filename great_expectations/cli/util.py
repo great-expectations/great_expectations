891aa7356bb0bcba48e67cbf547c99b2a322f586
Robert Moses Lim <robert.m.lim@gmail.com>
2020-03-23 00:36:17 -0700

    Add util function for printing dicts in cli

--- great_expectations/cli/util.py
+++ great_expectations/cli/util.py
@@ -47,6 +47,54 @@ def cli_message_list(string_list, list_intro_string=None):
         cli_message(string)
 
 
+def action_list_to_string(action_list):
+    action_list_string = ""
+    for idx, action in enumerate(action_list):
+        action_list_string += "{} ({})".format(action["name"], action["action"]["class_name"])
+        if idx == len(action_list) - 1:
+            continue
+        action_list_string += " => "
+    return action_list_string
+
+
+def cli_message_dict(dict_, indent=3, bullet_char="-", message_list=None, recursion_flag=False):
+    if message_list is None:
+        message_list = []
+    if dict_.get("name"):
+        name = dict_.pop("name")
+        message = "{}<cyan>name:</cyan> {}".format(" " * indent, name)
+        message_list.append(message)
+    if dict_.get("module_name"):
+        module_name = dict_.pop("module_name")
+        message = "{}<cyan>module_name:</cyan> {}".format(" " * indent, module_name)
+        message_list.append(message)
+    if dict_.get("class_name"):
+        class_name = dict_.pop("class_name")
+        message = "{}<cyan>class_name:</cyan> {}".format(" " * indent, class_name)
+        message_list.append(message)
+    if dict_.get("action_list"):
+        action_list = dict_.pop("action_list")
+        action_list_string = action_list_to_string(action_list)
+        message = "{}<cyan>action_list:</cyan> {}".format(" " * indent, action_list_string)
+        message_list.append(message)
+    for key, val in dict_.items():
+        if key == "credentials":
+            continue
+        if isinstance(val, dict):
+            message = "{}<cyan>{}:</cyan>".format(" " * indent, key)
+            message_list.append(message)
+            cli_message_dict(val, indent=indent + 2, message_list=message_list, recursion_flag=True)
+        else:
+            message = "{}<cyan>{}:</cyan> {}".format(" " * indent, key, str(val))
+            message_list.append(message)
+    if recursion_flag == False:
+        if bullet_char and indent > 1:
+            first = message_list[0]
+            new_first = first[:1] + bullet_char + first[2:]
+            message_list[0] = new_first
+        cli_message_list(message_list)
+
+
 def is_sane_slack_webhook(url):
     """Really basic sanity checking."""
     if url is None:
