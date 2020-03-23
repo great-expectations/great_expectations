d6cac26d3f834ab0eea7d70a0f4b6670120af1f5
Robert Moses Lim <robert.m.lim@gmail.com>
2020-03-22 22:31:35 -0700

    Clean up output

--- great_expectations/cli/docs.py
+++ great_expectations/cli/docs.py
@@ -98,10 +98,10 @@ def build_docs(context, site_name=None, view=True):
     for site_name, index_page_locator_info in index_page_locator_infos.items():
         if os.path.isfile(index_page_locator_info):
             msg += " - <cyan>{}</cyan>: ".format(site_name)
-            msg += "<green>file://{}</green>\n".format(index_page_locator_info)
+            msg += "file://{}\n".format(index_page_locator_info)
         else:
             msg += " - <cyan>{}</cyan>: ".format(site_name)
-            msg += "<green>{}</green>\n".format(index_page_locator_info)
+            msg += "{}\n".format(index_page_locator_info)
 
     msg = msg.rstrip("\n")
     cli_message(msg)
