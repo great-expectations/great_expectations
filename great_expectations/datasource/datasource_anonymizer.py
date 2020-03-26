006749318cd1f86efaaab16af1c24b459d583fc0
Robert Moses Lim <robert.m.lim@gmail.com>
2020-03-26 11:17:30 -0700

    Add anonymized name and sqlalchemy_dialect

--- great_expectations/datasource/datasource_anonymizer.py
+++ great_expectations/datasource/datasource_anonymizer.py
@@ -5,31 +5,37 @@ from great_expectations.util import load_class
 
 class DatasourceAnonymizer(Anonymizer):
     def anonymize_datasource_info(self, datasource_obj):
-        try:
-            datasource_config = datasource_obj.config
-            class_name = datasource_config.get("class_name")
-            class_ = datasource_obj.__class__
+        datasource_config = datasource_obj.config
+        datasource_name = datasource_obj.name
+        class_name = datasource_config.get("class_name")
+        class_ = datasource_obj.__class__
 
-            # Get a baseline:
-            anonymized_class_info = dict()
-            if issubclass(class_, Datasource):
-                anonymized_class_info["parent_class"] = "Datasource"
-            else:
-                anonymized_class_info["parent_class"] = "__not_datasource__"
-                anonymized_class_info["custom_class"] = self.anonymize(class_name)
+        # Get a baseline:
+        anonymized_datasource_info = dict()
+        if issubclass(class_, Datasource):
+            anonymized_datasource_info["parent_class"] = "Datasource"
+        else:
+            anonymized_datasource_info["parent_class"] = "__not_datasource__"
+            anonymized_datasource_info["anonymized_class"] = self.anonymize(class_name)
 
-            if issubclass(class_, PandasDatasource):
-                anonymized_class_info["parent_class"] = "PandasDatasource"
-                if not class_ == PandasDatasource:
-                    anonymized_class_info["custom_class"] = self.anonymize(class_name)
-            elif issubclass(class_, SqlAlchemyDatasource):
-                anonymized_class_info["parent_class"] = "SqlAlchemyDatasource"
-                if not class_ == SqlAlchemyDatasource:
-                    anonymized_class_info["custom_class"] = self.anonymize(class_name)
-            elif issubclass(class_, SparkDFDatasource):
-                anonymized_class_info["parent_class"] = "SparkDFDatasource"
-                if not class_ == SparkDFDatasource:
-                    anonymized_class_info["custom_class"] = self.anonymize(class_name)
-            return anonymized_class_info
-        except AttributeError:
-            return {"parent_class": "__unrecognized__", "custom_class": self.anonymize(class_name)}
+        if issubclass(class_, PandasDatasource):
+            anonymized_datasource_info["parent_class"] = "PandasDatasource"
+            if not class_ == PandasDatasource:
+                anonymized_datasource_info["anonymized_class"] = self.anonymize(class_name)
+        elif issubclass(class_, SqlAlchemyDatasource):
+            sqlalchemy_dialect = datasource_obj.engine.name
+            anonymized_datasource_info["parent_class"] = "SqlAlchemyDatasource"
+            anonymized_datasource_info["sqlalchemy_dialect"] = sqlalchemy_dialect
+            if not class_ == SqlAlchemyDatasource:
+                anonymized_datasource_info["anonymized_class"] = self.anonymize(class_name)
+        elif issubclass(class_, SparkDFDatasource):
+            anonymized_datasource_info["parent_class"] = "SparkDFDatasource"
+            if not class_ == SparkDFDatasource:
+                anonymized_datasource_info["anonymized_class"] = self.anonymize(class_name)
+
+        anonymized_datasource_info["anonymized_name"] = self.anonymize(datasource_name)
+        if not anonymized_datasource_info.get("parent_class"):
+            anonymized_datasource_info["parent_class"] = "__unrecognized__"
+            anonymized_datasource_info["anonymized_class"] = self.anonymize(class_name)
+
+        return anonymized_datasource_info
