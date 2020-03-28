95bbae83f5a198ae130d4feb86f7e7c8ca4a28bb
Robert Moses Lim <robert.m.lim@gmail.com>
2020-03-27 18:25:00 -0700

    Update tests

--- tests/integration/usage_statistics/test_integration_usage_statistics.py
+++ tests/integration/usage_statistics/test_integration_usage_statistics.py
@@ -18,23 +18,95 @@ logGroupName = "/great_expectations/usage_statistics/qa"
 @pytest.fixture(scope="session")
 def valid_usage_statistics_message():
     return {
-        "event_time": "2020-01-01T05:02:00.012Z",
-        "data_context_id": "51ff737e-33af-455a-8a11-0dc923dcbfb5",
-        "data_context_instance_id": "2d24776c-abef-4521-b182-4b18375b7259",
-        "ge_version": "0.9.4",
-        "event": "data_context.__init__",
-        "success": True,
-        "event_payload": {
-            "platform.system": "Darwin",
-            "platform.release": "19.3",
-            "version_info": "sys.version_info(major=3, minor=7, micro=5, releaselevel='final', serial=0)",
-            "anonymized_datasources": [
-                {
-                    "parent_class": "PandasDatasource"
+    'event_payload': {
+        'platform.system': 'Darwin',
+        'platform.release': '19.3.0',
+        'version_info': "sys.version_info(major=3, minor=7, micro=4, releaselevel='final', serial=0)",
+        'anonymized_datasources': [
+            {
+                'anonymized_name': 'f57d8a6edae4f321b833384801847498',
+                'parent_class': 'SqlAlchemyDatasource',
+                'sqlalchemy_dialect': 'postgresql'
+            }
+        ],
+        'anonymized_stores': [
+            {
+                'anonymized_name': '078eceafc1051edf98ae2f911484c7f7',
+                'parent_class': 'ExpectationsStore',
+                'anonymized_store_backend': {
+                    'parent_class': 'TupleFilesystemStoreBackend'
                 }
-            ]
-        }
-    }
+            },
+            {
+                'anonymized_name': '313cbd9858dd92f3fc2ef1c10ab9c7c8',
+                'parent_class': 'ValidationsStore',
+                'anonymized_store_backend': {
+                    'parent_class': 'TupleFilesystemStoreBackend'
+                }
+            },
+            {
+                'anonymized_name': '2d487386aa7b39e00ed672739421473f',
+                'parent_class': 'EvaluationParameterStore',
+                'anonymized_store_backend': {
+                    'parent_class': 'InMemoryStoreBackend'
+                }
+            }
+        ],
+        'anonymized_validation_operators': [
+            {
+                'anonymized_name': '99d14cc00b69317551690fb8a61aca94',
+                'parent_class': 'ActionListValidationOperator',
+                'anonymized_action_list': [
+                    {
+                        'anonymized_name': '5a170e5b77c092cc6c9f5cf2b639459a',
+                        'parent_class': 'StoreValidationResultAction'
+                    },
+                    {
+                        'anonymized_name': '0fffe1906a8f2a5625a5659a848c25a3',
+                        'parent_class': 'StoreEvaluationParametersAction'
+                    },
+                    {
+                        'anonymized_name': '101c746ab7597e22b94d6e5f10b75916',
+                        'parent_class': 'UpdateDataDocsAction'
+                    }
+                ]
+            }
+        ],
+        'anonymized_data_docs_sites': [
+            {
+                'parent_class': 'SiteBuilder',
+                'anonymized_name': 'eaf0cf17ad63abf1477f7c37ad192700',
+                'anonymized_store_backend': {'parent_class': 'TupleFilesystemStoreBackend'},
+                'anonymized_site_index_builder': {
+                    'parent_class': 'DefaultSiteIndexBuilder',
+                    'show_cta_footer': True
+                }
+            }
+        ],
+        'anonymized_expectation_suites': [
+            {
+                'anonymized_name': '238e99998c7674e4ff26a9c529d43da4',
+                'expectation_count': 8,
+                'anonymized_expectation_type_counts': {
+                    'expect_column_value_lengths_to_be_between': 1,
+                    'expect_table_row_count_to_be_between': 1,
+                    'expect_column_values_to_not_be_null': 2,
+                    'expect_column_distinct_values_to_be_in_set': 1,
+                    'expect_column_kl_divergence_to_be_less_than': 1,
+                    'expect_table_column_count_to_equal': 1,
+                    'expect_table_columns_to_match_ordered_list': 1
+                }
+            }
+        ]
+    },
+    'event': 'data_context.__init__',
+    'success': True,
+    'version': '1.0.0',
+    'event_time': '2020-03-28T01:14:21.155Z',
+    'data_context_id': '96c547fe-e809-4f2e-b122-0dc91bb7b3ad',
+    'data_context_instance_id': '445a8ad1-2bd0-45ce-bb6b-d066afe996dd',
+    'ge_version': '0.9.7+244.g56d67e51d.dirty'
+}
 
 
 @pytest.fixture(scope="session")
