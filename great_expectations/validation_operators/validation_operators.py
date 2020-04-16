4d57df71ba14bc8ac6104198b343de78fab7d0ed
Aylr <Aylr@users.noreply.github.com>
2020-04-16 10:42:53 -0600

    * design sketch for ValidationOperatorResult

--- great_expectations/validation_operators/validation_operators.py
+++ great_expectations/validation_operators/validation_operators.py
@@ -19,6 +19,42 @@ from great_expectations.exceptions import ClassInstantiationError
 # NOTE: Abe 2019/08/24 : This is first implementation of all these classes. Consider them UNSTABLE for now.
 
 
+class ValidationOperatorResult(object):
+    # TODO this is a design sketch
+    def __init__(self):
+        raise NotImplementedError
+
+    def get_run_id(self):
+        raise NotImplementedError
+
+    def list_assets_validated(self) -> list:
+        raise NotImplementedError
+
+    def get_evaluation_parameters(self):
+        raise NotImplementedError
+
+    @property
+    def statistics(self) -> dict:
+        raise NotImplementedError
+
+    @property
+    def actions_results(self) -> dict:
+        raise NotImplementedError
+
+    @property
+    def success(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def details(self):
+        # TODO I'm not sure what type this should return
+        raise NotImplementedError
+
+    @property
+    def validation_result_by_expectation_suite_identifier(self) -> dict:
+        raise NotImplementedError
+
+
 class ValidationOperator(object):
     """
     The base class of all validation operators.
