91628162a9e7c0d04a27edece7e5fd8103a4bd3a
Shahin <me@5hah.in>
2020-04-19 20:50:22 +0430

    Fix tests.

--- tests/datasource/test_util.py
+++ tests/datasource/test_util.py
@@ -4,10 +4,14 @@ from great_expectations.datasource.util import hash_dataframe
 
 
 def test_hash_dataframe_hashable_df():
-    df = pd.DataFrame([{"col_1": 1}])
-    assert hash_dataframe(df) == '03a57802981ad29e22de64b9a374ed86'
+    data = [{"col_1": 1}]
+    df1 = pd.DataFrame(data)
+    df2 = pd.DataFrame(data)
+    assert hash_dataframe(df1) == hash_dataframe(df2)
 
 
 def test_hash_dataframe_unhashable_df():
-    df = pd.DataFrame([{"col_1": {"val": 1}}])
-    assert hash_dataframe(df) == 'b7f282f9e759690a3092a401e00d2e47'
+    data = [{"col_1": {"val": 1}}]
+    df1 = pd.DataFrame(data)
+    df2 = pd.DataFrame(data)
+    assert hash_dataframe(df1) == hash_dataframe(df2)
