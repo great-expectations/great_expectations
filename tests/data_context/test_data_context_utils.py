5f8d474d2ed41ca1a375bec418bb3099865d372f
sbrugman <sfbbrugman@gmail.com>
2020-04-25 18:48:50 +0200

    Clean Python 2 references

--- tests/data_context/test_data_context_utils.py
+++ tests/data_context/test_data_context_utils.py
@@ -1,12 +1,12 @@
 import pytest
 import os
-import six
 
 from great_expectations.data_context.util import (
     safe_mmkdir,
     load_class,
 )
 import great_expectations.exceptions as gee
+
 
 def test_safe_mmkdir(tmp_path_factory):
     project_path = str(tmp_path_factory.mktemp('empty_dir'))
@@ -18,14 +18,6 @@ def test_safe_mmkdir(tmp_path_factory):
 
     with pytest.raises(TypeError):
         safe_mmkdir(1)
-
-    #This should trigger python 2
-    if six.PY2:
-        with pytest.raises(TypeError) as e:
-            next_project_path = tmp_path_factory.mktemp('test_safe_mmkdir__dir_b')
-            safe_mmkdir(next_project_path)
-
-        assert e.value.message == "directory must be of type str, not {'directory_type': \"<class 'pathlib2.PosixPath'>\"}"
 
 
 def test_load_class_raises_error_when_module_not_found():
