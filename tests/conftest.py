f0aeedb9eb8c2cb16136178c4f7bd8276057e8bf
James Campbell <james.p.campbell@gmail.com>
2020-04-28 19:40:14 -0400

    Use stats-enabled fixtures for integration tests
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -608,6 +608,18 @@ def empty_data_context(tmp_path_factory):
 
 
 @pytest.fixture
+def empty_data_context_stats_enabled(tmp_path_factory, monkeypatch):
+    # Reenable GE_USAGE_STATS
+    monkeypatch.delenv("GE_USAGE_STATS")
+    project_path = str(tmp_path_factory.mktemp('empty_data_context'))
+    context = ge.data_context.DataContext.create(project_path)
+    context_path = os.path.join(project_path, "great_expectations")
+    asset_config_path = os.path.join(context_path, "expectations")
+    os.makedirs(asset_config_path, exist_ok=True)
+    return context
+
+
+@pytest.fixture
 def titanic_data_context(tmp_path_factory):
     project_path = str(tmp_path_factory.mktemp('titanic_data_context'))
     context_path = os.path.join(project_path, "great_expectations")
@@ -620,6 +632,21 @@ def titanic_data_context(tmp_path_factory):
     shutil.copy(titanic_csv_path, str(os.path.join(context_path, "../data/Titanic.csv")))
     return ge.data_context.DataContext(context_path)
 
+
+@pytest.fixture
+def titanic_data_context_stats_enabled(tmp_path_factory, monkeypatch):
+    # Reenable GE_USAGE_STATS
+    monkeypatch.delenv("GE_USAGE_STATS")
+    project_path = str(tmp_path_factory.mktemp('titanic_data_context'))
+    context_path = os.path.join(project_path, "great_expectations")
+    os.makedirs(os.path.join(context_path, "expectations"), exist_ok=True)
+    data_path = os.path.join(context_path, "../data")
+    os.makedirs(os.path.join(data_path), exist_ok=True)
+    titanic_yml_path = file_relative_path(__file__, "./test_fixtures/great_expectations_titanic.yml")
+    shutil.copy(titanic_yml_path, str(os.path.join(context_path, "great_expectations.yml")))
+    titanic_csv_path = file_relative_path(__file__, "./test_sets/Titanic.csv")
+    shutil.copy(titanic_csv_path, str(os.path.join(context_path, "../data/Titanic.csv")))
+    return ge.data_context.DataContext(context_path)
 
 @pytest.fixture
 def titanic_sqlite_db():
