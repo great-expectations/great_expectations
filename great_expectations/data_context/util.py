3e1f80be82aa25b9cd2ae9d3f647842b99111dcb
ayirplm <priya@superconductivehealth.com>
2020-04-29 11:49:06 -0700

    sbrugmiss mmkdir whack-a-mole

--- great_expectations/data_context/util.py
+++ great_expectations/data_context/util.py
@@ -17,24 +17,6 @@ from great_expectations.exceptions import (
 logger = logging.getLogger(__name__)
 
 
-def safe_mmkdir(directory, exist_ok=True):
-    """Simple wrapper since exist_ok is not available in python 2"""
-    if not isinstance(directory, six.string_types):
-        raise TypeError("directory must be of type str, not {0}".format({
-            "directory_type": str(type(directory))
-        }))
-
-    if not exist_ok:
-        raise ValueError(
-            "This wrapper should only be used for exist_ok=True; it is designed to make porting easier later")
-    try:
-        os.makedirs(directory)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-
-
-
 def load_class(class_name, module_name):
     """Dynamically load a class from strings or raise a helpful error."""
     try:
