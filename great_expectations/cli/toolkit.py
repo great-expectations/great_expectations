bddf8db29294e4f7c2941c9b717d29386310c953
Taylor Miller <Aylr@users.noreply.github.com>
2020-05-01 11:36:54 -0600

    Checkpoints (#1351)
    * * marking validation operator commands as deprecating soon
    * DataContext.list_checkpoints() returns a list of checkpoint names found in the project
    * new test fixtures
    * new cli command: checkpoint list
    * DataContext.get_checkpoint() returns a validated dictionary loaded from yml
    * New error: CheckpointsError
    
    * * cli checkpoint run bare bones working
    * raise errors on not found suite and failed batch load
    * more checkpoint run error behaviors codified with tests
    * checkpoint list now sends usage stats
    * added checkpoints dir to titanic fixture
    
    * * WIP checkpoint new
    
    * merge fixes
    
    * * updated checkpoint test fixtures
    
    * * removed dead code
    
    * * new suite fixture
    * working checkpoint new happy path test with better messaging
    
    * * checkpoint new now works if checkpoint dir is missing
    * more tests for checkpoint new
    * validation_operator_list marked as experimental not deprecated it is still useful!
    
    * * checkpoint script
    * checkpoint script now raises error if checkpoint is not found
    * checkpoint script now raises error if existing python file
    * moved load_checkpoint and exit_with_failure_message_and_stats into toolkit
    * removed dead code
    * checkpoint run happy path tested for failed and succeeded validations
    * checkpoint script execution tested for failed and succeeded validations
    
    * * command line docs for checkpoint new, run, script
    
    * * checkpoint yml comment preservation and tests
    * better docs in checkpoint yml
    
    * * new checkpoint test
    * test audit and reorganization
    
    * * fixed failing validtion operator tests
    
    * * oops forgot the checkpoint script template
    
    * * updated changelog
    
    * * moved a few methods into the toolkit
    * cleaned up lots of imports
    * augmented checkpoint yml comments
    
    * * fix imports
    
    * Update docs/command_line.rst
    
    Co-authored-by: Daniel Mateus Pires <dmateusp@gmail.com>
    
    * Update docs/command_line.rst
    
    Co-authored-by: Eugene Mandel <eugene.mandel@gmail.com>
    
    * * added missing checkpoint script section
    * fixed a few sphinx warnings
    
    Co-authored-by: Daniel Mateus Pires <dmateusp@gmail.com>
    Co-authored-by: Eugene Mandel <eugene.mandel@gmail.com>

--- great_expectations/cli/toolkit.py
+++ great_expectations/cli/toolkit.py
@@ -8,17 +8,22 @@ import click
 
 from great_expectations import DataContext
 from great_expectations import exceptions as ge_exceptions
-from great_expectations.cli.datasource import (
-    get_batch_kwargs,
-    select_datasource,
-)
+from great_expectations.cli.datasource import get_batch_kwargs
 from great_expectations.cli.docs import build_docs
 from great_expectations.cli.util import cli_message
 from great_expectations.core import ExpectationSuite
 from great_expectations.core.id_dict import BatchKwargs
+from great_expectations.core.usage_statistics.usage_statistics import (
+    send_usage_message,
+)
 from great_expectations.data_asset import DataAsset
 from great_expectations.data_context.types.resource_identifiers import (
     ValidationResultIdentifier,
+)
+from great_expectations.datasource import Datasource
+from great_expectations.exceptions import (
+    CheckpointError,
+    CheckpointNotFoundError,
 )
 from great_expectations.profile import BasicSuiteBuilderProfiler
 
@@ -82,8 +87,10 @@ def create_expectation_suite(
             batch_kwargs, generator_asset
         )
         while True:
-            expectation_suite_name = click.prompt("\nName the new expectation suite",
-                                                  default=default_expectation_suite_name)
+            expectation_suite_name = click.prompt(
+                "\nName the new expectation suite",
+                default=default_expectation_suite_name,
+            )
             if expectation_suite_name in context.list_expectation_suite_names():
                 tell_user_suite_exists(expectation_suite_name)
             else:
@@ -204,7 +211,9 @@ def tell_user_suite_exists(suite_name: str) -> None:
     )
 
 
-def create_empty_suite(context: DataContext, expectation_suite_name: str, batch_kwargs) -> None:
+def create_empty_suite(
+    context: DataContext, expectation_suite_name: str, batch_kwargs
+) -> None:
     suite = context.create_expectation_suite(expectation_suite_name)
     suite.add_citation(comment="New suite added via CLI", batch_kwargs=batch_kwargs)
     context.save_expectation_suite(suite, expectation_suite_name)
@@ -214,9 +223,119 @@ def launch_jupyter_notebook(notebook_path: str) -> None:
     subprocess.call(["jupyter", "notebook", notebook_path])
 
 
-def load_batch(context: DataContext, suite: Union[str, ExpectationSuite], batch_kwargs: Union[dict, BatchKwargs]) -> DataAsset:
+def load_batch(
+    context: DataContext,
+    suite: Union[str, ExpectationSuite],
+    batch_kwargs: Union[dict, BatchKwargs],
+) -> DataAsset:
     batch: DataAsset = context.get_batch(batch_kwargs, suite)
     assert isinstance(
         batch, DataAsset
     ), "Batch failed to load. Please check your batch_kwargs"
     return batch
+
+
+def load_expectation_suite(
+    # TODO consolidate all the myriad CLI tests into this
+    context: DataContext,
+    suite_name: str,
+    usage_event: str,
+) -> ExpectationSuite:
+    """
+    Load an expectation suite from a given context.
+
+    Handles a suite name with or without `.json`
+    :param usage_event:
+    """
+    if suite_name.endswith(".json"):
+        suite_name = suite_name[:-5]
+    try:
+        suite = context.get_expectation_suite(suite_name)
+        return suite
+    except ge_exceptions.DataContextError as e:
+        exit_with_failure_message_and_stats(
+            context,
+            usage_event,
+            f"<red>Could not find a suite named `{suite_name}`.</red> Please check "
+            "the name by running `great_expectations suite list` and try again.",
+        )
+
+
+def exit_with_failure_message_and_stats(
+    context: DataContext, usage_event: str, message: str
+) -> None:
+    cli_message(message)
+    send_usage_message(context, event=usage_event, success=False)
+    sys.exit(1)
+
+
+def load_checkpoint(
+    context: DataContext, checkpoint_name: str, usage_event: str
+) -> dict:
+    """Load a checkpoint or raise helpful errors."""
+    try:
+        checkpoint_config = context.get_checkpoint(checkpoint_name)
+        return checkpoint_config
+    except CheckpointNotFoundError as e:
+        exit_with_failure_message_and_stats(
+            context,
+            usage_event,
+            f"""\
+<red>Could not find checkpoint `{checkpoint_name}`.</red> Try running:
+  - `<green>great_expectations checkpoint list</green>` to verify your checkpoint exists
+  - `<green>great_expectations checkpoint new</green>` to configure a new checkpoint""",
+        )
+    except CheckpointError as e:
+        exit_with_failure_message_and_stats(context, usage_event, f"<red>{e}</red>")
+
+
+def select_datasource(context: DataContext, datasource_name: str = None) -> Datasource:
+    """Select a datasource interactively."""
+    # TODO consolidate all the myriad CLI tests into this
+    data_source = None
+
+    if datasource_name is None:
+        data_sources = sorted(context.list_datasources(), key=lambda x: x["name"])
+        if len(data_sources) == 0:
+            cli_message(
+                "<red>No datasources found in the context. To add a datasource, run `great_expectations datasource new`</red>"
+            )
+        elif len(data_sources) == 1:
+            datasource_name = data_sources[0]["name"]
+        else:
+            choices = "\n".join(
+                [
+                    "    {}. {}".format(i, data_source["name"])
+                    for i, data_source in enumerate(data_sources, 1)
+                ]
+            )
+            option_selection = click.prompt(
+                "Select a datasource" + "\n" + choices + "\n",
+                type=click.Choice(
+                    [str(i) for i, data_source in enumerate(data_sources, 1)]
+                ),
+                show_choices=False,
+            )
+            datasource_name = data_sources[int(option_selection) - 1]["name"]
+
+    if datasource_name is not None:
+        data_source = context.get_datasource(datasource_name)
+
+    return data_source
+
+
+def load_data_context_with_error_handling(directory: str) -> DataContext:
+    """Return a DataContext with good error handling and exit codes."""
+    # TODO consolidate all the myriad CLI tests into this
+    try:
+        context = DataContext(directory)
+        return context
+    except (ge_exceptions.ConfigNotFoundError, ge_exceptions.InvalidConfigError) as err:
+        cli_message("<red>{}</red>".format(err.message))
+        sys.exit(1)
+    except ge_exceptions.PluginModuleNotFoundError as err:
+        cli_message(err.cli_colored_message)
+        sys.exit(1)
+    except ge_exceptions.PluginClassNotFoundError as err:
+        cli_message(err.cli_colored_message)
+        sys.exit(1)
