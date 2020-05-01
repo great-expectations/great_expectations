218712ad851e4c4f310676394474b4617db6e433
ayirplm <priyajoseph@priyas-MacBook-Pro.local>
2020-04-30 17:15:52 -0700

    ayirpmissing cannot reproduce split bug

--- great_expectations/data_context/data_context.py
+++ great_expectations/data_context/data_context.py
@@ -941,12 +941,15 @@ class BaseDataContext(object):
         Returns:
             True for Success and False for Failure.
         """
+        """
         key = None
         keys = self.stores[self.expectations_store_name].list_keys()
         for item in keys:
             sval = repr(item)
             if expectation_suite_name.expectation_suite_name in sval:
                 key=item
+        """
+        key = ExpectationSuiteIdentifier(expectation_suite_name)
         if not self._stores[self.expectations_store_name].has_key(key):
             raise ge_exceptions.DataContextError(
                 "expectation_suite with name {} does not exist."
