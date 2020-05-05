38ef41e12767a49edac6d50d1c7f461edadd997d
Zain Patel <zain.patel@quantumblack.com>
2020-05-05 18:36:04 +0100

    Delete test since click handles it

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -167,10 +167,6 @@ def test_cli_config_not_found_raises_error_for_all_commands(tmp_path_factory):
         result = runner.invoke(
             cli, ["datasource", "new", "-d", "./"], catch_exceptions=False
         )
-          
-        assert error_message in result.output
-        result = runner.invoke(cli, ["datasource", "delete"], catch_exceptions=False)
-        assert error_message in result.output
  
         # data_docs clean
         result = runner.invoke(
