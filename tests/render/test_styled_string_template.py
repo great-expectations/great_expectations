5f0bb83b0f84b3153df8544642990e115f2d826d
Robert Moses Lim <robert.m.lim@gmail.com>
2020-05-13 18:58:24 -0700

    Add tests for dollar signs in string template

--- tests/render/test_styled_string_template.py
+++ tests/render/test_styled_string_template.py
@@ -1,9 +1,8 @@
 import json
 
-import pytest
-
 import great_expectations as ge
 import great_expectations.render as render
+import pytest
 from great_expectations.render.renderer import (
     ExpectationSuiteColumnSectionRenderer,
     ExpectationSuitePageRenderer,
@@ -99,6 +98,60 @@ def test_render_template():
     ).replace(" ", "").replace("\t", "").replace(
         "\n", ""
     ) == '<span>It was the <span class="badge badge-warning" role="alert" style="padding:5px;" >best</span> of times; it was the worst of times.</span>'.replace(
+        " ", ""
+    ).replace(
+        "\t", ""
+    ).replace(
+        "\n", ""
+    )
+
+
+def test_render_template_with_extra_dollar_signs_in_template():
+    result = DefaultJinjaPageView().render_string_template(
+        {
+            "template": "It was the $first_adj of times; it was the $second_adj of times. Blahhh$hhhh. $Bloooop. "
+            "Bleep$.",
+            "params": {"first_adj": "best", "second_adj": "worst",},
+        }
+    )
+    assert result.replace(" ", "").replace("\t", "").replace(
+        "\n", ""
+    ) == "<span>It was the best of times; it was the worst of times. Blahhh$hhhh. $Bloooop. Bleep$.</span>".replace(
+        " ", ""
+    ).replace(
+        "\t", ""
+    ).replace(
+        "\n", ""
+    )
+
+    result = DefaultJinjaPageView().render_string_template(
+        {
+            "template": "It was the $first_adj of times; it was the $second_adj of times. Blahhh$$$hhhh. $$Bloooop. Bleep$$$$$.",
+            "params": {"first_adj": "best", "second_adj": "worst",},
+        }
+    )
+    assert result.replace(" ", "").replace("\t", "").replace(
+        "\n", ""
+    ) == "<span>It was the best of times; it was the worst of times. Blahhh$$$hhhh. $$Bloooop. Bleep$$$$$.</span>".replace(
+        " ", ""
+    ).replace(
+        "\t", ""
+    ).replace(
+        "\n", ""
+    )
+
+
+def test_render_template_with_extra_dollar_signs_in_param_values():
+    result = DefaultJinjaPageView().render_string_template(
+        {
+            "template": "It was the $first_adj of times; it was the $second_adj of times. Blahhh$hhhh. $Bloooop. "
+            "Bleep$.",
+            "params": {"first_adj": "$best$", "second_adj": "$$worst$",},
+        }
+    )
+    assert result.replace(" ", "").replace("\t", "").replace(
+        "\n", ""
+    ) == "<span>It was the $best$ of times; it was the $$worst$ of times. Blahhh$hhhh. $Bloooop. Bleep$.</span>".replace(
         " ", ""
     ).replace(
         "\t", ""
