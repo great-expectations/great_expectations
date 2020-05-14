1392ffd0ecbd7a125cee51006e9e5003ea143e4b
ayirplm <priya@superconductive.com>
2020-05-13 22:08:34 -0700

    ayirpBuilddocs handle only_if_exists in site_builder.build, and in accessible sites

--- great_expectations/cli/docs.py
+++ great_expectations/cli/docs.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import requests
 import click
 
 from great_expectations.cli import toolkit
@@ -111,33 +112,53 @@ def _build_intro_string(docs_sites_strings):
         list_intro_string = f"{doc_string_count} Data Docs sites configured:"
     return list_intro_string
 
-
 def build_docs(context, site_name=None, view=True):
     """Build documentation in a context"""
     logger.debug("Starting cli.datasource.build_docs")
 
     cli_message("Building Data Docs...")
-
+    p_sname = site_name
     if site_name is not None:
         site_names = [site_name]
     else:
         site_names = None
 
     index_page_locator_infos = context.build_data_docs(site_names=site_names)
-
+    b_nfound = False
     msg = "The following Data Docs sites were built:\n"
-    """
     for site_name, index_page_locator_info in index_page_locator_infos.items():
-        if os.path.isfile(index_page_locator_info):
-            msg += " - <cyan>{}:</cyan> ".format(site_name)
-            msg += "file://{}\n".format(index_page_locator_info)
+        if index_page_locator_info.startswith("file"):
+            if os.path.isfile(index_page_locator_info[6:]):
+                msg += " - <cyan>{}:</cyan> ".format(site_name)
+                msg += "{}\n".format(index_page_locator_info)
+            else:
+                if p_sname is not None:
+                    if site_name == p_sname:
+                        b_nfound = True
+                else:
+                     b_nfound = True
+                msg += " - <cyan>{}:</cyan> ".format(site_name)
+                msg += "{}\n".format("Site doesn’t exist or is inaccessible at "+ index_page_locator_info + ". If you just built data docs, please check permissions.")
         else:
-            msg += " - <cyan>{}:</cyan> ".format(site_name)
-            msg += "{}\n".format(index_page_locator_info)
-    """
-    msg += " - <cyan>{}:</cyan> ".format(site_name)
+           r = requests.get(index_page_locator_info, stream=True)
+           if r==200:
+               msg += " - <cyan>{}:</cyan> ".format(site_name)
+               msg += "{}\n".format(index_page_locator_info)
+           else:
+              if p_sname is not None:
+                  if site_name == p_sname:
+                      b_nfound = True
+              else:
+                   b_nfound = True
+              msg += " - <cyan>{}:</cyan> ".format(site_name)
+              msg += "{}\n".format("Site doesn’t exist or is inaccessible at "+ index_page_locator_info + ". If you just built data docs, please check permissions.")
+
     msg = msg.rstrip("\n")
     cli_message(msg)
 
     if view:
-        context.open_data_docs(site_name=site_name)
+        if b_nfound == False:
+            if p_sname is not None:
+                context.open_data_docs(site_name=p_sname)
+            else:
+                context.open_data_docs(site_name=site_name)
