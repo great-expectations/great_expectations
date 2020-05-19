0f53eb7799b3b9efc28e3b7a4bf24894affefb90
Robert Moses Lim <robert.m.lim@gmail.com>
2020-05-18 21:43:35 -0700

    Convert properties to list_ pattern; nix self._actions_results_by_validation_result_identifier
    Reduce properties to:
    self._run_id
    self._run_results
    self._evaluation_parameters
    self._validation_operator_config
    self._success

--- great_expectations/validation_operators/types/validation_operator_result.py
+++ great_expectations/validation_operators/types/validation_operator_result.py
@@ -50,72 +50,14 @@ class ValidationOperatorResult(DictDot):
         self._validation_result_identifiers = None
         self._expectation_suite_names = None
         self._data_asset_names = None
-        self._actions_results_by_validation_result_identifier = None
         self._validation_results_by_expectation_suite_name = None
         self._validation_results_by_data_asset_name = None
         self._batch_identifiers = None
         self._statistics = None
 
     @property
-    def batch_identifiers(self) -> List[str]:
-        if self._batch_identifiers is None:
-            self._batch_identifiers = list(
-                set(
-                    [
-                        validation_result_identifier.batch_identifier
-                        for validation_result_identifier in self.validation_result_identifiers
-                    ]
-                )
-            )
-        return self._batch_identifiers
-
-    @property
     def validation_operator_config(self) -> dict:
         return self._validation_operator_config
-
-    @property
-    def data_asset_names(self) -> List[str]:
-        if self._data_asset_names is None:
-            self._data_asset_names = list(
-                set(
-                    [
-                        data_asset["batch_kwargs"].get("data_asset_name") or "__none__"
-                        for data_asset in self.data_assets_validated
-                    ]
-                )
-            )
-        return self._data_asset_names
-
-    @property
-    def actions_results_by_validation_result_identifier(self) -> dict:
-        if self._actions_results_by_validation_result_identifier is None:
-            self._actions_results_by_validation_result_identifier = {
-                validation_result_identifier: run_result["actions_results"]
-                for (
-                    validation_result_identifier,
-                    run_result,
-                ) in self.run_results.items()
-            }
-        return self._actions_results_by_validation_result_identifier
-
-    @property
-    def expectation_suite_names(self) -> List[str]:
-        if self._expectation_suite_names is None:
-            self._expectation_suite_names = list(
-                set(
-                    [
-                        validation_result_identifier.expectation_suite_identifier.expectation_suite_name
-                        for validation_result_identifier in self._run_results.keys()
-                    ]
-                )
-            )
-        return self._expectation_suite_names
-
-    @property
-    def validation_result_identifiers(self) -> List[ValidationResultIdentifier]:
-        if self._validation_result_identifiers is None:
-            self._validation_result_identifiers = list(self._run_results.keys())
-        return self._validation_result_identifiers
 
     @property
     def run_results(
@@ -127,15 +69,6 @@ class ValidationOperatorResult(DictDot):
         return self._run_results
 
     @property
-    def validation_results(self) -> List[ExpectationSuiteValidationResult]:
-        if self._validation_results is None:
-            self._validation_results = [
-                run_result["validation_result"]
-                for run_result in self.run_results.values()
-            ]
-        return self._validation_results
-
-    @property
     def run_id(self) -> str:
         return self._run_id
 
@@ -144,19 +77,124 @@ class ValidationOperatorResult(DictDot):
         return self._evaluation_parameters
 
     @property
-    def data_assets_validated(self) -> List[dict]:
-        if self._data_assets_validated is None:
-            self._data_assets_validated = list(
-                self.data_assets_validated_by_batch_id.values()
+    def success(self) -> bool:
+        if self._success is None:
+            self._success = all(
+                [
+                    run_result["validation_result"].success
+                    for run_result in self.run_results.values()
+                ]
             )
-        return self._data_assets_validated
+        return self._success
 
-    @property
-    def data_assets_validated_by_batch_id(self) -> dict:
+    def list_batch_identifiers(self) -> List[str]:
+        if self._batch_identifiers is None:
+            self._batch_identifiers = list(
+                set(
+                    [
+                        validation_result_identifier.batch_identifier
+                        for validation_result_identifier in self.list_validation_result_identifiers()
+                    ]
+                )
+            )
+        return self._batch_identifiers
+
+    def list_data_asset_names(self) -> List[str]:
+        if self._data_asset_names is None:
+            self._data_asset_names = list(
+                set(
+                    [
+                        data_asset["batch_kwargs"].get("data_asset_name") or "__none__"
+                        for data_asset in self.list_data_assets_validated()
+                    ]
+                )
+            )
+        return self._data_asset_names
+
+    def list_expectation_suite_names(self) -> List[str]:
+        if self._expectation_suite_names is None:
+            self._expectation_suite_names = list(
+                set(
+                    [
+                        validation_result_identifier.expectation_suite_identifier.expectation_suite_name
+                        for validation_result_identifier in self.run_results.keys()
+                    ]
+                )
+            )
+        return self._expectation_suite_names
+
+    def list_validation_result_identifiers(self) -> List[ValidationResultIdentifier]:
+        if self._validation_result_identifiers is None:
+            self._validation_result_identifiers = list(self._run_results.keys())
+        return self._validation_result_identifiers
+
+    def list_validation_results(
+        self, group_by=None
+    ) -> Union[List[ExpectationSuiteValidationResult], dict]:
+        if group_by is None:
+            if self._validation_results is None:
+                self._validation_results = [
+                    run_result["validation_result"]
+                    for run_result in self.run_results.values()
+                ]
+            return self._validation_results
+        elif group_by == "expectation_suite_name":
+            return self._list_validation_results_by_expectation_suite_name()
+        elif group_by == "data_asset_name":
+            return self._list_validation_results_by_data_asset_name()
+
+    def _list_validation_results_by_expectation_suite_name(self) -> dict:
+        if self._validation_results_by_expectation_suite_name is None:
+            self._validation_results_by_expectation_suite_name = {
+                expectation_suite_name: [
+                    run_result["validation_result"]
+                    for run_result in self.run_results.values()
+                    if run_result["validation_result"].meta["expectation_suite_name"]
+                    == expectation_suite_name
+                ]
+                for expectation_suite_name in self.list_expectation_suite_names()
+            }
+        return self._validation_results_by_expectation_suite_name
+
+    def _list_validation_results_by_data_asset_name(self) -> dict:
+        if self._validation_results_by_data_asset_name is None:
+            validation_results_by_data_asset_name = {}
+            for data_asset_name in self.list_data_asset_names():
+                if data_asset_name == "__none__":
+                    validation_results_by_data_asset_name[data_asset_name] = [
+                        data_asset["validation_results"]
+                        for data_asset in self.list_data_assets_validated()
+                        if data_asset["batch_kwargs"].get("data_asset_name") is None
+                    ]
+                else:
+                    validation_results_by_data_asset_name[data_asset_name] = [
+                        data_asset["validation_results"]
+                        for data_asset in self.list_data_assets_validated()
+                        if data_asset["batch_kwargs"].get("data_asset_name")
+                        == data_asset_name
+                    ]
+            self._validation_results_by_data_asset_name = (
+                validation_results_by_data_asset_name
+            )
+        return self._validation_results_by_data_asset_name
+
+    def list_data_assets_validated(
+        self, group_by: str = None
+    ) -> Union[List[dict], dict]:
+        if group_by is None:
+            if self._data_assets_validated is None:
+                self._data_assets_validated = list(
+                    self._list_data_assets_validated_by_batch_id().values()
+                )
+            return self._data_assets_validated
+        if group_by == "batch_id":
+            return self._list_data_assets_validated_by_batch_id()
+
+    def _list_data_assets_validated_by_batch_id(self) -> dict:
         if self._data_assets_validated_by_batch_id is None:
             assets_validated_by_batch_id = {}
 
-            for validation_result in self.validation_results:
+            for validation_result in self.list_validation_results():
                 batch_kwargs = validation_result.meta["batch_kwargs"]
                 batch_id = BatchKwargs(batch_kwargs).to_id()
                 expectation_suite_name = validation_result.meta[
@@ -178,63 +216,14 @@ class ValidationOperatorResult(DictDot):
             self._data_assets_validated_by_batch_id = assets_validated_by_batch_id
         return self._data_assets_validated_by_batch_id
 
-    @property
-    def success(self) -> bool:
-        if self._success is None:
-            self._success = all(
-                [
-                    run_result["validation_result"].success
-                    for run_result in self.run_results.values()
-                ]
-            )
-        return self._success
-
-    @property
-    def validation_results_by_expectation_suite_name(self) -> dict:
-        if self._validation_results_by_expectation_suite_name is None:
-            self._validation_results_by_expectation_suite_name = {
-                expectation_suite_name: [
-                    run_result["validation_result"]
-                    for run_result in self.run_results.values()
-                    if run_result["validation_result"].meta["expectation_suite_name"]
-                    == expectation_suite_name
-                ]
-                for expectation_suite_name in self.expectation_suite_names
-            }
-        return self._validation_results_by_expectation_suite_name
-
-    @property
-    def validation_results_by_data_asset_name(self) -> dict:
-        if self._validation_results_by_data_asset_name is None:
-            validation_results_by_data_asset_name = {}
-            for data_asset_name in self.data_asset_names:
-                if data_asset_name == "__none__":
-                    validation_results_by_data_asset_name[data_asset_name] = [
-                        data_asset["validation_results"]
-                        for data_asset in self.data_assets_validated
-                        if data_asset["batch_kwargs"].get("data_asset_name") is None
-                    ]
-                else:
-                    validation_results_by_data_asset_name[data_asset_name] = [
-                        data_asset["validation_results"]
-                        for data_asset in self.data_assets_validated
-                        if data_asset["batch_kwargs"].get("data_asset_name")
-                        == data_asset_name
-                    ]
-            self._validation_results_by_data_asset_name = (
-                validation_results_by_data_asset_name
-            )
-        return self._validation_results_by_data_asset_name
-
-    @property
-    def statistics(self) -> dict:
+    def get_statistics(self) -> dict:
         if self._statistics is None:
-            data_asset_count = len(self.data_assets_validated)
-            validation_result_count = len(self.validation_results)
+            data_asset_count = len(self.list_data_assets_validated())
+            validation_result_count = len(self.list_validation_results())
             successful_validation_count = len(
                 [
                     validation_result
-                    for validation_result in self.validation_results
+                    for validation_result in self.list_validation_results()
                     if validation_result.success
                 ]
             )
