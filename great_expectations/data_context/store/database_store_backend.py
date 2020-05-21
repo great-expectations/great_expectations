5b3bd3abe58bb8773b4cfc58859407e30a749c1d
James Campbell <james.p.campbell@gmail.com>
2020-05-21 18:32:45 -0400

    Add 'value' column name to expected existing schema check

--- great_expectations/data_context/store/database_store_backend.py
+++ great_expectations/data_context/store/database_store_backend.py
@@ -58,8 +58,8 @@ class DatabaseStoreBackend(StoreBackend):
         try:
             table = Table(table_name, meta, autoload=True, autoload_with=self.engine)
             # We do a "light" check: if the columns' names match, we will proceed, otherwise, create the table
-            if set([str(col.name).lower() for col in table.columns]) != set(
-                key_columns
+            if set([str(col.name).lower() for col in table.columns]) != (
+                set(key_columns) | {"value"}
             ):
                 raise ge_exceptions.StoreBackendError(
                     f"Unable to use table {table_name}: it exists, but does not have the expected schema."
