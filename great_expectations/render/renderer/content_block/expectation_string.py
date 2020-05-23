cbcb6cbfd72658c4fda57e1c48d92fbcce7ce7f0
Malte Londschien <malte@londschien.de>
2020-05-23 15:42:47 +0200

    Added expectation string.

--- great_expectations/render/renderer/content_block/expectation_string.py
+++ great_expectations/render/renderer/content_block/expectation_string.py
@@ -319,6 +319,32 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         ]
 
     @classmethod
+    def expect_column_pair_values_to_be_independent(
+        cls, expectation, styling=None, include_column_name=True
+    ):
+        params = substitute_none_for_missing(
+            expectation.kwargs, ["column_A", "column_B"]
+        )
+
+        if (params["column_A"] is None) or (params["column_B"] is None):
+            template_str = " unrecognized kwargs for expect_column_pair_values_to_be_independent: missing column."
+
+        template_str = "Values in $column_A and $column_B must be independent."
+
+        return [
+            RenderedStringTemplateContent(
+                **{
+                    "content_block_type": "string_template",
+                    "string_template": {
+                        "template": template_str,
+                        "params": params,
+                        "styling": styling,
+                    },
+                }
+            )
+        ]
+
+    @classmethod
     def expect_multicolumn_values_to_be_unique(
         cls, expectation, styling=None, include_column_name=True
     ):
