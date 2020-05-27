278c339cedc916a2623951639c1b1817c62c310c
Robert Moses Lim <robert.m.lim@gmail.com>
2020-05-27 00:54:41 -0700

    isort

--- great_expectations/validation_operators/types/validation_operator_result.py
+++ great_expectations/validation_operators/types/validation_operator_result.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 from typing import Dict, List, Union
 
+from marshmallow import Schema, fields, post_load, pre_dump
+
 from great_expectations.core import (
     ExpectationSuiteValidationResult,
     RunIdentifier,
@@ -12,7 +14,6 @@ from great_expectations.data_context.types.resource_identifiers import (
     ValidationResultIdentifier,
 )
 from great_expectations.types import DictDot
-from marshmallow import Schema, fields, post_load, pre_dump
 
 
 class ValidationOperatorResult(DictDot):
--- tests/render/renderer/test_suite_edit_notebook_renderer.py
+++ tests/render/renderer/test_suite_edit_notebook_renderer.py
@@ -3,13 +3,14 @@ import os
 
 import nbformat
 import pytest
+from nbconvert.preprocessors import ExecutePreprocessor
+
 from great_expectations import DataContext
 from great_expectations.cli.suite import _suite_edit
 from great_expectations.core import ExpectationSuiteSchema
 from great_expectations.render.renderer.suite_edit_notebook_renderer import (
     SuiteEditNotebookRenderer,
 )
-from nbconvert.preprocessors import ExecutePreprocessor
 
 
 @pytest.fixture
