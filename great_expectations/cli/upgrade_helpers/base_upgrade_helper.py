319ac39c011c2ca22e2137f2ac8ba3a7d5e2c490
Robert Moses Lim <robert.m.lim@gmail.com>
2020-06-02 01:03:23 -0700

    Add get_upgrade_overview abstract method

--- great_expectations/cli/upgrade_helpers/base_upgrade_helper.py
+++ great_expectations/cli/upgrade_helpers/base_upgrade_helper.py
@@ -5,5 +5,9 @@ class BaseUpgradeHelper(ABC):
     """Base UpgradeHelper abstract class"""
 
     @abstractmethod
+    def get_upgrade_overview(self):
+        pass
+
+    @abstractmethod
     def upgrade_project(self):
         pass
