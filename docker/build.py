53eef973c822a938b8363e98ac7ce2f5a5e17d85
Daniel Mateus Pires <dpires@earnestresearch.com>
2020-06-11 15:06:21 +0100

    Fix docker repository name vs tag name

--- docker/build.py
+++ docker/build.py
@@ -1,3 +1,4 @@
+from docker import APIClient
 import docker
 import logging
 import click
@@ -20,12 +21,14 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+logger = logging.getLogger(__name__)
+
 PYTHON_DOCKER_TAGS = [
     "3.6-buster",
     "3.7-buster",
 ]
 
-DOCKER_REPOSITORY = "great-expectations"
+DOCKER_REPOSITORY = "great-expectations/great_expectations"
 
 client = docker.from_env()
 python_docker_tags_opt = click.option(
@@ -39,10 +42,16 @@ python_docker_tags_opt = click.option(
 great_expectations_version_opt = click.option(
     "--ge-version", "-g", default=versioneer.get_version(), show_default=True
 )
+docker_repository_opt = click.option(
+    "--repository",
+    "-r",
+    default=DOCKER_REPOSITORY,
+    help="Target repository for the docker images",
+)
 
 
 def mk_image_tag(python_docker_tag: str, ge_version: str):
-    return f"great_expectations:python-{python_docker_tag}_ge-{ge_version}"
+    return f"python-{python_docker_tag}-ge-{ge_version}"
 
 
 @click.group()
@@ -53,31 +62,51 @@ def cli():
 @cli.command()
 @python_docker_tags_opt
 @great_expectations_version_opt
-def build_images(python_docker_tags: List[str], ge_version: str):
+@docker_repository_opt
+def build_images(python_docker_tags: List[str], ge_version: str, repository: str):
     for python_docker_tag in python_docker_tags:
         tag = mk_image_tag(python_docker_tag, ge_version)
+        image_name = f"{repository}:{tag}"
 
-        logging.info("Building image with tag: " + tag)
+        logger.info("Building image: " + image_name)
 
-        client.images.build(
-            path="docker/", tag=tag, buildargs={"PYTHON_DOCKER_TAG": python_docker_tag}
-        )
+        for line in client.api.build(
+            path=".",
+            dockerfile="docker/Dockerfile",
+            tag=image_name,
+            buildargs={"PYTHON_DOCKER_TAG": python_docker_tag},
+            rm=True,
+            decode=True,
+        ):
+            stripped = line.get("stream", "").strip()
+            if stripped:
+                logger.info(stripped)
 
 
 @cli.command()
 @python_docker_tags_opt
 @great_expectations_version_opt
-@click.option(
-    "--repository",
-    "-r",
-    default=DOCKER_REPOSITORY,
-    help="Target repository for the docker images",
-)
+@docker_repository_opt
 def push_images(python_docker_tags: List[str], ge_version: str, repository: str):
     for python_docker_tag in python_docker_tags:
         tag = mk_image_tag(python_docker_tag, ge_version)
 
-        logging.info(f"Pushing image {repository}/{tag}")
+        logger.info(f"Pushing image {repository}:{tag}")
+
+        for line in client.api.push(
+            repository=repository, tag=tag, stream=True, decode=True
+        ):
+            status = line.get("status", "").strip()
+            if status:
+                progress = line.get("progress", "").strip()
+                if progress:
+                    logger.info(f"{status}: {progress}")
+                else:
+                    logger.info(status)
+
+            error_detail = line.get("errorDetail", {}).get("message", "").strip()
+            if error_detail:
+                logger.error(error_detail)
 
 
 if __name__ == "__main__":
