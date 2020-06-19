a906b4ba1c62813a094136a39e73ed36c472f47b
Eugene Mandel <eugene.mandel@gmail.com>
2020-06-19 12:35:49 -0700

    [DOCS] How to guide: How to add a Validation Operator (#1609)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update docs/how_to_guides/validation/how_to_add_a_validation_operator.rst
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * Update docs/how_to_guides/validation/how_to_add_a_validation_operator.rst
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * Update docs/how_to_guides/validation/how_to_add_a_validation_operator.rst
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * Update docs/how_to_guides/validation/how_to_add_a_validation_operator.rst
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * use double backticks for var names
    
    * added a ref link to the first mention of the ActionListValidationOperator class
    
    * typo
    
    * Added the links to the Validation Operator implementations included in GE
    
    * Removed the how to guide stubs for ActionListValidationOperator and WarningAndFailureExpectationSuitesValidationOperator, moved the content into the docstrings
    
    * minor edits
    
    * Moved the module docstring to the top
    
    * minor edits in response to PR comments
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>

--- great_expectations/validation_operators/actions.py
+++ great_expectations/validation_operators/actions.py
@@ -1,3 +1,9 @@
+"""
+An action is a way to take an arbitrary method and make it configurable and runnable within a Data Context.
+
+The only requirement from an action is for it to have a take_action method.
+"""
+
 import logging
 import warnings
 
@@ -14,9 +20,9 @@ logger = logging.getLogger(__name__)
 class ValidationAction(object):
     """
     This is the base class for all actions that act on validation results
-    and are aware of a data context namespace structure.
+    and are aware of a Data Context namespace structure.
 
-    The data context is passed to this class in its constructor.
+    The Data Context is passed to this class in its constructor.
     """
 
     def __init__(self, data_context):
@@ -64,17 +70,25 @@ class NoOpAction(ValidationAction):
 
 class SlackNotificationAction(ValidationAction):
     """
-    SlackNotificationAction sends a Slack notification to a given webhook.
+SlackNotificationAction sends a Slack notification to a given webhook.
 
-    Example config:
-    {
-        "renderer": {
-            "module_name": "great_expectations.render.renderer.slack_renderer",
-            "class_name": "SlackRenderer",
-        },
-        "slack_webhook": "https://example_webhook",
-        "notify_on": "all"
-    }
+**Configuration**
+
+.. code-block:: yaml
+
+    - name: send_slack_notification_on_validation_result
+    action:
+      class_name: StoreValidationResultAction
+      # put the actual webhook URL in the uncommitted/config_variables.yml file
+      slack_webhook: ${validation_notification_slack_webhook}
+      notify_on: all # possible values: "all", "failure", "success"
+      renderer:
+        # the class that implements the message to be sent
+        # this is the default implementation, but you can
+        # implement a custom one
+        module_name: great_expectations.render.renderer.slack_renderer
+        class_name: SlackRenderer
+
     """
 
     def __init__(
@@ -145,6 +159,18 @@ class SlackNotificationAction(ValidationAction):
 class StoreValidationResultAction(ValidationAction):
     """
     StoreValidationResultAction stores a validation result in the ValidationsStore.
+
+**Configuration**
+
+.. code-block:: yaml
+
+    - name: store_validation_result
+    action:
+      class_name: StoreValidationResultAction
+      # name of the store where the actions will store validation results
+      # the name must refer to a store that is configured in the great_expectations.yml file
+      target_store_name: validations_store
+
     """
 
     def __init__(
@@ -152,8 +178,8 @@ class StoreValidationResultAction(ValidationAction):
     ):
         """
 
-        :param data_context: data context
-        :param target_store_name: the name of the param_store in the data context which
+        :param data_context: Data Context
+        :param target_store_name: the name of the param_store in the Data Context which
                 should be used to param_store the validation result
         """
 
@@ -187,20 +213,31 @@ class StoreValidationResultAction(ValidationAction):
 
 class StoreEvaluationParametersAction(ValidationAction):
     """
-    StoreEvaluationParametersAction is a namespeace-aware validation action that
-    extracts evaluation parameters from a validation result and stores them in the param_store
-    configured for this action.
+StoreEvaluationParametersAction extracts evaluation parameters from a validation result and stores them in the store
+configured for this action.
 
-    Evaluation parameters allow expectations to refer to statistics/metrics computed
-    in the process of validating other prior expectations.
+Evaluation parameters allow expectations to refer to statistics/metrics computed
+in the process of validating other prior expectations.
+
+**Configuration**
+
+.. code-block:: yaml
+
+    - name: store_evaluation_params
+    action:
+      class_name: StoreEvaluationParametersAction
+      # name of the store where the action will store the parameters
+      # the name must refer to a store that is configured in the great_expectations.yml file
+      target_store_name: evaluation_parameter_store
+
     """
 
     def __init__(self, data_context, target_store_name=None):
         """
 
         Args:
-            data_context: data context
-            target_store_name: the name of the store in the data context which
+            data_context: Data Context
+            target_store_name: the name of the store in the Data Context which
                 should be used to store the evaluation parameters
         """
         super(StoreEvaluationParametersAction, self).__init__(data_context)
@@ -231,13 +268,30 @@ class StoreEvaluationParametersAction(ValidationAction):
 
 
 class StoreMetricsAction(ValidationAction):
+    """
+StoreMetricsAction extracts metrics from a Validation Result and stores them
+in a metrics store.
+
+**Configuration**
+
+.. code-block:: yaml
+
+    - name: store_evaluation_params
+    action:
+      class_name: StoreMetricsAction
+      # name of the store where the action will store the metrics
+      # the name must refer to a store that is configured in the great_expectations.yml file
+      target_store_name: my_metrics_store
+
+    """
+
     def __init__(
         self, data_context, requested_metrics, target_store_name="metrics_store"
     ):
         """
 
         Args:
-            data_context: data context
+            data_context: Data Context
             requested_metrics: dictionary of metrics to store. Dictionary should have the following structure:
 
                 expectation_suite_name:
@@ -245,7 +299,7 @@ class StoreMetricsAction(ValidationAction):
                         - metric_kwargs_id
 
                 You may use "*" to denote that any expectation suite should match.
-            target_store_name: the name of the store in the data context which
+            target_store_name: the name of the store in the Data Context which
                 should be used to store the metrics
         """
         super(StoreMetricsAction, self).__init__(data_context)
@@ -288,14 +342,32 @@ class StoreMetricsAction(ValidationAction):
 
 class UpdateDataDocsAction(ValidationAction):
     """
-    UpdateDataDocsAction is a namespace-aware validation action that
-    notifies the site builders of all the data docs sites of the data context
-    that a validation result should be added to the data docs.
+UpdateDataDocsAction is a validation action that
+notifies the site builders of all the data docs sites of the Data Context
+that a validation result should be added to the data docs.
+
+**Configuration**
+
+.. code-block:: yaml
+
+    - name: update_data_docs
+    action:
+      class_name: UpdateDataDocsAction
+
+You can also instruct ``UpdateDataDocsAction`` to build only certain sites by providing a ``site_names`` key with a
+list of sites to update:
+
+    - name: update_data_docs
+    action:
+      class_name: UpdateDataDocsAction
+      site_names:
+        - production_site
+
     """
 
     def __init__(self, data_context, site_names=None, target_site_names=None):
         """
-        :param data_context: data context
+        :param data_context: Data Context
         :param site_names: *optional* List of site names for building data docs
         """
         super(UpdateDataDocsAction, self).__init__(data_context)
