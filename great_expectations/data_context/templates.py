f3204e3d4ad599f1548759cefdb59d38d957e075
talagluck <tal@superconductive.com>
2020-06-19 20:26:58 +0000

    [ENHANCEMENT] added env variable substitution for config_variables.yml (#1447)
    * added env variable substitution and test for vars referenced in config_variables.yml
    
    * added test to check conflicting environment variables from different sources
    
    * Fix path typo
    
    * Update changelog
    
    * Update config_variables.yml template
    
    * Update how-to guide
    
    * Tweak function
    
    - only get env var if the config_variable_value itself has a env var sub
    
    * catch TypeError
    
    * Fix test
    
    * Isort
    
    * Lint
    
    Co-authored-by: Robert Moses Lim <robert.m.lim@gmail.com>

--- great_expectations/data_context/templates.py
+++ great_expectations/data_context/templates.py
@@ -32,12 +32,10 @@ CONFIG_VARIABLES_INTRO = """
 # Environment variables take precedence over variables defined here.
 #
 # Substitution values defined here can be a simple (non-nested) value,
-# or a nested value such as a dictionary.
+# nested value such as a dictionary, or an environment variable (i.e. ${ENV_VAR})
 #
-# NOTE: Environment variables are currently *only* supported in
-# great_expectations.yml, but not in this config file!
 #
-# https://docs.greatexpectations.io/en/latest/reference/data_context_reference.html#managing-environment-and-secrets
+# https://docs.greatexpectations.io/en/latest/how_to_guides/configuring_data_contexts/how_to_use_a_yaml_file_or_environment_variables_to_populate_credentials.html
 
 """
 
