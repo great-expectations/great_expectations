0298cc571ccfdd54abe55387556d6f27e0a3f97f
Daniel Mateus Pires <dpires@earnestresearch.com>
2020-06-23 16:55:43 +0100

    Hello world, modify generated notebook

--- great_expectations/render/renderer/suite_edit_notebook_renderer.py
+++ great_expectations/render/renderer/suite_edit_notebook_renderer.py
@@ -239,7 +239,8 @@ context.open_data_docs(validation_result_identifier)"""
 
 Add expectations by calling specific expectation methods on the `batch` object. They all begin with `.expect_` which makes autocompleting easy using tab.
 
-You can see all the available expectations in the **[expectation glossary](https://docs.greatexpectations.io/en/latest/reference/glossary_of_expectations.html?utm_source=notebook&utm_medium=create_expectations)**."""
+You can see all the available expectations in the **[expectation glossary](https://docs.greatexpectations.io/en/latest/reference/glossary_of_expectations.html?utm_source=notebook&utm_medium=create_expectations)**.
+Hello world"""
         )
 
     def get_batch_kwargs(
