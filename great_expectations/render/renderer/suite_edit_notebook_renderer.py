cc7018b2608e400bbb3c5aa618ec86ecdce9cfb0
Daniel Mateus Pires <dpires@earnestresearch.com>
2020-07-07 20:56:05 +0100

    [FEATURE] Customizable "Suite Edit" generated notebooks (#1642)
    * Hello world, modify generated notebook
    
    * Separate markdown files from suite edit generation
    
    * Render using markdown files
    
    * Add notebook assets to manifest
    
    * Use code files instead of code strings
    
    * Able to overwrite markdown section
    
    * Cover cases with no template kwargs
    
    * Make all markdown sections template-able
    
    * Add code section overwrites and test
    
    * Rename python template files in main project to include .j2 extension so they are not included by apidocs
    
    * Lint and update test affected by format of output
    
    * Add documentation
    
    * Add j2 files to manifest
    
    * Run pre-commit hooks on all files
    
    * Pass class name explicitly
    
    * Update docs/how_to_guides/configuring_generated_notebooks/how_to_configure_suite_edit_generated_notebooks.rst
    
    Co-authored-by: Taylor Miller <Aylr@users.noreply.github.com>
    
    * Update docs/how_to_guides/configuring_generated_notebooks/how_to_configure_suite_edit_generated_notebooks.rst
    
    Co-authored-by: Taylor Miller <Aylr@users.noreply.github.com>
    
    * Update changelog
    
    Co-authored-by: Taylor Miller <Aylr@users.noreply.github.com>

--- great_expectations/render/renderer/suite_edit_notebook_renderer.py
+++ great_expectations/render/renderer/suite_edit_notebook_renderer.py
@@ -1,10 +1,19 @@
 import os
-from typing import Union
+from typing import Dict, Optional, Union
 
+import jinja2
 import nbformat
 
 from great_expectations.core import ExpectationSuite
 from great_expectations.core.id_dict import BatchKwargs
+from great_expectations.data_context.types.base import (
+    NotebookTemplateConfig,
+    notebookConfigSchema,
+)
+from great_expectations.data_context.util import instantiate_class_from_config
+from great_expectations.exceptions import (
+    SuiteEditNotebookCustomTemplateModuleNotFoundError,
+)
 from great_expectations.render.renderer.renderer import Renderer
 from great_expectations.util import lint_code
 
@@ -17,6 +26,81 @@ class SuiteEditNotebookRenderer(Renderer):
     - Make an easy path to edit a suite that a Profiler created.
     - Make it easy to edit a suite where only JSON exists.
     """
+
+    def __init__(
+        self,
+        custom_templates_module: Optional[str] = None,
+        header_markdown: Optional[NotebookTemplateConfig] = None,
+        footer_markdown: Optional[NotebookTemplateConfig] = None,
+        table_expectations_header_markdown: Optional[NotebookTemplateConfig] = None,
+        column_expectations_header_markdown: Optional[NotebookTemplateConfig] = None,
+        table_expectations_not_found_markdown: Optional[NotebookTemplateConfig] = None,
+        column_expectations_not_found_markdown: Optional[NotebookTemplateConfig] = None,
+        authoring_intro_markdown: Optional[NotebookTemplateConfig] = None,
+        column_expectations_markdown: Optional[NotebookTemplateConfig] = None,
+        header_code: Optional[NotebookTemplateConfig] = None,
+        footer_code: Optional[NotebookTemplateConfig] = None,
+        column_expectation_code: Optional[NotebookTemplateConfig] = None,
+        table_expectation_code: Optional[NotebookTemplateConfig] = None,
+    ):
+        super().__init__()
+        custom_loader = []
+
+        if custom_templates_module:
+            try:
+                custom_loader = [
+                    jinja2.PackageLoader(*custom_templates_module.rsplit(".", 1))
+                ]
+            except ModuleNotFoundError as e:
+                raise SuiteEditNotebookCustomTemplateModuleNotFoundError(
+                    custom_templates_module
+                ) from e
+
+        loaders = custom_loader + [
+            jinja2.PackageLoader(
+                "great_expectations.render.notebook_assets", "suite_edit"
+            ),
+        ]
+        self.template_env = None
+
+        self.template_env = jinja2.Environment(loader=jinja2.ChoiceLoader(loaders))
+
+        self.header_markdown = header_markdown
+        self.footer_markdown = footer_markdown
+        self.table_expectations_header_markdown = table_expectations_header_markdown
+        self.column_expectations_header_markdown = column_expectations_header_markdown
+        self.table_expectations_not_found_markdown = (
+            table_expectations_not_found_markdown
+        )
+        self.column_expectations_not_found_markdown = (
+            column_expectations_not_found_markdown
+        )
+        self.authoring_intro_markdown = authoring_intro_markdown
+        self.column_expectations_markdown = column_expectations_markdown
+
+        self.header_code = header_code
+        self.footer_code = footer_code
+        self.column_expectation_code = column_expectation_code
+        self.table_expectation_code = table_expectation_code
+
+    @staticmethod
+    def from_data_context(data_context):
+        suite_edit_notebook_config: Optional[NotebookConfig] = None
+        if data_context.notebooks and data_context.notebooks.get("suite_edit"):
+            suite_edit_notebook_config = notebookConfigSchema.load(
+                data_context.notebooks.get("suite_edit")
+            )
+
+        return instantiate_class_from_config(
+            config=suite_edit_notebook_config.__dict__
+            if suite_edit_notebook_config
+            else {
+                "module_name": "great_expectations.render.renderer.suite_edit_notebook_renderer",
+                "class_name": "SuiteEditNotebookRenderer",
+            },
+            runtime_environment={},
+            config_defaults={},
+        )
 
     @classmethod
     def _get_expectations_by_column(cls, expectations):
@@ -50,79 +134,48 @@ class SuiteEditNotebookRenderer(Renderer):
 
         return ", ".join(kwargs)
 
-    def add_header(self, suite_name: str, batch_kwargs) -> None:
-        self.add_markdown_cell(
-            """# Edit Your Expectation Suite
-Use this notebook to recreate and modify your expectation suite:
-
-**Expectation Suite Name**: `{}`
-
-We'd love it if you **reach out to us on** the [**Great Expectations Slack Channel**](https://greatexpectations.io/slack)""".format(
-                suite_name
+    def render_with_overwrite(
+        self,
+        notebook_config: Optional[NotebookTemplateConfig],
+        default_file_name: str,
+        **default_kwargs
+    ):
+        if notebook_config:
+            rendered = self.template_env.get_template(notebook_config.file_name).render(
+                **default_kwargs, **notebook_config.template_kwargs
             )
+        else:
+            rendered = self.template_env.get_template(default_file_name).render(
+                **default_kwargs
+            )
+        return rendered
+
+    def add_header(self, suite_name: str, batch_kwargs) -> None:
+        markdown = self.render_with_overwrite(
+            self.header_markdown, "HEADER.md", suite_name=suite_name
         )
+
+        self.add_markdown_cell(markdown)
 
         if not batch_kwargs:
             batch_kwargs = dict()
-        self.add_code_cell(
-            """\
-import datetime
-import great_expectations as ge
-import great_expectations.jupyter_ux
-from great_expectations.data_context.types.resource_identifiers import ValidationResultIdentifier
-
-context = ge.data_context.DataContext()
-
-# Feel free to change the name of your suite here. Renaming this will not
-# remove the other one.
-expectation_suite_name = "{}"
-suite = context.get_expectation_suite(expectation_suite_name)
-suite.expectations = []
-
-batch_kwargs = {}
-batch = context.get_batch(batch_kwargs, suite)
-batch.head()""".format(
-                suite_name, batch_kwargs
-            ),
-            lint=True,
+        code = self.render_with_overwrite(
+            self.header_code,
+            "header.py.j2",
+            suite_name=suite_name,
+            batch_kwargs=batch_kwargs,
         )
+        self.add_code_cell(code, lint=True)
 
     def add_footer(self) -> None:
-        self.add_markdown_cell(
-            """\
-## Save & Review Your Expectations
-
-Let's save the expectation suite as a JSON file in the `great_expectations/expectations` directory of your project.
-If you decide not to save some expectations that you created, use [remove_expectation method](https://docs.greatexpectations.io/en/latest/module_docs/data_asset_module.html?highlight=remove_expectation&utm_source=notebook&utm_medium=edit_expectations#great_expectations.data_asset.data_asset.DataAsset.remove_expectation).
-
-Let's now rebuild your Data Docs, which helps you communicate about your data with both machines and humans."""
-        )
+        markdown = self.render_with_overwrite(self.footer_markdown, "FOOTER.md")
+        self.add_markdown_cell(markdown)
         # TODO this may become confusing for users depending on what they are trying
         #  to accomplish in their dev loop
-        self.add_code_cell(
-            """\
-batch.save_expectation_suite(discard_failed_expectations=False)
+        code = self.render_with_overwrite(self.footer_code, "footer.py.j2")
+        self.add_code_cell(code)
 
-\"""
-Let's create a run_id. The run_id must be of type RunIdentifier, with optional run_name and run_time instantiation
-arguments (or a dictionary with these keys). The run_name can be any string (this could come from your pipeline
-runner, e.g. Airflow run id). The run_time can be either a dateutil parsable string or a datetime object.
-Note - any provided datetime will be assumed to be a UTC time. If no instantiation arguments are given, run_name will
-be None and run_time will default to the current UTC datetime.
-\"""
-
-run_id = {
-  "run_name": "some_string_that_uniquely_identifies_this_run",  # insert your own run_name here
-  "run_time": datetime.datetime.now(datetime.timezone.utc)
-}
-
-results = context.run_validation_operator("action_list_operator", assets_to_validate=[batch], run_id=run_id)
-validation_result_identifier = results.list_validation_result_identifiers()[0]
-context.build_data_docs()
-context.open_data_docs(validation_result_identifier)"""
-        )
-
-    def add_code_cell(self, code: str, lint: bool = False) -> None:
+    def add_code_cell(self, code: str, lint: bool = False, **template_params) -> None:
         """
         Add the given code as a new code cell.
         """
@@ -141,43 +194,65 @@ context.open_data_docs(validation_result_identifier)"""
 
     def add_expectation_cells_from_suite(self, expectations):
         expectations_by_column = self._get_expectations_by_column(expectations)
-        self.add_markdown_cell("### Table Expectation(s)")
+        markdown = self.render_with_overwrite(
+            self.table_expectations_header_markdown, "TABLE_EXPECTATIONS_HEADER.md"
+        )
+        self.add_markdown_cell(markdown)
         self._add_table_level_expectations(expectations_by_column)
         # Remove the table expectations since they are dealt with
         expectations_by_column.pop("table_expectations")
-        self.add_markdown_cell("### Column Expectation(s)")
+        markdown = self.render_with_overwrite(
+            self.column_expectations_header_markdown, "COLUMN_EXPECTATIONS_HEADER.md"
+        )
+        self.add_markdown_cell(markdown)
         self._add_column_level_expectations(expectations_by_column)
 
     def _add_column_level_expectations(self, expectations_by_column):
         if not expectations_by_column:
-            self.add_markdown_cell(
-                "No column level expectations are in this suite. Feel free to "
-                "add some here. They all begin with `batch.expect_column_...`."
+            markdown = self.render_with_overwrite(
+                self.column_expectations_not_found_markdown,
+                "COLUMN_EXPECTATIONS_NOT_FOUND.md",
             )
+            self.add_markdown_cell(markdown)
             return
 
         for column, expectations in expectations_by_column.items():
-            self.add_markdown_cell("#### `{}`".format(column))
+            markdown = self.render_with_overwrite(
+                self.column_expectations_markdown,
+                "COLUMN_EXPECTATIONS.md",
+                column=column,
+            )
+            self.add_markdown_cell(markdown)
 
             for exp in expectations:
-                kwargs_string = self._build_kwargs_string(exp)
-                meta_args = self._build_meta_arguments(exp.meta)
-                code = "batch.{}({}{})".format(
-                    exp["expectation_type"], kwargs_string, meta_args
+                code = self.render_with_overwrite(
+                    self.column_expectation_code,
+                    "column_expectation.py.j2",
+                    expectation=exp,
+                    kwargs_string=self._build_kwargs_string(exp),
+                    meta_args=self._build_meta_arguments(exp.meta),
                 )
-                self.add_code_cell(code, lint=True)
+                self.add_code_cell(
+                    code, lint=True,
+                )
 
     def _add_table_level_expectations(self, expectations_by_column):
         if not expectations_by_column["table_expectations"]:
-            self.add_markdown_cell(
-                "No table level expectations are in this suite. Feel free to "
-                "add some here. They all begin with `batch.expect_table_...`."
+            markdown = self.render_with_overwrite(
+                self.table_expectations_not_found_markdown,
+                "TABLE_EXPECTATIONS_NOT_FOUND.md",
             )
+            self.add_markdown_cell(markdown)
             return
 
         for exp in expectations_by_column["table_expectations"]:
-            kwargs_string = self._build_kwargs_string(exp)
-            code = "batch.{}({})".format(exp["expectation_type"], kwargs_string)
+            code = self.render_with_overwrite(
+                self.table_expectation_code,
+                "table_expectation.py.j2",
+                expectation=exp,
+                kwargs_string=self._build_kwargs_string(exp),
+                meta_args=self._build_meta_arguments(exp.meta),
+            )
             self.add_code_cell(code, lint=True)
 
     @staticmethod
@@ -233,14 +308,10 @@ context.open_data_docs(validation_result_identifier)"""
         self.write_notebook_to_disk(self._notebook, notebook_file_path)
 
     def add_authoring_intro(self):
-        self.add_markdown_cell(
-            """\
-## Create & Edit Expectations
-
-Add expectations by calling specific expectation methods on the `batch` object. They all begin with `.expect_` which makes autocompleting easy using tab.
-
-You can see all the available expectations in the **[expectation glossary](https://docs.greatexpectations.io/en/latest/reference/glossary_of_expectations.html?utm_source=notebook&utm_medium=create_expectations)**."""
+        markdown = self.render_with_overwrite(
+            self.authoring_intro_markdown, "AUTHORING_INTRO.md"
         )
+        self.add_markdown_cell(markdown)
 
     def get_batch_kwargs(
         self, suite: ExpectationSuite, batch_kwargs: Union[dict, BatchKwargs]
