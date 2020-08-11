7e0e88497954ea2d62ce937e600f42ebb7666f36
Abe Gong <abegong@users.noreply.github.com>
2020-08-10 18:34:36 -0600

    Update conf.py
    Trivial commit to re-trigger the stuck RTD build.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
 import re
 import sys
