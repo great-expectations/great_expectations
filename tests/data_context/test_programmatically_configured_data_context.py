f564fbf1a44609cb00c24dbfdf9872afce422359
Alex Sherstinsky <alex@superconductivehealth.com>
2020-08-16 14:43:52 -0700

    Update tests for in-memory data context initialization.

--- tests/data_context/test_programmatically_configured_data_context.py
+++ tests/data_context/test_programmatically_configured_data_context.py
@@ -5,10 +5,8 @@ from ruamel.yaml import YAML
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations.data_context import DataContext
-from great_expectations.data_context.types.base import (
-    DATA_CONTEXT_ID,
-    DataContextConfig,
-)
+from great_expectations.data_context.types.base import DataContextConfig
+from great_expectations.util import filter_properties_dict
 from tests.test_utils import not_raises
 
 yaml = YAML()
@@ -18,7 +16,7 @@ yaml.default_flow_style = False
 @pytest.fixture()
 def blank_in_memory_data_context() -> DataContext:
     context: DataContext = DataContext.create_blank_data_context(
-        allow_anonymous_usage_statistics=True, runtime_environment=None
+        runtime_environment=None, allow_anonymous_usage_statistics=True,
     )
     assert isinstance(context, DataContext)
     return context
@@ -40,51 +38,63 @@ def test_blank_context_structure_and_values(blank_in_memory_data_context):
     assert not set(context.list_validation_operator_names())
 
     assert not set(context.get_docs_sites_urls())
-    # assert(blank_in_memory_data_context.stores.keys() == {"anonymous_usage_statistics"})
 
-    project_yaml: str = context.get_config().to_yaml_str()
-    context_config_dict_from_yaml: dict = yaml.load(project_yaml)
+    project_yaml_str: str = context.get_project_config().to_yaml_str()
+    project_config_dict_from_yaml: dict = yaml.load(project_yaml_str)
 
-    assert len(context_config_dict_from_yaml.keys()) == 2
+    assert len(project_config_dict_from_yaml.keys()) == 12
+
+    assert (
+        len(
+            filter_properties_dict(
+                properties=project_config_dict_from_yaml,
+                clean_empty=True,
+                inplace=False,
+            ).keys()
+        )
+        == 2
+    )
+
+    assert set(
+        filter_properties_dict(
+            properties=project_config_dict_from_yaml, clean_empty=True, inplace=False
+        ).keys()
+    ) == {"config_version", "anonymous_usage_statistics"}
 
     data_context_config: DataContextConfig
     with not_raises(ge_exceptions.InvalidDataContextConfigError):
         # noinspection PyUnusedLocal
+        # noinspection PyTypeChecker
         data_context_config = DataContextConfig.from_commented_map(
-            context_config_dict_from_yaml
+            commented_map=project_config_dict_from_yaml
         )
 
     assert data_context_config.config_version == 2
 
     with not_raises(KeyError):
         # noinspection PyUnusedLocal
-        value: str = context_config_dict_from_yaml["anonymous_usage_statistics"]
+        value: str = project_config_dict_from_yaml["anonymous_usage_statistics"]
 
-    with not_raises(KeyError):
+    with pytest.raises(KeyError):
         # noinspection PyUnusedLocal
-        value: str = context_config_dict_from_yaml["anonymous_usage_statistics"][
+        value: str = project_config_dict_from_yaml["anonymous_usage_statistics"][
             "data_context_id"
         ]
 
-    assert (
-        data_context_config.anonymous_usage_statistics.data_context_id
-        == DATA_CONTEXT_ID
-    )
-
     with not_raises(KeyError):
         # noinspection PyUnusedLocal
-        value: bool = context_config_dict_from_yaml["anonymous_usage_statistics"][
+        value: bool = project_config_dict_from_yaml["anonymous_usage_statistics"][
             "enabled"
         ]
 
     with not_raises(KeyError):
         # noinspection PyUnusedLocal
-        value: str = context_config_dict_from_yaml["anonymous_usage_statistics"][
+        value: str = project_config_dict_from_yaml["anonymous_usage_statistics"][
             "usage_statistics_url"
         ]
 
     with not_raises(IOError):
-        usage_statistics_url: str = context_config_dict_from_yaml[
+        usage_statistics_url: str = project_config_dict_from_yaml[
             "anonymous_usage_statistics"
         ]["usage_statistics_url"]
         parse_result: ParseResult = urlparse(usage_statistics_url)
