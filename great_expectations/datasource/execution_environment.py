e98bf1bf2a112243c9efb6cb10bcc85eeb84e2fb
Tal Gluck <tal@superconductive.com>
2020-08-17 13:11:03 -0400

    moved a couple of methods

--- great_expectations/datasource/execution_environment.py
+++ great_expectations/datasource/execution_environment.py
@@ -215,3 +215,23 @@ An ExecutionEnvironment is the glue between an ExecutionEngine and a DataConnect
                 data_connectors.append({"name": key, "class_name": value["class_name"]})
 
         return data_connectors
+
+    # def build_batch_kwargs(
+    #     self, batch_kwargs_generator, data_asset_name=None, partition_id=None, **kwargs
+    # ):
+    #     if kwargs.get("name"):
+    #         if data_asset_name:
+    #             raise ValueError(
+    #                 "Cannot provide both 'name' and 'data_asset_name'. Please use 'data_asset_name' only."
+    #             )
+    #         warnings.warn(
+    #             "name is being deprecated as a batch_parameter. Please use data_asset_name instead.",
+    #             DeprecationWarning,
+    #         )
+    #         data_asset_name = kwargs.pop("name")
+    #     generator_obj = self.get_batch_kwargs_generator(batch_kwargs_generator)
+    #     if partition_id is not None:
+    #         kwargs["partition_id"] = partition_id
+    #     return generator_obj.build_batch_kwargs(
+    #         data_asset_name=data_asset_name, **kwargs
+    #     )
