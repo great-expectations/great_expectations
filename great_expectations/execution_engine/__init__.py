94611e08f0f445ce0ea299d622e56e3ecdd55d9b
Robert Moses Lim <robert.m.lim@gmail.com>
2020-08-18 08:33:01 -0700

    Create pandas_execution_engine module and move over util.py from dataset

---
+++ great_expectations/execution_engine/__init__.py
@@ -0,0 +1,6 @@
+from .execution_engine import ExecutionEngine
+from .pandas_execution_engine import MetaPandasExecutionEngine, PandasExecutionEngine
+
+import logging
+
+logger = logging.getLogger(__name__)
