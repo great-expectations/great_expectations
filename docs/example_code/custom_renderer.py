cfc01b47917c5133cd6d43468ab726d1195099b4
Alex Sherstinsky <alex@superconductivehealth.com>
2020-09-08 12:12:10 -0700

    isort

--- docs/example_code/custom_renderer.py
+++ docs/example_code/custom_renderer.py
@@ -10,7 +10,7 @@ from great_expectations.render.types import (
     RenderedSectionContent,
     RenderedStringTemplateContent,
     RenderedTableContent,
-    ValueListContent
+    ValueListContent,
 )
 
 
