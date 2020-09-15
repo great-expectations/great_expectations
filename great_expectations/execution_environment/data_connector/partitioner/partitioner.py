9a1149fe9ce34f7f2f9e36bacd1a9bdbd05f8cf3
William Shin <will@superconductive.com>
2020-09-14 22:15:51 -0700

    regex_partitioner and initial tests

---
+++ great_expectations/execution_environment/data_connector/partitioner/partitioner.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from great_expectations.core.id_dict import BatchSpec
+
+logger = logging.getLogger(__name__)
+
+class Partitioner(object):
+    r"""
+    Partitioners help
+    """
+
+    _batch_spec_type = BatchSpec  #TODO : is this really needed?
+    recognized_batch_definition_keys = {
+        "regex",
+        "ordered_fields"
+    }
+
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    # TODO : see if this can actually be reused
+    def get_available_partitions(self, **kwargs):
+        raise NotImplementedError
+
+    # TODO : see if this can actually be reused
+    def get_available_partition_keys(self, **kwargs):
+        raise NotImplementedError
+
+    # TODO : see if this can actually be reused
+    def get_available_partition_definitions(self, **kwargs):
+        raise NotImplementedError
+
+
