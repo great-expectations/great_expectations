6e7e426dafb03ee523952b7c4b787947900d0742
Alex Sherstinsky <alex@superconductivehealth.com>
2020-09-15 14:31:32 -0700

    Cleanup.

--- great_expectations/execution_environment/data_connector/partitioner/sorter.py
+++ great_expectations/execution_environment/data_connector/partitioner/sorter.py
@@ -2,21 +2,13 @@
 
 import logging
 
-from great_expectations.core.id_dict import BatchSpec
-
 logger = logging.getLogger(__name__)
 
 
 class Sorter(object):
     r"""
-    Partition help
+    Sorter help
     """
-
-    # _batch_spec_type = BatchSpec  #TODO : is this really needed?
-    # recognized_batch_definition_keys = {  #TODO : is this really needed?
-    #     "regex",
-    #     "partitions"
-    # }
 
     def __init__(self, name, **kwargs):
         self._name = name
@@ -31,15 +23,3 @@ class Sorter(object):
     @property
     def orderby(self):
         return self._orderby
-
-    # # TODO : see if this can actually be reused
-    # def get_available_partitions(self, **kwargs):
-    #     raise NotImplementedError
-    #
-    # # TODO : see if this can actually be reused
-    # def get_available_partition_names(self, **kwargs):
-    #     raise NotImplementedError
-    #
-    # # TODO : see if this can actually be reused
-    # def get_available_partition_definitions(self, **kwargs):
-    #     raise NotImplementedError
