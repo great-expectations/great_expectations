245d21e7b93312a6eb3dd264cd117d1f7bc53e17
Tal Gluck <tal@superconductive.com>
2020-09-16 13:46:12 -0400

    added expect_column_values_to_be_unique

---
+++ great_expectations/expectations/core/expect_column_values_to_be_unique.py
@@ -0,0 +1,97 @@
+from typing import Dict, List, Optional, Union
+
+import numpy as np
+import pandas as pd
+
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.execution_engine import PandasExecutionEngine
+
+from ...core.batch import Batch
+from ...data_asset.util import parse_result_format
+from ..expectation import (
+    ColumnMapDatasetExpectation,
+    Expectation,
+    InvalidExpectationConfigurationError,
+    _format_map_output,
+)
+from ..registry import extract_metrics, get_metric_kwargs
+
+
+class ExpectColumnValuesToBeUnique(ColumnMapDatasetExpectation):
+    map_metric = "map.are_unique"
+    metric_dependencies = ("map.are_unique.count", "map.nonnull.count")
+    success_keys = "mostly"
+
+    default_kwarg_values = {
+        "row_condition": None,
+        "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
+        "mostly": 1,
+        "result_format": "BASIC",
+        "include_config": True,
+        "catch_exceptions": False,
+    }
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        super().validate_configuration(configuration)
+        try:
+            assert (
+                "column" in configuration.kwargs
+            ), "'column' parameter is required for column map expectations"
+            if "mostly" in configuration.kwargs:
+                mostly = configuration.kwargs["mostly"]
+                assert isinstance(
+                    mostly, (int, float)
+                ), "'mostly' parameter must be an integer or float"
+                assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+        return True
+
+    @PandasExecutionEngine.column_map_metric(
+        metric_name="map.are_unique",
+        metric_domain_keys=ColumnMapDatasetExpectation.domain_keys,
+        metric_value_keys=("value_set",),
+        metric_dependencies=tuple(),
+    )
+    def _pandas_map_are_unique(
+        self,
+        series: pd.Series,
+        value_set: Union[list, set],
+        runtime_configuration: dict = None,
+    ):
+        return ~series.duplicated(keep=False)
+
+    @Expectation.validates(metric_dependencies=metric_dependencies)
+    def _validates(
+        self,
+        configuration: ExpectationConfiguration,
+        metrics: dict,
+        runtime_configuration: dict = None,
+    ):
+        validation_dependencies = self.get_validation_dependencies(configuration)[
+            "metrics"
+        ]
+        metric_vals = extract_metrics(validation_dependencies, metrics, configuration)
+        mostly = configuration.get_success_kwargs().get(
+            "mostly", self.default_kwarg_values.get("mostly")
+        )
+        if runtime_configuration:
+            result_format = runtime_configuration.get(
+                "result_format", self.default_kwarg_values.get("result_format")
+            )
+        else:
+            result_format = self.default_kwarg_values.get("result_format")
+        return _format_map_output(
+            result_format=parse_result_format(result_format),
+            success=(
+                metric_vals.get("map.are_unique.count")
+                / metric_vals.get("map.nonnull.count")
+            )
+            >= mostly,
+            element_count=metric_vals.get("map.count"),
+            nonnull_count=metric_vals.get("map.nonnull.count"),
+            unexpected_count=metric_vals.get("map.nonnull.count")
+            - metric_vals.get("map.are_unique.count"),
+            unexpected_list=metric_vals.get("map.are_unique.unexpected_values"),
+            unexpected_index_list=metric_vals.get("map.are_unique.unexpected_index"),
+        )
