48d9548d505092962dc97fcc0bd238eb698163d4
Tal Gluck <tal@superconductive.com>
2020-09-22 15:17:38 -0400

    bugfix

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -78,11 +78,11 @@ class Validator:
         self._batch = batch
         self._execution_engine = execution_engine
 
-        if self.execution_engine:
+        if execution_engine:
             self._data_context = execution_engine.data_context
             self._execution_engine._validator = self
 
-            if self.batch:
+            if batch:
                 if not execution_engine.batches.get(batch.to_id()):
                     execution_engine.batches[batch.to_id()] = batch
                 execution_engine._loaded_batch_id = batch.to_id()
