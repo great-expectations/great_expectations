735618d928c2505b60703a7f1e2adeee7219ca64
gilpasternak35 <gilpasternak35@gmail.com>
2020-09-22 17:50:13 -0700

    4 new fully functional aggregate expectations

---
+++ great_expectations/expectations/core/expect_column_sum_to_be_between.py
@@ -0,0 +1,158 @@
+
+from typing import Dict, List, Optional, Union
+
+import pandas as pd
+import numpy as np
+
+from great_expectations.core.batch import Batch
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.execution_engine import PandasExecutionEngine
+
+
+from ..expectation import (
+    ColumnMapDatasetExpectation,
+    DatasetExpectation,
+    Expectation,
+    InvalidExpectationConfigurationError,
+    _format_map_output,
+)
+from ..registry import extract_metrics
+
+
+class ExpectColumnSumToBeBetween(DatasetExpectation):
+    # Setting necessary computation metric dependencies and defining kwargs, as well as assigning kwargs default values\
+    metric_dependencies = ("column.aggregate.sum",)
+    success_keys = ("min_value", "strict_min", "max_value", "strict_max")
+
+
+    # Default values
+    default_kwarg_values = {
+        "row_condition": None,
+        "condition_parser": None,
+        "min_value": None,
+        "max_value": None,
+        "strict_min": None,
+        "strict_max": None,
+        "mostly": 1,
+        "result_format": "BASIC",
+        "include_config": True,
+        "catch_exceptions": False,
+    }
+
+    """ A Column Map Metric Decorator for the Minimum"""
+    @PandasExecutionEngine.metric(
+        metric_name="column.aggregate.sum",
+        metric_domain_keys=ColumnMapDatasetExpectation.domain_keys,
+        metric_value_keys=(),
+        metric_dependencies=tuple(),
+    )
+    def _pandas_sum(
+            self,
+            batches: Dict[str, Batch],
+            execution_engine: PandasExecutionEngine,
+            metric_domain_kwargs: dict,
+            metric_value_kwargs: dict,
+            metrics: dict,
+            runtime_configuration: dict = None,
+    ):
+        """Mean Metric Function"""
+        series = execution_engine.get_domain_dataframe(
+            domain_kwargs=metric_domain_kwargs, batches=batches)
+
+        return series.sum()
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        neccessary configuration arguments have been provided for the validation of the expectation.
+
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            True if the configuration has been validated successfully. Otherwise, raises an exception
+        """
+        min_val = None
+        max_val = None
+
+        # Setting up a configuration
+        super().validate_configuration(configuration)
+        if configuration is None:
+            configuration = self.configuration
+
+        # Ensuring basic configuration parameters are properly set
+        try:
+            assert (
+                    "column" in configuration.kwargs
+            ), "'column' parameter is required for column map expectations"
+            if "mostly" in configuration.kwargs:
+                mostly = configuration.kwargs["mostly"]
+                assert isinstance(
+                    mostly, (int, float)
+                ), "'mostly' parameter must be an integer or float"
+                assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+
+        # Validating that Minimum and Maximum values are of the proper format and type
+        if "min_value" in configuration.kwargs:
+            min_val = configuration.kwargs["min_value"]
+
+        if "max_value" in configuration.kwargs:
+            max_val = configuration.kwargs["max_value"]
+
+        try:
+            # Ensuring Proper interval has been provided
+            assert min_val or max_val, "min_value and max_value cannot both be None"
+            assert min_val is None or isinstance(min_val, (float, int)), "Provided min threshold must be a number"
+            assert max_val is None or isinstance(max_val, (float, int)), "Provided max threshold must be a number"
+
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+
+        if min_val is not None and max_val is not None and min_val > max_val:
+            raise InvalidExpectationConfigurationError("Minimum Threshold cannot be larger than Maximum Threshold")
+
+        return True
+
+    @Expectation.validates(metric_dependencies=metric_dependencies)
+    def _validates(
+            self,
+            configuration: ExpectationConfiguration,
+            metrics: dict,
+            runtime_configuration: dict = None,
+    ):
+        """Validates the given data against the set minimum and maximum value thresholds for the column min"""
+        # Obtaining dependencies used to validate the expectation
+        validation_dependencies = self.get_validation_dependencies(configuration)[
+            "metrics"
+        ]
+        metric_vals = extract_metrics(validation_dependencies, metrics, configuration)
+        column_sum = metric_vals.get("column.aggregate.sum")
+
+        # Obtaining components needed for validation
+        min_value = self.get_success_kwargs(configuration).get("min_value")
+        strict_min = self.get_success_kwargs(configuration).get("strict_min")
+        max_value = self.get_success_kwargs(configuration).get("max_value")
+        strict_max = self.get_success_kwargs(configuration).get("strict_max")
+
+        # Checking if mean lies between thresholds
+        if min_value is not None:
+            if strict_min:
+                above_min = column_sum > min_value
+            else:
+                above_min = column_sum >= min_value
+        else:
+            above_min = True
+
+        if max_value is not None:
+            if strict_max:
+                below_max = column_sum < max_value
+            else:
+                below_max = column_sum <= max_value
+        else:
+            below_max = True
+
+        success = above_min and below_max
+
+        return {"success": success, "result": {"observed_value": column_sum}}
