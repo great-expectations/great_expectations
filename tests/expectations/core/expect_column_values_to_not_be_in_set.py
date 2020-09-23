ca2ec1ffa65d3c2efc8ff8aa5172c1daa9c88b85
gilpasternak35 <gilpasternak35@gmail.com>
2020-09-23 13:55:12 -0700

    Pandas migration for expect column values to not be in set

---
+++ tests/expectations/core/expect_column_values_to_not_be_in_set.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from great_expectations import ExpectColumnValuesToNotBeInSet
+from great_expectations.core.batch import Batch
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.core.expectation_validation_result import (
+    ExpectationValidationResult,
+)
+from great_expectations.execution_engine import (
+    PandasExecutionEngine,
+    SparkDFExecutionEngine,
+)
+from great_expectations.execution_engine.sqlalchemy_execution_engine import (
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.execution_environment.types import (
+    BatchSpec,
+    SqlAlchemyDatasourceTableBatchSpec,
+)
+from great_expectations.expectations.core.expect_column_values_to_be_in_set import (
+    ExpectColumnValuesToBeInSet,
+)
+
+
+def test_expect_column_values_to_not_be_in_set_int_impl():
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    expectationConfiguration = ExpectationConfiguration(
+        expectation_type="expect_column_values_to_not_be_in_set",
+        kwargs={"column": "a", "value_set": [2,3], "mostly": .8},
+    )
+    expectation = ExpectColumnValuesToNotBeInSet(expectationConfiguration)
+    batch = Batch(data=df)
+    result = expectation.validate(
+        batches={"batch_id": batch}, execution_engine=PandasExecutionEngine()
+    )
+    assert result == ExpectationValidationResult(success=False,)
