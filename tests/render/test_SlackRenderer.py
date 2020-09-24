7c2f6413e8c0409bd2e033f002722b9e4a4ba990
William Shin <will@superconductive.com>
2020-09-23 18:45:21 -0700

    Update test_SlackRenderer.py

--- tests/render/test_SlackRenderer.py
+++ tests/render/test_SlackRenderer.py
@@ -46,7 +46,7 @@ def test_SlackRenderer_validation_results_with_datadocs():
                 "elements": [
                     {
                         "type": "mrkdwn",
-                        "text": "Learn how to review validation results in Data Docs: https://docs.greatexpectations.io/en/latest/tutorials/getting_started/set_up_data_docs.html",
+                        "text": "Learn how to review validation results in Data Docs: https://docs.greatexpectations.io/en/latest/guides/tutorials/getting_started/set_up_data_docs.html",
                     }
                 ],
             },
