42cf16705d3c01bd82091ba2d038501b3329b495
Alex Sherstinsky <alex@superconductivehealth.com>
2020-09-25 12:37:42 -0700

    cleanup

--- great_expectations/execution_environment/data_connector/__init__.py
+++ great_expectations/execution_environment/data_connector/__init__.py
@@ -1,5 +1,4 @@
 from .files_data_connector import FilesDataConnector
 # TODO: <Alex>Commenting not yet implemented Data Connectors for now, until they are properly implemented.</Alex>
 # from .query_data_connector import QueryDataConnector
-# from .subdir_reader_data_connector import SubdirReaderDataConnector
 # from .table_data_connector import TableDataConnector
