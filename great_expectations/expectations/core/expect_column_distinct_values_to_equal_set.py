ae075bf194eb608c542a90259e83b2e606a770b5
gilpasternak35 <gilpasternak35@gmail.com>
2020-09-25 08:53:43 -0700

    expect_column_values_to_equal_set fully functional

---
+++ great_expectations/expectations/core/expect_column_distinct_values_to_equal_set.py
@@ -0,0 +1,127 @@
+from typing import Dict, List, Optional, Union
+
+import numpy as np
+import pandas as pd
+
+from great_expectations.core.batch import Batch
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.execution_engine import ExecutionEngine, PandasExecutionEngine
+
+from ..expectation import (
+    ColumnMapDatasetExpectation,
+    DatasetExpectation,
+    Expectation,
+    InvalidExpectationConfigurationError,
+    _format_map_output,
+)
+from ..registry import extract_metrics
+
+
+class ExpectColumnDistinctValuesToEqualSet(DatasetExpectation):
+    # Setting necessary computation metric dependencies and defining kwargs, as well as assigning kwargs default values\
+    metric_dependencies = ("column.value_counts",)
+    success_keys = (
+        "value_set",
+        "parse_strings_as_datetimes",
+    )
+
+    # Default values
+    default_kwarg_values = {
+        "row_condition": None,
+        "condition_parser": None,
+        "value_set": None,
+        "parse_strings_as_datetimes": None,
+        "mostly": 1,
+        "result_format": "BASIC",
+        "include_config": True,
+        "catch_exceptions": False,
+    }
+
+    """ A Column Map Metric Decorator for the Mode metric"""
+
+    @PandasExecutionEngine.metric(
+        metric_name="column.value_counts",
+        metric_domain_keys=DatasetExpectation.domain_keys,
+        metric_value_keys=(),
+        metric_dependencies=tuple(),
+    )
+    def _pandas_value_counts(
+        self,
+        batches: Dict[str, Batch],
+        execution_engine: PandasExecutionEngine,
+        metric_domain_kwargs: dict,
+        metric_value_kwargs: dict,
+        metrics: dict,
+        runtime_configuration: dict = None,
+    ):
+        """Distinct value counts metric"""
+        series = execution_engine.get_domain_dataframe(
+            domain_kwargs=metric_domain_kwargs, batches=batches
+        )
+
+        return series.value_counts()
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        """Validating that user has inputted a value set and that configuration has been initialized"""
+        super().validate_configuration(configuration)
+        if configuration is None:
+            configuration = self.configuration
+        try:
+            assert "value_set" in configuration.kwargs, "value_set is required"
+            assert isinstance(
+                configuration.kwargs["value_set"], (list, set)
+            ), "value_set must be a list or a set"
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+        return True
+
+    @Expectation.validates(metric_dependencies=metric_dependencies)
+    def _validates(
+        self,
+        configuration: ExpectationConfiguration,
+        metrics: dict,
+        runtime_configuration: dict = None,
+        execution_engine: ExecutionEngine = None,
+    ):
+        """Validates that the Distinct values are equal to the given set"""
+        # Obtaining dependencies used to validate the expectation
+        validation_dependencies = self.get_validation_dependencies(
+            configuration, execution_engine, runtime_configuration
+        )["metrics"]
+        metric_vals = extract_metrics(
+            validation_dependencies, metrics, configuration, runtime_configuration
+        )
+
+        if runtime_configuration:
+            result_format = runtime_configuration.get(
+                "result_format",
+                configuration.kwargs.get(
+                    "result_format", self.default_kwarg_values.get("result_format")
+                ),
+            )
+        else:
+            result_format = configuration.kwargs.get(
+                "result_format", self.default_kwarg_values.get("result_format")
+            )
+
+        parse_strings_as_datetimes = self.get_success_kwargs(configuration).get(
+            "parse_strings_as_datetimes"
+        )
+        observed_value_counts = metric_vals.get("column.value_counts")
+        observed_value_set = set(observed_value_counts.index)
+        value_set = self.get_success_kwargs(configuration).get("value_set")
+
+        if parse_strings_as_datetimes:
+            parsed_value_set = PandasExecutionEngine._parse_value_set(value_set)
+        else:
+            parsed_value_set = value_set
+
+        expected_value_set = set(parsed_value_set)
+
+        return {
+            "success": observed_value_set == expected_value_set,
+            "result": {
+                "observed_value": sorted(list(observed_value_set)),
+                "details": {"value_counts": observed_value_counts},
+            },
+        }
--- tests/expectations/core/expect_column_value_z_scores_to_be_less_than.py
+++ tests/expectations/core/expect_column_value_z_scores_to_be_less_than.py
@@ -13,14 +13,14 @@ from great_expectations.expectations.core.expect_column_value_z_scores_to_be_les
 
 
 def test_expect_column_value_z_scores_to_be_less_than_impl():
-    df = pd.DataFrame({"a": [1, 5, 22, 3, 5]})
+    df = pd.DataFrame({"a": [1, 5, 22, 3, 5, 10]})
     expectationConfiguration = ExpectationConfiguration(
         expectation_type="expect_column_value_z_scores_to_be_less_than",
-        kwargs={"column": "a", "mostly": 0.9, "threshold": 1, "double_sided": True,},
+        kwargs={"column": "a", "mostly": 0.9, "threshold": 4, "double_sided": True,},
     )
     expectation = ExpectColumnValueZScoresToBeLessThan(expectationConfiguration)
     batch = Batch(data=df)
     result = expectation.validate(
         batches={"batch_id": batch}, execution_engine=PandasExecutionEngine()
     )
-    assert result == ExpectationValidationResult(success=False,)
+    assert result == ExpectationValidationResult(success=True,)
