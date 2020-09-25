29190428e345c2a89671d94054478693211a36da
William Shin <will@superconductive.com>
2020-09-25 15:32:22 -0700

    [BUGFIX] Fixed Issue where Slack Notifications did not include link to DataDocs in S3 (#1849)
    * simplified how we identify links to DataDocs in S3
    
    * mid-way push
    
    adding notify_with flag and base_public_url flag for GCS
    
    * draft 2
    
    * push before working on data connector
    
    * a bit of refactoring and beefing up the slack renderer test
    
    * added old test
    
    * edit to documentation
    
    * Update test_SlackRenderer.py
    
    * added documentation
    
    * Update test_SlackRenderer.py
    
    * updated documentation draft
    
    * small formatting change
    
    * added get_public_url_for_key
    
    * Update test_store_backends.py
    
    * Update slack_renderer.py
    
    * updated pr
    
    * bugfix
    
    * import issue
    
    * small bug fix on slack renderer
    
    * final push and clean up
    
    * added more logic to handle errors gracefully
    
    * added logger message
    
    * fix and changlog added

--- great_expectations/data_context/store/tuple_store_backend.py
+++ great_expectations/data_context/store/tuple_store_backend.py
@@ -29,6 +29,7 @@ class TupleStoreBackend(StoreBackend, metaclass=ABCMeta):
         forbidden_substrings=None,
         platform_specific_separator=True,
         fixed_length_key=False,
+        base_public_path=None,
     ):
         super().__init__(fixed_length_key=fixed_length_key)
         if forbidden_substrings is None:
@@ -52,6 +53,7 @@ class TupleStoreBackend(StoreBackend, metaclass=ABCMeta):
                 )
         self.filepath_prefix = filepath_prefix
         self.filepath_suffix = filepath_suffix
+        self.base_public_path = base_public_path
 
         if filepath_template is not None:
             # key length is the number of unique values to be substituted in the filepath_template
@@ -202,6 +204,7 @@ class TupleFilesystemStoreBackend(TupleStoreBackend):
         platform_specific_separator=True,
         root_directory=None,
         fixed_length_key=False,
+        base_public_path=None,
     ):
         super().__init__(
             filepath_template=filepath_template,
@@ -210,6 +213,7 @@ class TupleFilesystemStoreBackend(TupleStoreBackend):
             forbidden_substrings=forbidden_substrings,
             platform_specific_separator=platform_specific_separator,
             fixed_length_key=fixed_length_key,
+            base_public_path=base_public_path,
         )
         if os.path.isabs(base_directory):
             self.full_base_directory = base_directory
@@ -336,11 +340,22 @@ class TupleFilesystemStoreBackend(TupleStoreBackend):
     def get_url_for_key(self, key, protocol=None):
         path = self._convert_key_to_filepath(key)
         full_path = os.path.join(self.full_base_directory, path)
+
         if protocol is None:
             protocol = "file:"
         url = protocol + "//" + full_path
-
         return url
+
+    def get_public_url_for_key(self, key, protocol=None):
+        if not self.base_public_path:
+            raise StoreBackendError(
+                f"""Error: No base_public_path was configured!
+                    - A public URL was requested base_public_path was not configured for the TupleFilesystemStoreBackend
+                """
+            )
+        path = self._convert_key_to_filepath(key)
+        public_url = self.base_public_path + path
+        return public_url
 
     def _has_key(self, key):
         return os.path.isfile(
@@ -367,6 +382,7 @@ class TupleS3StoreBackend(TupleStoreBackend):
         forbidden_substrings=None,
         platform_specific_separator=False,
         fixed_length_key=False,
+        base_public_path=None,
     ):
         super().__init__(
             filepath_template=filepath_template,
@@ -375,6 +391,7 @@ class TupleS3StoreBackend(TupleStoreBackend):
             forbidden_substrings=forbidden_substrings,
             platform_specific_separator=platform_specific_separator,
             fixed_length_key=fixed_length_key,
+            base_public_path=base_public_path,
         )
         self.bucket = bucket
         if prefix:
@@ -525,9 +542,36 @@ class TupleS3StoreBackend(TupleStoreBackend):
         else:
             location = "s3-" + location
         s3_key = self._convert_key_to_filepath(key)
+
+        location = boto3.client("s3").get_bucket_location(Bucket=self.bucket)[
+            "LocationConstraint"
+        ]
+        if location is None:
+            location = "s3"
+        else:
+            location = "s3-" + location
         if not self.prefix:
-            return f"https://{location}.amazonaws.com/{self.bucket}/{s3_key}"
-        return f"https://{location}.amazonaws.com/{self.bucket}/{self.prefix}/{s3_key}"
+            url = f"https://{location}.amazonaws.com/{self.bucket}/{s3_key}"
+        else:
+            url = (
+                f"https://{location}.amazonaws.com/{self.bucket}/{self.prefix}/{s3_key}"
+            )
+        return url
+
+    def get_public_url_for_key(self, key, protocol=None):
+        if not self.base_public_path:
+            raise StoreBackendError(
+                f"""Error: No base_public_path was configured!
+                    - A public URL was requested base_public_path was not configured for the
+                """
+            )
+        s3_key = self._convert_key_to_filepath(key)
+        # <WILL> What happens if there is a prefix?
+        if self.base_public_path[-1] != "/":
+            public_url = self.base_public_path + "/" + s3_key
+        else:
+            public_url = self.base_public_path + s3_key
+        return public_url
 
     def remove_key(self, key):
         import boto3
@@ -587,6 +631,7 @@ class TupleGCSStoreBackend(TupleStoreBackend):
         platform_specific_separator=False,
         fixed_length_key=False,
         public_urls=True,
+        base_public_path=None,
     ):
         super().__init__(
             filepath_template=filepath_template,
@@ -595,6 +640,7 @@ class TupleGCSStoreBackend(TupleStoreBackend):
             forbidden_substrings=forbidden_substrings,
             platform_specific_separator=platform_specific_separator,
             fixed_length_key=fixed_length_key,
+            base_public_path=base_public_path,
         )
         self.bucket = bucket
         self.prefix = prefix
@@ -680,17 +726,40 @@ class TupleGCSStoreBackend(TupleStoreBackend):
 
     def get_url_for_key(self, key, protocol=None):
         path = self._convert_key_to_filepath(key)
+
         if self._public_urls:
             base_url = "https://storage.googleapis.com/"
         else:
             base_url = "https://storage.cloud.google.com/"
 
+        path_url = self._get_path_url(path)
+
+        return base_url + path_url
+
+    def get_public_url_for_key(self, key, protocol=None):
+        if not self.base_public_path:
+            raise StoreBackendError(
+                f"""Error: No base_public_path was configured!
+                    - A public URL was requested base_public_path was not configured for the
+                """
+            )
+        path = self._convert_key_to_filepath(key)
+        path_url = self._get_path_url(path)
+        public_url = self.base_public_path + path_url
+        return public_url
+
+    def _get_path_url(self, path):
         if self.prefix:
             path_url = "/".join((self.bucket, self.prefix, path))
         else:
-            path_url = "/".join((self.bucket, path))
-
-        return base_url + path_url
+            if self.base_public_path:
+                if self.base_public_path[-1] != "/":
+                    path_url = "/" + path
+                else:
+                    path_url = path
+            else:
+                path_url = "/".join((self.bucket, path))
+        return path_url
 
     def remove_key(self, key):
         from google.cloud import storage
