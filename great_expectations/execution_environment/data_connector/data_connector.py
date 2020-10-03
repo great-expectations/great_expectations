e72e1a9ae8b1540ba40edca7396ee554422918b7
Alex Sherstinsky <alex@superconductivehealth.com>
2020-10-02 17:34:48 -0700

    More elegant error checking.

--- great_expectations/execution_environment/data_connector/data_connector.py
+++ great_expectations/execution_environment/data_connector/data_connector.py
@@ -302,7 +302,7 @@ multiple partitions, including "{partition}", for the same data reference -- thi
         batch_spec_scaffold["execution_environment"] = self._execution_environment.name
 
         partition_query: dict = batch_definition.get("partition_query")
-        if partition_query.get("data_asset_name") and partition_query["data_asset_name"] != data_asset_name:
+        if "data_asset_name" in partition_query and partition_query["data_asset_name"] != data_asset_name:
             raise ge_exceptions.BatchSpecError(
                 message=f'''Unable to build batch_spec for data asset "{data_asset_name}", because the partition query
 specifies a different data asset name ("{partition_query["data_asset_name"]}").
