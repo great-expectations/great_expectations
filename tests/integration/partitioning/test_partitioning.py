e75cfe02a89a2bb8e04e2b081571ea9f6d617f3b
Alex Sherstinsky <alex@superconductivehealth.com>
2020-10-04 22:48:31 -0700

    tests for partition querying

--- tests/integration/partitioning/test_partitioning.py
+++ tests/integration/partitioning/test_partitioning.py
@@ -1,32 +1,10 @@
-import sys
-import pytest
-import os
-import shutil
 from typing import List
 import datetime
-from pathlib import Path
 
-from great_expectations.data_context.types.base import DataContextConfig
-from great_expectations.data_context import (
-    BaseDataContext,
-    DataContext,
-)
-from great_expectations.data_context.util import file_relative_path
+from great_expectations.data_context import DataContext
 from great_expectations.execution_environment import ExecutionEnvironment
 from great_expectations.execution_environment.data_connector.data_connector import DataConnector
-from great_expectations.execution_environment.data_connector.files_data_connector import FilesDataConnector
 from great_expectations.execution_environment.data_connector.partitioner.partition import Partition
-from great_expectations.util import gen_directory_tree_str
-from tests.test_utils import safe_remove
-
-# try:
-#     from unittest import mock
-# except ImportError:
-#     from unittest import mock
-
-
-# def test_config(execution_environment_files_data_connector_regex_partitioner_no_groups_no_sorters_data_context):
-#     execution_environment_files_data_connector_regex_partitioner_no_groups_no_sorters_data_context.get_config().to_yaml(outfile=sys.stdout)
 
 
 def test_return_all_available_partitions_unsorted(
@@ -224,7 +202,7 @@ def test_return_all_available_partitions_sorted(
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter(
+def test_return_partitions_sorted_filtered_by_custom_filter(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -303,7 +281,7 @@ def test_return_partitions_sorted_with_custom_filter(
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -379,7 +357,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit(
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_index_as_int(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_index_as_int(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -427,7 +405,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_index_as_in
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_index_as_str(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_index_as_str(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -475,7 +453,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_index_as_st
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_list(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_slice_as_list(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -529,7 +507,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_li
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_tuple(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_slice_as_tuple(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -583,7 +561,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_tu
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_str(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_slice_as_str(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -637,7 +615,7 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_as_st
     assert available_partitions == expected_returned_partitions
 
 
-def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_obj(
+def test_return_partitions_sorted_filtered_by_custom_filter_with_limit_with_slice_obj(
     execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
 ):
     execution_environment_name: str = "test_execution_environment"
@@ -685,6 +663,249 @@ def test_return_partitions_sorted_with_custom_filter_with_limit_with_slice_obj(
             data_asset_name="abe_20200809_1040",
             definition={"name": "abe", "timestamp": "20200809", "price": "1040"},
             source=f"{base_directory}/abe_20200809_1040.csv"
+        ),
+    ]
+
+    assert available_partitions == expected_returned_partitions
+
+
+def test_return_partitions_sorted_for_specific_data_asset_name(
+    execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+):
+    execution_environment_name: str = "test_execution_environment"
+    data_connector_name: str = "test_filesystem_data_connector"
+
+    data_context: DataContext = \
+        execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+
+    available_partitions: List[Partition] = data_context.get_available_partitions(
+        execution_environment_name=execution_environment_name,
+        data_connector_name=data_connector_name,
+        data_asset_name="abe_20200809_1040",
+        partition_query={
+            "custom_filter": None,
+            "partition_name": None,
+            "partition_definition": None,
+            "limit": None,
+            "partition_index": None
+        },
+        in_memory_dataset=None,
+        repartition=False
+    )
+
+    assert len(available_partitions) == 1
+
+    execution_environment: ExecutionEnvironment = data_context.get_execution_environment(
+        execution_environment_name=execution_environment_name
+    )
+    data_connector: DataConnector = execution_environment.get_data_connector(name=data_connector_name)
+    # noinspection PyUnresolvedReferences
+    base_directory: str = data_connector.base_directory
+
+    expected_returned_partitions: List[Partition] = [
+        Partition(
+            name="abe-20200809-1040",
+            data_asset_name="abe_20200809_1040",
+            definition={"name": "abe", "timestamp": "20200809", "price": "1040"},
+            source=f"{base_directory}/abe_20200809_1040.csv"
+        ),
+    ]
+
+    assert available_partitions == expected_returned_partitions
+
+
+def test_return_partitions_sorted_queried_by_partition_name(
+    execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+):
+    execution_environment_name: str = "test_execution_environment"
+    data_connector_name: str = "test_filesystem_data_connector"
+
+    data_context: DataContext = \
+        execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+
+    available_partitions: List[Partition] = data_context.get_available_partitions(
+        execution_environment_name=execution_environment_name,
+        data_connector_name=data_connector_name,
+        data_asset_name=None,
+        partition_query={
+            "custom_filter": None,
+            "partition_name": "alex-20200819-1300",
+            "partition_definition": None,
+            "limit": None,
+            "partition_index": None
+        },
+        in_memory_dataset=None,
+        repartition=False
+    )
+
+    assert len(available_partitions) == 1
+
+    execution_environment: ExecutionEnvironment = data_context.get_execution_environment(
+        execution_environment_name=execution_environment_name
+    )
+    data_connector: DataConnector = execution_environment.get_data_connector(name=data_connector_name)
+    # noinspection PyUnresolvedReferences
+    base_directory: str = data_connector.base_directory
+
+    expected_returned_partitions: List[Partition] = [
+        Partition(
+            name="alex-20200819-1300",
+            data_asset_name="alex_20200819_1300",
+            definition={"name": "alex", "timestamp": "20200819", "price": "1300"},
+            source=f"{base_directory}/alex_20200819_1300.csv"
+        ),
+    ]
+
+    assert available_partitions == expected_returned_partitions
+
+
+def test_return_partitions_sorted_queried_by_partition_definition_dict_1_key(
+    execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+):
+    execution_environment_name: str = "test_execution_environment"
+    data_connector_name: str = "test_filesystem_data_connector"
+
+    data_context: DataContext = \
+        execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+
+    available_partitions: List[Partition] = data_context.get_available_partitions(
+        execution_environment_name=execution_environment_name,
+        data_connector_name=data_connector_name,
+        data_asset_name=None,
+        partition_query={
+            "custom_filter": None,
+            "partition_name": None,
+            "partition_definition": {"timestamp": "20200809"},
+            "limit": None,
+            "partition_index": None
+        },
+        in_memory_dataset=None,
+        repartition=False
+    )
+
+    assert len(available_partitions) == 4
+
+    execution_environment: ExecutionEnvironment = data_context.get_execution_environment(
+        execution_environment_name=execution_environment_name
+    )
+    data_connector: DataConnector = execution_environment.get_data_connector(name=data_connector_name)
+    # noinspection PyUnresolvedReferences
+    base_directory: str = data_connector.base_directory
+
+    expected_returned_partitions: List[Partition] = [
+        Partition(
+            name="eugene-20200809-1500",
+            data_asset_name="eugene_20200809_1500",
+            definition={"name": "eugene", "timestamp": "20200809", "price": "1500"},
+            source=f"{base_directory}/eugene_20200809_1500.csv"
+        ),
+        Partition(
+            name="abe-20200809-1040",
+            data_asset_name="abe_20200809_1040",
+            definition={"name": "abe", "timestamp": "20200809", "price": "1040"},
+            source=f"{base_directory}/abe_20200809_1040.csv"
+        ),
+        Partition(
+            name="will-20200809-1002",
+            data_asset_name="will_20200809_1002",
+            definition={"name": "will", "timestamp": "20200809", "price": "1002"},
+            source=f"{base_directory}/will_20200809_1002.csv"
+        ),
+        Partition(
+            name="alex-20200809-1000",
+            data_asset_name="alex_20200809_1000",
+            definition={"name": "alex", "timestamp": "20200809", "price": "1000"},
+            source=f"{base_directory}/alex_20200809_1000.csv"
+        ),
+    ]
+
+    assert available_partitions == expected_returned_partitions
+
+
+def test_return_partitions_sorted_queried_by_partition_definition_dict_2_keys(
+    execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+):
+    execution_environment_name: str = "test_execution_environment"
+    data_connector_name: str = "test_filesystem_data_connector"
+
+    data_context: DataContext = \
+        execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+
+    available_partitions: List[Partition] = data_context.get_available_partitions(
+        execution_environment_name=execution_environment_name,
+        data_connector_name=data_connector_name,
+        data_asset_name=None,
+        partition_query={
+            "custom_filter": None,
+            "partition_name": None,
+            "partition_definition": {"timestamp": "20200809", "name": "will"},
+            "limit": None,
+            "partition_index": None
+        },
+        in_memory_dataset=None,
+        repartition=False
+    )
+
+    assert len(available_partitions) == 1
+
+    execution_environment: ExecutionEnvironment = data_context.get_execution_environment(
+        execution_environment_name=execution_environment_name
+    )
+    data_connector: DataConnector = execution_environment.get_data_connector(name=data_connector_name)
+    # noinspection PyUnresolvedReferences
+    base_directory: str = data_connector.base_directory
+
+    expected_returned_partitions: List[Partition] = [
+        Partition(
+            name="will-20200809-1002",
+            data_asset_name="will_20200809_1002",
+            definition={"name": "will", "timestamp": "20200809", "price": "1002"},
+            source=f"{base_directory}/will_20200809_1002.csv"
+        ),
+    ]
+
+    assert available_partitions == expected_returned_partitions
+
+
+def test_return_partitions_sorted_queried_by_partition_definition_dict_1_key_with_index_as_int(
+    execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+):
+    execution_environment_name: str = "test_execution_environment"
+    data_connector_name: str = "test_filesystem_data_connector"
+
+    data_context: DataContext = \
+        execution_environment_files_data_connector_regex_partitioner_with_groups_with_sorters_data_context
+
+    available_partitions: List[Partition] = data_context.get_available_partitions(
+        execution_environment_name=execution_environment_name,
+        data_connector_name=data_connector_name,
+        data_asset_name=None,
+        partition_query={
+            "custom_filter": None,
+            "partition_name": None,
+            "partition_definition": {"name": "james"},
+            "limit": None,
+            "partition_index": 0
+        },
+        in_memory_dataset=None,
+        repartition=False
+    )
+
+    assert len(available_partitions) == 1
+
+    execution_environment: ExecutionEnvironment = data_context.get_execution_environment(
+        execution_environment_name=execution_environment_name
+    )
+    data_connector: DataConnector = execution_environment.get_data_connector(name=data_connector_name)
+    # noinspection PyUnresolvedReferences
+    base_directory: str = data_connector.base_directory
+
+    expected_returned_partitions: List[Partition] = [
+        Partition(
+            name="james-20200811-1009",
+            data_asset_name="james_20200811_1009",
+            definition={"name": "james", "timestamp": "20200811", "price": "1009"},
+            source=f"{base_directory}/james_20200811_1009.csv"
         ),
     ]
 
