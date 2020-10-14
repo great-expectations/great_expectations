0c44b1b72a70bc037cae27b3674fb68c3945e6d7
Abe <abegong@gmail.com>
2020-10-14 15:46:16 -0600

    Decruft

--- tests/data_context/test_data_context_test_yaml_config.py
+++ tests/data_context/test_data_context_test_yaml_config.py
@@ -1,6 +1,9 @@
 import pytest
 import tempfile
 import os
+from tests.test_utils import (
+    create_files_in_directory,
+)
 
 def test_empty_store(empty_data_context):
 
@@ -61,21 +64,11 @@ store_backend:
     class_name: InMemoryStoreBackend
 """)
 
-def create_files(
-    path: str,
-    file_name_list: list
-):
-    #NOTE: A more sophisticated version of this method would create subdirectories, too.
-    for file_name in file_name_list:	
-        file_path = os.path.join(path, file_name)	
-        with open(file_path, "w") as fp:	
-            fp.writelines([f'The name of this file is: "{file_path}".\n'])	
-
 def test_execution_environment_config(empty_data_context):
 
     temp_dir = str(tempfile.mkdtemp())
-    create_files(
-        path=temp_dir,
+    create_files_in_directory(
+        directory=temp_dir,
         file_name_list=[
             "alex_20200809_1000.csv",	
             "eugene_20200809_1500.csv",	
@@ -91,28 +84,6 @@ def test_execution_environment_config(empty_data_context):
     )
     print(temp_dir)
 
-#     my_execution_environnment = empty_data_context.test_yaml_config(
-#         yaml_config=f"""
-# class_name: ExecutionEnvironment
-# execution_engine:
-#     module_name: great_expectations.execution_engine.pandas_execution_engine
-#     class_name: PandasExecutionEngine
-#     engine_spec_passthrough:
-#         reader_method: read_csv
-#         reader_options:
-#         header: 0
-
-# data_connectors:
-#     subdir:
-#         module_name: great_expectations.execution_environment.data_connector.files_data_connector
-#         class_name: FilesDataConnector
-#         assets:
-#         engine_spec_passthrough:
-#             reader_method: read_csv
-#             reader_options:
-#                 header: 0
-#         base_path: {temp_dir}
-# """)
     return_obj = empty_data_context.test_yaml_config(
         yaml_config=f"""
 class_name: ExecutionEnvironment
@@ -139,45 +110,6 @@ data_connectors:
 """, return_mode="return_object"
     )
 
-#     return_obj = empty_data_context.test_yaml_config(
-#         yaml_config=f"""
-# class_name: ExecutionEnvironment
-# execution_engine:
-#     module_name: great_expectations.execution_engine.pandas_execution_engine
-#     class_name: PandasExecutionEngine
-#     engine_spec_passthrough:
-#         reader_method: read_csv
-#         reader_options:
-#         header: 0
-
-# data_connectors:
-#     test_filesystem_data_connector:
-#         module_name: great_expectations.execution_environment.data_connector
-#         class_name: FilesDataConnector
-
-#         config_params:
-#             base_directory: {temp_dir}
-#             glob_directive: "*"
-            
-#         partitioners:
-#             test_regex_partitioner:
-#                 module_name: great_expectations.execution_environment.data_connector.partitioner
-#                 class_name: RegexPartitioner
-#                 config_params:
-#                     regex:
-#                         pattern: ".+\/(.+)_(.+)_(.+)\\\.csv"
-#                         group_names: group_names
-
-#                     allow_multipart_partitions: False,
-#                     sorters: sorters
-
-#         default_partitioner: test_regex_partitioner
-#         assets:
-#             test_asset_0:
-#                 config_params:
-#                     glob_directive: "alex*"
-#                 partitioner: test_regex_partitioner
-#                 base_directory: data_asset_base_directory
-# """, return_mode="return_object")
-    
     print(return_obj)
+
+    # assert False
