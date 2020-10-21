6a334ccc790713d5d065030b3dfa03c013e8c9a7
Robert Moses Lim <robert.m.lim@gmail.com>
2020-10-21 02:14:40 -0700

    Create ExpectColumnKlDivergenceToBeLessThan class and move over expectation string renderer

--- great_expectations/render/renderer/content_block/expectation_string.py
+++ great_expectations/render/renderer/content_block/expectation_string.py
@@ -1,17 +1,11 @@
-import json
-
-import altair as alt
-import pandas as pd
-
 from great_expectations.render.renderer.content_block.content_block import (
     ContentBlockRenderer,
 )
 from great_expectations.render.types import (
-    RenderedGraphContent,
     RenderedStringTemplateContent,
 )
-from great_expectations.render.util import num_to_str, ordinal, substitute_none_for_missing, \
-    parse_row_condition_string_pandas_engine, handle_strict_min_max
+from great_expectations.render.util import num_to_str, substitute_none_for_missing, \
+    parse_row_condition_string_pandas_engine
 
 
 class ExpectationStringRenderer(ContentBlockRenderer):
@@ -27,8 +21,8 @@ class ExpectationStringRenderer(ContentBlockRenderer):
                     "string_template": {
                         "template": "$expectation_type(**$kwargs)",
                         "params": {
-                            "expectation_type": expectation.expectation_type,
-                            "kwargs": expectation.kwargs,
+                            "expectation_type": expectation_configuration.expectation_type,
+                            "kwargs": expectation_configuration.kwargs,
                         },
                         "styling": {
                             "params": {
@@ -47,7 +41,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs, ["column_set", "exact_match"]
+            expectation_configuration.kwargs, ["column_set", "exact_match"]
         )
 
         if params["column_set"] is None:
@@ -86,7 +80,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs, ["column_A", "column_B"]
+            expectation_configuration.kwargs, ["column_A", "column_B"]
         )
         if (params["column_A"] is None) or (params["column_B"] is None):
             template_str = " unrecognized kwargs for expect_column_pair_cramers_phi_value_to_be_less_than: missing column."
@@ -110,7 +104,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs,
+            expectation_configuration.kwargs,
             [
                 "column_list",
                 "ignore_row_if",
@@ -175,7 +169,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         # expect_select_column_values_to_be_unique_within_record instead.
 
         params = substitute_none_for_missing(
-            expectation.kwargs,
+            expectation_configuration.kwargs,
             [
                 "column_list",
                 "ignore_row_if",
@@ -235,7 +229,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs,
+            expectation_configuration.kwargs,
             [
                 "column_list",
                 "ignore_row_if",
@@ -294,7 +288,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
     def expect_table_row_count_to_equal_other_table(
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
-        params = substitute_none_for_missing(expectation.kwargs, ["other_table_name"])
+        params = substitute_none_for_missing(expectation_configuration.kwargs, ["other_table_name"])
         template_str = "Row count must equal the row count of table $other_table_name."
 
         return [
@@ -315,7 +309,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs,
+            expectation_configuration.kwargs,
             ["column", "type_", "mostly", "row_condition", "condition_parser"],
         )
 
@@ -359,7 +353,7 @@ class ExpectationStringRenderer(ContentBlockRenderer):
         cls, expectation_configuration, styling=None, include_column_name=True
     ):
         params = substitute_none_for_missing(
-            expectation.kwargs,
+            expectation_configuration.kwargs,
             ["column", "type_list", "mostly", "row_condition", "condition_parser"],
         )
 
@@ -426,238 +420,3 @@ class ExpectationStringRenderer(ContentBlockRenderer):
                 }
             )
         ]
-
-    @classmethod
-    def _get_kl_divergence_partition_object_table(cls, partition_object, header=None):
-        table_rows = []
-        fractions = partition_object["weights"]
-
-        if partition_object.get("bins"):
-            bins = partition_object["bins"]
-
-            for idx, fraction in enumerate(fractions):
-                if idx == len(fractions) - 1:
-                    table_rows.append(
-                        [
-                            "[{} - {}]".format(
-                                num_to_str(bins[idx]), num_to_str(bins[idx + 1])
-                            ),
-                            num_to_str(fraction),
-                        ]
-                    )
-                else:
-                    table_rows.append(
-                        [
-                            "[{} - {})".format(
-                                num_to_str(bins[idx]), num_to_str(bins[idx + 1])
-                            ),
-                            num_to_str(fraction),
-                        ]
-                    )
-        else:
-            values = partition_object["values"]
-            table_rows = [
-                [value, num_to_str(fractions[idx])] for idx, value in enumerate(values)
-            ]
-
-        if header:
-            return {
-                "content_block_type": "table",
-                "header": header,
-                "header_row": ["Interval", "Fraction"]
-                if partition_object.get("bins")
-                else ["Value", "Fraction"],
-                "table": table_rows,
-                "styling": {
-                    "classes": ["table-responsive"],
-                    "body": {
-                        "classes": [
-                            "table",
-                            "table-sm",
-                            "table-bordered",
-                            "mt-2",
-                            "mb-2",
-                        ],
-                    },
-                    "parent": {
-                        "classes": ["show-scrollbars", "p-2"],
-                        "styles": {
-                            "list-style-type": "none",
-                            "overflow": "auto",
-                            "max-height": "80vh",
-                        },
-                    },
-                },
-            }
-        else:
-            return {
-                "content_block_type": "table",
-                "header_row": ["Interval", "Fraction"]
-                if partition_object.get("bins")
-                else ["Value", "Fraction"],
-                "table": table_rows,
-                "styling": {
-                    "classes": ["table-responsive"],
-                    "body": {
-                        "classes": [
-                            "table",
-                            "table-sm",
-                            "table-bordered",
-                            "mt-2",
-                            "mb-2",
-                        ],
-                    },
-                    "parent": {
-                        "classes": ["show-scrollbars", "p-2"],
-                        "styles": {
-                            "list-style-type": "none",
-                            "overflow": "auto",
-                            "max-height": "80vh",
-                        },
-                    },
-                },
-            }
-
-    @classmethod
-    def _get_kl_divergence_chart(cls, partition_object, header=None):
-        weights = partition_object["weights"]
-
-        if len(weights) > 60:
-            expected_distribution = cls._get_kl_divergence_partition_object_table(
-                partition_object, header=header
-            )
-        else:
-            chart_pixel_width = (len(weights) / 60.0) * 500
-            if chart_pixel_width < 250:
-                chart_pixel_width = 250
-            chart_container_col_width = round((len(weights) / 60.0) * 6)
-            if chart_container_col_width < 4:
-                chart_container_col_width = 4
-            elif chart_container_col_width >= 5:
-                chart_container_col_width = 6
-            elif chart_container_col_width >= 4:
-                chart_container_col_width = 5
-
-            mark_bar_args = {}
-            if len(weights) == 1:
-                mark_bar_args["size"] = 20
-
-            if partition_object.get("bins"):
-                bins = partition_object["bins"]
-                bins_x1 = [round(value, 1) for value in bins[:-1]]
-                bins_x2 = [round(value, 1) for value in bins[1:]]
-
-                df = pd.DataFrame(
-                    {"bin_min": bins_x1, "bin_max": bins_x2, "fraction": weights,}
-                )
-
-                bars = (
-                    alt.Chart(df)
-                    .mark_bar()
-                    .encode(
-                        x="bin_min:O",
-                        x2="bin_max:O",
-                        y="fraction:Q",
-                        tooltip=["bin_min", "bin_max", "fraction"],
-                    )
-                    .properties(width=chart_pixel_width, height=400, autosize="fit")
-                )
-
-                chart = bars.to_json()
-            elif partition_object.get("values"):
-                values = partition_object["values"]
-
-                df = pd.DataFrame({"values": values, "fraction": weights})
-
-                bars = (
-                    alt.Chart(df)
-                    .mark_bar()
-                    .encode(
-                        x="values:N", y="fraction:Q", tooltip=["values", "fraction"]
-                    )
-                    .properties(width=chart_pixel_width, height=400, autosize="fit")
-                )
-                chart = bars.to_json()
-
-            if header:
-                expected_distribution = RenderedGraphContent(
-                    **{
-                        "content_block_type": "graph",
-                        "graph": chart,
-                        "header": header,
-                        "styling": {
-                            "classes": [
-                                "col-" + str(chart_container_col_width),
-                                "mt-2",
-                                "pl-1",
-                                "pr-1",
-                            ],
-                            "parent": {"styles": {"list-style-type": "none"}},
-                        },
-                    }
-                )
-            else:
-                expected_distribution = RenderedGraphContent(
-                    **{
-                        "content_block_type": "graph",
-                        "graph": chart,
-                        "styling": {
-                            "classes": [
-                                "col-" + str(chart_container_col_width),
-                                "mt-2",
-                                "pl-1",
-                                "pr-1",
-                            ],
-                            "parent": {"styles": {"list-style-type": "none"}},
-                        },
-                    }
-                )
-        return expected_distribution
-
-    @classmethod
-    def expect_column_kl_divergence_to_be_less_than(
-        cls, expectation_configuration, styling=None, include_column_name=True
-    ):
-        params = substitute_none_for_missing(
-            expectation.kwargs,
-            [
-                "column",
-                "partition_object",
-                "threshold",
-                "row_condition",
-                "condition_parser",
-            ],
-        )
-
-        expected_distribution = None
-        if not params.get("partition_object"):
-            template_str = "can match any distribution."
-        else:
-            template_str = (
-                "Kullback-Leibler (KL) divergence with respect to the following distribution must be "
-                "lower than $threshold."
-            )
-            expected_distribution = cls._get_kl_divergence_chart(
-                params.get("partition_object")
-            )
-
-        if include_column_name:
-            template_str = "$column " + template_str
-
-        if params["row_condition"] is not None:
-            (
-                conditional_template_str,
-                conditional_params,
-            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
-            template_str = conditional_template_str + ", then " + template_str
-            params.update(conditional_params)
-
-        expectation_string_obj = {
-            "content_block_type": "string_template",
-            "string_template": {"template": template_str, "params": params},
-        }
-
-        if expected_distribution:
-            return [expectation_string_obj, expected_distribution]
-        else:
-            return [expectation_string_obj]
