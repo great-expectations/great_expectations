63ea771731f4822321cc85ff1155264aef8d5cb1
Neolin <joussonantonin@gmail.com>
2020-10-25 22:25:09 +0100

    Update MicrosoftTeamsRenderer implementation to make use of validation_result_suite_identifier

--- great_expectations/render/renderer/microsoft_teams_renderer.py
+++ great_expectations/render/renderer/microsoft_teams_renderer.py
@@ -1,8 +1,10 @@
 import logging
 
+from ...core import RunIdentifier
+from ...data_context.types.resource_identifiers import BatchIdentifier
+
 logger = logging.getLogger(__name__)
 
-from ...core.id_dict import BatchKwargs
 from .renderer import Renderer
 
 
@@ -14,7 +16,10 @@ class MicrosoftTeamsRenderer(Renderer):
         super().__init__()
 
     def render(
-        self, validation_result=None, data_docs_pages=None,
+        self,
+        validation_result=None,
+        validation_result_suite_identifier=None,
+        data_docs_pages=None,
     ):
         default_text = (
             "No validation occurred. Please ensure you passed a validation_result."
@@ -76,75 +81,82 @@ class MicrosoftTeamsRenderer(Renderer):
             ],
         }
 
+        validation_result_elements = []
         if validation_result:
-            expectation_suite_name = validation_result.meta.get(
-                "expectation_suite_name", "__no_expectation_suite_name__"
-            )
-
-            if "batch_kwargs" in validation_result.meta:
-                data_asset_name = validation_result.meta["batch_kwargs"].get(
-                    "data_asset_name", "__no_data_asset_name__"
-                )
-            else:
-                data_asset_name = "__no_data_asset_name__"
-
-            n_checks_succeeded = validation_result.statistics["successful_expectations"]
-            n_checks = validation_result.statistics["evaluated_expectations"]
-            run_name = validation_result.meta["run_id"].get(
-                "run_name", "__no_run_name_"
-            )
-            run_time = validation_result.meta["run_id"].get(
-                "run_time", "__no_run_time_"
-            )
-            batch_id = BatchKwargs(
-                validation_result.meta.get("batch_kwargs", {})
-            ).to_id()
-            check_details_text = "*{}* of *{}* expectations were met".format(
-                n_checks_succeeded, n_checks
-            )
-
             if validation_result.success:
                 status = "Success !!!"
 
-            status_element = self._get_validation_result_element(
+            status_element = self._render_validation_result_element(
                 key="Batch validation status",
                 value=status,
                 validation_result=validation_result,
             )
-            data_asset_name_element = self._get_validation_result_element(
-                key="Data asset name", value=data_asset_name
+            validation_result_elements.append(status_element)
+
+            if validation_result_suite_identifier:
+                batch_identifier = validation_result_suite_identifier.batch_identifier
+                if isinstance(batch_identifier, BatchIdentifier):
+                    data_asset_name = batch_identifier.data_asset_name
+                    batch_identifier = batch_identifier.batch_identifier
+                else:
+                    data_asset_name = "__no_data_asset_name_"
+
+                data_asset_name_element = self._render_validation_result_element(
+                    key="Data asset name", value=data_asset_name
+                )
+                validation_result_elements.append(data_asset_name_element)
+
+                expectation_suite_name = (
+                    validation_result_suite_identifier.expectation_suite_identifier.expectation_suite_name
+                )
+                expectation_suite_name_element = self._render_validation_result_element(
+                    key="Expectation suite name", value=expectation_suite_name
+                )
+                validation_result_elements.append(expectation_suite_name_element)
+
+                run_id = validation_result_suite_identifier.run_id
+                if isinstance(run_id, RunIdentifier):
+                    run_name = run_id.run_name
+                    run_time = run_id.run_time
+                else:
+                    run_time = "__no_run_time_"
+                    run_name = run_id
+
+                run_name_element = self._render_validation_result_element(
+                    key="Run name", value=run_name
+                )
+                validation_result_elements.append(run_name_element)
+
+                batch_id_element = self._render_validation_result_element(
+                    key="Batch ID", value=batch_identifier
+                )
+                validation_result_elements.append(batch_id_element)
+
+                query["attachments"][0]["content"]["body"][0]["items"][0]["columns"][0][
+                    "items"
+                ].append(
+                    {
+                        "type": "TextBlock",
+                        "spacing": "none",
+                        "text": run_time.strftime("%b %d %Y %H:%M:%S"),
+                        "isSubtle": True,
+                        "wrap": True,
+                    }
+                )
+
+            n_checks_succeeded = validation_result.statistics["successful_expectations"]
+            n_checks = validation_result.statistics["evaluated_expectations"]
+            check_details_text = "*{}* of *{}* expectations were met".format(
+                n_checks_succeeded, n_checks
             )
-            expectation_suite_name_element = self._get_validation_result_element(
-                key="Expectation suite name", value=expectation_suite_name
-            )
-            run_name_element = self._get_validation_result_element(
-                key="Run name", value=run_name
-            )
-            batch_id_element = self._get_validation_result_element(
-                key="Batch ID", value=batch_id
-            )
-            check_details_text_element = self._get_validation_result_element(
+            check_details_text_element = self._render_validation_result_element(
                 key="Summary", value=check_details_text
             )
+            validation_result_elements.append(check_details_text_element)
 
-            query["attachments"][0]["content"]["body"][0]["items"][0]["columns"][0][
+            query["attachments"][0]["content"]["body"][1][
                 "items"
-            ][1] = {
-                "type": "TextBlock",
-                "spacing": "none",
-                "text": "{{DATE({run_time}, SHORT)}}".format(run_time=run_time),
-                "isSubtle": True,
-                "wrap": True,
-            }
-
-            query["attachments"][0]["content"]["body"][1]["items"] = [
-                status_element,
-                data_asset_name_element,
-                expectation_suite_name_element,
-                run_name_element,
-                batch_id_element,
-                check_details_text_element,
-            ]
+            ] = validation_result_elements
 
             if data_docs_pages:
                 for docs_link_key in data_docs_pages.keys():
@@ -169,27 +181,26 @@ class MicrosoftTeamsRenderer(Renderer):
         return report_element
 
     @staticmethod
-    def _get_validation_result_element(key, value, validation_result=None):
+    def _render_validation_result_element(key, value, validation_result=None):
 
         if validation_result and validation_result.success:
             validation_result_element = {
                 "type": "TextBlock",
-                "text": "**{key}}:** {value}".format(key=key, value=value),
+                "text": "**{key}:** {value}".format(key=key, value=value),
                 "horizontalAlignment": "left",
                 "color": "good",
             }
         elif validation_result and not validation_result.success:
             validation_result_element = {
                 "type": "TextBlock",
-                "text": "**{key}}:** {value}".format(key=key, value=value),
+                "text": "**{key}:** {value}".format(key=key, value=value),
                 "horizontalAlignment": "left",
                 "color": "attention",
             }
         else:
             validation_result_element = {
                 "type": "TextBlock",
-                "text": "**{key}}:** {value}".format(key=key, value=value),
+                "text": "**{key}:** {value}".format(key=key, value=value),
                 "horizontalAlignment": "left",
             }
-
         return validation_result_element
