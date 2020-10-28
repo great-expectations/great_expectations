af3fe29aa0781e29ecc26dbe02dbe5e49b9e8e80
Robert Moses Lim <robert.m.lim@gmail.com>
2020-10-27 17:35:24 -0700

    Move out rendering logic into new expectation classes

---
+++ great_expectations/expectations/core/expect_column_values_to_be_of_type.py
@@ -0,0 +1,74 @@
+from great_expectations.expectations.expectation import ColumnMapExpectation
+from great_expectations.render.renderer.renderer import renderer
+from great_expectations.render.types import RenderedStringTemplateContent
+from great_expectations.render.util import substitute_none_for_missing, num_to_str, \
+    parse_row_condition_string_pandas_engine
+
+
+class ExpectColumnValuesToBeOfType(ColumnMapExpectation):
+    metric_dependencies = tuple()
+    success_keys = (
+        "type_",
+        "mostly",
+    )
+    default_kwarg_values = {
+        "type_": None,
+        "mostly": 1,
+        "result_format": "BASIC",
+        "include_config": True,
+        "catch_exceptions": False,
+    }
+
+    @classmethod
+    @renderer(renderer_type="renderer.prescriptive")
+    def _prescriptive_renderer(
+            cls,
+            configuration=None,
+            result=None,
+            language=None,
+            runtime_configuration=None,
+            **kwargs
+    ):
+        runtime_configuration = runtime_configuration or {}
+        include_column_name = runtime_configuration.get("include_column_name", True)
+        styling = runtime_configuration.get("styling")
+
+        params = substitute_none_for_missing(
+            configuration.kwargs,
+            ["column", "type_", "mostly", "row_condition", "condition_parser"],
+        )
+
+        if params["mostly"] is not None:
+            params["mostly_pct"] = num_to_str(
+                params["mostly"] * 100, precision=15, no_scientific=True
+            )
+            # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+            template_str = (
+                "values must be of type $type_, at least $mostly_pct % of the time."
+            )
+        else:
+            template_str = "values must be of type $type_."
+
+        if include_column_name:
+            template_str = "$column " + template_str
+
+        if params["row_condition"] is not None:
+            (
+                conditional_template_str,
+                conditional_params,
+            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
+            template_str = conditional_template_str + ", then " + template_str
+            params.update(conditional_params)
+
+        return [
+            RenderedStringTemplateContent(
+                **{
+                    "content_block_type": "string_template",
+                    "string_template": {
+                        "template": template_str,
+                        "params": params,
+                        "styling": styling,
+                    },
+                }
+            )
+        ]
