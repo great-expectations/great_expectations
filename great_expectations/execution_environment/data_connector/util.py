d99a1bdf72c6f4fc63d0a973d5d0fa9d14b3b084
William Shin <will@superconductive.com>
2020-10-29 16:29:26 -0700

    Update util.py

--- great_expectations/execution_environment/data_connector/util.py
+++ great_expectations/execution_environment/data_connector/util.py
@@ -2,7 +2,7 @@
 
 # Utility methods for dealing with DataConnector objects
 
-from typing import List, Optional
+from typing import List, Dict, Union, Callable, Any, Tuple, Optional
 import copy
 import re
 import sre_parse
@@ -11,8 +11,13 @@ import sre_constants
 import logging
 
 from great_expectations.core.batch import BatchRequest
-from great_expectations.core.id_dict import PartitionDefinition
+from great_expectations.core.id_dict import (
+    PartitionDefinitionSubset,
+    PartitionDefinition,
+)
 from great_expectations.core.batch import BatchDefinition
+from great_expectations.execution_environment.data_connector.partition_request import PartitionRequest
+import great_expectations.exceptions as ge_exceptions
 
 logger = logging.getLogger(__name__)
 
@@ -20,9 +25,6 @@ logger = logging.getLogger(__name__)
 DEFAULT_DATA_ASSET_NAME: str = "DEFAULT_ASSET_NAME"
 
 
-# TODO: <Alex>The semantics of this method are too rigid (see below) and are incomplete.
-#  It should replaced with a rich query interface (see great_expectations/execution_environment/data_connector/partition_request.py).
-#  </Alex>
 def batch_definition_matches_batch_request(
     batch_definition: BatchDefinition,
     batch_request: BatchRequest,
@@ -55,32 +57,59 @@ def map_data_reference_string_to_batch_definition_list_using_regex(
     regex_pattern: str,
     group_names: List[str],
 ) -> Optional[List[BatchDefinition]]:
-    # noinspection PyUnresolvedReferences
-    matches: Optional[re.Match] = re.match(regex_pattern, data_reference)
-    if matches is None:
+    # TODO: <Alex>Per Abe: The two-steps process involving a BatchRequest intermediary can be simplified by turning
+    # convert_data_reference_string_to_batch_request_using_regex() into convert_data_reference_string_to_batch_definition_using_regex()
+    # and passing it all the arguments it needs to create a BatchDefinition.
+    # We could not do it before, because the Partitioner difd not have all the required arguments. But the DataConnector does have them.
+    #</Alex>
+
+    batch_request: BatchRequest = convert_data_reference_string_to_batch_request_using_regex(
+        data_reference=data_reference,
+        regex_pattern=regex_pattern,
+        group_names=group_names,
+    )
+    if batch_request is None:
         return None
 
-    groups: list = list(matches.groups())
-    partition_definition: PartitionDefinition = PartitionDefinition(
-        dict(zip(group_names, groups))
-    )
-
-    # TODO: <Alex>Accommodating "data_asset_name" inside partition_definition (e.g., via "group_names") is problematic, because it comingles concepts.</Alex>
-    data_asset_name_of_partition: str = DEFAULT_DATA_ASSET_NAME
-    if "data_asset_name" in partition_definition:
-        data_asset_name_of_partition = partition_definition.pop("data_asset_name")
-
     if data_asset_name is None:
-        data_asset_name = data_asset_name_of_partition
+        data_asset_name = batch_request.data_asset_name
 
     return [
         BatchDefinition(
             execution_environment_name=execution_environment_name,
             data_connector_name=data_connector_name,
             data_asset_name=data_asset_name,
-            partition_definition=partition_definition
+            partition_definition=PartitionDefinition(batch_request.partition_request),
         )
     ]
+
+
+def convert_data_reference_string_to_batch_request_using_regex(
+    data_reference: str,
+    regex_pattern: str,
+    group_names: List[str],
+) -> Optional[BatchRequest]:
+    # noinspection PyUnresolvedReferences
+    matches: Optional[re.Match] = re.match(regex_pattern, data_reference)
+    if matches is None:
+        return None
+
+    groups: list = list(matches.groups())
+    partition_definition: PartitionDefinitionSubset = PartitionDefinitionSubset(
+        dict(zip(group_names, groups))
+    )
+
+    # TODO: <Alex>Accommodating "data_asset_name" inside partition_definition (e.g., via "group_names") is problematic; idea: resurrect the Partition class.</Alex>
+    data_asset_name: str = DEFAULT_DATA_ASSET_NAME
+    if "data_asset_name" in partition_definition:
+        data_asset_name = partition_definition.pop("data_asset_name")
+
+    batch_request: BatchRequest = BatchRequest(
+        data_asset_name=data_asset_name,
+        partition_request=partition_definition,
+    )
+
+    return batch_request
 
 
 def map_batch_definition_to_data_reference_string_using_regex(
@@ -88,16 +117,39 @@ def map_batch_definition_to_data_reference_string_using_regex(
     regex_pattern: str,
     group_names: List[str],
 ) -> str:
-    if not isinstance(batch_definition, BatchDefinition):
-        raise TypeError("batch_definition is not of an instance of type BatchDefinition")
-
+    # TODO: <Alex>Per Abe: The two-steps process involving a BatchRequest intermediary can be simplified by turning
+    # convert_data_reference_string_to_batch_request_using_regex() into convert_data_reference_string_to_batch_definition_using_regex()
+    # and passing it all the arguments it needs to create a BatchDefinition.
+    # We could not do it before, because the Partitioner difd not have all the required arguments. But the DataConnector does have them.
+    #</Alex>
     data_asset_name: str = batch_definition.data_asset_name
     partition_definition: PartitionDefinition = batch_definition.partition_definition
+    partition_request: dict = partition_definition
+    batch_request: BatchRequest = BatchRequest(
+        data_asset_name=data_asset_name,
+        partition_request=partition_request,
+    )
+    data_reference: str = convert_batch_request_to_data_reference_string_using_regex(
+        batch_request=batch_request,
+        regex_pattern=regex_pattern,
+        group_names=group_names
+    )
+    return data_reference
 
-    template_arguments: dict = copy.deepcopy(partition_definition)
+
+# TODO: <Alex>How are we able to recover the full file path, including the file extension?  Relying on file extension being part of the regex_pattern does not work when multiple file extensions are specified as part of the regex_pattern.</Alex>
+def convert_batch_request_to_data_reference_string_using_regex(
+    batch_request: BatchRequest,
+    regex_pattern: str,
+    group_names: List[str],
+) -> str:
+    if not isinstance(batch_request, BatchRequest):
+        raise TypeError("batch_request is not of an instance of type BatchRequest")
+
+    template_arguments: dict = copy.deepcopy(batch_request.partition_request)
     # TODO: <Alex>How does "data_asset_name" factor in the computation of "converted_string"?  Does it have any effect?</Alex>
-    if data_asset_name is not None:
-        template_arguments["data_asset_name"] = data_asset_name
+    if batch_request.data_asset_name is not None:
+        template_arguments["data_asset_name"] = batch_request.data_asset_name
 
     filepath_template: str = _invert_regex_to_data_reference_template(
         regex_pattern=regex_pattern,
