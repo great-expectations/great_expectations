3d458a84e9896a203e4484ddc3f8689757868ad5
Robert Moses Lim <robert.m.lim@gmail.com>
2020-10-28 18:05:46 -0700

    Move rendering code into expectation classes

--- great_expectations/expectations/core/expect_column_unique_value_count_to_be_between.py
+++ great_expectations/expectations/core/expect_column_unique_value_count_to_be_between.py
@@ -230,6 +230,34 @@ class ExpectColumnUniqueValueCountToBeBetween(TableExpectation):
             )
         ]
 
+    @classmethod
+    @renderer(renderer_type="renderer.descriptive.column_properties_table.distinct_count_row")
+    def _descriptive_column_properties_table_distinct_count_row_renderer(
+        cls,
+        configuration=None,
+        result=None,
+        language=None,
+        runtime_configuration=None,
+        **kwargs
+    ):
+        assert result, "Must pass in result."
+        observed_value = result.result["observed_value"]
+        template_string_object = RenderedStringTemplateContent(
+            **{
+                "content_block_type": "string_template",
+                "string_template": {
+                    "template": "Distinct (%)",
+                    "tooltip": {
+                        "content": "expect_column_proportion_of_unique_values_to_be_between"
+                    },
+                },
+            }
+        )
+        if not observed_value:
+            return [template_string_object, "--"]
+        else:
+            return [template_string_object, "%.1f%%" % (100 * observed_value)]
+
     # @Expectation.validates(metric_dependencies=metric_dependencies)
     def _validates(
         self,
--- great_expectations/render/renderer/content_block/profiling_column_properties_table_content_block.py
+++ great_expectations/render/renderer/content_block/profiling_column_properties_table_content_block.py
@@ -1,3 +1,4 @@
+from great_expectations.expectations.registry import get_renderer_impl
 from great_expectations.render.types import (
     RenderedStringTemplateContent,
     RenderedTableContent,
@@ -7,136 +8,48 @@ from .content_block import ContentBlockRenderer
 
 
 class ProfilingColumnPropertiesTableContentBlockRenderer(ContentBlockRenderer):
+    expectation_renderers = {
+        "expect_column_values_to_not_match_regex": ["renderer.descriptive.column_properties_table.regex_count_row"],
+        "expect_column_unique_value_count_to_be_between": [
+            "renderer.descriptive.column_properties_table.distinct_count_row"],
+        "expect_column_proportion_of_unique_values_to_be_between": [
+            "renderer.descriptive.column_properties_table.distinct_percent_row"],
+        "expect_column_values_to_not_be_null": [
+            "renderer.descriptive.column_properties_table.missing_count_row",
+            "renderer.descriptive.column_properties_table.missing_percent_row"
+        ]
+    }
+
     @classmethod
     def render(cls, ge_object, header_row=None):
         """Each expectation method should return a list of rows"""
         if header_row is None:
             header_row = []
 
+        table_rows = []
+
         if isinstance(ge_object, list):
-            table_entries = []
             for sub_object in ge_object:
                 expectation_type = cls._get_expectation_type(sub_object)
-                extra_rows_fn = getattr(cls, expectation_type, None)
-                if extra_rows_fn is not None:
-                    rows = extra_rows_fn(sub_object)
-                    table_entries.extend(rows)
+                if expectation_type in cls.expectation_renderers:
+                    new_rows = [
+                        get_renderer_impl(expectation_type, renderer_type)[1](result=sub_object)
+                        for renderer_type in cls.expectation_renderers.get(expectation_type)
+                    ]
+                    table_rows.extend(new_rows)
         else:
-            table_entries = []
             expectation_type = cls._get_expectation_type(ge_object)
-            extra_rows_fn = getattr(cls, expectation_type, None)
-            if extra_rows_fn is not None:
-                rows = extra_rows_fn(ge_object)
-                table_entries.extend(rows)
+            if expectation_type in cls.expectation_renderers:
+                new_rows = [
+                    get_renderer_impl(expectation_type, renderer_type)[1](result=ge_object)
+                    for renderer_type in cls.expectation_renderers.get(expectation_type)
+                ]
+                table_rows.extend(new_rows)
 
         return RenderedTableContent(
             **{
                 "content_block_type": "table",
                 "header_row": header_row,
-                "table": table_entries,
+                "table": table_rows,
             }
         )
-
-    @classmethod
-    def expect_column_values_to_not_match_regex(cls, ge_object):
-        regex = ge_object.expectation_config.kwargs["regex"]
-        unexpected_count = ge_object.result["unexpected_count"]
-        if regex == "^\\s+|\\s+$":
-            return [["Leading or trailing whitespace (n)", unexpected_count]]
-        else:
-            return [["Regex: %s" % regex, unexpected_count]]
-
-    @classmethod
-    def expect_column_unique_value_count_to_be_between(cls, ge_object):
-        observed_value = ge_object.result["observed_value"]
-        return [
-            [
-                RenderedStringTemplateContent(
-                    **{
-                        "content_block_type": "string_template",
-                        "string_template": {
-                            "template": "Distinct (n)",
-                            "tooltip": {
-                                "content": "expect_column_unique_value_count_to_be_between"
-                            },
-                        },
-                    }
-                ),
-                observed_value,
-            ]
-        ]
-
-    @classmethod
-    def expect_column_proportion_of_unique_values_to_be_between(cls, ge_object):
-        observed_value = ge_object.result["observed_value"]
-        template_string_object = RenderedStringTemplateContent(
-            **{
-                "content_block_type": "string_template",
-                "string_template": {
-                    "template": "Distinct (%)",
-                    "tooltip": {
-                        "content": "expect_column_proportion_of_unique_values_to_be_between"
-                    },
-                },
-            }
-        )
-        if not observed_value:
-            return [[template_string_object, "--"]]
-        else:
-            return [[template_string_object, "%.1f%%" % (100 * observed_value)]]
-
-    @classmethod
-    def expect_column_max_to_be_between(cls, ge_object):
-        observed_value = ge_object.result["observed_value"]
-        return [["Max", observed_value]]
-
-    @classmethod
-    def expect_column_mean_to_be_between(cls, ge_object):
-        observed_value = ge_object.result["observed_value"]
-        return [["Mean", observed_value]]
-
-    @classmethod
-    def expect_column_values_to_not_be_null(cls, ge_object):
-        return [
-            [
-                RenderedStringTemplateContent(
-                    **{
-                        "content_block_type": "string_template",
-                        "string_template": {
-                            "template": "Missing (n)",
-                            "tooltip": {
-                                "content": "expect_column_values_to_not_be_null"
-                            },
-                        },
-                    }
-                ),
-                ge_object.result["unexpected_count"]
-                if "unexpected_count" in ge_object.result
-                and ge_object.result["unexpected_count"] is not None
-                else "--",
-            ],
-            [
-                RenderedStringTemplateContent(
-                    **{
-                        "content_block_type": "string_template",
-                        "string_template": {
-                            "template": "Missing (%)",
-                            "tooltip": {
-                                "content": "expect_column_values_to_not_be_null"
-                            },
-                        },
-                    }
-                ),
-                "%.1f%%" % ge_object.result["unexpected_percent"]
-                if "unexpected_percent" in ge_object.result
-                and ge_object.result["unexpected_percent"] is not None
-                else "--",
-            ],
-        ]
-
-    @classmethod
-    def expect_column_values_to_be_null(cls, ge_object):
-        return [
-            ["Populated (n)", ge_object.result["unexpected_count"]],
-            ["Populated (%)", "%.1f%%" % ge_object.result["unexpected_percent"]],
-        ]
