4a25b5909a91922379ce144226442c24e08172f8
gilpasternak35 <gilpasternak35@gmail.com>
2020-11-03 09:00:55 -0800

    Test file, testing for _get_reader_fn

---
+++ tests/execution_engine/test_pandas_execution_engine.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+from great_expectations.execution_engine import PandasExecutionEngine
+
+
+def test_reader_fn():
+    engine = PandasExecutionEngine()
+
+    # Testing that can recognize basic excel file
+    fn = engine._get_reader_fn(path="myfile.xlsx")
+    assert "<function read_excel" in str(fn)
+
+    # Ensuring that other way around works as well - reader_method should always override path
+    fn_new = engine._get_reader_fn(reader_method="read_csv")
+    assert "<function" in str(fn_new)
+
+
+def test_column_compute_domain():
+    pass
+
+
+def test_row_condition_compute_domain():
+    pass
+
+
+def test_resolve_metric_bundle():
+    pass
+
+
+def test_resolve_metric_bundle_with_nonexistent_metric():
+    pass
+
+
+def test_dataframe_method():
+    pass
+
+
+
+
+
+
