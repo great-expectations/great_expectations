fde3e1681e3d02f6d730927c90475e8036d54c31
William Shin <will@superconductive.com>
2020-11-04 11:42:20 -0800

    partitioners are gone

--- great_expectations/execution_environment/data_connector/partitioner/__init__.py
+++
@@ -1,5 +0,0 @@
-# TODO: <Alex>RegexPartitioner is being deprecated.</Alex>
-# from .partitioner import Partitioner
-# from .regex_partitioner import RegexPartitioner
-# from .pipeline_partitioner import PipelinePartitioner
-# from .partition import Partition
