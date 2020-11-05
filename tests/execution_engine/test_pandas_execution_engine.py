700b9e3f5d7a2c92f9f79ccfe6ff78c049b729cd
gilpasternak35 <gilpasternak35@gmail.com>
2020-11-05 09:09:31 -0800

    compute_domain_kwargs finished and passing, some things remain unclarified

--- tests/execution_engine/test_pandas_execution_engine.py
+++ tests/execution_engine/test_pandas_execution_engine.py
@@ -17,12 +17,48 @@ def test_reader_fn():
     assert "<function" in str(fn_new)
 
 
-def test_column_compute_domain():
-    pass
+def test_get_compute_domain_with_no_domain_kwargs():
+    engine = PandasExecutionEngine()
+    df = pd.DataFrame({"a": [1, 2, 3, 4], "b":[2,3,4,None]})
+
+    # Loading batch data
+    engine.load_batch_data(batch_data=df, batch_id="1234")
+    data, compute_kwargs, accessor_kwargs = engine.get_compute_domain(domain_kwargs={})
+    assert data.equals(df), "Data does not match after getting compute domain"
+    assert compute_kwargs is not None, "Compute domain kwargs should be existent"
+    assert accessor_kwargs == {}, "Accessor kwargs have been modified"
 
 
-def test_row_condition_compute_domain():
-    pass
+def test_get_compute_domain_with_column_domain():
+    engine = PandasExecutionEngine()
+    df = pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]})
+
+    # Loading batch data
+    engine.load_batch_data(batch_data=df, batch_id="1234")
+    data, compute_kwargs, accessor_kwargs = engine.get_compute_domain(domain_kwargs={"column": "a"})
+    # Todo: why is this not querying the column?
+    assert data.equals(df), "Data does not match after getting compute domain"
+    assert compute_kwargs is not None, "Compute domain kwargs should be existent"
+    assert accessor_kwargs == {"column": "a"}, "Accessor kwargs have been modified"
+
+
+def test_get_compute_domain_with_row_condition():
+    engine = PandasExecutionEngine()
+    df = pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]})
+    expected_df = df[df['b'] > 2].reset_index()
+
+    # Loading batch data
+    engine.load_batch_data(batch_data=df, batch_id="1234")
+
+    # Todo: why is condition parser necessary?
+    data, compute_kwargs, accessor_kwargs = engine.get_compute_domain(domain_kwargs={"row_condition": "b > 2",
+                                                                                     "condition_parser": "pandas"})
+    # Ensuring data has been properly queried
+    assert data['b'].equals(expected_df['b']), "Data does not match after getting compute domain"
+
+    # Ensuring compute kwargs have not been modified
+    assert "row_condition" in compute_kwargs.keys(), "Row condition should be located within compute kwargs"
+    assert accessor_kwargs == {}, "Accessor kwargs have been modified"
 
 
 def test_resolve_metric_bundle():
