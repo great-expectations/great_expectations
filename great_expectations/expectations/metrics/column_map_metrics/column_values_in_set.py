c1b83451aeac5688400a03534157c5368ecabb89
Robert Moses Lim <robert.m.lim@gmail.com>
2020-11-09 12:24:54 -0800

    Missing value_set should be vacuously true

--- great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
@@ -9,6 +9,32 @@ from great_expectations.expectations.metrics.column_map_metric import (
     ColumnMapMetricProvider,
     column_map_condition,
 )
+import logging
+logger = logging.getLogger(__name__)
+import numpy as np
+try:
+    # import pyspark.sql.types as sparktypes
+    # from pyspark.ml.feature import Bucketizer
+    # from pyspark.sql import SQLContext, Window
+    # from pyspark.sql.functions import (
+    #     array,
+    #     col,
+    #     count,
+    #     countDistinct,
+    #     datediff,
+    #     desc,
+    #     expr,
+    #     isnan,
+    #     lag,
+    # )
+    from pyspark.sql.functions import (
+        lit,
+    )
+except ImportError as e:
+    logger.debug(str(e))
+    logger.debug(
+        "Unable to load spark context; install optional spark dependency for support."
+    )
 
 
 class ColumnValuesInSet(ColumnMapMetricProvider):
@@ -17,12 +43,21 @@ class ColumnValuesInSet(ColumnMapMetricProvider):
 
     @column_map_condition(engine=PandasExecutionEngine)
     def _pandas(cls, column, value_set, **kwargs):
+        if value_set is None:
+            # Vacuously true
+            return np.ones(len(column), dtype=np.bool_)
         return column.isin(value_set)
 
     @column_map_condition(engine=SqlAlchemyExecutionEngine)
     def _sqlalchemy(cls, column, value_set, **kwargs):
+        if value_set is None:
+            # vacuously true
+            return True
         return column.in_(value_set)
 
     @column_map_condition(engine=SparkDFExecutionEngine)
     def _spark(cls, column, value_set, **kwargs):
+        if value_set is None:
+            # vacuously true
+            return column.withColumn("__success", lit(True))
         return column.isin(value_set)
