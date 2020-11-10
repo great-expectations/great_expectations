090614d098ab9ccfd5f0022d170a7547d870cf02
Abe <abegong@gmail.com>
2020-11-09 17:15:48 -0700

    Add SqlAlchemyExecutionEngine to __init__

--- great_expectations/execution_engine/__init__.py
+++ great_expectations/execution_engine/__init__.py
@@ -3,5 +3,6 @@ import logging
 from .execution_engine import ExecutionEngine
 from .pandas_execution_engine import PandasExecutionEngine
 from .sparkdf_execution_engine import SparkDFExecutionEngine
+from .sqlalchemy_execution_engine import SqlAlchemyExecutionEngine
 
 logger = logging.getLogger(__name__)
