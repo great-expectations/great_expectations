e3704ff825bd12d9222464f587441d686a48a43b
James Campbell <james.p.campbell@gmail.com>
2020-11-12 09:18:23 -0500

    Add _no_data_docs test config
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2226,6 +2226,27 @@ def titanic_data_context(tmp_path_factory):
 
 
 @pytest.fixture
+def titanic_data_context_no_data_docs(tmp_path_factory):
+    project_path = str(tmp_path_factory.mktemp("titanic_data_context"))
+    context_path = os.path.join(project_path, "great_expectations")
+    os.makedirs(os.path.join(context_path, "expectations"), exist_ok=True)
+    os.makedirs(os.path.join(context_path, "checkpoints"), exist_ok=True)
+    data_path = os.path.join(context_path, "../data")
+    os.makedirs(os.path.join(data_path), exist_ok=True)
+    titanic_yml_path = file_relative_path(
+        __file__, "./test_fixtures/great_expectations_titanic_no_data_docs.yml"
+    )
+    shutil.copy(
+        titanic_yml_path, str(os.path.join(context_path, "great_expectations.yml"))
+    )
+    titanic_csv_path = file_relative_path(__file__, "./test_sets/Titanic.csv")
+    shutil.copy(
+        titanic_csv_path, str(os.path.join(context_path, "../data/Titanic.csv"))
+    )
+    return ge.data_context.DataContext(context_path)
+
+
+@pytest.fixture
 def titanic_data_context_stats_enabled(tmp_path_factory, monkeypatch):
     # Reenable GE_USAGE_STATS
     monkeypatch.delenv("GE_USAGE_STATS")
