59c3a8c018632d3bcc0477da4bb263496cf94273
Alex Sherstinsky <alex@superconductivehealth.com>
2020-11-13 11:53:06 -0800

    Fix sqlalchemy imports.

--- great_expectations/execution_engine/sparkdf_execution_engine.py
+++ great_expectations/execution_engine/sparkdf_execution_engine.py
@@ -6,12 +6,26 @@ import hashlib
 
 from typing import Any, Callable, Dict, Iterable, Tuple, Optional
 
+from .execution_engine import ExecutionEngine
+from great_expectations.core.id_dict import IDDict
+from great_expectations.core.batch import BatchSpec, BatchMarkers
+from great_expectations.execution_environment.types.batch_spec import(
+    PathBatchSpec,
+    S3BatchSpec,
+    RuntimeDataBatchSpec,
+)
+from ..exceptions import BatchKwargsError, GreatExpectationsError, ValidationError, BatchSpecError
+from ..expectations.row_conditions import parse_condition_to_spark
+from ..validator.validation_graph import MetricConfiguration
+from great_expectations.exceptions import exceptions as ge_exceptions
+
+
 logger = logging.getLogger(__name__)
 
 try:
     import pyspark
     import pyspark.sql.functions as F
-    from pyspark.sql import SparkSession
+    from pyspark.sql import DataFrame, SparkSession
     from pyspark.sql.types import (
         StructType,
         StructField,
@@ -25,33 +39,7 @@ except ImportError:
     F = None
     pyspark = None
     SparkSession = None
-    logger.debug(
-        "Unable to load pyspark; install optional spark dependency for support."
-    )
-
-
-
-from great_expectations.core.id_dict import IDDict
-from great_expectations.core.batch import BatchSpec, BatchMarkers
-from great_expectations.exceptions import exceptions as ge_exceptions
-
-from great_expectations.execution_environment.types.batch_spec import(
-    PathBatchSpec,
-    S3BatchSpec,
-    RuntimeDataBatchSpec,
-)
-
-from ..exceptions import BatchKwargsError, GreatExpectationsError, ValidationError, BatchSpecError
-from ..expectations.row_conditions import parse_condition_to_spark
-from ..validator.validation_graph import MetricConfiguration
-from .execution_engine import ExecutionEngine
-
-
-try:
-    from pyspark.sql import DataFrame, SparkSession
-except ImportError:
     DataFrame = None
-    SparkSession = None
     logger.debug(
         "Unable to load pyspark; install optional spark dependency for support."
     )
--- great_expectations/execution_engine/sqlalchemy_execution_engine.py
+++ great_expectations/execution_engine/sqlalchemy_execution_engine.py
@@ -7,14 +7,26 @@ from pathlib import Path
 from typing import Any, Callable, Dict, Iterable, List, Tuple
 from urllib.parse import urlparse
 
+from great_expectations.execution_engine import ExecutionEngine
 from great_expectations.core import IDDict
+from great_expectations.core.batch import Batch, BatchMarkers
 from great_expectations.execution_environment.types import (
     SqlAlchemyDatasourceQueryBatchSpec,
     SqlAlchemyDatasourceTableBatchSpec,
 )
 from great_expectations.expectations.row_conditions import parse_condition_to_sqlalchemy
-from great_expectations.util import import_library_module
 from great_expectations.validator.validation_graph import MetricConfiguration
+from great_expectations.util import import_library_module
+from great_expectations.exceptions import (
+    BatchSpecError,
+    DatasourceKeyPairAuthBadPassphraseError,
+    GreatExpectationsError,
+    InvalidConfigError,
+    ValidationError,
+)
+
+logger = logging.getLogger(__name__)
+
 
 try:
     import sqlalchemy as sa
@@ -30,17 +42,6 @@ except ImportError:
     TextClause = None
     quoted_name = None
 
-from great_expectations.core.batch import Batch, BatchMarkers
-from great_expectations.exceptions import (
-    BatchSpecError,
-    DatasourceKeyPairAuthBadPassphraseError,
-    GreatExpectationsError,
-    InvalidConfigError,
-    ValidationError,
-)
-from great_expectations.execution_engine import ExecutionEngine
-
-logger = logging.getLogger(__name__)
 
 try:
     import psycopg2
