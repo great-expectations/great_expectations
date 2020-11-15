0d1704d1f7fe52e772d091936b0eb0dcbc9f03c2
gilpasternak35 <gilpasternak35@gmail.com>
2020-11-15 14:15:27 -0800

    Running into bugs, some bugs fixed

--- tests/execution_engine/test_sqlalchemy_execution_engine.py
+++ tests/execution_engine/test_sqlalchemy_execution_engine.py
@@ -7,73 +7,62 @@ from great_expectations.core.batch import Batch
 from great_expectations.exceptions import GreatExpectationsError
 from great_expectations.exceptions.metric_exceptions import MetricProviderError
 from great_expectations.execution_engine import SqlAlchemyExecutionEngine
+from great_expectations.execution_engine.sqlalchemy_execution_engine import SqlAlchemyBatchData
 from great_expectations.expectations.metrics import ColumnMean, ColumnStandardDeviation, ColumnValuesZScore
 from great_expectations.validator.validation_graph import MetricConfiguration
 import pyspark.sql.functions as F
 
 
 # Function to test for spark dataframe equality
+from tests.conftest import postgresql_engine
+
+
 def dataframes_equal(first_table, second_table):
-    if first_table.schema != second_table.schema:
-        return False
-    if first_table.collect() != second_table.collect():
-        return False
-    return True
+    return first_table == second_table
 
 
 # Builds a Spark Execution Engine
-def _build_spark_engine(df):
-    from pyspark.sql import SparkSession
+def _build_sqlalchemy_engine(df):
 
-    spark = SparkSession.builder.getOrCreate()
-    df = spark.createDataFrame(
-        [
-            tuple(
-                None if isinstance(x, (float, int)) and np.isnan(x) else x
-                for x in record.tolist()
-            )
-            for record in df.to_records(index=False)
-        ],
-        df.columns.tolist(),
-    )
-    batch = Batch(data=df)
-    engine = SparkDFExecutionEngine(batch_data_dict={batch.id: batch.data})
+    df.to_sql("z_score_test_data", postgresql_engine, if_exists="replace")
+    batch_data = SqlAlchemyBatchData(
+        engine=postgresql_engine, table_name="z_score_test_data")
+
+    batch = Batch(data=batch_data)
+    engine = SqlAlchemyExecutionEngine(
+        engine=postgresql_engine, batch_data_dict={batch.id: batch_data})
+
     return engine
 
 
-# Ensuring that, given aggregate metrics, they can be properly bundled together
-def test_sparkdf_batch_aggregate_metrics(caplog):
+def test_sa_batch_aggregate_metrics(caplog, sa):
     import datetime
 
-    engine = _build_spark_engine(
+    engine = _build_sqlalchemy_engine(
         pd.DataFrame({"a": [1, 2, 1, 2, 3, 3], "b": [4, 4, 4, 4, 4, 4]})
     )
 
-    # Building metric configurations
     desired_metric_1 = MetricConfiguration(
-        metric_name="column.aggregate.max",
+        metric_name="column.max.aggregate_fn",
         metric_domain_kwargs={"column": "a"},
         metric_value_kwargs=dict(),
     )
     desired_metric_2 = MetricConfiguration(
-        metric_name="column.aggregate.min",
+        metric_name="column.min.aggregate_fn",
         metric_domain_kwargs={"column": "a"},
         metric_value_kwargs=dict(),
     )
     desired_metric_3 = MetricConfiguration(
-        metric_name="column.aggregate.max",
+        metric_name="column.max.aggregate_fn",
         metric_domain_kwargs={"column": "b"},
         metric_value_kwargs=dict(),
     )
     desired_metric_4 = MetricConfiguration(
-        metric_name="column.aggregate.min",
+        metric_name="column.min.aggregate_fn",
         metric_domain_kwargs={"column": "b"},
         metric_value_kwargs=dict(),
     )
-    start = datetime.datetime.now()
-    caplog.clear()
-    caplog.set_level(logging.DEBUG, logger="great_expectations")
-    res = engine.resolve_metrics(
+    metrics = engine.resolve_metrics(
         metrics_to_resolve=(
             desired_metric_1,
             desired_metric_2,
@@ -81,7 +70,44 @@ def test_sparkdf_batch_aggregate_metrics(caplog):
             desired_metric_4,
         )
     )
+    desired_metric_1 = MetricConfiguration(
+        metric_name="column.max",
+        metric_domain_kwargs={"column": "a"},
+        metric_value_kwargs=dict(),
+        metric_dependencies={"metric_partial_fn": desired_metric_1},
+    )
+    desired_metric_2 = MetricConfiguration(
+        metric_name="column.min",
+        metric_domain_kwargs={"column": "a"},
+        metric_value_kwargs=dict(),
+        metric_dependencies={"metric_partial_fn": desired_metric_2},
+    )
+    desired_metric_3 = MetricConfiguration(
+        metric_name="column.max",
+        metric_domain_kwargs={"column": "b"},
+        metric_value_kwargs=dict(),
+        metric_dependencies={"metric_partial_fn": desired_metric_3},
+    )
+    desired_metric_4 = MetricConfiguration(
+        metric_name="column.min",
+        metric_domain_kwargs={"column": "b"},
+        metric_value_kwargs=dict(),
+        metric_dependencies={"metric_partial_fn": desired_metric_4},
+    )
+    caplog.clear()
+    caplog.set_level(logging.DEBUG, logger="great_expectations")
+    start = datetime.datetime.now()
+    res = engine.resolve_metrics(
+        metrics_to_resolve=(
+            desired_metric_1,
+            desired_metric_2,
+            desired_metric_3,
+            desired_metric_4,
+        ),
+        metrics=metrics,
+    )
     end = datetime.datetime.now()
+    print("t1")
     print(end - start)
     assert res[desired_metric_1.id] == 3
     assert res[desired_metric_2.id] == 1
@@ -92,8 +118,8 @@ def test_sparkdf_batch_aggregate_metrics(caplog):
     found_message = False
     for record in caplog.records:
         if (
-                record.message
-                == "SparkDFExecutionEngine computed 4 metrics on domain_id ()"
+            record.message
+            == "SqlAlchemyExecutionEngine computed 4 metrics on domain_id ()"
         ):
             found_message = True
     assert found_message
@@ -101,7 +127,7 @@ def test_sparkdf_batch_aggregate_metrics(caplog):
 
 # Ensuring functionality of compute_domain when no domain kwargs are given
 def test_get_compute_domain_with_no_domain_kwargs():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
 
     # Loading batch data
@@ -116,7 +142,7 @@ def test_get_compute_domain_with_no_domain_kwargs():
 
 # Testing whether compute domain is properly calculated, but this time obtaining a column
 def test_get_compute_domain_with_column_domain():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
 
     # Loading batch data
@@ -131,7 +157,7 @@ def test_get_compute_domain_with_column_domain():
 
 # Using an unmeetable row condition to see if empty dataset will result in errors
 def test_get_compute_domain_with_row_condition():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
     expected_df = df.where('b > 2')
 
@@ -151,7 +177,7 @@ def test_get_compute_domain_with_row_condition():
 
 # What happens when we filter such that no value meets the condition?
 def test_get_compute_domain_with_unmeetable_row_condition():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
     expected_df = df.where('b > 24')
 
@@ -170,7 +196,7 @@ def test_get_compute_domain_with_unmeetable_row_condition():
 
 # Testing to ensure that great expectation experimental parser also works in terms of defining a compute domain
 def test_get_compute_domain_with_ge_experimental_condition_parser():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
 
     # Filtering expected data based on row condition
@@ -192,7 +218,7 @@ def test_get_compute_domain_with_ge_experimental_condition_parser():
 
 
 def test_get_compute_domain_with_nonexistent_condition_parser():
-    engine = _build_spark_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
+    engine = _build_sqlalchemy_engine(pd.DataFrame({"a": [1, 2, 3, 4], "b": [2, 3, 4, None]}))
     df = engine.dataframe
 
     # Loading batch data
@@ -208,7 +234,7 @@ def test_get_compute_domain_with_nonexistent_condition_parser():
 def test_resolve_metric_bundle_with_nonaggregate_metric(caplog):
     import datetime
 
-    engine = _build_spark_engine(
+    engine = _build_sqlalchemy_engine(
         pd.DataFrame({"a": [1, 2, 1, 2, 3, 3], "b": [4, 4, 4, 4, 4, 4]})
     )
 
@@ -262,7 +288,7 @@ def test_resolve_metric_bundle_with_nonaggregate_metric(caplog):
 
 # Ensuring that we can properly inform user when metric doesn't exist - should get a metric provider error
 def test_resolve_metric_bundle_with_nonexistent_metric():
-    engine = _build_spark_engine(
+    engine = _build_sqlalchemy_engine(
         pd.DataFrame({"a": [1, 2, 1, 2, 3, 3], "b": [4, 4, 4, 4, 4, 4]})
     )
 
