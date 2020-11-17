19d4b4bbd44c636e2bb466860ab0962f414df1e1
Robert Moses Lim <robert.m.lim@gmail.com>
2020-11-16 22:43:12 -0800

    Update implementation

--- great_expectations/expectations/core/expect_column_unique_value_count_to_be_between.py
+++ great_expectations/expectations/core/expect_column_unique_value_count_to_be_between.py
@@ -20,12 +20,12 @@ from ..expectation import (
     Expectation,
     InvalidExpectationConfigurationError,
     TableExpectation,
-    _format_map_output,
+    _format_map_output, ColumnExpectation,
 )
 from ..registry import extract_metrics
 
 
-class ExpectColumnUniqueValueCountToBeBetween(TableExpectation):
+class ExpectColumnUniqueValueCountToBeBetween(ColumnExpectation):
     """Expect the number of unique values to be between a minimum value and a maximum value.
 
             expect_column_unique_value_count_to_be_between is a \
@@ -80,7 +80,7 @@ class ExpectColumnUniqueValueCountToBeBetween(TableExpectation):
             """
 
     # Setting necessary computation metric dependencies and defining kwargs, as well as assigning kwargs default values\
-    metric_dependencies = ("column.unique_value_count",)
+    metric_dependencies = ("column.distinct_values.count",)
     success_keys = (
         "min_value",
         "max_value",
@@ -110,50 +110,8 @@ class ExpectColumnUniqueValueCountToBeBetween(TableExpectation):
         Returns:
             True if the configuration has been validated successfully. Otherwise, raises an exception
         """
-        min_val = None
-        max_val = None
-
-        # Setting up a configuration
         super().validate_configuration(configuration)
-        if configuration is None:
-            configuration = self.configuration
-
-        # Ensuring basic configuration parameters are properly set
-        try:
-            assert (
-                "column" in configuration.kwargs
-            ), "'column' parameter is required for any column expectation"
-        except AssertionError as e:
-            raise InvalidExpectationConfigurationError(str(e))
-
-        # Validating that Minimum and Maximum values are of the proper format and type
-        if "min_value" in configuration.kwargs:
-            min_val = configuration.kwargs["min_value"]
-
-        if "max_value" in configuration.kwargs:
-            max_val = configuration.kwargs["max_value"]
-
-        try:
-            # Ensuring Proper interval has been provided
-            assert (
-                min_val is not None or max_val is not None
-            ), "min_value and max_value cannot both be None"
-            assert min_val is None or isinstance(
-                min_val, (float, int)
-            ), "Provided min threshold must be a number"
-            assert max_val is None or isinstance(
-                max_val, (float, int)
-            ), "Provided max threshold must be a number"
-
-        except AssertionError as e:
-            raise InvalidExpectationConfigurationError(str(e))
-
-        if min_val is not None and max_val is not None and min_val > max_val:
-            raise InvalidExpectationConfigurationError(
-                "Minimum Threshold cannot be larger than Maximum Threshold"
-            )
-
-        return True
+        self.validate_metric_value_between_configuration(configuration=configuration)
 
     @classmethod
     @renderer(renderer_type="renderer.prescriptive")
@@ -263,56 +221,17 @@ class ExpectColumnUniqueValueCountToBeBetween(TableExpectation):
         else:
             return [template_string_object, "%.1f%%" % (100 * observed_value)]
 
-    # @Expectation.validates(metric_dependencies=metric_dependencies)
-    def _validates(
+    def _validate(
         self,
         configuration: ExpectationConfiguration,
         metrics: Dict,
         runtime_configuration: dict = None,
         execution_engine: ExecutionEngine = None,
     ):
-        """Validates the given data against the set minimum and maximum value thresholds for the column median"""
-        # Obtaining dependencies used to validate the expectation
-        validation_dependencies = self.get_validation_dependencies(
-            configuration, execution_engine, runtime_configuration
-        )["metrics"]
-        # Extracting metrics
-        metric_vals = extract_metrics(
-            validation_dependencies, metrics, configuration, runtime_configuration
+        return self._validate_metric_value_between(
+            metric_name="column.distinct_values.count",
+            configuration=configuration,
+            metrics=metrics,
+            runtime_configuration=runtime_configuration,
+            execution_engine=execution_engine,
         )
-
-        # Runtime configuration has preference
-        if runtime_configuration:
-            result_format = runtime_configuration.get(
-                "result_format",
-                configuration.kwargs.get(
-                    "result_format", self.default_kwarg_values.get("result_format")
-                ),
-            )
-        else:
-            result_format = configuration.kwargs.get(
-                "result_format", self.default_kwarg_values.get("result_format")
-            )
-
-        # Obtaining components needed for validation
-        min_value = self.get_success_kwargs(configuration).get("min_value")
-        max_value = self.get_success_kwargs(configuration).get("max_value")
-
-        unique_value_count = metric_vals.get("column.unique_value_count")
-
-        if unique_value_count is None:
-            return {"success": False, "result": {"observed_value": unique_value_count}}
-
-        if min_value is not None:
-            above_min = unique_value_count >= min_value
-        else:
-            above_min = True
-
-        if max_value is not None:
-            below_max = unique_value_count <= max_value
-        else:
-            below_max = True
-
-        success = above_min and below_max
-
-        return {"success": success, "result": {"observed_value": unique_value_count}}
