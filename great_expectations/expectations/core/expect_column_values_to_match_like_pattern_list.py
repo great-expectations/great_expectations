e70d53ed6aebff159015c1147c3e37190c3e893f
Robert Moses Lim <robert.m.lim@gmail.com>
2020-11-16 23:46:26 -0800

    Fix typos

--- great_expectations/expectations/core/expect_column_values_to_match_like_pattern_list.py
+++ great_expectations/expectations/core/expect_column_values_to_match_like_pattern_list.py
@@ -44,7 +44,6 @@ class ExpectColumnValuesToMatchLikePatternList(ColumnMapExpectation):
         "row_condition": None,
         "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
         "mostly": 1,
-        "parse_strings_as_datetimes": None,
         "result_format": "BASIC",
         "include_config": True,
         "catch_exceptions": True,
@@ -87,38 +86,3 @@ class ExpectColumnValuesToMatchLikePatternList(ColumnMapExpectation):
             configuration.kwargs,
             ["column", "mostly", "row_condition", "condition_parser"],
         )
-
-        if include_column_name:
-            template_str = "$column values must be unique"
-        else:
-            template_str = "values must be unique"
-
-        if params["mostly"] is not None:
-            params["mostly_pct"] = num_to_str(
-                params["mostly"] * 100, precision=15, no_scientific=True
-            )
-            # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
-            template_str += ", at least $mostly_pct % of the time."
-        else:
-            template_str += "."
-
-        if params["row_condition"] is not None:
-            (
-                conditional_template_str,
-                conditional_params,
-            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
-            template_str = conditional_template_str + ", then " + template_str
-            params.update(conditional_params)
-
-        return [
-            RenderedStringTemplateContent(
-                **{
-                    "content_block_type": "string_template",
-                    "string_template": {
-                        "template": template_str,
-                        "params": params,
-                        "styling": styling,
-                    },
-                }
-            )
-        ]
