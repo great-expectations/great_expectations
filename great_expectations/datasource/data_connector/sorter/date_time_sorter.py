9007922a50ac9bb42ce03d1d4058876ddd8979fb
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2020-12-02 08:47:54 -0800

    Alexsherstinsky/post 0.13 release cleanup 12 01 2020 2 (#2096)
    * Remove numeric checkers from Sorter modules.
    
    * Remove numeric checkers from Sorter modules.
    
    * imports
    
    * Isort for Sorters must not default to alphabetical order.

--- great_expectations/datasource/data_connector/sorter/date_time_sorter.py
+++ great_expectations/datasource/data_connector/sorter/date_time_sorter.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations.core.batch import BatchDefinition
-from great_expectations.datasource.data_connector.sorter.sorter import Sorter
+from great_expectations.datasource.data_connector.sorter import Sorter
 
 logger = logging.getLogger(__name__)
 
