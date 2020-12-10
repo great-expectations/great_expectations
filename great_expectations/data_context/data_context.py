1bb2cd66ccf24270c857d393a3ff4a362c3d70ab
Alex Sherstinsky <alex@superconductivehealth.com>
2020-12-10 09:40:52 -0800

    Removing V3 WIP

--- great_expectations/data_context/data_context.py
+++ great_expectations/data_context/data_context.py
@@ -8,6 +8,7 @@ import logging
 import os
 import shutil
 import sys
+import traceback
 import uuid
 import warnings
 import webbrowser
@@ -1579,16 +1580,21 @@ class BaseDataContext:
         else:
             config = kwargs
 
+        return self._instantiate_datasource_from_config_and_update_project_config(
+            name=name, config=config, initialize=initialize,
+        )
+
+    def _instantiate_datasource_from_config_and_update_project_config(
+        self, name: str, config: dict, initialize: bool = True
+    ) -> Optional[Union[LegacyDatasource, BaseDatasource]]:
         datasource_config: DatasourceConfig = datasourceConfigSchema.load(
             CommentedMap(**config)
         )
         self._project_config["datasources"][name] = datasource_config
-
         datasource_config = self._project_config_with_variables_substituted.datasources[
             name
         ]
-        config = dict(datasourceConfigSchema.dump(datasource_config))
-
+        config: dict = dict(datasourceConfigSchema.dump(datasource_config))
         datasource: Optional[Union[LegacyDatasource, BaseDatasource]]
         if initialize:
             try:
@@ -1602,7 +1608,6 @@ class BaseDataContext:
                 raise e
         else:
             datasource = None
-
         return datasource
 
     def _instantiate_datasource_from_config(
@@ -2755,6 +2760,136 @@ Generated, evaluated, and stored %d Expectations during profiling. Please review
         checkpoints_dir = os.path.join(self.root_directory, self.CHECKPOINTS_DIR)
         files = glob.glob(os.path.join(checkpoints_dir, "*.yml"), recursive=False)
         return files
+
+    def test_yaml_config(
+        self,
+        yaml_config: str,
+        name=None,
+        pretty_print=True,
+        return_mode="instantiated_class",
+        shorten_tracebacks=False,
+    ):
+        """ Convenience method for testing yaml configs
+
+        test_yaml_config is a convenience method for configuring the moving
+        parts of a Great Expectations deployment. It allows you to quickly
+        test out configs for system components, especially Datasources,
+        Checkpoints, and Stores.
+
+        For many deployments of Great Expectations, these components (plus
+        Expectations) are the only ones you'll need.
+
+        test_yaml_config is mainly intended for use within notebooks and tests.
+
+        Parameters
+        ----------
+        yaml_config : str
+            A string containing the yaml config to be tested
+
+        name: str
+            (Optional) A string containing the name of the component to instantiate
+
+        pretty_print : bool
+            Determines whether to print human-readable output
+
+        return_mode : str
+            Determines what type of object test_yaml_config will return
+            Valid modes are "instantiated_class" and "report_object"
+
+        shorten_tracebacks : bool
+            If true, catch any errors during instantiation and print only the
+            last element of the traceback stack. This can be helpful for
+            rapid iteration on configs in a notebook, because it can remove
+            the need to scroll up and down a lot.
+
+        Returns
+        -------
+        The instantiated component (e.g. a Datasource)
+        OR
+        a json object containing metadata from the component's self_check method
+
+        The returned object is determined by return_mode.
+        """
+        if pretty_print:
+            print("Attempting to instantiate class from config...")
+
+        if return_mode not in ["instantiated_class", "report_object"]:
+            raise ValueError(f"Unknown return_mode: {return_mode}.")
+
+        substituted_config_variables = substitute_all_config_variables(
+            self.config_variables, dict(os.environ),
+        )
+
+        substitutions = {
+            **substituted_config_variables,
+            **dict(os.environ),
+            **self.runtime_environment,
+        }
+
+        config_str_with_substituted_variables = substitute_all_config_variables(
+            yaml_config, substitutions,
+        )
+
+        config = yaml.load(config_str_with_substituted_variables)
+
+        if "class_name" in config:
+            class_name = config["class_name"]
+        else:
+            class_name = None
+
+        try:
+            if class_name in [
+                "ExpectationsStore",
+                "ValidationsStore",
+                "HtmlSiteStore",
+                "EvaluationParameterStore",
+                "MetricStore",
+                "SqlAlchemyQueryStore",
+            ]:
+                print(f"\tInstantiating as a Store, since class_name is {class_name}")
+                instantiated_class = self._build_store_from_config(
+                    "my_temp_store", config
+                )
+
+            elif class_name in [
+                "Datasource",
+                "SimpleSqlalchemyDatasource",
+            ]:
+                print(
+                    f"\tInstantiating as a Datasource, since class_name is {class_name}"
+                )
+                datasource_name = name or "my_temp_datasource"
+                instantiated_class = self._instantiate_datasource_from_config_and_update_project_config(
+                    name=datasource_name, config=config, initialize=True,
+                )
+
+            else:
+                print(
+                    "\tNo matching class found. Attempting to instantiate class from the raw config..."
+                )
+                instantiated_class = instantiate_class_from_config(
+                    config, runtime_environment={}, config_defaults={}
+                )
+
+            if pretty_print:
+                print(
+                    f"\tSuccessfully instantiated {instantiated_class.__class__.__name__}"
+                )
+                print()
+
+            report_object = instantiated_class.self_check(pretty_print)
+
+            if return_mode == "instantiated_class":
+                return instantiated_class
+
+            elif return_mode == "report_object":
+                return report_object
+
+        except Exception as e:
+            if shorten_tracebacks:
+                traceback.print_exc(limit=1)
+            else:
+                raise e
 
 
 class DataContext(BaseDataContext):
