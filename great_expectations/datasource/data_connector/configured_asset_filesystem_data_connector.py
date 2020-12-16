61c7559981adf4870e9998974c57001bbbfbc2ae
Shinnnyshinshin <will@superconductive.com>
2020-12-16 14:48:44 -0800

    Update configured_asset_filesystem_data_connector.py

--- great_expectations/datasource/data_connector/configured_asset_filesystem_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_filesystem_data_connector.py
@@ -91,13 +91,12 @@ class ConfiguredAssetFilesystemDataConnector(ConfiguredAssetFilePathDataConnecto
         if asset is not None:
             if asset.base_directory:
                 base_directory = normalize_directory_path(
-                    dir_path=asset.base_directory,
-                    root_directory_path=self.data_context_root_directory,
+                    dir_path=asset.base_directory, root_directory_path=base_directory,
                 )
         return str(Path(base_directory).joinpath(path))
 
     @property
-    def base_directory(self):
+    def base_directory(self) -> str:
         """
         Accessor method for base_directory. If directory is a relative path, interpret it as relative to the
         root directory. If it is absolute, then keep as-is.
