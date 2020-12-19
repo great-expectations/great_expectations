dfc687b5540b645d5108668444a2792ba08997e0
Alex Sherstinsky <alex@superconductivehealth.com>
2020-12-19 09:34:06 -0800

    troubleshooting azure errors

--- tests/cli/test_checkpoint.py
+++ tests/cli/test_checkpoint.py
@@ -483,8 +483,9 @@ def test_checkpoint_run_on_checkpoint_with_batch_load_problem_raises_error(
     #     "Please verify these batch kwargs in checkpoint bad_batch`"
     #     in stdout
     # )
-    print(f'\n[ALEX_TEST] STDOUT: {stdout} ;\nTYPE: {str(type(stdout))}')
-    assert "No such file or directory" in stdout
+    print(f"\n[ALEX_TEST] STDOUT: {stdout} ;\nTYPE: {str(type(stdout))}")
+    # assert "No such file or directory" in stdout
+    assert ("No such file or directory" in stdout) or ("does not exist" in stdout)
 
     assert mock_emit.call_count == 2
     assert mock_emit.call_args_list == [
