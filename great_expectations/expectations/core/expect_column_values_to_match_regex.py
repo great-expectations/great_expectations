b40b231d010453af86c80f2471ee03698f4d6e3c
William Shin <will@superconductive.com>
2020-12-21 10:04:48 -0800

    [ENHANCEMENT] DataDocs show values of Evaluation Parameters (#2165)
    * Update validation_results_table_content_block.py
    * Update validation_results_table_content_block.py
    * clean up and making tests pass
    * added test to column_section_renderer
    * cleaning up old fixtures
    * Update test_default_markdown_view.py
    * renamed and made evaluation_parameter consistent

--- great_expectations/expectations/core/expect_column_values_to_match_regex.py
+++ great_expectations/expectations/core/expect_column_values_to_match_regex.py
@@ -9,6 +9,7 @@ from great_expectations.execution_engine import (
     PandasExecutionEngine,
     SparkDFExecutionEngine,
 )
+from great_expectations.expectations.util import render_evaluation_parameter_string
 
 from ...core.batch import Batch
 from ...data_asset.util import parse_result_format
@@ -143,6 +144,7 @@ class ExpectColumnValuesToMatchRegex(ColumnMapExpectation):
 
     @classmethod
     @renderer(renderer_type="renderer.prescriptive")
+    @render_evaluation_parameter_string
     def _prescriptive_renderer(
         cls,
         configuration=None,
