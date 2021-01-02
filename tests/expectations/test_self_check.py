94324e57837d659999c08d79b05a475dfc1bb2d2
Abe <abegong@gmail.com>
2021-01-02 13:25:05 -0700

    Add third iteration of ExpectColumnValuesToEqualThree, with renderers defined.

--- tests/expectations/test_self_check.py
+++ tests/expectations/test_self_check.py
@@ -19,6 +19,13 @@ from great_expectations.expectations.expectation import (
 from great_expectations.execution_engine import (
     PandasExecutionEngine
 )
+from great_expectations.render.renderer.renderer import renderer
+from great_expectations.expectations.util import render_evaluation_parameter_string
+from great_expectations.render.util import (
+    num_to_str,
+    substitute_none_for_missing,
+)
+from great_expectations.render.types import RenderedStringTemplateContent
 
 class ColumnValuesEqualThree(ColumnMapMetricProvider):
     condition_metric_name = "column_values.equal_three"
@@ -69,11 +76,88 @@ class ExpectColumnValuesToEqualThree__SecondIteration(ExpectColumnValuesToEqualT
         "updated_at": 1609165558,
     }
 
-class ExpectColumnValuesToEqualThree__ThirdIteration(ColumnMapExpectation):
+class ExpectColumnValuesToEqualThree__ThirdIteration(ExpectColumnValuesToEqualThree__SecondIteration):
 
-    map_metric = "column_values.equal_three"
-    success_keys = ("mostly",)
+    @classmethod
+    @renderer(renderer_type="renderer.question")
+    def _question_renderer(
+        cls, configuration, result=None, language=None, runtime_configuration=None
+    ):
+        column = configuration.kwargs.get("column")
+        mostly = configuration.kwargs.get("mostly")
+        regex = configuration.kwargs.get("regex")
 
+        return f'Do at least {mostly * 100}% of values in column "{column}" equal 3?'
+
+    @classmethod
+    @renderer(renderer_type="renderer.answer")
+    def _answer_renderer(
+        cls, configuration=None, result=None, language=None, runtime_configuration=None
+    ):
+        column = result.expectation_config.kwargs.get("column")
+        mostly = result.expectation_config.kwargs.get("mostly")
+        regex = result.expectation_config.kwargs.get("regex")
+        if result.success:
+            return f'At least {mostly * 100}% of values in column "{column}" equal 3.'
+        else:
+            return f'Less than {mostly * 100}% of values in column "{column}" equal 3.'
+
+
+    @classmethod
+    @renderer(renderer_type="renderer.prescriptive")
+    @render_evaluation_parameter_string
+    def _prescriptive_renderer(
+        cls,
+        configuration=None,
+        result=None,
+        language=None,
+        runtime_configuration=None,
+        **kwargs,
+    ):
+        runtime_configuration = runtime_configuration or {}
+        include_column_name = runtime_configuration.get("include_column_name", True)
+        include_column_name = (
+            include_column_name if include_column_name is not None else True
+        )
+        styling = runtime_configuration.get("styling")
+        params = substitute_none_for_missing(
+            configuration.kwargs,
+            ["column", "regex", "mostly", "row_condition", "condition_parser"],
+        )
+
+        template_str = "values must be equal to 3"
+        if params["mostly"] is not None:
+            params["mostly_pct"] = num_to_str(
+                params["mostly"] * 100, precision=15, no_scientific=True
+            )
+            # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+            template_str += ", at least $mostly_pct % of the time."
+        else:
+            template_str += "."
+
+        if include_column_name:
+            template_str = "$column " + template_str
+
+        if params["row_condition"] is not None:
+            (
+                conditional_template_str,
+                conditional_params,
+            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
+            template_str = conditional_template_str + ", then " + template_str
+            params.update(conditional_params)
+
+        return [
+            RenderedStringTemplateContent(
+                **{
+                    "content_block_type": "string_template",
+                    "string_template": {
+                        "template": template_str,
+                        "params": params,
+                        "styling": styling,
+                    },
+                }
+            )
+        ]
 
 def test_expectation_self_check():
 
@@ -252,7 +336,7 @@ def test_expectation__get_renderers():
     }
 
 
-    # Expectation with no renderers specified
+    # Expectation with no new renderers specified
     print([x for x in _registered_expectations.keys() if "second" in x])
     expectation_name = "expect_column_values_to_equal_three___second_iteration"
     my_expectation = _registered_expectations[expectation_name]()
@@ -282,4 +366,38 @@ def test_expectation__get_renderers():
         "renderer.diagnostic.unexpected_statement": "",
         "renderer.diagnostic.unexpected_table": None,
         "renderer.prescriptive": "expect_column_values_to_equal_three___second_iteration(**{'column': 'mostly_threes', 'mostly': 0.6})",
+    }
+
+    # Expectation with no renderers specified
+    print([x for x in _registered_expectations.keys() if "second" in x])
+    expectation_name = "expect_column_values_to_equal_three___third_iteration"
+    my_expectation = _registered_expectations[expectation_name]()
+
+    supported_renderers = my_expectation._get_supported_renderers(expectation_name)
+    examples = my_expectation._get_examples()
+    example_data, example_test = my_expectation._choose_example(examples)
+    my_batch, my_expectation_config, my_validation_results = my_expectation._instantiate_example_objects(
+        expectation_name,
+        example_data,
+        example_test,
+    )
+    my_validation_result = my_validation_results[0]
+
+    renderer_dict = my_expectation._get_rendered_dict(
+        expectation_name,
+        supported_renderers,
+        my_expectation_config,
+        my_validation_result,
+    )
+    
+    print(json.dumps(renderer_dict, indent=2))
+
+    assert renderer_dict == {
+        "renderer.answer": "At least 60.0% of values in column \"mostly_threes\" equal 3.",
+        "renderer.diagnostic.observed_value": "20% unexpected",
+        "renderer.diagnostic.status_icon": "",
+        "renderer.diagnostic.unexpected_statement": "",
+        "renderer.diagnostic.unexpected_table": None,
+        "renderer.prescriptive": "mostly_threes values must be equal to 3, at least 60 % of the time.",
+        "renderer.question": "Do at least 60.0% of values in column \"mostly_threes\" equal 3?"
     }
