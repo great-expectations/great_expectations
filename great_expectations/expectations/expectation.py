b7b32b78ead4ea45625fa8997ecaae1af22d7fa0
Abe <abegong@gmail.com>
2021-01-16 19:07:52 -0700

    Factor out _get_test_results and fix bug due to mutated config

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -758,39 +758,11 @@ class Expectation(ABC, metaclass=MetaExpectation):
             )
             report_obj.update({"execution_engines": execution_engines})
 
-            test_results = []
-
-            exp_tests = generate_expectation_tests(
+            test_results = self._get_test_results(
                 snake_name,
                 examples,
-                expectation_execution_engines_dict=execution_engines,
+                execution_engines,
             )
-
-            for exp_test in exp_tests:
-                try:
-                    evaluate_json_test_cfe(
-                        validator=exp_test["validator_with_data"],
-                        expectation_type=exp_test["expectation_type"],
-                        test=exp_test["test"],
-                    )
-                    test_results.append(
-                        {
-                            "test title": exp_test["test"]["title"],
-                            "backend": exp_test["backend"],
-                            "success": "true",
-                        }
-                    )
-                except Exception as e:
-                    test_results.append(
-                        {
-                            "test title": exp_test["test"]["title"],
-                            "backend": exp_test["backend"],
-                            "success": "false",
-                            "error_message": str(e),
-                            "stack_trace": traceback.format_exc(),
-                        }
-                    )
-
             report_obj.update({"test_report": test_results})
 
         return report_obj
@@ -864,6 +836,47 @@ class Expectation(ABC, metaclass=MetaExpectation):
         supported_renderers = list(_registered_renderers[snake_name].keys())
         supported_renderers.sort()
         return supported_renderers
+
+    def _get_test_results(
+        self,
+        snake_name,
+        examples,
+        execution_engines,
+    ):
+        test_results = []
+
+        exp_tests = generate_expectation_tests(
+            snake_name,
+            examples,
+            expectation_execution_engines_dict=execution_engines,
+        )
+
+        for exp_test in exp_tests:
+            try:
+                evaluate_json_test_cfe(
+                    validator=exp_test["validator_with_data"],
+                    expectation_type=exp_test["expectation_type"],
+                    test=exp_test["test"],
+                )
+                test_results.append(
+                    {
+                        "test title": exp_test["test"]["title"],
+                        "backend": exp_test["backend"],
+                        "success": "true",
+                    }
+                )
+            except Exception as e:
+                test_results.append(
+                    {
+                        "test title": exp_test["test"]["title"],
+                        "backend": exp_test["backend"],
+                        "success": "false",
+                        "error_message": str(e),
+                        "stack_trace": traceback.format_exc(),
+                    }
+                )
+        
+        return test_results
 
     from great_expectations.render.types import RenderedStringTemplateContent
 
--- great_expectations/expectations/self_check_util.py
+++ great_expectations/expectations/self_check_util.py
@@ -1730,6 +1730,7 @@ def generate_expectation_tests(
 
     for c in backends:
         for d in examples_config:
+            d = copy.deepcopy(d)
             datasets = []
             if candidate_test_is_on_temporary_notimplemented_list_cfe(
                 c, expectation_type
