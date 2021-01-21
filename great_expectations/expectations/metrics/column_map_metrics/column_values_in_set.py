b0f130e46ab73eecb30bdcb23bed884fc53bffa4
James Campbell <james.p.campbell@gmail.com>
2021-01-21 12:23:13 -0500

    [MAINTENANCE] Code Cleanup
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
@@ -1,4 +1,4 @@
-import logging
+import numpy as np
 
 from great_expectations.execution_engine import (
     PandasExecutionEngine,
@@ -12,31 +12,6 @@ from great_expectations.expectations.metrics.map_metric import (
     ColumnMapMetricProvider,
     column_condition_partial,
 )
-
-logger = logging.getLogger(__name__)
-import numpy as np
-
-try:
-    # import pyspark.sql.types as sparktypes
-    # from pyspark.ml.feature import Bucketizer
-    # from pyspark.sql import SQLContext, Window
-    # from pyspark.sql.functions import (
-    #     array,
-    #     col,
-    #     count,
-    #     countDistinct,
-    #     datediff,
-    #     desc,
-    #     expr,
-    #     isnan,
-    #     lag,
-    # )
-    from pyspark.sql.functions import lit
-except ImportError as e:
-    logger.debug(str(e))
-    logger.debug(
-        "Unable to load spark context; install optional spark dependency for support."
-    )
 
 
 class ColumnValuesInSet(ColumnMapMetricProvider):
