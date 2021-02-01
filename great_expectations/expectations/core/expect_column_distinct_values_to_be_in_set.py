cace0591a38e712103a7a0fbe92c74dca1a69ae5
Sam Bail <sam@superconductive.com>
2021-02-01 17:56:53 -0500

    Adding library meta to a core expectation

--- great_expectations/expectations/core/expect_column_distinct_values_to_be_in_set.py
+++ great_expectations/expectations/core/expect_column_distinct_values_to_be_in_set.py
@@ -93,6 +93,14 @@ class ExpectColumnDistinctValuesToBeInSet(ColumnExpectation):
 
             """
 
+    library_metadata = {
+        "maturity": "production",
+        "package": "great_expectations",
+        "tags": ["core expectation", "column aggregate expectation"],
+        "contributors": ["@great_expectations"],
+        "requirements": [],
+    }
+
     # Setting necessary computation metric dependencies and defining kwargs, as well as assigning kwargs default values\
     metric_dependencies = ("column.value_counts",)
     success_keys = (
