066ca1e0b8035cfdd1bc2844250253b01b37654d
Anthony Burdi <anthony@superconductive.com>
2021-02-02 16:42:46 -0500

    add checkpoint store to GCSStoreBackendDefaults

--- great_expectations/data_context/types/base.py
+++ great_expectations/data_context/types/base.py
@@ -1243,15 +1243,19 @@ class GCSStoreBackendDefaults(BaseStoreBackendDefaults):
         expectations_store_bucket_name: Overrides default_bucket_name if supplied
         validations_store_bucket_name: Overrides default_bucket_name if supplied
         data_docs_bucket_name: Overrides default_bucket_name if supplied
+        checkpoint_store_bucket_name: Overrides default_bucket_name if supplied
         expectations_store_project_name: Overrides default_project_name if supplied
         validations_store_project_name: Overrides default_project_name if supplied
         data_docs_project_name: Overrides default_project_name if supplied
+        checkpoint_store_project_name: Overrides default_project_name if supplied
         expectations_store_prefix: Overrides default if supplied
         validations_store_prefix: Overrides default if supplied
         data_docs_prefix: Overrides default if supplied
+        checkpoint_store_prefix: Overrides default if supplied
         expectations_store_name: Overrides default if supplied
         validations_store_name: Overrides default if supplied
         evaluation_parameter_store_name: Overrides default if supplied
+        checkpoint_store_name: Overrides default if supplied
     """
 
     def __init__(
@@ -1261,15 +1265,19 @@ class GCSStoreBackendDefaults(BaseStoreBackendDefaults):
         expectations_store_bucket_name: Optional[str] = None,
         validations_store_bucket_name: Optional[str] = None,
         data_docs_bucket_name: Optional[str] = None,
+        checkpoint_store_bucket_name: Optional[str] = None,
         expectations_store_project_name: Optional[str] = None,
         validations_store_project_name: Optional[str] = None,
         data_docs_project_name: Optional[str] = None,
+        checkpoint_store_project_name: Optional[str] = None,
         expectations_store_prefix: str = "expectations",
         validations_store_prefix: str = "validations",
         data_docs_prefix: str = "data_docs",
+        checkpoint_store_prefix: str = "data_docs",
         expectations_store_name: str = "expectations_GCS_store",
         validations_store_name: str = "validations_GCS_store",
         evaluation_parameter_store_name: str = "evaluation_parameter_store",
+        checkpoint_store_name: str = "evaluation_parameter_store",
     ):
         # Initialize base defaults
         super().__init__()
@@ -1281,6 +1289,8 @@ class GCSStoreBackendDefaults(BaseStoreBackendDefaults):
             validations_store_bucket_name = default_bucket_name
         if data_docs_bucket_name is None:
             data_docs_bucket_name = default_bucket_name
+        if checkpoint_store_bucket_name is None:
+            checkpoint_store_bucket_name = default_bucket_name
 
         # Use default_project_name if separate store projects are not provided
         if expectations_store_project_name is None:
@@ -1289,11 +1299,14 @@ class GCSStoreBackendDefaults(BaseStoreBackendDefaults):
             validations_store_project_name = default_project_name
         if data_docs_project_name is None:
             data_docs_project_name = default_project_name
+        if checkpoint_store_project_name is None:
+            checkpoint_store_project_name = default_project_name
 
         # Overwrite defaults
         self.expectations_store_name = expectations_store_name
         self.validations_store_name = validations_store_name
         self.evaluation_parameter_store_name = evaluation_parameter_store_name
+        self.checkpoint_store_name = checkpoint_store_name
         self.stores = {
             expectations_store_name: {
                 "class_name": "ExpectationsStore",
@@ -1314,6 +1327,14 @@ class GCSStoreBackendDefaults(BaseStoreBackendDefaults):
                 },
             },
             evaluation_parameter_store_name: {"class_name": "EvaluationParameterStore"},
+            checkpoint_store_name: {
+                "class_name": "CheckpointStore",
+                "store_backend": {
+                    "class_name": "TupleS3StoreBackend",
+                    "bucket": checkpoint_store_bucket_name,
+                    "prefix": checkpoint_store_prefix,
+                },
+            },
         }
         self.data_docs_sites = {
             "gcs_site": {
