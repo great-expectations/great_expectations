098b66f4974a37b6579bf486bb71ff03aa807987
Sam Bail <sam@superconductive.com>
2021-02-02 10:05:59 -0500

    [ENHANCEMENT] Adding library_metadata to another batch of expectations (#2362)
    * Adding library_metadata to another batch of expectations
    
    * Linter fixes

--- great_expectations/expectations/core/expect_column_values_to_be_dateutil_parseable.py
+++ great_expectations/expectations/core/expect_column_values_to_be_dateutil_parseable.py
@@ -65,6 +65,15 @@ class ExpectColumnValuesToBeDateutilParseable(ColumnMapExpectation):
 
     """
 
+    # This dictionary contains metadata for display in the public gallery
+    library_metadata = {
+        "maturity": "production",
+        "package": "great_expectations",
+        "tags": ["core expectation", "column map expectation"],
+        "contributors": ["@great_expectations"],
+        "requirements": [],
+    }
+
     map_metric = "column_values.dateutil_parsable"
     success_keys = ("mostly",)
 
