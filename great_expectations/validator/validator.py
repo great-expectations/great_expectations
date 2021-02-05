d53cb92601aed02d409e6712f584b7108c918239
James Campbell <james.p.campbell@gmail.com>
2021-02-05 12:59:40 -0500

    [FEATURE] Add get_metrics API to Validator
    Signed-off-by: James Campbell <james.p.campbell@gmail.com>

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -278,6 +278,47 @@ class Validator:
             expectation for expectation in keys if expectation.startswith("expect_")
         ]
 
+    def get_metrics(self, metrics: Dict[str, MetricConfiguration]) -> Dict[str, Any]:
+        """Return a dictionary with the requested metrics"""
+        graph = ValidationGraph()
+        resolved_metrics = {}
+        for metric_name, metric_configuration in metrics.items():
+            provider_cls, _ = get_metric_provider(
+                metric_configuration.metric_name, self.execution_engine
+            )
+            for key in provider_cls.domain_keys:
+                if (
+                    key not in metric_configuration.metric_domain_kwargs
+                    and key in provider_cls.default_kwarg_values
+                ):
+                    metric_configuration.metric_domain_kwargs[
+                        key
+                    ] = provider_cls.default_kwarg_values[key]
+            for key in provider_cls.value_keys:
+                if (
+                    key not in metric_configuration.metric_value_kwargs
+                    and key in provider_cls.default_kwarg_values
+                ):
+                    metric_configuration.metric_value_kwargs[
+                        key
+                    ] = provider_cls.default_kwarg_values[key]
+            self.build_metric_dependency_graph(
+                graph,
+                child_node=metric_configuration,
+                configuration=None,
+                execution_engine=self._execution_engine,
+                runtime_configuration=None,
+            )
+        self.resolve_validation_graph(graph, resolved_metrics)
+        return {
+            metric_name: resolved_metrics[metric_configuration.id]
+            for (metric_name, metric_configuration) in metrics.items()
+        }
+
+    def get_metric(self, metric: MetricConfiguration) -> Any:
+        """return the value of the requested metric."""
+        return self.get_metrics({"_": metric})["_"]
+
     def build_metric_dependency_graph(
         self,
         graph: ValidationGraph,
