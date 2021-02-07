a173c3d9238754a7308bdacb90a2e55b124924b7
sethdmay <seth@teammay.com>
2021-02-06 19:56:48 -0600

    Ran black and isort

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_alphabetical.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_alphabetical.py
@@ -1,15 +1,23 @@
 import json
-import pandas
 import operator
 from typing import Any, Dict, Optional, Tuple
+
+import pandas
 from pyspark.sql.functions import lag
+
+from great_expectations.core import ExpectationConfiguration
 
 #!!! This giant block of imports should be something simpler, such as:
 # from great_exepectations.helpers.expectation_creation import *
 from great_expectations.execution_engine import (
+    ExecutionEngine,
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
+)
+from great_expectations.execution_engine.execution_engine import (
+    MetricDomainTypes,
+    MetricPartialFunctionTypes,
 )
 from great_expectations.expectations.expectation import (
     ColumnMapExpectation,
@@ -20,6 +28,18 @@ from great_expectations.expectations.metrics import (
     ColumnMapMetricProvider,
     column_condition_partial,
 )
+from great_expectations.expectations.metrics.import_manager import F, Window, sparktypes
+from great_expectations.expectations.metrics.map_metric import (
+    ColumnMapMetricProvider,
+    column_condition_partial,
+)
+from great_expectations.expectations.metrics.metric_provider import (
+    metric_partial,
+    metric_value,
+)
+from great_expectations.expectations.metrics.table_metrics.table_column_types import (
+    ColumnTypes,
+)
 from great_expectations.expectations.registry import (
     _registered_expectations,
     _registered_metrics,
@@ -29,30 +49,8 @@ from great_expectations.expectations.util import render_evaluation_parameter_str
 from great_expectations.render.renderer.renderer import renderer
 from great_expectations.render.types import RenderedStringTemplateContent
 from great_expectations.render.util import num_to_str, substitute_none_for_missing
-from great_expectations.validator.validator import Validator
-
-from great_expectations.core import ExpectationConfiguration
-from great_expectations.execution_engine import (
-    ExecutionEngine,
-    PandasExecutionEngine,
-    SparkDFExecutionEngine,
-)
-from great_expectations.execution_engine.execution_engine import (
-    MetricDomainTypes,
-    MetricPartialFunctionTypes,
-)
-from great_expectations.expectations.metrics.import_manager import F, Window, sparktypes
-from great_expectations.expectations.metrics.map_metric import (
-    ColumnMapMetricProvider,
-    column_condition_partial,
-)
-from great_expectations.expectations.metrics.table_metrics.table_column_types import ColumnTypes
-from great_expectations.expectations.metrics.metric_provider import (
-    metric_partial,
-    metric_value,
-)
 from great_expectations.validator.validation_graph import MetricConfiguration
-
+from great_expectations.validator.validator import Validator
 
 
 # This class defines a Metric to support your Expectation
@@ -66,7 +64,6 @@ class ColumnValuesAreAlphabetical(ColumnMapMetricProvider):
     condition_value_keys = ("reverse",)
 
     # This method defines the business logic for evaluating your metric when using a PandasExecutionEngine
-
 
     @column_condition_partial(engine=PandasExecutionEngine)
     def _pandas(cls, column, reverse=False, **kwargs):
@@ -84,18 +81,16 @@ class ColumnValuesAreAlphabetical(ColumnMapMetricProvider):
         else:
             compare_function = operator.le
 
-
-        output = [True] # first value is automatically in order
-        for i in range(1,column_length):
-            if column_lower[i] and column_lower[i - 1]: # make sure we aren't comparing Nones
+        output = [True]  # first value is automatically in order
+        for i in range(1, column_length):
+            if (
+                column_lower[i] and column_lower[i - 1]
+            ):  # make sure we aren't comparing Nones
                 output.append(compare_function(column_lower[i - 1], column_lower[i]))
             else:
                 output.append(None)
 
         return pandas.Series(output)
-
-
-
 
 
 # This method defines the business logic for evaluating your metric when using a SqlAlchemyExecutionEngine
@@ -121,80 +116,79 @@ class ColumnValuesAreAlphabetical(ColumnMapMetricProvider):
 ######
 
 
-    # @metric_partial(
-    #     engine=SparkDFExecutionEngine,
-    #     partial_fn_type=MetricPartialFunctionTypes.WINDOW_CONDITION_FN,
-    #     domain_type=MetricDomainTypes.COLUMN,
-    # )
-    # def _spark(
-    #     cls,
-    #     execution_engine: SparkDFExecutionEngine,
-    #     metric_domain_kwargs: Dict,
-    #     metric_value_kwargs: Dict,
-    #     metrics: Dict[Tuple, Any],
-    #     runtime_configuration: Dict,
-    # ):
-    #     # check if column is any type that could have na (numeric types)
-    #     column_name = metric_domain_kwargs["column"]
-    #     # table_columns = metrics["table.column_types"]
-    #     # column_metadata = [col for col in table_columns if col["name"] == column_name][
-    #     #     0
-    #     # ]
-    #     # if isinstance(
-    #     #     column_metadata["type"],
-    #     #     (
-    #     #         sparktypes.LongType,
-    #     #         sparktypes.DoubleType,
-    #     #         sparktypes.IntegerType,
-    #     #     ),
-    #     # ):
-    #     #     # if column is any type that could have NA values, remove them (not filtered by .isNotNull())
-    #     #     compute_domain_kwargs = execution_engine.add_column_row_condition(
-    #     #         metric_domain_kwargs,
-    #     #         filter_null=cls.filter_column_isnull,
-    #     #         filter_nan=True,
-    #     #     )
-    #     # else:
-    #     compute_domain_kwargs = metric_domain_kwargs
-    #     (
-    #         df,
-    #         compute_domain_kwargs,
-    #         accessor_domain_kwargs,
-    #     ) = execution_engine.get_compute_domain(
-    #         compute_domain_kwargs, MetricDomainTypes.COLUMN
-    #     )
-    #
-    #     # # NOTE: 20201105 - parse_strings_as_datetimes is not supported here;
-    #     # # instead detect types naturally
-    #     column = F.col(column_name)
-    #     column = F.lower(column)
-    #     # if isinstance(
-    #     #     column_metadata["type"], (sparktypes.TimestampType, sparktypes.DateType)
-    #     # ):
-    #     #     diff = F.datediff(
-    #     #         column, F.lag(column).over(Window.orderBy(F.lit("constant")))
-    #     #     )
-    #     # else:
-    #     diff = F.lag(column, default="a").over(Window.orderBy(F.lit("constant")))
-    #     diff = F.when(column > diff, True).otherwise(False)
-    #
-    #     # NOTE: because in spark we are implementing the window function directly,
-    #     # we have to return the *unexpected* condition
-    #     # if metric_value_kwargs["strictly"]:
-    #     #     return (
-    #     #         F.when(diff >= 0, F.lit(True)).otherwise(F.lit(False)),
-    #     #         compute_domain_kwargs,
-    #     #         accessor_domain_kwargs,
-    #     #     )
-    #     # # If we expect values to be flat or decreasing then unexpected values are those
-    #     # # that are decreasing
-    #     # else:
-    #     return (
-    #         diff,
-    #         compute_domain_kwargs,
-    #         accessor_domain_kwargs,
-    #     )
-
+# @metric_partial(
+#     engine=SparkDFExecutionEngine,
+#     partial_fn_type=MetricPartialFunctionTypes.WINDOW_CONDITION_FN,
+#     domain_type=MetricDomainTypes.COLUMN,
+# )
+# def _spark(
+#     cls,
+#     execution_engine: SparkDFExecutionEngine,
+#     metric_domain_kwargs: Dict,
+#     metric_value_kwargs: Dict,
+#     metrics: Dict[Tuple, Any],
+#     runtime_configuration: Dict,
+# ):
+#     # check if column is any type that could have na (numeric types)
+#     column_name = metric_domain_kwargs["column"]
+#     # table_columns = metrics["table.column_types"]
+#     # column_metadata = [col for col in table_columns if col["name"] == column_name][
+#     #     0
+#     # ]
+#     # if isinstance(
+#     #     column_metadata["type"],
+#     #     (
+#     #         sparktypes.LongType,
+#     #         sparktypes.DoubleType,
+#     #         sparktypes.IntegerType,
+#     #     ),
+#     # ):
+#     #     # if column is any type that could have NA values, remove them (not filtered by .isNotNull())
+#     #     compute_domain_kwargs = execution_engine.add_column_row_condition(
+#     #         metric_domain_kwargs,
+#     #         filter_null=cls.filter_column_isnull,
+#     #         filter_nan=True,
+#     #     )
+#     # else:
+#     compute_domain_kwargs = metric_domain_kwargs
+#     (
+#         df,
+#         compute_domain_kwargs,
+#         accessor_domain_kwargs,
+#     ) = execution_engine.get_compute_domain(
+#         compute_domain_kwargs, MetricDomainTypes.COLUMN
+#     )
+#
+#     # # NOTE: 20201105 - parse_strings_as_datetimes is not supported here;
+#     # # instead detect types naturally
+#     column = F.col(column_name)
+#     column = F.lower(column)
+#     # if isinstance(
+#     #     column_metadata["type"], (sparktypes.TimestampType, sparktypes.DateType)
+#     # ):
+#     #     diff = F.datediff(
+#     #         column, F.lag(column).over(Window.orderBy(F.lit("constant")))
+#     #     )
+#     # else:
+#     diff = F.lag(column, default="a").over(Window.orderBy(F.lit("constant")))
+#     diff = F.when(column > diff, True).otherwise(False)
+#
+#     # NOTE: because in spark we are implementing the window function directly,
+#     # we have to return the *unexpected* condition
+#     # if metric_value_kwargs["strictly"]:
+#     #     return (
+#     #         F.when(diff >= 0, F.lit(True)).otherwise(F.lit(False)),
+#     #         compute_domain_kwargs,
+#     #         accessor_domain_kwargs,
+#     #     )
+#     # # If we expect values to be flat or decreasing then unexpected values are those
+#     # # that are decreasing
+#     # else:
+#     return (
+#         diff,
+#         compute_domain_kwargs,
+#         accessor_domain_kwargs,
+#     )
 
 
 # This class defines the Expectation itself
@@ -210,98 +204,150 @@ class ExpectColumnValuesToBeAlphabetical(ColumnMapExpectation):
     """
 
     # These examples will be shown in the public gallery, and also executed as unit tests for your Expectation
-    examples = [{
-        "data": {
-            "is_alphabetical_lowercase": ["apple", "banana", "coconut", "donut", "eggplant", "flour", "grapes", "jellybean", None, None, None],
-            "is_alphabetical_lowercase_reversed": ["moon", "monster", "messy", "mellow", "marble", "maple", "malted", "machine", None, None, None],
-            "is_alphabetical_mixedcase": ["Atlanta", "bonnet", "Delaware", "gymnasium", "igloo", "Montreal", "Tennessee", "toast", "Washington", "xylophone", "zebra"],
-            "out_of_order": ["Right", "wrong", "up", "down", "Opposite", "Same", "west", "east", None, None, None]
-        },
-        "tests": [
-            {
-                "title": "positive_test_with_all_values_alphabetical_lowercase",
-                "exact_match_out": False,
-                "include_in_gallery": True,
-                "in": {
-                    "column": "is_alphabetical_lowercase",
-                    "reverse": False,
-                    "mostly": 1.0
-                },
-                "out": {
-                    "success": True,
-                    "unexpected_index_list": [],
-                    "unexpected_list": [],
-                },
+    examples = [
+        {
+            "data": {
+                "is_alphabetical_lowercase": [
+                    "apple",
+                    "banana",
+                    "coconut",
+                    "donut",
+                    "eggplant",
+                    "flour",
+                    "grapes",
+                    "jellybean",
+                    None,
+                    None,
+                    None,
+                ],
+                "is_alphabetical_lowercase_reversed": [
+                    "moon",
+                    "monster",
+                    "messy",
+                    "mellow",
+                    "marble",
+                    "maple",
+                    "malted",
+                    "machine",
+                    None,
+                    None,
+                    None,
+                ],
+                "is_alphabetical_mixedcase": [
+                    "Atlanta",
+                    "bonnet",
+                    "Delaware",
+                    "gymnasium",
+                    "igloo",
+                    "Montreal",
+                    "Tennessee",
+                    "toast",
+                    "Washington",
+                    "xylophone",
+                    "zebra",
+                ],
+                "out_of_order": [
+                    "Right",
+                    "wrong",
+                    "up",
+                    "down",
+                    "Opposite",
+                    "Same",
+                    "west",
+                    "east",
+                    None,
+                    None,
+                    None,
+                ],
             },
-            {
-                "title": "negative_test_with_all_values_alphabetical_lowercase_reversed",
-                "exact_match_out": False,
-                "include_in_gallery": True,
-                "in": {
-                    "column": "is_alphabetical_lowercase_reversed",
-                    "reverse": False,
-                    "mostly": 1.0
+            "tests": [
+                {
+                    "title": "positive_test_with_all_values_alphabetical_lowercase",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "is_alphabetical_lowercase",
+                        "reverse": False,
+                        "mostly": 1.0,
+                    },
+                    "out": {
+                        "success": True,
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
                 },
-                "out": {
-                    "success": False,
-                    "unexpected_index_list": [1,2,3,4,5,6,7],
-                    "unexpected_list": ["monster", "messy", "mellow", "marble", "maple", "malted", "machine"],
+                {
+                    "title": "negative_test_with_all_values_alphabetical_lowercase_reversed",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "is_alphabetical_lowercase_reversed",
+                        "reverse": False,
+                        "mostly": 1.0,
+                    },
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [1, 2, 3, 4, 5, 6, 7],
+                        "unexpected_list": [
+                            "monster",
+                            "messy",
+                            "mellow",
+                            "marble",
+                            "maple",
+                            "malted",
+                            "machine",
+                        ],
+                    },
                 },
-            },
-            {
-                "title": "positive_test_with_all_values_alphabetical_lowercase_reversed",
-                "exact_match_out": False,
-                "include_in_gallery": True,
-                "in": {
-                    "column": "is_alphabetical_lowercase_reversed",
-                    "reverse": True,
-                    "mostly": 1.0
+                {
+                    "title": "positive_test_with_all_values_alphabetical_lowercase_reversed",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "is_alphabetical_lowercase_reversed",
+                        "reverse": True,
+                        "mostly": 1.0,
+                    },
+                    "out": {
+                        "success": True,
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
                 },
-                "out": {
-                    "success": True,
-                    "unexpected_index_list": [],
-                    "unexpected_list": [],
+                {
+                    "title": "positive_test_with_all_values_alphabetical_mixedcase",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "is_alphabetical_mixedcase", "mostly": 1.0},
+                    "out": {
+                        "success": True,
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
                 },
-            },
-            {
-                "title": "positive_test_with_all_values_alphabetical_mixedcase",
-                "exact_match_out": False,
-                "include_in_gallery": True,
-                "in": {
-                    "column": "is_alphabetical_mixedcase",
-                    "mostly": 1.0
+                {
+                    "title": "negative_test_with_out_of_order_mixedcase",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "out_of_order", "mostly": 1.0},
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [2, 3, 7],
+                        "unexpected_list": ["up", "down", "east"],
+                    },
                 },
-                "out": {
-                    "success": True,
-                    "unexpected_index_list": [],
-                    "unexpected_list": [],
-                },
-            },
-            {
-                "title": "negative_test_with_out_of_order_mixedcase",
-                "exact_match_out": False,
-                "include_in_gallery": True,
-                "in": {
-                    "column": "out_of_order",
-                    "mostly": 1.0
-                },
-                "out": {
-                    "success": False,
-                    "unexpected_index_list": [2, 3, 7],
-                    "unexpected_list": ["up", "down", "east"],
-                },
-            }
-        ],
-    }]
+            ],
+        }
+    ]
 
     # This dictionary contains metadata for display in the public gallery
     library_metadata = {
         "maturity": "experimental",  # "experimental", "beta", or "production"
-        "tags": [  # Tags for this Expectation in the gallery
-            "experimental"
-        ],
+        "tags": ["experimental"],  # Tags for this Expectation in the gallery
         "contributors": [  # Github handles for all contributors to this Expectation.
-            "@sethdmay", "@maximetokman", "@Harriee02" # Don't forget to add your github handle here!
+            "@sethdmay",
+            "@maximetokman",
+            "@Harriee02",  # Don't forget to add your github handle here!
         ],
         "package": "experimental_expectations",
     }
@@ -312,7 +358,7 @@ class ExpectColumnValuesToBeAlphabetical(ColumnMapExpectation):
 
     # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
     # Please see {some doc} for more information about domain and success keys, and other arguments to Expectations
-    success_keys = ("mostly","reverse")
+    success_keys = ("mostly", "reverse")
 
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {}
