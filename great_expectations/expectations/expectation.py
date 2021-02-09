70f298bd13f618dbb4d886f205f02b6a23865ba8
Victor Yee <w.victoryee@gmail.com>
2021-02-08 21:28:11 -0800

    Add timestamp tests for ExpectColumnValuesToBeBetween.

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -1081,6 +1081,9 @@ class TableExpectation(Expectation, ABC):
 
         try:
             # Ensuring Proper interval has been provided
+            assert (
+                min_val is not None or max_val is not None
+            ), "min_value and max_value cannot both be None"
             assert min_val is None or isinstance(
                 min_val, (float, int, dict)
             ), "Provided min threshold must be a number"
