cd98373b38a7275467b0f211cc01a6e7113f3ead
Tal Gluck <tal@superconductive.com>
2021-02-18 14:02:58 -0500

    Removed extra expectation file, and made some modifications to the expect_column_values_to_follow_rule

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_follow_rule.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_follow_rule.py
@@ -30,10 +30,10 @@ from great_expectations.validator.validator import Validator
 
 class ColumnRuleFollowers(ColumnMapMetricProvider):
     """
-    Checks to see if all rows  satisfy a given minimum length and rule.
+    Checks to see if all rows  satisfy a given rule.
     A rule is defined as a JSON object (interpreted as a Python dict) with the following fields:
 
-        -ranges (dict): a nonempty dictionary that contains nested lists.
+        - ranges (dict): a nonempty dictionary that contains nested lists.
             +Expected Values: Nested lists must be length 2, holding ints in increasing order, or length 0.
             +Special Cases: [] will pass in the entire string contained in the row.
             Some examples. Suppose the row is the string "1234".
@@ -42,41 +42,47 @@ class ColumnRuleFollowers(ColumnMapMetricProvider):
             {"x1": [0, 1],   - The variable x1 in the expression is set to "1", and x2 is set to "4".
                 "x2": [3, 4]}
 
-        -expr (string): The expression. This is a string in the form of a Python expression. Python functions are
+        - expr (string): The expression. This is a string in the form of a Python expression. Python functions are
             available, as it is parsed using exec(). Each variable is passed in as a string.
             +Expected Value: A well-formed python expression that evaluates to a boolean value.
             +Special Cases: "True" and "False" will evaluate to True and False, respectively.
     """
 
-    condition_metric_name = "column_values.expect_column_to_follow_rule"
-    condition_value_keys = ("min_len", "rule")
+    condition_metric_name = "column_values.expect_column_values_to_follow_rule"
+    condition_value_keys = ("rule",)
 
     @column_condition_partial(engine=PandasExecutionEngine)
-    def _pandas(cls, column, min_len, rule, **kwargs):
+    def _pandas(cls, column, rule, **kwargs):
 
         if rule["ranges"] is {}:
             raise ValueError("Ranges must contain at least 1 variable!")
 
-        return column.apply(lambda x: ColumnRuleFollowers._helper(x, rule, min_len))
+        return column.apply(lambda x: ColumnRuleFollowers._helper(x, rule))
 
     @staticmethod
-    def _helper(x, rule, min_len):
+    def _helper(x, rule):
         """Helper function since Python doesn't like multiline functions"""
         strings = {}
         ldict = {}
         names = ""
         if x is None:
             x = ""
+        if not isinstance(x, str):
+            raise TypeError(
+                "Column values must be strings in order to use 'expect_column_values_to_follow_rule'"
+            )
         for name, rnge in rule["ranges"].items():
             if rnge[0] < rnge[1]:
                 strings[name] = str(x[rnge[0] : rnge[1]])
                 names += name + ","
             else:
-                raise ValueError("Unexpected range!")
+                raise ValueError(
+                    "Unexpected range. Ensure that the second number in your range is larger than the first."
+                )
 
         exec("expr = lambda " + names + ":" + rule["expr"], None, ldict)
         func = ldict["expr"]
-        return len(x) >= min_len and func(**strings)
+        return func(**strings)
 
 
 # This method defines the business logic for evaluating your metric when using a SqlAlchemyExecutionEngine
@@ -92,24 +98,23 @@ class ColumnRuleFollowers(ColumnMapMetricProvider):
 #         return column.isin([3])
 
 
-class expect_column_to_follow_rule(ColumnMapExpectation):
+class expect_column_values_to_follow_rule(ColumnMapExpectation):
     """This expectation compares all rows of a column against a given input expression."""
 
     # These examples will be shown in the public gallery, and also executed as unit tests for your Expectation
     examples = [
         {
             "data": {
-                "in": ["12345", "1abdfadfasf", "1234", "555555", "124214"],
-                "min_len_in": ["1", "0111", "", "2a", "-345"],
+                "a_column": ["12345", "1abdfadfasf", "1234", "555555", "124214"],
+                "or_column": ["1200", "1234", "9812", "1212", "9912"],
             },
             "tests": [
                 {
-                    "title": "general_test",
+                    "title": "positive_mostly_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
                     "in": {
-                        "column": "in",
-                        "min_len": 5,
+                        "column": "a_column",
                         "rule": {
                             "ranges": {"x": [0, 2], "y": [2, 5]},
                             "expr": "x == '12' and int(y) > 100",
@@ -123,19 +128,37 @@ class expect_column_to_follow_rule(ColumnMapExpectation):
                     },
                 },
                 {
-                    "title": "min_len test",
+                    "title": "positive_or_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
                     "in": {
-                        "column": "min_len_in",
-                        "min_len": 3,
-                        "rule": {"ranges": {"x": [0, 2]}, "expr": "True"},
-                        "mostly": 0.4,
+                        "column": "or_column",
+                        "rule": {
+                            "ranges": {"x": [0, 2], "y": [2, 4]},
+                            "expr": "x == '12' or y == '12'",
+                        },
                     },
                     "out": {
                         "success": True,
-                        "unexpected_index_list": [0, 2, 3],
-                        "unexpected_list": ["1", "", "2a"],
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
+                },
+                {
+                    "title": "negative_test",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "or_column",
+                        "rule": {
+                            "ranges": {"x": [0, 2], "y": [2, 4]},
+                            "expr": "x == '12'",
+                        },
+                    },
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [2, 4],
+                        "unexpected_list": ["9812", "9912"],
                     },
                 },
             ],
@@ -145,15 +168,19 @@ class expect_column_to_follow_rule(ColumnMapExpectation):
     # This dictionary contains metadata for display in the public gallery
     library_metadata = {
         "maturity": "experimental",
-        "tags": ["experimental"],  # Tags for this Expectation in the gallery
+        "tags": [
+            "experimental",
+            "hackathon-20210206",
+        ],  # Tags for this Expectation in the gallery
         "contributors": [  # Github handles for all contributors to this Expectation.
             "@firenoo",
-            "@rldejournett",  #         "@your_name_here", # Don't forget to add your github handle here!
+            "@rldejournett",
+            "@talagluck",
         ],
         "package": "experimental_expectations",
     }
 
-    map_metric = "column_values.expect_column_to_follow_rule"
+    map_metric = "column_values.expect_column_values_to_follow_rule"
 
     success_keys = (
         "min_len",
@@ -253,5 +280,5 @@ class expect_column_to_follow_rule(ColumnMapExpectation):
 #         ]
 
 if __name__ == "__main__":
-    diagnostics_report = expect_column_to_follow_rule().run_diagnostics()
+    diagnostics_report = expect_column_values_to_follow_rule().run_diagnostics()
     print(json.dumps(diagnostics_report, indent=2))
