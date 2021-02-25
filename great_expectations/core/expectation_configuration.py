c6828710e93cc46437a81f0029cf217a71284bc8
Robert Moses Lim <robert.m.lim@gmail.com>
2021-02-25 11:03:40 -0800

    Add FileDataAsset expectations

--- great_expectations/core/expectation_configuration.py
+++ great_expectations/core/expectation_configuration.py
@@ -772,6 +772,42 @@ class ExpectationConfiguration(SerializableDictDot):
                 "catch_exceptions": False,
             },
         },
+        ####
+        "expect_file_line_regex_match_count_to_be_between": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_line_regex_match_count_to_equal": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_hash_to_equal": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_size_to_be_between": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_to_exist": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_to_have_valid_table_header": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
+        "expect_file_to_be_valid_json": {
+            "domain_kwargs": [],
+            "success_kwargs": [],
+            "default_kwarg_values": {}
+        },
     }
 
     runtime_kwargs = ["result_format", "include_config", "catch_exceptions"]
