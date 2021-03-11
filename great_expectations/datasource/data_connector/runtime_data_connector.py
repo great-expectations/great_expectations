983b1d607e7958a701c619be2873febb230497b5
William Shin <will@superconductive.com>
2021-03-11 15:59:23 -0800

    Update great_expectations/datasource/data_connector/runtime_data_connector.py
    Co-authored-by: talagluck <tal@superconductive.com>

--- great_expectations/datasource/data_connector/runtime_data_connector.py
+++ great_expectations/datasource/data_connector/runtime_data_connector.py
@@ -132,10 +132,11 @@ class RuntimeDataConnector(DataConnector):
         """
         self._validate_batch_request(batch_request=batch_request)
 
-        self._validate_batch_identifiers(
-            batch_identifiers=batch_request.partition_request.get("batch_identifiers")
-        )
         batch_identifiers = batch_request.partition_request.get("batch_identifiers")
+        
+        self._validate_batch_identifiers(
+            batch_identifiers=batch_identifiers
+        )
 
         batch_definition_list: List[BatchDefinition]
         batch_definition: BatchDefinition = BatchDefinition(
