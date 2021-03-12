f37673cb6e49f22d0f4351fb9e075c27578e8c69
William Shin <will@superconductive.com>
2021-03-11 16:01:42 -0800

    Update tests/datasource/data_connector/test_data_connector.py
    Co-authored-by: talagluck <tal@superconductive.com>

--- tests/datasource/data_connector/test_data_connector.py
+++ tests/datasource/data_connector/test_data_connector.py
@@ -113,7 +113,7 @@ def test__file_object_caching_for_FileDataConnector(tmp_path_factory):
     )
 
     assert my_data_connector.get_data_reference_list_count() == 0
-    assert my_data_connector.get_unmatched_data_references() == []
+    assert len(my_data_connector.get_unmatched_data_references()) == 0
 
     # noinspection PyProtectedMember
     my_data_connector._refresh_data_references_cache()
