32706b73b104372390fddf227a3b3bd78c581044
Robert Moses Lim <robert.m.lim@gmail.com>
2021-03-14 15:50:48 -0700

    Update tests

--- tests/datasource/data_connector/test_runtime_data_connector.py
+++ tests/datasource/data_connector/test_runtime_data_connector.py
@@ -80,15 +80,18 @@ def test_error_checking(basic_datasource):
         )
 
     # test for missing runtime_parameters arg
-    with pytest.raises(TypeError):
+    with pytest.raises(ge_exceptions.DataConnectorError):
         # noinspection PyUnusedLocal
         batch_definition_list: List[
             BatchDefinition
         ] = test_runtime_data_connector.get_batch_definition_list_from_batch_request(
             batch_request=RuntimeBatchRequest(
                 datasource_name=basic_datasource.name,
-                data_connector_name="non_existent_data_connector",
+                data_connector_name="test_runtime_data_connector",
                 data_asset_name="my_data_asset",
+                batch_identifiers={
+                    "pipeline_stage_name": "munge"
+                }
             )
         )
 
@@ -100,9 +103,12 @@ def test_error_checking(basic_datasource):
         ] = test_runtime_data_connector.get_batch_definition_list_from_batch_request(
             batch_request=RuntimeBatchRequest(
                 datasource_name=basic_datasource.name,
-                data_connector_name="non_existent_data_connector",
+                data_connector_name="test_runtime_data_connector",
                 data_asset_name="my_data_asset",
                 runtime_parameters={"batch_data": test_df, "path": "my_path"},
+                batch_identifiers={
+                    "pipeline_stage_name": "munge"
+                }
             )
         )
 
@@ -128,11 +134,11 @@ def test_batch_identifiers_and_runtime_keys_success_all_keys_present(
     batch_request: dict = {
         "datasource_name": basic_datasource.name,
         "data_connector_name": test_runtime_data_connector.name,
-        "data_asset_name": "my_data_asset_name",
+        "data_asset_name": "IN_MEMORY_DATA_ASSET",
         "runtime_parameters": {"batch_data": test_df},
         "batch_identifiers": batch_identifiers,
     }
-    batch_request: BatchRequest = RuntimeBatchRequest(**batch_request)
+    batch_request: RuntimeBatchRequest = RuntimeBatchRequest(**batch_request)
 
     batch_definition_list: List[
         BatchDefinition
@@ -238,17 +244,17 @@ def test_get_batch_definition_list_from_batch_request_length_one(
     batch_request: dict = {
         "datasource_name": basic_datasource.name,
         "data_connector_name": test_runtime_data_connector.name,
-        "data_asset_name": "my_data_asset_name",
+        "data_asset_name": "IN_MEMORY_DATA_ASSET",
         "runtime_parameters": {"batch_data": test_df},
         "batch_identifiers": batch_identifiers,
     }
-    batch_request: BatchRequest = RuntimeBatchRequest(**batch_request)
+    batch_request: RuntimeBatchRequest = RuntimeBatchRequest(**batch_request)
 
     expected_batch_definition_list: List[BatchDefinition] = [
         BatchDefinition(
             datasource_name="my_datasource",
             data_connector_name="test_runtime_data_connector",
-            data_asset_name="my_data_asset_name",
+            data_asset_name="IN_MEMORY_DATA_ASSET",
             partition_definition=PartitionDefinition(batch_identifiers),
         )
     ]
@@ -260,6 +266,39 @@ def test_get_batch_definition_list_from_batch_request_length_one(
     )
 
     assert batch_definition_list == expected_batch_definition_list
+
+
+def test_get_batch_definition_list_from_batch_request_length_zero(
+    basic_datasource,
+):
+    test_df: pd.DataFrame = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+
+    batch_identifiers = {
+        "airflow_run_id": 1234567890,
+    }
+
+    test_runtime_data_connector: RuntimeDataConnector = (
+        basic_datasource.data_connectors["test_runtime_data_connector"]
+    )
+
+    batch_request: dict = {
+        "datasource_name": basic_datasource.name,
+        "data_connector_name": test_runtime_data_connector.name,
+        "data_asset_name": "nonexistent_data_asset",
+        "runtime_parameters": {
+            "batch_data": test_df,
+        },
+        "batch_identifiers": batch_identifiers,
+    }
+    batch_request: RuntimeBatchRequest = RuntimeBatchRequest(**batch_request)
+
+    batch_definition_list: List[
+        BatchDefinition
+    ] = test_runtime_data_connector.get_batch_definition_list_from_batch_request(
+        batch_request=batch_request
+    )
+
+    assert len(batch_definition_list) == 0
 
 
 def test__get_data_reference_list(basic_datasource):
