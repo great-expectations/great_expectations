5dfae4d4b4082671ba9acd74d9ae641aa748d2de
Cedric-Magnan <43343135+Cedric-Magnan@users.noreply.github.com>
2021-03-18 08:09:02 +0100

    Update great_expectations/data_context/util.py
    Co-authored-by: William Shin <will@superconductive.com>

--- great_expectations/data_context/util.py
+++ great_expectations/data_context/util.py
@@ -335,9 +335,10 @@ def substitute_value_from_aws_secrets_manager(value):
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region_name)
 
-    secret_response = client.get_secret_value(
-        SecretId=secret_name, VersionId=secret_version
-    )
+    if secret_version:
+        secret_response = client.get_secret_value(SecretId=secret_name, VersionId=secret_version)
+    else:
+        secret_response = client.get_secret_value(SecretId=secret_name)
     # Decrypts secret using the associated KMS CMK.
     # Depending on whether the secret is a string or binary, one of these fields will be populated.
     if "SecretString" in secret_response:
