bcee90d34abdf2b0a1917029cf4b547999d69e0b
Alex Sherstinsky <alex@superconductivehealth.com>
2021-03-19 23:15:34 -0700

    Troubleshooting.

--- tests/datasource/test_new_datasource.py
+++ tests/datasource/test_new_datasource.py
@@ -197,7 +197,7 @@ def test_basic_pandas_datasource_v013_self_check(basic_pandas_datasource_v013):
 
 def test_basic_spark_datasource_self_check(basic_spark_datasource):
     report = basic_spark_datasource.self_check()
-    print(f'\n[ALEX_TEST] SELF_CHECK_REPORT: {report} ; TYPE: {str(type(report))}')
+    print(f"\n[ALEX_TEST] SELF_CHECK_REPORT: {report} ; TYPE: {str(type(report))}")
     report["execution_engine"]["spark_config"].pop("spark.app.id")
     report["execution_engine"]["spark_config"].pop("spark.executor.id")
     report["execution_engine"]["spark_config"].pop("spark.driver.host")
