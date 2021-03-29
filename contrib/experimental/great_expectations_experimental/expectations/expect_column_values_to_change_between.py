3c6491fcc1962e15d5fa1e59e3bbbc1d3a30505e
Maikel Penz <maikelpenz@gmail.com>
2021-03-29 19:28:39 +1300

    feat: create expectation expect column values to change between

---
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_change_between.py
@@ -0,0 +1,259 @@
+import json
+
+import pandas as pd
+
+#!!! This giant block of imports should be something simpler, such as:
+# from great_exepectations.helpers.expectation_creation import *
+from great_expectations.execution_engine import (
+    PandasExecutionEngine,
+    SparkDFExecutionEngine,
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.expectations.expectation import (
+    ColumnMapExpectation,
+    Expectation,
+    ExpectationConfiguration,
+)
+from great_expectations.expectations.metrics import (
+    ColumnMapMetricProvider,
+    column_condition_partial,
+)
+from great_expectations.expectations.registry import (
+    _registered_expectations,
+    _registered_metrics,
+    _registered_renderers,
+)
+
+
+# This class defines a Metric to support your Expectation
+# For most Expectations, the main business logic for calculation will live here.
+# To learn about the relationship between Metrics and Expectations, please visit
+# https://docs.greatexpectations.io/en/latest/reference/core_concepts.html#expectations-and-metrics.
+class ColumnValuesToChangeBetween(ColumnMapMetricProvider):
+
+    # This is the id string that will be used to reference your metric.
+    # Please see https://docs.greatexpectations.io/en/latest/reference/core_concepts/metrics.html#metrics
+    # for information on how to choose an id string for your Metric.
+    condition_metric_name = "column_values.change_between"
+    condition_value_keys = (
+        "from_value",
+        "to_value",
+    )
+
+    # This method defines the business logic for evaluating your metric when using a PandasExecutionEngine
+
+    @column_condition_partial(engine=PandasExecutionEngine)
+    def _pandas(cls, column, from_value, to_value, **kwargs):
+
+        # throw an error if one of the values is not numeric
+        if not pd.to_numeric(column, errors="coerce").notnull().all():
+            raise TypeError("Column values must be numeric !")
+
+        # calculate the difference of the current row with the previous.
+        # If previous is NaN fills with the initial value "from_value" to consider it true
+        difference = (column - column.shift()).fillna(from_value)
+
+        def is_change_rate_compliant(value: int):
+            return True if from_value <= abs(value) <= to_value else False
+
+        return difference.map(lambda x: is_change_rate_compliant(x))
+
+
+# This method defines the business logic for evaluating your metric when using a SqlAlchemyExecutionEngine
+#     @column_condition_partial(engine=SqlAlchemyExecutionEngine)
+#     def _sqlalchemy(cls, column, _dialect, **kwargs):
+#         return column.in_([3])
+
+# This method defines the business logic for evaluating your metric when using a SparkDFExecutionEngine
+#     @column_condition_partial(engine=SparkDFExecutionEngine)
+#     def _spark(cls, column, **kwargs):
+#         return column.isin([3])
+
+
+# This class defines the Expectation itself
+# The main business logic for calculation lives here.
+class ExpectColumnValuesToChangeBetween(ColumnMapExpectation):
+    """
+    Given a list of numeric values,
+    check if the difference between the current and the previous row
+    is within the expected difference range.
+
+    E.g:
+    input = [1,2,5]
+    expected difference range = between 1 and 2
+    Result: `false` because the difference between 2 and 5 is not between 1 and 2
+
+    parameters:
+        from_value: low range value
+        to_value: high range value
+    """
+
+    # These examples will be shown in the public gallery, and also executed as unit tests for your Expectation
+    examples = [
+        {
+            "data": {
+                "numbers_difference_max_3": [1, 3, 5, 7.5, 10, 12, 15],
+                "numbers_difference_max_5": [-3, -1, 2, 5, 13, 15, 21],
+            },
+            "tests": [
+                {
+                    "title": "positive_test_with_difference_between_1_and_3",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "numbers_difference_max_3",
+                        "from_value": 1,
+                        "to_value": 3,
+                    },
+                    "out": {
+                        "success": True,
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
+                },
+                {
+                    "title": "positive_test_with_difference_between_1_and_8",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "numbers_difference_max_5",
+                        "from_value": 1,
+                        "to_value": 8,
+                    },
+                    "out": {
+                        "success": True,
+                        "unexpected_index_list": [],
+                        "unexpected_list": [],
+                    },
+                },
+                {
+                    "title": "negative_test_with_difference_between_1_and_5",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "numbers_difference_max_5",
+                        "from_value": 1,
+                        "to_value": 5,
+                    },
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [4, 6],
+                        "unexpected_list": [13, 21],
+                    },
+                },
+            ],
+        }
+    ]
+
+    # This dictionary contains metadata for display in the public gallery
+    library_metadata = {
+        "maturity": "experimental",  # "experimental", "beta", or "production"
+        "tags": ["experimental"],  # Tags for this Expectation in the gallery
+        "contributors": ["@maikelpenz"],  # Don't forget to add your github handle here!
+        "package": "experimental_expectations",
+    }
+
+    # This is the id string of the Metric used by this Expectation.
+    # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
+    map_metric = "column_values.change_between"
+
+    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
+    # Please see https://docs.greatexpectations.io/en/latest/reference/core_concepts/expectations/expectations.html#expectation-concepts-domain-and-success-keys
+    # for more information about domain and success keys, and other arguments to Expectations
+    success_keys = ("from_value", "to_value")
+
+    # This dictionary contains default values for any parameters that should have default values
+    default_kwarg_values = {}
+
+    # This method defines a question Renderer
+    # For more info on Renderers, see
+    # https://docs.greatexpectations.io/en/latest/guides/how_to_guides/configuring_data_docs/how_to_create_renderers_for_custom_expectations.html
+    #!!! This example renderer should render RenderedStringTemplateContent, not just a string
+
+
+#     @classmethod
+#     @renderer(renderer_type="renderer.question")
+#     def _question_renderer(
+#         cls, configuration, result=None, language=None, runtime_configuration=None
+#     ):
+#         column = configuration.kwargs.get("column")
+#         mostly = configuration.kwargs.get("mostly")
+
+#         return f'Do at least {mostly * 100}% of values in column "{column}" equal 3?'
+
+# This method defines an answer Renderer
+#!!! This example renderer should render RenderedStringTemplateContent, not just a string
+#     @classmethod
+#     @renderer(renderer_type="renderer.answer")
+#     def _answer_renderer(
+#         cls, configuration=None, result=None, language=None, runtime_configuration=None
+#     ):
+#         column = result.expectation_config.kwargs.get("column")
+#         mostly = result.expectation_config.kwargs.get("mostly")
+#         regex = result.expectation_config.kwargs.get("regex")
+#         if result.success:
+#             return f'At least {mostly * 100}% of values in column "{column}" equal 3.'
+#         else:
+#             return f'Less than {mostly * 100}% of values in column "{column}" equal 3.'
+
+# This method defines a prescriptive Renderer
+#     @classmethod
+#     @renderer(renderer_type="renderer.prescriptive")
+#     @render_evaluation_parameter_string
+#     def _prescriptive_renderer(
+#         cls,
+#         configuration=None,
+#         result=None,
+#         language=None,
+#         runtime_configuration=None,
+#         **kwargs,
+#     ):
+#!!! This example renderer should be shorter
+#         runtime_configuration = runtime_configuration or {}
+#         include_column_name = runtime_configuration.get("include_column_name", True)
+#         include_column_name = (
+#             include_column_name if include_column_name is not None else True
+#         )
+#         styling = runtime_configuration.get("styling")
+#         params = substitute_none_for_missing(
+#             configuration.kwargs,
+#             ["column", "regex", "mostly", "row_condition", "condition_parser"],
+#         )
+
+#         template_str = "values must be equal to 3"
+#         if params["mostly"] is not None:
+#             params["mostly_pct"] = num_to_str(
+#                 params["mostly"] * 100, precision=15, no_scientific=True
+#             )
+#             # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+#             template_str += ", at least $mostly_pct % of the time."
+#         else:
+#             template_str += "."
+
+#         if include_column_name:
+#             template_str = "$column " + template_str
+
+#         if params["row_condition"] is not None:
+#             (
+#                 conditional_template_str,
+#                 conditional_params,
+#             ) = parse_row_condition_string_pandas_engine(params["row_condition"])
+#             template_str = conditional_template_str + ", then " + template_str
+#             params.update(conditional_params)
+
+#         return [
+#             RenderedStringTemplateContent(
+#                 **{
+#                     "content_block_type": "string_template",
+#                     "string_template": {
+#                         "template": template_str,
+#                         "params": params,
+#                         "styling": styling,
+#                     },
+#                 }
+#             )
+#         ]
+
+if __name__ == "__main__":
+    diagnostics_report = ExpectColumnValuesToChangeBetween().run_diagnostics()
+    print(json.dumps(diagnostics_report, indent=2))
