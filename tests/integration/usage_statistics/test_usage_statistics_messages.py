83127af48f58c5165e2bc0bbb3c9e6c24beeca2a
Anthony Burdi <anthony@superconductive.com>
2021-04-08 16:33:47 -0400

    Datasource delete usage stats messages and success:False for checkpoint & datasource commands

--- tests/integration/usage_statistics/test_usage_statistics_messages.py
+++ tests/integration/usage_statistics/test_usage_statistics_messages.py
@@ -507,6 +507,12 @@ valid_usage_statistics_messages = {
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
+            {
+                "event": "cli.checkpoint.new.end",
+                "success": False,
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
         ],
     ),
     "cli.checkpoint.script": generate_messages_with_defaults(
@@ -540,6 +546,12 @@ valid_usage_statistics_messages = {
             },
             {
                 "event": "cli.checkpoint.script.end",
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.checkpoint.script.end",
+                "success": False,
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
@@ -579,6 +591,12 @@ valid_usage_statistics_messages = {
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
+            {
+                "event": "cli.checkpoint.run.end",
+                "success": False,
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
         ],
     ),
     "cli.checkpoint.list": generate_messages_with_defaults(
@@ -612,6 +630,12 @@ valid_usage_statistics_messages = {
             },
             {
                 "event": "cli.checkpoint.list.end",
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.checkpoint.list.end",
+                "success": False,
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
@@ -649,6 +673,17 @@ valid_usage_statistics_messages = {
             {
                 "event": "cli.checkpoint.delete.end",
                 "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.checkpoint.delete.end",
+                "success": False,
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.checkpoint.delete.end",
+                "event_payload": {"api_version": "v3", "cancelled": True},
                 "ge_version": "0.13.18.manual_testing",
             },
         ],
@@ -694,11 +729,6 @@ valid_usage_statistics_messages = {
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
-            {
-                "event": "cli.datasource.list.end",
-                "event_payload": {"api_version": "v3", "cancelled": True},
-                "ge_version": "0.13.18.manual_testing",
-            },
         ],
     ),
     "cli.datasource.new": generate_messages_with_defaults(
@@ -741,45 +771,55 @@ valid_usage_statistics_messages = {
                 "event_payload": {"api_version": "v3"},
                 "ge_version": "0.13.18.manual_testing",
             },
+        ],
+    ),
+    "cli.datasource.delete": generate_messages_with_defaults(
+        defaults={
+            "success": True,
+            "version": "1.0.0",
+            "event_time": "2020-08-04T22:50:58.837Z",
+            "data_context_id": "00000000-0000-0000-0000-000000000002",
+            "data_context_instance_id": "10000000-0000-0000-0000-000000000002",
+        },
+        message_stubs=[
             {
-                "event": "cli.datasource.new.end",
+                "event": "cli.datasource.delete",
+                "event_payload": {},
+                "ge_version": "0.11.9.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete",
+                "event_payload": {"api_version": "v2"},
+                "ge_version": "0.13.0.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete",
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.0.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete.begin",
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete.end",
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete.end",
+                "success": False,
+                "event_payload": {"api_version": "v3"},
+                "ge_version": "0.13.18.manual_testing",
+            },
+            {
+                "event": "cli.datasource.delete.end",
                 "event_payload": {"api_version": "v3", "cancelled": True},
                 "ge_version": "0.13.18.manual_testing",
             },
         ],
     ),
-    "cli.datasource.new": [
-        {
-            "event": "cli.datasource.new",
-            "event_payload": {},
-            "success": True,
-            "version": "1.0.0",
-            "event_time": "2020-08-04T00:38:32.701Z",
-            "data_context_id": "00000000-0000-0000-0000-000000000002",
-            "data_context_instance_id": "10000000-0000-0000-0000-000000000002",
-            "ge_version": "0.11.9.manual_testing",
-        },
-        {
-            "event": "cli.datasource.new",
-            "event_payload": {"api_version": "v2"},
-            "success": True,
-            "version": "1.0.0",
-            "event_time": "2020-08-04T00:38:32.701Z",
-            "data_context_id": "00000000-0000-0000-0000-000000000002",
-            "data_context_instance_id": "10000000-0000-0000-0000-000000000002",
-            "ge_version": "0.13.0.manual_testing",
-        },
-        {
-            "event": "cli.datasource.new",
-            "event_payload": {"api_version": "v3"},
-            "success": True,
-            "version": "1.0.0",
-            "event_time": "2020-08-04T00:38:32.701Z",
-            "data_context_id": "00000000-0000-0000-0000-000000000002",
-            "data_context_instance_id": "10000000-0000-0000-0000-000000000002",
-            "ge_version": "0.13.0.manual_testing",
-        },
-    ],
     "cli.datasource.profile": [
         {
             "event": "cli.datasource.profile",
