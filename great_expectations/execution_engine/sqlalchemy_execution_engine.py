8cf7401e3daa610e8d4397ba61efce0095cb5d7f
Shinnnyshinshin <will@superconductive.com>
2021-04-13 15:42:18 -0700

    fixed mssql test that was failing

--- great_expectations/execution_engine/sqlalchemy_execution_engine.py
+++ great_expectations/execution_engine/sqlalchemy_execution_engine.py
@@ -798,30 +798,6 @@ class SqlAlchemyExecutionEngine(ExecutionEngine):
                     .where(split_clause)
                     .limit(batch_spec["sampling_kwargs"]["n"])
                 )
-            elif batch_spec["sampling_method"] == "_sample_using_a_list":
-                # <WILL> This check is necessary because of a bug in sqlalchemy that prevents the compilation
-                # of an IN statement using literal_binds. Issue is being tracked by Sqlalchemy https://github.com/sqlalchemy/sqlalchemy/issues/6222
-                # Check if we can remove this entire elif statement in 202105
-                if parse_version(sa.__version__) >= parse_version("1.4.0"):
-                    raise GreatExpectationsError(
-                        f"""
-                        This functionality in Great Expectations version {__version__} is currently incompatible with SqlAlchemy 1.4.0 and higher.
-                        You currently have SqlAlchemy version {sa.__version__}. Please downgrade SqlAlchemy to < 1.4.0 while we work on a proper fix.
-                        """
-                    )
-                sampler_fn = getattr(self, batch_spec["sampling_method"])
-                return (
-                    sa.select("*")
-                    .select_from(
-                        sa.table(table_name, schema=batch_spec.get("schema_name", None))
-                    )
-                    .where(
-                        sa.and_(
-                            split_clause,
-                            sampler_fn(**batch_spec["sampling_kwargs"]),
-                        )
-                    )
-                )
             else:
 
                 sampler_fn = getattr(self, batch_spec["sampling_method"])
