7cb628cadd8789f74d1eb160702129a13ce08c16
Anthony Burdi <anthony@superconductive.com>
2021-04-16 19:43:21 -0400

    WIP Multibatch fixture generator and test

--- tests/profiler/test_profiler.py
+++ tests/profiler/test_profiler.py
@@ -1,46 +1,154 @@
 import datetime
 import os
+from typing import List, Optional
 
 import pandas as pd
 import pytest
+from ruamel.yaml import YAML
 
-from great_expectations.profiler.profiler import Profiler
+from great_expectations import DataContext
+
+# from great_expectations.profiler.profiler import Profiler
+
+yaml = YAML()
 
 
-# TODO: AJB 20210416 Move this fixture, generalize, add to quagga
-@pytest.fixture()
-def multibatch_csv_generator(tmp_path):
+# TODO: AJB 20210416 Move this fixture, generalize, add to quagga, add more column types
+@pytest.fixture(scope="function")
+def multibatch_generic_csv_generator():
+    """
+    Construct a series of csv files with many data types for use in multibatch testing
+    """
 
-    data_path = tmp_path / "data"
-    data_path.mkdir()
+    def _multibatch_generic_csv_generator(
+        data_path: str,
+        start_date: Optional[datetime.datetime] = None,
+        num_event_batches: Optional[int] = 20,
+        num_events_per_batch: Optional[int] = 5,
+    ) -> List[str]:
 
-    start_date = datetime.datetime(2000, 1, 1)
+        if start_date is None:
+            start_date = datetime.datetime(2000, 1, 1)
 
-    num_event_batches = 20
-    num_events_per_batch = 5
+        file_list = []
+        for batch_num in range(num_event_batches):
+            # generate a dataframe with multiple column types
+            batch_start_date = start_date + datetime.timedelta(
+                days=(batch_num * num_events_per_batch)
+            )
+            # TODO: AJB 20210416 Add more column types
+            df = pd.DataFrame(
+                {
+                    "event_date": [
+                        (batch_start_date + datetime.timedelta(days=i)).strftime(
+                            "%Y-%m-%d"
+                        )
+                        for i in range(num_events_per_batch)
+                    ],
+                    "batch_num": [batch_num + 1 for _ in range(num_events_per_batch)],
+                }
+            )
+            filename = f"csv_batch_{batch_num + 1:03}_of_{num_event_batches:03}.csv"
+            file_list.append(filename)
+            df.to_csv(
+                os.path.join(data_path, filename),
+                index_label="intra_batch_index",
+            )
 
-    for batch_num in range(num_event_batches):
-        # generate a dataframe with multiple column types
-        batch_start_date = start_date + datetime.timedelta(
-            days=(batch_num * num_events_per_batch)
+        return file_list
+
+    return _multibatch_generic_csv_generator
+
+
+def test_batches_are_accessible(
+    monkeypatch, multibatch_generic_csv_generator, empty_data_context
+):
+    """
+    What does this test and why?
+    Batches created in the multibatch_generic_csv_generator fixture should be available using the
+    multibatch_generic_csv_generator_context
+    This test most likely duplicates tests elsewhere, but it is more of a test of the configurable fixture.
+    """
+
+    context: DataContext = empty_data_context
+    monkeypatch.chdir(context.root_directory)
+    data_relative_path = "../data"
+    data_path = os.path.join(context.root_directory, data_relative_path)
+    os.makedirs(data_path, exist_ok=True)
+    file_list = multibatch_generic_csv_generator(data_path=data_path)
+
+    data_connector_base_directory = "./"
+    monkeypatch.setenv("base_directory", data_connector_base_directory)
+    monkeypatch.setenv("data_fixtures_root", data_relative_path)
+
+    datasource_name = "generic_csv_generator"
+    data_connector_name = "daily_data_connector"
+    asset_name = "daily_data_asset"
+    datasource_config = fr"""
+class_name: Datasource
+module_name: great_expectations.datasource
+execution_engine:
+  module_name: great_expectations.execution_engine
+  class_name: PandasExecutionEngine
+data_connectors:
+  {data_connector_name}:
+    class_name: ConfiguredAssetFilesystemDataConnector
+    assets:
+      {asset_name}:
+        module_name: great_expectations.datasource.data_connector.asset
+        group_names:
+          - batch
+          - total_batches
+        pattern: csv_batch_(\d.+)_of_(\d.+)\.csv
+        reader_options:
+          delimiter: ","
+        class_name: Asset
+        base_directory: $data_fixtures_root
+        glob_directive: "*.csv"
+    base_directory: $base_directory
+    module_name: great_expectations.datasource.data_connector
+    """
+
+    context.add_datasource(name=datasource_name, **yaml.load(datasource_config))
+
+    assert context.list_datasources() == [
+        {
+            "class_name": "Datasource",
+            "data_connectors": {
+                data_connector_name: {
+                    "assets": {
+                        asset_name: {
+                            "base_directory": data_relative_path,
+                            "class_name": "Asset",
+                            "glob_directive": "*.csv",
+                            "group_names": ["batch", "total_batches"],
+                            "module_name": "great_expectations.datasource.data_connector.asset",
+                            "pattern": "csv_batch_(\\d.+)_of_(\\d.+)\\.csv",
+                        }
+                    },
+                    "base_directory": data_connector_base_directory,
+                    "class_name": "ConfiguredAssetFilesystemDataConnector",
+                    "module_name": "great_expectations.datasource.data_connector",
+                }
+            },
+            "execution_engine": {
+                "class_name": "PandasExecutionEngine",
+                "module_name": "great_expectations.execution_engine",
+            },
+            "module_name": "great_expectations.datasource",
+            "name": "generic_csv_generator",
+        }
+    ]
+
+    datasource = context.datasources[datasource_name]
+
+    data_connector = datasource.data_connectors[data_connector_name]
+    assert (
+        data_connector._get_data_reference_list_from_cache_by_data_asset_name(
+            data_asset_name=asset_name
         )
-        df = pd.DataFrame(
-            {
-                "event_date": [
-                    (batch_start_date + datetime.timedelta(days=i)).strftime("%Y-%m-%d")
-                    for i in range(num_events_per_batch)
-                ],
-                "batch_num": [batch_num + 1 for _ in range(num_events_per_batch)],
-            }
-        )
-        df.to_csv(
-            os.path.join(
-                data_path, f"csv_batch_{batch_num + 1}_of_{num_event_batches}.csv"
-            ),
-            index_label="intra_batch_index",
-        )
-
-    return data_path
+        == file_list
+    )
 
 
 @pytest.fixture(scope="module")
@@ -92,13 +200,13 @@ rules:
 
 
 def test_profiler_init_manual(
-    taxicab_context,
+    multibatch_generic_csv_generator_context,
     simple_multibatch_profiler_configuration_yaml,
-    multibatch_csv_generator,
+    multibatch_generic_csv_generator,
 ):
     profiler = Profiler(
         rule_configs=simple_multibatch_profiler_configuration_yaml,
-        data_context=taxicab_context,
+        data_context=multibatch_generic_csv_generator_context,
     )
     assert False
 
