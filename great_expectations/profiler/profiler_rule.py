a00e23281af7370c8d32684046b94f66d163dd64
gilpasternak35 <gilpasternak35@gmail.com>
2021-04-19 11:27:55 -0700

    Profiling Documentation additions complete

--- great_expectations/profiler/profiler_rule.py
+++ great_expectations/profiler/profiler_rule.py
@@ -1,15 +1,32 @@
+from typing import Union
+
+from .configuration_builder.configuration_builder import ConfigurationBuilder
+from .domain_builder.domain_builder import DomainBuilder
+from .parameter_builder.parameter_builder import ParameterBuilder
 from .rule_state import RuleState
+from ..core import ExpectationConfiguration
 
 
 class ProfilerRule:
     def __init__(
         self,
-        name,
-        domain_builder,
-        parameter_builders,
-        configuration_builders,
+        name: str,
+        domain_builder: DomainBuilder,
+        parameter_builders: [ParameterBuilder],
+        configuration_builders: [ConfigurationBuilder],
         variables=None,
     ):
+        """
+        Sets Profiler rule name, domain builders, parameters builders, configuration builders,
+        and other necessary instance data (variables)
+        :param name: A string representing the name of the ProfilerRule
+        :param domain_builder: A Domain Builder object used to build rule data domain
+        :param parameter_builders: A Parameter Builder list used to configure necessary rule evaluation parameters for
+        every configuration
+        :param configuration_builders: A list of Configuration Builders initializing state configurations (utilizes the info in
+        a RuleState object)
+        :param variables: Any instance data required to verify a rule
+        """
         self._name = name
         self._domain_builder = domain_builder
         self._parameter_builders = parameter_builders
@@ -18,7 +35,17 @@ class ProfilerRule:
             variables = dict()
         self._variables = variables
 
-    def evaluate(self, validator, batch_ids=None):
+    def evaluate(self, validator, batch_ids=None) -> [ExpectationConfiguration]:
+        """
+        Builds a RuleState object for configured information (domain_builder, parameter_builder,
+        and configuration_builder which are defined at __init__) and proceeds to use this
+        RuleState to build a list of Expectation Configurations, returning a single Expectation
+        Configuration entry for every ConfigurationBuilder given).
+
+        :param validator: A Validator object utilized to obtain domain
+        :param batch_ids: Batch Identifiers used to specify evaluated batches of data
+        :return: List of Corresponding Expectation Configurations representing every configured rule
+        """
         rule_state = RuleState(variables=self._variables)
         configurations = []
 
