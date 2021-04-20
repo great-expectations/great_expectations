e964732530de12ab68e38f009ed25deb47293733
Alex Sherstinsky <alex@superconductivehealth.com>
2021-04-20 08:16:35 -0700

    comment

--- great_expectations/profiler/domain_builder/simple_semantic_type_domain_builder.py
+++ great_expectations/profiler/domain_builder/simple_semantic_type_domain_builder.py
@@ -35,6 +35,7 @@ class SimpleSemanticTypeColumnDomainBuilder(ColumnDomainBuilder):
             ...
         ]
         """
+        # TODO: <Alex>ALEX -- How does this work?  What information is provided in **kwargs?  Can this be made explicit?  Where is "_type_filters" defined?</Alex>
         config = kwargs
         # TODO: AJB 20210416 If the type keyword for the DomainBuilder can contain multiple semantic types, should it be renamed types and take a list instead? Not that we can’t guess from what a user adds but something to make it clear that multiple semantic types can be used to construct a domain?
         # TODO: <Alex>ALEX -- In general, to avoid confusion, we should avoid the use of "type" because it is a function in Python.</Alex>
@@ -48,6 +49,7 @@ class SimpleSemanticTypeColumnDomainBuilder(ColumnDomainBuilder):
         elif isinstance(type_filters, Iterable):
             type_filters = [self.SemanticDomainTypes[x] for x in type_filters]
         else:
+            # TODO: <Alex>ALEX -- We should make this error message more informative.</Alex>
             raise ValueError("unrecognized")
         columns = validator.get_metric(MetricConfiguration("table.columns", dict()))
         domains = []
@@ -62,7 +64,7 @@ class SimpleSemanticTypeColumnDomainBuilder(ColumnDomainBuilder):
                 )
         return domains
 
-    # TODe: <Alex>ALEX -- This method seems to always return the same value ("integer")...</Alex>
+    # TODO: <Alex>ALEX -- This method seems to always return the same value ("integer")...</Alex>
     def _get_column_semantic_type(self, validator, column):
         # FIXME: DO CHECKS
         return self.SemanticDomainTypes["INTEGER"]
