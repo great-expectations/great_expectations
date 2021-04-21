35458bac5e5b34cdf460d3011a9d13b0902385d3
Shinnnyshinshin <will@superconductive.com>
2021-04-20 17:27:58 -0700

    moving try except block to correct place

--- assets/scripts/build_gallery.py
+++ assets/scripts/build_gallery.py
@@ -144,14 +144,8 @@ def build_gallery(
             impl = great_expectations.expectations.registry.get_expectation_impl(
                 expectation
             )
-            try:
-                diagnostics = impl().run_diagnostics()
-                gallery_info[expectation] = diagnostics
-            except Exception as e:
-                logger.error(
-                    f"Was not able to run diagnostics for Expectation: {expectation}"
-                )
-                gallery_info[expectation] = {"error": str(e)}
+            diagnostics = impl().run_diagnostics()
+            gallery_info[expectation] = diagnostics
             built_expectations.add(expectation)
     else:
         built_expectations = set(core_expectations)
@@ -206,8 +200,15 @@ def build_gallery(
                 impl = great_expectations.expectations.registry.get_expectation_impl(
                     expectation
                 )
-                diagnostics = impl().run_diagnostics()
-                gallery_info[expectation] = diagnostics
+
+                try:
+                    diagnostics = impl().run_diagnostics()
+                    gallery_info[expectation] = diagnostics
+                except Exception as e:
+                    logger.error(
+                        f"Was not able to run diagnostics for Expectation: {expectation}"
+                    )
+                    gallery_info[expectation] = {"error": str(e)}
                 built_expectations.add(expectation)
             logger.info(f"Unloading just-installed for module {expectation_module}")
             for req in just_installed:
