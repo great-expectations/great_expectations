4422bade4cd25ed88a2b4f7ab8f181c3eab2733e
gilpasternak35 <gilpasternak35@gmail.com>
2021-04-21 13:37:52 -0700

    sync additions

--- tests/integration/Profiling/fixtures/multibatch_taxi_data.py
+++ tests/integration/Profiling/fixtures/multibatch_taxi_data.py
@@ -5,6 +5,8 @@ from great_expectations.data_context.data_context import DataContext
 context = DataContext()
 suite = context.get_expectation_suite("yellow_trip_data_validations")
 
+### Workflow 1
+
 batch_request_march = BatchRequest(
   datasource_name="taxi_pandas",
   data_connector_name="monthly",
@@ -29,3 +31,40 @@ batch_request_february = BatchRequest(
 validator_february = context.get_validator(batch_request=batch_request_february, expectation_suite=suite)
 
 print(validator_february.expect_table_row_count_to_equal(value=march_table_row_count))
+
+
+### Workflow 2
+
+multi_batch_request = BatchRequest(
+  datasource_name="taxi_pandas",
+  data_connector_name="monthly",
+  data_asset_name="my_reports",
+  data_connector_query={
+    "index": "[-2:]"
+  }
+)
+
+validator = context.get_validator(batch_request=multi_batch_request, expectation_suite=suite)
+validator.expect_table_row_count_to_equal(
+  value=validator.get_metric(MetricConfiguration("table.row_count", metric_domain_kwargs={batch_id=...}))
+)
+
+
+### Workflow 3
+
+validator = context.get_validator(batch_request=batch_request_february, expectation_suite=suite)
+batch_march = context.get_batch(batch_request=batch_request_march)
+validator.load_batch(batch_march)
+validator.expect_table_row_count_to_equal(
+  value=validator.get_metric(MetricConfiguration("table.row_count", metric_domain_kwargs={batch_id=...}))
+)
+
+### Workflow 4
+
+batch_february = context.get_batch(batch_request=batch_request_february)
+batch_march = context.get_batch(batch_request=batch_request_march)
+validator = context.get_validator(batches=[batch_february, batch_march], expectation_suite=suite)
+
+validator.expect_table_row_count_to_equal(
+  value=validator.get_metric(MetricConfiguration("table.row_count", metric_domain_kwargs={batch_id=...}))
+)
