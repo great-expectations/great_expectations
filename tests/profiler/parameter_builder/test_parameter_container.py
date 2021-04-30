d4351da252697a9089d4f85900d1fc4e23ab6573
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2021-04-30 09:47:16 -0700

    [ENHANCEMENT] Support Domain Parameters That Start With Multiple First (Root) Part (#2736)
    * Update ParameterContainer to handle multiple starting (root) parameter names.
    * Elaborate in-code documentation (docstrings).

---
+++ tests/profiler/parameter_builder/test_parameter_container.py
@@ -0,0 +1,14 @@
+from great_expectations.profiler.parameter_builder.parameter_container import (
+    ParameterContainer,
+    build_parameter_container,
+)
+
+
+def test_build_parameter_container(
+    parameter_values_nine_parameters_multiple_depths,
+    multi_part_name_parameter_container,
+):
+    parameter_container: ParameterContainer = build_parameter_container(
+        parameter_values=parameter_values_nine_parameters_multiple_depths
+    )
+    assert parameter_container == multi_part_name_parameter_container
