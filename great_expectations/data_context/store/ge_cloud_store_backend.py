f8f6b27899800ae28c551324e32afd60e6ef4a77
Robert Moses Lim <robert.m.lim@gmail.com>
2021-05-03 01:43:58 -0700

    Add ge_cloud_resource_type attribute; update _set

--- great_expectations/data_context/store/ge_cloud_store_backend.py
+++ great_expectations/data_context/store/ge_cloud_store_backend.py
@@ -10,7 +10,6 @@ from urllib.parse import urljoin
 import requests
 
 from great_expectations.data_context.store.store_backend import StoreBackend
-from great_expectations.data_context.types.resource_identifiers import ConfigurationIdentifier
 from great_expectations.exceptions import InvalidKeyError, StoreBackendError
 from great_expectations.util import filter_properties_dict
 
@@ -22,6 +21,7 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
         self,
         ge_cloud_base_url,
         ge_cloud_resource_name,
+        ge_cloud_resource_type,
         suppress_store_backend_id=False,
         manually_initialize_store_backend_id: str = "",
         store_name=None,
@@ -34,6 +34,7 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
         )
         self._ge_cloud_base_url = ge_cloud_base_url
         self._ge_cloud_resource_name = ge_cloud_resource_name
+        self._ge_cloud_resource_type = ge_cloud_resource_type
 
         # Initialize with store_backend_id if not part of an HTMLSiteStore
         if not self._suppress_store_backend_id:
@@ -44,6 +45,7 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
         self._config = {
             "ge_cloud_base_url": ge_cloud_base_url,
             "ge_cloud_resource_name": ge_cloud_resource_name,
+            "ge_cloud_resource_type": ge_cloud_resource_type,
             "fixed_length_key": True,
             "suppress_store_backend_id": suppress_store_backend_id,
             "manually_initialize_store_backend_id": manually_initialize_store_backend_id,
@@ -58,7 +60,6 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
         headers = {
             "Content-Type": "application/vnd.api+json"
         }
-        print(ge_cloud_url)
         response = requests.get(ge_cloud_url, headers=headers)
         return response.json()
 
@@ -66,16 +67,11 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
         pass
 
     def _set(self, key, value, **kwargs):
-        new_ge_cloud_id = str(uuid.uuid4())
-        key: ConfigurationIdentifier = ConfigurationIdentifier(
-            configuration_key=new_ge_cloud_id,
-        )
 
         data = {
             "data": {
-                'type': 'checkpoint',
+                'type': self.ge_cloud_resource_type,
                 "attributes": {
-                    "created_at": '2021-04-27 02:41:36.885264',
                     "created_by_id": "df665fc4-1891-4ef7-9a12-a0c46015c92c",
                     "checkpoint_config": value.to_json_dict()
                 },
@@ -85,31 +81,13 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
             "Content-Type": "application/vnd.api+json"
         }
         url = urljoin(self.ge_cloud_base_url, self.ge_cloud_resource_name)
-        print(url)
-        response = requests.post(url, json=data, headers=headers)
-        return response.json()
-        # checkpoint_config = CheckpointConfig(**new_checkpoint.config.to_json_dict())
-        # self.checkpoint_store.set(key=key, value=checkpoint_config)
-        #
-        # s3_object_key = self._build_s3_object_key(key)
-        #
-        # s3 = self._create_resource()
-        #
-        # try:
-        #     result_s3 = s3.Object(self.bucket, s3_object_key)
-        #     if isinstance(value, str):
-        #         result_s3.put(
-        #             Body=value.encode(content_encoding),
-        #             ContentEncoding=content_encoding,
-        #             ContentType=content_type,
-        #         )
-        #     else:
-        #         result_s3.put(Body=value, ContentType=content_type)
-        # except s3.meta.client.exceptions.ClientError as e:
-        #     logger.debug(str(e))
-        #     raise StoreBackendError("Unable to set object in s3.")
-        #
-        # return s3_object_key
+
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            return response.json()
+        except Exception as e:
+            logger.debug(str(e))
+            raise StoreBackendError("Unable to set object in GE Cloud Store Backend.")
 
     @property
     def ge_cloud_base_url(self):
@@ -119,16 +97,16 @@ class GeCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
     def ge_cloud_resource_name(self):
         return self._ge_cloud_resource_name
 
+    @property
+    def ge_cloud_resource_type(self):
+        return self._ge_cloud_resource_type
+
     def list_keys(self):
         pass
 
     def get_url_for_key(self, key, protocol=None):
-        print("=====")
-        print(key[0])
-        print("======")
         ge_cloud_id = key[0]
         url = urljoin(self.ge_cloud_base_url, f"{self.ge_cloud_resource_name}/{ge_cloud_id}")
-        print(url)
         return url
 
     def remove_key(self, key):
