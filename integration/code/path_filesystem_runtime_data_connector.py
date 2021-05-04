e64bd7e96b5e7f52e94eec3390d95943cb6380f7
Shinnnyshinshin <will@superconductive.com>
2021-05-04 10:25:18 -0700

    lint

--- integration/code/path_filesystem_runtime_data_connector.py
+++ integration/code/path_filesystem_runtime_data_connector.py
@@ -1,9 +1,8 @@
 import great_expectations as ge
 from great_expectations.cli.datasource import sanitize_yaml_and_save_datasource
 
-
-#ge.DataContext.create(project_root_dir="../fixtures/runtime_data_taxi_monthly", )
-#ge.DataContext.set_ge_config_version(3, "../fixtures/runtime_data_taxi_monthly")
+# ge.DataContext.create(project_root_dir="../fixtures/runtime_data_taxi_monthly", )
+# ge.DataContext.set_ge_config_version(3, "../fixtures/runtime_data_taxi_monthly")
 
 context = ge.get_context()
 
@@ -27,19 +26,16 @@ sanitize_yaml_and_save_datasource(context, config, overwrite_existing=True)
 batch_request = ge.core.batch.RuntimeBatchRequest(
     datasource_name="taxi_datasource_with_runtime_data_connector",
     data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="default_name", # this can be anything that identifies this data_asset for you
+    data_asset_name="default_name",  # this can be anything that identifies this data_asset for you
     runtime_parameters={
         "path": "./test_data/reports/yellow_tripdata_sample_2019-01.csv"
-
     },
-    batch_identifiers={
-        "default_identifier_name": "something_something"
-    }
+    batch_identifiers={"default_identifier_name": "something_something"},
 )
 
 # getting Batch
 
 batch = context.get_batch(batch_request=batch_request)
 
-# this may not be the right test at the end. 
+# this may not be the right test at the end.
 print(batch.__str__())
