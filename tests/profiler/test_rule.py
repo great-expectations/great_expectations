ca95e6cf95c94a427a2f13944ec0f4ac9b90c351
Alex Sherstinsky <alex@superconductivehealth.com>
2021-05-05 12:41:10 -0700

    Typo

--- tests/profiler/test_rule.py
+++ tests/profiler/test_rule.py
@@ -3,11 +3,10 @@ from typing import Any
 import pytest
 
 import great_expectations.exceptions as ge_exceptions
-
-# noinspection PyPep8Naming
 from great_expectations.profiler.util import get_parameter_value
 
 
+# noinspection PyPep8Naming
 def test_get_parameter_value_invalid_parameter_name(
     rule_with_variables_with_parameters, column_Age_structured_type_domain
 ):
