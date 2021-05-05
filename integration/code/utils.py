d18e69bfada1d44a4f06048a536d503722828d3d
Taylor Miller <Aylr@users.noreply.github.com>
2021-05-05 14:33:30 -0600

    docs tests now respect pytest backend flags such as --no-postgresql etc (#2754)
    GE-86

--- integration/code/utils.py
+++ integration/code/utils.py
@@ -1,11 +1,10 @@
-from integration.code.query_postgres_runtime_data_connector import CONNECTION_STRING
-
-
-def load_data_into_database(table_name: str, csv_path: str) -> None:
+def load_data_into_database(
+    table_name: str, csv_path: str, connection_string: str
+) -> None:
     import sqlalchemy as sa
     import pandas as pd
 
-    engine = sa.create_engine(CONNECTION_STRING)
+    engine = sa.create_engine(connection_string)
     engine.execute(f"DROP TABLE IF EXISTS {table_name}")
     print(f"Dropping table {table_name}")
     df = pd.read_csv(csv_path)
