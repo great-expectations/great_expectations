a5d9f6a13354306bbc6721ea47951c2ddd3301b7
William Shin <will@superconductive.com>
2021-05-05 10:31:10 -0700

    [KNOXPOD] `postgresql` and `RuntimeDataConnector` (#2751)
    * integration test with postgres and runtimedataconnector
    * taxi data loaded per script in an idempotent way
    * added a batch snippet to the doc
    * file loader util
    
    Co-authored-by: Taylor Miller <Aylr@users.noreply.github.com>

--- docs/guides/connecting_to_your_data/where_is_your_data/my-data-is-in-a-database.md
+++ docs/guides/connecting_to_your_data/where_is_your_data/my-data-is-in-a-database.md
@@ -2,4 +2,40 @@
 title: My data is in a database
 ---
 
-This article is a stub.
+Great! GE supports the following Databases:
+- [Postgres](#my-data-is-in-postgres)
+- Sqlite
+- Snowflake
+- MsSQL
+- MySql
+- Oracle
+
+
+## My Data is in postgres
+
+### First make sure you have the necessary pre-requisites
+
+You will need
+1. credentials
+2. required packages
+
+There are two ways to add credentials :
+- connection_string (link tbd)
+- credentials (link tbd)
+
+Make sure you have pre-reqs:
+```console
+pip install sqlalchemy psycopg2
+```
+
+### Here is your configuration
+
+Great here is an example of your configuration:
+
+```python file=../../../../integration/code/query_postgres_runtime_data_connector.py#L17-L31
+```
+
+Here is an example of loading a batch from a query.
+
+```python file=../../../../integration/code/query_postgres_runtime_data_connector.py#L36-L43
+```
---
+++ integration/code/query_postgres_runtime_data_connector.py
@@ -0,0 +1,48 @@
+import great_expectations as ge
+from great_expectations.cli.datasource import sanitize_yaml_and_save_datasource
+from great_expectations.core.batch import Batch
+from integration.code.utils import load_data_into_database
+
+CONNECTION_STRING = """postgresql+psycopg2://postgres:@localhost/test_ci"""
+load_data_into_database(
+    "taxi_data", "../fixtures/test_data/reports/yellow_tripdata_sample_2019-01.csv"
+)
+
+# context = ge.get_context()
+context = ge.DataContext(
+    context_root_dir="../fixtures/runtime_data_taxi_monthly/great_expectations"
+)
+
+# with a connection_string
+config = """
+name: my_postgres_datasource
+class_name: Datasource
+execution_engine:
+  class_name: SqlAlchemyExecutionEngine
+  connection_string: <YOUR_CONNECTION_STRING_HERE>
+data_connectors:
+   default_runtime_data_connector_name:
+       class_name: RuntimeDataConnector
+       batch_identifiers:
+           - default_identifier_name
+   default_inferred_data_connector_name:
+       class_name: InferredAssetSqlDataConnector
+       name: whole_table
+"""
+config = config.replace("<YOUR_CONNECTION_STRING_HERE>", CONNECTION_STRING)
+
+sanitize_yaml_and_save_datasource(context, config, overwrite_existing=True)
+
+batch_request = ge.core.batch.RuntimeBatchRequest(
+    datasource_name="my_postgres_datasource",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="default_name",  # this can be anything that identifies this data_asset for you
+    runtime_parameters={"query": "SELECT * from taxi_data LIMIT 10"},
+    batch_identifiers={"default_identifier_name": "something_something"},
+)
+batch = context.get_batch(batch_request=batch_request)
+assert isinstance(batch, Batch)
+batch_data = batch.data
+assert isinstance(
+    batch_data, ge.execution_engine.sqlalchemy_batch_data.SqlAlchemyBatchData
+)
