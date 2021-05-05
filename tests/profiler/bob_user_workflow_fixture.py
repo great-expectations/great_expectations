1ce78f63ebfb52dc4508790b4addbbd83db3d4dd
Anthony Burdi <anthony@superconductive.com>
2021-05-05 17:30:14 -0400

    Remove TODO about Rule being user configurable

--- tests/profiler/bob_user_workflow_fixture.py
+++ tests/profiler/bob_user_workflow_fixture.py
@@ -6,9 +6,6 @@ import pytest
 @pytest.fixture
 def bob_columnar_table_multi_batch():
 
-    # TODO: this config specifies a class_name and module_name for the Rule class which is currently not configurable
-    #  please decide whether Rule class is configurable before considering this a finalized configuration.
-
     with open("bob_user_workflow_verbose_profiler_config.yml") as f:
         verbose_profiler_config = f.read()
 
