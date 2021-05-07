4f76f5759f57b10a5d99c9c61a343f61a6deef94
Anthony Burdi <anthony@superconductive.com>
2021-05-07 15:14:47 -0400

    Linting

--- tests/profiler/user_workflow_fixtures.py
+++ tests/profiler/user_workflow_fixtures.py
@@ -46,7 +46,9 @@ def alice_columnar_table_single_batch():
         ExpectationConfiguration(
             **{
                 "expectation_type": "expect_column_values_to_not_be_null",
-                "kwargs": {"column": "user_id",},
+                "kwargs": {
+                    "column": "user_id",
+                },
                 "meta": {},
             }
         ),
@@ -67,9 +69,18 @@ def alice_columnar_table_single_batch():
         ExpectationConfiguration
     ] = []
     my_rule_for_timestamps_column_data: List[Dict[str, str]] = [
-        {"column_name": "event_ts", "observed_max_time_str": "2004-10-19 11:05:20",},
-        {"column_name": "server_ts", "observed_max_time_str": "2004-10-19 11:05:20",},
-        {"column_name": "device_ts", "observed_max_time_str": "2004-10-19 11:05:22",},
+        {
+            "column_name": "event_ts",
+            "observed_max_time_str": "2004-10-19 11:05:20",
+        },
+        {
+            "column_name": "server_ts",
+            "observed_max_time_str": "2004-10-19 11:05:20",
+        },
+        {
+            "column_name": "device_ts",
+            "observed_max_time_str": "2004-10-19 11:05:22",
+        },
     ]
     for column_data in my_rule_for_timestamps_column_data:
         my_rule_for_timestamps_expectation_configurations.extend(
@@ -87,14 +98,18 @@ def alice_columnar_table_single_batch():
                 ExpectationConfiguration(
                     **{
                         "expectation_type": "expect_column_values_to_be_increasing",
-                        "kwargs": {"column": column_data["column_name"],},
+                        "kwargs": {
+                            "column": column_data["column_name"],
+                        },
                         "meta": {},
                     }
                 ),
                 ExpectationConfiguration(
                     **{
                         "expectation_type": "expect_column_values_to_be_dateutil_parseable",
-                        "kwargs": {"column": column_data["column_name"],},
+                        "kwargs": {
+                            "column": column_data["column_name"],
+                        },
                         "meta": {},
                     }
                 ),
