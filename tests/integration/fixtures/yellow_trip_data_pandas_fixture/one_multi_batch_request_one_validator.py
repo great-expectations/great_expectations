549b2dea25e4ea9c889c93cfbb3b58f5ce3b83e1
Tal Gluck <tal@superconductive.com>
2021-05-11 12:35:01 -0400

    linting

--- tests/integration/fixtures/yellow_trip_data_pandas_fixture/one_multi_batch_request_one_validator.py
+++ tests/integration/fixtures/yellow_trip_data_pandas_fixture/one_multi_batch_request_one_validator.py
@@ -14,11 +14,7 @@ multi_batch_request = BatchRequest(
     datasource_name="taxi_pandas",
     data_connector_name="monthly",
     data_asset_name="my_reports",
-    data_connector_query={
-        "batch_filter_parameters": {
-            "year": "2019"
-        }
-    },
+    data_connector_query={"batch_filter_parameters": {"year": "2019"}},
 )
 
 # Instantiate the Validator
@@ -30,16 +26,23 @@ validator_multi_batch = context.get_validator(
 assert validator_multi_batch.active_batch_definition.batch_identifiers["month"] == "03"
 
 # Get the list of all batches contained by the Validator for use in the BatchFileter
-total_batch_definition_list: list = [v.batch_definition for k, v in validator_multi_batch.batches.items()]
+total_batch_definition_list: list = [
+    v.batch_definition for k, v in validator_multi_batch.batches.items()
+]
 
 # Filter to all batch_definitions prior to March
 jan_feb_batch_filter: BatchFilter = build_batch_filter(
     data_connector_query_dict={
-        "custom_filter_function": lambda batch_identifiers: int(batch_identifiers["month"]) < 3
+        "custom_filter_function": lambda batch_identifiers: int(
+            batch_identifiers["month"]
+        )
+        < 3
     }
 )
-jan_feb_batch_definition_list: list = jan_feb_batch_filter.select_from_data_connector_query(
-    batch_definition_list=total_batch_definition_list
+jan_feb_batch_definition_list: list = (
+    jan_feb_batch_filter.select_from_data_connector_query(
+        batch_definition_list=total_batch_definition_list
+    )
 )
 
 # Get the highest max and lowest min between January and February
@@ -48,22 +51,23 @@ cumulative_min = 0
 for batch_definition in jan_feb_batch_definition_list:
     batch_id: str = batch_definition.id
     current_max = validator_multi_batch.get_metric(
-        MetricConfiguration("column.max", metric_domain_kwargs={
-            "column": "fare_amount",
-            "batch_id": batch_id
-        })
+        MetricConfiguration(
+            "column.max",
+            metric_domain_kwargs={"column": "fare_amount", "batch_id": batch_id},
+        )
     )
     cumulative_max = current_max if current_max > cumulative_max else cumulative_max
 
     current_min = validator_multi_batch.get_metric(
-        MetricConfiguration("column.min", metric_domain_kwargs={
-            "column": "fare_amount",
-            "batch_id": batch_id
-        })
+        MetricConfiguration(
+            "column.min",
+            metric_domain_kwargs={"column": "fare_amount", "batch_id": batch_id},
+        )
     )
     cumulative_min = current_min if current_min < cumulative_min else cumulative_min
 
 # Use the highest max and lowest min from Jan and Feb to create an expectation which we validate against March
-result = validator_multi_batch.expect_column_values_to_be_between("fare_amount", min_value=cumulative_min,
-                                                                  max_value=cumulative_max)
+result = validator_multi_batch.expect_column_values_to_be_between(
+    "fare_amount", min_value=cumulative_min, max_value=cumulative_max
+)
 assert result["success"]
