81a83770acf90a1aa36e043e430ab6e0b29d14d0
Alex Sherstinsky <alex@superconductivehealth.com>
2021-05-14 12:49:50 -0700

    Updated an abstract class to fit interfaces properly.

--- great_expectations/rule_based_profiler/parameter_builder/multi_batch_parameter_builder.py
+++ great_expectations/rule_based_profiler/parameter_builder/multi_batch_parameter_builder.py
@@ -1,13 +1,18 @@
 from abc import abstractmethod
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations import DataContext
 from great_expectations.core.batch import BatchRequest
+from great_expectations.rule_based_profiler.domain_builder.domain import Domain
 from great_expectations.rule_based_profiler.parameter_builder.parameter_builder import (
     ParameterBuilder,
 )
+from great_expectations.rule_based_profiler.parameter_builder.parameter_container import (
+    ParameterContainer,
+)
 from great_expectations.rule_based_profiler.util import get_batch_ids
+from great_expectations.validator.validator import Validator
 
 
 class MultiBatchParameterBuilder(ParameterBuilder):
