94532e9f74d6a5cad65cad81cf8e8abdba605613
Bryan Wilkinson <bwilkinson@minerkasch.com>
2021-05-17 13:26:39 -0400

    Fixed formatting of mime type expectation

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_of_mime_type.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_of_mime_type.py
@@ -1,16 +1,18 @@
 import json
 import re
+from typing import Optional
 
-
+import magic
 
 #!!! This giant block of imports should be something simpler, such as:
 # from great_exepectations.helpers.expectation_creation import *
 from great_expectations.execution_engine import (
+    ExecutionEngine,
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
-    ExecutionEngine,
 )
+from great_expectations.execution_engine.execution_engine import MetricDomainTypes
 from great_expectations.expectations.expectation import (
     ColumnMapExpectation,
     Expectation,
@@ -19,34 +21,31 @@ from great_expectations.expectations.expectation import (
 from great_expectations.expectations.metrics import (
     ColumnMapMetricProvider,
     column_condition_partial,
-    column_function_partial
+    column_function_partial,
 )
 from great_expectations.expectations.registry import (
     _registered_expectations,
     _registered_metrics,
     _registered_renderers,
 )
-from great_expectations.execution_engine.execution_engine import MetricDomainTypes
-from great_expectations.validator.validation_graph import MetricConfiguration
 from great_expectations.expectations.util import render_evaluation_parameter_string
 from great_expectations.render.renderer.renderer import renderer
 from great_expectations.render.types import RenderedStringTemplateContent
 from great_expectations.render.util import num_to_str, substitute_none_for_missing
+from great_expectations.validator.validation_graph import MetricConfiguration
 from great_expectations.validator.validator import Validator
 
-from typing import Optional
-
-import magic
 
 class ColumnValuesToBeOfMimeType(ColumnMapMetricProvider):
     """
     MetricProvider tests whether binary data's mime type matches the provided string.
-    Uses the python-magic package which is an interface to libmagic. 
+    Uses the python-magic package which is an interface to libmagic.
     """
+
     condition_metric_name = "column_values.mime_type.is_type"
     condition_value_keys = ("mime_type",)
 
-    default_kwargs_values = {"mime_type":None}
+    default_kwargs_values = {"mime_type": None}
 
     function_metric_name = "column_values.mime_type"
     function_value_keys = tuple()
@@ -54,7 +53,7 @@ class ColumnValuesToBeOfMimeType(ColumnMapMetricProvider):
     @column_function_partial(engine=PandasExecutionEngine)
     def _pandas_function(self, column, **kwargs):
         try:
-            return column.apply(lambda x: magic.from_buffer(x,mime=True))
+            return column.apply(lambda x: magic.from_buffer(x, mime=True))
         except TypeError:
             raise (
                 TypeError(
@@ -64,8 +63,8 @@ class ColumnValuesToBeOfMimeType(ColumnMapMetricProvider):
 
     @column_condition_partial(engine=PandasExecutionEngine)
     def _pandas_condition(cls, column, _metrics, mime_type, **kwargs):
-        types, _ , _ = _metrics["column_values.mime_type.map"]
-        return types  == mime_type
+        types, _, _ = _metrics["column_values.mime_type.map"]
+        return types == mime_type
 
     @classmethod
     def _get_evaluation_dependencies(
@@ -91,6 +90,7 @@ class ColumnValuesToBeOfMimeType(ColumnMapMetricProvider):
             )
 
         return dependencies
+
 
 class ExpectColumnValuesToBeOfMimeType(ColumnMapExpectation):
     """Expect column entries to be binary values encoding a file of a given mime type,\
@@ -127,15 +127,12 @@ class ExpectColumnValuesToBeOfMimeType(ColumnMapExpectation):
         "maturity": "experimental",
         "tags": ["experimental", "column map expectation", "binary"],
         "package": "experimental_expectations",
-        "contributors": [
-            "@zdata-inc",
-            "@wilbry"
-        ],
+        "contributors": ["@zdata-inc", "@wilbry"],
         "requirements": ["python-magic"],
     }
 
     map_metric = "column_values.mime_type.is_type"
-    success_keys = ("mostly","mime_type")
+    success_keys = ("mostly", "mime_type")
 
     default_kwarg_values = {
         "row_condition": None,
@@ -144,80 +141,92 @@ class ExpectColumnValuesToBeOfMimeType(ColumnMapExpectation):
         "mostly": 1,
         "result_format": "BASIC",
         "include_config": True,
-        "catch_exceptions": False
+        "catch_exceptions": False,
     }
 
-
     examples = [
-     {
-         "data":{
-              "all_png":[ #One pixel PNGs
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU\x00\x00\x00\nIDATx\x9cc\xd0\x00\x00\x00*\x00)k1Ts\x00\x00\x00\x00IEND\xaeB`\x82',
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc\xd0`\x80\x01k\x00\x01\xff\x00d\xda\x15Wd\x00\x00\x00\x00IEND\xaeB`\x82',
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82',
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82',
-                  ],
-              "all_flac": [ #Mono
-                  b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00(\x00\x00(\n\xc4Ap\x00\x00\x00\n\x84?\xae\x81\xf8\xf4\xbd\xe6\x01\xe5n\xbf\x1a\x8b\xac\x13\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8i\x0c\x00\t\x89\x02(\x12t\xec\xac\x1c\x93L6`[\xd9\x11n=\xd3\x0c\x9b\x7f\xff\xff\x18\xd9\xa9P\x87*\x80\x00\x00lr',
-                  b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1e\x00\x00\x1e\x05b \xf0\x00\x00\x00\n\x1f/P\xd0\x01\xb2\x9e=\xe56\x88~\xe3\x0cs\xbe\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x08\x00\t\xf0\x02\xeb\xe0\xa9GJ\xa8V6\xef4\xb5\x19\xea\x83\x7f\xff\xb5\x7f\x7f\xff\xc0\x17',
-                 #Stereo
-                  b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1f\x00\x00\x1f\x05b"\xf0\x00\x00\x00\x05\xd5|Z\xf9\x7f\xc7\xe1\x91\x88\xad\xa2:w}\x17\xd0\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x18\x00\x04q\x02\xc5k\xa4\xfeT\xd8\xd4<\xe7\xd6\x02\x7fB\x0c\x7fd\xc5:WJ\xe6\xea\xb1',
-                  b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1f\x00\x00\x1f\x05b"\xf0\x00\x00\x00\x056\x17\x17\xa2AN\xb0\x05Y\xa3\xf0\xb5,a\xde\xa7\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x18\x00\x04q\x02\xfb\xe6\xaef\xfe\xf0|\xcaD\x1b\x02"C\xc5\xe8\x7f\xff\x80\x00i\x7f\xa2U'
-                  ],
-              "mostly_png": [ #One pixel PNGs
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU\x00\x00\x00\nIDATx\x9cc\xd0\x00\x00\x00*\x00)k1Ts\x00\x00\x00\x00IEND\xaeB`\x82',
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc\xd0`\x80\x01k\x00\x01\xff\x00d\xda\x15Wd\x00\x00\x00\x00IEND\xaeB`\x82',
-                  b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82',
-                  # Mono Sound
-                  b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1e\x00\x00\x1e\x05b \xf0\x00\x00\x00\n\x1f/P\xd0\x01\xb2\x9e=\xe56\x88~\xe3\x0cs\xbe\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x08\x00\t\xf0\x02\xeb\xe0\xa9GJ\xa8V6\xef4\xb5\x19\xea\x83\x7f\xff\xb5\x7f\x7f\xff\xc0\x17'
-                  ]
-             },
-             "tests": [
+        {
+            "data": {
+                "all_png": [  # One pixel PNGs
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU\x00\x00\x00\nIDATx\x9cc\xd0\x00\x00\x00*\x00)k1Ts\x00\x00\x00\x00IEND\xaeB`\x82",
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc\xd0`\x80\x01k\x00\x01\xff\x00d\xda\x15Wd\x00\x00\x00\x00IEND\xaeB`\x82",
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82",
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82",
+                ],
+                "all_flac": [  # Mono
+                    b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00(\x00\x00(\n\xc4Ap\x00\x00\x00\n\x84?\xae\x81\xf8\xf4\xbd\xe6\x01\xe5n\xbf\x1a\x8b\xac\x13\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8i\x0c\x00\t\x89\x02(\x12t\xec\xac\x1c\x93L6`[\xd9\x11n=\xd3\x0c\x9b\x7f\xff\xff\x18\xd9\xa9P\x87*\x80\x00\x00lr',
+                    b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1e\x00\x00\x1e\x05b \xf0\x00\x00\x00\n\x1f/P\xd0\x01\xb2\x9e=\xe56\x88~\xe3\x0cs\xbe\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x08\x00\t\xf0\x02\xeb\xe0\xa9GJ\xa8V6\xef4\xb5\x19\xea\x83\x7f\xff\xb5\x7f\x7f\xff\xc0\x17',
+                    # Stereo
+                    b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1f\x00\x00\x1f\x05b"\xf0\x00\x00\x00\x05\xd5|Z\xf9\x7f\xc7\xe1\x91\x88\xad\xa2:w}\x17\xd0\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x18\x00\x04q\x02\xc5k\xa4\xfeT\xd8\xd4<\xe7\xd6\x02\x7fB\x0c\x7fd\xc5:WJ\xe6\xea\xb1',
+                    b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1f\x00\x00\x1f\x05b"\xf0\x00\x00\x00\x056\x17\x17\xa2AN\xb0\x05Y\xa3\xf0\xb5,a\xde\xa7\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x18\x00\x04q\x02\xfb\xe6\xaef\xfe\xf0|\xcaD\x1b\x02"C\xc5\xe8\x7f\xff\x80\x00i\x7f\xa2U',
+                ],
+                "mostly_png": [  # One pixel PNGs
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU\x00\x00\x00\nIDATx\x9cc\xd0\x00\x00\x00*\x00)k1Ts\x00\x00\x00\x00IEND\xaeB`\x82",
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc\xd0`\x80\x01k\x00\x01\xff\x00d\xda\x15Wd\x00\x00\x00\x00IEND\xaeB`\x82",
+                    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x03\x08\x02\x00\x00\x00\xdd\xbf\xf2\xd5\x00\x00\x00\rIDATx\x9cc`a\x80\x01V\x00\x00=\x00\n{\xe1\xb5y\x00\x00\x00\x00IEND\xaeB`\x82",
+                    # Mono Sound
+                    b'fLaC\x00\x00\x00"\x10\x00\x10\x00\x00\x00\x1e\x00\x00\x1e\x05b \xf0\x00\x00\x00\n\x1f/P\xd0\x01\xb2\x9e=\xe56\x88~\xe3\x0cs\xbe\x84\x00\x00( \x00\x00\x00reference libFLAC 1.3.3 20190804\x00\x00\x00\x00\xff\xf8f\x08\x00\t\xf0\x02\xeb\xe0\xa9GJ\xa8V6\xef4\xb5\x19\xea\x83\x7f\xff\xb5\x7f\x7f\xff\xc0\x17',
+                ],
+            },
+            "tests": [
                 {
                     "title": "pass_all_png",
                     "include_in_gallery": True,
                     "exact_match_out": False,
-                    "in": {"column": "all_png", "mime_type": "image/png", "mostly":1},
+                    "in": {"column": "all_png", "mime_type": "image/png", "mostly": 1},
                     "out": {"success": True},
                 },
                 {
                     "title": "fail_all_png",
                     "include_in_gallery": True,
                     "exact_match_out": True,
-                    "in": {"column": "all_png", "mime_type": "image/jpeg", "mostly":1},
+                    "in": {"column": "all_png", "mime_type": "image/jpeg", "mostly": 1},
                     "out": {"success": False},
                 },
                 {
                     "title": "pass_all_flac",
                     "include_in_gallery": True,
                     "exact_match_out": False,
-                    "in": {"column": "all_flac", "mime_type": "audio/flac", "mostly":1},
+                    "in": {
+                        "column": "all_flac",
+                        "mime_type": "audio/flac",
+                        "mostly": 1,
+                    },
                     "out": {"success": True},
                 },
                 {
                     "title": "fail_all_flac",
                     "include_in_gallery": True,
                     "exact_match_out": True,
-                    "in": {"column": "all_flac", "mime_type": "audio/mp3", "mostly":1},
+                    "in": {"column": "all_flac", "mime_type": "audio/mp3", "mostly": 1},
                     "out": {"success": False},
                 },
                 {
                     "title": "pass_mostly_png",
                     "include_in_gallery": True,
                     "exact_match_out": True,
-                    "in": {"column": "mostly_png", "mime_type": "image/png", "mostly":0.5},
-                    "out": {"success": True} 
+                    "in": {
+                        "column": "mostly_png",
+                        "mime_type": "image/png",
+                        "mostly": 0.5,
+                    },
+                    "out": {"success": True},
                 },
                 {
                     "title": "fail_mostly_png",
                     "include_in_gallery": True,
                     "exact_match_out": True,
-                    "in": {"column": "mostly_png", "mime_type": "image/png", "mostly":0.8},
+                    "in": {
+                        "column": "mostly_png",
+                        "mime_type": "image/png",
+                        "mostly": 0.8,
+                    },
                     "out": {"success": False},
                 },
-                ]
-     }
+            ],
+        }
     ]
+
     @classmethod
     @renderer(renderer_type="renderer.prescriptive")
     @render_evaluation_parameter_string
@@ -228,7 +237,7 @@ class ExpectColumnValuesToBeOfMimeType(ColumnMapExpectation):
         language=None,
         runtime_configuration=None,
         **kwargs,
-        ):
+    ):
         include_column_name = runtime_configuration.get("include_column_name", True)
         include_column_name = (
             include_column_name if include_column_name is not None else True
@@ -281,7 +290,8 @@ class ExpectColumnValuesToBeOfMimeType(ColumnMapExpectation):
                     },
                 }
             )
-        ] 
+        ]
+
 
 if __name__ == "__main__":
     diagnostics_report = ExpectColumnValuesToBeOfMimeType().run_diagnostics()
