24a73830b15dba990acfd9f52a1e3e35aaf06a20
Shinnnyshinshin <will@superconductive.com>
2021-06-01 17:05:10 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4356,7 +4356,7 @@ def yellow_trip_pandas_data_context(
     project_path: str = str(tmp_path_factory.mktemp("taxi_data_context"))
     context_path: str = os.path.join(project_path, "great_expectations")
     os.makedirs(os.path.join(context_path, "expectations"), exist_ok=True)
-    data_path: str = os.path.join(context_path, "..", "test_data")
+    data_path: str = os.path.join(context_path, "..", "data")
     os.makedirs(os.path.join(data_path), exist_ok=True)
     shutil.copy(
         file_relative_path(
