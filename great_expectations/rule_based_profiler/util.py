a730a6b14a11b216595f3bce372d519503323280
Alex Sherstinsky <alex@superconductivehealth.com>
2021-06-14 23:19:14 -0700

    Bootstrapped Standard Error Optimization Based Estimator WIP

--- great_expectations/rule_based_profiler/util.py
+++ great_expectations/rule_based_profiler/util.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
+import scipy.stats
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations.rule_based_profiler.domain_builder.domain import Domain
@@ -81,45 +82,293 @@ def get_parameter_value(
     return parameter_reference
 
 
-class BootstrapStandardErrorEstimator(ABC):
+class NumericStatisticEstimator(ABC):
+    @abstractmethod
+    def get_original_data_sample_ids(
+        self,
+    ) -> List[Union[bytes, str, int, float, complex, tuple, frozenset]]:
+        """
+        :return: List of Hashable objects
+        """
+        pass
+
+    @abstractmethod
+    def compute_numeric_statistic(
+        self,
+        randomized_data_sample_identifiers: List[
+            Union[
+                bytes,
+                str,
+                int,
+                float,
+                complex,
+                tuple,
+                frozenset,
+            ]
+        ],
+    ) -> np.float64:
+        """
+        Computes numeric statistic from unique identifiers of data samples (a unique identifier must be hashable).
+        :parameter: randomized_data_sample_identifiers -- List of Hashable objects
+        :return: np.float64
+        """
+        pass
+
+
+class BootstrappingStandardErrorOptimizationBasedEstimator:
+    BOOTSTRAPPED_STATISTIC_DEVIATION_STANDARD_VARIANCE_INITIAL: np.float64 = 5.0e-1
+
     def __init__(
         self,
-        sample_size_n: int,
+        numeric_statistic_estimator: NumericStatisticEstimator,
+        sample_size: int,
+        bootstrapped_statistic_deviation_bound: Optional[float] = 1.0e-1,
+        prob_bootstrapped_statistic_deviation_outside_bound: Optional[float] = 5.0e-2,
     ):
         """
         # TODO: <Alex>ALEX -- Docstring</Alex>
         """
-        self._sample_size_n = sample_size_n
+        self._numeric_statistic_estimator = numeric_statistic_estimator
+        if sample_size < 2:
+            raise ValueError(
+                f"""Argument "sample_size" in {self.__class__.__name__} must be an integer greater than 1 \
+(the value {sample_size} was encountered).
+"""
+            )
+        self._sample_size = sample_size
 
-    # sample_mean = []
-    # for i in range(50):
-    #     y = random.sample(x.tolist(), 4)
-    #     avg = np.mean(y)
-    #     sample_mean.append(avg)
-    #
-    # print(np.mean(sample_mean))
-    """
->>> import scipy.stats
->>> scipy.stats.norm.ppf(0.975)
-1.959963984540054
->>> scipy.stats.norm.ppf(1.0)
-inf
->>> scipy.stats.norm.ppf(0.0)
--inf
->>> scipy.stats.norm.ppf(0.5)
-0.0
->>> """
+        self._bootstrapped_statistic_deviation_bound = (
+            bootstrapped_statistic_deviation_bound
+        )
+        self._prob_bootstrapped_statistic_deviation_outside_bound = (
+            prob_bootstrapped_statistic_deviation_outside_bound
+        )
 
-    def generate_random_sample_indexes(
+        self._optimal_num_bootstrap_samples_estimations = []
+
+    def compute_bootstrapped_statistic_mean_and_std(self) -> Dict[str, np.float64]:
+        optimal_num_bootstrap_samples: int = (
+            self._estimate_optimal_num_bootstrap_samples()
+        )
+        bootstrap_samples = self._generate_bootstrap_samples(
+            num_bootstrap_samples=optimal_num_bootstrap_samples
+        )
+        return {
+            "mean": self._bootstrapped_sample_mean(bootstrap_samples=bootstrap_samples),
+            "std": self._bootstrapped_sample_standard_error(
+                bootstrap_samples=bootstrap_samples
+            ),
+        }
+
+    def _estimate_optimal_num_bootstrap_samples(
+        self,
+    ) -> int:
+        optimal_num_bootstrap_samples: int = self._estimate_num_bootstrap_samples(
+            bootstrap_samples=None
+        )
+        self._optimal_num_bootstrap_samples_estimations.append(
+            optimal_num_bootstrap_samples
+        )
+
+        previous_max_optimal_num_bootstrap_samples: int = 0
+        current_max_optimal_num_bootstrap_samples: int = max(
+            self._optimal_num_bootstrap_samples_estimations
+        )
+
+        while (
+            current_max_optimal_num_bootstrap_samples
+            > previous_max_optimal_num_bootstrap_samples
+        ):
+            bootstrap_samples = self._generate_bootstrap_samples(
+                num_bootstrap_samples=optimal_num_bootstrap_samples
+            )
+            optimal_num_bootstrap_samples = self._estimate_num_bootstrap_samples(
+                bootstrap_samples=bootstrap_samples
+            )
+            self._optimal_num_bootstrap_samples_estimations.append(
+                optimal_num_bootstrap_samples
+            )
+            previous_max_optimal_num_bootstrap_samples = (
+                current_max_optimal_num_bootstrap_samples
+            )
+            current_max_optimal_num_bootstrap_samples = max(
+                self._optimal_num_bootstrap_samples_estimations
+            )
+
+        return current_max_optimal_num_bootstrap_samples
+
+    def _generate_bootstrap_samples(self, num_bootstrap_samples: int) -> np.ndarray:
+        idx: int
+        # noinspection PyUnusedLocal
+        bootstrap_samples: Union[
+            np.ndarray,
+            List[Union[float, np.float32, np.float64]],
+        ] = [
+            self._compute_statistic_for_random_sample()
+            for idx in range(num_bootstrap_samples)
+        ]
+        bootstrap_samples = np.array(bootstrap_samples, dtype=np.float64)
+        return bootstrap_samples
+
+    def _estimate_num_bootstrap_samples(
+        self, bootstrap_samples: Optional[np.ndarray] = None
+    ) -> int:
+        quantile_complement_prob_outside_bound_divided_by_2: np.float64 = (
+            scipy.stats.norm.ppf(
+                1.0 - self._prob_bootstrapped_statistic_deviation_outside_bound
+            )
+        )
+
+        statistic_deviation_standard_variance: np.float64
+        if bootstrap_samples is None:
+            statistic_deviation_standard_variance = (
+                self.BOOTSTRAPPED_STATISTIC_DEVIATION_STANDARD_VARIANCE_INITIAL
+            )
+        else:
+            statistic_deviation_standard_variance = (
+                self._bootstrapped_statistic_deviation_standard_variance(
+                    bootstrap_samples=bootstrap_samples
+                )
+            )
+
+        bootstrap_samples_fractional: np.float64 = np.float64(
+            quantile_complement_prob_outside_bound_divided_by_2
+            * statistic_deviation_standard_variance
+            / self._bootstrapped_statistic_deviation_bound
+        )
+        bootstrap_samples: int = round(bootstrap_samples_fractional)
+
+        return bootstrap_samples
+
+    def _generate_random_sample_indexes(
         self,
     ) -> List[int]:
-        permutation: List[int] = np.arange(self.sample_size_n)
-        return random.choices(permutation, k=self.sample_size_n)
+        permutation: List[int] = np.arange(self._sample_size)
+        return random.choices(permutation, k=self._sample_size)
 
-    @abstractmethod
-    def compute_statistic(self) -> Union[float, np.float32, np.float64]:
-        pass
+    def _compute_statistic_for_random_sample(self) -> np.float64:
+        random_sample_indexes: List[int] = self._generate_random_sample_indexes()
+        original_data_sample_ids: List[
+            Union[
+                bytes,
+                str,
+                int,
+                float,
+                complex,
+                tuple,
+                frozenset,
+            ]
+        ] = self._numeric_statistic_estimator.get_original_data_sample_ids()
+        idx: int
+        randomized_data_sample_identifiers: List[
+            Union[
+                bytes,
+                str,
+                int,
+                float,
+                complex,
+                tuple,
+                frozenset,
+            ]
+        ] = [original_data_sample_ids[idx] for idx in random_sample_indexes]
+        computed_sample_statistic: np.float64 = (
+            self._numeric_statistic_estimator.compute_numeric_statistic(
+                randomized_data_sample_identifiers=randomized_data_sample_identifiers
+            )
+        )
+        return computed_sample_statistic
 
-    @property
-    def sample_size_n(self) -> int:
-        return self._sample_size_n
+    def _bootstrapped_statistic_deviation_standard_variance(
+        self,
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        return np.float64(
+            (
+                2.0
+                + self._bootstrapped_sample_excess_kurtosis(
+                    bootstrap_samples=bootstrap_samples
+                )
+            )
+            / 4.0
+        )
+
+    def _bootstrapped_sample_excess_kurtosis(
+        self,
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        return np.float64(
+            self._bootstrapped_sample_kurtosis(bootstrap_samples=bootstrap_samples)
+            - 3.0
+        )
+
+    def _bootstrapped_sample_kurtosis(
+        self,
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        num_bootstrap_samples: int = bootstrap_samples.size
+        if num_bootstrap_samples < 2:
+            raise ValueError(
+                f"""Number of bootstrap samples in {self.__class__.__name__} must be an integer greater than 1 \
+(the value {num_bootstrap_samples} was encountered).
+"""
+            )
+        sample_mean: np.float64 = self._bootstrapped_sample_mean(
+            bootstrap_samples=num_bootstrap_samples
+        )
+        bootstrap_samples_mean_removed: np.ndarray = bootstrap_samples - sample_mean
+        bootstrap_samples_mean_removed_power_4: np.ndarray = np.power(
+            bootstrap_samples_mean_removed, 4
+        )
+        sample_kurtosis: np.float64 = np.sum(
+            bootstrap_samples_mean_removed_power_4
+        ) / np.power(
+            self._bootstrapped_sample_standard_variance(
+                bootstrap_samples=bootstrap_samples
+            ),
+            2,
+        )
+        return sample_kurtosis
+
+    def _bootstrapped_sample_standard_error(
+        self,
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        return np.sqrt(
+            self._bootstrapped_sample_standard_variance(
+                bootstrap_samples=bootstrap_samples
+            )
+        )
+
+    def _bootstrapped_sample_standard_variance(
+        self,
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        num_bootstrap_samples: int = bootstrap_samples.size
+        if num_bootstrap_samples < 2:
+            raise ValueError(
+                f"""Number of bootstrap samples in {self.__class__.__name__} must be an integer greater than 1 \
+(the value {num_bootstrap_samples} was encountered).
+"""
+            )
+
+        sample_variance: np.float64 = self._bootstrapped_sample_variance(
+            bootstrap_samples=num_bootstrap_samples
+        )
+        sample_standard_variance: np.float64 = np.float64(
+            num_bootstrap_samples * sample_variance / (num_bootstrap_samples - 1)
+        )
+        return sample_standard_variance
+
+    @staticmethod
+    def _bootstrapped_sample_mean(
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        sample_mean: Union[np.ndarray, np.float64] = np.mean(bootstrap_samples)
+        return np.float64(sample_mean)
+
+    @staticmethod
+    def _bootstrapped_sample_variance(
+        bootstrap_samples: np.ndarray,
+    ) -> np.float64:
+        sample_variance: Union[np.ndarray, np.float64] = np.var(bootstrap_samples)
+        return np.float64(sample_variance)
