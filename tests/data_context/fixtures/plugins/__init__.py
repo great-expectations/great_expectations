0a3b913c77802a2037c727bf5a90b0a0d2e74c4e
Anthony Burdi <anthony@superconductive.com>
2021-06-30 13:40:26 -0400

    Custom expectation_suite fixture plugin for tests

---
+++ tests/data_context/fixtures/plugins/__init__.py
@@ -0,0 +1,1 @@
+from .my_custom_expectation_store import MyCustomExpectationsStore
---
+++ tests/data_context/fixtures/plugins/my_custom_expectation_store.py
@@ -0,0 +1,5 @@
+from great_expectations.data_context.store import ExpectationsStore
+
+
+class MyCustomExpectationsStore(ExpectationsStore):
+    pass
