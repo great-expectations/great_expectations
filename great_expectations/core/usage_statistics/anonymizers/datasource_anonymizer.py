7ae669dedcbbd43eab8ef62af266605271bcde8d
Anthony Burdi <anthony@superconductive.com>
2021-06-30 13:21:38 -0400

    Handle SimpleSqlalchemyDatasource in datasource_anonymizer

--- great_expectations/core/usage_statistics/anonymizers/datasource_anonymizer.py
+++ great_expectations/core/usage_statistics/anonymizers/datasource_anonymizer.py
@@ -6,9 +6,11 @@ from great_expectations.core.usage_statistics.anonymizers.execution_engine_anony
     ExecutionEngineAnonymizer,
 )
 from great_expectations.datasource import (
+    BaseDatasource,
     Datasource,
     LegacyDatasource,
     PandasDatasource,
+    SimpleSqlalchemyDatasource,
     SparkDFDatasource,
     SqlAlchemyDatasource,
 )
@@ -26,7 +28,13 @@ class DatasourceAnonymizer(Anonymizer):
             LegacyDatasource,
         ]
 
-        self._ge_classes = [Datasource]
+        # ordered bottom up in terms of inheritance order
+        self._ge_classes = [
+            SimpleSqlalchemyDatasource,
+            Datasource,
+            BaseDatasource,
+        ]
+
         self._execution_engine_anonymizer = ExecutionEngineAnonymizer(salt=salt)
         self._data_connector_anonymizer = DataConnectorAnonymizer(salt=salt)
 
@@ -64,6 +72,85 @@ class DatasourceAnonymizer(Anonymizer):
                 )
                 for data_connector_name, data_connector_config in data_connector_configs.items()
             ]
+
+        return anonymized_info_dict
+
+    def anonymize_simple_sqlalchemy_datasource(self, name, config):
+        """
+
+        Args:
+            name:
+            config:
+
+        Returns:
+
+        """
+        anonymized_info_dict = dict()
+        anonymized_info_dict["anonymized_name"] = self.anonymize(name)
+        assert (
+            config.get("class_name") == "SimpleSqlalchemyDatasource"
+        ), "Must pass a config for SimpleSqlalchemyDatasource"
+        if config.get("module_name") is None:
+            config["module_name"] = "great_expectations.datasource"
+        self.anonymize_object_info(
+            anonymized_info_dict=anonymized_info_dict,
+            ge_classes=self._ge_classes,
+            object_config=config,
+        )
+
+        # Only and directly provide parent_class of execution engine
+        anonymized_info_dict["anonymized_execution_engine"] = {
+            "parent_class": "SqlAlchemyExecutionEngine"
+        }
+
+        # Use the `introspection` and `tables` keys to find data_connectors in SimpleSqlalchemyDatasources
+        introspection_data_connector_configs = config.get("introspection")
+        tables_data_connector_configs = config.get("tables")
+
+        introspection_data_connector_anonymized_configs = []
+        if introspection_data_connector_configs is not None:
+            for (
+                data_connector_name,
+                data_connector_config,
+            ) in introspection_data_connector_configs.items():
+                if data_connector_config.get("class_name") is None:
+                    data_connector_config[
+                        "class_name"
+                    ] = "InferredAssetSqlDataConnector"
+                if data_connector_config.get("module_name") is None:
+                    data_connector_config[
+                        "module_name"
+                    ] = "great_expectations.datasource.data_connector"
+                introspection_data_connector_anonymized_configs.append(
+                    self._data_connector_anonymizer.anonymize_data_connector_info(
+                        name=data_connector_name, config=data_connector_config
+                    )
+                )
+
+        tables_data_connector_anonymized_configs = []
+        if tables_data_connector_configs is not None:
+            for (
+                data_connector_name,
+                data_connector_config,
+            ) in tables_data_connector_configs.items():
+                if data_connector_config.get("class_name") is None:
+                    data_connector_config[
+                        "class_name"
+                    ] = "ConfiguredAssetSqlDataConnector"
+                if data_connector_config.get("module_name") is None:
+                    data_connector_config[
+                        "module_name"
+                    ] = "great_expectations.datasource.data_connector"
+                tables_data_connector_anonymized_configs.append(
+                    self._data_connector_anonymizer.anonymize_data_connector_info(
+                        name=data_connector_name, config=data_connector_config
+                    )
+                )
+
+        anonymized_info_dict["anonymized_data_connectors"] = (
+            introspection_data_connector_anonymized_configs
+            + tables_data_connector_anonymized_configs
+        )
 
         return anonymized_info_dict
 
