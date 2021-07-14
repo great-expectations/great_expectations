a0a395232c7e0e994992ad796d6ac636a940721c
Chetan Kini <chetan@superconductive.com>
2021-07-14 16:23:41 -0400

    Add test file

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+from .trace_docs_deps import (
+    find_docusaurus_refs,
+    get_local_imports,
+    get_import_paths
+)
---
+++ tests/scripts/test_trace_docs_deps.py
@@ -0,0 +1,12 @@
+from scripts import find_docusaurus_refs, get_local_imports, get_import_paths
+
+
+def test_find_docusaurus_refs():
+    pass
+
+def test_get_local_imports():
+    pass
+
+def test_get_import_paths():
+    pass
+
