2a146ed18b397d09a929c38a3dd898944da07c33
Daniel Walter <daniel.walter@imperva.com>
2021-07-29 15:29:22 -0400

    feature: add python3.9 to python versions

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ config = {
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+
     ],
 }
 
