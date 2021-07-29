1de97f8e69c9bbc52d37f282307b5a495dc03ed1
Shinnnyshinshin <will@superconductive.com>
2021-07-29 14:48:07 -0700

    Revert "Update test_data_context_v013.py"
    This reverts commit 11282a277f5fe7e0d43ee73604313005ad904cb8.

--- tests/data_context/test_data_context_v013.py
+++ tests/data_context/test_data_context_v013.py
@@ -530,12 +530,12 @@ def test_get_batch_with_query_in_runtime_parameters_using_runtime_data_connector
 
     selectable_table_name = batch.data.selectable.name
     selectable_count_sql_str = f"select count(*) from {selectable_table_name}"
-    sa_engine = batch.data.execution_engine.connection
+    sa_engine = batch.data.execution_engine.engine
 
     assert sa_engine.execute(selectable_count_sql_str).scalar() == 120
     assert batch.batch_markers.get("ge_load_time") is not None
     # since create_temp_table defaults to True, there should be 1 temp table
-    assert len(get_sqlite_temp_table_names(batch.data.execution_engine.connection)) == 1
+    assert len(get_sqlite_temp_table_names(batch.data.execution_engine.engine)) == 1
 
     # if create_temp_table in batch_spec_passthrough is set to False, no new temp tables should be created
     batch = context.get_batch(
@@ -553,7 +553,7 @@ def test_get_batch_with_query_in_runtime_parameters_using_runtime_data_connector
             batch_spec_passthrough={"create_temp_table": False},
         ),
     )
-    assert len(get_sqlite_temp_table_names(batch.data.execution_engine.connection)) == 1
+    assert len(get_sqlite_temp_table_names(batch.data.execution_engine.engine)) == 1
 
 
 def test_get_validator_with_query_in_runtime_parameters_using_runtime_data_connector(
