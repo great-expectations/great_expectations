e4f28f6d8f00a95e4c851b9c0c737714d1718a5c
Shinnnyshinshin <will@superconductive.com>
2021-07-29 14:48:02 -0700

    Revert "Update util.py"
    This reverts commit b7df5ca53a7e084d2382f12fcff4205f5e88d7ab.

--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -920,11 +920,9 @@ def build_sa_validator_with_data(
     )
     # <WILL> it always comes down to this section of hte code. Is there something that should be done here
     # CAN THIS ORDER BE CHANGED?
-
-    # engine is actually an sa engine, not an ExecutionEngine object.
-    execution_engine = SqlAlchemyExecutionEngine(caching=caching, engine=engine)
-    batch_data = SqlAlchemyBatchData(execution_engine=execution_engine, table_name=table_name)
+    batch_data = SqlAlchemyBatchData(execution_engine=engine, table_name=table_name)
     batch = Batch(data=batch_data, batch_definition=batch_definition)
+    execution_engine = SqlAlchemyExecutionEngine(caching=caching, engine=engine)
 
     return Validator(execution_engine=execution_engine, batches=(batch,))
 
