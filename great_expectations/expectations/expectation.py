ed3b6a9c3f6b1897b29d433b7c23bf7976d7d55e
Alex Sherstinsky <alex@superconductive.com>
2021-07-30 10:59:42 -0700

    Update default value of ignore_row_if for MulticolumnMapExpectation.

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -1387,7 +1387,7 @@ class MulticolumnMapExpectation(TableExpectation, ABC):
     default_kwarg_values = {
         "row_condition": None,
         "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
-        "ignore_row_if": "any_value_is_missing",
+        "ignore_row_if": "all_values_are_missing",
         "result_format": "BASIC",
         "include_config": True,
         "catch_exceptions": True,
