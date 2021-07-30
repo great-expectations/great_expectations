a60c52d62ee8068b75f3f26eae2e5e100e6bb210
John DiMatteo <jdimatteo@verily.com>
2021-07-29 21:29:19 -0600

    Enabled remaining passing tests, added comments for all remaining failing ones

--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -1404,25 +1404,15 @@ def candidate_test_is_on_temporary_notimplemented_list_cfe(context, expectation_
             "expect_column_values_to_match_strftime_format",  # todo(jdimatteo): AssertionError and KeyError: 'unexpected_list'
             "expect_column_values_to_match_json_schema",  # todo(jdimatteo): KeyError: 'unexpected_list'
             "expect_column_mean_to_be_between",  # todo(jdimatteo) 400 No matching signature for operator *
-            "expect_column_median_to_be_between",
-            "expect_column_quantile_values_to_be_between",
-            "expect_column_stdev_to_be_between",
-            "expect_column_unique_value_count_to_be_between",
-            "expect_column_proportion_of_unique_values_to_be_between",
-            "expect_column_most_common_value_to_be_in_set",
-            "expect_column_max_to_be_between",
-            "expect_column_min_to_be_between",
-            "expect_column_sum_to_be_between",
-            "expect_column_pair_values_A_to_be_greater_than_B",
-            "expect_column_pair_values_to_be_equal",
-            "expect_column_pair_values_to_be_in_set",
-            "expect_multicolumn_values_to_be_unique",
-            "expect_multicolumn_sum_to_equal",
-            "expect_column_pair_cramers_phi_value_to_be_less_than",
-            "expect_column_kl_divergence_to_be_less_than",
-            "expect_column_bootstrapped_ks_test_p_value_to_be_greater_than",
-            "expect_column_chisquare_test_p_value_to_be_greater_than",
-            "expect_column_parameterized_distribution_ks_test_p_value_to_be_greater_than",
+            "expect_column_quantile_values_to_be_between",  # todo(jdimatteo): takes over 15 minutes to "collect" (haven't actually seen it complete yet)
+            "expect_column_pair_values_A_to_be_greater_than_B",  # todo(jdimatteo): AttributeError: 'Validator'  object has no attribute 'expect_column_pair_values_A_to_be_greater_than_B'
+            "expect_column_pair_values_to_be_equal",  # todo(jdimatteo): AttributeError: 'Validator'  object has no attribute 'expect_column_pair_values_to_be_equal'
+            "expect_column_pair_values_to_be_in_set",  # todo(jdimatteo): ColumnPairMapExpectation must override get_validation_dependencies or declare exactly one map_metric
+            "expect_multicolumn_sum_to_equal",  # todo(jdimatteo): AttributeError: 'Validator'  object has no attribute 'expect_multicolumn_sum_to_equal'
+            "expect_column_kl_divergence_to_be_less_than",  # todo(jdimatteo): takes over 64 minutes to "collect" (haven't actually seen it complete yet)
+            "expect_column_bootstrapped_ks_test_p_value_to_be_greater_than",  # todo(jdimatte): took 43 minutes, AttributeError: 'Validator'  object has no attribute 'expect_column_bootstrapped_ks...
+            "expect_column_chisquare_test_p_value_to_be_greater_than",  # todo(jdimatteo): takes over 27 minutes to "collect" (haven't actually seen it complete yet)
+            "expect_column_parameterized_distribution_ks_test_p_value_to_be_greater_than",  # todo(jdimatteo): after 22 minutes, 'Validator'  object has no attribute 'expect_column_parameteriz...
             "expect_compound_columns_to_be_unique",  # TODO(https://github.com/great-expectations/great_expectations/issues/3095)
         ]
     return False
