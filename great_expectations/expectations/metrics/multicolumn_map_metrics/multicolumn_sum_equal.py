96bd10c20ddcddf0e4abe75eb2d8a3ae2edefbac
Nathan Farmer <nathan@superconductive.com>
2021-08-05 23:02:41 -0400

    [FEATURE] GREAT-56/GREAT-4 Implement MulticolumnSumEqual class for SQLAlchemy.

--- great_expectations/expectations/metrics/multicolumn_map_metrics/multicolumn_sum_equal.py
+++ great_expectations/expectations/metrics/multicolumn_map_metrics/multicolumn_sum_equal.py
@@ -1,4 +1,8 @@
-from great_expectations.execution_engine import PandasExecutionEngine
+from great_expectations.execution_engine import (
+    PandasExecutionEngine,
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.expectations.metrics.import_manager import sa
 from great_expectations.expectations.metrics.map_metric_provider import (
     MulticolumnMapMetricProvider,
     multicolumn_condition_partial,
@@ -10,9 +14,20 @@ class MulticolumnSumEqual(MulticolumnMapMetricProvider):
     condition_value_keys = ("sum_total",)
     condition_domain_keys = ("batch_id", "table", "column_list")
 
-    # TODO: <Alex>ALEX -- temporarily only a Pandas implementation is provided (others to follow).</Alex>
+    # TODO: <Alex>ALEX -- temporarily only Pandas and SQLAlchemy implementations are provided (others to follow).</Alex>
     @multicolumn_condition_partial(engine=PandasExecutionEngine)
     def _pandas(cls, column_list, **kwargs):
         sum_total = kwargs.get("sum_total")
         row_wise_cond = column_list.sum(axis=1) == sum_total
+        return row_wise_cond
+
+    @multicolumn_condition_partial(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(cls, column_clause, **kwargs):
+        sum_total = kwargs.get("sum_total")
+        engine = kwargs.get("_sqlalchemy_engine")
+        table = kwargs.get("_table")
+        stmt = sa.select(column_clause).select_from(table)
+        with engine.connect() as connection:
+            result = connection.execute(stmt)
+        row_wise_cond = [sum(row) == sum_total for row in result]
         return row_wise_cond
