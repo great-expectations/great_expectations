bb34885e6f391c053640deb41f39a9aa53bd3f7e
Shinnnyshinshin <will@superconductive.com>
2021-08-06 21:46:46 -0700

    multi-column map expectations

--- tests/test_definitions/test_expectations.py
+++ tests/test_definitions/test_expectations.py
@@ -44,8 +44,8 @@ def pytest_generate_tests(metafunc):
     # expectation_dirs = ["column_map_expectations"]
 
     # expectation_dirs = ["column_pair_map_expectations"]
-    expectation_dirs = ["multi_table_expectations"]
-    # expectation_dirs = ["multicolumn_map_expectations"]
+    # expectation_dirs = ["multi_table_expectations"]
+    expectation_dirs = ["multicolumn_map_expectations"]
     # expectation_dirs = ["other_expectations"]
 
     for expectation_category in expectation_dirs:
