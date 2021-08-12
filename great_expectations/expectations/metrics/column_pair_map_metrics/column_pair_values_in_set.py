ab0c64dffc827b94223bf60bf090798bf78c34bb
Alex Sherstinsky <alex@superconductive.com>
2021-08-12 14:22:41 -0700

    WIP

--- great_expectations/expectations/metrics/column_pair_map_metrics/column_pair_values_in_set.py
+++ great_expectations/expectations/metrics/column_pair_map_metrics/column_pair_values_in_set.py
@@ -143,3 +143,50 @@ class ColumnPairValuesInSet(MapMetricProvider):
             F.when(F.col("set_AB").isNull(), F.lit(False)).otherwise(F.lit(True)),
         )
         return df["__success"], compute_domain_kwargs, accessor_domain_kwargs
+
+#ALEX
+from dateutil.parser import parse
+
+from great_expectations.execution_engine import PandasExecutionEngine
+from great_expectations.expectations.metrics.map_metric_provider import (
+    ColumnPairMapMetricProvider,
+    column_pair_condition_partial,
+)
+
+
+class ColumnPairValuesInSet(ColumnPairMapMetricProvider):
+    #ALEX
+    condition_domain_keys = ("batch_id", "table", "column_A", "column_B")
+    #ALEX
+    condition_metric_name = "column_pair_values.in_set"
+    condition_value_keys = (
+        "value_pairs_set",
+        "ignore_row_if",
+    )
+    domain_keys = ("batch_id", "table", "column_A", "column_B")
+
+    # TODO: <Alex>ALEX -- temporarily only a Pandas implementation is provided (others to follow).</Alex>
+    # noinspection PyPep8Naming
+    @column_pair_condition_partial(engine=PandasExecutionEngine)
+    def _pandas(cls, column_A, column_B, **kwargs):
+        allow_cross_type_comparisons = kwargs.get("allow_cross_type_comparisons")
+        if allow_cross_type_comparisons:
+            raise NotImplementedError
+
+        parse_strings_as_datetimes = kwargs.get("parse_strings_as_datetimes")
+        if parse_strings_as_datetimes:
+            # noinspection PyPep8Naming
+            temp_column_A = column_A.map(parse)
+            # noinspection PyPep8Naming
+            temp_column_B = column_B.map(parse)
+        else:
+            temp_column_A = column_A
+            temp_column_B = column_B
+
+        or_equal = kwargs.get("or_equal")
+        if or_equal:
+            return temp_column_A >= temp_column_B
+        else:
+            return temp_column_A > temp_column_B
+
+#ALEX
