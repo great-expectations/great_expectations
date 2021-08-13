851cd04b879f071ffdecc59cee447bdfc7d1eafb
Micha BA Kunze <michabenachim.kunze@maersk.com>
2021-08-13 23:32:59 +0200

    allow None for force_reuse_spark_context

--- great_expectations/data_context/types/base.py
+++ great_expectations/data_context/types/base.py
@@ -716,7 +716,7 @@ class DatasourceConfigSchema(Schema):
     class_name = fields.String(missing="Datasource")
     module_name = fields.String(missing="great_expectations.datasource")
 
-    force_reuse_spark_context = fields.Bool(required=False, missing=False)
+    force_reuse_spark_context = fields.Bool(required=False, missing=False, allow_none=True)
 
     execution_engine = fields.Nested(
         ExecutionEngineConfigSchema, required=False, allow_none=True
