3bc694290ada739cc641a2eb4de2e456f10b25a0
John DiMatteo <jdimatteo@verily.com>
2021-08-17 15:01:04 -0600

    Performance test runs queries
    tests/performance$ time pytest test_performance_using_bigquery.py --no-spark --no-postgresql -v
    ------------------------------------------------- benchmark: 1 tests ------------------------------------------------
    Name (time in s)                    Min     Max    Mean  StdDev  Median     IQR  Outliers     OPS  Rounds  Iterations
    ---------------------------------------------------------------------------------------------------------------------
    test_bikeshare_trips_1_table     3.7753  3.7753  3.7753  0.0000  3.7753  0.0000       0;0  0.2649       1           1
    ---------------------------------------------------------------------------------------------------------------------

--- tests/performance/test_performance_using_bigquery.py
+++ tests/performance/test_performance_using_bigquery.py
@@ -2,10 +2,11 @@
 #  include git describe output in json
 
 import os
-import time
 
 from pytest_benchmark.fixture import BenchmarkFixture
 
+from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
 from great_expectations.data_context import BaseDataContext
 from great_expectations.data_context.types.base import (
     DataContextConfig,
@@ -13,25 +14,76 @@ from great_expectations.data_context.types.base import (
 )
 
 
-def foo(seconds):
-    time.sleep(seconds)
-    return 123
-
-
 def test_bikeshare_trips_1_table(benchmark: BenchmarkFixture):
-    context = _create_context()
-    assert 123 == benchmark.pedantic(foo, args=(0.042,), iterations=1, rounds=1)
+    checkpoint_name = "my_checkpoint"
+    suite_and_asset_name = (
+        "bikeshare_trips_1"  # The table name is used as the asset and suite name.
+    )
+    datasource_and_dataconnector_name = "my_datasource_and_dataconnector"
+
+    context = _create_context(datasource_and_dataconnector_name, [suite_and_asset_name])
+    _add_expectation_configuration(context=context, suite_name=suite_and_asset_name)
+    _add_checkpoint(
+        context,
+        datasource_and_dataconnector_name,
+        checkpoint_name,
+        [suite_and_asset_name],
+    )
+
+    result: CheckpointResult = benchmark.pedantic(
+        context.run_checkpoint,
+        kwargs={"checkpoint_name": checkpoint_name},
+        iterations=1,
+        rounds=1,
+    )
+    assert result.success, result
 
 
-# todo(jdimatteo): test with multiple datasources?
-_datasource_name = "my_bigquery_datasource"
+def _add_checkpoint(
+    context: BaseDataContext,
+    datasource_and_dataconnector_name: str,
+    checkpoint_name: str,
+    suite_and_asset_names=[],
+):
+    validations = [
+        {
+            "expectation_suite_name": suite_and_asset_name,
+            "batch_request": {
+                "datasource_name": datasource_and_dataconnector_name,
+                "data_connector_name": datasource_and_dataconnector_name,
+                "data_asset_name": suite_and_asset_name,
+                "batch_spec_passthrough": {"create_temp_table": False},
+            },
+        }
+        for suite_and_asset_name in suite_and_asset_names
+    ]
+    context.add_checkpoint(
+        name=checkpoint_name, class_name="SimpleCheckpoint", validations=validations
+    )
+
+
+def _add_expectation_configuration(context: BaseDataContext, suite_name: str):
+    suite = context.create_expectation_suite(expectation_suite_name=suite_name)
+    # todo(jdimatteo) add more expectations to be more representative of use case I want to optimize
+    suite.add_expectation(
+        expectation_configuration=ExpectationConfiguration(
+            expectation_type="expect_column_values_to_not_be_null",
+            kwargs={"column": "trip_id"},
+        )
+    )
+    # Save the expectation suite or else it doesn't show up in the data docs.
+    context.save_expectation_suite(
+        expectation_suite=suite, expectation_suite_name=suite_name
+    )
 
 
 def _html_dir() -> str:
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), "html")
 
 
-def _create_context() -> BaseDataContext:
+def _create_context(
+    datasource_and_dataconnector_name: str, asset_names: list[str]
+) -> BaseDataContext:
     store_backend = {
         "class_name": "TupleFilesystemStoreBackend",
         "base_directory": _html_dir(),
@@ -44,24 +96,26 @@ def _create_context() -> BaseDataContext:
         }
     }
     bigquery_project = os.environ["GE_TEST_BIGQUERY_PROJECT"]
-    bigquery_dataset = os.environ.get("GE_TEST_BIGQUERY_PROJECT", "performance_ci")
+    bigquery_dataset = os.environ.get(
+        "GE_TEST_BIGQUERY_PEFORMANCE_DATASET", "performance_ci"
+    )
 
     data_context_config = DataContextConfig(
         store_backend_defaults=InMemoryStoreBackendDefaults(),
         data_docs_sites=data_docs_sites,
         anonymous_usage_statistics={"enabled": False},
         datasources={
-            _datasource_name: {
+            datasource_and_dataconnector_name: {
                 "class_name": "Datasource",
                 "execution_engine": {
                     "class_name": "SqlAlchemyExecutionEngine",
                     "connection_string": f"bigquery://{bigquery_project}/{bigquery_dataset}",
                 },
                 "data_connectors": {
-                    _datasource_name: {
+                    datasource_and_dataconnector_name: {
                         "class_name": "ConfiguredAssetSqlDataConnector",
                         "name": "whole_table",
-                        "assets": {f"bikeshare_trips_{i}": {} for i in range(1, 100)},
+                        "assets": {asset_name: {} for asset_name in asset_names},
                     },
                 },
             },
