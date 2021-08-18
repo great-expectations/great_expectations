9e38789f8e2f747e715c54e4eb697bb2412ed9e7
Alex Sherstinsky <alex@superconductive.com>
2021-08-18 07:51:52 -0700

    cleanup

--- great_expectations/expectations/core/expect_column_pair_values_to_be_equal.py
+++ great_expectations/expectations/core/expect_column_pair_values_to_be_equal.py
@@ -66,10 +66,9 @@ class ExpectColumnPairValuesToBeEqual(ColumnPairMapExpectation):
         "row_condition": None,
         "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
         "mostly": 1.0,
-        "parse_strings_as_datetimes": None,
         "result_format": "BASIC",
         "include_config": True,
-        "catch_exceptions": True,
+        "catch_exceptions": False,
     }
 
     def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
