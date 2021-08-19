dce4e9a9b1c3205c6268d3cac4b3605072462928
Chetan Kini <chetan@superconductive.com>
2021-08-19 16:56:34 -0400

    test: Write tests for error checking and self_check

--- tests/datasource/data_connector/test_inferred_asset_gcs_data_connector.py
+++ tests/datasource/data_connector/test_inferred_asset_gcs_data_connector.py
@@ -269,7 +269,9 @@ def test_simple_regex_example_with_implicit_data_asset_names_self_check(
 @mock.patch(
     "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.storage.Client"
 )
-def test_complex_regex_example_with_implicit_data_asset_names():
+def test_complex_regex_example_with_implicit_data_asset_names(
+    mock_gcs_conn, mock_list_keys, mock_emit
+):
     my_data_connector: InferredAssetGCSDataConnector = InferredAssetGCSDataConnector(
         name="my_data_connector",
         datasource_name="FAKE_DATASOURCE_NAME",
@@ -282,34 +284,6 @@ def test_complex_regex_example_with_implicit_data_asset_names():
     )
 
     my_data_connector._refresh_data_references_cache()
-
-    # TODO(cdkini): Review this test! Might have to split up into a few.
-    # Additionally, think about changing `list_gcs_keys` return vals
-
-    # Test for an unknown execution environment
-    with pytest.raises(ValueError):
-        batch_definition_list: List[
-            BatchDefinition
-        ] = my_data_connector.get_batch_definition_list_from_batch_request(
-            batch_request=BatchRequest(
-                datasource_name="non_existent_datasource",
-                data_connector_name="my_data_connector",
-                data_asset_name="my_data_asset",
-            )
-        )
-
-    # Test for an unknown data_connector
-    with pytest.raises(ValueError):
-        # noinspection PyUnusedLocal
-        batch_definition_list: List[
-            BatchDefinition
-        ] = my_data_connector.get_batch_definition_list_from_batch_request(
-            batch_request=BatchRequest(
-                datasource_name="FAKE_DATASOURCE_NAME",
-                data_connector_name="non_existent_data_connector",
-                data_asset_name="my_data_asset",
-            )
-        )
 
     assert (
         len(
@@ -362,62 +336,143 @@ def test_complex_regex_example_with_implicit_data_asset_names():
     ]
 
 
-# @mock_s3
-# def test_self_check():
-#     region_name: str = "us-east-1"
-#     bucket: str = "test_bucket"
-#     conn = boto3.resource("s3", region_name=region_name)
-#     conn.create_bucket(Bucket=bucket)
-#     client = boto3.client("s3", region_name=region_name)
+@mock.patch(
+    "great_expectations.core.usage_statistics.usage_statistics.UsageStatisticsHandler.emit"
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.list_gcs_keys",
+    return_value=[
+        "2020/01/alpha-1001.csv",
+        "2020/01/beta-1002.csv",
+        "2020/02/alpha-1003.csv",
+        "2020/02/beta-1004.csv",
+        "2020/03/alpha-1005.csv",
+        "2020/03/beta-1006.csv",
+        "2020/04/beta-1007.csv",
+    ],
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.storage.Client"
+)
+def test_get_batch_definition_list_from_batch_request_with_unknown_execution_env_raises_error(
+    mock_gcs_conn, mock_list_keys, mock_emit
+):
+    my_data_connector: InferredAssetGCSDataConnector = InferredAssetGCSDataConnector(
+        name="my_data_connector",
+        datasource_name="FAKE_DATASOURCE_NAME",
+        default_regex={
+            "pattern": r"(\d{4})/(\d{2})/(.+)-\d+\.csv",
+            "group_names": ["year_dir", "month_dir", "data_asset_name"],
+        },
+        bucket_or_name="test_bucket",
+        prefix="",
+    )
 
-#     test_df: pd.DataFrame = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+    my_data_connector._refresh_data_references_cache()
 
-#     keys: List[str] = [
-#         "A-100.csv",
-#         "A-101.csv",
-#         "B-1.csv",
-#         "B-2.csv",
-#     ]
-#     for key in keys:
-#         client.put_object(
-#             Bucket=bucket, Body=test_df.to_csv(index=False).encode("utf-8"), Key=key
-#         )
+    # Test for an unknown execution environment
+    with pytest.raises(ValueError):
+        my_data_connector.get_batch_definition_list_from_batch_request(
+            batch_request=BatchRequest(
+                datasource_name="non_existent_datasource",
+                data_connector_name="my_data_connector",
+                data_asset_name="my_data_asset",
+            )
+        )
 
-#     my_data_connector: InferredAssetS3DataConnector = InferredAssetS3DataConnector(
-#         name="my_data_connector",
-#         datasource_name="FAKE_DATASOURCE_NAME",
-#         default_regex={
-#             "pattern": r"(.+)-(\d+)\.csv",
-#             "group_names": ["data_asset_name", "number"],
-#         },
-#         bucket=bucket,
-#         prefix="",
-#     )
 
-#     # noinspection PyProtectedMember
-#     my_data_connector._refresh_data_references_cache()
+@mock.patch(
+    "great_expectations.core.usage_statistics.usage_statistics.UsageStatisticsHandler.emit"
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.list_gcs_keys",
+    return_value=[
+        "2020/01/alpha-1001.csv",
+        "2020/01/beta-1002.csv",
+        "2020/02/alpha-1003.csv",
+        "2020/02/beta-1004.csv",
+        "2020/03/alpha-1005.csv",
+        "2020/03/beta-1006.csv",
+        "2020/04/beta-1007.csv",
+    ],
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.storage.Client"
+)
+def test_get_batch_definition_list_from_batch_request_with_unknown_data_connector_raises_error(
+    mock_gcs_conn, mock_list_keys, mock_emit
+):
+    my_data_connector: InferredAssetGCSDataConnector = InferredAssetGCSDataConnector(
+        name="my_data_connector",
+        datasource_name="FAKE_DATASOURCE_NAME",
+        default_regex={
+            "pattern": r"(\d{4})/(\d{2})/(.+)-\d+\.csv",
+            "group_names": ["year_dir", "month_dir", "data_asset_name"],
+        },
+        bucket_or_name="test_bucket",
+        prefix="",
+    )
 
-#     self_check_report_object = my_data_connector.self_check()
+    my_data_connector._refresh_data_references_cache()
 
-#     assert self_check_report_object == {
-#         "class_name": "InferredAssetS3DataConnector",
-#         "data_asset_count": 2,
-#         "example_data_asset_names": ["A", "B"],
-#         "data_assets": {
-#             "A": {
-#                 "example_data_references": ["A-100.csv", "A-101.csv"],
-#                 "batch_definition_count": 2,
-#             },
-#             "B": {
-#                 "example_data_references": ["B-1.csv", "B-2.csv"],
-#                 "batch_definition_count": 2,
-#             },
-#         },
-#         "example_unmatched_data_references": [],
-#         "unmatched_data_reference_count": 0,
-#         # FIXME: (Sam) example_data_reference removed temporarily in PR #2590:
-#         # "example_data_reference": {},
-#     }
+    # Test for an unknown data_connector
+    with pytest.raises(ValueError):
+        my_data_connector.get_batch_definition_list_from_batch_request(
+            batch_request=BatchRequest(
+                datasource_name="FAKE_DATASOURCE_NAME",
+                data_connector_name="non_existent_data_connector",
+                data_asset_name="my_data_asset",
+            )
+        )
+
+
+@mock.patch(
+    "great_expectations.core.usage_statistics.usage_statistics.UsageStatisticsHandler.emit"
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.list_gcs_keys",
+    return_value=[
+        "A-100.csv",
+        "A-101.csv",
+        "B-1.csv",
+        "B-2.csv",
+    ],
+)
+@mock.patch(
+    "great_expectations.datasource.data_connector.inferred_asset_gcs_data_connector.storage.Client"
+)
+def test_self_check(mock_gcs_conn, mock_list_keys, mock_emit):
+    my_data_connector: InferredAssetGCSDataConnector = InferredAssetGCSDataConnector(
+        name="my_data_connector",
+        datasource_name="FAKE_DATASOURCE_NAME",
+        default_regex={
+            "pattern": r"(.+)-(\d+)\.csv",
+            "group_names": ["data_asset_name", "number"],
+        },
+        bucket_or_name="test_bucket",
+        prefix="",
+    )
+
+    my_data_connector._refresh_data_references_cache()
+    self_check_report_object = my_data_connector.self_check()
+
+    assert self_check_report_object == {
+        "class_name": "InferredAssetGCSDataConnector",
+        "data_asset_count": 2,
+        "example_data_asset_names": ["A", "B"],
+        "data_assets": {
+            "A": {
+                "example_data_references": ["A-100.csv", "A-101.csv"],
+                "batch_definition_count": 2,
+            },
+            "B": {
+                "example_data_references": ["B-1.csv", "B-2.csv"],
+                "batch_definition_count": 2,
+            },
+        },
+        "example_unmatched_data_references": [],
+        "unmatched_data_reference_count": 0,
+    }
 
 
 # @mock.patch(
