787241d99644653a03edc338c638ea226385f694
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2021-08-18 23:57:47 -0700

    [MAINTENANCE] Allow Wrong ignore_row_if Directive from V2 with Deprecation Warning (#3274)
    * Deprecation warning for the incorrect use of the ignore_row_if directive.

--- great_expectations/expectations/core/expect_column_pair_values_to_be_equal.py
+++ great_expectations/expectations/core/expect_column_pair_values_to_be_equal.py
@@ -65,8 +65,8 @@ class ExpectColumnPairValuesToBeEqual(ColumnPairMapExpectation):
     default_kwarg_values = {
         "row_condition": None,
         "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
-        "mostly": 1.0,
         "ignore_row_if": "both_values_are_missing",
+        "mostly": 1.0,
         "result_format": "BASIC",
         "include_config": True,
         "catch_exceptions": False,
