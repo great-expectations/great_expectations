84f1222d1010e581a84d3207240239e1427d04ee
Chetan Kini <chetan@superconductive.com>
2021-08-23 14:47:04 -0400

    test: Write Azure tests for Configured

--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -38,11 +38,11 @@ docs_test_matrix = [
         "extra_backend_dependencies": BackendDependencies.BIGQUERY,
     },
     {
-        "user_flow_script": "tests/integration/docusaurus/connecting_to_your_data/cloud/azure/pandas/yaml_example.py",
+        "user_flow_script": "tests/integration/docusaurus/connecting_to_your_data/cloud/azure/pandas/configured_yaml_example.py",
         "data_context_dir": "tests/integration/fixtures/no_datasources/great_expectations",
     },
     {
-        "user_flow_script": "tests/integration/docusaurus/connecting_to_your_data/cloud/azure/pandas/python_example.py",
+        "user_flow_script": "tests/integration/docusaurus/connecting_to_your_data/cloud/azure/pandas/configured_python_example.py",
         "data_context_dir": "tests/integration/fixtures/no_datasources/great_expectations",
     },
     # {
