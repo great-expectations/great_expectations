de2b4d1445f206e43f808e1c9df8acb4298e548e
Nathan Farmer <nathan@superconductive.com>
2021-09-01 20:14:14 -0400

    Move minimal additonal functionality from override method into inherited class DataConnector

--- great_expectations/datasource/data_connector/data_connector.py
+++ great_expectations/datasource/data_connector/data_connector.py
@@ -82,6 +82,7 @@ class DataConnector:
     def get_batch_data_and_metadata(
         self,
         batch_definition: BatchDefinition,
+        runtime_parameters: Optional[dict] = None
     ) -> Tuple[Any, BatchSpec, BatchMarkers,]:  # batch_data
         """
         Uses batch_definition to retrieve batch_data and batch_markers by building a batch_spec from batch_definition,
@@ -91,7 +92,13 @@ class DataConnector:
             batch_definition (BatchDefinition): required batch_definition parameter for retrieval
 
         """
-        batch_spec: BatchSpec = self.build_batch_spec(batch_definition=batch_definition)
+        if runtime_parameters:
+            batch_spec: BatchSpec = self.build_batch_spec(
+                batch_definition=batch_definition,
+                runtime_parameters=runtime_parameters,
+            )
+        else:
+            batch_spec: BatchSpec = self.build_batch_spec(batch_definition=batch_definition)
         batch_data, batch_markers = self._execution_engine.get_batch_data_and_markers(
             batch_spec=batch_spec
         )
--- great_expectations/datasource/data_connector/runtime_data_connector.py
+++ great_expectations/datasource/data_connector/runtime_data_connector.py
@@ -114,26 +114,6 @@ class RuntimeDataConnector(DataConnector):
         """Please see note in : _get_batch_definition_list_from_batch_request()"""
         return list(self._data_references_cache.keys())
 
-    # noinspection PyMethodOverriding
-    def get_batch_data_and_metadata(
-        self,
-        batch_definition: BatchDefinition,
-        runtime_parameters: dict,
-    ) -> Tuple[Any, BatchSpec, BatchMarkers,]:  # batch_data
-        batch_spec: RuntimeDataBatchSpec = self.build_batch_spec(
-            batch_definition=batch_definition,
-            runtime_parameters=runtime_parameters,
-        )
-        batch_data, batch_markers = self._execution_engine.get_batch_data_and_markers(
-            batch_spec=batch_spec
-        )
-        self._execution_engine.load_batch_data(batch_definition.id, batch_data)
-        return (
-            batch_data,
-            batch_spec,
-            batch_markers,
-        )
-
     def get_batch_definition_list_from_batch_request(
         self,
         batch_request: RuntimeBatchRequest,
