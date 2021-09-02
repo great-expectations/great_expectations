b2a534d6b7fb7ead8ab7d590bbecfa35615c513a
Tal Gluck <tal@superconductive.com>
2021-09-01 22:48:48 -0400

    modernized test structures

--- tests/expectations/core/test_expect_column_value_z_scores_to_be_less_than.py
+++ tests/expectations/core/test_expect_column_value_z_scores_to_be_less_than.py
@@ -1,25 +1,14 @@
 import pandas as pd
 import pytest
 
-from great_expectations.core.expectation_configuration import ExpectationConfiguration
 from great_expectations.core.expectation_validation_result import (
     ExpectationValidationResult,
-)
-from great_expectations.core.util import get_or_create_spark_application
-from great_expectations.execution_engine import PandasExecutionEngine
-from great_expectations.execution_engine.sqlalchemy_execution_engine import (
-    SqlAlchemyBatchData,
-    SqlAlchemyExecutionEngine,
-)
-from great_expectations.expectations.core.expect_column_value_z_scores_to_be_less_than import (
-    ExpectColumnValueZScoresToBeLessThan,
 )
 from great_expectations.self_check.util import (
     build_pandas_validator_with_data,
     build_sa_validator_with_data,
     build_spark_validator_with_data,
 )
-from great_expectations.validator.validator import Validator
 
 
 @pytest.fixture
