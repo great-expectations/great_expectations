dbe33ad824215c7a359ce15f3474bdf6b83b2feb
Robert Moses Lim <robert.m.lim@gmail.com>
2021-09-02 20:30:51 -0700

    Nix superceded code

--- great_expectations/data_context/data_context.py
+++ great_expectations/data_context/data_context.py
@@ -3970,40 +3970,6 @@ class DataContext(BaseDataContext):
 
         return GeCloudConfig(**ge_cloud_config_dict)
 
-    @classmethod
-    def build_ge_cloud_data_context_store(
-        cls, ge_cloud_base_url, ge_cloud_account_id, ge_cloud_access_token
-    ):
-        store_config = {
-            "class_name": "DataContextStore",
-            "store_backend": {
-                "class_name": "GeCloudStoreBackend",
-                "ge_cloud_base_url": ge_cloud_base_url,
-                "ge_cloud_resource_type": "data_context",
-                "ge_cloud_credentials": {
-                    "access_token": ge_cloud_access_token,
-                    "account_id": ge_cloud_account_id,
-                },
-                "suppress_store_backend_id": True,
-            },
-        }
-        return build_store_from_config(
-            store_name="data_context_store",
-            store_config=store_config,
-        )
-
-    @property
-    def ge_cloud_data_context_store(self):
-        if not self._ge_cloud_data_context_store:
-            data_context_store = self.build_ge_cloud_data_context_store(
-                ge_cloud_base_url=self.ge_cloud_config.ge_cloud_base_url,
-                ge_cloud_account_id=self.ge_cloud_config.ge_cloud_account_id,
-                ge_cloud_access_token=self.ge_cloud_config.ge_cloud_access_token,
-            )
-            self._ge_cloud_data_context_store = data_context_store
-            return data_context_store
-        return self._ge_cloud_data_context_store
-
     def __init__(
         self,
         context_root_dir: Optional[str] = None,
