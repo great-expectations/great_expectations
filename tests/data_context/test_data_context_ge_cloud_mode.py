3073da9f5c91b4d5aebdd2a0d4f04cdaa5bffd42
Robert Moses Lim <robert.m.lim@gmail.com>
2021-09-02 23:56:36 -0700

    Add test stubs

--- tests/data_context/test_data_context_ge_cloud_mode.py
+++ tests/data_context/test_data_context_ge_cloud_mode.py
@@ -7,10 +7,25 @@ from great_expectations.data_context import DataContext
 from great_expectations.data_context.util import file_relative_path
 
 
-def test_data_context_ge_cloud_mode_runtime_config():
-    pass
-    # context = DataContext(
-    #     ge_cloud_mode=True,
-    #     ge_cloud_base_url=,
-    #     ge_cloud_account_id=
-    # )
+def test_data_context_ge_cloud_mode_with_runtime_cloud_config():
+    context = DataContext(
+        ge_cloud_mode=True,
+        ge_cloud_base_url="",
+        ge_cloud_account_id=""
+    )
+
+
+def test_data_context_ge_cloud_mode_with_env_var_cloud_config():
+    context = DataContext(ge_cloud_mode=True)
+
+
+def test_data_context_ge_cloud_mode_with_conf_file_cloud_config():
+    context = DataContext(ge_cloud_mode=True)
+
+
+def test_data_context_ge_cloud_mode_mixed_cloud_config_precedence():
+    context = DataContext(ge_cloud_mode=True)
+
+
+def test_data_context_ge_cloud_mode_with_incomplete_cloud_config():
+    context = DataContext(ge_cloud_mode=True)
