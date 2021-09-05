72f65c3abfa7477207e81b468930c56d103fcd5c
John DiMatteo <jdimatteo@gmail.com>
2021-09-05 17:53:47 -0600

    hacked in unlimitted python requests pool size

--- tests/performance/test_bigquery_benchmarks.py
+++ tests/performance/test_bigquery_benchmarks.py
@@ -19,6 +19,29 @@ from great_expectations.checkpoint.types.checkpoint_result import CheckpointResu
 from tests.performance import taxi_benchmark_util
 
 
+# todo(jdimatteo) this should probably be removed
+def patch_https_connection_pool(**constructor_kwargs):
+    """
+    This allows to override the default parameters of the
+    HTTPConnectionPool constructor.
+    For example, to increase the poolsize to fix problems
+    with "HttpSConnectionPool is full, discarding connection"
+    call this function with maxsize=16 (or whatever size
+    you want to give to the connection pool)
+    """
+    from urllib3 import connectionpool, poolmanager
+
+    class MyHTTPSConnectionPool(connectionpool.HTTPSConnectionPool):
+        def __init__(self, *args, **kwargs):
+            kwargs.update(constructor_kwargs)
+            super().__init__(*args, **kwargs)
+
+    poolmanager.pool_classes_by_scheme["https"] = MyHTTPSConnectionPool
+
+
+patch_https_connection_pool(maxsize=100)
+
+
 @pytest.mark.parametrize(
     "backend_api",
     [
