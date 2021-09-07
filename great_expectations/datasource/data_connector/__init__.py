8aed0612bfd3868c44c8a41f8031267f4af783b0
fjork3 <horkley@superconductive.com>
2021-09-07 17:38:37 -0400

    lint (imports)

--- great_expectations/datasource/data_connector/__init__.py
+++ great_expectations/datasource/data_connector/__init__.py
@@ -1,20 +1,20 @@
+from .configured_asset_azure_data_connector import ConfiguredAssetAzureDataConnector
 from .configured_asset_file_path_data_connector import (
     ConfiguredAssetFilePathDataConnector,
 )
 from .configured_asset_filesystem_data_connector import (
     ConfiguredAssetFilesystemDataConnector,
 )
-from .configured_asset_azure_data_connector import ConfiguredAssetAzureDataConnector
 from .configured_asset_gcs_data_connector import ConfiguredAssetGCSDataConnector
 from .configured_asset_s3_data_connector import ConfiguredAssetS3DataConnector
 from .configured_asset_sql_data_connector import ConfiguredAssetSqlDataConnector
 from .data_connector import DataConnector
 from .file_path_data_connector import FilePathDataConnector
+from .inferred_asset_azure_data_connector import InferredAssetAzureDataConnector
 from .inferred_asset_file_path_data_connector import InferredAssetFilePathDataConnector
 from .inferred_asset_filesystem_data_connector import (
     InferredAssetFilesystemDataConnector,
 )
-from .inferred_asset_azure_data_connector import InferredAssetAzureDataConnector
 from .inferred_asset_gcs_data_connector import InferredAssetGCSDataConnector
 from .inferred_asset_s3_data_connector import InferredAssetS3DataConnector
 from .inferred_asset_sql_data_connector import InferredAssetSqlDataConnector
