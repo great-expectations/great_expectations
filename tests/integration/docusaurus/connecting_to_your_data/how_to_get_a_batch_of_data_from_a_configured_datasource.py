5fd211916912f6d001d119b4cc23621bbe792007
Anthony Burdi <anthony@superconductive.com>
2021-09-09 19:15:43 -0400

    Add limit param to doc script

--- tests/integration/docusaurus/connecting_to_your_data/how_to_get_a_batch_of_data_from_a_configured_datasource.py
+++ tests/integration/docusaurus/connecting_to_your_data/how_to_get_a_batch_of_data_from_a_configured_datasource.py
@@ -6,15 +6,6 @@ from great_expectations.validator.validation_graph import MetricConfiguration
 
 context = ge.get_context()
 
-docs_test_matrix = [
-    {
-        "name": "how_to_get_a_batch_of_data_from_a_configured_datasource",
-        "user_flow_script": "tests/integration/docusaurus/connecting_to_your_data/how_to_get_a_batch_of_data_from_a_configured_datasource.py",
-        "data_context_dir": "tests/integration/fixtures/yellow_trip_data_pandas_fixture/great_expectations",
-        "data_dir": "tests/test_sets/taxi_yellow_trip_data_samples",
-    },
-]
-
 # Please note the naming of this datasource is only to provide good UX for docs and tests.
 datasource_yaml = fr"""
 name: insert_your_datasource_name_here
@@ -66,6 +57,20 @@ batch_request_2020 = BatchRequest(
 )
 # NOTE: The following assertion is only for testing and can be ignored by users.
 assert len(context.get_batch_list(batch_request=batch_request_2020)) == 12
+
+# This BatchRequest adds a query to retrieve only the first 5 batches from 2020
+data_connector_query_2020 = {
+    "batch_filter_parameters": {"param_1_from_your_data_connector_eg_year": "2020"}
+}
+batch_request_2020 = BatchRequest(
+    datasource_name="insert_your_datasource_name_here",
+    data_connector_name="insert_your_data_connector_name_here",
+    data_asset_name="insert_your_data_asset_name_here",
+    data_connector_query=data_connector_query_2020,
+    limit=5,
+)
+# NOTE: The following assertion is only for testing and can be ignored by users.
+assert len(context.get_batch_list(batch_request=batch_request_2020)) == 5
 
 # Here is an example `data_connector_query` filtering based on parameters from `group_names`
 # previously defined in a regex pattern in your Data Connector:
@@ -129,13 +134,15 @@ batch_list_last_index_batch_request = context.get_batch_list(
 assert len(batch_list_last_index_batch_request) == 1
 
 # Or limit to a specific number of batches
-batch_list_all_limit_10 = context.get_batch_list(batch_request=batch_request, limit=10)
-# TODO: Fix limit param and un-comment out this assertion
-# assert len(batch_list_all_limit_10) == 10
+batch_list_all_limit_10 = context.get_batch_list(
+    datasource_name="insert_your_datasource_name_here",
+    data_connector_name="insert_your_data_connector_name_here",
+    data_asset_name="insert_your_data_asset_name_here",
+    limit=10,
+)
+assert len(batch_list_all_limit_10) == 10
 
-
-# TODO: CLEAN UP THE BELOW vvvv
-
+# Now we can review a sample of data using a Validator
 context.create_expectation_suite(
     expectation_suite_name="test_suite", overwrite_existing=True
 )
@@ -146,18 +153,14 @@ print(validator.head())
 
 
 # NOTE: The following assertions are only for testing and can be ignored by users.
+assert validator.active_batch_id == batch_list_last_index_batch_request[0].id
+
 row_count = validator.get_metric(
     MetricConfiguration(
-        "table.row_count",
-        # metric_domain_kwargs={"batch_id": validator.active_batch_id}
-        metric_domain_kwargs={"batch_id": batch_list_last_index_batch_request[0].id},
+        "table.row_count", metric_domain_kwargs={"batch_id": validator.active_batch_id}
     )
 )
-# TODO: uncomment and clean up when limit is fixed
-# assert row_count == limit, f"row_count: {row_count}"
-
-# NOTE: The following assertions are only for testing and can be ignored by users.
-assert validator.active_batch_id == batch_list_last_index_batch_request[0].id
+assert row_count == 10000
 
 assert (
     validator.active_batch.batch_definition.batch_identifiers["name"]
