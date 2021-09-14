4da1d90ce38415e0923b08745d965f60093f1eda
Robert Moses Lim <robert.m.lim@gmail.com>
2021-09-14 16:29:00 -0400

    Handle case where suite is str

--- great_expectations/data_context/store/expectations_store.py
+++ great_expectations/data_context/store/expectations_store.py
@@ -1,3 +1,4 @@
+import json
 import random
 import uuid
 from typing import Dict
@@ -161,6 +162,8 @@ class ExpectationsStore(Store):
         """
         ge_cloud_expectation_suite_id = response_json["data"]["id"]
         expectation_suite_dict = response_json["data"]["attributes"]["suite"]
+        if isinstance(expectation_suite_dict, str):
+            expectation_suite_dict = json.loads(expectation_suite_dict)
         expectation_suite_dict["ge_cloud_id"] = ge_cloud_expectation_suite_id
 
         return expectation_suite_dict
