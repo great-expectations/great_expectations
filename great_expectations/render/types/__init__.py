dfa0f91da77beb01f366deba0d8009b6fac3eebf
Robert Moses Lim <robert.m.lim@gmail.com>
2021-10-22 17:12:09 -0700

    Change to snake case

--- great_expectations/render/types/__init__.py
+++ great_expectations/render/types/__init__.py
@@ -506,11 +506,11 @@ class RenderedAtomicContent(RenderedContent):
     ):
         self.name = name
         self.value = value
-        self.valuetype = valuetype
+        self.value_type = value_type
 
     def to_json_dict(self):
         d = super().to_json_dict()
         d["name"] = self.name
         d["value"] = self.value.__dict__
-        d["valuetype"] = self.valuetype
+        d["value_type"] = self.value_type
         return d
