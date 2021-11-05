5d7537da8ab6bea2d3d5b95a930fb7b98539596b
Anthony Burdi <anthony@superconductive.com>
2021-11-05 19:27:18 -0400

    Change inheritance: ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilesystemDataConnector)

--- great_expectations/datasource/data_connector/configured_asset_dbfs_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_dbfs_data_connector.py
@@ -9,49 +9,19 @@ from great_expectations.core.batch import BatchDefinition
 from great_expectations.core.batch_spec import PathBatchSpec
 from great_expectations.datasource.data_connector import (
     ConfiguredAssetFilePathDataConnector,
+    ConfiguredAssetFilesystemDataConnector,
 )
 from great_expectations.datasource.data_connector.asset import Asset
 from great_expectations.datasource.data_connector.util import (
     get_filesystem_one_level_directory_glob_path_list,
     normalize_directory_path,
 )
-from great_expectations.execution_engine import (
-    ExecutionEngine,
-    PandasExecutionEngine,
-    SparkDFExecutionEngine,
-)
+from great_expectations.execution_engine import ExecutionEngine
 
 logger = logging.getLogger(__name__)
 
 
-# TODO: AJB Alex: make this the same as AzureDataConnector and GCSDataConnector, S3DataConnector
-#  i.e. copy those classes and override the same methods they do with my conversion from /dbfs to dbfs:
-# TODO: AJB Inherit from ConfiguredAssetFilePathDataConnector or inferred version
-# class ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilesystemDataConnector):
-#
-#     @staticmethod
-#     def _convert_path_to_dbfs_root(path: str) -> str:
-#
-#         # TODO: AJB Strengthen with regex to make sure there are no preceding tabs etc
-#         if path.startswith("/dbfs"):
-#             path = path.replace("/dbfs/foldername/", "dbfs:/foldername/file.csv", 1)
-#             # os.listdir("/dbfs")
-#         return path
-#
-#     def _generate_batch_spec_parameters_from_batch_definition(
-#             self, batch_definition: BatchDefinition
-#     ) -> dict:
-#
-#         unmodified_path = super()._generate_batch_spec_parameters_from_batch_definition(
-#             batch_definition=batch_definition
-#         )
-#
-#         modified_path = self._convert_path_to_dbfs_root(unmodified_path["path"])
-#
-#         return {"path": modified_path}
-
-
-class ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilePathDataConnector):
+class ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilesystemDataConnector):
     """
     Extension of ConfiguredAssetFilePathDataConnector used to connect to the DataBricks File System (DBFS)
 
@@ -98,45 +68,20 @@ class ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilePathDataConnector):
         super().__init__(
             name=name,
             datasource_name=datasource_name,
-            execution_engine=execution_engine,
+            base_directory=base_directory,
             assets=assets,
+            execution_engine=execution_engine,
             default_regex=default_regex,
+            glob_directive=glob_directive,
             sorters=sorters,
             batch_spec_passthrough=batch_spec_passthrough,
         )
 
-        self._base_directory = base_directory
-        self._glob_directive = glob_directive
-
-    def _get_data_reference_list_for_asset(self, asset: Optional[Asset]) -> List[str]:
-        base_directory: str = self.base_directory
-        glob_directive: str = self._glob_directive
-
-        if asset is not None:
-            if asset.base_directory:
-                base_directory = normalize_directory_path(
-                    dir_path=asset.base_directory, root_directory_path=base_directory
-                )
-            if asset.glob_directive:
-                glob_directive = asset.glob_directive
-
-        path_list: List[str] = get_filesystem_one_level_directory_glob_path_list(
-            base_directory_path=base_directory, glob_directive=glob_directive
-        )
-
-        return sorted(path_list)
-
     def _get_full_file_path_for_asset(
         self, path: str, asset: Optional[Asset] = None
     ) -> str:
-        base_directory: str = self.base_directory
-        if asset is not None:
-            if asset.base_directory:
-                base_directory = normalize_directory_path(
-                    dir_path=asset.base_directory,
-                    root_directory_path=base_directory,
-                )
-        full_path = str(Path(base_directory).joinpath(path))
+
+        full_path = super()._get_full_file_path_for_asset(path=path, asset=asset)
 
         template_arguments: dict = {
             "path": full_path,
@@ -151,14 +96,3 @@ class ConfiguredAssetDBFSDataConnector(ConfiguredAssetFilePathDataConnector):
             raise ge_exceptions.DataConnectorError(
                 "A non-existent/unknown ExecutionEngine instance was referenced."
             )
-
-    @property
-    def base_directory(self) -> str:
-        """
-        Accessor method for base_directory. If directory is a relative path, interpret it as relative to the
-        root directory. If it is absolute, then keep as-is.
-        """
-        return normalize_directory_path(
-            dir_path=self._base_directory,
-            root_directory_path=self.data_context_root_directory,
-        )
