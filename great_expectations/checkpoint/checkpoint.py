b7dd5ae6ed83d93a951fd750c02abff2d6e21262
Nathan Farmer <nathan@superconductive.com>
2021-11-19 12:51:02 -0500

    Convert BatchRequests to dict

--- great_expectations/checkpoint/checkpoint.py
+++ great_expectations/checkpoint/checkpoint.py
@@ -344,7 +344,7 @@ class Checkpoint:
         template_name: Optional[str] = None,
         run_name_template: Optional[str] = None,
         expectation_suite_name: Optional[str] = None,
-        batch_request: Optional[dict] = None,
+        batch_request: Optional[Union[dict, BatchRequest]] = None,
         action_list: Optional[List[dict]] = None,
         evaluation_parameters: Optional[dict] = None,
         runtime_configuration: Optional[dict] = None,
@@ -366,6 +366,37 @@ class Checkpoint:
         result_format: Optional[dict] = result_format or runtime_configuration.get(
             "result_format"
         )
+
+        if isinstance(batch_request, BatchRequest):
+            if batch_request.runtime_parameters.get("batch_data") is not None:
+                batch_data = batch_request.runtime_parameters.get("batch_data")
+                batch_request = batch_request.to_json_dict()
+                batch_request["runtime_parameters"]["batch_data"] = batch_data
+            else:
+                batch_request = batch_request.to_json_dict()
+
+        if validations:
+            for val in validations:
+                if val.get("batch_request") is not None and isinstance(
+                        val["batch_request"], BatchRequest
+                ):
+                    if (
+                            val["batch_request"].runtime_parameters is not None
+                            and val["batch_request"].runtime_parameters.get(
+                        "batch_data"
+                    )
+                            is not None
+                    ):
+                        batch_data = val["batch_request"].runtime_parameters.get(
+                            "batch_data"
+                        )
+                        val["batch_request"] = val["batch_request"].to_json_dict()
+                        val["batch_request"]["runtime_parameters"][
+                            "batch_data"
+                        ] = batch_data
+                    else:
+                        val["batch_request"] = val["batch_request"].to_json_dict()
+
 
         runtime_kwargs = {
             "template_name": template_name,
