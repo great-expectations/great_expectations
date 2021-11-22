9be488a89e63988b6954ced4e663c72031bbf2fd
Nathan Farmer <nathan@superconductive.com>
2021-11-21 21:13:18 -0500

    Imitate failing integration test

--- tests/checkpoint/test_simple_checkpoint.py
+++ tests/checkpoint/test_simple_checkpoint.py
@@ -1089,10 +1089,9 @@ def test_simple_checkpoint_instantiates_and_produces_a_validation_result_when_ru
                 },
             },
         ],
-        validations=[{"batch_request": batch_request}],
     )
 
-    results = checkpoint.run()
+    results = checkpoint.run(validations=[{"batch_request": batch_request}],)
 
     assert len(context.validations_store.list_keys()) == 1
     assert results["success"] == True
