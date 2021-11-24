4dae6ba5572b59a4c68a083e6c444cfe403f2d6c
Amol Patil <85225591+imamolp@users.noreply.github.com>
2021-11-24 10:33:57 +0530

    Update great_expectations/execution_engine/sqlalchemy_batch_data.py
    Co-authored-by: Chetan Kini <ckini123@gmail.com>

--- great_expectations/execution_engine/sqlalchemy_batch_data.py
+++ great_expectations/execution_engine/sqlalchemy_batch_data.py
@@ -250,6 +250,7 @@ class SqlAlchemyBatchData(BatchData):
             stmt_2 = "CREATE GLOBAL TEMPORARY TABLE {temp_table_name} ON COMMIT PRESERVE ROWS AS {query}".format(
                 temp_table_name=temp_table_name, query=query
             )
+        # Please note that Teradata is currently experimental (as of 0.13.43)
         elif self.sql_engine_dialect.name.lower() == "teradatasql":
             stmt = 'CREATE VOLATILE TABLE "{temp_table_name}" AS ({query}) WITH DATA NO PRIMARY INDEX ON COMMIT PRESERVE ROWS'.format(
                 temp_table_name=temp_table_name, query=query
