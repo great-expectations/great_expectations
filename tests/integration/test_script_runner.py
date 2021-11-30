ae14d0adaac3b6e6e594f7e17c75c99cf5495ec3
Nathan Farmer <nathan@superconductive.com>
2021-11-30 09:55:09 -0500

    Add new integration test file to test_script_runner.py

--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -374,6 +374,11 @@ docs_test_matrix = [
         "data_dir": "tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
         "extra_backend_dependencies": BackendDependencies.SPARK,
     },
+    {
+        "user_flow_script": "tests/integration/docusaurus/reference/core_concepts/checkpoints_and_actions.py",
+        "data_context_dir": "tests/integration/fixtures/no_datasources/great_expectations",
+        "data_dir": "tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
+    },
 ]
 
 integration_test_matrix = [
