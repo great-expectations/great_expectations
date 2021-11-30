65da9bad6abf23e427219bcc2131cdbc3d091f5e
Nathan Farmer <nathan@superconductive.com>
2021-11-30 15:42:18 -0500

    Lint

--- docs/guides/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.md
+++ docs/guides/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.md
@@ -146,7 +146,7 @@ df_1 = pd.read_csv("<PATH TO DATA 1>")
 df_2 = pd.read_csv("<PATH TO DATA 2>")
 ```
 
-```python file=../../../../tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py#L148-L170
+```python file=../../../../tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py#L150-L172
 ```
 
 ### Additional Notes
--- tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py
+++ tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py
@@ -140,7 +140,9 @@ test_python = context.test_yaml_config(
 
 # NOTE: The following code is only for testing and can be ignored by users.
 assert test_yaml == test_python
-assert set(context.list_checkpoints()) == set(["my_missing_keys_checkpoint", "my_missing_batch_request_checkpoint"])
+assert set(context.list_checkpoints()) == set(
+    ["my_missing_keys_checkpoint", "my_missing_batch_request_checkpoint"]
+)
 
 df_1 = pd.read_csv("./data/yellow_tripdata_sample_2019-01.csv")
 df_2 = pd.read_csv("./data/yellow_tripdata_sample_2019-02.csv")
