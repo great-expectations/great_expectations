eb8c4c4ad5352a609e8bd1d50b9fbbb571efab54
Chetan Kini <chetan@superconductive.com>
2021-12-08 09:57:10 -0500

    feat: Use JSON loads/dumps to clean code cell

--- great_expectations/render/renderer/notebook_renderer.py
+++ great_expectations/render/renderer/notebook_renderer.py
@@ -1,10 +1,11 @@
+import json
 from typing import Optional
 
 import nbformat
 
 from great_expectations import DataContext
 from great_expectations.render.renderer.renderer import Renderer
-from great_expectations.util import convert_nulls_to_None, lint_code
+from great_expectations.util import lint_code
 
 
 class BaseNotebookRenderer(Renderer):
@@ -19,9 +20,7 @@ class BaseNotebookRenderer(Renderer):
         # self.render() and/or self.render_to_disk() method(s):
         self._notebook: Optional[nbformat.NotebookNode] = None
 
-    def add_code_cell(
-        self, code: str, lint: bool = False, sanitize_nulls: bool = True
-    ) -> None:
+    def add_code_cell(self, code: str, lint: bool = False) -> None:
         """
         Add the given code as a new code cell.
         Args:
@@ -31,11 +30,12 @@ class BaseNotebookRenderer(Renderer):
         Returns:
             Nothing, adds a cell to the class instance notebook
         """
-        if sanitize_nulls:
-            code = convert_nulls_to_None(code)
-
         if lint:
             code = lint_code(code).rstrip("\n")
+
+        # Sanitize cell contents of any nulls
+        code = json.loads(code)
+        code = json.dumps(code)
 
         cell = nbformat.v4.new_code_cell(code)
         self._notebook["cells"].append(cell)
