8f2b044591d5a62c7659ea62abc9703f45c5ea2b
Alex Sherstinsky <alex@superconductive.com>
2021-12-10 17:15:30 -0800

    remove DataContext registry

--- great_expectations/data_context/data_context.py
+++ great_expectations/data_context/data_context.py
@@ -395,7 +395,6 @@ class BaseDataContext:
 
         self._evaluation_parameter_dependencies_compiled = False
         self._evaluation_parameter_dependencies = {}
-        BaseDataContext.set_data_context(self)
 
     @property
     def ge_cloud_config(self):
