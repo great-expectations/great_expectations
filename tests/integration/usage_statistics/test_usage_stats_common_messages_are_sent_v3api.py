9c1dc96e358cbb0a6436b6aab7ec9658891eab5a
Anthony Burdi <anthony@superconductive.com>
2021-12-15 18:46:13 -0500

    Some temporary investigatory steps

--- tests/integration/usage_statistics/test_usage_stats_common_messages_are_sent_v3api.py
+++ tests/integration/usage_statistics/test_usage_stats_common_messages_are_sent_v3api.py
@@ -1,5 +1,9 @@
+import atexit
 import logging
 import os
+import threading
+import time
+from typing import List
 
 import pandas as pd
 import pytest
@@ -8,7 +12,10 @@ from ruamel import yaml
 from great_expectations.core.batch import RuntimeBatchRequest
 from great_expectations.data_context import BaseDataContext
 from great_expectations.data_context.types.base import DataContextConfig
-from tests.core.usage_statistics.util import assert_no_usage_stats_exceptions
+from tests.core.usage_statistics.util import (
+    assert_no_usage_stats_exceptions,
+    usage_stats_invalid_messages_exist,
+)
 from tests.integration.usage_statistics.test_integration_usage_statistics import (
     USAGE_STATISTICS_QA_URL,
 )
@@ -23,7 +30,7 @@ def in_memory_data_context_config_usage_stats_enabled():
     return DataContextConfig(
         **{
             "commented_map": {},
-            "config_version": 2,
+            "config_version": 3,
             "plugins_directory": None,
             "evaluation_parameter_store_name": "evaluation_parameter_store",
             "validations_store_name": "validations_store",
@@ -85,6 +92,23 @@ def test_common_usage_stats_are_sent_no_mocking(
         in_memory_data_context_config_usage_stats_enabled
     )
 
+    # def slow_print():
+    #     print("sleeping...")
+    #     time.sleep(5)
+    #     print("waking up...")
+
+    # Pause sending messages
+    # context._usage_statistics_handler._worker = None
+    # context._usage_statistics_handler._requests_worker = None
+    # context._usage_statistics_handler._worker = threading.Thread(target=slow_print, daemon=True)
+    # atexit.unregister(context._usage_statistics_handler._close_worker)
+
+    context._usage_statistics_handler._dry_run = True
+    assert context._usage_statistics_handler._dry_run
+
+    # wait_event = threading.Event()
+    # wait_event.wait()
+
     # Make sure usage stats are enabled
     assert not context._check_global_usage_statistics_opt_out()
     assert context.anonymous_usage_statistics.enabled
@@ -110,8 +134,10 @@ def test_common_usage_stats_are_sent_no_mocking(
 
     # context.test_yaml_config() uses send_usage_message()
     context.test_yaml_config(yaml_config=datasource_yaml)
+    expected_events: List[str] = ["data_context.test_yaml_config"]
 
     context.add_datasource(**yaml.load(datasource_yaml))
+    expected_events.append("data_context.add_datasource")
 
     df = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
 
@@ -157,3 +183,48 @@ def test_common_usage_stats_are_sent_no_mocking(
     )
 
     assert_no_usage_stats_exceptions(messages=caplog.messages)
+
+    print("Message Queue")
+    # print(context._usage_statistics_handler._message_queue.queue)
+    message_queue = context._usage_statistics_handler._message_queue.queue
+    print("len(message_queue):", len(message_queue))
+    for event in message_queue:
+        print(event["event"])
+    events = [event["event"] for event in message_queue]
+    print(events)
+    print("Message Queue")
+
+    expected_events = [
+        "data_context.test_yaml_config",
+        "data_context.get_batch_list",
+        "data_context.save_expectation_suite",
+        "data_context.test_yaml_config",
+        "data_context.build_data_docs",
+        "checkpoint.run",
+        "data_context.run_checkpoint",
+    ]
+    # assert events == expected_events
+
+    # ['data_context.get_batch_list',
+    #  'data_context.save_expectation_suite',
+    #  'data_context.test_yaml_config',
+    #  'data_context.build_data_docs',
+    #  'checkpoint.run',
+    #  'data_context.run_checkpoint'] != ['data_context.test_yaml_config',
+    #                                     'data_context.get_batch_list',
+    #                                     'data_context.save_expectation_suite',
+    #                                     'data_context.test_yaml_config',
+    #                                     'data_context.build_data_docs',
+    #                                     'checkpoint.run',
+    #                                     'data_context.run_checkpoint']
+
+    # assert context._usage_statistics_handler._message_queue.queue == [{'event': 'data_context.test_yaml_config', 'event_payload': {'anonymized_name': '8123af7f2122286e90309eeec8348d29', 'parent_class': 'SimpleCheckpoint'}, 'success': True, 'version': '1.0.0', 'ge_version': '0.13.46+11.ga7b921a40.dirty', 'data_context_id': '00000000-0000-0000-0000-000000000001', 'data_context_instance_id': 'ad4d0286-5599-4099-9c17-068c255212af', 'event_time': '2021-12-15T22:36:12.633Z'}, {'event_payload': {}, 'event': 'data_context.build_data_docs', 'success': True, 'version': '1.0.0', 'ge_version': '0.13.46+11.ga7b921a40.dirty', 'data_context_id': '00000000-0000-0000-0000-000000000001', 'data_context_instance_id': 'ad4d0286-5599-4099-9c17-068c255212af', 'event_time': '2021-12-15T22:36:12.769Z', 'event_duration': 3}, {'event_payload': {'anonymized_name': '8123af7f2122286e90309eeec8348d29', 'config_version': 1.0, 'anonymized_action_list': [{'anonymized_name': 'fdbdf58d2ea2482971f49bb26337bafb', 'parent_class': 'StoreValidationResultAction'}, {'anonymized_name': '1bcb639ed40bcea7a5e78539b6baf538', 'parent_class': 'StoreEvaluationParametersAction'}, {'anonymized_name': '548269a6423f5f7dac01585d2ed192e7', 'parent_class': 'UpdateDataDocsAction'}], 'anonymized_validations': [{'anonymized_batch_request': {'anonymized_batch_request_required_top_level_properties': {'anonymized_datasource_name': 'da89dda9f4e5192c65e791f0ba90bbda', 'anonymized_data_connector_name': '783632790e85169ed6ab26faf3fc8975', 'anonymized_data_asset_name': '51ecb6dcb3d835fb13ff51a89923f4f7'}, 'batch_request_optional_top_level_keys': ['batch_identifiers', 'runtime_parameters'], 'runtime_parameters_keys': ['batch_data']}, 'anonymized_expectation_suite_name': '50ccea61f2913be7e227f74f848363d9', 'anonymized_action_list': [{'anonymized_name': 'fdbdf58d2ea2482971f49bb26337bafb', 'parent_class': 'StoreValidationResultAction'}, {'anonymized_name': '1bcb639ed40bcea7a5e78539b6baf538', 'parent_class': 'StoreEvaluationParametersAction'}, {'anonymized_name': '548269a6423f5f7dac01585d2ed192e7', 'parent_class': 'UpdateDataDocsAction'}]}]}, 'event': 'checkpoint.run', 'success': True, 'version': '1.0.0', 'ge_version': '0.13.46+11.ga7b921a40.dirty', 'data_context_id': '00000000-0000-0000-0000-000000000001', 'data_context_instance_id': 'ad4d0286-5599-4099-9c17-068c255212af', 'event_time': '2021-12-15T22:36:12.793Z', 'event_duration': 119}, {'event_payload': {}, 'event': 'data_context.run_checkpoint', 'success': True, 'version': '1.0.0', 'ge_version': '0.13.46+11.ga7b921a40.dirty', 'data_context_id': '00000000-0000-0000-0000-000000000001', 'data_context_instance_id': 'ad4d0286-5599-4099-9c17-068c255212af', 'event_time': '2021-12-15T22:36:12.816Z', 'event_duration': 154}]
+
+    # wait_event.set()
+
+    print("========== caplog.messages ==========")
+    # assert not usage_stats_invalid_messages_exist(messages=caplog.messages)
+    print(caplog.messages)
+    for idx, message in enumerate(caplog.messages):
+        print(f"{idx}. {message}")
+    print("========== caplog.messages ==========")
