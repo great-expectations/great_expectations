d869eedf9ec39505449ff9176f11ee0e1f625a90
Anthony Burdi <anthony@superconductive.com>
2021-12-17 10:48:04 -0500

    Fix test import

--- tests/core/usage_statistics/test_util.py
+++ tests/core/usage_statistics/test_util.py
@@ -4,27 +4,18 @@ from great_expectations.core.usage_statistics.usage_statistics import (
     UsageStatsExceptionPrefix,
 )
 from tests.core.usage_statistics.util import (
-    assert_no_usage_stats_exceptions,
+    usage_stats_exceptions_exist,
     usage_stats_invalid_messages_exist,
 )
 
 
 @pytest.mark.parametrize(
-    "test_input",
+    "test_input,test_output",
     [
         pytest.param(
-            ["just", "some", "logger", "messages"], id="list_without_exceptions"
+            ["just", "some", "logger", "messages"], False, id="list_without_exceptions"
         ),
-        pytest.param([], id="empty_list"),
-    ],
-)
-def test_assert_no_usage_stats_exceptions_passing(test_input):
-    assert_no_usage_stats_exceptions(messages=test_input)
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    [
+        pytest.param([], False, id="empty_list"),
         pytest.param(
             [
                 "just",
@@ -33,6 +24,7 @@ def test_assert_no_usage_stats_exceptions_passing(test_input):
                 "messages",
                 f"{UsageStatsExceptionPrefix.EMIT_EXCEPTION.value} some error message",
             ],
+            True,
             id="list_with_exceptions",
         ),
         pytest.param(
@@ -43,14 +35,13 @@ def test_assert_no_usage_stats_exceptions_passing(test_input):
                 "messages",
                 f"{UsageStatsExceptionPrefix.EMIT_EXCEPTION.value}some error message",
             ],
+            True,
             id="list_with_exceptions_no_whitespace",
         ),
     ],
 )
-def test_assert_no_usage_stats_exceptions_failing(test_input):
-
-    with pytest.raises(AssertionError):
-        assert_no_usage_stats_exceptions(messages=test_input)
+def test_assert_no_usage_stats_exceptions_passing(test_input, test_output):
+    assert usage_stats_exceptions_exist(messages=test_input) == test_output
 
 
 @pytest.mark.parametrize(
