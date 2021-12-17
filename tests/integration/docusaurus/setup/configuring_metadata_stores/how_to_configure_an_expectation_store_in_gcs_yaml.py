54fb634cf56341bc5510e53f53f1cfba3038bd8d
Nathan Farmer <nathan@superconductive.com>
2021-12-17 12:55:32 -0500

    Rearrange

--- tests/integration/docusaurus/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_gcs_yaml.py
+++ tests/integration/docusaurus/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_gcs_yaml.py
@@ -5,71 +5,7 @@ from ruamel import yaml
 
 import great_expectations as ge
 
-expected_existing_expectations_store_yaml = """
-stores:
-  expectations_store:
-    class_name: ExpectationsStore
-    store_backend:
-      class_name: TupleFilesystemStoreBackend
-      base_directory: expectations/
-      
-expectations_store_name: expectations_store
-"""
-
-configured_expectations_store_yaml = """
-stores:
-  expectations_GCS_store:
-    class_name: ExpectationsStore
-    store_backend:
-      class_name: TupleGCSStoreBackend
-      project: <YOUR GCP PROJECT NAME>
-      bucket: <YOUR GCS BUCKET NAME>
-      prefix: <YOUR GCS PREFIX NAME>
-      
-expectations_store_name: expectations_GCS_store
-"""
-
-copy_expectation_command = """
-gsutil cp expectations/my_expectation_suite.json gs://<YOUR GCS BUCKET NAME>/<YOUR GCS PREFIX NAME>/my_expectation_suite.json
-"""
-
-copy_expectation_output = """
-Operation completed over 1 objects
-"""
-
-list_expectation_stores_command = """
-great_expectations --v3-api store list
-"""
-
-list_expectation_stores_output = """
-- name: expectations_store
-class_name: ExpectationsStore
-store_backend:
-  class_name: TupleFilesystemStoreBackend
-  base_directory: expectations/
-
-- name: expectations_GCS_store
-class_name: ExpectationsStore
-store_backend:
-  class_name: TupleGCSStoreBackend
-  project: <YOUR GCP PROJECT NAME>
-  bucket: <YOUR GCS BUCKET NAME>
-  prefix: <YOUR GCS PREFIX NAME>
-"""
-
-list_expectation_suites_command = """
-great_expectations --v3-api suite list
-"""
-
-list_expectation_suites_output = """
-1 Expectation Suite found:
- - my_expectation_suite
-"""
-
-
-# NOTE: The following code is only for testing and can be ignored by users.
 context = ge.get_context()
-
 
 # parse great_expectations.yml for comparison
 great_expectations_yaml_file_path = os.path.join(
@@ -89,9 +25,33 @@ actual_existing_expectations_store["expectations_store_name"] = great_expectatio
     "expectations_store_name"
 ]
 
+expected_existing_expectations_store_yaml = """
+stores:
+  expectations_store:
+    class_name: ExpectationsStore
+    store_backend:
+      class_name: TupleFilesystemStoreBackend
+      base_directory: expectations/
+
+expectations_store_name: expectations_store
+"""
+
 assert actual_existing_expectations_store == yaml.safe_load(
     expected_existing_expectations_store_yaml
 )
+
+configured_expectations_store_yaml = """
+stores:
+  expectations_GCS_store:
+    class_name: ExpectationsStore
+    store_backend:
+      class_name: TupleGCSStoreBackend
+      project: <YOUR GCP PROJECT NAME>
+      bucket: <YOUR GCS BUCKET NAME>
+      prefix: <YOUR GCS PREFIX NAME>
+
+expectations_store_name: expectations_GCS_store
+"""
 
 # replace example code with integration test configuration
 configured_expectations_store = yaml.safe_load(configured_expectations_store_yaml)
@@ -117,15 +77,14 @@ great_expectations_yaml["stores"]["expectations_GCS_store"]["store_backend"].pop
 with open(great_expectations_yaml_file_path, "w") as f:
     yaml.dump(great_expectations_yaml, f, default_flow_style=False)
 
-with open(great_expectations_yaml_file_path, "r") as f:
-    text = f.read()
-
-print(text)
-
 expectation_suite_name = "my_expectation_suite"
 context.create_expectation_suite(expectation_suite_name=expectation_suite_name)
 
 # try gsutil cp command
+copy_expectation_command = """
+gsutil cp expectations/my_expectation_suite.json gs://<YOUR GCS BUCKET NAME>/<YOUR GCS PREFIX NAME>/my_expectation_suite.json
+"""
+
 local_expectation_suite_file_path = os.path.join(
     context.root_directory, "expectations", f"{expectation_suite_name}.json"
 )
@@ -150,12 +109,54 @@ result = subprocess.run(
     copy_expectation_command.strip().split(), check=True, stderr=subprocess.PIPE
 )
 stderr = result.stderr.decode("utf-8")
+
+copy_expectation_output = """
+Operation completed over 1 objects
+"""
+
 assert copy_expectation_output.strip() in stderr
 
 # list expectation stores
+list_expectation_stores_command = """
+great_expectations --v3-api store list
+"""
+
 result = subprocess.run(
     list_expectation_stores_command.strip().split(), check=True, stdout=subprocess.PIPE
 )
 stdout = result.stdout.decode("utf-8")
-print(stdout)
-assert stdout == list_expectation_stores_output
+
+list_expectation_stores_output = """
+  - name: expectations_GCS_store
+    class_name: ExpectationsStore
+    store_backend:
+      class_name: TupleGCSStoreBackend
+      project: <YOUR GCP PROJECT NAME>
+      bucket: <YOUR GCS BUCKET NAME>
+      prefix: <YOUR GCS PREFIX NAME>
+"""
+
+assert "expectations_GCS_store" in list_expectation_stores_output
+assert "expectations_GCS_store" in stdout
+assert "TupleGCSStoreBackend" in list_expectation_stores_output
+assert "TupleGCSStoreBackend" in stdout
+
+# list expectation suites
+list_expectation_suites_command = """
+great_expectations --v3-api suite list
+"""
+
+result = subprocess.run(
+    list_expectation_suites_command.strip().split(), check=True, stdout=subprocess.PIPE
+)
+stdout = result.stdout.decode("utf-8")
+
+list_expectation_suites_output = """
+1 Expectation Suite found:
+ - my_expectation_suite
+"""
+
+assert "1 Expectation Suite found:" in list_expectation_suites_output
+assert "1 Expectation Suite found:" in stdout
+assert "my_expectation_suite" in list_expectation_suites_output
+assert "my_expectation_suite" in stdout
