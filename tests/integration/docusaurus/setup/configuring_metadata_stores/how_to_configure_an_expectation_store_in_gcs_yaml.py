e337da9a14c31e830f8cc85ec725465c9fc44d91
Nathan Farmer <nathan@superconductive.com>
2021-12-17 11:29:25 -0500

    Integration test for list stores

--- tests/integration/docusaurus/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_gcs_yaml.py
+++ tests/integration/docusaurus/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_gcs_yaml.py
@@ -34,7 +34,7 @@ gsutil cp expectations/my_expectation_suite.json gs://<YOUR GCS BUCKET NAME>/<YO
 """
 
 copy_expectation_output = """
-Operation completed over 1 objects/58.8 KiB.
+Operation completed over 1 objects
 """
 
 list_expectation_stores_command = """
@@ -97,18 +97,25 @@ assert actual_existing_expectations_store == yaml.safe_load(
 configured_expectations_store = yaml.safe_load(configured_expectations_store_yaml)
 configured_expectations_store["stores"]["expectations_GCS_store"]["store_backend"][
     "project"
-] = ""
+] = None
 configured_expectations_store["stores"]["expectations_GCS_store"]["store_backend"][
     "bucket"
 ] = "superconductive-integration-tests"
 configured_expectations_store["stores"]["expectations_GCS_store"]["store_backend"][
     "prefix"
-] = "data/taxi_yellow_tripdata_samples"
+] = "data/taxi_yellow_tripdata_samples/"
 
+# add and set the new expectation store
 context.add_store(
     store_name=configured_expectations_store["expectations_store_name"],
     store_config=configured_expectations_store["stores"]["expectations_GCS_store"],
 )
+with open(great_expectations_yaml_file_path, "r") as f:
+    great_expectations_yaml_str = f.read()
+great_expectations_yaml_str = great_expectations_yaml_str.replace("expectations_store_name: expectations_store", "expectations_store_name: expectations_GCS_store")
+with open(great_expectations_yaml_file_path, "w") as f:
+    f.write(great_expectations_yaml_str)
+
 expectation_suite_name = "my_expectation_suite"
 context.create_expectation_suite(expectation_suite_name=expectation_suite_name)
 
@@ -130,12 +137,20 @@ copy_expectation_command = copy_expectation_command.replace(
     configured_expectations_store["stores"]["expectations_GCS_store"]["store_backend"][
         "prefix"
     ]
-    + f"/{expectation_suite_name}.json",
+    + f"{expectation_suite_name}.json",
 )
-print(copy_expectation_command.strip())
 
-stdout = subprocess.run(
-    copy_expectation_command.strip(), check=True, stdout=subprocess.PIPE
-).stdout
+result = subprocess.run(
+    copy_expectation_command.strip().split(), check=True, stderr=subprocess.PIPE
+)
+stderr = result.stderr.decode("utf-8")
+assert copy_expectation_output.strip() in stderr
+
+# list expectation stores
+print(great_expectations_yaml_str)
+result = subprocess.run(
+    list_expectation_stores_command.strip().split(), check=True, stdout=subprocess.PIPE
+)
+stdout = result.stdout.decode("utf-8")
 print(stdout)
-assert stdout == copy_expectation_output
+assert stdout == list_expectation_stores_output
