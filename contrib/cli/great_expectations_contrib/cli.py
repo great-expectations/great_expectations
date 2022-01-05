2f7e1a55c8429cc9dc91c6657f694bfb4ae8a7cc
Chetan Kini <chetan@superconductive.com>
2022-01-05 16:41:41 -0500

    feat: misc cleanup to ensure proper serialization

--- contrib/cli/great_expectations_contrib/cli.py
+++ contrib/cli/great_expectations_contrib/cli.py
@@ -1,12 +1,13 @@
 import click
 from great_expectations_contrib.commands import check_cmd, init_cmd, publish_cmd
+from great_expectations_contrib.types import GreatExpectationsContribPackage
 
 # The following link points to the repo where the Cookiecutter template is hosted
 URL = "https://github.com/great-expectations/great-expectations-contrib-cookiecutter"
 
 
 @click.group()
-def cli() -> None:
+def cli(ctx: click.Context) -> None:
     """
     Welcome to the great_expectations_contrib CLI!
     This tool is meant to make contributing new packages to Great Expectations as smooth as possible.
@@ -15,7 +16,8 @@ def cli() -> None:
 
     Create a package using `init`, check your code with `check`, and upload your work to PyPi with `publish`.
     """
-    pass
+    pkg = GreatExpectationsContribPackage.from_json_file()
+    ctx.obj = pkg
 
 
 @cli.command(help="Initialize a contributor package")
@@ -24,13 +26,17 @@ def init() -> None:
 
 
 @cli.command(help="Publish your package to PyPi")
-def publish() -> None:
+@click.pass_obj
+def publish(pkg: GreatExpectationsContribPackage) -> None:
     publish_cmd()
+    pkg.update_package_state()
 
 
 @cli.command(help="Check your package to make sure it's met all the requirements")
-def check() -> None:
+@click.pass_obj
+def check(pkg: GreatExpectationsContribPackage) -> None:
     check_cmd()
+    pkg.update_package_state()
 
 
 if __name__ == "__main__":
--- contrib/cli/great_expectations_contrib/types.py
+++ contrib/cli/great_expectations_contrib/types.py
@@ -1,13 +1,20 @@
 import importlib
 import inspect
 import json
+import logging
 import os
 import sys
 from dataclasses import asdict, dataclass
 from enum import Enum
-from typing import Any, List, Optional
+from typing import Any, ClassVar, Dict, List, Optional, Type
 
 from great_expectations.expectations.expectation import Expectation
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Type alias that will need to be updated to reflect the complex nature of the 'run_diagnostics' return object
+Diagnostics = Dict[str, Any]
 
 
 @dataclass
@@ -76,95 +83,139 @@ class Maturity(Enum):
 @dataclass
 class GreatExpectationsContribPackage:
     # Core
-    package_name: str
-    icon: str
-    description: str
-    expectations: List[RenderedExpectationDetail]
-    expectation_count: int
-    dependencies: List[Dependency]
-    maturity: Maturity
-    status: PackageCompletenessStatus
+    package_name: Optional[str] = None
+    icon: Optional[str] = None
+    description: Optional[str] = None
+    expectations: Optional[List[RenderedExpectationDetail]] = None
+    expectation_count: Optional[int] = None
+    dependencies: Optional[List[Dependency]] = None
+    maturity: Optional[Maturity] = None
+    status: Optional[PackageCompletenessStatus] = None
 
     # Users
-    owners: List[GitHubUser]
-    contributors: List[GitHubUser]
-    domain_experts: List[DomainExpert]
+    owners: Optional[List[GitHubUser]] = None
+    contributors: Optional[List[GitHubUser]] = None
+    domain_experts: Optional[List[DomainExpert]] = None
 
     # Metadata
-    version: str
+    version: Optional[str] = None
 
-    def to_json_dict(self) -> dict:
-        return asdict(self)
+    # Existing configuration
+    config_file: ClassVar[str] = ".great_expectations_package.json"
+
+    def to_json_file(self) -> None:
+        """
+        Write the contents of the class instance to disk.
+        Any null values are excluded.
+
+        """
+        # Convert to JSON and remove nulls
+        json_dict = asdict(self)
+        for k, v in json_dict.items():
+            if v is None:
+                json_dict.pop(k)
+
+        data = json.dumps(json_dict, indent=4)
+        with open(self.config_file, "w") as f:
+            f.write(data)
+            logger.info(f"Succesfully wrote state to {self.config_file}.")
 
     @classmethod
-    def from_json_file(cls, file: str) -> "GreatExpectationsContribPackage":
-        with open(file) as f:
+    def from_json_file(cls) -> "GreatExpectationsContribPackage":
+        """Reads data from JSON file to create class instance.
+
+        If the target file is not available, an empty package is created.
+
+        Returns:
+            An instance of GreatExpectationsContribPackage to represent the package's current state
+
+        """
+        # If config file isn't found, create a blank JSON and write to disk
+        if not os.path.exists(cls.config_file):
+            instance = cls()
+            logger.info(f"Could not find existing package JSON; instantiated a new one")
+            return instance
+
+        with open(cls.config_file) as f:
             contents = f.read()
 
         data = json.loads(contents)
+        logger.info(f"Succesfully read existing package data from {cls.config_file}")
         return cls(**data)
 
-    def determine_values(self) -> None:
+    def update_package_state(self) -> None:
+        """
+        Parses diagnostic reports from package Expectations and uses them to update JSON state
+
+        """
+        diagnostics = self._retrieve_package_expectations_diagnostics()
+        self._update_attrs_with_diagnostics(diagnostics)
+        self.to_json_file()
+
+    def _update_attrs_with_diagnostics(self, diagnostics: List[Diagnostics]) -> None:
+        # TODO: Write logic to assign values to attrs
+        for diagnostic in diagnostics:
+            pass
+
+    def _retrieve_package_expectations_diagnostics(self) -> List[Diagnostics]:
         package = self._identify_user_package()
         expectations_module = self._import_expectations_module(package)
+
+        # Could not import successfully so we exit early
+        if expectations_module is None:
+            return []
+
         expectations = self._retrieve_expectations_from_module(expectations_module)
         diagnostics = self._gather_diagnostics(expectations)
+        return diagnostics
 
-        self.expectations = diagnostics
+    def _identify_user_package(self) -> str:
+        # Guaranteed to have a dir named '<MY_PACKAGE>_expectations' through Cookiecutter validation
+        packages = [
+            d for d in os.listdir() if os.path.isdir(d) and d.endswith("_expectations")
+        ]
 
-        self._determine_core_values()
-        self._determine_user_values()
-        self._determine_metadata()
-
-        with open(".great_expectations_package.json", "w") as f:
-            data = json.dumps(self.to_json_dict(), indent=4)
-            f.write(data)
-
-    def _determine_core_values(self) -> None:
-        pass
-
-    def _determine_user_values(self) -> None:
-        pass
-
-    def _determine_metadata(self) -> None:
-        pass
-
-    def _identify_user_package(self):
-        packages = [d for d in os.listdir() if os.path.isdir(d) and "expectations" in d]
         if len(packages) == 0:
-            pass
+            logger.info("Could not find a user-defined package")
+            return ""
         elif len(packages) > 1:
-            pass
+            logger.info("Found multiple user-defined packages")
+            return ""
 
-        package = packages[0]
-        return package
+        return packages[0]
 
     def _import_expectations_module(self, package: str) -> Any:
+        # Need to add user's project to the PYTHONPATH
+        cwd = os.getcwd()
+        sys.path.append(cwd)
         try:
-            cwd = os.getcwd()
-            sys.path.append(cwd)
             expectations_module = importlib.import_module(f"{package}.expectations")
             return expectations_module
-        except ModuleNotFoundError:
-            print(f"Could not import user expectations")
+        except ModuleNotFoundError as e:
+            logger.info(f"Could not import user expectations: {e}")
+            return None
 
     def _retrieve_expectations_from_module(
         self, expectations_module: Any
-    ) -> List[Expectation]:
-        expectations = []
-        names = []
+    ) -> List[Type[Expectation]]:
+        expectations: List[Type[Expectation]] = []
+        names: List[str] = []
         for name, obj in inspect.getmembers(expectations_module):
             if inspect.isclass(obj) and issubclass(obj, Expectation):
                 expectations.append(obj)
                 names.append(name)
 
-        print(f"Found {len(names)} expectation(s): {names}")
+        logger.info(f"Found {len(names)} expectation(s): {names}")
         return expectations
 
-    def _gather_diagnostics(self, expectations: List[Expectation]) -> List[dict]:
+    def _gather_diagnostics(
+        self, expectations: List[Type[Expectation]]
+    ) -> List[Diagnostics]:
         diagnostics_list = []
         for expectation in expectations:
             instance = expectation()
             diagnostics = instance.run_diagnostics()
             diagnostics_list.append(diagnostics)
+            logger.info(f"Succesfully retrieved diagnostics from {expectation}")
+
         return diagnostics_list
