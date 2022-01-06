0854844810d2059a46be823edba7689218fd2546
Chetan Kini <chetan@superconductive.com>
2022-01-06 11:32:25 -0500

    refactor: update method used to determine docs deps

--- scripts/trace_docs_deps.py
+++ scripts/trace_docs_deps.py
@@ -1,28 +1,17 @@
 """
-Usage: `python trace_docs_deps.py [DOCS_DIR]`
+Usage: `python trace_docs_deps.py`
 
 This script is used in our Azure Docs Integration pipeline (azure-pipelines-docs-integration.yml) to determine whether
 a change has been made in the `great_expectations/` directory that change impacts `docs/` and the snippets therein.
 
 The script takes the following steps:
-    1. Parses all markdown files in `docs/`, using regex to find any Docusaurus links (i.e. ```python file=...#L10-20)
-    2. Goes to each linked file and uses AST to parse imports used there
-    3. Filters for only relative imports and determines the paths to those files
+    1. Uses AST to parse the source code in `great_expectations/`; the result is a mapping between function/class definition and the origin file of that symbol
+    2. Parses all markdown files in `docs/`, using regex to find any Docusaurus links (i.e. ```python file=...#L10-20)
+    3. Evalutes each linked file using AST and uses the definition map from step #1 to determine which source files are relevant to docs under test
 
 The resulting output list is all of the dependencies `docs/` has on the primary `great_expectations/` directory.
 If a change is identified in any of these files during the pipeline runtime, we know that a docs dependency has possibly
 been impacted and the pipeline should run to ensure adequate test coverage.
-
-Please note that this solution has some inherit disclaimers one should be familiar with. As there is no actual execution
-of source code (but rather parsing of the source file's AST), it is hard to pinpoint the location of a given import with
-absolute certainty.  Utilizing the `__file__` attr would provide the exact path of an import but without that, we need
-to assume that an `ast.Import` node corresponds to an accurate path in our codebase (ex: `great_expectations.core.batch`
-results in great_expectations/core/batch.py).
-
-As long as imports correspond to the relative path of modules, this method will continue to work as intended. Deviations
-here could result in certain imports not being picked up by the script, reducing the accuracy and frequency of the
-Azure Docs Integration pipeline's triggers. As GE has maintained this consistency throughout its development, this
-script and the methods used herein are deemed to be appropriate.
 
 """
 
@@ -31,7 +20,7 @@ import glob
 import os
 import re
 from collections import defaultdict
-from typing import DefaultDict, Dict, List, Optional, Set, Union
+from typing import DefaultDict, Dict, List, Set
 
 
 def parse_definition_nodes_from_source_code(directory: str) -> Dict[str, Set[str]]:
@@ -56,11 +45,13 @@ def _parse_definition_nodes_from_file(file: str) -> Dict[str, Set[str]]:
     with open(file) as f:
         root = ast.parse(f.read(), file)
 
+    # Parse all 'def ...' and 'class ...' statements in the source code
     definition_nodes = []
-    for node in root.body:
+    for node in ast.walk(root):
         if isinstance(node, (ast.FunctionDef, ast.ClassDef)):
             definition_nodes.append(node.name)
 
+    # Associate the function/class name with the file it comes from
     file_definition_map: DefaultDict[str, Set[str]] = defaultdict(set)
     for name in definition_nodes:
         file_definition_map[name].add(file)
@@ -90,32 +81,23 @@ def find_docusaurus_refs(directory: str) -> List[str]:
     """
     linked_files: Set[str] = set()
     pattern: str = (
-        r"\`\`\`[a-zA-Z]+ file"  # Format of internal links used by Docusaurus
+        r"```python file=([\.\/l\w]+)"  # Format of internal links used by Docusaurus
     )
+
+    r = re.compile(pattern)
 
     for doc in glob.glob(f"{directory}/**/*.md", recursive=True):
         with open(doc) as f:
-            lines = f.readlines()
+            contents = f.read()
 
-        for line in lines:
-            if re.search(pattern, line):
-                file = _parse_file_from_docusaurus_link(line)
-                if not file:
-                    continue
-                path: str = os.path.join(os.path.dirname(doc), file)
-                # only interested in looking at .py files for now (excludes .yml files)
-                if path[-3:] == ".py":
-                    linked_files.add(path)
+        matches = r.findall(contents)
+        for match in matches:
+            path: str = os.path.join(os.path.dirname(doc), match)
+            # only interested in looking at .py files for now (excludes .yml files)
+            if path[-3:] == ".py":
+                linked_files.add(path)
 
     return [file for file in linked_files]
-
-
-def _parse_file_from_docusaurus_link(line: str) -> Optional[str]:
-    pattern: str = "=(.+?)#"  # Parse just the path from the Docusaurus link
-    search: Union[re.Match[str], None] = re.search(pattern, line)
-    if search:
-        return search.group(1)
-    return None
 
 
 def determine_relevant_source_files(
@@ -147,15 +129,23 @@ def _retrieve_symbols_from_file(file: str) -> Set[str]:
 
     symbols = set()
     for node in ast.walk(root):
-        if isinstance(node, ast.Name):
-            symbols.add(node.id)
+        if isinstance(node, ast.Call):
+            func = node.func
+            if isinstance(func, ast.Attribute):
+                symbols.add(func.attr)
+            elif isinstance(func, ast.Name):
+                symbols.add(func.id)
 
     return symbols
 
 
-if __name__ == "__main__":
+def main() -> None:
     definition_map = parse_definition_nodes_from_source_code("great_expectations")
     files_referenced_in_docs = find_docusaurus_refs("docs")
     paths = determine_relevant_source_files(files_referenced_in_docs, definition_map)
     for path in paths:
         print(path)
+
+
+if __name__ == "__main__":
+    main()
