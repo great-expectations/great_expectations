3deffaacd9670710a708eabe76feb15b6d070895
Chetan Kini <chetan@superconductive.com>
2022-01-12 11:52:17 -0500

    test: write initial round of tests for builders

--- great_expectations/rule_based_profiler/config/base.py
+++ great_expectations/rule_based_profiler/config/base.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, List, Optional
 
+from great_expectations.data_context.types.base import BaseYamlConfig
 from great_expectations.marshmallow__shade import INCLUDE, Schema, fields, post_load
 from great_expectations.types import DictDot
 
@@ -86,14 +87,14 @@ class ExpectationConfigurationBuilderConfig(DictDot):
         class_name: str,
         module_name: Optional[str] = None,
         mostly: Optional[float] = None,
-        meta: Optional[str] = None,
+        meta: Optional[Dict] = None,
         **kwargs,
     ):
         self.expectation_type = expectation_type
         self._class_name = class_name
         self._module_name = module_name
         self.mostly = mostly
-        self.meta = meta
+        self.meta = meta or {}
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -114,7 +115,7 @@ class ExpectationConfigurationBuilderConfigSchema(Schema):
     module_name = fields.String(required=False, allow_none=True)
     expectation_type = fields.String(required=True)
     mostly = fields.Float(required=False, allow_none=True)
-    meta = fields.String(required=False, allow_none=True)
+    meta = fields.Dict(required=False, allow_none=True)
 
     @post_load
     def make_config(self, data, **kwargs) -> ExpectationConfigurationBuilderConfig:
@@ -157,7 +158,7 @@ class RuleConfigSchema(Schema):
         return RuleConfig(**data)
 
 
-class RuleBasedProfilerConfig(DictDot):
+class RuleBasedProfilerConfig(BaseYamlConfig):
     def __init__(
         self,
         name: str,
@@ -169,9 +170,7 @@ class RuleBasedProfilerConfig(DictDot):
         self.name = name
         self.config_version = config_version
         self.rules = rules
-        if variables is None:
-            variables = {}
-        self.variables = variables
+        self.variables = variables or {}
         for k, v in kwargs.items():
             setattr(self, k, v)
 
--- tests/rule_based_profiler/config/test_base.py
+++ tests/rule_based_profiler/config/test_base.py
@@ -1,24 +1,144 @@
+import pytest
+
+from great_expectations.marshmallow__shade.exceptions import ValidationError
 from great_expectations.rule_based_profiler.config.base import (
     DomainBuilderConfig,
     DomainBuilderConfigSchema,
+    ExpectationConfigurationBuilderConfig,
+    ExpectationConfigurationBuilderConfigSchema,
+    ParameterBuilderConfig,
+    ParameterBuilderConfigSchema,
 )
 
 
-def test_DomainBuilderConfigSchema_valid_load():
-    data = {"class_name": "DomainBuilder"}
+def test_domain_builder_config_successfully_loads_with_required_args():
+    data = {
+        "class_name": "DomainBuilder",
+    }
     schema = DomainBuilderConfigSchema()
     config = schema.load(data)
     assert isinstance(config, DomainBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
 
 
-def test_DomainBuilderConfigSchema_invalid_load():
+def test_domain_builder_config_successfully_loads_with_optional_args():
+    data = {
+        "class_name": "DomainBuilder",
+        "module_name": "great_expectations.rule_based_profiler.domain_builder",
+        "batch_request": {"datasource_name": "my_datasource"},
+    }
+    schema = DomainBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, DomainBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def test_domain_builder_config_successfully_loads_with_kwargs():
+    data = {"class_name": "DomainBuilder", "author": "Charles Dickens"}
+    schema = DomainBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, DomainBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def test_domain_builder_config_unsuccessfully_loads_with_missing_required_fields():
     data = {"klass": "DomainBuilder"}
     schema = DomainBuilderConfigSchema()
 
+    with pytest.raises(ValidationError) as e:
+        schema.load(data)
+
+    assert "'class_name': ['Missing data for required field.']" in str(e.value)
+
+
+def test_parameter_builder_config_successfully_loads_with_required_args():
+    data = {"class_name": "ParameterBuilder", "parameter_name": "my_parameter_builder"}
+    schema = ParameterBuilderConfigSchema()
     config = schema.load(data)
-    assert isinstance(config, DomainBuilderConfig)
+    assert isinstance(config, ParameterBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
 
 
-"""
-great_expectations.marshmallow__shade.exceptions.ValidationError: {'class_name': ['Missing data for required field.']}
-"""
+def test_parameter_builder_config_successfully_loads_with_optional_args():
+    data = {
+        "parameter_name": "my_parameter_builder",
+        "class_name": "ParameterBuilder",
+        "module_name": "great_expectations.rule_based_profiler.parameter_builder",
+        "batch_request": {"datasource_name": "my_datasource"},
+    }
+    schema = ParameterBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, ParameterBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def tests_parameter_builder_config_successfully_loads_with_kwargs():
+    data = {
+        "parameter_name": "my_parameter_builder",
+        "class_name": "ParameterBuilder",
+        "created_on": "2022-01-12",
+    }
+    schema = ParameterBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, ParameterBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def test_parameter_builder_config_unsuccessfully_loads_with_missing_required_fields():
+    data = {"klass": "ParameterBuilder", "param": "my_param"}
+    schema = ParameterBuilderConfigSchema()
+
+    with pytest.raises(ValidationError) as e:
+        schema.load(data)
+
+    assert "'class_name': ['Missing data for required field.']" in str(e.value)
+    assert "'parameter_name': ['Missing data for required field.']" in str(e.value)
+
+
+def test_expectation_configuration_builder_config_successfully_loads_with_required_args():
+    data = {
+        "class_name": "ExpectationConfigurationBuilder",
+        "expectation_type": "expect_column_pair_values_A_to_be_greater_than_B",
+    }
+    schema = ExpectationConfigurationBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, ExpectationConfigurationBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def test_expectation_configuration_builder_config_successfully_loads_with_optional_args():
+    data = {
+        "expectation_type": "expect_column_pair_values_A_to_be_greater_than_B",
+        "class_name": "ExpectationConfigurationBuilder",
+        "module_name": "great_expectations.rule_based_profiler.expectation_configuration_builder",
+        "mostly": 0.9,
+        "meta": {"foo": "bar"},
+    }
+    schema = ExpectationConfigurationBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, ExpectationConfigurationBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def tests_expectation_configuration_builder_config_successfully_loads_with_kwargs():
+    data = {
+        "expectation_type": "expect_column_pair_values_A_to_be_greater_than_B",
+        "class_name": "ExpectationConfigurationBuilder",
+        "created_on": "2022-01-12",
+        "author": "Charles Dickens",
+    }
+    schema = ExpectationConfigurationBuilderConfigSchema()
+    config = schema.load(data)
+    assert isinstance(config, ExpectationConfigurationBuilderConfig)
+    assert all(getattr(config, k) == v for k, v in data.items())
+
+
+def test_expectation_configuration_builder_config_unsuccessfully_loads_with_missing_required_fields():
+    data = {"klass": "ExpectationConfigurationBuilder"}
+    schema = ExpectationConfigurationBuilderConfigSchema()
+
+    with pytest.raises(ValidationError) as e:
+        schema.load(data)
+
+    assert "'class_name': ['Missing data for required field.']" in str(e.value)
+    assert "'expectation_type': ['Missing data for required field.']" in str(e.value)
