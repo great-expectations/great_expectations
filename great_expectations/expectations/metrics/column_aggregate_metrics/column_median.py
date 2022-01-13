842237cbe96c5dcf253f8f780381bf4d26f50690
Alex Sherstinsky <alex@superconductive.com>
2022-01-12 17:45:39 -0800

    clean up

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_median.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_median.py
@@ -139,14 +139,4 @@ class ColumnMedian(ColumnAggregateMetricProvider):
                 metric_domain_kwargs=metric.metric_domain_kwargs,
             )
 
-        table_domain_kwargs: dict = {
-            k: v for k, v in metric.metric_domain_kwargs.items() if k != "column"
-        }
-        dependencies["table.row_count"] = MetricConfiguration(
-            metric_name="table.row_count",
-            metric_domain_kwargs=table_domain_kwargs,
-            metric_value_kwargs=None,
-            metric_dependencies=None,
-        )
-
         return dependencies
