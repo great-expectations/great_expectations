c2527cb8d8dc0b664c65a8a97351719ba6cf1795
Alex Sherstinsky <alex@superconductive.com>
2022-01-12 21:20:07 -0800

    fix

--- great_expectations/expectations/metrics/column_map_metrics/column_values_decreasing.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_decreasing.py
@@ -15,7 +15,7 @@ from great_expectations.execution_engine.execution_engine import (
     MetricDomainTypes,
     MetricPartialFunctionTypes,
 )
-from great_expectations.expectations.metrics.import_manager import Window, sparktypes
+from great_expectations.expectations.metrics.import_manager import F, Window, sparktypes
 from great_expectations.expectations.metrics.map_metric_provider import (
     ColumnMapMetricProvider,
     column_condition_partial,
