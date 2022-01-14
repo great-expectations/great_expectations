0d679f1b1d194c0b7218dcebe11bec0c1fa86e19
Rachel-Reverie <94694058+Rachel-Reverie@users.noreply.github.com>
2022-01-14 13:37:46 -0600

    Doc 52 connect to data overview (#3970)
    * Partial rough draft of overview.md for the Connect to Data step.
    
    * More fleshed out rough draft of overview.md
    
    * complete rough draft for overview.md
    
    * Minor edits to overview.md
    Added overview.md and corresponding how to documents into the ToC
    
    * Partial rough draft of overview.md for the Connect to Data step.
    
    * More fleshed out rough draft of overview.md
    
    * complete rough draft for overview.md
    
    * rebased on parent branch
    
    * Nested Connect to Data guides under How to guides in the ToC
    
    * Starting updates on index pages for the Connect to Data how to documents.
    
    * Update docs/guides/connecting_to_your_data/overview.md
    
    Co-authored-by: talagluck <tal@superconductive.com>
    
    * Update docs/guides/connecting_to_your_data/overview.md
    
    Co-authored-by: talagluck <tal@superconductive.com>
    
    * Extended section on `test_yaml_config()`
    
    * Removed sections of the ToC that were moved into "1. Setup" and "2. Connecting to Data".
    
    * Added `batch_identifiers` as a potential key to replace <additional_keys_based_on_source_data_systems> in the examples when the source data system is a filesystem.
    
    * Updated `InferredAssetDataConnector` section to include that `InferredAssetSqlDataConnector` provides similar functionality for SQL based source data systems.
    
    * Added index for Step 2: Connect to Data to sidebar.js; updated contents of index to correspond to folder structure in sidebar.js
    
    * Updated relative links for index.md
    
    * Update docs/guides/connecting_to_your_data/overview.md
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * Update docs/guides/connecting_to_your_data/overview.md
    
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>
    
    * - Changed heading of section 1. Boilerplate to 1. Prepare scaffolding
    - Refactored name of overview file to match naming convention of other step's overview files.
    - Updated internal links and references to the refactored overview file's name.
    
    * - Corrected spelling of `test_yaml_config()`
    
    Co-authored-by: talagluck <tal@superconductive.com>
    Co-authored-by: Abe Gong <abegong@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -96,144 +96,12 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'Step 3: Create Expectations',
+      label: 'Step 2: Connect To Data',
       items: [
-        { type: 'doc', id: 'guides/expectations/create_expectations_overview', label: 'Overview' },
+        { type: 'doc', id: 'guides/connecting_to_your_data/connect_to_data_overview', label: 'Overview' },
         {
           type: 'category',
           label: 'How to guides',
-          items: [
-            {
-              type: 'category',
-              label: 'Core skills',
-              items: [
-                'guides/expectations/how_to_create_and_edit_expectations_based_on_domain_knowledge_without_inspecting_data_directly',
-                'guides/expectations/how_to_create_and_edit_expectations_in_bulk',
-                'guides/expectations/how_to_create_and_edit_expectations_with_a_profiler',
-                'guides/expectations/how_to_create_and_edit_expectations_with_instant_feedback_from_a_sample_batch_of_data'
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Configuring Profilers',
-              items: []
-            },
-            {
-              type: 'category',
-              label: 'Creating Custom Expectations',
-              items: [
-                'guides/expectations/creating_custom_expectations/how_to_create_custom_expectations',
-                // 'guides/expectations/creating_custom_expectations/how_to_create_custom_expectations_from_a_sql_query',
-                'guides/expectations/creating_custom_expectations/how_to_create_custom_parameterized_expectations'
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Contributing',
-              items: [
-                'guides/expectations/contributing/how_to_contribute_a_new_expectation_to_great_expectations'
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Advanced',
-              items: [
-                'guides/expectations/advanced/how_to_add_comments_to_expectations_and_display_them_in_data_docs',
-                'guides/expectations/advanced/how_to_create_renderers_for_custom_expectations',
-                'guides/expectations/advanced/how_to_create_a_new_expectation_suite_by_profiling_from_a_jsonschema_file',
-                'guides/expectations/advanced/how_to_create_expectations_that_span_multiple_batches_using_evaluation_parameters',
-                'guides/expectations/advanced/how_to_dynamically_load_evaluation_parameters_from_a_database',
-                'guides/expectations/advanced/how_to_create_a_new_expectation_suite_using_rule_based_profilers'
-              ]
-            },
-            { type: 'doc', id: 'guides/expectations/index', label: 'Index' }
-          ]
-        }
-      ]
-    },
-    {
-      type: 'category',
-      label: 'How to Guides',
-      items: [
-        {
-          type: 'category',
-          label: 'Setting up Great Expectations',
-          items: [
-            {
-              type: 'category',
-              label: 'Core skills',
-              items: [
-                // 'guides/setup/how_to_instantiate_a_data_context',
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Installation',
-              items: [
-                'guides/setup/installation/local',
-                'guides/setup/installation/hosted_environment'
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Data Contexts',
-              items: [
-                'guides/setup/configuring_data_contexts/how_to_configure_a_new_data_context_with_the_cli',
-                'guides/setup/configuring_data_contexts/how_to_configure_datacontext_components_using_test_yaml_config',
-                'guides/setup/configuring_data_contexts/how_to_configure_credentials',
-                'guides/setup/configuring_data_contexts/how_to_instantiate_a_data_context_without_a_yml_file'
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Stores',
-              items: [
-                {
-                  type: 'category',
-                  label: 'Expectation Stores',
-                  items: [
-                    'guides/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_amazon_s3',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_azure_blob_storage',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_in_gcs',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_on_a_filesystem',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_an_expectation_store_to_postgresql'
-                  ]
-                },
-                {
-                  type: 'category',
-                  label: 'Validation Result Stores',
-                  items: [
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_validation_result_store_in_amazon_s3',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_validation_result_store_in_azure_blob_storage',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_validation_result_store_in_gcs',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_validation_result_store_on_a_filesystem',
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_validation_result_store_to_postgresql'
-                  ]
-                },
-                {
-                  type: 'category',
-                  label: 'Metric Stores',
-                  items: [
-                    'guides/setup/configuring_metadata_stores/how_to_configure_a_metricsstore'
-                  ]
-                }
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Data Docs',
-              items: [
-                'guides/setup/configuring_data_docs/how_to_host_and_share_data_docs_on_a_filesystem',
-                'guides/setup/configuring_data_docs/how_to_host_and_share_data_docs_on_azure_blob_storage',
-                'guides/setup/configuring_data_docs/how_to_host_and_share_data_docs_on_gcs',
-                'guides/setup/configuring_data_docs/how_to_host_and_share_data_docs_on_amazon_s3'
-              ]
-            }
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Connecting to your data',
           items: [
             {
               type: 'category',
@@ -308,9 +176,73 @@ module.exports = {
                 // 'guides/connecting_to_your_data/advanced/how_to_create_a_lightweight_data_catalog_by_applying_a_descriptive_profiler_to_a_configured_datasource',
                 // 'guides/connecting_to_your_data/advanced/how_to_explore_changes_in_data_over_time_using_a_configured_datasource'
               ]
-            }
+            },
+            { type: 'doc', id: 'guides/connecting_to_your_data/index', label: 'Index' }
           ]
-        },
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Step 3: Create Expectations',
+      items: [
+        { type: 'doc', id: 'guides/expectations/create_expectations_overview', label: 'Overview' },
+        {
+          type: 'category',
+          label: 'How to guides',
+          items: [
+            {
+              type: 'category',
+              label: 'Core skills',
+              items: [
+                'guides/expectations/how_to_create_and_edit_expectations_based_on_domain_knowledge_without_inspecting_data_directly',
+                'guides/expectations/how_to_create_and_edit_expectations_in_bulk',
+                'guides/expectations/how_to_create_and_edit_expectations_with_a_profiler',
+                'guides/expectations/how_to_create_and_edit_expectations_with_instant_feedback_from_a_sample_batch_of_data'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Configuring Profilers',
+              items: []
+            },
+            {
+              type: 'category',
+              label: 'Creating Custom Expectations',
+              items: [
+                'guides/expectations/creating_custom_expectations/how_to_create_custom_expectations',
+                // 'guides/expectations/creating_custom_expectations/how_to_create_custom_expectations_from_a_sql_query',
+                'guides/expectations/creating_custom_expectations/how_to_create_custom_parameterized_expectations'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Contributing',
+              items: [
+                'guides/expectations/contributing/how_to_contribute_a_new_expectation_to_great_expectations'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Advanced',
+              items: [
+                'guides/expectations/advanced/how_to_add_comments_to_expectations_and_display_them_in_data_docs',
+                'guides/expectations/advanced/how_to_create_renderers_for_custom_expectations',
+                'guides/expectations/advanced/how_to_create_a_new_expectation_suite_by_profiling_from_a_jsonschema_file',
+                'guides/expectations/advanced/how_to_create_expectations_that_span_multiple_batches_using_evaluation_parameters',
+                'guides/expectations/advanced/how_to_dynamically_load_evaluation_parameters_from_a_database',
+                'guides/expectations/advanced/how_to_create_a_new_expectation_suite_using_rule_based_profilers'
+              ]
+            },
+            { type: 'doc', id: 'guides/expectations/index', label: 'Index' }
+          ]
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'How to Guides',
+      items: [
         {
           type: 'category',
           label: 'Validating your data',
