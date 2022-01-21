2446dc76bf635fb5216b9d76fa82d082cc2e4879
Chetan Kini <chetan@superconductive.com>
2022-01-21 12:32:48 -0500

    test: write test stubs

--- tests/rule_based_profiler/test_toolkit.py
+++ tests/rule_based_profiler/test_toolkit.py
@@ -1,9 +1,33 @@
 from unittest import mock
 
+from great_expectations.data_context.data_context import DataContext
+from great_expectations.data_context.store.profiler_store import ProfilerStore
 from great_expectations.data_context.types.resource_identifiers import (
     ConfigurationIdentifier,
 )
-from great_expectations.rule_based_profiler.toolkit import list_profilers
+from great_expectations.rule_based_profiler.toolkit import get_profiler, list_profilers
+
+
+def test_get_profiler(empty_data_context: DataContext):
+    store = ProfilerStore("profiler_store")
+    get_profiler(
+        data_context=empty_data_context,
+        profiler_store=store,
+        name="my_profiler",
+        ge_cloud_id=None,
+    )
+
+
+def test_get_profiler_in_cloud_mode():
+    pass
+
+
+def test_get_profiler_with_non_existent_profiler_raises_error():
+    pass
+
+
+def test_get_profiler_with_invalid_configuration_raises_error():
+    pass
 
 
 @mock.patch("great_expectations.data_context.store.ProfilerStore")
