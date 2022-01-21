318fe5e8c9a5e3dce03bde44a7fcf3733dded429
David Maxson <david.maxson@rearc.io>
2022-01-21 12:13:42 -0800

    Updated S3 sanitizer to behave more like previous sanitizer when a file or empty prefix is given

--- great_expectations/datasource/data_connector/inferred_asset_s3_data_connector.py
+++ great_expectations/datasource/data_connector/inferred_asset_s3_data_connector.py
@@ -95,8 +95,13 @@ class InferredAssetS3DataConnector(InferredAssetFilePathDataConnector):
 
     @staticmethod
     def sanitize_prefix(text: str) -> str:
-        text = text.rstrip('/') + '/'
-        return text
+        path_parts = text.split('/')
+        if not path_parts:  # Empty prefix
+            return text
+        elif '.' in path_parts[-1]:  # File, not folder
+            return text
+        else:  # Folder, should have trailing /
+            return text.rstrip('/') + '/'
 
     def build_batch_spec(self, batch_definition: BatchDefinition) -> S3BatchSpec:
         """
