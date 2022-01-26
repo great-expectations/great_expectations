f9841bc5858c43b4129e1f065f6b7e9faee50959
David Maxson <david.maxson@rearc.io>
2022-01-26 10:12:52 -0800

    Fixed tests for new int types

--- tests/expectations/core/test_expect_column_values_to_be_in_type_list.py
+++ tests/expectations/core/test_expect_column_values_to_be_in_type_list.py
@@ -3,7 +3,10 @@ import pandas as pd
 from great_expectations.core.expectation_validation_result import (
     ExpectationValidationResult,
 )
-from great_expectations.self_check.util import build_sa_validator_with_data
+from great_expectations.self_check.util import (
+    build_pandas_validator_with_data,
+    build_sa_validator_with_data,
+)
 
 
 def test_expect_column_values_to_be_in_type_list_dialect_pyathena(sa):
@@ -30,6 +33,42 @@ def test_expect_column_values_to_be_in_type_list_dialect_pyathena(sa):
         },
         result={
             "element_count": 2,
+            "unexpected_count": 0,
+            "unexpected_percent": 0.0,
+            "partial_unexpected_list": [],
+            "missing_count": 0,
+            "missing_percent": 0.0,
+            "unexpected_percent_total": 0.0,
+            "unexpected_percent_nonmissing": 0.0,
+        },
+        exception_info={
+            "raised_exception": False,
+            "exception_traceback": None,
+            "exception_message": None,
+        },
+        meta={},
+    )
+
+
+def test_expect_column_values_to_be_in_type_list_nullable_int():
+    df = pd.DataFrame({"col": pd.Series([1, 2, None], dtype=pd.Int32Dtype())})
+    validator = build_pandas_validator_with_data(df)
+
+    result = validator.expect_column_values_to_be_in_type_list(
+        "col", type_list=["Int32Dtype"]
+    )
+    assert result == ExpectationValidationResult(
+        success=True,
+        expectation_config={
+            "expectation_type": "expect_column_values_to_be_in_type_list",
+            "kwargs": {
+                "column": "col",
+                "type_list": ["Int32Dtype"],
+            },
+            "meta": {},
+        },
+        result={
+            "element_count": 3,
             "unexpected_count": 0,
             "unexpected_percent": 0.0,
             "partial_unexpected_list": [],
