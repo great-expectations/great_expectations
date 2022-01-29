7845d3ba8120a1fb0628566be7250eb39ba0156f
Alex Sherstinsky <alex@superconductive.com>
2022-01-29 00:34:52 -0800

    Fix YAML config tests.

--- great_expectations/rule_based_profiler/rule_based_profiler.py
+++ great_expectations/rule_based_profiler/rule_based_profiler.py
@@ -573,6 +573,32 @@ class RuleBasedProfiler:
         rule: Rule
         return {rule.name: rule for rule in self._rules}
 
+    def self_check(self, pretty_print=True) -> dict:
+        """
+        Necessary to enable integration with `DataContext.test_yaml_config`
+        Args:
+            pretty_print: flag to turn on verbose output
+        Returns:
+            Dictionary that contains RuleBasedProfiler state
+        """
+        # Provide visibility into parameters that RuleBasedProfiler was instantiated with.
+        report_object: dict = {"config": self._citation}
+
+        if pretty_print:
+            print(f"\nRuleBasedProfiler class name: {self.name}")
+
+            if not self._variables:
+                print(
+                    'Your current RuleBasedProfiler configuration has an empty "variables" attribute. \
+                    Please ensure you populate it if you\'d like to reference values in your "rules" attribute.'
+                )
+
+        return report_object
+
+    @property
+    def name(self) -> str:
+        return self._name
+
     @property
     def variables(self) -> Optional[ParameterContainer]:
         # Returning a copy of the "self._variables" state variable in order to prevent write-before-read hazard.
