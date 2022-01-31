b47339155709aaaac6e6da5bfc8a977ad0d8ed01
Chetan Kini <chetan@superconductive.com>
2022-01-31 09:17:25 -0500

    [FEATURE] Delete profilers from DataContext (#4067)
    * feat: start impl
    
    * chore: reference new store in init and anonymizer
    
    * chore: also reference class in test_yaml_config
    
    * feat: update class interface
    
    * chore: start marking parts of codebase that need ref to ProfilerStore
    
    * chore: fix defaults in schema
    
    * feat: implement serialization_self_check
    
    * test: start writing unit tests
    
    * test: continue test impl
    
    * test: continue test impl
    
    * feat: update backend defaults schemas to include profiler
    
    * test: fix all DataContext UI tests to work with profiler store
    
    * feat: implement properties
    
    * chore: saving work from Alex pairing
    
    * feat: continue refactored impl
    
    * feat: finish refactor impl
    
    * chore: remove print statement
    
    * chore: fix typo
    
    * test: fix tests
    
    * test: rename test
    
    * feat: add other schema vars
    
    * feat: update properties per Alex's review
    
    * refactor: move utility to test utils
    
    * chore: minor change to error message in properties
    
    * feat: ensure that filesystem and config is updated
    
    * chore: reuse profilers_path var
    
    * chore: misc cleanup
    
    * feat: add toolkit
    
    * feat: start impl
    
    * test: add final tests
    
    * feat: start impl
    
    * test: write test stubs
    
    * fix: correct cyclic import issue with type hints
    
    * test: continue test impl
    
    * chore: remove invalid path from Checkpoint toolkit
    
    * feat: continue impl
    
    * refactor: start refactor
    
    * refactor: continue modifying __init__
    
    * refactor: clean up constructor
    
    * refactor: additional refactoring
    
    * docs passing sqlalchemy.create_engine kwargs through connection_string
    
    Added to docs in docs/guides/connecting_to_your_data/database/*.md;
    explains how to pass custom params via use of `connection_string`
    passing kwargs.
    
    * discuss and check with sql, stub submitted; passed reader_options and reader_method thus far
    
    * refactor: post-Alex comments
    
    * refactor: continue refactor
    
    * fix: ensure profiler store works with changes
    
    * docs: update docs under test
    
    * chore: restore misc docs changes
    
    * test: update docs under test
    
    * chore: add linted file
    
    * chore: remove DataContext import to remove cyclic issue
    
    * refactor: continue impl
    
    * docs: revert docs changes
    
    * feat: misc changes after convo with Alex & Will
    
    * test: fix all tests
    
    * docs: misc comments to clarify `pop()`
    
    * refactor: clean up init
    
    * chore: misc changes from Alex review
    
    * chore: type annotate batch requests
    
    * chore: remove accidental whitespace
    
    * chore: additional refactors per Alex comments
    
    * chore: add back str to batch request type annotation
    
    * refactor: ensure rename from parameter_name to name
    
    * fix: fix schema validation in RBP constructor
    
    * chore: misc updates
    
    * chore: another update
    
    * chore: revert checkpoint change
    
    * chore: misc changes
    
    * chore: continue debugging
    
    * fix: update post_dump to include unknown fields
    
    * docs: write docstring for post_dump
    
    * refactor: bump up fixtures to global conftest
    
    * feat: implement method and tests
    
    * test: add additional assert to test
    
    Co-authored-by: h4nn3rs <spacehanners@gmail.com>
    Co-authored-by: Hannah (✿◕‿◕) <hannah@superconductive.com>

--- tests/rule_based_profiler/test_toolkit.py
+++ tests/rule_based_profiler/test_toolkit.py
@@ -9,7 +9,27 @@ from great_expectations.data_context.types.resource_identifiers import (
 )
 from great_expectations.rule_based_profiler import RuleBasedProfiler
 from great_expectations.rule_based_profiler.config.base import RuleBasedProfilerConfig
-from great_expectations.rule_based_profiler.toolkit import get_profiler, list_profilers
+from great_expectations.rule_based_profiler.toolkit import (
+    delete_profiler,
+    get_profiler,
+    list_profilers,
+)
+
+
+@mock.patch("great_expectations.data_context.data_context.DataContext")
+def test_get_profiler_with_too_many_args_raises_error(
+    mock_data_context: mock.MagicMock,
+    populated_profiler_store: ProfilerStore,
+):
+    with pytest.raises(AssertionError) as e:
+        get_profiler(
+            data_context=mock_data_context,
+            profiler_store=populated_profiler_store,
+            name="my_profiler",
+            ge_cloud_id="my_ge_cloud_id",
+        )
+
+    assert "either name or ge_cloud_id" in str(e.value)
 
 
 @mock.patch("great_expectations.data_context.data_context.DataContext")
@@ -41,6 +61,50 @@ def test_get_profiler_non_existent_profiler_raises_error(
             data_context=mock_data_context,
             profiler_store=empty_profiler_store,
             name="my_profiler",
+            ge_cloud_id=None,
+        )
+
+    assert "Non-existent Profiler" in str(e.value)
+
+
+def test_delete_profiler(
+    populated_profiler_store: ProfilerStore,
+):
+    with mock.patch(
+        "great_expectations.data_context.store.profiler_store.ProfilerStore.remove_key",
+    ) as mock_remove_key:
+        delete_profiler(
+            profiler_store=populated_profiler_store,
+            name="my_profiler",
+            ge_cloud_id=None,
+        )
+
+    assert mock_remove_key.call_count == 1
+    assert mock_remove_key.call_args == mock.call(
+        key=ConfigurationIdentifier("my_profiler")
+    )
+
+
+def test_delete_profiler_with_too_many_args_raises_error(
+    populated_profiler_store: ProfilerStore,
+):
+    with pytest.raises(AssertionError) as e:
+        delete_profiler(
+            profiler_store=populated_profiler_store,
+            name="my_profiler",
+            ge_cloud_id="my_ge_cloud_id",
+        )
+
+    assert "either name or ge_cloud_id" in str(e.value)
+
+
+def test_delete_profiler_non_existent_profiler_raises_error(
+    populated_profiler_store: ProfilerStore,
+):
+    with pytest.raises(ge_exceptions.ProfilerNotFoundError) as e:
+        delete_profiler(
+            profiler_store=populated_profiler_store,
+            name="my_non_existent_profiler",
             ge_cloud_id=None,
         )
 
