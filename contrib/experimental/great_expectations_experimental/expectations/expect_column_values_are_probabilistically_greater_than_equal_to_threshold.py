f32158c82d28c3f7fb5342e9c6ff56bc7cd0eaab
taylorfturner <taylorfturner@gmail.com>
2022-02-01 13:39:01 -0500

    update docstring on expectations

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_are_probabilistically_greater_than_equal_to_threshold.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_are_probabilistically_greater_than_equal_to_threshold.py
@@ -7,8 +7,10 @@ For detailed instructions on how to use it, please see:
 import dataprofiler as dp
 import json
 import numpy as np
+
 # remove extra tf loggin
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from great_expectations.execution_engine import (
@@ -26,9 +28,11 @@ from great_expectations.expectations.metrics import (
 class ColumnValuesConfidenceToBeGreaterThanOrEqualToThreshold(ColumnMapMetricProvider):
     """MetricProvider Class for determining if the dataprofiler's model confidence value is\
         greater than or equal to the user-specified threshold"""
-    
+
     # This is the id string that will be used to reference your metric.
-    condition_metric_name = "column_values.prediction_confidence_greater_than_or_equal_to_threshold"
+    condition_metric_name = (
+        "column_values.prediction_confidence_greater_than_or_equal_to_threshold"
+    )
 
     condition_value_keys = ("threshold",)
 
@@ -38,19 +42,27 @@ class ColumnValuesConfidenceToBeGreaterThanOrEqualToThreshold(ColumnMapMetricPro
         """
         Implement the yes/no question for the expectation
         """
-        labeler = dp.DataLabeler(labeler_type='structured')
+        labeler = dp.DataLabeler(labeler_type="structured")
         labeler.postprocessor.set_params(is_pred_labels=False)
         try:
-            results = labeler.predict(column, predict_options={"show_confidences": True})
+            results = labeler.predict(
+                column, predict_options={"show_confidences": True}
+            )
         except:
             results = None
-        return np.choose(results['pred'].astype(int, copy=False), results['conf'].T) >= threshold
+        return (
+            np.choose(results["pred"].astype(int, copy=False), results["conf"].T)
+            >= threshold
+        )
+
+
 class ExpectColumnValuesToBeGreaterThanOrEqualToThreshold(ColumnMapExpectation):
     """
     This function builds upon the custom column map expectations of Great Expectations. This function asks the question a yes/no question of each row in the user-specified column; namely, does the confidence threshold provided by the DataProfiler model exceed the user-specified threshold.
-
-    :param: column,
-    :param: threshold
+    
+    Args: 
+        column (str): The column name that you want to check.
+        threshold (float): The value, usually as a decimal (e.g. .32), you want to use to flag low confidence predictions
 
     df.expect_column_values_to_probabilistically_match_data_label(
         column,
@@ -61,32 +73,54 @@ class ExpectColumnValuesToBeGreaterThanOrEqualToThreshold(ColumnMapExpectation):
     examples = [
         {
             "data": {
-                "OPEID6": ['1002', '1052', '25034', "McRoomyRoom"],
-                "INSTNM": ['Alabama A & M University',
-                    'University of Alabama at Birmingham', 'Amridge University', "McRoomyRoom"],
-                "ZIP": ['35762', '35294-0110', '36117-3553', "McRoomyRoom"],
-                "ACCREDAGENCY": ['Southern Association of Colleges and Schools Commission on Colleges',
-                    'Southern Association of Colleges and Schools Commission on Colleges',
-                    'Southern Association of Colleges and Schools Commission on Colleges', "McRoomyRoom"],
-                "INSTURL": ['www.aamu.edu/', 'https://www.uab.edu',
-                    'www.amridgeuniversity.edu', "McRoomyRoom"],
-                "NPCURL": ['www.aamu.edu/admissions-aid/tuition-fees/net-price-calculator.html',
-                    'https://uab.studentaidcalculator.com/survey.aspx',
-                    'www2.amridgeuniversity.edu:9091/', "McRoomyRoom"],
-                "LATITUDE": ['34.783368', '33.505697', '32.362609', "McRoomyRoom"],
-                "LONGITUDE": ['-86.568502', '-86.799345', '-86.17401', "McRoomyRoom"],
-                "RELAFFIL": ['NULL', 'NULL', '74', "McRoomyRoom"],
-                "DEATH_YR2_RT": ['PrivacySuppressed', 'PrivacySuppressed', 'PrivacySuppressed', "McRoomyRoom"],
-                "SEARCH_STRING": ['Alabama A & M University AAMU',
-                    'University of Alabama at Birmingham ',
-                    'Amridge University Southern Christian University  Regions University', "McRoomyRoom"],
+                "OPEID6": ["1002", "1052", "25034", "McRoomyRoom"],
+                "INSTNM": [
+                    "Alabama A & M University",
+                    "University of Alabama at Birmingham",
+                    "Amridge University",
+                    "McRoomyRoom",
+                ],
+                "ZIP": ["35762", "35294-0110", "36117-3553", "McRoomyRoom"],
+                "ACCREDAGENCY": [
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "McRoomyRoom",
+                ],
+                "INSTURL": [
+                    "www.aamu.edu/",
+                    "https://www.uab.edu",
+                    "www.amridgeuniversity.edu",
+                    "McRoomyRoom",
+                ],
+                "NPCURL": [
+                    "www.aamu.edu/admissions-aid/tuition-fees/net-price-calculator.html",
+                    "https://uab.studentaidcalculator.com/survey.aspx",
+                    "www2.amridgeuniversity.edu:9091/",
+                    "McRoomyRoom",
+                ],
+                "LATITUDE": ["34.783368", "33.505697", "32.362609", "McRoomyRoom"],
+                "LONGITUDE": ["-86.568502", "-86.799345", "-86.17401", "McRoomyRoom"],
+                "RELAFFIL": ["NULL", "NULL", "74", "McRoomyRoom"],
+                "DEATH_YR2_RT": [
+                    "PrivacySuppressed",
+                    "PrivacySuppressed",
+                    "PrivacySuppressed",
+                    "McRoomyRoom",
+                ],
+                "SEARCH_STRING": [
+                    "Alabama A & M University AAMU",
+                    "University of Alabama at Birmingham ",
+                    "Amridge University Southern Christian University  Regions University",
+                    "McRoomyRoom",
+                ],
             },
             "tests": [
                 {
                     "title": "positive_test_with_column_one",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "OPEID6",  "threshold": .00},
+                    "in": {"column": "OPEID6", "threshold": 0.00},
                     "out": {
                         "success": True,
                         "unexpected_index_list": [],
@@ -97,8 +131,8 @@ class ExpectColumnValuesToBeGreaterThanOrEqualToThreshold(ColumnMapExpectation):
                     "title": "error_test_threshold",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "ZIP", "threshold": .61},
-                    "out":{
+                    "in": {"column": "ZIP", "threshold": 0.61},
+                    "out": {
                         "success": False,
                     },
                 },
@@ -108,10 +142,15 @@ class ExpectColumnValuesToBeGreaterThanOrEqualToThreshold(ColumnMapExpectation):
 
     # This is the id string of the Metric used by this Expectation.
     # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
-    map_metric = "column_values.prediction_confidence_greater_than_or_equal_to_threshold"
+    map_metric = (
+        "column_values.prediction_confidence_greater_than_or_equal_to_threshold"
+    )
 
     # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("threshold", "mostly",)
+    success_keys = (
+        "threshold",
+        "mostly",
+    )
 
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {}
@@ -125,6 +164,9 @@ class ExpectColumnValuesToBeGreaterThanOrEqualToThreshold(ColumnMapExpectation):
         ],
     }
 
+
 if __name__ == "__main__":
-    diagnostics_report = ExpectColumnValuesToBeGreaterThanOrEqualToThreshold().run_diagnostics()
+    diagnostics_report = (
+        ExpectColumnValuesToBeGreaterThanOrEqualToThreshold().run_diagnostics()
+    )
     print(json.dumps(diagnostics_report, indent=2))
--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_confidence_to_be_greater_than_or_equal_to_threshold_for_data_label.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_confidence_to_be_greater_than_or_equal_to_threshold_for_data_label.py
@@ -7,8 +7,10 @@ For detailed instructions on how to use it, please see:
 import dataprofiler as dp
 import json
 import numpy as np
+
 # remove extra tf loggin
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from great_expectations.execution_engine import (
@@ -23,14 +25,19 @@ from great_expectations.expectations.metrics import (
 )
 
 
-class ColumnValuesConfidenceForDataLabelToBeGreaterThanOrEqualToThreshold(ColumnMapMetricProvider):
+class ColumnValuesConfidenceForDataLabelToBeGreaterThanOrEqualToThreshold(
+    ColumnMapMetricProvider
+):
     """MetricProvider Class for Data Label Probability greater than \
     or equal to the user-specified threshold"""
-    
+
     # This is the id string that will be used to reference your metric.
     condition_metric_name = "column_values.prediction_confidence_for_data_label_greater_than_or_equal_to_threshold"
 
-    condition_value_keys = ("threshold", "data_label",)
+    condition_value_keys = (
+        "threshold",
+        "data_label",
+    )
 
     # This method implements the core logic for the PandasExecutionEngine
     @column_condition_partial(engine=PandasExecutionEngine)
@@ -38,33 +45,42 @@ class ColumnValuesConfidenceForDataLabelToBeGreaterThanOrEqualToThreshold(Column
         """
         Implement the yes/no question for the expectation
         """
-        labeler = dp.DataLabeler(labeler_type='structured')
+        labeler = dp.DataLabeler(labeler_type="structured")
         labeler.postprocessor.set_params(is_pred_labels=False)
         try:
-            results = labeler.predict(column, predict_options={"show_confidences": True},)
+            results = labeler.predict(
+                column,
+                predict_options={"show_confidences": True},
+            )
         except:
             results = None
 
         if data_label.upper() in labeler.label_mapping.keys():
             data_label_ind = labeler.label_mapping[data_label.upper()]
         else:
-            raise ValueError("""
+            raise ValueError(
+                """
                 The only values acceptable for the data label parameter are as follows:
                 ['PAD', 'UNKNOWN', 'ADDRESS', 'BAN', 'CREDIT_CARD', 'DATE', 'TIME', 'DATETIME',\
                     'DRIVERS_LICENSE', 'EMAIL_ADDRESS', 'UUID', 'HASH_OR_KEY', 'IPV4', 'IPV6',\
                     'MAC_ADDRESS', 'PERSON', 'PHONE_NUMBER', 'SSN', 'URL', 'US_STATE', 'INTEGER',\
                     'FLOAT', 'QUANTITY', 'ORDINAL']
-            """)
-        data_label_conf = results['conf'][:, data_label_ind]
+            """
+            )
+        data_label_conf = results["conf"][:, data_label_ind]
         return data_label_conf >= threshold
 
-class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold(ColumnMapExpectation):
+
+class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold(
+    ColumnMapExpectation
+):
     """
     This function builds upon the custom column map expectations of Great Expectations. This function asks the question a yes/no question of each row in the user-specified column; namely, does the confidence threshold provided by the DataProfiler model exceed the user-specified threshold.
-
-    :param: column,  
-    :param: data_label,
-    :param: threshold
+    
+    Args: 
+        column (str): The column name that you want to check.
+        data_label(str): The data label for which you want to check confidences against the threshold value
+        threshold (float): The value, usually as a decimal (e.g. .32), you want to use to flag low confidence predictions
 
     df.expect_column_values_to_probabilistically_match_data_label(
         column,
@@ -76,36 +92,54 @@ class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold
     examples = [
         {
             "data": {
-                "OPEID6": ['1002', '1052', '25034', "McRoomyRoom"],
-                "INSTNM": ['Alabama A & M University',
-                    'University of Alabama at Birmingham', 'Amridge University', "McRoomyRoom"],
-                "ZIP": ['35762', '35294-0110', '36117-3553', "McRoomyRoom"],
-                "ACCREDAGENCY": ['Southern Association of Colleges and Schools Commission on Colleges',
-                    'Southern Association of Colleges and Schools Commission on Colleges',
-                    'Southern Association of Colleges and Schools Commission on Colleges', "McRoomyRoom"],
-                "INSTURL": ['www.aamu.edu/', 'https://www.uab.edu',
-                    'www.amridgeuniversity.edu', "McRoomyRoom"],
-                "NPCURL": ['www.aamu.edu/admissions-aid/tuition-fees/net-price-calculator.html',
-                    'https://uab.studentaidcalculator.com/survey.aspx',
-                    'www2.amridgeuniversity.edu:9091/', "McRoomyRoom"],
-                "LATITUDE": ['34.783368', '33.505697', '32.362609', "McRoomyRoom"],
-                "LONGITUDE": ['-86.568502', '-86.799345', '-86.17401', "McRoomyRoom"],
-                "RELAFFIL": ['NULL', 'NULL', '74', "McRoomyRoom"],
-                "DEATH_YR2_RT": ['PrivacySuppressed', 'PrivacySuppressed', 'PrivacySuppressed', "McRoomyRoom"],
-                "SEARCH_STRING": ['Alabama A & M University AAMU',
-                    'University of Alabama at Birmingham ',
-                    'Amridge University Southern Christian University  Regions University', "McRoomyRoom"],
+                "OPEID6": ["1002", "1052", "25034", "McRoomyRoom"],
+                "INSTNM": [
+                    "Alabama A & M University",
+                    "University of Alabama at Birmingham",
+                    "Amridge University",
+                    "McRoomyRoom",
+                ],
+                "ZIP": ["35762", "35294-0110", "36117-3553", "McRoomyRoom"],
+                "ACCREDAGENCY": [
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "Southern Association of Colleges and Schools Commission on Colleges",
+                    "McRoomyRoom",
+                ],
+                "INSTURL": [
+                    "www.aamu.edu/",
+                    "https://www.uab.edu",
+                    "www.amridgeuniversity.edu",
+                    "McRoomyRoom",
+                ],
+                "NPCURL": [
+                    "www.aamu.edu/admissions-aid/tuition-fees/net-price-calculator.html",
+                    "https://uab.studentaidcalculator.com/survey.aspx",
+                    "www2.amridgeuniversity.edu:9091/",
+                    "McRoomyRoom",
+                ],
+                "LATITUDE": ["34.783368", "33.505697", "32.362609", "McRoomyRoom"],
+                "LONGITUDE": ["-86.568502", "-86.799345", "-86.17401", "McRoomyRoom"],
+                "RELAFFIL": ["NULL", "NULL", "74", "McRoomyRoom"],
+                "DEATH_YR2_RT": [
+                    "PrivacySuppressed",
+                    "PrivacySuppressed",
+                    "PrivacySuppressed",
+                    "McRoomyRoom",
+                ],
+                "SEARCH_STRING": [
+                    "Alabama A & M University AAMU",
+                    "University of Alabama at Birmingham ",
+                    "Amridge University Southern Christian University  Regions University",
+                    "McRoomyRoom",
+                ],
             },
             "tests": [
                 {
                     "title": "positive_test_with_column_one",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {
-                        "column": "ZIP",
-                        "data_label": "ADDRESS",
-                        "threshold": .00
-                        },
+                    "in": {"column": "ZIP", "data_label": "ADDRESS", "threshold": 0.00},
                     "out": {
                         "success": True,
                     },
@@ -114,11 +148,7 @@ class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold
                     "title": "failing_test_with_column_one",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {
-                        "column": "ZIP",
-                        "data_label": "ADDRESS",
-                        "threshold": 1.00
-                        },
+                    "in": {"column": "ZIP", "data_label": "ADDRESS", "threshold": 1.00},
                     "out": {
                         "success": False,
                     },
@@ -132,7 +162,11 @@ class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold
     map_metric = "column_values.prediction_confidence_for_data_label_greater_than_or_equal_to_threshold"
 
     # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("threshold", "data_label", "mostly",)
+    success_keys = (
+        "threshold",
+        "data_label",
+        "mostly",
+    )
 
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {}
@@ -146,6 +180,9 @@ class ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold
         ],
     }
 
+
 if __name__ == "__main__":
-    diagnostics_report = ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold().run_diagnostics()
+    diagnostics_report = (
+        ExpectColumnsValuesConfidenceForDataLabelToBeGreaterThanOrEqualtoThreshold().run_diagnostics()
+    )
     print(json.dumps(diagnostics_report, indent=2))
