03cec7266b283065930c7d062ddbc0102770fed0
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2022-02-01 14:40:29 -0500

    move process_evaluation_parameters into conditional

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -292,9 +292,6 @@ class Validator:
             configuration = ExpectationConfiguration(
                 expectation_type=name, kwargs=expectation_kwargs, meta=meta
             )
-            configuration.process_evaluation_parameters(
-                self._expectation_suite.evaluation_parameters, True, self._data_context
-            )
 
             exception_info: ExceptionInfo
 
@@ -307,6 +304,9 @@ class Validator:
                         expectation_config=copy.deepcopy(expectation.configuration)
                     )
                 else:
+                    configuration.process_evaluation_parameters(
+                        self._expectation_suite.evaluation_parameters, True, self._data_context
+                    )
                     validation_result = expectation.validate(
                         validator=self,
                         evaluation_parameters=self._expectation_suite.evaluation_parameters,
