166706b1a2135ced56d9c4e538e3d2906275ba61
Shinnnyshinshin <will@superconductive.com>
2022-02-07 22:00:06 -0800

    Revert "before final clean up"
    This reverts commit 6035660b8a31f61cc0525ebd37a7114a8d523e7c.

--- tests/integration/docusaurus/miscellaneous/migration_guide_spark_v2_api.py
+++ tests/integration/docusaurus/miscellaneous/migration_guide_spark_v2_api.py
@@ -84,6 +84,16 @@ batches:
       - Titanic.profiled
 """
 
+
+import os
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
+
+print("~~~~~~")
 assert actual_checkpoint_yaml == yaml.safe_load(expected_checkpoint_yaml)
 
 # override for integration tests
