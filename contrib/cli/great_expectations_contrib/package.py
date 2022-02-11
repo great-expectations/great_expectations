2ebb65b5d405c6e41aed556a119fac7625678bf0
Chetan Kini <chetan@superconductive.com>
2022-02-11 08:33:13 -0500

    [FEATURE] Add parsing of Expectation diagnostics to contrib packaging JSON object (#4114)
    * feat: add JSON object classes
    
    * feat: add script to JSON parse
    
    * chore: update enum value per @abegong
    
    * refactor: move types into CLI tool
    
    * feat: add diagnostic parsing support to package class
    
    * feat: implement dynamic importing to retrieve Expectations
    
    * refactor: use issubclass checks
    
    * feat: misc cleanup to ensure proper serialization
    
    * fix: misc fixes to get hook working
    
    * chore: update logger calls
    
    * chore: add example Package JSON
    
    * refactor: use enum for social media types
    
    * feat: make dataclasses frozen
    
    * chore: make to_json_file func private
    
    * chore: remove unnecessary imports
    
    * refactor: rename types to package
    
    * chore: add better exception/errors
    
    * chore: misc comments
    
    * refactor: move serialization methods out of class
    
    * refactor: use Config suffix
    
    * refactor: remove Config in place of Manifest
    
    * fix: rename config to manifest
    
    * feat: start parsing diagnostics
    
    * feat: parse requirements
    
    * feat: update requirements parsing
    
    * refactor: make manifest mutable
    
    * feat: continue impl
    
    * feat: continue update attr feature
    
    * test: add tests for update methods
    
    * chore: revert files back to hackathon-docs state
    
    * fix: push unlinted changes
    
    * chore: revert docs
    
    * chore: revert azure yml
    
    * chore: delete extraneous files
    
    * chore: revert additional files
    
    * chore: revert final files
    
    * test: add test for static attrs
    
    * chore: revert cli.py
    
    * feat: exit early with bad requirements path
    
    * feat: ensure enums are JSON serializable
    
    * feat: add sync cmd
    
    * chore: update comments
    
    * chore: additional cleanup
    
    * chore: misc updates
    
    * feat: add parsing support for package_info.yml
    
    * test: start writing tests for addl parsing
    
    * fix: fix filenotfound error issue
    
    * feat: write build script for CI/CD uploading to S3
    
    * chore: misc updates
    
    * chore: misc cleanup
    
    * chore: bump black requirement

--- contrib/cli/great_expectations_contrib/package.py
+++ contrib/cli/great_expectations_contrib/package.py
@@ -5,32 +5,38 @@ import os
 import sys
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, List, Optional, Type
+from typing import Any, List, Optional, Type
 
+import pkg_resources
+from ruamel.yaml import YAML
+
+from great_expectations.core.expectation_diagnostics.expectation_diagnostics import (
+    ExpectationDiagnostics,
+)
 from great_expectations.expectations.expectation import Expectation
+from great_expectations.types import SerializableDictDot
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# Type alias that will need to be updated to reflect the complex nature of the 'run_diagnostics' return object
-Diagnostics = Dict[str, Any]
+yaml = YAML()
 
 
-@dataclass(frozen=True)
-class ExpectationCompletenessCheck:
+@dataclass
+class ExpectationCompletenessCheck(SerializableDictDot):
     message: str
     passed: bool
 
 
-@dataclass(frozen=True)
-class ExpectationCompletenessChecklist:
+@dataclass
+class ExpectationCompletenessChecklist(SerializableDictDot):
     experimental: List[ExpectationCompletenessCheck]
     beta: List[ExpectationCompletenessCheck]
     production: List[ExpectationCompletenessCheck]
 
 
-@dataclass(frozen=True)
-class PackageCompletenessStatus:
+@dataclass
+class PackageCompletenessStatus(SerializableDictDot):
     concept_only: int
     experimental: int
     beta: int
@@ -38,56 +44,56 @@ class PackageCompletenessStatus:
     total: int
 
 
-@dataclass(frozen=True)
-class RenderedExpectation:
+@dataclass
+class RenderedExpectation(SerializableDictDot):
     name: str
     tags: List[str]
     supported: List[str]
     status: ExpectationCompletenessChecklist
 
 
-@dataclass(frozen=True)
-class Dependency:
+@dataclass
+class Dependency(SerializableDictDot):
     text: str
     link: str
-    version: Optional[str]
+    version: Optional[str] = None
 
 
-@dataclass(frozen=True)
-class GitHubUser:
+@dataclass
+class GitHubUser(SerializableDictDot):
     username: str
-    full_name: Optional[str]
+    full_name: Optional[str] = None
 
 
-class SocialLinkType(Enum):
+class SocialLinkType(str, Enum):
     TWITTER = "TWITTER"
     INSTAGRAM = "INSTAGRAM"
     LINKEDIN = "LINKEDIN"
     MEDIUM = "MEDIUM"
 
 
-@dataclass(frozen=True)
-class SocialLink:
+@dataclass
+class SocialLink(SerializableDictDot):
     account_type: SocialLinkType
     identifier: str
 
 
-@dataclass(frozen=True)
-class DomainExpert:
+@dataclass
+class DomainExpert(SerializableDictDot):
     full_name: str
-    social_links: List[SocialLink]
-    picture: str
+    social_links: Optional[List[SocialLink]] = None
+    picture: Optional[str] = None
 
 
-class Maturity(Enum):
+class Maturity(str, Enum):
     CONCEPT_ONLY = "CONCEPT_ONLY"
     EXPERIMENTAL = "EXPERIMENTAL"
     BETA = "BETA"
     PRODUCTION = "PRODUCTION"
 
 
-@dataclass(frozen=True)
-class GreatExpectationsContribPackageManifest:
+@dataclass
+class GreatExpectationsContribPackageManifest(SerializableDictDot):
     # Core
     package_name: Optional[str] = None
     icon: Optional[str] = None
@@ -110,22 +116,131 @@ class GreatExpectationsContribPackageManifest:
         """
         Parses diagnostic reports from package Expectations and uses them to update JSON state
         """
-        diagnostics = self._retrieve_package_expectations_diagnostics()
+        diagnostics = (
+            GreatExpectationsContribPackageManifest.retrieve_package_expectations_diagnostics()
+        )
         self._update_attrs_with_diagnostics(diagnostics)
 
-    def _update_attrs_with_diagnostics(self, diagnostics: List[Diagnostics]) -> None:
-        # TODO: Write logic to assign values to attrs
-        # This is a black box for now
-        # for diagnostic in diagnostics:
-        #     pass
-        raise NotImplementedError
+    def _update_attrs_with_diagnostics(
+        self, diagnostics: List[ExpectationDiagnostics]
+    ) -> None:
+        self._update_from_package_info("package_info.yml")
+        self._update_expectations(diagnostics)
+        self._update_dependencies("requirements.txt")
+        self._update_contributors(diagnostics)
 
-    def _retrieve_package_expectations_diagnostics(self) -> List[Diagnostics]:
+    def _update_from_package_info(self, path: str) -> None:
+        if not os.path.exists(path):
+            logger.warning(f"Could not find package info file {path}")
+            return
+
+        with open(path) as f:
+            data: dict = yaml.load(f.read())
+
+        if not data:
+            logger.warning(f"{path} is empty so exiting early")
+            return
+
+        # Assign general attrs
+        general = data.get("general")
+        if general:
+            for attr in ("package_name", "icon", "description"):
+                self[attr] = general.get(attr)
+
+        # Assign code owners
+        code_owners = data.get("code_owners")
+        if code_owners:
+            self.code_owners = []
+            for owner in code_owners:
+                code_owner = GitHubUser(**owner)
+                self.code_owners.append(code_owner)
+
+        # Assign domain experts
+        domain_experts = data.get("domain_experts")
+        if domain_experts:
+            self.domain_experts = []
+            for expert in domain_experts:
+                domain_expert = DomainExpert(**expert)
+                self.domain_experts.append(domain_expert)
+
+    def _update_expectations(self, diagnostics: List[ExpectationDiagnostics]) -> None:
+        expectations = []
+        status = {maturity.name: 0 for maturity in Maturity}
+
+        for diagnostic in diagnostics:
+            expectation = RenderedExpectation(
+                name=diagnostic.description.snake_name,
+                tags=diagnostic.library_metadata.tags,
+                supported=[],
+                status=diagnostic.maturity_checklist,  # Should be converted to the proper type
+            )
+            expectations.append(expectation)
+
+            expectation_maturity = diagnostic.library_metadata.maturity
+            status[expectation_maturity] += 1
+
+        self.expectations = expectations
+        self.expectation_count = len(expectations)
+
+        # Enum is all caps but status attributes are lowercase
+        lowercase_status = {k.lower(): v for k, v in status.items()}
+        lowercase_status["total"] = sum(status.values())
+        self.status = PackageCompletenessStatus(**lowercase_status)
+
+        maturity = max(status, key=status.get)  # Get the key with the max value
+        self.maturity = Maturity[maturity]
+
+    def _update_dependencies(self, path: str) -> None:
+        if not os.path.exists(path):
+            logger.warning(f"Could not find requirements file {path}")
+            return
+
+        with open(path) as f:
+            requirements = [req for req in pkg_resources.parse_requirements(f)]
+
+        def _convert_to_dependency(
+            requirement: pkg_resources.Requirement,
+        ) -> Dependency:
+            name = requirement.project_name
+            pypi_url = f"https://pypi.org/project/{name}"
+            if requirement.specs:
+                # Stringify tuple of pins
+                version = ", ".join(
+                    "".join(symbol for symbol in pin)
+                    for pin in sorted(requirement.specs)
+                )
+            else:
+                version = None
+            return Dependency(text=name, link=pypi_url, version=version)
+
+        dependencies = list(map(_convert_to_dependency, requirements))
+        self.dependencies = dependencies
+
+    def _update_contributors(self, diagnostics: List[ExpectationDiagnostics]) -> None:
+        contributors = []
+        for diagnostic in diagnostics:
+            for contributor in diagnostic.library_metadata.contributors:
+                github_user = GitHubUser(contributor)
+                if github_user not in contributors:
+                    contributors.append(github_user)
+
+        self.contributors = contributors
+
+    @staticmethod
+    def retrieve_package_expectations_diagnostics() -> List[ExpectationDiagnostics]:
         try:
-            package = self._identify_user_package()
-            expectations_module = self._import_expectations_module(package)
-            expectations = self._retrieve_expectations_from_module(expectations_module)
-            diagnostics = self._gather_diagnostics(expectations)
+            package = GreatExpectationsContribPackageManifest._identify_user_package()
+            expectations_module = (
+                GreatExpectationsContribPackageManifest._import_expectations_module(
+                    package
+                )
+            )
+            expectations = GreatExpectationsContribPackageManifest._retrieve_expectations_from_module(
+                expectations_module
+            )
+            diagnostics = GreatExpectationsContribPackageManifest._gather_diagnostics(
+                expectations
+            )
             return diagnostics
         except Exception as e:
             # Exceptions should not break the CLI - this behavior should be working in the background
@@ -135,7 +250,8 @@ class GreatExpectationsContribPackageManifest:
             )
             return []
 
-    def _identify_user_package(self) -> str:
+    @staticmethod
+    def _identify_user_package() -> str:
         # Guaranteed to have a dir named '<MY_PACKAGE>_expectations' through Cookiecutter validation
         packages = [
             d for d in os.listdir() if os.path.isdir(d) and d.endswith("_expectations")
@@ -149,7 +265,8 @@ class GreatExpectationsContribPackageManifest:
 
         return packages[0]
 
-    def _import_expectations_module(self, package: str) -> Any:
+    @staticmethod
+    def _import_expectations_module(package: str) -> Any:
         # Need to add user's project to the PYTHONPATH
         cwd = os.getcwd()
         sys.path.append(cwd)
@@ -159,8 +276,9 @@ class GreatExpectationsContribPackageManifest:
         except ModuleNotFoundError:
             raise
 
+    @staticmethod
     def _retrieve_expectations_from_module(
-        self, expectations_module: Any
+        expectations_module: Any,
     ) -> List[Type[Expectation]]:
         expectations: List[Type[Expectation]] = []
         names: List[str] = []
@@ -172,9 +290,10 @@ class GreatExpectationsContribPackageManifest:
         logger.info(f"Found {len(names)} expectation(s): {names}")
         return expectations
 
+    @staticmethod
     def _gather_diagnostics(
-        self, expectations: List[Type[Expectation]]
-    ) -> List[Diagnostics]:
+        expectations: List[Type[Expectation]],
+    ) -> List[ExpectationDiagnostics]:
         diagnostics_list = []
         for expectation in expectations:
             instance = expectation()
