349d057f49df6cef40ef58a46c8579d42e6216a3
Shinnnyshinshin <will@superconductive.com>
2022-02-10 17:21:23 -0800

    Merge branch 'develop' into feature/GREAT-494/refactor-rbp-tests
    * develop:
      [FEATURE] SimpleDateFormatParameterBuilder (#4156)

--- tests/rule_based_profiler/test_user_workflow_fixtures.py
+++ tests/rule_based_profiler/test_user_workflow_fixtures.py
@@ -23,7 +23,7 @@ def test_alice_fixture_generation(
         len(
             alice_columnar_table_single_batch["expected_expectation_suite"].expectations
         )
-        == 18
+        == 21
     )
 
 def test_bobby_fixture_generation(
@@ -44,5 +44,5 @@ def test_bobby_fixture_generation(
                 "test_configuration_oneshot_sampling_method"
             ]["expected_expectation_suite"].expectations
         )
-        == 31
+        == 33
     )
