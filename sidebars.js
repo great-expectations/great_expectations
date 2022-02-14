058ffb78371bf892b12089fb44fcb996ba3582cc
Rachel-Reverie <rachel@superconductive.com>
2022-02-14 13:57:40 -0600

    - Added base content to technical term page for Data Docs. - Added Data Docs technical term page to ToC.

--- sidebars.js
+++ sidebars.js
@@ -374,6 +374,7 @@ module.exports = {
             'terms/datasource',
             'terms/data_context',
             'terms/data_asset',
+            'terms/data_docs',
             'terms/checkpoint',
             'terms/execution_engine',
             'terms/metric',
