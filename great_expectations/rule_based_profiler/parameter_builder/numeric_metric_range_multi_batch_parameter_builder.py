8c9383669ea9c9e104f3fdca3dfc6208d6769c87
Alex Sherstinsky <alex@superconductive.com>
2022-02-14 20:38:57 -0800

    quantile test

--- great_expectations/rule_based_profiler/parameter_builder/numeric_metric_range_multi_batch_parameter_builder.py
+++ great_expectations/rule_based_profiler/parameter_builder/numeric_metric_range_multi_batch_parameter_builder.py
@@ -349,12 +349,10 @@ detected.
             for metric_value_shape_idx in metric_value_shape
         ]
 
-        metric_value_indices: List[tuple]
-        metric_value_indices = list(itertools.product(*tuple(axes)))
+        metric_value_indices: List[tuple] = list(itertools.product(*tuple(axes)))
 
-        metric_value_vector_indices: List[tuple]
         metric_value_idx: tuple
-        metric_value_vector_indices = [
+        metric_value_vector_indices: List[tuple] = [
             (slice(None, None, None),) + metric_value_idx
             for metric_value_idx in metric_value_indices
         ]
@@ -369,7 +367,7 @@ detected.
             metric_value_vector = metric_values[metric_value_idx]
             if np.all(np.isclose(metric_value_vector, metric_value_vector[0])):
                 # Computation is unnecessary if distribution is degenerate.
-                lower_quantile = upper_quantile = metric_values[0]
+                lower_quantile = upper_quantile = metric_value_vector[0]
             else:
                 lower_quantile, upper_quantile = estimator(
                     metric_values=metric_value_vector,
