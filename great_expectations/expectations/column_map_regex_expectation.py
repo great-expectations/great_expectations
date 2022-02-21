adafb320d16905d6a75c5789bb1087b7068dc87f
“Abe <abe@superconductive.com>
2022-02-21 08:44:22 -0700

    Begin working on renderers

--- great_expectations/expectations/column_map_regex_expectation.py
+++ great_expectations/expectations/column_map_regex_expectation.py
@@ -15,6 +15,18 @@ from great_expectations.expectations.metrics.map_metric_provider import (
     column_condition_partial,
 )
 from great_expectations.expectations.metrics.util import get_dialect_regex_expression
+from great_expectations.render.renderer.renderer import (
+    renderer
+)
+from great_expectations.render.util import (
+    handle_strict_min_max,
+    parse_row_condition_string_pandas_engine,
+    substitute_none_for_missing,
+)
+from great_expectations.render.types import RenderedStringTemplateContent
+from great_expectations.expectations.util import (
+    render_evaluation_parameter_string,
+)
 
 class ColumnMapRegexMetricProvider(ColumnMapMetricProvider):
     condition_value_keys = ()
@@ -60,20 +72,196 @@ class ColumnMapRegexExpectation(ColumnMapExpectation, ABC):
 
         return map_metric
 
-    # def validate_configuration(self, configuration: ExpectationConfiguration):
+    # def validate_configuration(
+    #     self,
+    #     configuration: ExpectationConfiguration
+    # ) -> bool :
     #     pass
-        # if not super().validate_configuration(configuration):
-        #     return False
-        # try:
-        #     assert (
-        #         "column" in configuration.kwargs
-        #     ), "'column' parameter is required for column map expectations"
-        #     if "mostly" in configuration.kwargs:
-        #         mostly = configuration.kwargs["mostly"]
-        #         assert isinstance(
-        #             mostly, (int, float)
-        #         ), "'mostly' parameter must be an integer or float"
-        #         assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
-        # except AssertionError as e:
-        #     raise InvalidExpectationConfigurationError(str(e))
-        # return True
+    #     if not super().validate_configuration(configuration):
+    #         return False
+    #     try:
+    #         assert (
+    #             "column" in configuration.kwargs
+    #         ), "'column' parameter is required for column map expectations"
+    #         if "mostly" in configuration.kwargs:
+    #             mostly = configuration.kwargs["mostly"]
+    #             assert isinstance(
+    #                 mostly, (int, float)
+    #             ), "'mostly' parameter must be an integer or float"
+    #             assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
+    #     except AssertionError as e:
+    #         raise InvalidExpectationConfigurationError(str(e))
+    #     return True
+
+
+
+    # question, descriptive, prescriptive, diagnostic
+    @classmethod
+    @renderer(renderer_type="renderer.question")
+    def _question_renderer(
+        cls, configuration, result=None, language=None, runtime_configuration=None
+    ):
+        column = configuration.kwargs.get("column")
+        mostly = configuration.kwargs.get("mostly")
+        regex = configuration.kwargs.get("regex")
+
+        return f'Do at least {mostly * 100}% of values in column "{column}" match the regular expression {regex}?'
+
+#     @classmethod
+#     @renderer(renderer_type="renderer.answer")
+#     def _answer_renderer(
+#         cls, configuration=None, result=None, language=None, runtime_configuration=None
+#     ):
+#         column = result.expectation_config.kwargs.get("column")
+#         mostly = result.expectation_config.kwargs.get("mostly")
+#         regex = result.expectation_config.kwargs.get("regex")
+#         if result.success:
+#             return f'At least {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+#         else:
+#             return f'Less than {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+
+#     @classmethod
+#     def _atomic_prescriptive_template(
+#         cls,
+#         configuration=None,
+#         result=None,
+#         language=None,
+#         runtime_configuration=None,
+#         **kwargs,
+#     ):
+#         runtime_configuration = runtime_configuration or {}
+#         include_column_name = runtime_configuration.get("include_column_name", True)
+#         include_column_name = (
+#             include_column_name if include_column_name is not None else True
+#         )
+#         styling = runtime_configuration.get("styling")
+#         params = substitute_none_for_missing(
+#             configuration.kwargs,
+#             ["column", "regex", "mostly", "row_condition", "condition_parser"],
+#         )
+#         params_with_json_schema = {
+#             "column": {"schema": {"type": "string"}, "value": params.get("column")},
+#             "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
+#             "mostly_pct": {
+#                 "schema": {"type": "number"},
+#                 "value": params.get("mostly_pct"),
+#             },
+#             "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
+#             "row_condition": {
+#                 "schema": {"type": "string"},
+#                 "value": params.get("row_condition"),
+#             },
+#             "condition_parser": {
+#                 "schema": {"type": "string"},
+#                 "value": params.get("condition_parser"),
+#             },
+#         }
+
+#         if not params.get("regex"):
+#             template_str = (
+#                 "values must match a regular expression but none was specified."
+#             )
+#         else:
+#             template_str = "values must match this regular expression: $regex"
+#             if params["mostly"] is not None:
+#                 params_with_json_schema["mostly_pct"]["value"] = num_to_str(
+#                     params["mostly"] * 100, precision=15, no_scientific=True
+#                 )
+#                 # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+#                 template_str += ", at least $mostly_pct % of the time."
+#             else:
+#                 template_str += "."
+
+#         if include_column_name:
+#             template_str = "$column " + template_str
+
+#         if params["row_condition"] is not None:
+#             (
+#                 conditional_template_str,
+#                 conditional_params,
+#             ) = parse_row_condition_string_pandas_engine(
+#                 params["row_condition"], with_schema=True
+#             )
+#             template_str = conditional_template_str + ", then " + template_str
+#             params_with_json_schema.update(conditional_params)
+
+#         return (template_str, params_with_json_schema, styling)
+
+    @classmethod
+    @renderer(renderer_type="renderer.prescriptive")
+    @render_evaluation_parameter_string
+    def _prescriptive_renderer(
+        cls,
+        configuration=None,
+        result=None,
+        language=None,
+        runtime_configuration=None,
+        **kwargs,
+    ):
+        runtime_configuration = runtime_configuration or {}
+        include_column_name = runtime_configuration.get("include_column_name", True)
+        include_column_name = (
+            include_column_name if include_column_name is not None else True
+        )
+        styling = runtime_configuration.get("styling")
+        params = substitute_none_for_missing(
+            configuration.kwargs,
+            ["column", "regex", "mostly", "row_condition", "condition_parser"],
+        )
+
+        if not params.get("regex"):
+            template_str = (
+                "values must match a regular expression but none was specified."
+            )
+        else:
+            template_str = "values must match this regular expression: $regex"
+            if params["mostly"] is not None:
+                params["mostly_pct"] = num_to_str(
+                    params["mostly"] * 100, precision=15, no_scientific=True
+                )
+                # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+                template_str += ", at least $mostly_pct % of the time."
+            else:
+                template_str += "."
+
+        if include_column_name:
+            template_str = "$column " + template_str
+
+        if params["row_condition"] is not None:
+            (
+                conditional_template_str,
+                conditional_params,
+            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
+            template_str = conditional_template_str + ", then " + template_str
+            params.update(conditional_params)
+
+        params_with_json_schema = {
+            "column": {"schema": {"type": "string"}, "value": params.get("column")},
+            "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
+            "mostly_pct": {
+                "schema": {"type": "number"},
+                "value": params.get("mostly_pct"),
+            },
+            "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
+            "row_condition": {
+                "schema": {"type": "string"},
+                "value": params.get("row_condition"),
+            },
+            "condition_parser": {
+                "schema": {"type": "string"},
+                "value": params.get("condition_parser"),
+            },
+        }
+
+        return [
+            RenderedStringTemplateContent(
+                **{
+                    "content_block_type": "string_template",
+                    "string_template": {
+                        "template": template_str,
+                        "params": params,
+                        "styling": styling,
+                    },
+                }
+            )
+        ]
--- tests/expectations/test_column_map_regex_expectations.py
+++ tests/expectations/test_column_map_regex_expectations.py
@@ -8,7 +8,10 @@ def test_expectation_self_check():
 
     my_expectation = ExpectColumnValuesToOnlyContainVowels()
     expectation_diagnostic = my_expectation.run_diagnostics()
-    print(json.dumps(expectation_diagnostic.to_dict(), indent=2))
+    # print(json.dumps(expectation_diagnostic.to_dict(), indent=2))
+    print(json.dumps(expectation_diagnostic.to_dict()["renderers"], indent=2))
 
     for test in expectation_diagnostic.tests:
         assert test.test_passed
+
+    assert False
