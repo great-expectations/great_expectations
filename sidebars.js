93a5c06b5cdfd6f4debada555fb88ab9023b5009
Rachel-Reverie <rachel@superconductive.com>
2022-02-22 08:34:10 -0600

    - Added technical term page for Custom Expectations - Added Custom Expectations page to ToC - Added technical term tags to Custom Expectations page

--- sidebars.js
+++ sidebars.js
@@ -371,6 +371,7 @@ module.exports = {
           label: 'Technical Terms',
           items: [
             'terms/action',
+            'terms/custom_expectation',
             'terms/datasource',
             'terms/data_context',
             'terms/data_asset',
