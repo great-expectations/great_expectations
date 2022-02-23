af11da0da5d4f0cea5235415bcf19d527a9a7994
Chetan Kini <chetan@superconductive.com>
2022-02-23 11:24:35 -0500

    test: add additional test stubs for run anonymizer

--- tests/rule_based_profiler/test_profiler_run_anonymizer.py
+++ tests/rule_based_profiler/test_profiler_run_anonymizer.py
@@ -7,5 +7,17 @@ def test_anonymize_profiler_run():
     pass
 
 
+def test_anonymize_profiler_run_with_batch_requests_in_builder_attrs():
+    pass
+
+
 def test_resolve_config_using_acceptable_arguments():
+    pass
+
+
+def test_resolve_config_using_acceptable_arguments_with_batch_requests():
+    pass
+
+
+def test_resolve_config_using_acceptable_arguments_with_runtime_rule_overrides():
     pass
