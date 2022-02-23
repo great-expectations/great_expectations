8e45b195fabdef78d96ea08ad68eff06f7010554
Anthony Burdi <anthony@superconductive.com>
2022-02-23 17:31:09 -0500

    Update imports in __init__.py

--- great_expectations/rule_based_profiler/parameter_builder/__init__.py
+++ great_expectations/rule_based_profiler/parameter_builder/__init__.py
@@ -8,6 +8,10 @@ from great_expectations.rule_based_profiler.parameter_builder.simple_date_format
     SimpleDateFormatStringParameterBuilder,
 )
 
+from great_expectations.rule_based_profiler.parameter_builder.value_set_multi_batch_parameter_builder import (  # isort:skip
+    ValueSetMultiBatchParameterBuilder,
+)
+
 from great_expectations.rule_based_profiler.parameter_builder.metric_multi_batch_parameter_builder import (  # isort:skip
     MetricMultiBatchParameterBuilder,
 )
