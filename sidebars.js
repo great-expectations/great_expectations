d49fee89158dfc365c1b5e536e0b55363bc10b6b
Rachel-Reverie <rachel@superconductive.com>
2022-02-28 09:18:09 -0600

    Step 4 ToC content

--- sidebars.js
+++ sidebars.js
@@ -239,7 +239,46 @@ module.exports = {
         {
           type: 'category',
           label: 'How-to guides',
-          items: []
+          items: [
+            {
+              type: 'category',
+              label: 'Core skills',
+              items: [
+                'guides/validation/how_to_validate_data_by_running_a_checkpoint'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Checkpoints',
+              items: [
+                'guides/validation/checkpoints/how_to_add_validations_data_or_suites_to_a_checkpoint',
+                'guides/validation/checkpoints/how_to_create_a_new_checkpoint',
+                'guides/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config',
+                'guides/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint',
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Actions',
+              items: [
+                'guides/validation/validation_actions/how_to_trigger_email_as_a_validation_action',
+                'guides/validation/validation_actions/how_to_collect_openlineage_metadata_using_a_validation_action',
+                'guides/validation/validation_actions/how_to_trigger_opsgenie_notifications_as_a_validation_action',
+                'guides/validation/validation_actions/how_to_trigger_slack_notifications_as_a_validation_action',
+                'guides/validation/validation_actions/how_to_update_data_docs_as_a_validation_action'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Advanced',
+              items: [
+                'guides/validation/advanced/how_to_deploy_a_scheduled_checkpoint_with_cron',
+                'guides/validation/advanced/how_to_implement_custom_notifications',
+                'guides/validation/advanced/how_to_validate_data_without_a_checkpoint'
+              ]
+            },
+            { type: 'doc', id: 'guides/validation/index', label: 'Index' }
+          ]
         }
       ]
     },
