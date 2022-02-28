70728794522a7691a5dea7e540074a2605e9f697
Rachel-Reverie <rachel@superconductive.com>
2022-02-28 08:49:14 -0600

    Step 2 ToC content

--- sidebars.js
+++ sidebars.js
@@ -98,7 +98,73 @@ module.exports = {
         {
           type: 'category',
           label: 'How-to guides',
-          items: []
+          items: [
+            {
+              type: 'category',
+              label: 'Core skills',
+              items: [
+                'guides/connecting_to_your_data/how_to_choose_which_dataconnector_to_use',
+                'guides/connecting_to_your_data/how_to_configure_an_inferredassetdataconnector',
+                'guides/connecting_to_your_data/how_to_configure_a_configuredassetdataconnector',
+                'guides/connecting_to_your_data/how_to_configure_a_runtimedataconnector',
+                'guides/connecting_to_your_data/how_to_configure_a_dataconnector_to_introspect_and_partition_a_file_system_or_blob_store',
+                'guides/connecting_to_your_data/how_to_configure_a_dataconnector_to_introspect_and_partition_tables_in_sql',
+                'guides/connecting_to_your_data/how_to_create_a_batch_of_data_from_an_in_memory_spark_or_pandas_dataframe',
+                'guides/connecting_to_your_data/how_to_get_a_batch_of_data_from_a_configured_datasource'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'In memory',
+              items: [
+                'guides/connecting_to_your_data/in_memory/pandas',
+                'guides/connecting_to_your_data/in_memory/spark'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Database',
+              items: [
+                'guides/connecting_to_your_data/database/athena',
+                'guides/connecting_to_your_data/database/bigquery',
+                'guides/connecting_to_your_data/database/mssql',
+                'guides/connecting_to_your_data/database/mysql',
+                'guides/connecting_to_your_data/database/postgres',
+                'guides/connecting_to_your_data/database/redshift',
+                'guides/connecting_to_your_data/database/snowflake',
+                'guides/connecting_to_your_data/database/sqlite'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Filesystem',
+              items: [
+                'guides/connecting_to_your_data/filesystem/pandas',
+                'guides/connecting_to_your_data/filesystem/spark'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Cloud',
+              items: [
+                'guides/connecting_to_your_data/cloud/s3/pandas',
+                'guides/connecting_to_your_data/cloud/s3/spark',
+                'guides/connecting_to_your_data/cloud/gcs/pandas',
+                'guides/connecting_to_your_data/cloud/gcs/spark',
+                'guides/connecting_to_your_data/cloud/azure/pandas',
+                'guides/connecting_to_your_data/cloud/azure/spark'
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Advanced',
+              items: [
+                'guides/connecting_to_your_data/advanced/how_to_configure_a_dataconnector_for_splitting_and_sampling_a_file_system_or_blob_store',
+                'guides/connecting_to_your_data/advanced/how_to_configure_a_dataconnector_for_splitting_and_sampling_tables_in_sql'
+              ]
+            },
+            { type: 'doc', id: 'guides/connecting_to_your_data/index', label: 'Index' }
+          ]
         }
       ]
     },
@@ -106,12 +172,7 @@ module.exports = {
       type: 'category',
       label: 'Step 3: Create Expectations',
       items: [
-        { type: 'doc', id: 'guides/expectations/create_expectations_overview', label: 'Overview' },
-        {
-          type: 'category',
-          label: 'How-to guides',
-          items: []
-        }
+        { type: 'doc', id: 'guides/expectations/create_expectations_overview', label: 'Overview' }
       ]
     },
     {
