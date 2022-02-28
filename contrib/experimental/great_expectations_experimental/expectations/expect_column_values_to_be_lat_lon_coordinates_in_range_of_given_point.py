65b1d03b09617ac0c5476f3724fd98347c9090a4
Austin Robinson <austin@superconductive.com>
2022-02-28 14:09:49 -0700

    removing package from library metadata

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_lat_lon_coordinates_in_range_of_given_point.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_lat_lon_coordinates_in_range_of_given_point.py
@@ -438,7 +438,6 @@ class ExpectColumnValuesToBeLatLonCoordinatesInRangeOfGivenPoint(ColumnMapExpect
         "contributors": [  # Github handles for all contributors to this Expectation.
             "@austiezr",  # Don't forget to add your github handle here!
         ],
-        "package": "Geospatial Data",
     }
 
 
