981873bf7cb471b4d2c95589dd2bb667bde10b40
Austin Ziech Robinson <44794138+austiezr@users.noreply.github.com>
2022-03-02 08:05:43 -0700

    [BUGFIX] Fixes validate_configuration checking in diagnostics (#4307)
    * updates to validate_configuration checking | adding method to templates | fixing docs code refs
    
    * updating validate_config methods | fixing diagnostics flow
    
    * update test_run_diagnostics
    
    * Update column_aggregate_expectation_template.py
    
    * Update column_map_expectation_template.py

--- examples/expectations/column_map_expectation_template.py
+++ examples/expectations/column_map_expectation_template.py
@@ -6,6 +6,7 @@ For detailed instructions on how to use it, please see:
 
 import json
 
+from great_expectations.exceptions import InvalidExpectationConfigurationError
 from great_expectations.execution_engine import (
     PandasExecutionEngine,
     SparkDFExecutionEngine,
@@ -58,6 +59,35 @@ class ExpectColumnValuesToMatchSomeCriteria(ColumnMapExpectation):
 
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {}
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        necessary configuration arguments have been provided for the validation of the expectation.
+
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            True if the configuration has been validated successfully. Otherwise, raises an exception
+        """
+
+        super().validate_configuration(configuration)
+        if configuration is None:
+            configuration = self.configuration
+
+        # # Check other things in configuration.kwargs and raise Exceptions if needed
+        # try:
+        #     assert (
+        #         ...
+        #     ), "message"
+        #     assert (
+        #         ...
+        #     ), "message"
+        # except AssertionError as e:
+        #     raise InvalidExpectationConfigurationError(str(e))
+
+        return True
 
     # This object contains metadata for display in the public Gallery
     library_metadata = {
