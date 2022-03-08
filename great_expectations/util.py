370778fee339cf83a9cbd714319ab2115ecad94e
Austin Robinson <austin@superconductive.com>
2022-03-08 13:16:20 -0700

    helper function to handle type mapping by pyathena version

--- great_expectations/util.py
+++ great_expectations/util.py
@@ -1443,8 +1443,12 @@ def probabilistic_test(
 
 def get_pyathena_potential_type(type_module, type_):
     if version.parse(type_module.pyathena.__version__) >= version.parse("2.5.0"):
-        # Types are now nested in type_module.types
-        potential_type = getattr(type_module.types, type_)
+        # introduction of new column type mapping in 2.5
+        potential_type = type_module.AthenaDialect()._get_column_type(type_)
     else:
-        potential_type = getattr(type_module, type_)
+        if type_ == "string":
+            type_ = "varchar"
+        # < 2.5 column type mapping
+        potential_type = type_module._TYPE_MAPPINGS.get(type_)
+
     return potential_type
