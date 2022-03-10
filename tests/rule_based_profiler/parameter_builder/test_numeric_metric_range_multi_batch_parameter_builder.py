d0f2d29ccdcc9ae6042cc40b1af93d1946818e83
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2022-03-10 17:22:59 -0500

    [MAINTENANCE] Set random seed to make probabilistic tests deterministic (#4364)
    * Remove MAX_PROBABILISTIC_TEST_ASSERTION_RETRIES variable
    
    * Set random seed for probabilistic tests
    
    * Update variable_count in RBP test config
    
    * Add seed to tests, but not default self-initializing expectation configuration
    
    * Remove test for probabilistic_test decorator
    
    * Default profiler config does not set random seed and therefore results are probabilistic
    
    * Remove probabilistic portion of this test
    
    * Utilizing np.testing.assert_allclose instead of custom method

--- tests/rule_based_profiler/parameter_builder/test_numeric_metric_range_multi_batch_parameter_builder.py
+++ tests/rule_based_profiler/parameter_builder/test_numeric_metric_range_multi_batch_parameter_builder.py
@@ -13,10 +13,8 @@ from great_expectations.rule_based_profiler.types import (
     ParameterNode,
     get_parameter_value_by_fully_qualified_parameter_name,
 )
-from great_expectations.util import probabilistic_test
 
 
-@probabilistic_test
 def test_bootstrap_numeric_metric_range_multi_batch_parameter_builder_bobby(
     bobby_columnar_table_multi_batch_deterministic_data_context,
 ):
