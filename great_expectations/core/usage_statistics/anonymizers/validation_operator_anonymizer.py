cecd3f719b5f3f7d7d61489498c52e1267f17b53
Chetan Kini <chetan@superconductive.com>
2022-03-11 15:08:24 -0500

    refactor: continue removing ge_class attrs

--- great_expectations/core/usage_statistics/anonymizers/validation_operator_anonymizer.py
+++ great_expectations/core/usage_statistics/anonymizers/validation_operator_anonymizer.py
@@ -2,22 +2,12 @@ from great_expectations.core.usage_statistics.anonymizers.action_anonymizer impo
     ActionAnonymizer,
 )
 from great_expectations.core.usage_statistics.anonymizers.anonymizer import Anonymizer
-from great_expectations.validation_operators import (
-    ActionListValidationOperator,
-    ValidationOperator,
-    WarningAndFailureExpectationSuitesValidationOperator,
-)
 
 
 class ValidationOperatorAnonymizer(Anonymizer):
     def __init__(self, salt=None):
         super().__init__(salt=salt)
         # ordered bottom up in terms of inheritance order
-        self._ge_classes = [
-            ActionListValidationOperator,
-            WarningAndFailureExpectationSuitesValidationOperator,
-            ValidationOperator,
-        ]
         self._action_anonymizer = ActionAnonymizer(salt=salt)
 
     def anonymize_validation_operator_info(
