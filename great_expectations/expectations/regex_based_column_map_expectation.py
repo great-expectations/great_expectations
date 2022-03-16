f98cba6d5f0be07326739375f8b05dd2ca119c08
Austin Robinson <austin@superconductive.com>
2022-03-16 11:13:44 -0600

    initial cleanup

--- great_expectations/expectations/regex_based_column_map_expectation.py
+++ great_expectations/expectations/regex_based_column_map_expectation.py
@@ -1,32 +1,37 @@
-from abc import ABC
 import json
+import logging
+from abc import ABC
+from typing import Optional
 
-from great_expectations.expectations.expectation import (
-    ColumnMapExpectation,
-    ExpectationConfiguration,
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.exceptions.exceptions import (
+    InvalidExpectationConfigurationError,
 )
 from great_expectations.execution_engine import (
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
 )
+from great_expectations.expectations.expectation import (
+    ColumnMapExpectation,
+    ExpectationConfiguration,
+)
 from great_expectations.expectations.metrics.map_metric_provider import (
     ColumnMapMetricProvider,
     column_condition_partial,
 )
 from great_expectations.expectations.metrics.util import get_dialect_regex_expression
-from great_expectations.render.renderer.renderer import (
-    renderer
-)
+from great_expectations.expectations.util import render_evaluation_parameter_string
+from great_expectations.render.renderer.renderer import renderer
+from great_expectations.render.types import RenderedStringTemplateContent
 from great_expectations.render.util import (
     handle_strict_min_max,
     parse_row_condition_string_pandas_engine,
     substitute_none_for_missing,
 )
-from great_expectations.render.types import RenderedStringTemplateContent
-from great_expectations.expectations.util import (
-    render_evaluation_parameter_string,
-)
+
+logger = logging.getLogger(__name__)
+
 
 class ColumnMapRegexMetricProvider(ColumnMapMetricProvider):
     condition_value_keys = ()
@@ -35,30 +40,31 @@ class ColumnMapRegexMetricProvider(ColumnMapMetricProvider):
     def _pandas(cls, column, **kwargs):
         return column.astype(str).str.contains(cls.regex)
 
-    # @column_condition_partial(engine=SqlAlchemyExecutionEngine)
-    # def _sqlalchemy(cls, column, _dialect, **kwargs):
-    #     regex_expression = get_dialect_regex_expression(column, cls.regex, _dialect)
-    #     if regex_expression is None:
-    #         logger.warning(
-    #             "Regex is not supported for dialect %s" % str(_dialect.dialect.name)
-    #         )
-    #         raise NotImplementedError
+    @column_condition_partial(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(cls, column, _dialect, **kwargs):
+        regex_expression = get_dialect_regex_expression(column, cls.regex, _dialect)
+        breakpoint()
+        if regex_expression is None:
+            logger.warning(
+                "Regex is not supported for dialect %s" % str(_dialect.dialect.name)
+            )
+            raise NotImplementedError
 
-    #     return regex_expression
+        return regex_expression
 
     # @column_condition_partial(engine=SparkDFExecutionEngine)
     # def _spark(cls, column, **kwargs):
     #     return column.rlike(cls.regex)
 
-class ColumnMapRegexExpectation(ColumnMapExpectation, ABC):
 
+class ColumnMapRegexExpectation(ColumnMapExpectation, ABC):
     @staticmethod
     def _register_metric(
-        regex_snake_name : str,
-        regex_camel_name : str,
-        regex_ : str,
+        regex_snake_name: str,
+        regex_camel_name: str,
+        regex_: str,
     ):
-        map_metric = "column_values.match_"+regex_snake_name+"_regex"
+        map_metric = "column_values.match_" + regex_snake_name + "_regex"
 
         # Define the class using `type`. This allows us to name it dynamically.
         NewColumnRegexMetricProvider = type(
@@ -67,33 +73,26 @@ class ColumnMapRegexExpectation(ColumnMapExpectation, ABC):
             {
                 "condition_metric_name": map_metric,
                 "regex": regex_,
-            }
+            },
         )
 
         return map_metric
 
-    # def validate_configuration(
-    #     self,
-    #     configuration: ExpectationConfiguration
-    # ) -> bool :
-    #     pass
-    #     if not super().validate_configuration(configuration):
-    #         return False
-    #     try:
-    #         assert (
-    #             "column" in configuration.kwargs
-    #         ), "'column' parameter is required for column map expectations"
-    #         if "mostly" in configuration.kwargs:
-    #             mostly = configuration.kwargs["mostly"]
-    #             assert isinstance(
-    #                 mostly, (int, float)
-    #             ), "'mostly' parameter must be an integer or float"
-    #             assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
-    #     except AssertionError as e:
-    #         raise InvalidExpectationConfigurationError(str(e))
-    #     return True
-
-
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        super().validate_configuration(configuration)
+        try:
+            assert (
+                "column" in configuration.kwargs
+            ), "'column' parameter is required for column map expectations"
+            if "mostly" in configuration.kwargs:
+                mostly = configuration.kwargs["mostly"]
+                assert isinstance(
+                    mostly, (int, float)
+                ), "'mostly' parameter must be an integer or float"
+                assert 0 <= mostly <= 1, "'mostly' parameter must be between 0 and 1"
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+        return True
 
     # question, descriptive, prescriptive, diagnostic
     @classmethod
@@ -107,85 +106,85 @@ class ColumnMapRegexExpectation(ColumnMapExpectation, ABC):
 
         return f'Do at least {mostly * 100}% of values in column "{column}" match the regular expression {regex}?'
 
-#     @classmethod
-#     @renderer(renderer_type="renderer.answer")
-#     def _answer_renderer(
-#         cls, configuration=None, result=None, language=None, runtime_configuration=None
-#     ):
-#         column = result.expectation_config.kwargs.get("column")
-#         mostly = result.expectation_config.kwargs.get("mostly")
-#         regex = result.expectation_config.kwargs.get("regex")
-#         if result.success:
-#             return f'At least {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
-#         else:
-#             return f'Less than {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+    #     @classmethod
+    #     @renderer(renderer_type="renderer.answer")
+    #     def _answer_renderer(
+    #         cls, configuration=None, result=None, language=None, runtime_configuration=None
+    #     ):
+    #         column = result.expectation_config.kwargs.get("column")
+    #         mostly = result.expectation_config.kwargs.get("mostly")
+    #         regex = result.expectation_config.kwargs.get("regex")
+    #         if result.success:
+    #             return f'At least {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+    #         else:
+    #             return f'Less than {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
 
-#     @classmethod
-#     def _atomic_prescriptive_template(
-#         cls,
-#         configuration=None,
-#         result=None,
-#         language=None,
-#         runtime_configuration=None,
-#         **kwargs,
-#     ):
-#         runtime_configuration = runtime_configuration or {}
-#         include_column_name = runtime_configuration.get("include_column_name", True)
-#         include_column_name = (
-#             include_column_name if include_column_name is not None else True
-#         )
-#         styling = runtime_configuration.get("styling")
-#         params = substitute_none_for_missing(
-#             configuration.kwargs,
-#             ["column", "regex", "mostly", "row_condition", "condition_parser"],
-#         )
-#         params_with_json_schema = {
-#             "column": {"schema": {"type": "string"}, "value": params.get("column")},
-#             "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
-#             "mostly_pct": {
-#                 "schema": {"type": "number"},
-#                 "value": params.get("mostly_pct"),
-#             },
-#             "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
-#             "row_condition": {
-#                 "schema": {"type": "string"},
-#                 "value": params.get("row_condition"),
-#             },
-#             "condition_parser": {
-#                 "schema": {"type": "string"},
-#                 "value": params.get("condition_parser"),
-#             },
-#         }
+    #     @classmethod
+    #     def _atomic_prescriptive_template(
+    #         cls,
+    #         configuration=None,
+    #         result=None,
+    #         language=None,
+    #         runtime_configuration=None,
+    #         **kwargs,
+    #     ):
+    #         runtime_configuration = runtime_configuration or {}
+    #         include_column_name = runtime_configuration.get("include_column_name", True)
+    #         include_column_name = (
+    #             include_column_name if include_column_name is not None else True
+    #         )
+    #         styling = runtime_configuration.get("styling")
+    #         params = substitute_none_for_missing(
+    #             configuration.kwargs,
+    #             ["column", "regex", "mostly", "row_condition", "condition_parser"],
+    #         )
+    #         params_with_json_schema = {
+    #             "column": {"schema": {"type": "string"}, "value": params.get("column")},
+    #             "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
+    #             "mostly_pct": {
+    #                 "schema": {"type": "number"},
+    #                 "value": params.get("mostly_pct"),
+    #             },
+    #             "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
+    #             "row_condition": {
+    #                 "schema": {"type": "string"},
+    #                 "value": params.get("row_condition"),
+    #             },
+    #             "condition_parser": {
+    #                 "schema": {"type": "string"},
+    #                 "value": params.get("condition_parser"),
+    #             },
+    #         }
 
-#         if not params.get("regex"):
-#             template_str = (
-#                 "values must match a regular expression but none was specified."
-#             )
-#         else:
-#             template_str = "values must match this regular expression: $regex"
-#             if params["mostly"] is not None:
-#                 params_with_json_schema["mostly_pct"]["value"] = num_to_str(
-#                     params["mostly"] * 100, precision=15, no_scientific=True
-#                 )
-#                 # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
-#                 template_str += ", at least $mostly_pct % of the time."
-#             else:
-#                 template_str += "."
+    #         if not params.get("regex"):
+    #             template_str = (
+    #                 "values must match a regular expression but none was specified."
+    #             )
+    #         else:
+    #             template_str = "values must match this regular expression: $regex"
+    #             if params["mostly"] is not None:
+    #                 params_with_json_schema["mostly_pct"]["value"] = num_to_str(
+    #                     params["mostly"] * 100, precision=15, no_scientific=True
+    #                 )
+    #                 # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+    #                 template_str += ", at least $mostly_pct % of the time."
+    #             else:
+    #                 template_str += "."
 
-#         if include_column_name:
-#             template_str = "$column " + template_str
+    #         if include_column_name:
+    #             template_str = "$column " + template_str
 
-#         if params["row_condition"] is not None:
-#             (
-#                 conditional_template_str,
-#                 conditional_params,
-#             ) = parse_row_condition_string_pandas_engine(
-#                 params["row_condition"], with_schema=True
-#             )
-#             template_str = conditional_template_str + ", then " + template_str
-#             params_with_json_schema.update(conditional_params)
+    #         if params["row_condition"] is not None:
+    #             (
+    #                 conditional_template_str,
+    #                 conditional_params,
+    #             ) = parse_row_condition_string_pandas_engine(
+    #                 params["row_condition"], with_schema=True
+    #             )
+    #             template_str = conditional_template_str + ", then " + template_str
+    #             params_with_json_schema.update(conditional_params)
 
-#         return (template_str, params_with_json_schema, styling)
+    #         return (template_str, params_with_json_schema, styling)
 
     @classmethod
     @renderer(renderer_type="renderer.prescriptive")
---
+++ tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_only_contain_vowels.py
@@ -0,0 +1,156 @@
+from typing import Dict, Optional
+
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.exceptions.exceptions import (
+    InvalidExpectationConfigurationError,
+)
+from great_expectations.expectations.regex_based_column_map_expectation import (
+    ColumnMapRegexExpectation,
+    ColumnMapRegexMetricProvider,
+)
+
+
+class ExpectColumnValuesToOnlyContainVowels(ColumnMapRegexExpectation):
+    """Values in this column should only contain vowels"""
+
+    regex_snake_name = "vowel"
+    regex_camel_name = "Vowel"
+    regex = "^[aeiouyAEIOUY]*$"
+
+    examples = [
+        {
+            "data": {
+                "only_vowels": ["a", "e", "I", "O", "U", "y", ""],
+                "mixed": ["A", "b", "c", "D", "E", "F", "g"],
+                "longer_vowels": ["aei", "YAY", "oYu", "eee", "", "aeIOUY", None],
+                "contains_vowels_but_also_other_stuff": [
+                    "baa",
+                    "aba",
+                    "aab",
+                    "1a1",
+                    "a a",
+                    " ",
+                    "*",
+                ],
+            },
+            "tests": [
+                {
+                    "title": "positive_test",
+                    "exact_match_out": False,
+                    "in": {"column": "only_vowels"},
+                    "out": {
+                        "success": True,
+                    },
+                    "include_in_gallery": True,
+                },
+                {
+                    "title": "negative_test",
+                    "exact_match_out": False,
+                    "in": {"column": "mixed"},
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [1, 2, 3, 5, 6],
+                    },
+                    "include_in_gallery": True,
+                },
+                {
+                    "title": "another_postive_test",
+                    "exact_match_out": False,
+                    "in": {"column": "longer_vowels"},
+                    "out": {
+                        "success": True,
+                    },
+                    "include_in_gallery": True,
+                },
+                {
+                    "title": "another_negative_test",
+                    "exact_match_out": False,
+                    "in": {"column": "contains_vowels_but_also_other_stuff"},
+                    "out": {
+                        "success": False,
+                        "unexpected_index_list": [0, 1, 2, 3, 4, 5, 6],
+                    },
+                    "include_in_gallery": True,
+                },
+                {
+                    "title": "mostly_positive_test",
+                    "exact_match_out": False,
+                    "in": {"column": "mixed", "mostly": 0.1},
+                    "out": {
+                        "success": True,
+                    },
+                    "include_in_gallery": True,
+                },
+                {
+                    "title": "mostly_negative_test",
+                    "exact_match_out": False,
+                    "in": {"column": "mixed", "mostly": 0.3},
+                    "out": {
+                        "success": False,
+                    },
+                    "include_in_gallery": True,
+                },
+            ],
+            "test_backends": [
+                {
+                    "backend": "pandas",
+                    "dialects": None,
+                },
+                {
+                    "backend": "sqlalchemy",
+                    "dialects": ["sqlite", "postgresql"],
+                },
+                # {
+                #     "backend": "spark",
+                #     "dialects": None,
+                # },
+            ],
+        }
+    ]
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        necessary configuration arguments have been provided for the validation of the expectation.
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            True if the configuration has been validated successfully. Otherwise, raises an exception
+        """
+
+        # Setting up a configuration
+        super().validate_configuration(configuration)
+        if configuration is None:
+            configuration = self.configuration
+
+    library_metadata = {
+        "tags": ["regex"],
+        "contributors": ["@abegong", "@austiezr"],
+    }
+
+    map_metric = ColumnMapRegexExpectation._register_metric(
+        regex_snake_name=regex_snake_name,
+        regex_camel_name=regex_camel_name,
+        regex_=regex,
+    )
+
+
+if __name__ == "__main__":
+    ExpectColumnValuesToOnlyContainVowels().print_diagnostic_checklist()
+
+# Note to users: code below this line is only for integration testing -- ignore!
+
+diagnostics = ExpectColumnValuesToOnlyContainVowels().run_diagnostics()
+
+breakpoint()
+for check in diagnostics["tests"]:
+    assert check["test_passed"] is True
+    assert check["error_message"] is None
+    assert check["stack_trace"] is None
+
+for check in diagnostics["errors"]:
+    assert check is None
+
+for check in diagnostics["maturity_checklist"]["experimental"]:
+    assert check["passed"] is True
