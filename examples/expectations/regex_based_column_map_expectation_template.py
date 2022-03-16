d5b643feb7d070610570ea1204e90b964fed93e3
Austin Robinson <austin@superconductive.com>
2022-03-16 12:56:57 -0600

    created template

---
+++ examples/expectations/regex_based_column_map_expectation_template.py
@@ -0,0 +1,78 @@
+"""
+This is a template for creating custom ColumnMapExpectations.
+For detailed instructions on how to use it, please see:
+    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_column_map_expectations
+"""
+
+from typing import Dict, Optional
+
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.exceptions.exceptions import (
+    InvalidExpectationConfigurationError,
+)
+from great_expectations.expectations.regex_based_column_map_expectation import (
+    ColumnMapRegexExpectation,
+    ColumnMapRegexMetricProvider,
+)
+
+
+# This class defines the Expectation itself
+class ExpectColumnValuesToMatchSomeRegex(ColumnMapRegexExpectation):
+    """TODO: Add a docstring here"""
+
+    # These values will be used to configure the metric created by your expectation
+    regex_snake_name = "vowel_name"
+    regex_camel_name = "VowelName"
+    regex = "regex pattern"
+
+    # These examples will be shown in the public gallery.
+    # They will also be executed as unit tests for your Expectation.
+    examples = []
+
+    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        necessary configuration arguments have been provided for the validation of the expectation.
+
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            True if the configuration has been validated successfully. Otherwise, raises an exception
+        """
+
+        super().validate_configuration(configuration)
+        if configuration is None:
+            configuration = self.configuration
+
+        # # Check other things in configuration.kwargs and raise Exceptions if needed
+        # try:
+        #     assert (
+        #         ...
+        #     ), "message"
+        #     assert (
+        #         ...
+        #     ), "message"
+        # except AssertionError as e:
+        #     raise InvalidExpectationConfigurationError(str(e))
+
+        return True
+
+    # This object contains metadata for display in the public Gallery
+    library_metadata = {
+        "tags": [],  # Tags for this Expectation in the Gallery
+        "contributors": [  # Github handles for all contributors to this Expectation.
+            "@your_name_here",  # Don't forget to add your github handle here!
+        ],
+    }
+
+    # Here your regex is used to create a custom metric for this expectation
+    map_metric = ColumnMapRegexExpectation._register_metric(
+        regex_snake_name=regex_snake_name,
+        regex_camel_name=regex_camel_name,
+        regex_=regex,
+    )
+
+
+if __name__ == "__main__":
+    ExpectColumnValuesToMatchSomeRegex().print_diagnostic_checklist()
