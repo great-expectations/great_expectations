4fc18d8fb12da4ebf95587adc4808643ee6ff81a
Austin Robinson <austin@superconductive.com>
2022-03-16 14:34:40 -0600

    doc creation

--- sidebars.js
+++ sidebars.js
@@ -219,6 +219,7 @@ module.exports = {
                 'guides/expectations/creating_custom_expectations/overview',
                 'guides/expectations/creating_custom_expectations/how_to_create_custom_column_aggregate_expectations',
                 'guides/expectations/creating_custom_expectations/how_to_create_custom_column_map_expectations',
+                'guides/expectations/creating_custom_expectations/how_to_create_custom_regex_based_column_map_expectations',
                 'guides/expectations/creating_custom_expectations/how_to_create_custom_parameterized_expectations',
                 'guides/expectations/creating_custom_expectations/how_to_create_custom_metrics',
                 {
