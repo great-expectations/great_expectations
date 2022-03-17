c5307461d68b52bda43e6c18f4b2f8b7b33d2743
Chetan Kini <chetan@superconductive.com>
2022-03-17 13:41:00 -0400

    chore: defer basic anonymization to parent class

--- great_expectations/execution_engine/sqlalchemy_execution_engine.py
+++ great_expectations/execution_engine/sqlalchemy_execution_engine.py
@@ -336,7 +336,7 @@ class SqlAlchemyExecutionEngine(ExecutionEngine):
             handler.send_usage_message(
                 event="execution_engine.sqlalchemy.connect",
                 event_payload={
-                    "anonymized_name": handler.anonymizer.anonymize(self.name),
+                    "anonymized_name": handler.anonymizer.anonymize_string(self.name),
                     "sqlalchemy_dialect": self.engine.name,
                 },
                 success=True,
