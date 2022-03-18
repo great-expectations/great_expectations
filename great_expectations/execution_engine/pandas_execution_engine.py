d6eafdea6263c9814390742b003177dc77007a4f
Chetan Kini <chetan@superconductive.com>
2022-03-18 08:30:37 -0400

    chore: use 'instead'

--- great_expectations/execution_engine/pandas_execution_engine.py
+++ great_expectations/execution_engine/pandas_execution_engine.py
@@ -510,7 +510,7 @@ Please check your config."""
                     # deprecated-v0.13.29
                     warnings.warn(
                         f"""The correct "no-action" value of the "ignore_row_if" directive for the column pair case is \
-"neither" (the use of "{ignore_row_if}" is deprecated as of v0.13.29 and will be removed in v0.16).  Please use "neither" moving forward.
+"neither" (the use of "{ignore_row_if}" is deprecated as of v0.13.29 and will be removed in v0.16).  Please use "neither" instead.
 """,
                         DeprecationWarning,
                     )
