1188319ca01cb44100a4ac65d971e5e07be98223
Chetan Kini <chetan@superconductive.com>
2022-03-21 10:59:48 -0400

    feat: start impl of anonymize on Anonymizer

--- great_expectations/core/usage_statistics/anonymizers/anonymizer.py
+++ great_expectations/core/usage_statistics/anonymizers/anonymizer.py
@@ -1,20 +1,13 @@
 import logging
-from typing import Optional
+from typing import List, Optional, Type
 
 from great_expectations.core.usage_statistics.anonymizers.base import BaseAnonymizer
-from great_expectations.core.usage_statistics.util import (
-    aggregate_all_core_expectation_types,
-)
 
 logger = logging.getLogger(__name__)
 
 
 class Anonymizer(BaseAnonymizer):
     """Anonymize string names in an optionally-consistent way."""
-
-    # Any class that starts with this __module__ is considered a "core" object
-    CORE_GE_OBJECT_MODULE_PREFIX = "great_expectations"
-    CORE_GE_EXPECTATION_TYPES = aggregate_all_core_expectation_types()
 
     def __init__(self, salt: Optional[str] = None) -> None:
         super().__init__(salt=salt)
@@ -29,15 +22,24 @@ class Anonymizer(BaseAnonymizer):
             ProfilerRunAnonymizer,
         )
 
-        self.strategies = [
+        self.strategies: List[Type[BaseAnonymizer]] = [
             CheckpointRunAnonymizer,
             ProfilerRunAnonymizer,
             DatasourceAnonymizer,
         ]
 
     def anonymize(self, obj: object) -> dict:
-        raise NotImplementedError
+        anonymizer: Optional[BaseAnonymizer] = None
+        for anonymizer_cls in self.strategies:
+            if anonymizer_cls.can_handle(obj):
+                anonymizer = anonymizer_cls(salt=self._salt)
+                return anonymizer.anonymize(obj)
+
+        return self._anonymize(obj)
+
+    def _anonymize(self, obj: object) -> dict:
+        pass
 
     @staticmethod
     def can_handle(obj: object) -> bool:
-        raise NotImplementedError
+        return isinstance(obj, object)
