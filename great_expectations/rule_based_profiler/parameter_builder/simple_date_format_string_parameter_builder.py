3b96a5edc7055f1bb64676df1afdc85f4fc7e085
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2022-03-22 07:13:30 -0700

    [MAINTENANCE] Insure consistent ordering of arguments to ParameterBuilder instantiations (#4496)

--- great_expectations/rule_based_profiler/parameter_builder/simple_date_format_string_parameter_builder.py
+++ great_expectations/rule_based_profiler/parameter_builder/simple_date_format_string_parameter_builder.py
@@ -97,11 +97,11 @@ class SimpleDateFormatStringParameterBuilder(ParameterBuilder):
         metric_value_kwargs: Optional[Union[str, dict]] = None,
         threshold: Union[str, float] = 1.0,
         candidate_strings: Optional[Union[Iterable[str], str]] = None,
+        json_serialize: bool = True,
         batch_list: Optional[List[Batch]] = None,
         batch_request: Optional[
             Union[str, BatchRequest, RuntimeBatchRequest, dict]
         ] = None,
-        json_serialize: bool = True,
         data_context: Optional["DataContext"] = None,  # noqa: F821
     ):
         """
@@ -114,16 +114,16 @@ class SimpleDateFormatStringParameterBuilder(ParameterBuilder):
             metric_value_kwargs: used in MetricConfiguration
             threshold: the ratio of values that must match a format string for it to be accepted
             candidate_strings: a list of candidate date format strings that will replace the default
+            json_serialize: If True (default), convert computed value to JSON prior to saving results.
             batch_list: explicitly passed Batch objects for parameter computation (take precedence over batch_request).
             batch_request: specified in ParameterBuilder configuration to get Batch objects for parameter computation.
-            json_serialize: If True (default), convert computed value to JSON prior to saving results.
             data_context: DataContext
         """
         super().__init__(
             name=name,
+            json_serialize=json_serialize,
             batch_list=batch_list,
             batch_request=batch_request,
-            json_serialize=json_serialize,
             data_context=data_context,
         )
 
