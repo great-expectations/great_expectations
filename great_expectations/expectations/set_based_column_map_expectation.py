c56aaf23e2ec2888fef3df51ab357d99222870cf
Abe Gong <abegong@gmail.com>
2022-03-21 21:10:14 -0600

    Comment unimplemented renderers

--- great_expectations/expectations/set_based_column_map_expectation.py
+++ great_expectations/expectations/set_based_column_map_expectation.py
@@ -98,7 +98,7 @@ class SetBasedColumnMapExpectation(ColumnMapExpectation, ABC):
 
         except AssertionError as e:
             raise InvalidExpectationConfigurationError(str(e))
-            
+
         return True
 
     # question, descriptive, prescriptive, diagnostic
@@ -123,176 +123,176 @@ class SetBasedColumnMapExpectation(ColumnMapExpectation, ABC):
             else:
                 return f'Are at least {mostly * 100}% of values in column "{column}" in the set {str(set_)}?'
 
-    @classmethod
-    @renderer(renderer_type="renderer.answer")
-    def _answer_renderer(
-        cls, configuration=None, result=None, language=None, runtime_configuration=None
-    ):
-        column = result.expectation_config.kwargs.get("column")
-        mostly = result.expectation_config.kwargs.get("mostly")
-        regex = result.expectation_config.kwargs.get("regex")
-        semantic_type_name_plural = configuration.kwargs.get(
-            "semantic_type_name_plural"
-        )
+    # @classmethod
+    # @renderer(renderer_type="renderer.answer")
+    # def _answer_renderer(
+    #     cls, configuration=None, result=None, language=None, runtime_configuration=None
+    # ):
+    #     column = result.expectation_config.kwargs.get("column")
+    #     mostly = result.expectation_config.kwargs.get("mostly")
+    #     regex = result.expectation_config.kwargs.get("regex")
+    #     semantic_type_name_plural = configuration.kwargs.get(
+    #         "semantic_type_name_plural"
+    #     )
 
-        if result.success:
-            if mostly == 1 or mostly is None:
-                if semantic_type_name_plural is not None:
-                    return f'All values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
-                else:
-                    return f'All values in column "{column}" match the regular expression {regex}.'
-            else:
-                if semantic_type_name_plural is not None:
-                    return f'At least {mostly * 100}% of values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
-                else:
-                    return f'At least {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
-        else:
-            if semantic_type_name_plural is not None:
-                return f' Less than {mostly * 100}% of values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
-            else:
-                return f'Less than {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+    #     if result.success:
+    #         if mostly == 1 or mostly is None:
+    #             if semantic_type_name_plural is not None:
+    #                 return f'All values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
+    #             else:
+    #                 return f'All values in column "{column}" match the regular expression {regex}.'
+    #         else:
+    #             if semantic_type_name_plural is not None:
+    #                 return f'At least {mostly * 100}% of values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
+    #             else:
+    #                 return f'At least {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
+    #     else:
+    #         if semantic_type_name_plural is not None:
+    #             return f' Less than {mostly * 100}% of values in column "{column}" are valid {semantic_type_name_plural}, as judged by matching the regular expression {regex}.'
+    #         else:
+    #             return f'Less than {mostly * 100}% of values in column "{column}" match the regular expression {regex}.'
 
-    @classmethod
-    def _atomic_prescriptive_template(
-        cls,
-        configuration=None,
-        result=None,
-        language=None,
-        runtime_configuration=None,
-        **kwargs,
-    ):
-        runtime_configuration = runtime_configuration or {}
-        include_column_name = runtime_configuration.get("include_column_name", True)
-        include_column_name = (
-            include_column_name if include_column_name is not None else True
-        )
-        styling = runtime_configuration.get("styling")
-        params = substitute_none_for_missing(
-            configuration.kwargs,
-            ["column", "regex", "mostly", "row_condition", "condition_parser"],
-        )
-        params_with_json_schema = {
-            "column": {"schema": {"type": "string"}, "value": params.get("column")},
-            "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
-            "mostly_pct": {
-                "schema": {"type": "number"},
-                "value": params.get("mostly_pct"),
-            },
-            "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
-            "row_condition": {
-                "schema": {"type": "string"},
-                "value": params.get("row_condition"),
-            },
-            "condition_parser": {
-                "schema": {"type": "string"},
-                "value": params.get("condition_parser"),
-            },
-        }
+    # @classmethod
+    # def _atomic_prescriptive_template(
+    #     cls,
+    #     configuration=None,
+    #     result=None,
+    #     language=None,
+    #     runtime_configuration=None,
+    #     **kwargs,
+    # ):
+    #     runtime_configuration = runtime_configuration or {}
+    #     include_column_name = runtime_configuration.get("include_column_name", True)
+    #     include_column_name = (
+    #         include_column_name if include_column_name is not None else True
+    #     )
+    #     styling = runtime_configuration.get("styling")
+    #     params = substitute_none_for_missing(
+    #         configuration.kwargs,
+    #         ["column", "regex", "mostly", "row_condition", "condition_parser"],
+    #     )
+    #     params_with_json_schema = {
+    #         "column": {"schema": {"type": "string"}, "value": params.get("column")},
+    #         "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
+    #         "mostly_pct": {
+    #             "schema": {"type": "number"},
+    #             "value": params.get("mostly_pct"),
+    #         },
+    #         "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
+    #         "row_condition": {
+    #             "schema": {"type": "string"},
+    #             "value": params.get("row_condition"),
+    #         },
+    #         "condition_parser": {
+    #             "schema": {"type": "string"},
+    #             "value": params.get("condition_parser"),
+    #         },
+    #     }
 
-        if not params.get("regex"):
-            template_str = (
-                "values must match a regular expression but none was specified."
-            )
-        else:
-            template_str = "values must match this regular expression: $regex"
-            if params["mostly"] is not None:
-                params_with_json_schema["mostly_pct"]["value"] = num_to_str(
-                    params["mostly"] * 100, precision=15, no_scientific=True
-                )
-                template_str += ", at least $mostly_pct % of the time."
-            else:
-                template_str += "."
+    #     if not params.get("regex"):
+    #         template_str = (
+    #             "values must match a regular expression but none was specified."
+    #         )
+    #     else:
+    #         template_str = "values must match this regular expression: $regex"
+    #         if params["mostly"] is not None:
+    #             params_with_json_schema["mostly_pct"]["value"] = num_to_str(
+    #                 params["mostly"] * 100, precision=15, no_scientific=True
+    #             )
+    #             template_str += ", at least $mostly_pct % of the time."
+    #         else:
+    #             template_str += "."
 
-        if include_column_name:
-            template_str = "$column " + template_str
+    #     if include_column_name:
+    #         template_str = "$column " + template_str
 
-        if params["row_condition"] is not None:
-            (
-                conditional_template_str,
-                conditional_params,
-            ) = parse_row_condition_string_pandas_engine(
-                params["row_condition"], with_schema=True
-            )
-            template_str = conditional_template_str + ", then " + template_str
-            params_with_json_schema.update(conditional_params)
+    #     if params["row_condition"] is not None:
+    #         (
+    #             conditional_template_str,
+    #             conditional_params,
+    #         ) = parse_row_condition_string_pandas_engine(
+    #             params["row_condition"], with_schema=True
+    #         )
+    #         template_str = conditional_template_str + ", then " + template_str
+    #         params_with_json_schema.update(conditional_params)
 
-        return (template_str, params_with_json_schema, styling)
+    #     return (template_str, params_with_json_schema, styling)
 
-    @classmethod
-    @renderer(renderer_type="renderer.prescriptive")
-    @render_evaluation_parameter_string
-    def _prescriptive_renderer(
-        cls,
-        configuration=None,
-        result=None,
-        language=None,
-        runtime_configuration=None,
-        **kwargs,
-    ):
-        runtime_configuration = runtime_configuration or {}
-        include_column_name = runtime_configuration.get("include_column_name", True)
-        include_column_name = (
-            include_column_name if include_column_name is not None else True
-        )
-        styling = runtime_configuration.get("styling")
-        params = substitute_none_for_missing(
-            configuration.kwargs,
-            ["column", "regex", "mostly", "row_condition", "condition_parser"],
-        )
+    # @classmethod
+    # @renderer(renderer_type="renderer.prescriptive")
+    # @render_evaluation_parameter_string
+    # def _prescriptive_renderer(
+    #     cls,
+    #     configuration=None,
+    #     result=None,
+    #     language=None,
+    #     runtime_configuration=None,
+    #     **kwargs,
+    # ):
+    #     runtime_configuration = runtime_configuration or {}
+    #     include_column_name = runtime_configuration.get("include_column_name", True)
+    #     include_column_name = (
+    #         include_column_name if include_column_name is not None else True
+    #     )
+    #     styling = runtime_configuration.get("styling")
+    #     params = substitute_none_for_missing(
+    #         configuration.kwargs,
+    #         ["column", "regex", "mostly", "row_condition", "condition_parser"],
+    #     )
 
-        if not params.get("regex"):
-            template_str = (
-                "values must match a regular expression but none was specified."
-            )
-        else:
-            template_str = "values must match this regular expression: $regex"
-            if params["mostly"] is not None:
-                params["mostly_pct"] = num_to_str(
-                    params["mostly"] * 100, precision=15, no_scientific=True
-                )
-                # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
-                template_str += ", at least $mostly_pct % of the time."
-            else:
-                template_str += "."
+    #     if not params.get("regex"):
+    #         template_str = (
+    #             "values must match a regular expression but none was specified."
+    #         )
+    #     else:
+    #         template_str = "values must match this regular expression: $regex"
+    #         if params["mostly"] is not None:
+    #             params["mostly_pct"] = num_to_str(
+    #                 params["mostly"] * 100, precision=15, no_scientific=True
+    #             )
+    #             # params["mostly_pct"] = "{:.14f}".format(params["mostly"]*100).rstrip("0").rstrip(".")
+    #             template_str += ", at least $mostly_pct % of the time."
+    #         else:
+    #             template_str += "."
 
-        if include_column_name:
-            template_str = "$column " + template_str
+    #     if include_column_name:
+    #         template_str = "$column " + template_str
 
-        if params["row_condition"] is not None:
-            (
-                conditional_template_str,
-                conditional_params,
-            ) = parse_row_condition_string_pandas_engine(params["row_condition"])
-            template_str = conditional_template_str + ", then " + template_str
-            params.update(conditional_params)
+    #     if params["row_condition"] is not None:
+    #         (
+    #             conditional_template_str,
+    #             conditional_params,
+    #         ) = parse_row_condition_string_pandas_engine(params["row_condition"])
+    #         template_str = conditional_template_str + ", then " + template_str
+    #         params.update(conditional_params)
 
-        params_with_json_schema = {
-            "column": {"schema": {"type": "string"}, "value": params.get("column")},
-            "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
-            "mostly_pct": {
-                "schema": {"type": "number"},
-                "value": params.get("mostly_pct"),
-            },
-            "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
-            "row_condition": {
-                "schema": {"type": "string"},
-                "value": params.get("row_condition"),
-            },
-            "condition_parser": {
-                "schema": {"type": "string"},
-                "value": params.get("condition_parser"),
-            },
-        }
+    #     params_with_json_schema = {
+    #         "column": {"schema": {"type": "string"}, "value": params.get("column")},
+    #         "mostly": {"schema": {"type": "number"}, "value": params.get("mostly")},
+    #         "mostly_pct": {
+    #             "schema": {"type": "number"},
+    #             "value": params.get("mostly_pct"),
+    #         },
+    #         "regex": {"schema": {"type": "string"}, "value": params.get("regex")},
+    #         "row_condition": {
+    #             "schema": {"type": "string"},
+    #             "value": params.get("row_condition"),
+    #         },
+    #         "condition_parser": {
+    #             "schema": {"type": "string"},
+    #             "value": params.get("condition_parser"),
+    #         },
+    #     }
 
-        return [
-            RenderedStringTemplateContent(
-                **{
-                    "content_block_type": "string_template",
-                    "string_template": {
-                        "template": template_str,
-                        "params": params,
-                        "styling": styling,
-                    },
-                }
-            )
-        ]
+    #     return [
+    #         RenderedStringTemplateContent(
+    #             **{
+    #                 "content_block_type": "string_template",
+    #                 "string_template": {
+    #                     "template": template_str,
+    #                     "params": params,
+    #                     "styling": styling,
+    #                 },
+    #             }
+    #         )
+    #     ]
