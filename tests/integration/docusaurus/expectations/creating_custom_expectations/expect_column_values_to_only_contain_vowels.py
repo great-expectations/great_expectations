d843bf9b07b1ba15ad0b615abd65cf3a22004cb5
Austin Robinson <austin@superconductive.com>
2022-03-22 11:52:58 -0600

    removed validate_config & doc updates for regex

--- tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_only_contain_vowels.py
+++ tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_only_contain_vowels.py
@@ -110,25 +110,6 @@ class ExpectColumnValuesToOnlyContainVowels(RegexBasedColumnMapExpectation):
         }
     ]
 
-    def validate_configuration(self, configuration: Optional[ExpectationConfiguration]):
-        """
-        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
-        necessary configuration arguments have been provided for the validation of the expectation.
-        Args:
-            configuration (OPTIONAL[ExpectationConfiguration]): \
-                An optional Expectation Configuration entry that will be used to configure the expectation
-        Returns:
-            True if the configuration has been validated successfully. Otherwise, raises an exception
-        """
-
-        # Setting up a configuration
-        super().validate_configuration(configuration)
-        if configuration is None:
-            configuration = self.configuration
-
-        # Vacuously True as all parameter validation is inherited
-        return True
-
     map_metric = RegexBasedColumnMapExpectation.register_metric(
         regex_snake_name=regex_snake_name,
         regex_camel_name=regex_camel_name,
