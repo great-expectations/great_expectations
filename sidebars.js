0b73469d4032ba2f36a42d35e55f4f0549f5ed48
pnadolny13 <patnadolny@gmail.com>
2022-03-22 18:06:28 -0400

    add docs on how to use GE with Meltano

--- sidebars.js
+++ sidebars.js
@@ -307,6 +307,7 @@ module.exports = {
         'deployment_patterns/how_to_use_great_expectations_in_flyte',
         'deployment_patterns/how_to_use_great_expectations_in_deepnote',
         'deployment_patterns/how_to_use_great_expectations_with_prefect',
+        'deployment_patterns/how_to_use_great_expectations_with_meltano',
 
         { type: 'doc', id: 'deployment_patterns/index', label: 'Index' }
       ]
