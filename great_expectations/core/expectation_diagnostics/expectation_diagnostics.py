0526352f6cc970d7db35d49855ac8aed61f61c63
Ken Wade <ken@superconductive.com>
2022-03-23 08:44:30 -0500

    Update _check_input_validation try to catch config error

--- great_expectations/core/expectation_diagnostics/expectation_diagnostics.py
+++ great_expectations/core/expectation_diagnostics/expectation_diagnostics.py
@@ -21,6 +21,7 @@ from great_expectations.core.expectation_diagnostics.supporting_types import (
     ExpectationTestDiagnostics,
 )
 from great_expectations.core.util import convert_to_json_serializable
+from great_expectations.exceptions import InvalidExpectationConfigurationError
 from great_expectations.expectations.registry import get_expectation_impl
 from great_expectations.types import SerializableDictDot
 from great_expectations.util import camel_to_snake, lint_code
@@ -358,7 +359,12 @@ class ExpectationDiagnostics(SerializableDictDot):
                     expectation_type=expectation_instance.expectation_type,
                     kwargs=first_test.input,
                 )
-                passed = expectation_instance.validate_configuration(expectation_config)
+                try:
+                    expectation_instance.validate_configuration(expectation_config)
+                except InvalidExpectationConfigurationError:
+                    pass
+                else:
+                    passed = True
 
         return ExpectationDiagnosticCheckMessage(
             message="Has basic input validation and type checking",
