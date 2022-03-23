3267d97ab7bd0bc02e206d287353b5941beea1d1
Austin Robinson <austin@superconductive.com>
2022-03-23 12:11:40 -0500

    linting

--- tests/actions/test_sns_action.py
+++ tests/actions/test_sns_action.py
@@ -1,25 +1,23 @@
 import pytest
 
 from great_expectations.checkpoint.util import send_sns_notification
-
 from great_expectations.core import ExpectationSuiteValidationResult
+
 from .conftest import sns
 
+
 def test_send_sns_notification(sns):
-    results =   {
-            "success": True,
-            "result": {
-                "observed_value": 5.0,
-                "element_count": 5,
-                "missing_count": None,
-                "missing_percent": None,
-            },
-        }
+    results = {
+        "success": True,
+        "result": {
+            "observed_value": 5.0,
+            "element_count": 5,
+            "missing_count": None,
+            "missing_percent": None,
+        },
+    }
     result = ExpectationSuiteValidationResult(**results)
     topic = "test"
-    sns.create_topic({"Name":topic})
-    response = send_sns_notification(topic,result.success,result.results)
+    sns.create_topic({"Name": topic})
+    response = send_sns_notification(topic, result.success, result.results)
     assert response.startswith("Successfully")
-
-
-
