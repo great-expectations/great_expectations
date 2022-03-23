626d2a63734426e10404e6b5bb7161ab607a847e
Shinnnyshinshin <will@superconductive.com>
2022-03-23 13:36:45 -0700

    update: after review and cleanup

--- great_expectations/core/yaml_handler.py
+++ great_expectations/core/yaml_handler.py
@@ -33,6 +33,7 @@ class YAMLHandler:
 
     def __init__(self):
         self._handler: YAML = YAML(typ="safe")
+        # TODO: ensure this does not break all usage of ruamel in GE codebase.
         self._handler.indent(mapping=2, sequence=4, offset=2)
         self._handler.default_flow_style = False
 
--- tests/core/test_yaml_handler.py
+++ tests/core/test_yaml_handler.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 from pathlib import Path
-from typing import Any, AnyStr, Optional
+from typing import Any, Optional
 
 import pytest
 
@@ -28,23 +28,25 @@ def simple_dict() -> dict:
     return simple_dict
 
 
-def test_load_correct_input(simple_yaml: str, simple_dict: dict) -> None:
-    yaml_handler: YAMLHandler = YAMLHandler()
+@pytest.fixture
+def yaml_handler() -> YAMLHandler:
+    return YAMLHandler()
+
+
+def test_load_correct_input(
+    simple_yaml: str, simple_dict: dict, yaml_handler: YAMLHandler
+) -> None:
     res: dict = yaml_handler.load(simple_yaml)
 
     assert res == simple_dict
 
 
-def test_load_incorrect_input() -> None:
-    yaml_handler: YAMLHandler = YAMLHandler()
-
+def test_load_incorrect_input(yaml_handler: YAMLHandler) -> None:
     with pytest.raises(TypeError):
         yaml_handler.load(12345)
 
 
-def test_file_output(tmp_path: Path) -> None:
-    yaml_handler: YAMLHandler = YAMLHandler()
-
+def test_file_output(tmp_path: Path, yaml_handler: YAMLHandler) -> None:
     simplest_yaml: str = "abc: 1"
     test_file: str = os.path.join(tmp_path, "out.yaml")
     out: Path = Path(test_file)
@@ -60,18 +62,16 @@ def test_file_output(tmp_path: Path) -> None:
     assert data_from_file == data
 
 
-def test_dump_default_behavior_with_no_stream_specified() -> None:
-    yaml_handler: YAMLHandler = YAMLHandler()
-
+def test_dump_default_behavior_with_no_stream_specified(
+    yaml_handler: YAMLHandler,
+) -> None:
     # when we specify no stream, then StringIO is used by default
     simplest_dict: dict = dict(abc=1)
     dumped: Optional[str] = yaml_handler.dump(simplest_dict)
     assert dumped == "abc: 1\n"
 
 
-def test_dump_stdout_specified(capsys) -> None:
-    yaml_handler: YAMLHandler = YAMLHandler()
-
+def test_dump_stdout_specified(capsys, yaml_handler: YAMLHandler) -> None:
     # ruamel documentation recommends that we specify the stream as stdout when we are using YAML to return a string.
     simplest_dict: dict = dict(abc=1)
     yaml_handler.dump(simplest_dict, stream=sys.stdout)
