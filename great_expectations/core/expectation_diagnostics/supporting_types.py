8665494aea556be845cb5b8581725321c6cbcc0d
kenwade4 <95714847+kenwade4@users.noreply.github.com>
2022-03-25 14:55:05 -0500

    [FEATURE] Improve diagnostic checklist details (#4548)
    * Update library_metadata check to provide details when it doesn't pass
    
    * In linting check, if snake_case doesn't match filename, show computed snake_case
    
    * Change class name for expect_column_values_to_be_valid_ipv4.py and remove package attr
    
    * Update convert_to_json_serializable to handle bytes
    
    * Update build_gallery.py script to convert diagnostics to JSON in separate try/except
    
    * Update build_gallery.py script to write expectation_library_v2.json file with indenting
    
    * Update _check_input_validation to tell if custom assert statements are used in validate_configuration

--- great_expectations/core/expectation_diagnostics/supporting_types.py
+++ great_expectations/core/expectation_diagnostics/supporting_types.py
@@ -37,6 +37,7 @@ class AugmentedLibraryMetadata(SerializableDictDot):
     library_metadata_passed_checks: bool
     has_full_test_suite: bool
     manually_reviewed_code: bool
+    problems: List[str] = field(default_factory=list)
 
     legacy_maturity_level_substitutions = {
         "experimental": "EXPERIMENTAL",
--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -1486,27 +1486,35 @@ class Expectation(metaclass=MetaExpectation):
             "has_full_test_suite": False,
             "manually_reviewed_code": False,
         }
+        required_keys = {"contributors", "tags"}
+        allowed_keys = {
+            "contributors",
+            "has_full_test_suite",
+            "manually_reviewed_code",
+            "maturity",
+            "requirements",
+            "tags",
+        }
+        problems = []
 
         if hasattr(self, "library_metadata"):
             augmented_library_metadata.update(self.library_metadata)
+            keys = set(self.library_metadata.keys())
+            missing_required_keys = required_keys - keys
+            forbidden_keys = keys - allowed_keys
 
-            keys = self.library_metadata.keys()
-            has_all_required_keys = all(key in keys for key in {"tags", "contributors"})
-            has_no_forbidden_keys = all(
-                key
-                in {
-                    "maturity",
-                    "tags",
-                    "contributors",
-                    "requirements",
-                    "has_full_test_suite",
-                    "manually_reviewed_code",
-                }
-                for key in keys
-            )
-            if has_all_required_keys and has_no_forbidden_keys:
+            if missing_required_keys:
+                problems.append(
+                    f"Missing required key(s): {sorted(missing_required_keys)}"
+                )
+            if forbidden_keys:
+                problems.append(f"Extra key(s) found: {sorted(forbidden_keys)}")
+            if not problems:
                 augmented_library_metadata["library_metadata_passed_checks"] = True
+        else:
+            problems.append("No library_metadata attribute found")
 
+        augmented_library_metadata["problems"] = problems
         return AugmentedLibraryMetadata.from_legacy_dict(augmented_library_metadata)
 
     def _get_maturity_checklist(
