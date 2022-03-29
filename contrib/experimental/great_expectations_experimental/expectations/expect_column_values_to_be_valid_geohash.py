906f53741539080a0eea8175edd67c5579f2a3e1
Chris Arnold <chrisarnold391@gmail.com>
2022-03-29 08:45:32 -0400

    use RegexBasedColumnMapExpectation base class instead

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_geohash.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_geohash.py
@@ -1,55 +1,30 @@
 """
-This is a template for creating custom ColumnMapExpectations.
+This is a template for creating custom RegexBasedColumnMapExpectations.
 For detailed instructions on how to use it, please see:
-    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_column_map_expectations
+    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_regex_based_column_map_expectations
 """
 
-import json
-import re
-from typing import Optional
+from typing import Dict, Optional
 
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
-from great_expectations.exceptions import InvalidExpectationConfigurationError
-from great_expectations.execution_engine import (
-    PandasExecutionEngine,
-    SparkDFExecutionEngine,
-    SqlAlchemyExecutionEngine,
+from great_expectations.exceptions.exceptions import (
+    InvalidExpectationConfigurationError,
 )
-from great_expectations.expectations.expectation import ColumnMapExpectation
-from great_expectations.expectations.metrics import (
-    ColumnMapMetricProvider,
-    column_condition_partial,
+from great_expectations.expectations.regex_based_column_map_expectation import (
+    RegexBasedColumnMapExpectation,
+    RegexColumnMapMetricProvider,
 )
 
 
-# This class defines a Metric to support your Expectation.
-# For most ColumnMapExpectations, the main business logic for calculation will live in this class.
-class ColumnValuesGeohash(ColumnMapMetricProvider):
-
-    # This is the id string that will be used to reference your metric.
-    condition_metric_name = "column_values.geohash"
-
-    # This method implements the core logic for the PandasExecutionEngine
-    @column_condition_partial(engine=PandasExecutionEngine)
-    def _pandas(cls, column, **kwargs):
-        return column.str.match(pat="^[0123456789bcdefghjkmnpqrstuvwxyz]+$")
-        # search = re.compile(r'[a-z0-9]').search
-        # return bool(search(column))
-
-    # This method defines the business logic for evaluating your metric when using a SqlAlchemyExecutionEngine
-    # @column_condition_partial(engine=SqlAlchemyExecutionEngine)
-    # def _sqlalchemy(cls, column, _dialect, **kwargs):
-    #     raise NotImplementedError
-
-    # This method defines the business logic for evaluating your metric when using a SparkDFExecutionEngine
-    # @column_condition_partial(engine=SparkDFExecutionEngine)
-    # def _spark(cls, column, **kwargs):
-    #     raise NotImplementedError
-
-
+# <snippet>
 # This class defines the Expectation itself
-class ExpectColumnValuesToBeValidGeohash(ColumnMapExpectation):
+class ExpectColumnValuesToBeValidGeohash(RegexBasedColumnMapExpectation):
     """Expect values in this column to be a valid geohash."""
+
+    # These values will be used to configure the metric created by your expectation
+    regex_camel_name = "GeohashCharacter"
+    regex = "^[0123456789bcdefghjkmnpqrstuvwxyz]+$"
+    semantic_type_name_plural = "geohash_characters"
 
     # These examples will be shown in the public gallery.
     # They will also be executed as unit tests for your Expectation.
@@ -59,7 +34,7 @@ class ExpectColumnValuesToBeValidGeohash(ColumnMapExpectation):
                 "valid_geohash": ["dpz8"],
                 "invalid_alphanumeric": ["apz8"],  # "a" is an invalid geohash char
                 "invalid_non_alphanumeric": ["dp2-"],
-                "empty": [""]
+                "empty": [""],
             },
             "tests": [
                 {
@@ -102,44 +77,11 @@ class ExpectColumnValuesToBeValidGeohash(ColumnMapExpectation):
         }
     ]
 
-    # This is the id string of the Metric used by this Expectation.
-    # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
-    map_metric = "column_values.geohash"
-
-    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("mostly",)
-
-    # This dictionary contains default values for any parameters that should have default values
-    default_kwarg_values = {}
-
-    def validate_configuration(
-        self, configuration: Optional[ExpectationConfiguration]
-    ) -> None:
-        """
-        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
-        necessary configuration arguments have been provided for the validation of the expectation.
-
-        Args:
-            configuration (OPTIONAL[ExpectationConfiguration]): \
-                An optional Expectation Configuration entry that will be used to configure the expectation
-        Returns:
-            None. Raises InvalidExpectationConfigurationError if the config is not validated successfully
-        """
-
-        super().validate_configuration(configuration)
-        if configuration is None:
-            configuration = self.configuration
-
-        # # Check other things in configuration.kwargs and raise Exceptions if needed
-        # try:
-        #     assert (
-        #         ...
-        #     ), "message"
-        #     assert (
-        #         ...
-        #     ), "message"
-        # except AssertionError as e:
-        #     raise InvalidExpectationConfigurationError(str(e))
+    # Here your regex is used to create a custom metric for this expectation
+    map_metric = RegexBasedColumnMapExpectation.register_metric(
+        regex_camel_name=regex_camel_name,
+        regex_=regex,
+    )
 
     # This object contains metadata for display in the public Gallery
     library_metadata = {
@@ -153,5 +95,6 @@ class ExpectColumnValuesToBeValidGeohash(ColumnMapExpectation):
     }
 
 
+# </snippet>
 if __name__ == "__main__":
     ExpectColumnValuesToBeValidGeohash().print_diagnostic_checklist()
