f49d4c787d2657c47535afc4562d39b69f2dc725
luismdiaz01 <luismdiaz002@gmail.com>
2022-04-03 12:39:06 -0700

    modified imports

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_line_km_between.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_line_km_between.py
@@ -1,15 +1,13 @@
 import json
 from typing import Optional
+
 import geopandas
 from shapely.geometry import LineString
 
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
 from great_expectations.exceptions import InvalidExpectationConfigurationError
-from great_expectations.execution_engine import (
-    PandasExecutionEngine,
-    SparkDFExecutionEngine,
-    SqlAlchemyExecutionEngine,
-)
+from great_expectations.execution_engine import PandasExecutionEngine
+
 from great_expectations.expectations.expectation import ColumnMapExpectation
 from great_expectations.expectations.metrics import (
     ColumnMapMetricProvider,
