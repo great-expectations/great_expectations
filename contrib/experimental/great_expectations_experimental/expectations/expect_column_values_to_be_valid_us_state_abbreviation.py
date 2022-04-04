5e005e26fda71def8bc25b439b1ccec63c4d27b6
Derek Martin <derekma73@gmail.com>
2022-04-03 21:19:53 -0400

    removing unused libraries

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_us_state_abbreviation.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_us_state_abbreviation.py
@@ -3,17 +3,12 @@ This is a template for creating custom ColumnMapExpectations.
 For detailed instructions on how to use it, please see:
     https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_column_map_expectations
 """
-import json
 from typing import Optional
 
 import us
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
-from great_expectations.exceptions import InvalidExpectationConfigurationError
-from great_expectations.execution_engine import (
-    PandasExecutionEngine,
-    SparkDFExecutionEngine,
-    SqlAlchemyExecutionEngine,
-)
+from great_expectations.execution_engine import PandasExecutionEngine
+
 from great_expectations.expectations.expectation import ColumnMapExpectation
 from great_expectations.expectations.metrics import (
     ColumnMapMetricProvider,
