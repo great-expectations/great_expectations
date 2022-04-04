096c3767a78b60c08fb8b17966ede0a4bf14325e
luismdiaz01 <ldiaz@afscme.org>
2022-04-04 16:22:07 -0400

    fixed linestring, reformatted with black and istring

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_line_miles_between.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_line_miles_between.py
@@ -7,7 +7,6 @@ from shapely.geometry import LineString
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
 from great_expectations.exceptions import InvalidExpectationConfigurationError
 from great_expectations.execution_engine import PandasExecutionEngine
-
 from great_expectations.expectations.expectation import ColumnMapExpectation
 from great_expectations.expectations.metrics import (
     ColumnMapMetricProvider,
@@ -35,8 +34,8 @@ class ColumnValuesLinestringMilesDistanceBetween(ColumnMapMetricProvider):
         column = geopandas.GeoSeries(column)
         # Set crs to meters
         column = column.to_crs({"proj": "cea"})
-        #access the length of the column. In meters so have to turn to miles
-        col_len = column.length *0.000621371192
+        # access the length of the column. In meters so have to turn to miles
+        col_len = column.length * 0.000621371192
         return (col_len >= min_distance) & (col_len <= max_distance)
 
 
@@ -57,20 +56,19 @@ class ExpectColumnValuesToBeLinestringMilesDistanceBetween(ColumnMapExpectation)
     """This expectation will compute the distance of Linestring
     in miles and check if it's between two values."""
 
-
     # These examples will be shown in the public gallery, and also executed as unit tests for your Expectation
     examples = [
         {
             "data": {
                 "linestring_less_than_500_miles": [
-                    "LINESTRING (0 0, 111319.490793 110568.8124, 0 110568.8124)",
-                    "LINESTRING (0 0, 111319.490793 110568.8124, 111319.490793 0, 0 110568.8124)",
-                    "LINESTRING (0 0, 222638.981587 221104.845779, 222638.981587 0)"
+                    "LineString ([0 0, 111319.490793 110568.8124, 0 110568.8124])",
+                    "LineString ([0 0, 111319.490793 110568.8124, 111319.490793 0, 0 110568.8124])",
+                    "LineString ([0 0, 222638.981587 221104.845779, 222638.981587 0])",
                 ],
                 "linestring_between_1000_and_2000_miles": [
-                    "LINESTRING (222638.981587 552188.640112, 111319.490793 772147.013102, 1113194.907933 1209055.279421)",
-                    "LINESTRING (111319.490793 881798.964757, 1001875.417139 221104.845779, 111319.490793 0, 556597.453966 1317466.085138)",
-                    "LINESTRING (556597.453966 552188.640112, 1224514.398726 1209055.279421, 779236.435553 881798.964757)"
+                    "LineString ([222638.981587 552188.640112, 111319.490793 772147.013102, 1113194.907933 1209055.279421])",
+                    "LineString ([111319.490793 881798.964757, 1001875.417139 221104.845779, 111319.490793 0, 556597.453966 1317466.085138])",
+                    "LineString ([556597.453966 552188.640112, 1224514.398726 1209055.279421, 779236.435553 881798.964757])",
                 ],
             },
             "tests": [
@@ -112,8 +110,10 @@ class ExpectColumnValuesToBeLinestringMilesDistanceBetween(ColumnMapExpectation)
             "hackathon-22",
         ],  # Tags for this Expectation in the gallery
         "contributors": [  # Github handles for all contributors to this Expectation.
-            "@luismdiaz01","@derekma73"
+            "@luismdiaz01",
+            "@derekma73",
         ],
+        "requirements": ["geopandas", "shapely"],
     }
 
     # This is the id string of the Metric used by this Expectation.
