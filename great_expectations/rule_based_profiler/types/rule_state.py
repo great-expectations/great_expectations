c1fa8b5907be2987ca1c77f3e51527a877d93ad2
Alex Sherstinsky <alex@superconductive.com>
2022-04-05 13:36:33 -0700

    Introduce RuleState class.

---
+++ great_expectations/rule_based_profiler/types/rule_state.py
@@ -0,0 +1,104 @@
+from typing import Dict, List, Optional
+
+import great_expectations.exceptions as ge_exceptions
+from great_expectations.rule_based_profiler.types import Domain, ParameterContainer
+
+
+class RuleState:
+    def __init__(
+        self,
+        rule: "Rule",  # noqa: F821
+        domains: Optional[List[Domain]] = None,
+        variables: Optional[ParameterContainer] = None,
+        parameters: Optional[Dict[str, ParameterContainer]] = None,
+    ):
+        self._rule = rule
+
+        if domains is None:
+            domains = []
+
+        self._domains = domains
+
+        self._variables = variables
+
+        if parameters is None:
+            parameters = {}
+
+        self._parameters = parameters
+
+    @property
+    def rule(self) -> "Rule":  # noqa: F821:
+        return self._rule
+
+    @property
+    def domains(self) -> List[Domain]:
+        return self._domains
+
+    @domains.setter
+    def domains(self, value: Optional[List[Domain]]) -> None:
+        self._domains = value
+
+    @property
+    def variables(self) -> Optional[ParameterContainer]:
+        return self._variables
+
+    @property
+    def parameters(self) -> Dict[str, ParameterContainer]:
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, value: Optional[Dict[str, ParameterContainer]]) -> None:
+        self._parameters = value
+
+    def reset(self) -> None:
+        self.reset_domains()
+        self.reset_parameter_containers()
+
+    def reset_domains(self) -> None:
+        self.domains = []
+
+    def reset_parameter_containers(self) -> None:
+        self.parameters = {}
+
+    def add_domain(
+        self,
+        domain: Domain,
+        allow_duplicates: bool = False,
+    ) -> None:
+        domain_cursor: Domain
+        if not allow_duplicates and domain.id in [
+            domain_cursor.id for domain_cursor in self.domains
+        ]:
+            raise ge_exceptions.ProfilerConfigurationError(
+                f"""Error: Domain\n{domain}\nalready exists.  In order to add it, either pass "allow_duplicates=True" \
+or call "RuleState.remove_domain_if_exists()" with Domain having ID equal to "{domain.id}" as argument first.
+"""
+            )
+        self.domains.append(domain)
+
+    def remove_domain_if_exists(self, domain: Domain) -> None:
+        domain_cursor: Domain
+        if domain.id in [domain_cursor.id for domain_cursor in self.domains]:
+            self.domains.remove(domain)
+            self.remove_domain_if_exists(domain=domain)
+
+    def initialize_parameter_container_for_domain(
+        self,
+        domain: Domain,
+        overwrite: bool = True,
+    ) -> None:
+        if not overwrite and domain.id in self.parameters:
+            raise ge_exceptions.ProfilerConfigurationError(
+                f"""Error: ParameterContainer for Domain\n{domain}\nalready exists.  In order to overwrite it, either \
+pass "overwrite=True" or call "RuleState.remove_parameter_container_from_domain()" with Domain having ID equal to \
+"{domain.id}" as argument first.
+"""
+            )
+
+        parameter_container: ParameterContainer = ParameterContainer(
+            parameter_nodes=None
+        )
+        self._parameters[domain.id] = parameter_container
+
+    def remove_parameter_container_from_domain_if_exists(self, domain: Domain) -> None:
+        self.parameters.pop(domain.id, None)
