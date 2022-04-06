e2082831bf3c103b8dfa09f3f97b18cf78b1365f
Nathan Farmer <nathan@superconductive.com>
2022-04-06 10:33:30 -0400

    Documentation

--- great_expectations/datasource/data_connector/configured_asset_sql_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_sql_data_connector.py
@@ -185,8 +185,8 @@ class ConfiguredAssetSqlDataConnector(DataConnector):
         batch_definition_list: List[BatchDefinition] = []
         if batch_request.data_asset_name in self._data_references_cache:
             sub_cache = self._data_references_cache[batch_request.data_asset_name]
-        # if the batch_request is missing data_asset we need to add it to references cache for batch_definition and
-        # update the data_connector config to propagate this new asset to data_context config
+        # if the data_asset_name does not exist in references cache we need to add it as a table_name in order to get
+        # a batch_definition and update the data_connector config to propagate this new asset to data_context config
         else:
             self.add_data_asset(
                 batch_request.data_asset_name,
