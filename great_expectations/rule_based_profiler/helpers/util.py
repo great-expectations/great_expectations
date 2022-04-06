5320ac34a4d6c7bc8c9e1dd279cf4b6d47bdd125
Nathan Farmer <nathan@superconductive.com>
2022-04-06 15:07:41 -0400

    Add ML Flow Experiment ID to identify where the previous versions and testing live

--- great_expectations/rule_based_profiler/helpers/util.py
+++ great_expectations/rule_based_profiler/helpers/util.py
@@ -417,6 +417,9 @@ def _compute_bootstrap_quantiles_point_estimate(
     random_seed: Optional[int] = None,
 ) -> Tuple[Number, Number]:
     """
+    ML Flow Experiment: bootstrap_quantiles
+    ML Flow Experiment ID: 4129654509298109
+
     An internal implementation of the "bootstrap" estimator method, returning a point estimate for a population
     parameter of interest (lower and upper quantiles in this case). See
     https://en.wikipedia.org/wiki/Bootstrapping_(statistics) for an introduction to "bootstrapping" in statistics.
