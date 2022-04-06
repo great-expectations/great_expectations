1d6fa6b065abe6d70cb6ebd8179c41dbe6e32666
backfire <lin.yuan@epfl.ch>
2022-04-06 02:17:40 +0200

    fix lint problem

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_powerful_number.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_powerful_number.py
@@ -10,6 +10,7 @@ from great_expectations.expectations.metrics import (
     column_condition_partial,
 )
 
+
 # This method compares a string to the valid powerful number
 def is_valid_powerful_number(num: str) -> bool:
     try:
