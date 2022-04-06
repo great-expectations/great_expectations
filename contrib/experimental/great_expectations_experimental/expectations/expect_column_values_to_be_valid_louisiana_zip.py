9090d48174bdfd87151fbf0df2ce081e2c396112
Derek Martin <46758585+Derekma73@users.noreply.github.com>
2022-04-06 16:32:00 -0400

    fixing doc strings

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_louisiana_zip.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_louisiana_zip.py
@@ -55,7 +55,7 @@ class ColumnValuesToBeValidLouisianaZip(ColumnMapMetricProvider):
 
 # This class defines the Expectation itself
 class ExpectColumnValuesToBeValidLouisianaZip(ColumnMapExpectation):
-    """Expect values in this column to be valid Kenucky zipcodes.
+    """Expect values in this column to be valid Louisiana zipcodes.
     See https://pypi.org/project/zipcodes/ for more information.
     """
 
