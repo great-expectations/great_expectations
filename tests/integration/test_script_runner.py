be35a1601c70d8e10d21051df37a6ee7758dace2
Rachel-Reverie <rachel@superconductive.com>
2022-04-07 12:00:25 -0500

    -correction to name values in test_script_runner.py

--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -204,13 +204,13 @@ local_tests = [
         data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
     ),
     IntegrationTestFixture(
-        name="how_to_pass_an_in_memory_dataframe_to_a_checkpoint",
+        name="how_to_validate_data_with_a_yaml_configured_in_memory_checkpoint",
         user_flow_script="tests/integration/docusaurus/validation/checkpoints/how_to_validate_data_with_a_yaml_configured_in_memory_checkpoint.py",
         data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
         data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
     ),
     IntegrationTestFixture(
-        name="how_to_pass_an_in_memory_dataframe_to_a_checkpoint",
+        name="how_to_validate_data_with_a_python_configured_in_memory_checkpoint",
         user_flow_script="tests/integration/docusaurus/validation/checkpoints/how_to_validate_data_with_a_python_configured_in_memory_checkpoint.py",
         data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
         data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
