727fde3013e47cd99e2d388761ca34c836b5bffa
Derek Martin <46758585+Derekma73@users.noreply.github.com>
2022-04-08 15:11:32 -0400

    linting

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_north_carolina_zip.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_north_carolina_zip.py
@@ -19,7 +19,9 @@ from great_expectations.expectations.metrics import (
 
 def is_valid_north_carolina_zip(zip: str):
     list_of_dicts_of_north_carolina_zips = zipcodes.filter_by(state="NC")
-    list_of_north_carolina_zips = [d["zip_code"] for d in list_of_dicts_of_north_carolina_zips]
+    list_of_north_carolina_zips = [
+        d["zip_code"] for d in list_of_dicts_of_north_carolina_zips
+    ]
     if len(zip) > 10:
         return False
     elif type(zip) != str:
@@ -102,7 +104,6 @@ class ExpectColumnValuesToBeValidNorthCarolinaZip(ColumnMapExpectation):
         """
         Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
         necessary configuration arguments have been provided for the validation of the expectation.
-
         Args:
             configuration (OPTIONAL[ExpectationConfiguration]): \
                 An optional Expectation Configuration entry that will be used to configure the expectation
