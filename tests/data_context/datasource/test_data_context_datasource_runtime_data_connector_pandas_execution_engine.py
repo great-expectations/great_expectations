a3057c529ca2dc2e6d3670f31102c781f52f9ae9
Shinnnyshinshin <will@superconductive.com>
2022-04-13 15:08:14 -0700

    added data_context level tests

--- tests/data_context/datasource/test_data_context_datasource_runtime_data_connector_pandas_execution_engine.py
+++ tests/data_context/datasource/test_data_context_datasource_runtime_data_connector_pandas_execution_engine.py
@@ -42,6 +42,65 @@ def test_get_batch_successful_specification_pandas_engine(
     assert isinstance(batch_list[0], Batch)
 
 
+def test_get_batch_successful_specification_pandas_engine_named_asset(
+    data_context_with_datasource_pandas_engine, test_df_pandas
+):
+    context = data_context_with_datasource_pandas_engine
+    test_df: pd.DataFrame = test_df_pandas
+    batch_identifiers: dict = {"day": 1, "month": 12}
+    batch_list: list = context.get_batch_list(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"batch_data": test_df},
+            batch_identifiers=batch_identifiers,
+        )
+    )
+    assert len(batch_list) == 1
+    assert isinstance(batch_list[0], Batch)
+
+    batch_1: Batch = batch_list[0]
+    assert batch_1.batch_definition.batch_identifiers == batch_identifiers
+
+
+def test_get_batch_successful_specification_pandas_engine_named_asset_two_batch_definition(
+    data_context_with_datasource_pandas_engine, test_df_pandas
+):
+    context = data_context_with_datasource_pandas_engine
+    test_df: pd.DataFrame = test_df_pandas
+
+    batch_identifiers: dict = {"day": 1, "month": 12}
+    batch_list: list = context.get_batch_list(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"batch_data": test_df},
+            batch_identifiers=batch_identifiers,
+        )
+    )
+    assert len(batch_list) == 1
+    assert isinstance(batch_list[0], Batch)
+    batch_1: Batch = batch_list[0]
+    assert batch_1.batch_definition.batch_identifiers == batch_identifiers
+
+    batch_identifiers: dict = {"day": 2, "month": 12}
+    batch_list: list = context.get_batch_list(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"batch_data": test_df},
+            batch_identifiers=batch_identifiers,
+        )
+    )
+    assert len(batch_list) == 1
+    assert isinstance(batch_list[0], Batch)
+    batch_2: Batch = batch_list[0]
+    assert batch_2.batch_definition.batch_identifiers == batch_identifiers
+
+
 def test_get_batch_ambiguous_parameter_pandas_engine(
     data_context_with_datasource_pandas_engine, test_df_pandas
 ):
@@ -215,6 +274,31 @@ def test_get_validator_successful_specification_pandas_engine(
     assert isinstance(my_validator, Validator)
 
 
+def test_get_validator_successful_specification_pandas_engine_named_asset(
+    data_context_with_datasource_pandas_engine, test_df_pandas
+):
+    context = data_context_with_datasource_pandas_engine
+    test_df: pd.DataFrame = test_df_pandas
+    batch_identifiers: dict = {"day": 1, "month": 12}
+    context.create_expectation_suite("my_expectations")
+    # Successful specification using a RuntimeBatchRequest
+    my_validator = context.get_validator(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"batch_data": test_df},
+            batch_identifiers=batch_identifiers,
+        ),
+        expectation_suite_name="my_expectations",
+    )
+    assert isinstance(my_validator, Validator)
+    assert (
+        my_validator.active_batch.batch_definition.batch_identifiers
+        == batch_identifiers
+    )
+
+
 def test_get_validator_ambiguous_parameter_pandas_engine(
     data_context_with_datasource_pandas_engine, test_df_pandas
 ):
@@ -382,6 +466,9 @@ def test_get_validator_wrong_runtime_parameters_pandas_engine(
         )
 
 
+# TODO : get_batch_list()
+# TODO : get_validator()
+
 ###################################
 # Tests with data passed in as path
 ###################################
@@ -491,3 +578,48 @@ def test_get_batch_pandas_wrong_reader_method(
                 batch_spec_passthrough={"reader_method": "i_am_not_valid"},
             )
         )
+
+
+def test_get_batch_successful_specification_pandas_engine_named_asset_file_path(
+    data_context_with_datasource_pandas_engine, taxi_test_file
+):
+    context = data_context_with_datasource_pandas_engine
+    batch_identifiers: dict = {"day": 1, "month": 12}
+    batch_list: list = context.get_batch_list(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"path": taxi_test_file},
+            batch_identifiers=batch_identifiers,
+        )
+    )
+    assert len(batch_list) == 1
+    assert isinstance(batch_list[0], Batch)
+
+    batch_1: Batch = batch_list[0]
+    assert batch_1.batch_definition.batch_identifiers == batch_identifiers
+
+
+def test_get_validator_successful_specification_pandas_engine_named_asset_file_path(
+    data_context_with_datasource_pandas_engine, taxi_test_file
+):
+    context = data_context_with_datasource_pandas_engine
+    batch_identifiers: dict = {"day": 1, "month": 12}
+    context.create_expectation_suite("my_expectations")
+    # Successful specification using a RuntimeBatchRequest
+    my_validator = context.get_validator(
+        batch_request=RuntimeBatchRequest(
+            datasource_name="my_datasource",
+            data_connector_name="default_runtime_data_connector_name",
+            data_asset_name="asset_a",
+            runtime_parameters={"path": taxi_test_file},
+            batch_identifiers=batch_identifiers,
+        ),
+        expectation_suite_name="my_expectations",
+    )
+    assert isinstance(my_validator, Validator)
+    assert (
+        my_validator.active_batch.batch_definition.batch_identifiers
+        == batch_identifiers
+    )
