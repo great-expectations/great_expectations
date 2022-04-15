8fdd8f2ea857d0302ab967c6022f780511351e7d
Nathan Farmer <nathan@superconductive.com>
2022-04-15 14:21:01 -0400

    Use regex instead of string matching

--- great_expectations/rule_based_profiler/types/data_assistant_result.py
+++ great_expectations/rule_based_profiler/types/data_assistant_result.py
@@ -1,3 +1,4 @@
+import re
 from abc import ABCMeta
 from dataclasses import asdict, dataclass
 from numbers import Number
@@ -5,7 +6,15 @@ from typing import Any, Dict, List, Optional
 
 from great_expectations.core import ExpectationConfiguration, ExpectationSuite
 from great_expectations.core.util import convert_to_json_serializable
-from great_expectations.rule_based_profiler.types import Domain, ParameterNode
+from great_expectations.rule_based_profiler.types import (
+    FULLY_QUALIFIED_PARAMETER_NAME_ATTRIBUTED_VALUE_KEY,
+    FULLY_QUALIFIED_PARAMETER_NAME_DELIMITER_CHARACTER,
+    FULLY_QUALIFIED_PARAMETER_NAME_METADATA_KEY,
+    FULLY_QUALIFIED_PARAMETER_NAME_SEPARATOR_CHARACTER,
+    PARAMETER_NAME_ROOT_FOR_PARAMETERS,
+    Domain,
+    ParameterNode,
+)
 from great_expectations.types import SerializableDictDot
 
 
@@ -33,14 +42,35 @@ class DataAssistantResult(SerializableDictDot):
         metric_nodes: Dict[str, ParameterNode]
         parameter_node_name: str
         parameter_node: ParameterNode
+
+        parameter_node_name_regex_list: list = [
+            re.escape(FULLY_QUALIFIED_PARAMETER_NAME_DELIMITER_CHARACTER),
+            PARAMETER_NAME_ROOT_FOR_PARAMETERS,
+            re.escape(FULLY_QUALIFIED_PARAMETER_NAME_SEPARATOR_CHARACTER),
+            "\\w+",
+            re.escape(FULLY_QUALIFIED_PARAMETER_NAME_SEPARATOR_CHARACTER),
+        ]
+
         for metric_domain, metric_nodes in metrics.items():
             for parameter_node_name, parameter_node in metric_nodes.items():
-                if parameter_node_name.endswith(".details"):
+                details_key_regex_str: str = "".join(
+                    parameter_node_name_regex_list
+                    + [FULLY_QUALIFIED_PARAMETER_NAME_METADATA_KEY]
+                )
+                if re.match(details_key_regex_str, parameter_node_name) is not None:
                     details_parameter_node: ParameterNode = parameter_node
                     metric_names.append(
                         details_parameter_node.metric_configuration.metric_name
                     )
-                if parameter_node_name.endswith(".attributed_value"):
+
+                attributed_value_key_regex_str: str = "".join(
+                    parameter_node_name_regex_list
+                    + [FULLY_QUALIFIED_PARAMETER_NAME_ATTRIBUTED_VALUE_KEY]
+                )
+                if (
+                    re.match(attributed_value_key_regex_str, parameter_node_name)
+                    is not None
+                ):
                     attributed_value_parameter_node: ParameterNode = parameter_node
                     data: list[Number] = sum(
                         attributed_value_parameter_node.values(), []
