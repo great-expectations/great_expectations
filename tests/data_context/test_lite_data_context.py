f0227a67fe685685bf7eaf10e94dd0c268b89c6e
Abe Gong <abegong@gmail.com>
2022-04-17 07:58:39 -0600

    Add a test for .sources. Fix fixtures path for other tests

--- tests/data_context/test_lite_data_context.py
+++ tests/data_context/test_lite_data_context.py
@@ -7,7 +7,7 @@ from tests.test_utils import _get_batch_request_from_validator
 def test_LiteDataContext_read_csv_with_default_pandas_reader():
     ldc = LiteDataContext()
     my_validator = ldc.datasources["default_pandas_reader"].read_csv(
-        file_relative_path(__file__, "example_1.csv"),
+        file_relative_path(__file__, "../datasource/fixtures/example_1.csv"),
     )
     my_batch_request = _get_batch_request_from_validator(my_validator)
 
@@ -21,7 +21,7 @@ def test_LiteDataContext_read_csv_with_default_pandas_reader():
 def test_LiteDataContext_read_csv_with_default_pandas_reader_using_dot_notation():
     ldc = LiteDataContext()
     my_validator = ldc.datasources.default_pandas_reader.read_csv(
-        file_relative_path(__file__, "example_1.csv"),
+        file_relative_path(__file__, "../datasource/fixtures/example_1.csv"),
     )
     my_batch_request = _get_batch_request_from_validator(my_validator)
 
@@ -31,3 +31,7 @@ def test_LiteDataContext_read_csv_with_default_pandas_reader_using_dot_notation(
         "b": {0:2, 1:5},
         "c": {0:3, 1:6},
     }
+
+def test_LiteDataContext_sources():
+    ldc = LiteDataContext()
+    assert ldc.sources.default_pandas_reader == ldc.datasources.default_pandas_reader
