e821b2212c783b6623a653c219f9d756f6d986b8
Chetan Kini <chetan@superconductive.com>
2022-04-20 12:26:08 -0400

    chore: write docstring

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -2446,6 +2446,15 @@ def _format_map_output(
 def get_default_profiler_config_for_expectation_type(
     expectation_type: str,
 ) -> Optional[RuleBasedProfilerConfig]:
+    """Retrieves the default profiler config as defined within a given Expectation.
+
+    Args:
+        expectation_type (str): The name of the Expectation to parse
+
+    Returns:
+        The default profiler config within the target Expectation.
+        If not available, returns None.
+    """
     expectation_impl = get_expectation_impl(expectation_name=expectation_type)
     profiler_config: Optional[
         RuleBasedProfilerConfig
