d0a753eeb88753837937c3b427c5e541f9fddc53
Austin Robinson <austin@superconductive.com>
2022-04-21 16:14:48 -0600

    bringing regex & set based column map expectation scripts under test

--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -259,6 +259,14 @@ local_tests = [
         name="expect_table_columns_to_be_unique",
         user_flow_script="tests/integration/docusaurus/expectations/creating_custom_expectations/expect_table_columns_to_be_unique.py",
     ),
+    IntegrationTestFixture(
+        name="expect_column_values_to_be_in_solfege_scale_set",
+        user_flow_script="tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_be_in_solfege_scale_set",
+    ),
+    IntegrationTestFixture(
+        name="expect_column_values_to_only_contain_vowels",
+        user_flow_script="tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_only_contain_vowels",
+    ),
 ]
 
 
