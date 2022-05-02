ee844bafd7f799787a2fdb3f07aa41b060a1bad0
Chetan Kini <chetan@superconductive.com>
2022-05-02 10:02:58 -0400

    chore: update error message

--- great_expectations/rule_based_profiler/data_assistant/data_assistant_dispatcher.py
+++ great_expectations/rule_based_profiler/data_assistant/data_assistant_dispatcher.py
@@ -78,7 +78,7 @@ class DataAssistantDispatcher:
         registered_data_assistants = cls._registered_data_assistants
 
         if name in registered_data_assistants:
-            raise ValueError(f'Multiple declarations of DataAssistant "{name}" found.')
+            raise ValueError(f'Existing declarations of DataAssistant "{name}" found.')
 
         logger.debug(
             f'Registering the declaration of DataAssistant "{name}" took place.'
