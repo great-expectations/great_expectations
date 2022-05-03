250a2bed59b58009454331f0e455da4624f28655
Austin Robinson <austin@superconductive.com>
2022-05-03 13:39:29 -0600

    cross-table script complete

--- tests/integration/docusaurus/expectations/advanced/user_configurable_profiler_cross_table_comparison.py
+++ tests/integration/docusaurus/expectations/advanced/user_configurable_profiler_cross_table_comparison.py
@@ -3,55 +3,40 @@ import os
 from ruamel import yaml
 
 import great_expectations as ge
-from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
-
-redshift_username = os.environ.get("REDSHIFT_USERNAME")
-redshift_password = os.environ.get("REDSHIFT_PASSWORD")
-redshift_host = os.environ.get("REDSHIFT_HOST")
-redshift_port = os.environ.get("REDSHIFT_PORT")
-redshift_database = os.environ.get("REDSHIFT_DATABASE")
-redshift_sslmode = os.environ.get("REDSHIFT_SSLMODE")
-
-sfAccount = os.environ.get("SNOWFLAKE_ACCOUNT")
-sfUser = os.environ.get("SNOWFLAKE_USER")
-sfPswd = os.environ.get("SNOWFLAKE_PW")
-sfDatabase = os.environ.get("SNOWFLAKE_DATABASE")
-sfSchema = os.environ.get("SNOWFLAKE_SCHEMA")
-sfWarehouse = os.environ.get("SNOWFLAKE_WAREHOUSE")
-
-SF_CONNECTION_STRING = f"snowflake://{sfUser}:{sfPswd}@{sfAccount}/{sfDatabase}/{sfSchema}?warehouse={sfWarehouse}"
-
-CONNECTION_STRING = f"postgresql+psycopg2://{redshift_username}:{redshift_password}@{redshift_host}:{redshift_port}/{redshift_database}?sslmode={redshift_sslmode}"
+from great_expectations.core.batch import BatchRequest
+from great_expectations.profile.user_configurable_profiler import (
+    UserConfigurableProfiler,
+)
 
 # This utility is not for general use. It is only to support testing.
 from tests.test_utils import load_data_into_test_database
 
+MY_CONNECTION_STRING = "mysql+pymysql://root:root@localhost/test_ci"
+
+PG_CONNECTION_STRING = "postgresql+psycopg2://postgres:@localhost/test_ci"
+
 load_data_into_test_database(
-    table_name="taxi_data",
+    table_name="mysql_taxi_data",
     csv_path="tests/test_sets/taxi_yellow_tripdata_samples/yellow_tripdata_sample_2019-01.csv",
-    connection_string=CONNECTION_STRING,
+    connection_string=MY_CONNECTION_STRING,
 )
 
 load_data_into_test_database(
-    table_name="taxi_data",
+    table_name="postgres_taxi_data",
     csv_path="tests/test_sets/taxi_yellow_tripdata_samples/yellow_tripdata_sample_2019-01.csv",
-    connection_string=SF_CONNECTION_STRING,
+    connection_string=PG_CONNECTION_STRING,
 )
 
 context = ge.get_context()
 
-datasource_config = {
-    "name": "my_redshift_datasource",
+pg_datasource_config = {
+    "name": "my_postgresql_datasource",
     "class_name": "Datasource",
     "execution_engine": {
         "class_name": "SqlAlchemyExecutionEngine",
-        "connection_string": "postgresql+psycopg2://<USER_NAME>:<PASSWORD>@<HOST>:<PORT>/<DATABASE>?sslmode=<SSLMODE>",
+        "connection_string": f"{PG_CONNECTION_STRING}",
     },
     "data_connectors": {
-        "default_runtime_data_connector_name": {
-            "class_name": "RuntimeDataConnector",
-            "batch_identifiers": ["default_identifier_name"],
-        },
         "default_inferred_data_connector_name": {
             "class_name": "InferredAssetSqlDataConnector",
             "include_schema_name": True,
@@ -59,18 +44,14 @@ datasource_config = {
     },
 }
 
-sf_datasource_config = {
-    "name": "my_snowflake_datasource",
+mysql_datasource_config = {
+    "name": "my_mysql_datasource",
     "class_name": "Datasource",
     "execution_engine": {
         "class_name": "SqlAlchemyExecutionEngine",
-        "connection_string": "snowflake://<USER_NAME>:<PASSWORD>@<ACCOUNT_NAME>/<DATABASE_NAME>/<SCHEMA_NAME>?warehouse=<WAREHOUSE_NAME>&role=<ROLE_NAME>",
+        "connection_string": f"{MY_CONNECTION_STRING}",
     },
     "data_connectors": {
-        "default_runtime_data_connector_name": {
-            "class_name": "RuntimeDataConnector",
-            "batch_identifiers": ["default_identifier_name"],
-        },
         "default_inferred_data_connector_name": {
             "class_name": "InferredAssetSqlDataConnector",
             "include_schema_name": True,
@@ -80,28 +61,70 @@ sf_datasource_config = {
 
 # Please note this override is only to provide good UX for docs and tests.
 # In normal usage you'd set your path directly in the yaml above.
-sf_datasource_config["execution_engine"]["connection_string"] = SF_CONNECTION_STRING
+pg_datasource_config["execution_engine"]["connection_string"] = PG_CONNECTION_STRING
 
-context.test_yaml_config(yaml.dump(datasource_config))
+context.test_yaml_config(yaml.dump(pg_datasource_config))
 
-context.add_datasource(**datasource_config)
+context.add_datasource(**pg_datasource_config)
 
 # Please note this override is only to provide good UX for docs and tests.
 # In normal usage you'd set your path directly in the yaml above.
-datasource_config["execution_engine"]["connection_string"] = CONNECTION_STRING
+mysql_datasource_config["execution_engine"]["connection_string"] = MY_CONNECTION_STRING
 
-context.test_yaml_config(yaml.dump(datasource_config))
+context.test_yaml_config(yaml.dump(mysql_datasource_config))
 
-context.add_datasource(**datasource_config)
+context.add_datasource(**mysql_datasource_config)
 
-batch_request = BatchRequest(
-    datasource_name="my_redshift_datasource",
+mysql_batch_request = BatchRequest(
+    datasource_name="my_mysql_datasource",
     data_connector_name="default_inferred_data_connector_name",
-    data_asset_name="default_name",
+    data_asset_name="test_ci.mysql_taxi_data",
+)
+
+pg_batch_request = BatchRequest(
+    datasource_name="my_postgresql_datasource",
+    data_connector_name="default_inferred_data_connector_name",
+    data_asset_name="public.postgres_taxi_data",
 )
 
 expectation_suite_name = "compare_two_tables"
 
-validator = context.get_validator(
-    batch_request=batch_request, expectation_suite_name=expectation_suite_name
+validator = context.get_validator(batch_request=mysql_batch_request)
+
+profiler = UserConfigurableProfiler(
+    profile_dataset=validator,
+    excluded_expectations=["expect_column_quantile_values_to_be_between"],
 )
+
+suite = profiler.build_suite()
+context.save_expectation_suite(
+    expectation_suite=suite, expectation_suite_name=expectation_suite_name
+)
+
+my_checkpoint_name = "comparison_checkpoint"
+
+yaml_config = f"""
+name: {my_checkpoint_name}
+config_version: 1.0
+class_name: SimpleCheckpoint
+run_name_template: "%Y%m%d-%H%M%S-my-run-name-template"
+expectation_suite_name: {expectation_suite_name}
+"""
+
+context.add_checkpoint(**yaml.load(yaml_config))
+
+results = context.run_checkpoint(
+    checkpoint_name=my_checkpoint_name, batch_request=pg_batch_request
+)
+
+
+# Note to users: code below this line is only for integration testing -- ignore!
+
+assert results["success"] is True
+statistics = results["run_results"][list(results["run_results"].keys())[0]][
+    "validation_result"
+]["statistics"]
+assert statistics["evaluated_expectations"] != 0
+assert statistics["evaluated_expectations"] == statistics["successful_expectations"]
+assert statistics["unsuccessful_expectations"] == 0
+assert statistics["success_percent"] == 100.0
