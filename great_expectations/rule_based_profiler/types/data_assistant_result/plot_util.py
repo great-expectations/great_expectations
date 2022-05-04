b468cd2cc7a042ac46b834919181337bd10a7e31
Chetan Kini <chetan@superconductive.com>
2022-05-04 18:33:17 -0400

    docs: write docstrs

--- great_expectations/rule_based_profiler/types/data_assistant_result/plot_util.py
+++ great_expectations/rule_based_profiler/types/data_assistant_result/plot_util.py
@@ -21,6 +21,15 @@ class PlotUtil:
         return self.name.replace("_", " ").title()
 
     def generate_tooltip(self, format: str = "") -> alt.Tooltip:
+        """Wrapper arount alt.Tooltip creation.
+
+        Args:
+            format (str): Desired format within tooltip
+
+        Returns:
+            An instance of alt.Tooltip containing relevant information from the PlotUtil class.
+
+        """
         return alt.Tooltip(
             field=self.name,
             type=self.alt_type,
@@ -29,11 +38,23 @@ class PlotUtil:
         )
 
     def plot_on_axis(self, shorthand: Optional[str] = None) -> Union[alt.X, alt.Y]:
+        """Wrapper around alt.X/alt.Y plotting utility.
+
+        Args:
+            shorthand (Optional[str]): If provided, used as shorthand for field, aggregate, and type.
+
+        Returns:
+            Either an alt.X or alt.Y instance based on desired axis.
+
+        """
         raise NotImplementedError
 
 
 class MetricPlotUtil(PlotUtil):
     def plot_on_axis(self, shorthand: Optional[str] = None) -> alt.Y:
+        """
+        Plots metric on Y axis - see parenet `PlotUtil` for more details.
+        """
         if shorthand is None:
             shorthand = self.name
         return alt.Y(
@@ -49,6 +70,9 @@ class DomainPlotUtil(PlotUtil):
         return self.name.title()
 
     def plot_on_axis(self, shorthand: Optional[str] = None) -> alt.X:
+        """
+        Plots domain on X axis - see parenet `PlotUtil` for more details.
+        """
         if shorthand is None:
             shorthand = self.name
         return alt.X(
