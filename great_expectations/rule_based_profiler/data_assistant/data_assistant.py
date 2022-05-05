733684f29750b5e2a8c8bad46b3c45969133ac41
Chetan Kini <chetan@superconductive.com>
2022-05-05 12:55:51 -0400

    chore: revert more files

--- great_expectations/rule_based_profiler/data_assistant/data_assistant.py
+++ great_expectations/rule_based_profiler/data_assistant/data_assistant.py
@@ -5,7 +5,6 @@ from typing import Any, Dict, List, Optional, Union
 from great_expectations.core import ExpectationSuite
 from great_expectations.core.batch import Batch, BatchRequestBase
 from great_expectations.execution_engine.execution_engine import MetricDomainTypes
-from great_expectations.expectations.registry import register_data_assistant
 from great_expectations.rule_based_profiler.domain_builder import DomainBuilder
 from great_expectations.rule_based_profiler.expectation_configuration_builder import (
     ExpectationConfigurationBuilder,
@@ -48,6 +47,7 @@ class MetaDataAssistant(ABCMeta):
         if not newclass.is_abstract():
             # Only particular "DataAssistant" implementations must be registered.
             newclass.data_assistant_type = camel_to_snake(name=clsname)
+
             from great_expectations.rule_based_profiler.data_assistant.data_assistant_dispatcher import (
                 DataAssistantDispatcher,
             )
