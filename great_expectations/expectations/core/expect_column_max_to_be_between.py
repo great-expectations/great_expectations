cfbe324195c5100e6f25b0e348537386f88bffcb
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2022-05-09 08:26:45 -0700

    corrections to self-initializing expectations (#5068)

--- great_expectations/expectations/core/expect_column_max_to_be_between.py
+++ great_expectations/expectations/core/expect_column_max_to_be_between.py
@@ -156,7 +156,7 @@ class ExpectColumnMaxToBeBetween(ColumnExpectation):
                         "lower_bound": None,
                         "upper_bound": None,
                     },
-                    "round_decimals": 0,
+                    "round_decimals": 1,
                 },
                 "domain_builder": {
                     "class_name": "ColumnDomainBuilder",
