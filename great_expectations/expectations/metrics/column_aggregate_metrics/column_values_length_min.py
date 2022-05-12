ccd9bdc6a1f3dcf7b7d912a8a9b664a7c28fb636
Chetan Kini <chetan@superconductive.com>
2022-05-12 17:19:22 -0400

    chore: type hint

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_values_length_min.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_values_length_min.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from great_expectations.execution_engine import (
     PandasExecutionEngine,
     SparkDFExecutionEngine,
@@ -15,13 +17,14 @@ class ColumnValuesLengthMin(ColumnAggregateMetricProvider):
     metric_name = "column_values.length.min"
 
     @column_aggregate_value(engine=PandasExecutionEngine)
-    def _pandas(cls, column, **kwargs):
+    def _pandas(cls, column: pd.Series, **kwargs: dict) -> int:
         return column.map(len).min()
 
     @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(cls, column, **kwargs):
+    def _sqlalchemy(cls, column: "ColumnClause", **kwargs: dict) -> int:  # noqa: F821
+        breakpoint()
         return sa.func.min(sa.func.length(column))
 
     @column_aggregate_partial(engine=SparkDFExecutionEngine)
-    def _spark(cls, column, **kwargs):
+    def _spark(cls, column, **kwargs: dict) -> int:
         return F.min(F.length(column))
