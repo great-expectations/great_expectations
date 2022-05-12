215a902003d80daaf00308fb4c2e5d510b5e6b6b
Chetan Kini <chetan@superconductive.com>
2022-05-12 14:18:42 -0400

    feat: create new metrics

---
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_values_length_min.py
@@ -0,0 +1,27 @@
+from great_expectations.execution_engine import (
+    PandasExecutionEngine,
+    SparkDFExecutionEngine,
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.expectations.metrics.column_aggregate_metric_provider import (
+    ColumnAggregateMetricProvider,
+    column_aggregate_partial,
+    column_aggregate_value,
+)
+from great_expectations.expectations.metrics.import_manager import F, sa
+
+
+class ColumnValuesLengthMin(ColumnAggregateMetricProvider):
+    metric_name = "column_values.length.min"
+
+    @column_aggregate_value(engine=PandasExecutionEngine)
+    def _pandas(cls, column, **kwargs):
+        return column.map(len).min()
+
+    @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(cls, column, **kwargs):
+        return sa.func.min(sa.func.length(column))
+
+    @column_aggregate_partial(engine=SparkDFExecutionEngine)
+    def _spark(cls, column, **kwargs):
+        return F.min(F.length(column))
