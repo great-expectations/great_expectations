8a30a3ca5c99d199b321826e37ab0eec4fa6a087
Shinnnyshinshin <will@superconductive.com>
2022-05-26 14:28:07 -0700

    Update data_context.py

--- great_expectations/data_context/data_context/data_context.py
+++ great_expectations/data_context/data_context/data_context.py
@@ -9,7 +9,6 @@ from ruamel.yaml import YAML, YAMLError
 from ruamel.yaml.constructor import DuplicateKeyError
 
 import great_expectations.exceptions as ge_exceptions
-from great_expectations.core.config_peer import ConfigPeer
 from great_expectations.data_context.data_context.base_data_context import (
     BaseDataContext,
 )
