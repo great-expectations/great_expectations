37358cebf62aa403e7162dd6dfdf4d38a7651cc7
Shinnnyshinshin <will@superconductive.com>
2022-06-02 16:41:22 -0700

    removed print statement

--- great_expectations/data_context/data_context/base_data_context.py
+++ great_expectations/data_context/data_context/base_data_context.py
@@ -804,7 +804,6 @@ class BaseDataContext(
         Returns:
             None
         """
-        print("hi Chetan")
         config_variables = self._load_config_variables_file()
         value = self.escape_all_config_variables(
             value,
