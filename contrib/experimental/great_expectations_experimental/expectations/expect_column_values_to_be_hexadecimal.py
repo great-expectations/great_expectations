bac32a482510a8391456ad6e8dcb1e150dfcce94
Andrew Xiao <andrewxiao@Andrews-MacBook-Air.local>
2022-06-03 16:29:53 -0400

    fix linting error from flake

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_hexadecimal.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_hexadecimal.py
@@ -16,11 +16,14 @@ from great_expectations.expectations.metrics import (
 
 # This class defines a Metric to support your Expectation.
 # For most ColumnMapExpectations, the main business logic for calculation will live in this class.
-class ColumnValuesToBeHexadecimal(ColumnMapMetricProvider):
 
+
+class ColumnValuesToBeHexadecimal(ColumnMapMetricProvider):
     # This is the id string that will be used to reference your metric.
     condition_metric_name = "column_values.is_hexadecimal"
+
     filter_column_isnull = False
+
     # This method implements the core logic for the PandasExecutionEngine
     @column_condition_partial(engine=PandasExecutionEngine)
     def _pandas(cls, column, **kwargs):
