3616e8ff797668bf004488d883aba1f5d40c77ba
Alex Sherstinsky <alex@superconductive.com>
2022-06-02 23:50:51 -0700

    threading/pickling

--- great_expectations/core/batch.py
+++ great_expectations/core/batch.py
@@ -1,4 +1,3 @@
-import copy
 import datetime
 import json
 import logging
@@ -873,7 +872,9 @@ def get_batch_request_from_acceptable_arguments(
 def standardize_batch_request_display_ordering(
     batch_request: Dict[str, Union[str, int, Dict[str, Any]]]
 ) -> Dict[str, Union[str, Dict[str, Any]]]:
-    batch_request_as_dict: Union[str, Dict[str, Any]] = copy.deepcopy(batch_request)
+    batch_request_as_dict: Union[str, Dict[str, Any]] = safe_deep_copy(
+        data=batch_request
+    )
     datasource_name: str = batch_request_as_dict["datasource_name"]
     data_connector_name: str = batch_request_as_dict["data_connector_name"]
     data_asset_name: str = batch_request_as_dict["data_asset_name"]
