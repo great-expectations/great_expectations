33a65ab7118b403354768a27eb2900f9b7b7b792
Nathan Farmer <nathan@superconductive.com>
2022-06-08 11:59:27 -0400

    WIP

--- great_expectations/rule_based_profiler/types/data_assistant_result/onboarding_data_assistant_result.py
+++ great_expectations/rule_based_profiler/types/data_assistant_result/onboarding_data_assistant_result.py
@@ -1,3 +1,4 @@
+from great_expectations.rule_based_profiler.types.altair import AltairDataTypes
 from great_expectations.rule_based_profiler.types.data_assistant_result import (
     DataAssistantResult,
 )
@@ -10,9 +11,17 @@ class OnboardingDataAssistantResult(DataAssistantResult):
         "expect_column_unique_value_count_to_be_between": "column.distinct_values.count",
         "expect_column_min_to_be_between": "column.min",
         "expect_column_max_to_be_between": "column.max",
-        # "expect_column_values_to_be_between": "column_values.between",
         "expect_column_mean_to_be_between": "column.mean",
         "expect_column_median_to_be_between": "column.median",
-        # "expect_column_quantile_values_to_be_between": "column.quantile_values",
         "expect_column_stdev_to_be_between": "column.standard_deviation",
+    }
+
+    METRIC_TYPES = {
+        "table.row_count": AltairDataTypes.QUANTITATIVE,
+        "column.distinct_values.count": AltairDataTypes.QUANTITATIVE,
+        "column.min": AltairDataTypes.QUANTITATIVE,
+        "column.max": AltairDataTypes.QUANTITATIVE,
+        "column.mean": AltairDataTypes.QUANTITATIVE,
+        "column.median": AltairDataTypes.QUANTITATIVE,
+        "column.standard_deviation": AltairDataTypes.QUANTITATIVE,
     }
