95f4892b79ace55746eeb6566ec9e66cb93ef3ea
Nathan Farmer <nathan@superconductive.com>
2022-06-09 09:33:03 -0400

    Increase precision

--- great_expectations/rule_based_profiler/data_assistant/onboarding_data_assistant.py
+++ great_expectations/rule_based_profiler/data_assistant/onboarding_data_assistant.py
@@ -590,7 +590,7 @@ class OnboardingDataAssistant(DataAssistant):
                 "lower_bound": None,
                 "upper_bound": None,
             },
-            "round_decimals": 1,
+            "round_decimals": 4,
         }
         parameter_builders: List[ParameterBuilder] = [
             column_mean_metric_multi_batch_parameter_builder_for_metrics,
