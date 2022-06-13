c5bf76447fce467c7e7db13df1ed59070f9076ba
Ruben Orduz <rubenoz@gmail.com>
2022-06-13 15:52:30 -0400

    copy-paste fix

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_github_username.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_github_username.py
@@ -15,7 +15,7 @@ BASE_API_URL = "https://api.github.com/users/"
 
 # This class defines a Metric to support your Expectation.
 # For most ColumnMapExpectations, the main business logic for calculation will live in this class.
-class ColumnValuesToBeValidHexColor(ColumnMapMetricProvider):
+class ColumnValuesToBeValidGithubUser(ColumnMapMetricProvider):
 
     # This is the id string that will be used to reference your metric.
     condition_metric_name = "column_values.valid_github_username"
