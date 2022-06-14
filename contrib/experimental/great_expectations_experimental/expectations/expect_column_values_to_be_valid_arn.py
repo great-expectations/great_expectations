b7f57aad5b98a8e2dd9bf078cfa5a0bc6f4309a4
Ruben Orduz <rubenoz@gmail.com>
2022-06-14 09:14:24 -0400

    Using RegexBasedColumn Expectation

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_arn.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_valid_arn.py
@@ -1,126 +1,100 @@
-import re
-from typing import Optional
+"""
+This is a template for creating custom RegexBasedColumnMapExpectations.
+For detailed instructions on how to use it, please see:
+    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_regex_based_column_map_expectations
+"""
+
+from typing import Dict, Optional
 
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
-from great_expectations.execution_engine import PandasExecutionEngine
-from great_expectations.expectations.expectation import ColumnMapExpectation
-from great_expectations.expectations.metrics import (
-    ColumnMapMetricProvider,
-    column_condition_partial,
+from great_expectations.exceptions.exceptions import (
+    InvalidExpectationConfigurationError,
 )
+from great_expectations.expectations.regex_based_column_map_expectation import (
+    RegexBasedColumnMapExpectation,
+    RegexColumnMapMetricProvider,
+)
+class ExpectColumnValuesToBeValidArn(RegexBasedColumnMapExpectation):
+    """Expect values in this column to be a valid geohash."""
 
-ARN_REGEX = r"^arn:(?P<Partition>[^:\n]*):(?P<Service>[^:\n]*):(?P<Region>[^:\n]*):(?P<AccountID>[^:\n]*):(?P<Ignore>(?P<ResourceType>[^:\/\n]*)[:\/])?(?P<Resource>.*)$"
-
-
-# This class defines a Metric to support your Expectation.
-# For most ColumnMapExpectations, the main business logic for calculation will live in this class.
-class ColumnValuesToBeValidArn(ColumnMapMetricProvider):
-
-    # This is the id string that will be used to reference your metric.
-    condition_metric_name = "column_values.valid_arn"
-
-    # This method implements the core logic for the PandasExecutionEngine
-    @column_condition_partial(engine=PandasExecutionEngine)
-    def _pandas(cls, column, **kwargs):
-        def matches_arn_regex(x):
-            if re.match(ARN_REGEX, str(x)):
-                return True
-            return False
-
-        return column.apply(lambda x: matches_arn_regex(x) if x else False)
-
-    # This method defines the business logic for evaluating your metric when using a SqlAlchemyExecutionEngine
-    # @column_condition_partial(engine=SqlAlchemyExecutionEngine)
-    # def _sqlalchemy(cls, column, _dialect, **kwargs):
-    #     raise NotImplementedError
-
-    # This method defines the business logic for evaluating your metric when using a SparkDFExecutionEngine
-    # @column_condition_partial(engine=SparkDFExecutionEngine)
-    # def _spark(cls, column, **kwargs):
-    #     raise NotImplementedError
-
-
-# This class defines the Expectation itself
-class ExpectColumnValuesToBeValidArn(ColumnMapExpectation):
-    """This Expectation validates data as conforming to the valid hexadecimal color code format."""
+    # These values will be used to configure the metric created by your expectation
+    regex_camel_name = "AmazonResourceName"
+    regex = "^arn:(?P<Partition>[^:\n]*):(?P<Service>[^:\n]*):(?P<Region>[^:\n]*):(?P<AccountID>[^:\n]*):(?P<Ignore>(?P<ResourceType>[^:\/\n]*)[:\/])?(?P<Resource>.*)$"
+    semantic_type_name_plural = "arns"
 
     # These examples will be shown in the public gallery.
     # They will also be executed as unit tests for your Expectation.
     examples = [
         {
             "data": {
-                "well_formed_arns": [
+                "valid_arns": [
                     "arn:aws:s3:::my-bucket/my-object",
                     "arn:partition:service:region:account-id:resource",
                 ],
-                "malformed_arns": [
-                    "arn:aws:s3:my-bucket/my-object",
-                    "arn:partition:us-east-1/west-1/account-id:resource",
+                "invalid_alphanumeric": ["apz8", "bubba:arn:123",],  # "a" is an invalid geohash char
+                "invalid_arn": [
+                    "arn:aws:::::::my-bucket/my-object",
+                    "arn::::",
                 ],
+                "empty": ["", None],
             },
             "tests": [
                 {
                     "title": "basic_positive_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "well_formed_arns"},
-                    "out": {"success": True},
+                    "in": {"column": "valid_arns"},
+                    "out": {
+                        "success": True,
+                    },
                 },
                 {
                     "title": "basic_negative_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "malformed_arns"},
-                    "out": {"success": False},
+                    "in": {"column": "invalid_alphanumeric", "mostly": 1},
+                    "out": {
+                        "success": False,
+                    },
+                },
+                {
+                    "title": "invalid_non_alphanumeric",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "invalid_arn", "mostly": 1},
+                    "out": {
+                        "success": False,
+                    },
+                },
+                {
+                    "title": "empty",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {"column": "empty", "mostly": 1},
+                    "out": {
+                        "success": False,
+                    },
                 },
             ],
         }
     ]
 
-    # This is the id string of the Metric used by this Expectation.
-    # For most Expectations, it will be the same as the `condition_metric_name` defined in your Metric class above.
-    map_metric = "column_values.valid_arn"
-
-    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("mostly",)
-
-    # This dictionary contains default values for any parameters that should have default values
-    default_kwarg_values = {}
-
-    def validate_configuration(
-        self, configuration: Optional[ExpectationConfiguration]
-    ) -> None:
-        """
-        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
-        necessary configuration arguments have been provided for the validation of the expectation.
-        Args:
-            configuration (OPTIONAL[ExpectationConfiguration]): \
-                An optional Expectation Configuration entry that will be used to configure the expectation
-        Returns:
-            None. Raises InvalidExpectationConfigurationError if the config is not validated successfully
-        """
-
-        super().validate_configuration(configuration)
-        if configuration is None:
-            configuration = self.configuration
-
-        # # Check other things in configuration.kwargs and raise Exceptions if needed
-        # try:
-        #     assert (
-        #         ...
-        #     ), "message"
-        #     assert (
-        #         ...
-        #     ), "message"
-        # except AssertionError as e:
-        #     raise InvalidExpectationConfigurationError(str(e))
+    # Here your regex is used to create a custom metric for this expectation
+    map_metric = RegexBasedColumnMapExpectation.register_metric(
+        regex_camel_name=regex_camel_name,
+        regex_=regex,
+    )
 
     # This object contains metadata for display in the public Gallery
     library_metadata = {
         "maturity": "experimental",
-        "tags": ["experimental", "expectation", "validation"],
-        "contributors": [
-            "@rdodev",
+        "tags": [
+            "amazon",
+            "arn",
+            "expectation",
+        ],  # Tags for this Expectation in the Gallery
+        "contributors": [  # Github handles for all contributors to this Expectation.
+            "@rdodev",  # Don't forget to add your github handle here!
         ],
     }
 
