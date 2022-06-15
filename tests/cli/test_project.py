9045cdf669d140d8f49f6aff75638958b1f2ecc0
Shinnnyshinshin <will@superconductive.com>
2022-06-14 18:00:56 -0700

    remove redundant fixture

--- tests/cli/test_project.py
+++ tests/cli/test_project.py
@@ -42,33 +42,6 @@ def titanic_data_context_clean_usage_stats_enabled(
 
 
 @pytest.fixture
-def titanic_data_context_v2_config_to_upgrade_usage_stats_enabled(
-    tmp_path_factory, monkeypatch
-) -> DataContext:
-    # Re-enable GE_USAGE_STATS
-    monkeypatch.delenv("GE_USAGE_STATS")
-
-    project_path = str(tmp_path_factory.mktemp("titanic_data_context"))
-    context_path = os.path.join(project_path, "great_expectations")
-    os.makedirs(os.path.join(context_path, "expectations"), exist_ok=True)
-    os.makedirs(os.path.join(context_path, "checkpoints"), exist_ok=True)
-    data_path = os.path.join(context_path, "..", "data")
-    os.makedirs(os.path.join(data_path), exist_ok=True)
-    # this is a v2 great_expectations.yml
-    titanic_yml_path = file_relative_path(
-        __file__, "../test_fixtures/great_expectations_titanic.yml"
-    )
-    shutil.copy(
-        titanic_yml_path, str(os.path.join(context_path, "great_expectations.yml"))
-    )
-    titanic_csv_path = file_relative_path(__file__, "../test_sets/Titanic.csv")
-    shutil.copy(
-        titanic_csv_path, str(os.path.join(context_path, "..", "data", "Titanic.csv"))
-    )
-    return ge.data_context.DataContext(context_path)
-
-
-@pytest.fixture
 def titanic_data_context_v2_datasources_and_validation_operators_usage_stats_enabled(
     tmp_path_factory, monkeypatch
 ) -> DataContext:
