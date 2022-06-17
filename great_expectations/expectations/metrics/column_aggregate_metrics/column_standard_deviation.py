2d92642226dcfe117fc976d6dc0b85d29f3700f0
Alex Sherstinsky <alex@superconductive.com>
2022-06-17 13:49:33 -0700

    Enable the "column.standard_deviation" metric to work properly for SQLLite database.

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_standard_deviation.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_standard_deviation.py
@@ -1,6 +1,9 @@
 import logging
+from typing import Optional
 
+from great_expectations.core import ExpectationConfiguration
 from great_expectations.execution_engine import (
+    ExecutionEngine,
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
@@ -11,6 +14,7 @@ from great_expectations.expectations.metrics.column_aggregate_metric_provider im
     column_aggregate_value,
 )
 from great_expectations.expectations.metrics.import_manager import F, sa
+from great_expectations.validator.metric_configuration import MetricConfiguration
 
 logger = logging.getLogger(__name__)
 
@@ -34,15 +38,56 @@ class ColumnStandardDeviation(ColumnAggregateMetricProvider):
         return column.std()
 
     @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(cls, column, _dialect, **kwargs):
-        """SqlAlchemy Standard Deviation implementation"""
+    def _sqlalchemy(cls, column, _dialect, _metrics, **kwargs):
+        """SQLAlchemy Standard Deviation implementation (to be precise, Standard Error is computed and returned)"""
         if _dialect.name.lower() == "mssql":
             standard_deviation = sa.func.stdev(column)
+        elif _dialect.name.lower() == "sqlite":
+            mean = _metrics["column.mean"]
+            nonnull_row_count = _metrics["column_values.null.unexpected_count"]
+            standard_deviation = sa.func.sqrt(
+                sa.func.sum((1.0 * column - mean) * (1.0 * column - mean))
+                / ((1.0 * nonnull_row_count) - 1.0)
+            )
         else:
             standard_deviation = sa.func.stddev_samp(column)
+
         return standard_deviation
 
     @column_aggregate_partial(engine=SparkDFExecutionEngine)
     def _spark(cls, column, **kwargs):
         """Spark Standard Deviation implementation"""
         return F.stddev_samp(column)
+
+    @classmethod
+    def _get_evaluation_dependencies(
+        cls,
+        metric: MetricConfiguration,
+        configuration: Optional[ExpectationConfiguration] = None,
+        execution_engine: Optional[ExecutionEngine] = None,
+        runtime_configuration: Optional[dict] = None,
+    ):
+        """Returns a dictionary of given metric names and their corresponding configuration, specifying the metric
+        types and their respective domains"""
+        dependencies: dict = super()._get_evaluation_dependencies(
+            metric=metric,
+            configuration=configuration,
+            execution_engine=execution_engine,
+            runtime_configuration=runtime_configuration,
+        )
+
+        if isinstance(execution_engine, SqlAlchemyExecutionEngine):
+            dependencies["column.mean"] = MetricConfiguration(
+                metric_name="column.mean",
+                metric_domain_kwargs=metric.metric_domain_kwargs,
+                metric_value_kwargs=None,
+                metric_dependencies=None,
+            )
+            dependencies["column_values.null.unexpected_count"] = MetricConfiguration(
+                metric_name="column_values.null.unexpected_count",
+                metric_domain_kwargs=metric.metric_domain_kwargs,
+                metric_value_kwargs=None,
+                metric_dependencies=None,
+            )
+
+        return dependencies
--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -1496,7 +1496,6 @@ def candidate_test_is_on_temporary_notimplemented_list_cfe(context, expectation_
             "expect_column_values_to_be_dateutil_parseable",
             "expect_column_values_to_be_json_parseable",
             "expect_column_values_to_match_json_schema",
-            "expect_column_stdev_to_be_between",
             "expect_multicolumn_values_to_be_unique",
             "expect_column_pair_cramers_phi_value_to_be_less_than",
             "expect_column_bootstrapped_ks_test_p_value_to_be_greater_than",
