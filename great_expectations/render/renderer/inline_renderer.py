4d132e42256c7e4315ea9b78700063d374229c4c
Nathan Farmer <nathan@superconductive.com>
2022-06-24 12:28:34 -0400

    Revert "Circular import workarounds"
    This reverts commit 859854b98571dc9adce09f20c80981fe98518a68.

--- great_expectations/render/renderer/inline_renderer.py
+++ great_expectations/render/renderer/inline_renderer.py
@@ -1,5 +1,9 @@
 from typing import Callable, List, Optional, Union
 
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.core.expectation_validation_result import (
+    ExpectationValidationResult,
+)
 from great_expectations.expectations.registry import (
     get_renderer_impl,
     get_renderer_names_with_renderer_prefix,
@@ -11,11 +15,14 @@ from great_expectations.render.types import RenderedContent
 class InlineRenderer(Renderer):
     def __init__(
         self,
-        render_object: Union["ExpectationConfiguration", "ExpectationValidationResult"],
+        render_object: Union[ExpectationConfiguration, ExpectationValidationResult],
     ) -> None:
         super().__init__()
 
         self._render_object = render_object
+
+    def render(self) -> List[RenderedContent]:
+        return self.get_atomic_rendered_content_for_object()
 
     def get_atomic_rendered_content_for_object(self) -> List[RenderedContent]:
         """Gets RenderedAtomicContent for a given ExpectationConfiguration or ExpectationValidationResult.
@@ -24,31 +31,24 @@ class InlineRenderer(Renderer):
             A list of RenderedAtomicContent objects for a given ExpectationConfiguration or ExpectationValidationResult.
         """
         render_object: Union[
-            "ExpectationConfiguration", "ExpectationValidationResult"
+            ExpectationConfiguration, ExpectationValidationResult
         ] = self._render_object
-
-        # This workaround was required to avoid circular imports
-        render_object_type: str
-        if hasattr(render_object, "expectation_type"):
-            render_object_type = "ExpectationConfiguration"
-        elif hasattr(render_object, "expectation_config"):
-            render_object_type = "ExpectationValidationResult"
-        else:
-            raise ValueError(
-                f"object must be of type ExpectationConfiguration or ExpectationValidationResult, but an object of type {type(render_object)} was passed"
-            )
 
         expectation_type: str
         atomic_renderer_prefix: str
         legacy_renderer_prefix: str
-        if render_object_type == "ExpectationConfiguration":
-            expectation_type = render_object.expectation_type
+        if isinstance(object, ExpectationConfiguration):
+            expectation_type = object.expectation_type
             atomic_renderer_prefix = "atomic.prescriptive"
             legacy_renderer_prefix = "renderer.prescriptive"
-        else:
-            expectation_type = render_object.expectation_config.expectation_type
+        elif isinstance(object, ExpectationValidationResult):
+            expectation_type = object.expectation_config.expectation_type
             atomic_renderer_prefix = "atomic.diagnostic"
             legacy_renderer_prefix = "renderer.diagnostic"
+        else:
+            raise ValueError(
+                f"object must be of type ExpectationConfiguration or ExpectationValidationResult, but an object of type {type(render_object)} was passed"
+            )
 
         renderer_names: List[str] = get_renderer_names_with_renderer_prefix(
             object_name=expectation_type,
@@ -64,7 +64,7 @@ class InlineRenderer(Renderer):
         renderer_fn: Callable
         renderer_rendered_content: Optional[
             Union[RenderedContent, List[RenderedContent]]
-        ]
+        ] = None
         rendered_content: List[RenderedContent] = []
         for renderer_name in renderer_names:
             renderer_tuple = get_renderer_impl(
@@ -73,17 +73,16 @@ class InlineRenderer(Renderer):
             if renderer_tuple is not None:
                 # index 0 is expectation class-name and index 1 is implementation of renderer
                 renderer_fn = renderer_tuple[1]
-                if render_object_type == "ExpectationConfiguration":
+                if isinstance(render_object, ExpectationConfiguration):
                     renderer_rendered_content = renderer_fn(configuration=render_object)
-                else:
+                elif isinstance(render_object, ExpectationValidationResult):
                     renderer_rendered_content = renderer_fn(result=render_object)
 
-                if isinstance(renderer_rendered_content, list):
+                if renderer_rendered_content is None:
+                    pass
+                elif isinstance(renderer_rendered_content, list):
                     rendered_content.extend(renderer_rendered_content)
                 else:
                     rendered_content.append(renderer_rendered_content)
 
         return rendered_content
-
-    def render(self) -> List[RenderedContent]:
-        return self.get_atomic_rendered_content_for_object()
