80d98534e2cbbfda63b379761466c6931a40fe71
Shinnnyshinshin <will@superconductive.com>
2022-06-26 23:38:30 -0700

    one additional comment to force linting

--- great_expectations/data_context/data_context/file_data_context.py
+++ great_expectations/data_context/data_context/file_data_context.py
@@ -39,6 +39,7 @@ class FileDataContext(AbstractDataContext):
         self._project_config = self._apply_global_config_overrides(
             config=project_config
         )
+        # we can only have the Abstract logic going after these DataContext-specific parameters are instantiated
         super().__init__(runtime_environment=runtime_environment)
 
     def _init_variables(self) -> FileDataContextVariables:
