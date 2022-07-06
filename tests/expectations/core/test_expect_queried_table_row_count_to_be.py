9e2bfb0296b81badf5280e3ab7b4630a7777a392
Austin Robinson <austin@superconductive.com>
2022-07-06 09:33:28 -0600

    test

--- tests/expectations/core/test_expect_queried_table_row_count_to_be.py
+++ tests/expectations/core/test_expect_queried_table_row_count_to_be.py
@@ -13,43 +13,30 @@ from tests.integration.docusaurus.expectations.creating_custom_expectations.expe
     ExpectQueriedTableRowCountToBe,
 )
 
-sqlite_runtime_batch_request = RuntimeBatchRequest(
-    datasource_name="my_sqlite_db_datasource",
-    data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="titanic",
-    runtime_parameters={"query": "SELECT * FROM titanic LIMIT 100"},
-    batch_identifiers={"default_identifier_name": "test_identifier"},
-    batch_spec_passthrough={"create_temp_table": False},
-)
-
-sqlite_batch_request = BatchRequest(
-    datasource_name="my_sqlite_db_datasource",
-    data_connector_name="default_inferred_data_connector_name",
-    data_asset_name="titanic",
-    batch_spec_passthrough={"create_temp_table": False},
-)
-
 
 @pytest.mark.parametrize(
-    "batch_request,success,value,observed,row_condition",
+    "success,value,observed,row_condition",
     [
-        (sqlite_runtime_batch_request, True, 100, 100, None),
-        (sqlite_batch_request, False, 100, 1313, None),
-        (sqlite_batch_request, False, 100, 96, 'col("Age")<18'),
-        (sqlite_runtime_batch_request, True, 70, 70, 'col("Age")>17'),
+        (False, 100, 1313, None),
+        (False, 100, 96, 'col("Age")<18'),
     ],
 )
-def test_expect_queried_column_value_frequency_to_meet_threshold_sqlite(
-    batch_request,
+def test_expect_queried_column_value_frequency_to_meet_threshold_active_sqlite(
     success,
     value,
     observed,
     row_condition,
     titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled,
 ):
+    sqlite_batch_request = BatchRequest(
+        datasource_name="my_sqlite_db_datasource",
+        data_connector_name="default_inferred_data_connector_name",
+        data_asset_name="titanic",
+        batch_spec_passthrough={"create_temp_table": False},
+    )
     context: DataContext = titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled
 
-    validator: Validator = context.get_validator(batch_request=batch_request)
+    validator: Validator = context.get_validator(batch_request=sqlite_batch_request)
 
     result: ExpectationValidationResult = (
         validator.expect_queried_table_row_count_to_be(
@@ -65,18 +52,50 @@ def test_expect_queried_column_value_frequency_to_meet_threshold_sqlite(
 
 
 @pytest.mark.parametrize(
-    "batch_request,success,query,value,observed,row_condition",
+    "success,value,observed,row_condition",
+    [
+        (True, 100, 100, None),
+        (True, 70, 70, 'col("Age")>17'),
+    ],
+)
+def test_expect_queried_column_value_frequency_to_meet_threshold_runtime_sqlite(
+    success,
+    value,
+    observed,
+    row_condition,
+    titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled,
+):
+    sqlite_runtime_batch_request = RuntimeBatchRequest(
+        datasource_name="my_sqlite_db_datasource",
+        data_connector_name="default_runtime_data_connector_name",
+        data_asset_name="titanic",
+        runtime_parameters={"query": "SELECT * FROM titanic LIMIT 100"},
+        batch_identifiers={"default_identifier_name": "test_identifier"},
+        batch_spec_passthrough={"create_temp_table": False},
+    )
+    context: DataContext = titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled
+
+    validator: Validator = context.get_validator(
+        batch_request=sqlite_runtime_batch_request
+    )
+
+    result: ExpectationValidationResult = (
+        validator.expect_queried_table_row_count_to_be(
+            value=value,
+            row_condition=row_condition,
+            condition_parser="great_expectations__experimental__",
+        )
+    )
+
+    assert (
+        result["success"] is success and result["result"]["observed_value"] == observed
+    )
+
+
+@pytest.mark.parametrize(
+    "success,query,value,observed,row_condition",
     [
         (
-            sqlite_runtime_batch_request,
-            True,
-            "SELECT COUNT(*) FROM titanic",
-            1313,
-            1313,
-            None,
-        ),
-        (
-            sqlite_batch_request,
             True,
             "SELECT COUNT (*) FROM (SELECT * FROM titanic LIMIT 100)",
             100,
@@ -85,8 +104,7 @@ def test_expect_queried_column_value_frequency_to_meet_threshold_sqlite(
         ),
     ],
 )
-def test_expect_queried_column_value_frequency_to_meet_threshold_override_query_sqlite(
-    batch_request,
+def test_expect_queried_column_value_frequency_to_meet_threshold_active_override_query_sqlite(
     success,
     query,
     value,
@@ -94,9 +112,63 @@ def test_expect_queried_column_value_frequency_to_meet_threshold_override_query_
     row_condition,
     titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled,
 ):
+    sqlite_batch_request = BatchRequest(
+        datasource_name="my_sqlite_db_datasource",
+        data_connector_name="default_inferred_data_connector_name",
+        data_asset_name="titanic",
+        batch_spec_passthrough={"create_temp_table": False},
+    )
     context: DataContext = titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled
 
-    validator: Validator = context.get_validator(batch_request=batch_request)
+    validator: Validator = context.get_validator(batch_request=sqlite_batch_request)
+
+    result: ExpectationValidationResult = (
+        validator.expect_queried_table_row_count_to_be(
+            value=value,
+            query=query,
+            row_condition=row_condition,
+            condition_parser="great_expectations__experimental__",
+        )
+    )
+
+    assert (
+        result["success"] is success and result["result"]["observed_value"] == observed
+    )
+
+
+@pytest.mark.parametrize(
+    "success,query,value,observed,row_condition",
+    [
+        (
+            True,
+            "SELECT COUNT(*) FROM titanic",
+            1313,
+            1313,
+            None,
+        ),
+    ],
+)
+def test_expect_queried_column_value_frequency_to_meet_threshold_runtime_override_query_sqlite(
+    success,
+    query,
+    value,
+    observed,
+    row_condition,
+    titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled,
+):
+    sqlite_runtime_batch_request = RuntimeBatchRequest(
+        datasource_name="my_sqlite_db_datasource",
+        data_connector_name="default_runtime_data_connector_name",
+        data_asset_name="titanic",
+        runtime_parameters={"query": "SELECT * FROM titanic LIMIT 100"},
+        batch_identifiers={"default_identifier_name": "test_identifier"},
+        batch_spec_passthrough={"create_temp_table": False},
+    )
+    context: DataContext = titanic_v013_multi_datasource_multi_execution_engine_data_context_with_checkpoints_v1_with_empty_store_stats_enabled
+
+    validator: Validator = context.get_validator(
+        batch_request=sqlite_runtime_batch_request
+    )
 
     result: ExpectationValidationResult = (
         validator.expect_queried_table_row_count_to_be(
