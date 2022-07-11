f62f1f152ae38a523f438b8fb6911f8210206356
Chetan Kini <chetan@superconductive.com>
2022-07-11 14:08:08 -0400

    chore: more changes!

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -158,12 +158,10 @@ class AbstractDataContext(ABC):
         self._data_context_id = self._construct_data_context_id()
 
         # Override the project_config data_context_id if an expectations_store was already set up
-        self.variables.config.anonymous_usage_statistics.data_context_id = (
+        self._variables.config.anonymous_usage_statistics.data_context_id = (
             self._data_context_id
         )
-        self._initialize_usage_statistics(
-            self.project_config_with_variables_substituted.anonymous_usage_statistics
-        )
+        self._initialize_usage_statistics(self.variables.anonymous_usage_statistics)
 
         # Store cached datasources but don't init them
         self._cached_datasources = {}
