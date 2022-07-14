3e2c8d8fa9cfde0e3b824ea27d75c4593ab0006a
Chetan Kini <chetan@superconductive.com>
2022-07-14 18:45:41 -0400

    chore: update linter thresholds

--- great_expectations/data_context/store/data_context_store.py
+++ great_expectations/data_context/store/data_context_store.py
@@ -22,6 +22,19 @@ class DataContextStore(ConfigurationStore):
     def serialize(
         self, key: Tuple[str, ...], value: DataContextConfig
     ) -> Union[dict, str]:
+        """
+        Please see `ConfigurationStore.serialize` for more information.
+
+        Note that GE Cloud utilizes a subset of the config; as such, an explicit
+        step to remove unnecessary keys is a required part of the serialization process.
+
+        Args:
+            key:
+            value: DataContextConfig to serialize utilizing the configured StoreBackend.
+
+        Returns:
+            Either a string or dictionary representation of the serialized config.
+        """
         payload: Union[str, dict] = super().serialize(key=key, value=value)
 
         # Cloud requires a subset of the DataContextConfig
