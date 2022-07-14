c1f960b48facb60476e47d16b60064bee58073c6
Chetan Kini <chetan@superconductive.com>
2022-07-14 17:20:44 -0400

    refactor: cleanup subset logic

--- great_expectations/data_context/store/data_context_store.py
+++ great_expectations/data_context/store/data_context_store.py
@@ -1,13 +1,10 @@
-from typing import Tuple, Union
+from typing import Set, Tuple, Union
 
 from great_expectations.data_context.data_context_variables import (
     DataContextVariableSchema,
 )
 from great_expectations.data_context.store.configuration_store import ConfigurationStore
-from great_expectations.data_context.types.base import (
-    DataContextConfig,
-    DataContextConfigSchema,
-)
+from great_expectations.data_context.types.base import DataContextConfig
 
 
 class DataContextStore(ConfigurationStore):
@@ -17,23 +14,20 @@ class DataContextStore(ConfigurationStore):
 
     _configuration_class = DataContextConfig
 
+    ge_cloud_exclude_field_names: Set[str] = {
+        DataContextVariableSchema.DATASOURCES,
+        DataContextVariableSchema.ANONYMOUS_USAGE_STATISTICS,
+    }
+
     def serialize(
         self, key: Tuple[str, ...], value: DataContextConfig
     ) -> Union[dict, str]:
+        payload: Union[str, dict] = super().serialize(key=key, value=value)
+
+        # Cloud requires a subset of the DataContextConfig
         if self.ge_cloud_mode:
-            return self._serialize_variables(key=key, value=value)
-        return value.to_yaml_str()
-
-    def _serialize_variables(
-        self, key: Tuple[str, ...], value: DataContextConfig
-    ) -> dict:
-        config_schema: DataContextConfigSchema = value.get_schema_class()()
-        payload: dict = config_schema.dump(value)
-
-        for attr in (
-            DataContextVariableSchema.DATASOURCES,
-            DataContextVariableSchema.ANONYMOUS_USAGE_STATISTICS,
-        ):
-            payload.pop(attr)
+            assert isinstance(payload, dict)
+            for attr in self.ge_cloud_exclude_field_names:
+                payload.pop(attr)
 
         return payload
