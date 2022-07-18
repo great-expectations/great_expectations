fe7d7c08931dc74c5fbe9a082e7738a01b776313
Gabriel Gore <gabriel59kg@gmail.com>
2022-07-18 14:49:30 -0400

    add --help page descriptions for flags

--- tasks.py
+++ tasks.py
@@ -13,9 +13,18 @@ import invoke
 from scripts import check_type_hint_coverage
 
 
-@invoke.task
+_CHECK_HELP_DESC = "Only checks for needed changes without writing back. Exit with error code if changes needed."
+_EXCLUDE_HELP_DESC = "Exclude files or directories"
+
+
+@invoke.task(
+    help={
+        "check": _CHECK_HELP_DESC,
+        "exclude": _EXCLUDE_HELP_DESC,
+    }
+)
 def sort(ctx, path=".", check=False, exclude=None):
-    """Sort module imports"""
+    """Sort module imports."""
     cmds = ["isort", path, "--profile", "black"]
     if check:
         cmds.append("--check-only")
@@ -24,9 +33,17 @@ def sort(ctx, path=".", check=False, exclude=None):
     ctx.run(" ".join(cmds))
 
 
-@invoke.task
+@invoke.task(
+    help={
+        "check": _CHECK_HELP_DESC,
+        "exclude": _EXCLUDE_HELP_DESC,
+        "sort": "Disable import sorting. Runs by default.",
+    }
+)
 def fmt(ctx, path=".", sort_=True, check=False, exclude=None):
-    """Run code formatter"""
+    """
+    Run code formatter.
+    """
     if sort_:
         sort(ctx, path, check=check, exclude=exclude)
 
@@ -44,17 +61,23 @@ def lint(ctx, path="great_expectations/core"):
     ctx.run(" ".join(["flake8", path]))
 
 
-@invoke.task
-def hooks(ctx, changes_only=False, diff=False):
-    """Run pre-commit hooks"""
+@invoke.task(
+    help={
+        "all_files": "Run hooks against all files, not just the current changes.",
+        "diff": "Show the diff of changes on hook failure.",
+    }
+)
+def hooks(ctx, all_files=False, diff=False):
+    """Run all pre-commit hooks."""
     cmds = ["pre-commit", "run"]
     if diff:
         cmds.append("--show-diff-on-failure")
-    # used in CI - runs faster and only checks files that have changed
-    if changes_only:
-        cmds.extend(["--from-ref", "origin/HEAD", "--to-ref", "HEAD"])
-    else:
+    if all_files:
         cmds.extend(["--all-files"])
+    else:
+        # used in CI - runs faster and only checks files that have changed
+        cmds.extend(["--from-ref", "origin/HEAD", "--to-ref", "HEAD"])
+
     ctx.run(" ".join(cmds))
 
 
