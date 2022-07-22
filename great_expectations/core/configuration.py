c91f5077ffced896a5e625a2b525fd5a42da4414
Anthony Burdi <anthony@superconductive.com>
2022-07-22 12:34:27 -0400

    Fix type hint

--- great_expectations/core/configuration.py
+++ great_expectations/core/configuration.py
@@ -1,5 +1,6 @@
 """Contains general abstract or base classes used across configuration objects."""
 from abc import ABC
+from typing import Optional
 
 from great_expectations.types import SerializableDictDot
 
@@ -7,7 +8,7 @@ from great_expectations.types import SerializableDictDot
 class AbstractConfig(ABC, SerializableDictDot):
     """Abstract base class for Config objects. Sets the fields that must be included on a Config."""
 
-    def __init__(self, id_: str = None, name: str = None) -> None:
+    def __init__(self, id_: Optional[str] = None, name: Optional[str] = None) -> None:
         # Note: name and id are optional currently to avoid updating all documentation within
         # the scope of this work.
         if id_ is not None:
