59b6928c08219d9fae461a55b6ad8bb1b869b7f9
Tanner Beam <tanner@superconductive.com>
2022-07-25 14:23:33 -0700

    pull dev changes; install mypy and run linter/type checker

--- tests/integration/usage_statistics/usage_stats_utils.py
+++ tests/integration/usage_statistics/usage_stats_utils.py
@@ -1,26 +1,31 @@
-from tests.integration.usage_statistics.test_usage_statistics_messages import valid_usage_statistics_messages
+from typing import List
+
 import requests
 
-
-def get_usage_stats_example_events():
-
-	msg = valid_usage_statistics_messages
-
-	# remove top-level items from the messages
-	keys = [k for k in msg.keys()]
-	msg_list = []
-	for key in keys:
-		for item in msg[key]:
-			msg_list.append(item)
-
-	return msg_list
+from tests.integration.usage_statistics.test_usage_statistics_messages import (
+    valid_usage_statistics_messages,
+)
 
 
-def get_gx_version():
+def get_usage_stats_example_events() -> List:
 
-	git_raw = "https://raw.githubusercontent.com"
-	git_repo = "great-expectations"
-	git_file = "great_expectations/develop/great_expectations/deployment_version"
-	path = f"{git_raw}/{git_repo}/{git_file}"
+    msg = valid_usage_statistics_messages
 
-	return requests.get(path).text.replace(".", "").replace("\n", "")
+    # remove top-level items from the messages
+    keys = [k for k in msg.keys()]
+    msg_list = []
+    for key in keys:
+        for item in msg[key]:
+            msg_list.append(item)
+
+    return msg_list
+
+
+def get_gx_version() -> str:
+
+    git_raw = "https://raw.githubusercontent.com"
+    git_repo = "great-expectations"
+    git_file = "great_expectations/develop/great_expectations/deployment_version"
+    path = f"{git_raw}/{git_repo}/{git_file}"
+
+    return requests.get(path).text.replace(".", "").replace("\n", "")
