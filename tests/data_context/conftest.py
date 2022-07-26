461109d91f097bb169d5d888664008c650ac7c85
Anthony Burdi <anthony@superconductive.com>
2022-07-26 13:11:21 -0400

    Remove unused fixture

--- tests/data_context/conftest.py
+++ tests/data_context/conftest.py
@@ -632,30 +632,3 @@ def datasource_config() -> DatasourceConfig:
             }
         },
     )
-
-
-@pytest.fixture
-def datasource_config_with_id() -> DatasourceConfig:
-    return DatasourceConfig(
-        class_name="Datasource",
-        execution_engine={
-            "class_name": "PandasExecutionEngine",
-            "module_name": "great_expectations.execution_engine",
-        },
-        id="foobarbaz",
-        data_connectors={
-            "tripdata_monthly_configured": {
-                "class_name": "ConfiguredAssetFilesystemDataConnector",
-                "module_name": "great_expectations.datasource.data_connector",
-                "base_directory": "/path/to/trip_data",
-                "assets": {
-                    "yellow": {
-                        "class_name": "Asset",
-                        "module_name": "great_expectations.datasource.data_connector.asset",
-                        "pattern": r"yellow_tripdata_(\d{4})-(\d{2})\.csv$",
-                        "group_names": ["year", "month"],
-                    }
-                },
-            }
-        },
-    )
