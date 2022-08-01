06a0a0fd31c0b5d819f272cd74317976bfbc25bb
Chetan Kini <chetan@superconductive.com>
2022-08-01 14:18:07 -0400

    test: clean up test with fixtures

--- tests/data_context/cloud_data_context/test_checkpoint_crud.py
+++ tests/data_context/cloud_data_context/test_checkpoint_crud.py
@@ -1,33 +1,18 @@
 import copy
-from typing import Callable
+from typing import Callable, Tuple
 from unittest import mock
+
+import pytest
 
 from great_expectations.data_context.data_context.base_data_context import (
     BaseDataContext,
 )
 
 
-def test_base_data_context_in_cloud_mode_add_checkpoint(
-    empty_base_data_context_in_cloud_mode: BaseDataContext,
-    mock_response_factory: Callable,
-):
-    """
-    A BaseDataContext in cloud mode should save to the cloud backed Datasource store when calling add_datasource
-    with save_changes=True and not save when save_changes=False. When saving, it should use the id from the response
-    to create the datasource.
-    """
-    context: BaseDataContext = empty_base_data_context_in_cloud_mode
-
-    # Make sure the fixture has the right configuration
-    assert isinstance(context, BaseDataContext)
-    assert context.ge_cloud_mode
-
-    checkpoint_id = "c83e4299-6188-48c6-83b7-f6dce8ad4ab5"
-    validation_id_1 = "v8764797-c486-4104-a764-1f2bf9630ee1"
-    validation_id_2 = "vd0185a8-11c2-11ed-861d-0242ac120002"
-
+@pytest.fixture
+def checkpoint_config() -> dict:
     checkpoint_config = {
-        "name": "my_missing_batch_request_checkpoint",
+        "name": "my_simple_checkpoint",
         "config_version": 1,
         "class_name": "SimpleCheckpoint",
         "expectation_suite_name": "my_expectation_suite",
@@ -40,11 +25,55 @@ def test_base_data_context_in_cloud_mode_add_checkpoint(
             },
         ],
     }
+    return checkpoint_config
 
-    checkpoint_config_with_ids = copy.deepcopy(checkpoint_config)
-    checkpoint_config_with_ids["id"] = checkpoint_id
-    checkpoint_config_with_ids["validations"][0]["id"] = validation_id_1
-    checkpoint_config_with_ids["validations"][1]["id"] = validation_id_2
+
+@pytest.fixture
+def checkpoint_id() -> str:
+    return "c83e4299-6188-48c6-83b7-f6dce8ad4ab5"
+
+
+@pytest.fixture
+def validation_ids() -> Tuple[str, str]:
+    validation_id_1 = "v8764797-c486-4104-a764-1f2bf9630ee1"
+    validation_id_2 = "vd0185a8-11c2-11ed-861d-0242ac120002"
+    return validation_id_1, validation_id_2
+
+
+@pytest.fixture
+def checkpoint_config_with_ids(
+    checkpoint_config: dict, checkpoint_id: str, validation_ids: Tuple[str, str]
+) -> dict:
+    validation_id_1, validation_id_2 = validation_ids
+
+    updated_checkpoint_config = copy.deepcopy(checkpoint_config)
+    updated_checkpoint_config["id"] = checkpoint_id
+    updated_checkpoint_config["validations"][0]["id"] = validation_id_1
+    updated_checkpoint_config["validations"][1]["id"] = validation_id_2
+
+    return updated_checkpoint_config
+
+
+def test_base_data_context_in_cloud_mode_add_checkpoint(
+    empty_base_data_context_in_cloud_mode: BaseDataContext,
+    mock_response_factory: Callable,
+    checkpoint_id: str,
+    validation_ids: Tuple[str, str],
+    checkpoint_config: dict,
+    checkpoint_config_with_ids: dict,
+) -> None:
+    """
+    A BaseDataContext in cloud mode should save to the cloud backed Datasource store when calling add_datasource
+    with save_changes=True and not save when save_changes=False. When saving, it should use the id from the response
+    to create the datasource.
+    """
+    context: BaseDataContext = empty_base_data_context_in_cloud_mode
+
+    # Make sure the fixture has the right configuration
+    assert isinstance(context, BaseDataContext)
+    assert context.ge_cloud_mode
+
+    validation_id_1, validation_id_2 = validation_ids
 
     def mocked_post_response(*args, **kwargs):
         return mock_response_factory(
@@ -70,7 +99,7 @@ def test_base_data_context_in_cloud_mode_add_checkpoint(
                     },
                 }
             },
-            201,
+            200,
         )
 
     with mock.patch(
@@ -78,12 +107,17 @@ def test_base_data_context_in_cloud_mode_add_checkpoint(
     ) as mock_post, mock.patch(
         "requests.get", autospec=True, side_effect=mocked_get_response
     ) as mock_get:
-        # Call add_datasource with and without the name field included in the datasource config
+
         checkpoint = context.add_checkpoint(**checkpoint_config)
 
         assert mock_post.call_count == 1
         assert mock_get.call_count == 1
 
     assert checkpoint.ge_cloud_id == checkpoint_id
+    assert checkpoint.config.ge_cloud_id == checkpoint_id
+
+    assert checkpoint.config.validations[0]["id"] == validation_id_1
     assert checkpoint.validations[0]["id"] == validation_id_1
+
+    assert checkpoint.config.validations[1]["id"] == validation_id_2
     assert checkpoint.validations[1]["id"] == validation_id_2
