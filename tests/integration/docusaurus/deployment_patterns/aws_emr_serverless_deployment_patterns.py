b219e6740f74e54a09dca84f393ca1f59fbc3e4a
bvolodarskiy <bvolodarskiy@provectus.com>
2022-08-01 21:34:06 +0300

    another one reformating of deployment pattern file

--- tests/integration/docusaurus/deployment_patterns/aws_emr_serverless_deployment_patterns.py
+++ tests/integration/docusaurus/deployment_patterns/aws_emr_serverless_deployment_patterns.py
@@ -18,42 +18,50 @@ if __name__ == "__main__":
     suite_name = "pandas_spark_suite"
     session = boto3.Session()
     s3_client = session.client("s3")
-    response = s3_client.get_object(Bucket="bucket_name",
-                                    Key="bucket_name/great_expectations/great_expectations.yml")
+    response = s3_client.get_object(
+        Bucket="bucket_name",
+        Key="bucket_name/great_expectations/great_expectations.yml",
+    )
     config_file = yaml.safe_load(response["Body"])
     df_spark = spark.read.parquet("s3://bucket_name/data_folder/" + spark_file)
 
-    config = DataContextConfig(config_version=config_file["config_version"],
-                               datasources=config_file["datasources"],
-                               expectations_store_name=config_file["expectations_store_name"],
-                               validations_store_name=config_file["validations_store_name"],
-                               evaluation_parameter_store_name=config_file["evaluation_parameter_store_name"],
-                               plugins_directory="/great_expectations/plugins",
-                               stores=config_file["stores"],
-                               data_docs_sites=config_file["data_docs_sites"],
-                               config_variables_file_path=config_file["config_variables_file_path"],
-                               anonymous_usage_statistics=config_file["anonymous_usage_statistics"],
-                               checkpoint_store_name=config_file["checkpoint_store_name"],
-                               store_backend_defaults=S3StoreBackendDefaults(
-                                   default_bucket_name=config_file["data_docs_sites"]["s3_site"]["store_backend"][
-                                       "bucket"])
-                               )
+    config = DataContextConfig(
+        config_version=config_file["config_version"],
+        datasources=config_file["datasources"],
+        expectations_store_name=config_file["expectations_store_name"],
+        validations_store_name=config_file["validations_store_name"],
+        evaluation_parameter_store_name=config_file["evaluation_parameter_store_name"],
+        plugins_directory="/great_expectations/plugins",
+        stores=config_file["stores"],
+        data_docs_sites=config_file["data_docs_sites"],
+        config_variables_file_path=config_file["config_variables_file_path"],
+        anonymous_usage_statistics=config_file["anonymous_usage_statistics"],
+        checkpoint_store_name=config_file["checkpoint_store_name"],
+        store_backend_defaults=S3StoreBackendDefaults(
+            default_bucket_name=config_file["data_docs_sites"]["s3_site"][
+                "store_backend"
+            ]["bucket"]
+        ),
+    )
 
     context_gx = BaseDataContext(project_config=config)
 
     expectation_suite_name = suite_name
     suite = context_gx.get_expectation_suite(suite_name)
 
-    batch_request = RuntimeBatchRequest(datasource_name="spark_s3",
-                                        data_connector_name="default_inferred_data_connector_name",
-                                        runtime_parameters={"path": "s3a://bucket_name/path_to_file.format"}
-                                        )
+    batch_request = RuntimeBatchRequest(
+        datasource_name="spark_s3",
+        data_connector_name="default_inferred_data_connector_name",
+        runtime_parameters={"path": "s3a://bucket_name/path_to_file.format"},
+    )
     validator = context_gx.get_validator(
         batch_request=batch_request,
         expectation_suite_name=expectation_suite_name,
     )
     print(validator.head())
-    validator.expect_column_values_to_not_be_null(column="passenger_count")  ## add some test
+    validator.expect_column_values_to_not_be_null(
+        column="passenger_count"
+    )  ## add some test
     validator.save_expectation_suite(discard_failed_expectations=False)
 
     my_checkpoint_name = "in_memory_checkpoint"
