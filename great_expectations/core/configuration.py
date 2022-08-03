d3f35000381995d5e847516a332b5d1794472c62
Anthony Burdi <anthony@superconductive.com>
2022-08-03 10:27:54 -0400

    Revert mandatory name until we can fix all callers, leave id

--- great_expectations/core/configuration.py
+++ great_expectations/core/configuration.py
@@ -9,8 +9,8 @@ class AbstractConfig(ABC, SerializableDictDot):
     """Abstract base class for Config objects. Sets the fields that must be included on a Config."""
 
     def __init__(self, id_: Optional[str] = None, name: Optional[str] = None) -> None:
-        # Note: name and id are optional currently to avoid updating all documentation within
-        # the scope of this work.
+        # Note: name is optional currently to avoid updating all documentation and callers
+        # within the scope of this work.
         self.id_ = id_
         if name is not None:
             self.name = name
