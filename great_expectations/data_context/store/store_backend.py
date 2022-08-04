b05edcb262bcb423643a8b9b1d6351cce235653a
Chetan Kini <chetan@superconductive.com>
2022-08-04 14:17:55 -0400

    refactor: move out class

--- great_expectations/data_context/store/store_backend.py
+++ great_expectations/data_context/store/store_backend.py
@@ -5,12 +5,10 @@ from typing import Any, Optional
 
 import pyparsing as pp
 
-from great_expectations.core.data_context_key import DataContextVariableKey
 from great_expectations.data_context.data_context_variables import (
     DataContextVariableSchema,
 )
 from great_expectations.exceptions import InvalidKeyError, StoreBackendError, StoreError
-from great_expectations.util import filter_properties_dict
 
 logger = logging.getLogger(__name__)
 
@@ -216,78 +214,3 @@ class StoreBackend(metaclass=ABCMeta):
     ) -> Any:
         """Build a key specific to the store backend implementation."""
         raise NotImplementedError
-
-
-class InMemoryStoreBackend(StoreBackend):
-    """Uses an in-memory dictionary as a store backend."""
-
-    # noinspection PyUnusedLocal
-    def __init__(
-        self,
-        runtime_environment=None,
-        fixed_length_key=False,
-        suppress_store_backend_id=False,
-        manually_initialize_store_backend_id: str = "",
-        store_name=None,
-    ) -> None:
-        super().__init__(
-            fixed_length_key=fixed_length_key,
-            suppress_store_backend_id=suppress_store_backend_id,
-            manually_initialize_store_backend_id=manually_initialize_store_backend_id,
-            store_name=store_name,
-        )
-        self._store = {}
-        # Initialize with store_backend_id if not part of an HTMLSiteStore
-        if not self._suppress_store_backend_id:
-            _ = self.store_backend_id
-
-        # Gather the call arguments of the present function (include the "module_name" and add the "class_name"), filter
-        # out the Falsy values, and set the instance "_config" variable equal to the resulting dictionary.
-        self._config = {
-            "runtime_environment": runtime_environment,
-            "fixed_length_key": fixed_length_key,
-            "suppress_store_backend_id": suppress_store_backend_id,
-            "manually_initialize_store_backend_id": manually_initialize_store_backend_id,
-            "store_name": store_name,
-            "module_name": self.__class__.__module__,
-            "class_name": self.__class__.__name__,
-        }
-        filter_properties_dict(properties=self._config, clean_falsy=True, inplace=True)
-
-    def _get(self, key):
-        try:
-            return self._store[key]
-        except KeyError as e:
-            raise InvalidKeyError(f"{str(e)}")
-
-    def _set(self, key, value, **kwargs) -> None:
-        self._store[key] = value
-
-    def _move(self, source_key, dest_key, **kwargs) -> None:
-        self._store[dest_key] = self._store[source_key]
-        self._store.pop(source_key)
-
-    def list_keys(self, prefix=()):
-        return [key for key in self._store.keys() if key[: len(prefix)] == prefix]
-
-    def _has_key(self, key):
-        return key in self._store
-
-    def remove_key(self, key) -> None:
-        del self._store[key]
-
-    @property
-    def config(self) -> dict:
-        return self._config
-
-    def build_key(
-        self,
-        resource_type: Optional[DataContextVariableSchema] = None,
-        id_: Optional[str] = None,
-        name: Optional[str] = None,
-    ) -> DataContextVariableKey:
-        """Get the store backend specific implementation of the key. id_ included for super class compatibility."""
-        return DataContextVariableKey(
-            resource_type=resource_type,
-            resource_name=name,
-        )
