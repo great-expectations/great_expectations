4e1409605281c8c1c96178d4dd9484b11944f77b
Austin Robinson <austin@superconductive.com>
2022-08-08 14:00:48 -0600

    import sorting

--- great_expectations/expectations/metrics/multicolumn_map_metrics/__init__.py
+++ great_expectations/expectations/metrics/multicolumn_map_metrics/__init__.py
@@ -1,6 +1,6 @@
 from .compound_columns_unique import CompoundColumnsUnique
-from .multicolumn_sum_equal import MulticolumnSumEqual
 from .multicolumn_sum_between import MulticolumnSumBetween
+from .multicolumn_sum_equal import MulticolumnSumEqual
 from .select_column_values_unique_within_record import (
     SelectColumnValuesUniqueWithinRecord,
 )
