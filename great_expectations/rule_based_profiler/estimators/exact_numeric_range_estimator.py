ca939b5cf63124bff61073a567430b12a3371ec0
Alex Sherstinsky <alex@superconductive.com>
2022-08-10 16:28:05 -0700

    numeric range estimators interface and implementations

---
+++ great_expectations/rule_based_profiler/estimators/exact_numeric_range_estimator.py
@@ -0,0 +1,48 @@
+import logging
+from typing import Dict, Optional
+
+import numpy as np
+
+from great_expectations.rule_based_profiler.domain import Domain
+from great_expectations.rule_based_profiler.estimators.numeric_range_estimator import (
+    NumericRangeEstimator,
+)
+from great_expectations.rule_based_profiler.helpers.util import (
+    build_numeric_range_estimation_result,
+)
+from great_expectations.rule_based_profiler.numeric_range_estimation_result import (
+    NumericRangeEstimationResult,
+)
+from great_expectations.rule_based_profiler.parameter_container import (
+    ParameterContainer,
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class ExactNumericRangeEstimator(NumericRangeEstimator):
+    """
+    Implements deterministic, incorporating entire observed value range, computation.
+    """
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__(
+            name="exact",
+            configuration=None,
+        )
+
+    def _get_numeric_range_estimate(
+        self,
+        metric_values: np.ndarray,
+        domain: Domain,
+        variables: Optional[ParameterContainer] = None,
+        parameters: Optional[Dict[str, ParameterContainer]] = None,
+    ) -> NumericRangeEstimationResult:
+        return build_numeric_range_estimation_result(
+            metric_values=metric_values,
+            min_value=np.amin(a=metric_values),
+            max_value=np.amax(a=metric_values),
+        )
--- great_expectations/rule_based_profiler/estimators/numeric_range_estimator.py
+++ great_expectations/rule_based_profiler/estimators/numeric_range_estimator.py
@@ -21,7 +21,8 @@ logger.setLevel(logging.INFO)
 
 class NumericRangeEstimator(ABC, SerializableDictDot):
     """
-    # TODO: <Alex>ALEX</Alex>
+    Parent class that incorporates the "get_numeric_range_estimate()" interface method, requiring all subclasses to
+    implement the "_get_numeric_range_estimate()" method (for encapsulation reasons, the former calls the latter).
     """
 
     def __init__(
@@ -31,7 +32,10 @@ class NumericRangeEstimator(ABC, SerializableDictDot):
     ) -> None:
         """
         Args:
-        # TODO: <Alex>ALEX</Alex>
+            name: the name of this estimator, which encodes the choice of the estimation algorithm: "oneshot" (one
+            observation), "bootstrap" (default), "exact" (deterministic, incorporating entire observed value range), or
+            "kde" (kernel density estimation).
+            configuration: attributes needed for the estimation algorithm (subject of the inherited class) to operate.
         """
         self._name = name
         self._configuration = configuration
@@ -48,7 +52,6 @@ class NumericRangeEstimator(ABC, SerializableDictDot):
     def configuration(self, value: Optional[Attributes]) -> None:
         self._configuration = value
 
-    @abstractmethod
     def get_numeric_range_estimate(
         self,
         metric_values: np.ndarray,
@@ -57,8 +60,33 @@ class NumericRangeEstimator(ABC, SerializableDictDot):
         parameters: Optional[Dict[str, ParameterContainer]] = None,
     ) -> NumericRangeEstimationResult:
         """
+        Method that invokes implementation of the estimation algorithm that is the subject of the inherited class.
         Args:
-        # TODO: <Alex>ALEX</Alex>
+            metric_values: "numpy.ndarray" of "dtype.float" values with elements corresponding to "Batch" data samples.
+            domain: "Domain" object that is context for execution of this "NumericRangeEstimator" object.
+            variables: attribute name/value pairs
+            parameters: Dictionary of "ParameterContainer" objects corresponding to all "Domain" objects in memory.
+
+        Returns:
+            "NumericRangeEstimationResult" object, containing computed "value_range" and "estimation_histogram" details.
+        """
+        return self._get_numeric_range_estimate(
+            metric_values=metric_values,
+            domain=domain,
+            variables=variables,
+            parameters=parameters,
+        )
+
+    @abstractmethod
+    def _get_numeric_range_estimate(
+        self,
+        metric_values: np.ndarray,
+        domain: Domain,
+        variables: Optional[ParameterContainer] = None,
+        parameters: Optional[Dict[str, ParameterContainer]] = None,
+    ) -> NumericRangeEstimationResult:
+        """
+        Essentials of the estimation algorithm (all subclasses must implement this method).
         """
         pass
 
