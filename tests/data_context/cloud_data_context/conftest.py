3ab62307e54028135063acee5a3b71e113888709
Gabriel Gore <gabriel59kg@gmail.com>
2022-08-11 11:40:29 -0400

    add `.raise_for_status()` method to `MockResponse`

--- tests/data_context/cloud_data_context/conftest.py
+++ tests/data_context/cloud_data_context/conftest.py
@@ -1,15 +1,27 @@
-from typing import Callable
+from typing import Callable, Optional
 
 import pytest
+from requests.exceptions import HTTPError
 
 
 class MockResponse:
-    def __init__(self, json_data: dict, status_code: int) -> None:
+    def __init__(
+        self,
+        json_data: dict,
+        status_code: int,
+        _exc_to_raise: Optional[HTTPError] = None,
+    ) -> None:
         self._json_data = json_data
         self._status_code = status_code
+        self._exc_to_raise: Optional[HTTPError] = _exc_to_raise
 
     def json(self):
         return self._json_data
+
+    def raise_for_status(self):
+        if self._exc_to_raise:
+            raise self._exc_to_raise
+        return None
 
 
 @pytest.fixture
