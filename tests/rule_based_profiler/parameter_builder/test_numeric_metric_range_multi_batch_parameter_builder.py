c83fc6fead3ddac7b38a3728ade42d412c767fc9
Nathan Farmer <nathan@superconductive.com>
2022-08-15 12:44:10 -0400

    Revert test value changes

--- tests/rule_based_profiler/parameter_builder/test_numeric_metric_range_multi_batch_parameter_builder.py
+++ tests/rule_based_profiler/parameter_builder/test_numeric_metric_range_multi_batch_parameter_builder.py
@@ -313,7 +313,7 @@ def test_quantiles_numeric_metric_range_multi_batch_parameter_builder_bobby(
     actual_value_05_lower: float = actual_values_05[0]
     actual_value_05_upper: float = actual_values_05[1]
     expected_value_05_lower: float = -50.5
-    expected_value_05_upper: float = -20.1
+    expected_value_05_upper: float = -21.1
 
     assert actual_value_05_lower == expected_value_05_lower
     assert actual_value_05_upper == expected_value_05_upper
@@ -641,7 +641,7 @@ def test_quantiles_numeric_metric_range_multi_batch_parameter_builder_with_evalu
 
     actual_value_05_lower: float = actual_values_05[0]
     actual_value_05_upper: float = actual_values_05[1]
-    expected_value_05_lower: float = -51.7
+    expected_value_05_lower: float = -50.5
     expected_value_05_upper: float = -21.1
 
     assert actual_value_05_lower == expected_value_05_lower
