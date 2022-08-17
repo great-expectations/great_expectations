b03c2c33823a1c0ab5bd766595dbd1e68af065d9
Chetan Kini <chetan@superconductive.com>
2022-08-17 12:25:17 -0400

    chore: remove identifier creation

--- great_expectations/checkpoint/checkpoint.py
+++ great_expectations/checkpoint/checkpoint.py
@@ -382,10 +382,7 @@ class BaseCheckpoint(ConfigPeer):
             )
             checkpoint_identifier = None
             if self.data_context.ge_cloud_mode:
-                checkpoint_identifier = GeCloudIdentifier(
-                    resource_type=GeCloudRESTResource.CHECKPOINT,
-                    ge_cloud_id=str(self.ge_cloud_id),
-                )
+                checkpoint_identifier = self.ge_cloud_id
 
             operator_run_kwargs = {}
 
