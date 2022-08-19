f19dd6bfee0d8b5b220f7a647309eaae383551d3
Nathan Farmer <nathan@superconductive.com>
2022-08-19 14:13:58 -0400

    Add INCLUDE_RENDERED_CONTENT to DataContextStore

--- great_expectations/data_context/store/data_context_store.py
+++ great_expectations/data_context/store/data_context_store.py
@@ -23,6 +23,7 @@ class DataContextStore(ConfigurationStore):
         DataContextVariableSchema.DATASOURCES,
         DataContextVariableSchema.EVALUATION_PARAMETER_STORE_NAME,
         DataContextVariableSchema.EXPECTATIONS_STORE_NAME,
+        DataContextVariableSchema.INCLUDE_RENDERED_CONTENT,
         DataContextVariableSchema.PROFILER_STORE_NAME,
         DataContextVariableSchema.VALIDATIONS_STORE_NAME,
         DataContextVariableSchema.VALIDATION_OPERATORS,
