0294f00b63e71b345041919042e1e32d65a0f52b
Alex Sherstinsky <alex@superconductive.com>
2022-08-22 15:43:19 -0700

    performance improvement by removing calls to str() in IDDict and by removing calls to copy.deepcopy() in ValidationGraph

--- great_expectations/core/id_dict.py
+++ great_expectations/core/id_dict.py
@@ -9,18 +9,17 @@ class IDDict(dict):
     def to_id(self, id_keys=None, id_ignore_keys=None):
         if id_keys is None:
             id_keys = self.keys()
+
         if id_ignore_keys is None:
             id_ignore_keys = self._id_ignore_keys
+
         id_keys = set(id_keys) - set(id_ignore_keys)
+
         if len(id_keys) == 0:
             return tuple()
-        elif len(id_keys) == 1:
-            key = list(id_keys)[0]
-            return f"{key}={str(self[key])}"
 
-        _id_dict = {k: self[k] for k in id_keys}
         return hashlib.md5(
-            json.dumps(_id_dict, sort_keys=True).encode("utf-8")
+            json.dumps({k: self[k] for k in id_keys}, sort_keys=True).encode("utf-8")
         ).hexdigest()
 
     @staticmethod
