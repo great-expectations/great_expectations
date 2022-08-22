7c659f3664bac066eb183ca76c30faaad4ef6155
Anthony Burdi <anthony@superconductive.com>
2022-08-22 10:57:07 -0400

    Test for name field in serialization to disk. Notes on how / where to serialize.

--- tests/data_context/store/test_datasource_store.py
+++ tests/data_context/store/test_datasource_store.py
@@ -1,10 +1,12 @@
 import copy
+import os
 from typing import cast
 from unittest.mock import PropertyMock, patch
 
 import pytest
 
 from great_expectations.core.data_context_key import DataContextVariableKey
+from great_expectations.core.yaml_handler import YAMLHandler
 from great_expectations.data_context.data_context.data_context import DataContext
 from great_expectations.data_context.data_context_variables import (
     DataContextVariableSchema,
@@ -18,6 +20,8 @@ from great_expectations.data_context.types.base import (
     datasourceConfigSchema,
 )
 from great_expectations.data_context.types.resource_identifiers import GeCloudIdentifier
+
+yaml = YAMLHandler()
 
 
 @pytest.fixture
@@ -143,6 +147,48 @@ def test_datasource_store_with_inline_store_backend(
 
     assert isinstance(res, DatasourceConfig)
     assert res.to_json_dict() == datasource_config.to_json_dict()
+
+
+def test_datasource_store_with_inline_store_backend_config_with_names(
+    datasource_config_with_names: DatasourceConfig,
+    datasource_config: DatasourceConfig,
+    empty_data_context: DataContext,
+) -> None:
+    inline_store_backend_config: dict = {
+        "class_name": "InlineStoreBackend",
+        "resource_type": DataContextVariableSchema.DATASOURCES,
+        "data_context": empty_data_context,
+        "suppress_store_backend_id": True,
+    }
+
+    store = DatasourceStore(
+        store_name="my_datasource_store",
+        store_backend=inline_store_backend_config,
+    )
+
+    key = DataContextVariableKey(
+        resource_name="my_datasource",
+    )
+
+    store.set(key=key, value=datasource_config_with_names)
+    res: DatasourceConfig = store.get(key=key)
+
+    assert isinstance(res, DatasourceConfig)
+    assert res.to_json_dict() == datasource_config.to_json_dict()
+
+    # TODO: 20220818 check on disk for name in datasource configs
+    with open(
+        os.path.join(empty_data_context.root_directory, "great_expectations.yml")
+    ) as f:
+        context_config_from_disk: dict = yaml.load(f)
+
+    assert "name" not in context_config_from_disk["datasources"]["my_datasource"]
+    assert (
+        "name"
+        not in context_config_from_disk["datasources"]["my_datasource"][
+            "data_connectors"
+        ]["tripdata_monthly_configured"]
+    )
 
 
 def test_datasource_store_set_by_name(
