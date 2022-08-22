a004501ad4bb34bc60b4b34d60a60517014f6908
Nathan Farmer <nathan@superconductive.com>
2022-08-22 11:44:51 -0400

    xfail test that needs to be fixed

--- tests/data_context/cloud_data_context/test_include_rendered_content.py
+++ tests/data_context/cloud_data_context/test_include_rendered_content.py
@@ -104,67 +104,68 @@ def test_cloud_backed_data_context_save_expectation_suite_include_rendered_conte
 
 
 # TODO: ACB - Enable this test after merging fixes in PRs 5778 and 5763
-# @pytest.mark.cloud
-# @pytest.mark.integration
-# @pytest.mark.parametrize(
-#     "data_context_fixture_name",
-#     [
-#         # In order to leverage existing fixtures in parametrization, we provide
-#         # their string names and dynamically retrieve them using pytest's built-in
-#         # `request` fixture.
-#         # Source: https://stackoverflow.com/a/64348247
-#         pytest.param(
-#             "cloud_base_data_context_in_cloud_mode_with_datasource_pandas_engine",
-#             id="BaseDataContext",
-#         ),
-#         pytest.param(
-#             "cloud_data_context_in_cloud_mode_with_datasource_pandas_engine",
-#             id="DataContext",
-#         ),
-#         pytest.param(
-#             "cloud_data_context_with_datasource_pandas_engine",
-#             id="CloudDataContext",
-#         ),
-#     ],
-# )
-# def test_cloud_backed_data_context_expectation_validation_result_include_rendered_content(
-#     data_context_fixture_name: str,
-#     request,
-# ) -> None:
-#     """
-#     All Cloud-backed contexts (DataContext, BaseDataContext, and CloudDataContext) should save an ExpectationValidationResult
-#     with rendered_content by default.
-#     """
-#     context = request.getfixturevalue(data_context_fixture_name)
-#
-#     df = pd.DataFrame([1, 2, 3, 4, 5])
-#
-#     batch_request = RuntimeBatchRequest(
-#         datasource_name="my_datasource",
-#         data_connector_name="default_runtime_data_connector_name",
-#         data_asset_name="my_data_asset",
-#         runtime_parameters={"batch_data": df},
-#         batch_identifiers={"default_identifier_name": "my_id"},
-#     )
-#
-#     with mock.patch(
-#         "great_expectations.data_context.store.ge_cloud_store_backend.GeCloudStoreBackend.list_keys"
-#     ), mock.patch(
-#         "great_expectations.data_context.store.ge_cloud_store_backend.GeCloudStoreBackend._set"
-#     ):
-#         validator: Validator = context.get_validator(
-#             batch_request=batch_request,
-#             create_expectation_suite_with_name="test_suite",
-#         )
-#
-#         expectation_validation_result: ExpectationValidationResult = (
-#             validator.expect_table_row_count_to_equal(value=10)
-#         )
-#
-#     assert isinstance(
-#         expectation_validation_result.rendered_content[0], RenderedAtomicContent
-#     )
-#     assert isinstance(
-#         expectation_validation_result.expectation_config.rendered_content[0],
-#         RenderedAtomicContent,
-#     )
+@pytest.mark.cloud
+@pytest.mark.integration
+@pytest.mark.xfail(strict=True, reason="Remove xfail on merge of PRs 5778 and 5763")
+@pytest.mark.parametrize(
+    "data_context_fixture_name",
+    [
+        # In order to leverage existing fixtures in parametrization, we provide
+        # their string names and dynamically retrieve them using pytest's built-in
+        # `request` fixture.
+        # Source: https://stackoverflow.com/a/64348247
+        pytest.param(
+            "cloud_base_data_context_in_cloud_mode_with_datasource_pandas_engine",
+            id="BaseDataContext",
+        ),
+        pytest.param(
+            "cloud_data_context_in_cloud_mode_with_datasource_pandas_engine",
+            id="DataContext",
+        ),
+        pytest.param(
+            "cloud_data_context_with_datasource_pandas_engine",
+            id="CloudDataContext",
+        ),
+    ],
+)
+def test_cloud_backed_data_context_expectation_validation_result_include_rendered_content(
+    data_context_fixture_name: str,
+    request,
+) -> None:
+    """
+    All Cloud-backed contexts (DataContext, BaseDataContext, and CloudDataContext) should save an ExpectationValidationResult
+    with rendered_content by default.
+    """
+    context = request.getfixturevalue(data_context_fixture_name)
+
+    df = pd.DataFrame([1, 2, 3, 4, 5])
+
+    batch_request = RuntimeBatchRequest(
+        datasource_name="my_datasource",
+        data_connector_name="default_runtime_data_connector_name",
+        data_asset_name="my_data_asset",
+        runtime_parameters={"batch_data": df},
+        batch_identifiers={"default_identifier_name": "my_id"},
+    )
+
+    with mock.patch(
+        "great_expectations.data_context.store.ge_cloud_store_backend.GeCloudStoreBackend.list_keys"
+    ), mock.patch(
+        "great_expectations.data_context.store.ge_cloud_store_backend.GeCloudStoreBackend._set"
+    ):
+        validator: Validator = context.get_validator(
+            batch_request=batch_request,
+            create_expectation_suite_with_name="test_suite",
+        )
+
+        expectation_validation_result: ExpectationValidationResult = (
+            validator.expect_table_row_count_to_equal(value=10)
+        )
+
+    assert isinstance(
+        expectation_validation_result.rendered_content[0], RenderedAtomicContent
+    )
+    assert isinstance(
+        expectation_validation_result.expectation_config.rendered_content[0],
+        RenderedAtomicContent,
+    )
