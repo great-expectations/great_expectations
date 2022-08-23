45f7a392fda1f1bb557fc7534259ee7193411cbf
Nathan Farmer <nathan@superconductive.com>
2022-08-23 09:10:01 -0400

    Use column_aggregate_partial for ColumnDistinctValuesCount

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_distinct_values.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_distinct_values.py
@@ -1,20 +1,18 @@
 from typing import Any, Dict, Optional, Set
 
-from great_expectations.core import ExpectationConfiguration
 from great_expectations.core.metric_domain_types import MetricDomainTypes
 from great_expectations.execution_engine import (
-    ExecutionEngine,
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
 )
 from great_expectations.expectations.metrics.column_aggregate_metric_provider import (
     ColumnAggregateMetricProvider,
+    column_aggregate_partial,
     column_aggregate_value,
 )
 from great_expectations.expectations.metrics.import_manager import F, sa
 from great_expectations.expectations.metrics.metric_provider import metric_value
-from great_expectations.validator.metric_configuration import MetricConfiguration
 
 
 class ColumnDistinctValues(ColumnAggregateMetricProvider):
@@ -70,50 +68,41 @@ class ColumnDistinctValuesCount(ColumnAggregateMetricProvider):
     metric_name = "column.distinct_values.count"
 
     @column_aggregate_value(engine=PandasExecutionEngine)
-    def _pandas(cls, column, **kwargs) -> Set[Any]:
+    def _pandas(cls, column: str, **kwargs: Optional[dict]) -> Set[Any]:
         return column.nunique()
 
-    @metric_value(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(
-        cls,
-        execution_engine: SqlAlchemyExecutionEngine,
-        metric_domain_kwargs: Dict,
-        metric_value_kwargs: Dict,
-        metrics: Dict[str, Any],
-        runtime_configuration: Dict,
-    ) -> Set[Any]:
-        selectable, _, accessor_domain_kwargs = execution_engine.get_compute_domain(
-            metric_domain_kwargs, MetricDomainTypes.COLUMN
-        )
-        column = accessor_domain_kwargs["column"]
+    @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(cls, column: str, **kwargs: Optional[dict]) -> Set[Any]:
+        return sa.select(column).distinct().count()
 
-        query = sa.select(column).distinct()
-        results = execution_engine.engine.execute(
-            query.select_from(selectable)
-        ).fetchall()
-
-        return set(results)
-
-    @metric_value(engine=SparkDFExecutionEngine)
+    @column_aggregate_partial(engine=SparkDFExecutionEngine)
     def _spark(
         cls,
-        execution_engine: SqlAlchemyExecutionEngine,
-        metric_domain_kwargs: Dict,
-        metric_value_kwargs: Dict,
-        metrics: Dict[str, Any],
-        runtime_configuration: Dict,
+        column: str,
+        **kwargs: Optional[dict],
     ) -> Set[Any]:
-        df, _, accessor_domain_kwargs = execution_engine.get_compute_domain(
-            metric_domain_kwargs, MetricDomainTypes.COLUMN
-        )
-        column = accessor_domain_kwargs["column"]
+        return F.select(column).where(F.col(column).isNotNull()).groupBy(column).count()
 
-        distinct_values = (
-            df.select(column)
-            .where(F.col(column).isNotNull())
-            .groupBy(column)
-            .count()
-            .collect()
-        )
 
-        return set(distinct_values)
+class ColumnDistinctValuesCountUnderThreshold(ColumnAggregateMetricProvider):
+    metric_name = "column.distinct_values.count.under_threshold"
+
+    @column_aggregate_value(engine=PandasExecutionEngine)
+    def _pandas(cls, column: str, **kwargs: Optional[dict]) -> Set[Any]:
+        return column.nunique()
+
+    @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(
+        cls,
+        column: str,
+        **kwargs: Optional[dict],
+    ) -> Set[Any]:
+        return None
+
+    @column_aggregate_partial(engine=SparkDFExecutionEngine)
+    def _spark(
+        cls,
+        column: str,
+        **kwargs: Optional[dict],
+    ) -> Set[Any]:
+        return None
