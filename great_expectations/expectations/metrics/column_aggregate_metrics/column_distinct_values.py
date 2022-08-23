3f6df5d01a603bd2a42b8854e7dc881821473e90
Nathan Farmer <nathan@superconductive.com>
2022-08-23 09:21:17 -0400

    column.distinct_values.count.under_threshold WIP

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_distinct_values.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_distinct_values.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict, Optional, Set
 
+from great_expectations.core import ExpectationConfiguration
 from great_expectations.core.metric_domain_types import MetricDomainTypes
 from great_expectations.execution_engine import (
+    ExecutionEngine,
     PandasExecutionEngine,
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
@@ -13,6 +15,7 @@ from great_expectations.expectations.metrics.column_aggregate_metric_provider im
 )
 from great_expectations.expectations.metrics.import_manager import F, sa
 from great_expectations.expectations.metrics.metric_provider import metric_value
+from great_expectations.validator.metric_configuration import MetricConfiguration
 
 
 class ColumnDistinctValues(ColumnAggregateMetricProvider):
@@ -95,14 +98,44 @@ class ColumnDistinctValuesCountUnderThreshold(ColumnAggregateMetricProvider):
     def _sqlalchemy(
         cls,
         column: str,
+        metrics: Dict[str, Any],
         **kwargs: Optional[dict],
     ) -> Set[Any]:
-        return None
+        column_distinct_values_count = metrics["column.distinct_values.count"]
+        return column_distinct_values_count
 
     @column_aggregate_partial(engine=SparkDFExecutionEngine)
     def _spark(
         cls,
         column: str,
+        metrics: Dict[str, Any],
         **kwargs: Optional[dict],
     ) -> Set[Any]:
-        return None
+        column_distinct_values_count = metrics["column.distinct_values.count"]
+        return column_distinct_values_count
+
+    @classmethod
+    def _get_evaluation_dependencies(
+        cls,
+        metric: MetricConfiguration,
+        configuration: Optional[ExpectationConfiguration] = None,
+        execution_engine: Optional[ExecutionEngine] = None,
+        runtime_configuration: Optional[Dict] = None,
+    ):
+        """Returns a dictionary of given metric names and their corresponding configuration,
+        specifying the metric types and their respective domains"""
+        dependencies: dict = super()._get_evaluation_dependencies(
+            metric=metric,
+            configuration=configuration,
+            execution_engine=execution_engine,
+            runtime_configuration=runtime_configuration,
+        )
+        if isinstance(
+            execution_engine, (SqlAlchemyExecutionEngine, SparkDFExecutionEngine)
+        ):
+            dependencies["column.distinct_values.count"] = MetricConfiguration(
+                metric_name="column.distinct_values.count",
+                metric_domain_kwargs=metric.metric_domain_kwargs,
+                metric_value_kwargs=None,
+            )
+        return dependencies
