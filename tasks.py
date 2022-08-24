e7d8a3cca4f28c5b6c7846f191046f2f5d53ba79
Gabriel Gore <gabriel59kg@gmail.com>
2022-08-24 07:28:27 -0400

    update `TYPE_HINT_ERROR_THRESHOLD`

--- tasks.py
+++ tasks.py
@@ -118,7 +118,7 @@ def type_coverage(ctx):
         check_type_hint_coverage.main()
     except AssertionError as err:
         raise invoke.Exit(
-            message=f"{err}\n  See {check_type_hint_coverage.__file__}", code=1
+            message=f"{err}\n\n  See {check_type_hint_coverage.__file__}", code=1
         )
 
 
