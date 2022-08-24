c35d1c54b3f1d067d0de032ea9bad91f9f2923f8
Anthony Burdi <anthony@superconductive.com>
2022-08-24 12:30:40 -0400

    Revert test fixture change

--- tests/render/renderer/v3/test_suite_profile_notebook_renderer.py
+++ tests/render/renderer/v3/test_suite_profile_notebook_renderer.py
@@ -278,7 +278,6 @@ def test_notebook_execution_onboarding_data_assistant_pandas_backend(
                         "group_names": ["data_asset_name"],
                     },
                     "class_name": "InferredAssetFilesystemDataConnector",
-                    "name": "my_basic_data_connector",
                 },
                 "my_special_data_connector": {
                     "glob_directive": "*.csv",
@@ -295,7 +294,6 @@ def test_notebook_execution_onboarding_data_assistant_pandas_backend(
                     "base_directory": f"{root_dir}/../data/titanic",
                     "default_regex": {"pattern": "(.+)\\.csv", "group_names": ["name"]},
                     "class_name": "ConfiguredAssetFilesystemDataConnector",
-                    "name": "my_special_data_connector",
                 },
                 "my_other_data_connector": {
                     "glob_directive": "*.csv",
@@ -309,13 +307,11 @@ def test_notebook_execution_onboarding_data_assistant_pandas_backend(
                     "base_directory": f"{root_dir}/../data/titanic",
                     "default_regex": {"pattern": "(.+)\\.csv", "group_names": ["name"]},
                     "class_name": "ConfiguredAssetFilesystemDataConnector",
-                    "name": "my_other_data_connector",
                 },
                 "my_runtime_data_connector": {
                     "module_name": "great_expectations.datasource.data_connector",
                     "batch_identifiers": ["pipeline_stage_name", "airflow_run_id"],
                     "class_name": "RuntimeDataConnector",
-                    "name": "my_runtime_data_connector",
                 },
             },
         },
@@ -336,7 +332,6 @@ def test_notebook_execution_onboarding_data_assistant_pandas_backend(
                     },
                     "base_directory": f"{root_dir}/../data/titanic",
                     "class_name": "InferredAssetFilesystemDataConnector",
-                    "name": "my_additional_data_connector",
                 }
             },
         },
