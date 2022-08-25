5419f647ab82c98a303a774a35dfb205bfb2b22a
Chetan Kini <chetan@superconductive.com>
2022-08-25 11:13:01 -0400

    test: write unit tests

--- tests/data_context/test_data_context_datasources.py
+++ tests/data_context/test_data_context_datasources.py
@@ -1,4 +1,5 @@
 import pathlib
+from unittest import mock
 
 import pytest
 
@@ -12,6 +13,7 @@ from great_expectations.data_context.store.inline_store_backend import (
     InlineStoreBackend,
 )
 from great_expectations.data_context.types.base import DataContextConfig, GeCloudConfig
+from great_expectations.datasource.pandas_datasource import PandasDatasource
 
 
 @pytest.mark.integration
@@ -54,3 +56,129 @@ def test_data_context_instantiates_inline_store_backend_with_filesystem_config(
     )
 
     assert isinstance(context._datasource_store.store_backend, InlineStoreBackend)
+
+
+@pytest.mark.unit
+def test_add_datasource_updates_cache(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = "my_new_datasource"
+
+    assert name not in context.datasources
+
+    context.add_datasource(
+        name,
+        class_name="PandasDatasource",
+        batch_kwargs_generators={
+            "subdir_reader": {
+                "class_name": "SubdirReaderBatchKwargsGenerator",
+            }
+        },
+    )
+
+    assert name in context.datasources
+
+
+@pytest.mark.unit
+def test_update_datasource_updates_existing_value_in_cache(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = context.list_datasources()[0]["name"]
+    boto3_options = {"my": "option"}
+    datasource = PandasDatasource(name=name, boto3_options=boto3_options)
+
+    assert name in context.datasources
+
+    # Ensure that our cache value is updated to reflect changes
+    context.update_datasource(datasource)
+
+    assert name in context.datasources
+
+    retrieved_datasource = context.get_datasource(datasource_name=name)
+    assert retrieved_datasource.boto3_options == boto3_options
+
+
+@pytest.mark.unit
+def test_update_datasource_creates_new_value_in_cache(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = "my_new_datasource"
+    boto3_options = {"my": "option"}
+    datasource = PandasDatasource(name=name, boto3_options=boto3_options)
+
+    assert name not in context.datasources
+
+    # Ensure that a brand new cache value is added to reflect changes
+    context.update_datasource(datasource)
+
+    assert name in context.datasources
+
+
+@pytest.mark.unit
+def test_delete_datasource_updates_cache(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = context.list_datasources()[0]["name"]
+
+    # If the value is in the cache, no store methods should be invoked
+    with mock.patch(
+        "great_expectations.data_context.store.DatasourceStore.get"
+    ) as mock_get:
+        context.delete_datasource(name)
+
+    assert not mock_get.called
+    assert name not in context.datasources
+
+
+@pytest.mark.unit
+def test_get_datasource_retrieves_from_cache(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = context.list_datasources()[0]["name"]
+
+    # If the value is in the cache, no store methods should be invoked
+    with mock.patch(
+        "great_expectations.data_context.store.DatasourceStore.get"
+    ) as mock_get:
+        context.get_datasource(name)
+
+    assert not mock_get.called
+
+
+@pytest.mark.unit
+def test_get_datasource_cache_miss(
+    in_memory_runtime_context: BaseDataContext,
+) -> None:
+    context = in_memory_runtime_context
+
+    name = "my_fake_datasource_name"
+
+    # Initial GET will miss the cache, necessistating store retrieval
+    with mock.patch(
+        "great_expectations.data_context.store.DatasourceStore.has_key"
+    ), mock.patch(
+        "great_expectations.data_context.data_context.BaseDataContext._instantiate_datasource_from_config"
+    ), mock.patch(
+        "great_expectations.data_context.store.DatasourceStore.get"
+    ) as mock_get:
+        context.get_datasource(name)
+
+    assert mock_get.called
+
+    # Subsequent GET will retrieve from the cache
+    with mock.patch(
+        "great_expectations.data_context.store.DatasourceStore.get"
+    ) as mock_get:
+        context.get_datasource(name)
+
+    assert not mock_get.called
