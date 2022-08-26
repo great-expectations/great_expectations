a074bce777d3e7b3ee6277ee2d25515b39a46628
Anthony Burdi <anthony@superconductive.com>
2022-08-26 17:11:03 -0400

    Test should now pass with updated develop

--- tests/core/test_serializers.py
+++ tests/core/test_serializers.py
@@ -1,7 +1,10 @@
 import pytest
 
 from great_expectations.core.configuration import AbstractConfigSchema
-from great_expectations.core.serializer import DictConfigSerializer
+from great_expectations.core.serializer import (
+    DictConfigSerializer,
+    JsonConfigSerializer,
+)
 from great_expectations.marshmallow__shade import Schema
 
 
@@ -20,15 +23,7 @@ def test_init_dict_config_serializer(empty_abstract_config_schema: Schema):
     assert serializer.schema == empty_abstract_config_schema
 
 
-@pytest.mark.xfail(
-    raises=(ImportError, NameError),
-    reason="xfail until PR 5778 merges with JsonConfigSerializer",
-    strict=True,
-)
 def test_init_json_config_serializer(empty_abstract_config_schema: Schema):
-
-    # Note: move this import statement to the top of the file once this test is meant to pass
-    from great_expectations.core.serializer import JsonConfigSerializer
 
     serializer = JsonConfigSerializer(schema=empty_abstract_config_schema)
 
