c0b5749440f5e55ee0bc072f3faf2e51031ffb06
Alex Sherstinsky <alex@superconductive.com>
2022-09-01 23:00:57 -0700

    init

--- great_expectations/rule_based_profiler/data_assistant_result/statistics_data_assistant_result.py
+++ great_expectations/rule_based_profiler/data_assistant_result/statistics_data_assistant_result.py
@@ -12,18 +12,7 @@ class StatisticsDataAssistantResult(DataAssistantResult):
         """
         A mapping is defined for which metrics to plot and their associated expectations.
         """
-        return {
-            "table.columns": "expect_table_columns_to_match_set",
-            "table.row_count": "expect_table_row_count_to_be_between",
-            "column.distinct_values.count": "expect_column_unique_value_count_to_be_between",
-            "column.min": "expect_column_min_to_be_between",
-            "column.max": "expect_column_max_to_be_between",
-            "column.mean": "expect_column_mean_to_be_between",
-            "column.median": "expect_column_median_to_be_between",
-            "column.standard_deviation": "expect_column_stdev_to_be_between",
-            "column.quantile_values": "expect_column_quantile_values_to_be_between",
-            ("column.min", "column.max"): "expect_column_values_to_be_between",
-        }
+        return {}
 
     @property
     def metric_types(self) -> Dict[str, AltairDataTypes]:
