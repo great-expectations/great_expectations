1e51c91c72293e0d1e45f961595e03bee16aea1d
Alex Sherstinsky <alex@superconductive.com>
2022-09-01 22:24:07 -0700

    statistics data assistant

--- great_expectations/rule_based_profiler/data_assistant_result/__init__.py
+++ great_expectations/rule_based_profiler/data_assistant_result/__init__.py
@@ -1,3 +1,4 @@
 from .data_assistant_result import DataAssistantResult
 from .onboarding_data_assistant_result import OnboardingDataAssistantResult
+from .statistics_data_assistant_result import StatisticsDataAssistantResult
 from .volume_data_assistant_result import VolumeDataAssistantResult
