2a563fac839497afa0cb7f8f8ce75d3186934329
Anthony Burdi <anthony@superconductive.com>
2022-09-06 12:48:26 -0400

    DatasourceStore.set() returns results of get() after setting value

--- great_expectations/data_context/store/datasource_store.py
+++ great_expectations/data_context/store/datasource_store.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import copy
 from typing import List, Optional, Union, Tuple
 
+from great_expectations.data_context.types.refs import GeCloudResourceRef
+
 from great_expectations.core.data_context_key import DataContextVariableKey, DataContextKey
 from great_expectations.core.serializer import AbstractConfigSerializer
 from great_expectations.data_context.store.store import Store
@@ -163,8 +165,8 @@ class DatasourceStore(Store):
         )
         self.set(datasource_key, datasource_config)
 
-    def set(
-        self, key: Union[DataContextKey, None], value: DatasourceConfig, **_: dict
+    def set(  # type: ignore[override]
+            self, key: Union[DataContextKey, None], value: DatasourceConfig, **_: dict
     ) -> DatasourceConfig:
         """Create a datasource config in the store using a store_backend-specific key.
         Args:
@@ -177,9 +179,13 @@ class DatasourceStore(Store):
         if not key:
             key = self._build_key_from_config(value)
 
-        super().set(key, value)
+        # Make two separate requests to set and get in order to obtain any additional
+        # values that may have been added to the config by the StoreBackend (i.e. object ids)
+        ref: Optional[Union[bool, GeCloudResourceRef]] = super().set(key, value)  # type: ignore[func-returns-value]
+        if ref and isinstance(ref, GeCloudResourceRef):
+            key.ge_cloud_id = ref.ge_cloud_id  # type: ignore[attr-defined]
 
-        return self.get(key)
+        return self.get(key)  # type: ignore[return-value]
 
     def update_by_name(
         self, datasource_name: str, datasource_config: DatasourceConfig
@@ -211,10 +217,10 @@ class DatasourceStore(Store):
         )
         return datasource_key
 
-    def _validate_key(self, key: Union[GeCloudIdentifier, Tuple[str, ...]]) -> None:
+    def _validate_key(self, key: Union[GeCloudIdentifier, Tuple[str, ...]]) -> None:  # type: ignore[override]
         # Continue to support tuple as a key for get method until we can refactor
         if not isinstance(key, GeCloudIdentifier):
-            super()._validate_key(key)
+            super()._validate_key(key)  # type: ignore[arg-type]
         else:
             assert isinstance(
                 key, GeCloudIdentifier
