b5b084e0d60c6b446ac8d82fdb2eef296177a364
Chetan Kini <chetan@superconductive.com>
2022-09-13 15:36:14 -0400

    doc: add docstring to handler class

--- great_expectations/core/http_handler.py
+++ great_expectations/core/http_handler.py
@@ -6,6 +6,24 @@ from great_expectations import __version__
 
 
 class HTTPHandler:
+    """
+    Facade class designed to be a lightweight wrapper around HTTP requests.
+    For all external HTTP requests made in Great Expectations, this is the entry point.
+
+    HTTPHandler is designed tobe library agnostic - the underlying implementation does not
+    matter as long as we fulfill the following contract:
+        * get
+        * post
+        * put
+        * patch
+        * delete
+
+    20220913 - Chetan - This class should be refactored to completely abstract away any
+    specific implementation details (explicit reference to `requests` lib). As it stands,
+    the handler returns a `requests.Response` object. Additionally, error checking is done
+    downstream in GeCloudStoreBackend that also directly references this lib.
+    """
+
     def __init__(self, access_token: str, timeout: int = 20) -> None:
         self._headers = self._init_headers(access_token)
         self._timeout = timeout
@@ -19,31 +37,26 @@ class HTTPHandler:
         return headers
 
     def get(self, url: str, params: Optional[dict] = None) -> requests.Response:
-        response = requests.get(
+        return requests.get(
             url=url, params=params, headers=self._headers, timeout=self._timeout
         )
-        return response
 
     def post(self, url: str, json: dict) -> requests.Response:
-        response = requests.post(
+        return requests.post(
             url, json=json, headers=self._headers, timeout=self._timeout
         )
-        return response
 
     def put(self, url: str, json: dict) -> requests.Response:
-        response = requests.put(
+        return requests.put(
             url, json=json, headers=self._headers, timeout=self._timeout
         )
-        return response
 
     def patch(self, url: str, json: dict) -> requests.Response:
-        response = requests.put(
+        return requests.put(
             url, json=json, headers=self._headers, timeout=self._timeout
         )
-        return response
 
     def delete(self, url: str, json: dict) -> requests.Response:
-        response = requests.delete(
+        return requests.delete(
             url, json=json, headers=self._headers, timeout=self._timeout
         )
-        return response
