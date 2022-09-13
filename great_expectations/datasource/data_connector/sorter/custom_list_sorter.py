83c5e0553ab015fc82324359fe37b2717a937b90
Nathan Farmer <nathan@superconductive.com>
2022-09-13 10:11:39 -0400

    Remove type hint import

--- great_expectations/datasource/data_connector/sorter/custom_list_sorter.py
+++ great_expectations/datasource/data_connector/sorter/custom_list_sorter.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Any, List, Optional
+from typing import Any, List
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations.core.batch import BatchDefinition
