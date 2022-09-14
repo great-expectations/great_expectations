b074c818edcec6124544815bb88ccaa002261aa0
Chetan Kini <chetan@superconductive.com>
2022-09-14 17:56:34 -0400

    chore: more updates

--- great_expectations/cli/datasource.py
+++ great_expectations/cli/datasource.py
@@ -454,8 +454,6 @@ data_connectors:
     class_name: InferredAssetSqlDataConnector
     include_schema_name: True
   default_configured_data_connector_name:
-    class_name: InferredAssetSqlDataConnector
-    include_schema_name: True
   default_configured_data_connector_name:
     class_name: ConfiguredAssetSqlDataConnector
     assets:
