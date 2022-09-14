8e6c5071a79c825f340b61a4d4ae8f22ace1506a
Nathan Farmer <nathan@superconductive.com>
2022-09-14 12:44:47 -0400

    Bugfix for identifying if None exists in batch_identifier values

--- great_expectations/datasource/data_connector/sorter/sorter.py
+++ great_expectations/datasource/data_connector/sorter/sorter.py
@@ -33,9 +33,9 @@ class Sorter:
             if len(list(batch_definition.batch_identifiers.values())) == 0:
                 batch_identifier_values = [None]
             elif isinstance(list(batch_definition.batch_identifiers.values())[0], dict):
-                batch_identifier_values = dict(
+                batch_identifier_values = list(
                     batch_definition.batch_identifiers.values()
-                ).values()
+                )[0].values()
             else:
                 batch_identifier_values = batch_definition.batch_identifiers.values()
 
