eb7c88789293e9e1748576e8ae19c342d10b6b37
James Campbell <james.p.campbell@gmail.com>
2022-09-15 17:11:04 -0400

    Revert "Vectorize conversions"
    This reverts commit 175c6a53a7fb61312379b36bb003c850b2a5c6fa.

--- great_expectations/util.py
+++ great_expectations/util.py
@@ -1515,8 +1515,8 @@ def convert_ndarray_datetime_to_float_dtype(data: np.ndarray) -> np.ndarray:
     Convert all elements of 1-D "np.ndarray" argument from "datetime.datetime" type to "timestamp" "float" type objects.
     """
     value: Any
-    return np.vectorize(lambda x: x.replace(tzinfo=datetime.timezone.utc).timestamp())(
-        data
+    return np.asarray(
+        [value.replace(tzinfo=datetime.timezone.utc).timestamp() for value in data]
     )
 
 
@@ -1525,7 +1525,7 @@ def convert_ndarray_float_to_datetime_dtype(data: np.ndarray) -> np.ndarray:
     Convert all elements of 1-D "np.ndarray" argument from "float" type to "datetime.datetime" type objects.
     """
     value: Any
-    return np.vectorize(lambda x: datetime.datetime.utcfromtimestamp(x))(data)
+    return np.asarray([datetime.datetime.utcfromtimestamp(value) for value in data])
 
 
 def convert_ndarray_float_to_datetime_tuple(
