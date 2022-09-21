f5e11fab59932c6152e05e5e34a64629a1aa0509
Chetan Kini <chetan@superconductive.com>
2022-09-21 09:58:32 -0400

    refactor: misc cleanup

--- great_expectations/core/http.py
+++ great_expectations/core/http.py
@@ -5,15 +5,14 @@ from great_expectations import __version__
 
 def session_factory(access_token: str) -> requests.Session:
     session = requests.Session()
-    headers = _init_headers(access_token)
-    session.headers.update(headers)
+    _update_headers(session, access_token)
     return session
 
 
-def _init_headers(access_token: str) -> dict:
+def _update_headers(session: requests.Session, access_token: str) -> None:
     headers = {
         "Content-Type": "application/vnd.api+json",
         "Authorization": f"Bearer {access_token}",
         "Gx-Version": __version__,
     }
-    return headers
+    session.headers.update(headers)
