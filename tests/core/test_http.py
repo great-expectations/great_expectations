a903f9481fe2a7adc89de0fb79545f640ade5baa
Chetan Kini <chetan@superconductive.com>
2022-09-21 10:33:14 -0400

    test: add retry adapter

--- tests/core/test_http.py
+++ tests/core/test_http.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 import pytest
 
-from great_expectations.core.http import session_factory
+from great_expectations.core.http import DEFAULT_TIMEOUT, session_factory
 
 
 @pytest.mark.unit
@@ -12,3 +12,22 @@ def test_session_factory_contains_appropriate_headers(
     session = session_factory(access_token=ge_cloud_access_token)
     for key, val in request_headers.items():
         assert session.headers.get(key) == val
+
+
+@pytest.mark.unit
+def test_session_factory_contains_appropriate_adapters() -> None:
+    access_token = "05f23701-d8e3-4c0a-8e38-2ad7bf16cb58"
+    retry_count = 10
+    backoff_factor = 0.1
+    session = session_factory(
+        access_token=access_token,
+        retry_count=retry_count,
+        backoff_factor=backoff_factor,
+    )
+
+    assert len(session.adapters) == 2
+    for adapter in session.adapters.values():
+        assert adapter.timeout == DEFAULT_TIMEOUT
+        retries = adapter.max_retries
+        assert retries.total == retry_count
+        assert retries.backoff_factor == backoff_factor
