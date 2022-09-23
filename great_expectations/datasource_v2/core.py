9be9a97b4b5ea414da42b9ed994e60acc0321a6c
Gabriel Gore <gabriel59kg@gmail.com>
2022-09-23 13:59:26 -0400

    type coercion example

--- great_expectations/datasource_v2/core.py
+++ great_expectations/datasource_v2/core.py
@@ -37,6 +37,21 @@ def create_source(config=None) -> Source:
     )
 
 
+@create_source.register(dict)
+def type_coercion(config: dict) -> Source:
+    # TODO: pull from all registerd types?
+    registed_types = [PandasSourceConfig, SQLSourceConfig]
+    for source_config in registed_types:
+        try:
+            coerced_type = source_config(**config)
+            return create_source(coerced_type)
+
+        except TypeError:
+            break
+
+    raise TypeError(f"Failed to coerce {config}")
+
+
 @create_source.register(PandasSourceConfig)
 def pandas_source(config: PandasSourceConfig) -> PandasSource:
     source = PandasSource()
@@ -54,4 +69,5 @@ def sql_source(config: SQLSourceConfig) -> SQLSource:
 if __name__ == "__main__":
     create_source(PandasSourceConfig(name="taxi", base_dir="."))
     create_source(SQLSourceConfig(name="taxi", connection_string="taxi.db"))
+    create_source({"name": "taxi", "base_dir": "."})
     create_source({"foo": "bar"})
