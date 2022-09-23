cf9f8f9f2b9fd3cc7a2d3b2baec01bcceabb94a0
Bill Dirks <bill@superconductive.com>
2022-09-23 16:01:56 -0700

    Rename self to cls

--- great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
@@ -52,10 +52,11 @@ please see: https://greatexpectations.io/blog/why_we_dont_do_transformations_for
         return column.isin(value_set)
 
     @column_condition_partial(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(self, column, value_set, **kwargs):
-        return self._sqlalchemy_impl(column, value_set, **kwargs)
+    def _sqlalchemy(cls, column, value_set, **kwargs):
+        return cls._sqlalchemy_impl(column, value_set, **kwargs)
 
-    def _sqlalchemy_impl(self, column, value_set, **kwargs):
+    @staticmethod
+    def _sqlalchemy_impl(column, value_set, **kwargs):
         # no need to parse as datetime; just compare the strings as is
         parse_strings_as_datetimes: bool = (
             kwargs.get("parse_strings_as_datetimes") or False
