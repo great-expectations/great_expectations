3c5b756ca623ef7dcfae8428aeb9238e915a4384
Gabriel Gore <gabriel59kg@gmail.com>
2022-09-23 14:06:25 -0400

    comment out POC

--- great_expectations/datasource_v2/core.py
+++ great_expectations/datasource_v2/core.py
@@ -37,19 +37,19 @@ def create_source(config=None) -> Source:
     )
 
 
-@create_source.register(dict)
-def type_coercion(config: dict) -> Source:
-    # TODO: pull from all registerd types?
-    registed_types = [PandasSourceConfig, SQLSourceConfig]
-    for source_config in registed_types:
-        try:
-            coerced_type = source_config(**config)
-            return create_source(coerced_type)
+# @create_source.register(dict)
+# def type_coercion(config: dict) -> Source:
+#     # TODO: pull from all registerd types?
+#     registed_types = [PandasSourceConfig, SQLSourceConfig]
+#     for source_config in registed_types:
+#         try:
+#             coerced_type = source_config(**config)
+#             return create_source(coerced_type)
 
-        except TypeError:
-            break
+#         except TypeError:
+#             break
 
-    raise TypeError(f"Failed to coerce {config}")
+#     raise TypeError(f"Failed to coerce {config}")
 
 
 @create_source.register(PandasSourceConfig)
