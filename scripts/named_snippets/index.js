10942e4c1fe77cad8867900719f4326e7573beec
Chetan Kini <chetan@superconductive.com>
2022-09-25 22:31:52 -0400

    docs: add module-level docstring

--- scripts/named_snippets/index.js
+++ scripts/named_snippets/index.js
@@ -1,3 +1,26 @@
+/*
+This script enables name-based snippet retrieval in Docusaurus-enabled docs using
+the following syntax:
+```
+    ```python name="getting_started_imports"
+```
+
+This pattern is directly inspired by remark-code-import, which references using line numbers:
+```
+    ```python file=../../tests/integration/docusaurus/tutorials/getting-started/getting_started.py#L1-L5
+```
+
+As snippets are bound by identifier and not specific line numbers, they are far less susceptible
+to breakage when docs and source code are being updated.
+
+Named snippets are defined with the following syntax:
+```
+    # <snippet name="getting_started_imports">
+    import great_expectations as gx
+    ...
+    # </snippet>
+```
+*/
 const visit = require('unist-util-visit');
 const constructSnippetMap = require("./snippet")
 
