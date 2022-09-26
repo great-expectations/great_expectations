e49af3bc40fbe4efdfb1c6c7dfd8cb8e25efdae6
Chetan Kini <chetan@superconductive.com>
2022-09-26 10:25:00 -0400

    chore: add comments

--- tests/data_context/test_cloud_migrator.py
+++ tests/data_context/test_cloud_migrator.py
@@ -19,21 +19,23 @@ def test_cloud_migrator_test_migrate_true(empty_data_context: DataContext):
 def test__send_configuration_bundle_sends_valid_http_request(
     serialized_configuration_bundle: dict,
 ):
+    # These values aren't actual creds but resemble values used in production
     ge_cloud_base_url = "https://app.test.greatexpectations.io"
     ge_cloud_organization_id = "229616e2-1bbc-4849-8161-4be89b79bd36"
     ge_cloud_access_token = "d7asdh2efads9afah2e0fadf8eh20da8"
 
+    # Mock any external dependencies so we're only testing HTTP request logic
     mock_context = mock.MagicMock()
+    configuration_bundle = mock.MagicMock()
+    serializer = mock.MagicMock()
+    serializer.serialize.return_value = serialized_configuration_bundle
+
     migrator = gx.CloudMigrator(
         context=mock_context,
         ge_cloud_base_url=ge_cloud_base_url,
         ge_cloud_organization_id=ge_cloud_organization_id,
         ge_cloud_access_token=ge_cloud_access_token,
     )
-
-    configuration_bundle = mock.MagicMock()
-    serializer = mock.MagicMock()
-    serializer.serialize.return_value = serialized_configuration_bundle
 
     with mock.patch("requests.Session.post", autospec=True) as mock_post:
         migrator._send_configuration_bundle(
