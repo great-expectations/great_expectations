430936d8493b92b1db2b40a16b6b3d18775dae21
Chetan Kini <chetan@superconductive.com>
2022-09-26 15:17:32 -0400

    test: start printing

--- great_expectations/data_context/cloud_migrator.py
+++ great_expectations/data_context/cloud_migrator.py
@@ -1,4 +1,5 @@
 """TODO: Add docstring"""
+import warnings
 from dataclasses import dataclass
 from typing import List, Optional, cast
 
@@ -18,7 +19,7 @@ from great_expectations.data_context.types.base import (
     CheckpointConfig,
     CheckpointConfigSchema,
     DataContextConfigSchema,
-    GeCloudConfig,
+    DatasourceConfig,
 )
 from great_expectations.data_context.types.resource_identifiers import (
     ValidationResultIdentifier,
@@ -39,6 +40,7 @@ class ConfigurationBundle:
 
         self._data_context_variables: DataContextVariables = context.variables
 
+        self._datasources = []
         self._expectation_suites: List[
             ExpectationSuite
         ] = self._get_all_expectation_suites()
@@ -47,6 +49,26 @@ class ConfigurationBundle:
         self._validation_results: List[
             ExpectationSuiteValidationResult
         ] = self._get_all_validation_results()
+
+    @property
+    def datasources(self) -> List[DatasourceConfig]:
+        return self._datasources
+
+    @property
+    def expectation_suites(self) -> List[ExpectationSuite]:
+        return self._expectation_suites
+
+    @property
+    def checkpoints(self) -> List[CheckpointConfig]:
+        return self._checkpoints
+
+    @property
+    def profilers(self) -> List[RuleBasedProfilerConfig]:
+        return self._profilers
+
+    @property
+    def validation_results(self) -> List[ExpectationSuiteValidationResult]:
+        return self._validation_results
 
     def is_usage_stats_enabled(self) -> bool:
         """Determine whether usage stats are enabled.
@@ -223,14 +245,19 @@ class CloudMigrator:
 
     def _migrate_to_cloud(self, test_migrate: bool):
         """TODO: This is a rough outline of the steps to take during the migration, verify against the spec before release."""
-        self._warn_if_test_migrate()
+        self._print_migration_introduction_message()
+        if test_migrate:
+            self._warn_about_test_migrate()
+
         configuration_bundle: ConfigurationBundle = ConfigurationBundle(
             context=self._context
         )
-        self._warn_if_usage_stats_disabled(
-            configuration_bundle.is_usage_stats_enabled()
-        )
-        self._print_configuration_bundle(configuration_bundle)
+
+        if not configuration_bundle.is_usage_stats_enabled():
+            self._warn_about_usage_stats_disabled()
+
+        self._print_configuration_bundle_summary(configuration_bundle)
+
         if not test_migrate:
             configuration_bundle_serializer = ConfigurationBundleJsonSerializer(
                 schema=ConfigurationBundleSchema()
@@ -242,57 +269,55 @@ class CloudMigrator:
             self._break_for_send_configuration_bundle_error(
                 configuration_bundle_response
             )
+
         errors: List[
             SendValidationResultsErrorDetails
         ] = self._send_and_print_validation_results(test_migrate)
         self._print_validation_result_error_summary(errors)
         self._print_migration_conclusion_message()
 
-    def _process_cloud_credential_overrides(
-        self,
-        ge_cloud_base_url: Optional[str] = None,
-        ge_cloud_access_token: Optional[str] = None,
-        ge_cloud_organization_id: Optional[str] = None,
-    ) -> GeCloudConfig:
-        """Get cloud credentials from environment variables or parameters.
+    def _warn_about_test_migrate(self) -> None:
+        warnings.warn(
+            "This is a test run! Please pass `test_migrate=False` to begin the "
+            "actual migration (e.g. `CloudMigrator.migrate(context=context, test_migrate=False)`)."
+        )
 
-        Check first for ge_cloud_base_url, ge_cloud_access_token and
-        ge_cloud_organization_id provided via params, if not then check
-        for the corresponding environment variable.
-
-        Args:
-            ge_cloud_base_url: Optional, you may provide this alternatively via
-                environment variable GE_CLOUD_BASE_URL
-            ge_cloud_access_token: Optional, you may provide this alternatively
-                via environment variable GE_CLOUD_ACCESS_TOKEN
-            ge_cloud_organization_id: Optional, you may provide this alternatively
-                via environment variable GE_CLOUD_ORGANIZATION_ID
-
-        Returns:
-            GeCloudConfig
-
-        Raises:
-            GeCloudError
-
-        """
-        # TODO: Use GECloudEnvironmentVariable enum for environment variables
-        # TODO: Merge with existing logic in Data Context - could be static method on CloudDataContext or
-        #  module level method in cloud_data_context.py Let's not duplicate this code.
-        pass
-
-    def _warn_if_test_migrate(self) -> None:
-        pass
-
-    def _warn_if_usage_stats_disabled(self, is_usage_stats_enabled: bool) -> None:
-        pass
+    def _warn_about_usage_stats_disabled(self) -> None:
+        warnings.warn(
+            "We noticed that you had disabled usage statistics tracking. "
+            "Please note that by migrating your context to GX Cloud your new Cloud Data Context "
+            "will emit usage statistics. These statistics help us understand how we can improve "
+            "the product and we hope you don't mind!"
+        )
 
     def _build_configuration_bundle(self) -> ConfigurationBundle:
         pass
 
-    def _print_configuration_bundle(
+    def _print_configuration_bundle_summary(
         self, configuration_bundle: ConfigurationBundle
     ) -> None:
-        pass
+        self._print("Bundling context configuration (step 1/4)")
+
+        to_print = (
+            ("Datasource", configuration_bundle.datasources),
+            ("Checkpoint", configuration_bundle.checkpoints),
+            ("Expectation Suite", configuration_bundle.expectation_suites),
+            ("Profiler", configuration_bundle.profilers),
+        )
+
+        for name, collection in to_print:
+            self._print_object_summary(obj_name=name, obj_collection=collection)
+
+    def _print_object_summary(self, obj_name: str, obj_collection: list) -> None:
+        length = len(obj_collection)
+        self._print(f"Bundled {length} {obj_name}s:", indent=2)
+        for obj in obj_collection[:10]:
+            self._print(obj.name, indent=4)
+
+        if length > 10:
+            self._print(
+                f"({length-10} other {obj_name.lower()} not displayed)", indent=4
+            )
 
     def _send_configuration_bundle(
         self,
@@ -322,5 +347,11 @@ class CloudMigrator:
     ) -> None:
         pass
 
+    def _print_migration_introduction_message(self):
+        pass
+
     def _print_migration_conclusion_message(self):
         pass
+
+    def _print(self, text: str, indent: int = 0) -> None:
+        print(f"{indent * ' '}{text}")
--- tests/data_context/test_cloud_migrator.py
+++ tests/data_context/test_cloud_migrator.py
@@ -1,8 +1,36 @@
 """TODO: Add docstring"""
+from unittest import mock
+
 import pytest
 
 import great_expectations as gx
 from great_expectations import DataContext
+from great_expectations.data_context.cloud_migrator import CloudMigrator
+
+
+@pytest.fixture
+def mock_context_with_disabled_usage_stats() -> mock.MagicMock:
+    context = mock.MagicMock()
+    variables = context.variables
+    usage_stats = variables.anonymous_usage_statistics
+    usage_stats.enabled = False
+    return context
+
+
+@pytest.fixture
+def migrator_with_mock_context(
+    mock_context_with_disabled_usage_stats: mock.MagicMock,
+) -> CloudMigrator:
+    ge_cloud_base_url = ""
+    ge_cloud_access_token = ""
+    ge_cloud_organization_id = ""
+
+    return CloudMigrator(
+        context=mock_context_with_disabled_usage_stats,
+        ge_cloud_base_url=ge_cloud_base_url,
+        ge_cloud_access_token=ge_cloud_access_token,
+        ge_cloud_organization_id=ge_cloud_organization_id,
+    )
 
 
 def test_cloud_migrator_test_migrate_true(empty_data_context: DataContext):
@@ -10,3 +38,23 @@ def test_cloud_migrator_test_migrate_true(empty_data_context: DataContext):
 
     with pytest.raises(NotImplementedError):
         gx.CloudMigrator.migrate(test_migrate=True, context=empty_data_context)
+
+
+@pytest.mark.unit
+@pytest.mark.cloud
+def test_cloud_migrator_test_migrate_true_emits_warnings(
+    migrator_with_mock_context: CloudMigrator,
+):
+    migrator = migrator_with_mock_context
+
+    with pytest.warns() as record:
+        migrator._migrate_to_cloud(test_migrate=True)
+
+    assert len(record) == 2
+    assert "Please pass `test_migrate=False` to begin the actual migration" in str(
+        record[0].message
+    )
+    assert (
+        "Please note that by migrating your context to GX Cloud your new Cloud Data Context will emit usage statistics"
+        in str(record[1].message)
+    )
