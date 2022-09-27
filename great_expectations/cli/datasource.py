93752cd414ce71894a123989af68b303653b9975
Przemek Denkiewicz <przemyslaw.denkiewicz@starburstdata.com>
2022-09-27 12:11:07 +0200

    Add Trino support to CLI

--- great_expectations/cli/datasource.py
+++ great_expectations/cli/datasource.py
@@ -39,6 +39,7 @@ class SupportedDatabaseBackends(enum.Enum):
     REDSHIFT = "Redshift"
     SNOWFLAKE = "Snowflake"
     BIGQUERY = "BigQuery"
+    TRINO = "Trino"
     OTHER = "other - Do you have a working SQLAlchemy connection string?"
     # TODO MSSQL
 
@@ -703,6 +704,41 @@ table_name = ""'''
         return "\n  connection_string: {connection_string}"
 
 
+class TrinoCredentialYamlHelper(SQLCredentialYamlHelper):
+    def __init__(self, datasource_name: Optional[str]) -> None:
+        super().__init__(
+            database="YOUR_TRINO_CATALOG",
+            datasource_name=datasource_name,
+            usage_stats_payload={
+                "type": "sqlalchemy",
+                "db": SupportedDatabaseBackends.TRINO.value,
+                "api_version": "v3",
+            },
+            driver="trino",
+        )
+
+    def verify_libraries_installed(self) -> bool:
+        return verify_library_dependent_modules(
+            python_import_name="trino.sqlalchemy.dialect",
+            pip_library_name="trino",
+            module_names_to_reload=CLI_ONLY_SQLALCHEMY_ORDERED_DEPENDENCY_MODULE_NAMES,
+        )
+
+    def credentials_snippet(self) -> str:
+        return f'''\
+host = "{self.host}"
+port = "{self.port}"
+username = "{self.username}"
+password = "{self.password}"
+
+# Trino has a concept of catalog which maps to database
+database = "{self.database}"
+schema_name = "{self.schema_name}"
+
+# A table that you would like to add initially as a Data Asset
+table_name = "{self.table_name}"'''
+
+
 class ConnectionStringCredentialYamlHelper(SQLCredentialYamlHelper):
     def __init__(self, datasource_name: Optional[str]) -> None:
         super().__init__(
@@ -750,6 +786,7 @@ def _get_sql_yaml_helper_class(
         SupportedDatabaseBackends.REDSHIFT: RedshiftCredentialYamlHelper,
         SupportedDatabaseBackends.SNOWFLAKE: SnowflakeCredentialYamlHelper,
         SupportedDatabaseBackends.BIGQUERY: BigqueryCredentialYamlHelper,
+        SupportedDatabaseBackends.TRINO: TrinoCredentialYamlHelper,
         SupportedDatabaseBackends.OTHER: ConnectionStringCredentialYamlHelper,
     }
     helper_class = helper_class_by_backend[selected_database]
