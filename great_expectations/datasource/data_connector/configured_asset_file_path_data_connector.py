9c4252ca366e5655678992761c9f9f2121240fda
Gabriel Gore <gabriel59kg@gmail.com>
2022-09-26 22:59:31 -0400

    configured_assert_file_path_data_connector

--- great_expectations/datasource/data_connector/configured_asset_file_path_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_file_path_data_connector.py
@@ -108,7 +108,7 @@ class ConfiguredAssetFilePathDataConnector(FilePathDataConnector):
             ):
                 mapped_batch_definition_list: List[
                     BatchDefinition
-                ] = self._map_data_reference_to_batch_definition_list(
+                ] = self._map_data_reference_to_batch_definition_list(  # type: ignore[assignment]
                     data_reference=data_reference,
                     data_asset_name=data_asset_name,
                 )
@@ -191,14 +191,14 @@ class ConfiguredAssetFilePathDataConnector(FilePathDataConnector):
                 regex_config["group_names"] = asset.group_names
         return regex_config
 
-    def _get_asset(self, data_asset_name: str) -> Asset:
+    def _get_asset(self, data_asset_name: Optional[str]) -> Union[Asset, None]:
         asset: Optional[Asset] = None
         if (
             data_asset_name is not None
             and self.assets
             and data_asset_name in self.assets
         ):
-            asset = self.assets[data_asset_name]
+            asset = self.assets[data_asset_name]  # type: ignore[assignment]
         return asset
 
     def _get_data_reference_list_for_asset(self, asset: Optional[Asset]) -> List[str]:
@@ -221,12 +221,12 @@ class ConfiguredAssetFilePathDataConnector(FilePathDataConnector):
         data_asset_name: str = batch_definition.data_asset_name
         if (
             data_asset_name in self.assets
-            and self.assets[data_asset_name].batch_spec_passthrough
-            and isinstance(self.assets[data_asset_name].batch_spec_passthrough, dict)
+            and self.assets[data_asset_name].batch_spec_passthrough  # type: ignore[union-attr]
+            and isinstance(self.assets[data_asset_name].batch_spec_passthrough, dict)  # type: ignore[union-attr]
         ):
             # batch_spec_passthrough from data_asset
             batch_spec_passthrough = deepcopy(
-                self.assets[data_asset_name]["batch_spec_passthrough"]
+                self.assets[data_asset_name]["batch_spec_passthrough"]  # type: ignore[index]
             )
             batch_definition_batch_spec_passthrough = (
                 deepcopy(batch_definition.batch_spec_passthrough) or {}
