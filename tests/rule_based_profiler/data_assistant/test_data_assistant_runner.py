7d5bd762a6156efb4287d9bb776efa40fa05d13d
Nathan Farmer <nathan@greatexpectations.io>
2022-09-28 08:48:59 -0400

    Add test for behavior when explicit None is passed to run top-level API

--- tests/rule_based_profiler/data_assistant/test_data_assistant_runner.py
+++ tests/rule_based_profiler/data_assistant/test_data_assistant_runner.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import pytest
 
+from great_expectations.core.batch import RuntimeBatchRequest
 from great_expectations.data_context import DataContext
 from great_expectations.rule_based_profiler.data_assistant_result import (
     DataAssistantResult,
@@ -61,3 +63,40 @@ def test_onboarding_data_assistant_runner_top_level_kwargs_behavior(
         if domain["domain_type"] == MetricDomainTypes.COLUMN
     }
     assert not any([column_name.endswith("ID") for column_name in columns_used])
+
+
+@pytest.mark.integration
+def test_onboarding_data_assistant_runner_top_level_kwargs_explicit_none(
+    data_context_with_datasource_pandas_engine,
+):
+    context: DataContext = data_context_with_datasource_pandas_engine
+
+    df = pd.DataFrame(
+        {
+            "user_id": [1, 2, 3, 4, 5, 6],
+            "total_spend": [131.24, 42.21, 516.55, 0.00, 12351.92, 0.52],
+        }
+    )
+
+    batch_request = RuntimeBatchRequest(
+        datasource_name="my_datasource",
+        data_connector_name="default_runtime_data_connector_name",
+        data_asset_name="users_df",
+        runtime_parameters={"batch_data": df},
+        batch_identifiers={"default_identifier_name": "onboarding_test"},
+    )
+
+    # Ensure that user_id is still excluded from categorical_columns_rule when we pass an explicit None to
+    # exclude_column_name_suffixes, because CategoricalColumnBuilder defaults will continue to be respected.
+    data_assistant_result: DataAssistantResult = context.assistants.onboarding.run(
+        batch_request=batch_request,
+        exclude_column_name_suffixes=None,
+    )
+    columns_used = {
+        domain["domain_kwargs"]["column"]
+        for domain in data_assistant_result.metrics_by_domain.keys()
+        if domain["domain_type"] == MetricDomainTypes.COLUMN
+        and domain["rule_name"] == "categorical_columns_rule"
+    }
+
+    assert "user_id" not in columns_used
