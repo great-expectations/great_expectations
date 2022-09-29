0e04fdd30fe8d64c12440cfb89918a972b94732c
Chetan Kini <chetan@superconductive.com>
2022-09-29 10:48:59 -0400

    chore: revert test changes

--- tests/integration/docusaurus/tutorials/getting-started/getting_started.py
+++ tests/integration/docusaurus/tutorials/getting-started/getting_started.py
@@ -20,7 +20,7 @@ assert context
 
 # First configure a new Datasource and add to DataContext
 
-# <snippet name="getting_started_datasource_yaml">
+# <snippet>
 datasource_yaml = f"""
 name: getting_started_datasource
 class_name: Datasource
