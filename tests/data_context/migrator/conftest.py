49a8f817b78422b3fe9c629325314bc813ecbf29
Chetan Kini <chetan@superconductive.com>
2022-09-29 14:27:56 -0400

    chore: update tests

--- tests/data_context/migrator/conftest.py
+++ tests/data_context/migrator/conftest.py
@@ -84,6 +84,7 @@ class StubBaseDataContext:
         datasource_names: Tuple[Optional[str]] = ("my_datasource",),
     ):
         """Set the configuration of the stub data context.
+
         Args:
             anonymized_usage_statistics_config: Config to use for anonymous usage statistics
         """
@@ -203,12 +204,82 @@ def empty_serialized_configuration_bundle() -> dict:
 
 
 @pytest.fixture
+def serialized_configuration_bundle() -> dict:
+    return {
+        "data_context_id": "877166bd-08f2-4d7b-b473-a2b97ab5e36f",
+        "checkpoints": [
+            {
+                "class_name": "Checkpoint",
+                "config_version": None,
+                "module_name": "great_expectations.checkpoint",
+                "name": "my_checkpoint",
+            }
+        ],
+        "data_context_variables": {
+            "config_variables_file_path": None,
+            "config_version": 3.0,
+            "data_docs_sites": None,
+            "evaluation_parameter_store_name": None,
+            "expectations_store_name": None,
+            "include_rendered_content": {
+                "expectation_suite": False,
+                "expectation_validation_result": False,
+                "globally": False,
+            },
+            "notebooks": None,
+            "plugins_directory": None,
+            "stores": None,
+            "validations_store_name": None,
+        },
+        "datasources": [
+            {
+                "class_name": "Datasource",
+                "data_connectors": {},
+                "execution_engine": {
+                    "class_name": "PandasExecutionEngine",
+                    "module_name": "great_expectations.execution_engine",
+                },
+                "module_name": "great_expectations.datasource",
+                "name": "my_datasource",
+            }
+        ],
+        "expectation_suites": [
+            {
+                "data_asset_type": None,
+                "expectation_suite_name": "my_suite",
+                "expectations": [],
+                "ge_cloud_id": None,
+            }
+        ],
+        "profilers": [
+            {
+                "class_name": "RuleBasedProfiler",
+                "config_version": 1.0,
+                "module_name": "great_expectations.rule_based_profiler",
+                "name": "my_profiler",
+                "rules": {},
+                "variables": {},
+            }
+        ],
+        "validation_results": {
+            "some_key": {
+                "evaluation_parameters": {},
+                "meta": {},
+                "results": [],
+                "statistics": {},
+                "success": True,
+            }
+        },
+    }
+
+
+@pytest.fixture
 def stub_serialized_configuration_bundle(
-    empty_serialized_configuration_bundle: dict,
+    serialized_configuration_bundle: dict,
 ) -> dict:
     """Configuration bundle based on StubBaseDataContext."""
-    assert "data_context_id" in empty_serialized_configuration_bundle
-    empty_serialized_configuration_bundle[
+    assert "data_context_id" in serialized_configuration_bundle
+    serialized_configuration_bundle[
         "data_context_id"
     ] = StubBaseDataContext.DATA_CONTEXT_ID
-    return empty_serialized_configuration_bundle
+    return serialized_configuration_bundle
