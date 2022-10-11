3d54c7a51accd910fcc85940dd6422146536b317
Gabriel Gore <gabriel59kg@gmail.com>
2022-10-11 12:37:23 -0400

    more realistic plugin import example

--- great_expectations/zep/bootstrap_plugins.py
+++ great_expectations/zep/bootstrap_plugins.py
@@ -2,12 +2,14 @@
 POC for importing and bootstrapping context.sources/Datasource with all it's registered methods via plugins.
 """
 import importlib
+import inspect
+import pathlib
 import pkgutil
 from pprint import pformat as pf
+from types import ModuleType
+from typing import List, Type
 
-from great_expectations.zep.core import PandasDatasource, SQLDatasource
-from great_expectations.zep import gx_sql
-from typing import Callable, List
+from great_expectations.zep.core import PandasDatasource
 
 
 def dir_public(o) -> List[str]:
@@ -38,23 +40,43 @@ class DataContext:
         return self._sources
 
 
+def find_plugins() -> List[str]:
+    # NOTE: actual implementation would use `pkgutil.iter_modules` on sys.path to look for any installed package
+    # that matches our plugin convention.
+    parent_dir = pathlib.Path(__file__).parent
+
+    found_plugins = []
+    for _, name, is_package in pkgutil.iter_modules([str(parent_dir)]):
+        # print(f"  {name} - {is_package}")
+        if name.startswith("gx_"):
+            found_plugins.append(name)
+    return found_plugins
+
+
+def set_methods_from_module(class_: Type, module: ModuleType) -> List:
+    module_functions = inspect.getmembers(module, inspect.isfunction)
+
+    added_methods = []
+    for name, method in module_functions:
+        if name.startswith("_"):
+            print(f"skipping private method {name}")
+            continue
+        setattr(class_, name, method)
+        added_methods.append(name)
+    return added_methods
+
+
 def bootstrap_sources():
     print(f"Before: {dir_public(Datasources)}")
 
-    pkgs = [
-        f"{name} - {ispkg}"
-        for _, name, ispkg in pkgutil.iter_modules()
-        # if name.startswith("gx_")
-    ]
-    print(f"packages: \n{pf(pkgs)}")
+    plugin_pkgs = find_plugins()
+    imported_plugins = {name: importlib.import_module(name) for name in plugin_pkgs}
+    print(f"\nplugins: \n{imported_plugins}\n")
 
-    # discovered_plugins = {
-    #     name: importlib.import_module(name)
-    #     for name in pkgs
-    # }
-    # print(f"plugins: \n{discovered_plugins}")
+    for module in imported_plugins.values():
+        set_methods_from_module(Datasources, module)
 
-    print(f"After: {dir_public(Datasources)}")
+    print(f"After: {dir_public(Datasources)}\n")
 
 
 def get_context() -> DataContext:
