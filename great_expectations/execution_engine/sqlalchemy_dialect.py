f8631d14ea6ee1c7f68932e52baa66057ccdfce4
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2022-10-13 09:02:23 -0400

    [BUGFIX] Regression due to `GESqlDialect` `Enum` for Hive (#6149)

--- great_expectations/execution_engine/sqlalchemy_dialect.py
+++ great_expectations/execution_engine/sqlalchemy_dialect.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import enum
+from enum import Enum
 from typing import Any, List, Union
 
 
-class GESqlDialect(enum.Enum):
+class GESqlDialect(Enum):
     """Contains sql dialects that have some level of support in Great Expectations.
     Also contains an unsupported attribute if the dialect is not in the list.
     """
@@ -24,9 +24,12 @@ class GESqlDialect(enum.Enum):
     TRINO = "trino"
     OTHER = "other"
 
-    def __eq__(self, other: Union[str, GESqlDialect]):
+    def __eq__(self, other: Union[str, bytes, GESqlDialect]):
         if isinstance(other, str):
             return self.value.lower() == other.lower()
+        # Comparison against byte string, e.g. `b"hive"` should be treated as unicode
+        elif isinstance(other, bytes):
+            return self.value.lower() == other.lower().decode("utf-8")
         return self.value.lower() == other.value.lower()
 
     def __hash__(self: GESqlDialect):
--- tests/execution_engine/test_sqlalchemy_dialect.py
+++ tests/execution_engine/test_sqlalchemy_dialect.py
@@ -14,6 +14,16 @@ def test_dialect_instantiation_with_byte_string():
 
 
 @pytest.mark.unit
+def test_string_equivalence():
+    assert GESqlDialect.HIVE == "hive"
+
+
+@pytest.mark.unit
+def test_byte_string_equivalence():
+    assert GESqlDialect.HIVE == b"hive"
+
+
+@pytest.mark.unit
 def test_get_all_dialect_names_no_other_dialects():
     assert GESqlDialect.OTHER.value not in GESqlDialect.get_all_dialect_names()
 
