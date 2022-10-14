1520927292ad643794257c3def7d93ece4634082
Gabriel Gore <gabriel59kg@gmail.com>
2022-10-14 10:12:19 -0400

    metaclass MVP example

--- great_expectations/zep/metaclass_poc/metaclass_poc.py
+++ great_expectations/zep/metaclass_poc/metaclass_poc.py
@@ -1,13 +1,16 @@
 """
 POC for dynamically bootstrapping context.sources/Datasource with all it's registered methods.
 """
-from typing import List, Callable
+from __future__ import annotations
 
-from great_expectations.zep.core import PandasDatasource, Datasource
+from typing import Callable, List
+
+from great_expectations.zep.core import Datasource, PandasDatasource
 
 
-def dir_public(o) -> List[str]:
-    return [x for x in dir(o) if not x.startswith("_")]
+def _camel_to_snake(s: str) -> str:
+    # https://stackoverflow.com/a/44969381/6304433
+    return "".join(["_" + c.lower() if c.isupper() else c for c in s]).lstrip("_")
 
 
 SourceFactoryFn = Callable[..., Datasource]
@@ -24,10 +27,16 @@ class _SourceFactories:
     @classmethod
     def add_factory(cls, name: str, fn: SourceFactoryFn) -> None:
         """Add/Register a datasource factory function."""
+        print(f"2. Adding {name} factory")
         prexisting = cls.__sources.pop(name, None)
         if not prexisting:
             cls.__sources[name] = fn
-        raise ValueError(f"{name} already exists")
+        else:
+            raise ValueError(f"{name} already exists")
+
+    @property
+    def factories(self) -> List[str]:
+        return list(self.__sources.keys())
 
     def __getattr__(self, name):
         return self.__sources[name]
@@ -35,6 +44,27 @@ class _SourceFactories:
     def __dir__(self) -> List[str]:
         # TODO: update to work for standard methods too
         return [k for k in self.__sources.keys()]
+
+
+class MetaDatasouce(type):
+    def __new__(meta_cls, cls_name, bases, cls_dict) -> MetaDatasouce:
+        print(f"1. Hello {meta_cls.__name__} __new__() -> {cls_name}")
+
+        def _datasource_factory(name: str) -> Datasource:
+            print(f"5. Adding `{name}` {cls_name}")
+            return type(
+                cls_name, bases, cls_dict
+            )  # should this return `super().__new__()` instead?
+
+        sources = _SourceFactories()
+        factory_name = f"add_{_camel_to_snake(cls_name)}"
+        sources.add_factory(factory_name, _datasource_factory)
+
+        return super().__new__(meta_cls, cls_name, bases, cls_dict)
+
+
+class PostgresDatasource(metaclass=MetaDatasouce):
+    pass
 
 
 class DataContext:
@@ -49,6 +79,7 @@ class DataContext:
 
     def __init__(self) -> None:
         self._sources: _SourceFactories = _SourceFactories()
+        print(f"4. Available Factories - {self._sources.factories}")
 
     @property
     def sources(self) -> _SourceFactories:
@@ -56,6 +87,7 @@ class DataContext:
 
 
 def get_context() -> DataContext:
+    print("3. Getting context")
     context = DataContext.get_context()
     return context
 
@@ -63,3 +95,4 @@ def get_context() -> DataContext:
 if __name__ == "__main__":
     context = get_context()
     context.sources.add_pandas("taxi")
+    context.sources.add_postgres_datasource("taxi_pg")
