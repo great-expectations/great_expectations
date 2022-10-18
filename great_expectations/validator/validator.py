4a1592250c06266485a5afdf2a50d8c9179a2889
Alex Sherstinsky <alex@superconductive.com>
2022-10-17 20:19:44 -0700

    cleanup

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -90,7 +90,7 @@ except ImportError:
     )
 
 if TYPE_CHECKING:
-    from great_expectations.data_context.data_context import DataContext
+    from great_expectations.data_context.data_context import AbstractDataContext
 
 ValidationStatistics = namedtuple(
     "ValidationStatistics",
@@ -119,7 +119,7 @@ class Validator:
         interactive_evaluation: bool = True,
         expectation_suite: Optional[ExpectationSuite] = None,
         expectation_suite_name: Optional[str] = None,
-        data_context: Optional[DataContext] = None,
+        data_context: Optional[AbstractDataContext] = None,
         batches: Optional[List[Batch]] = None,
         include_rendered_content: Optional[bool] = None,
         **kwargs,
@@ -139,7 +139,7 @@ class Validator:
             batches: The batches for which to validate.
             include_rendered_content: Whether or not to include rendered_content in the ExpectationValidationResult.
         """
-        self._data_context: Optional[DataContext] = data_context
+        self._data_context: Optional[AbstractDataContext] = data_context
 
         self._metrics_calculator: MetricsCalculator = MetricsCalculator(
             execution_engine=execution_engine,
@@ -183,7 +183,7 @@ class Validator:
         return self._metrics_calculator
 
     @property
-    def data_context(self) -> Optional[DataContext]:
+    def data_context(self) -> Optional[AbstractDataContext]:
         """Reference to DataContext object handle."""
         return self._data_context
 
