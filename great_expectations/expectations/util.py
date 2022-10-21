f810f6ccbb54c562bcf812516ee484dfb7ece784
Nathan Farmer <nathan@greatexpectations.io>
2022-10-21 10:41:42 -0400

    Deprecate add_values_with_json_schema_from_list_in_params

--- great_expectations/expectations/util.py
+++ great_expectations/expectations/util.py
@@ -1,9 +1,32 @@
+import warnings
+
 from great_expectations.expectations.expectation import (
-    add_values_with_json_schema_from_list_in_params,
+    add_values_with_json_schema_from_list_in_params as add_values_with_json_schema_from_list_in_params_expectation,
+)
+from great_expectations.expectations.expectation import (
     render_evaluation_parameter_string,
 )
 
-add_values_with_json_schema_from_list_in_params = (
-    add_values_with_json_schema_from_list_in_params
-)
+
+def add_values_with_json_schema_from_list_in_params(
+    params: dict,
+    params_with_json_schema: dict,
+    param_key_with_list: str,
+    list_values_type: str = "string",
+) -> dict:
+    # deprecated-v0.15.29
+    warnings.warn(
+        """The module great_expectations.expectations.util.py is deprecated as of v0.15.29 in v0.18. Please import \
+method add_values_with_json_schema_from_list_in_params from great_expectations.expectations.expectation.
+""",
+        DeprecationWarning,
+    )
+    return add_values_with_json_schema_from_list_in_params_expectation(
+        params=params,
+        params_with_json_schema=params_with_json_schema,
+        param_key_with_list=param_key_with_list,
+        list_values_type=list_values_type,
+    )
+
+
 render_evaluation_parameter_string = render_evaluation_parameter_string
