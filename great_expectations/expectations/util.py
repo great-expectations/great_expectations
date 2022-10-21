55c9c64d99bddf00efedeb9b8383aa1521fa9dad
Nathan Farmer <nathan@greatexpectations.io>
2022-10-21 10:46:05 -0400

    Deprecate render_evalutation_parameter_string

--- great_expectations/expectations/util.py
+++ great_expectations/expectations/util.py
@@ -1,10 +1,11 @@
 import warnings
+from typing import Callable
 
 from great_expectations.expectations.expectation import (
     add_values_with_json_schema_from_list_in_params as add_values_with_json_schema_from_list_in_params_expectation,
 )
 from great_expectations.expectations.expectation import (
-    render_evaluation_parameter_string,
+    render_evaluation_parameter_string as render_evaluation_parameter_string_expectation,
 )
 
 
@@ -29,4 +30,12 @@ method add_values_with_json_schema_from_list_in_params from great_expectations.e
     )
 
 
-render_evaluation_parameter_string = render_evaluation_parameter_string
+def render_evaluation_parameter_string(render_func) -> Callable:
+    # deprecated-v0.15.29
+    warnings.warn(
+        """The module great_expectations.expectations.util.py is deprecated as of v0.15.29 in v0.18. Please import \
+decorator render_evaluation_parameter_string from great_expectations.expectations.expectation.
+""",
+        DeprecationWarning,
+    )
+    return render_evaluation_parameter_string_expectation(render_func=render_func)
