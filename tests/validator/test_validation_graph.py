28627ef2d8186a2dc019e18f9889710c01fb09d0
Alex Sherstinsky <alex@greatexpectations.io>
2022-10-24 08:30:04 -0700

    clean up

--- tests/validator/test_validation_graph.py
+++ tests/validator/test_validation_graph.py
@@ -104,58 +104,6 @@ def expect_column_value_z_scores_to_be_less_than_expectation_validation_graph():
     return graph
 
 
-@pytest.mark.parametrize(
-    "left_fixture_name,right_fixture_name,id",
-    [
-        pytest.param(
-            "table_head_metric_config",
-            None,
-            (
-                (
-                    "table.head",
-                    "batch_id=abc123",
-                    "n_rows=5",
-                ),
-                None,
-            ),
-        ),
-        pytest.param(
-            "table_head_metric_config",
-            "column_histogram_metric_config",
-            (
-                (
-                    "table.head",
-                    "batch_id=abc123",
-                    "n_rows=5",
-                ),
-                (
-                    "column.histogram",
-                    "batch_id=def456",
-                    "bins=5",
-                ),
-            ),
-        ),
-    ],
-)
-@pytest.mark.unit
-def test_MetricEdge_init(
-    left_fixture_name: str,
-    right_fixture_name: Optional[str],
-    id: tuple,
-    request,
-) -> None:
-    left: MetricConfiguration = request.getfixturevalue(left_fixture_name)
-    right: Optional[MetricConfiguration] = None
-    if right_fixture_name:
-        right = request.getfixturevalue(right_fixture_name)
-
-    edge = MetricEdge(left=left, right=right)
-
-    assert edge.left == left
-    assert edge.right == right
-    assert edge.id == id
-
-
 @pytest.mark.unit
 def test_ValidationGraph_init_no_input_edges() -> None:
     graph = ValidationGraph()
@@ -186,21 +134,6 @@ def test_ValidationGraph_add(metric_edge: MetricEdge) -> None:
 
     assert graph.edges == [metric_edge]
     assert metric_edge.id in graph.edge_ids
-
-
-@pytest.mark.unit
-def test_ExpectationValidationGraph_constructor(
-    expect_column_values_to_be_unique_expectation_config: ExpectationConfiguration,
-    expect_column_values_to_be_unique_expectation_validation_graph: ExpectationValidationGraph,
-) -> None:
-    assert (
-        expect_column_values_to_be_unique_expectation_validation_graph.configuration
-        == expect_column_values_to_be_unique_expectation_config
-    )
-    assert (
-        expect_column_values_to_be_unique_expectation_validation_graph.graph.__dict__
-        == ValidationGraph().__dict__
-    )
 
 
 @pytest.mark.unit
