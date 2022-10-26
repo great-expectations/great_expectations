a091c5d81dc4fd5177a4df681c57a37b09b51aa8
Nathan Farmer <nathan@greatexpectations.io>
2022-10-26 19:30:25 -0400

    Update tests

--- tests/datasource/data_connector/test_sql_data_connector.py
+++ tests/datasource/data_connector/test_sql_data_connector.py
@@ -2409,7 +2409,7 @@ def test_include_schema_name_introspection_postgres(postgresql_sqlalchemy_dataso
         for table in my_data_connector._introspect_db()
         if table["type"] == "table"
     ]
-    assert len(introspected_tables) == 151
+    assert len(introspected_tables) == 152
 
     introspected_schemas = {table.get("schema_name") for table in introspected_tables}
     assert introspected_schemas == {"connection_test", "public"}
@@ -2443,7 +2443,7 @@ def test_include_schema_name_introspection_mysql(mysql_sqlalchemy_datasource):
         for table in my_data_connector._introspect_db()
         if table["type"] == "table"
     ]
-    assert len(introspected_tables) == 2
+    assert len(introspected_tables) == 145
 
     introspected_schemas = {table.get("schema_name") for table in introspected_tables}
     assert introspected_schemas == {"test_ci", "test_connection"}
