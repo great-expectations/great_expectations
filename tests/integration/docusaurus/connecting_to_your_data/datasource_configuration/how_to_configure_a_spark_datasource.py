b9998dde1d3405c4ddbadee5f60f5c47ec636ee1
Rachel-Reverie <rachel@superconductive.com>
2022-10-28 10:11:55 -0500

    DOC-379 - separates out the utility methods for testing if nested dictionary keys and values of one dictionary are a subset of another, since this will be used in multiple testing files. - separates out the configuration snippets that are identical between Spark, Pandas, and SQL Datasources so that they are not tested redundantly. - starts the spark datasource example script with necessary docstring and imports

--- tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
+++ tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
@@ -11,3 +11,13 @@ the snippets that are specified for use in documentation are maintained.  These 
 --documentation--
     https://docs.greatexpectations.io/docs/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource
 """
+
+# The following imports are used as part of verifying that all example snippets are consistent.
+# Users may disregard them.
+
+from datasource_configuration_test_utilities import is_subset
+from how_to_configure_a_datasource_universal_steps import (
+    get_full_universal_datasource_config_elements,
+)
+
+import great_expectations as gx
