648e90975661c1ad02f2d59f8db65d5f9cd45a2f
Rachel-Reverie <rachel@superconductive.com>
2022-10-28 07:47:22 -0500

    DOC-379 - Adds module level docstring for spark datasource example script. - Corrects docstring for pandas datasource example script.

---
+++ tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
@@ -0,0 +1,13 @@
+"""Example Script: How to configure a Spark Datasource
+
+This example script is intended for use in documentation on how to configure a Spark Datasource.
+
+Assert statements are included to ensure that if the behaviour shown in this script breaks it will not pass
+tests and will be updated.  These statements can be ignored by users.
+
+Comments with the tags `<snippet>` and `</snippet>` are used to ensure that if this script is updated
+the snippets that are specified for use in documentation are maintained.  These comments can be ignored by users.
+
+--documentation--
+    https://docs.greatexpectations.io/docs/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource
+"""
