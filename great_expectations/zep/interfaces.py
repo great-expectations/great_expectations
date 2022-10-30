46911e63ce631ddd9a75a11d5ee0115925516adc
Gabriel <gabriel59kg@gmail.com>
2022-10-29 21:49:21 -0400

    [MAINTENANCE] ZEP POC - Asset Type Registration (#6194)

--- great_expectations/zep/interfaces.py
+++ great_expectations/zep/interfaces.py
@@ -1,14 +1,25 @@
-from typing import List, Union
+import abc
+from typing import Dict, List, Type, Union
 
-from typing_extensions import Protocol
+from typing_extensions import Protocol, runtime_checkable
 
 from great_expectations.core.batch import Batch, BatchRequest, RuntimeBatchRequest
 from great_expectations.execution_engine import ExecutionEngine
 
 
+class DataAsset(abc.ABC):
+    name: str
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+@runtime_checkable
 class Datasource(Protocol):
     execution_engine: ExecutionEngine
-    asset_types: List[type]
+    asset_types: List[Type[DataAsset]]
+    name: str
+    assets: Dict[str, DataAsset]
 
     def get_batch_list_from_batch_request(
         self, batch_request: Union[BatchRequest, RuntimeBatchRequest]
@@ -21,3 +32,8 @@ class Datasource(Protocol):
         Returns:
             A list of batches. The list may be empty.
         """
+
+    def get_asset(self, asset_name: str) -> DataAsset:
+        """Returns the DataAsset referred to by name"""
+        # This default implementation will be used if protocol is inherited
+        return self.assets[asset_name]
