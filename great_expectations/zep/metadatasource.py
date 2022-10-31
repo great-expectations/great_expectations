07bb90126ac4237732d247887c3f965f37fbad48
Gabriel Gore <gabriel59kg@gmail.com>
2022-10-31 17:30:04 -0400

    `_recursion_check()`

--- great_expectations/zep/metadatasource.py
+++ great_expectations/zep/metadatasource.py
@@ -16,7 +16,16 @@ SourceFactoryFn = Callable[..., "Datasource"]
 
 LOGGER = logging.getLogger(__name__)
 
-CALLS = 0
+_CALLS = 0
+_MAX_CALLS = 5
+
+
+def _recursion_check(log_message: str):
+    global _CALLS
+    _CALLS += 1
+    print(f"\n{_CALLS} {log_message}")
+    if _CALLS >= _MAX_CALLS:
+        raise RecursionError(f"Calls exceeded {_MAX_CALLS}")
 
 
 class MetaDatasource(type):
@@ -30,15 +39,7 @@ class MetaDatasource(type):
 
         Also binds asset adding methods according to the declared `asset_types`.
         """
-        global CALLS
-        CALLS += 1
-        print(f"\n{CALLS} {bases=} {meta_cls=}")
-        LOGGER.info(f"  {cls_name} __dict__ ->\n{pf(cls_dict, depth=3)}")
-        if CALLS > 5:
-            raise NotImplementedError
-
-        if not bases:
-            return super().__new__(meta_cls, cls_name, bases, cls_dict)
+        _recursion_check(f"{cls_name}\n  bases={bases}\n  meta_cls={meta_cls.__name__}")
 
         LOGGER.info(f"1a. {meta_cls.__name__}.__new__() for `{cls_name}`")
 
@@ -49,6 +50,7 @@ class MetaDatasource(type):
 
         # TODO: fix max recursion error that happens here
         # Bad solution - remove the base, extract & merge the __dict__ methods from the base
+        # cls = type(cls_name, (), cls_dict)
         cls = type(cls_name, bases, cls_dict)
         LOGGER.debug(f"  {cls_name} __dict__ ->\n{pf(cls.__dict__, depth=3)}")
 
