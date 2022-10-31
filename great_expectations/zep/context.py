3639d1d7a207d823415e2c2076147492ede8a410
Bill Dirks <bill@superconductive.com>
2022-10-31 14:47:17 -0700

    Annotate class and instance variables in zep DataContext.

--- great_expectations/zep/context.py
+++ great_expectations/zep/context.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from pprint import pformat as pf
-from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Type, Union
+from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Type, Union, ClassVar
 
 from great_expectations.util import camel_to_snake
 from great_expectations.zep.interfaces import DataAsset, Datasource
@@ -133,7 +133,8 @@ class DataContext:
     Use `great_expectations.get_context()` for a real DataContext.
     """
 
-    _context: DataContext = None
+    _context: ClassVar[DataContext] = None
+    _datasources: Dict[str, Datasource]
 
     @classmethod
     def get_context(cls) -> DataContext:
