5f0b0b419411ef57db4be71e635c82a57ca07ae4
Gabriel Gore <gabriel59kg@gmail.com>
2022-11-03 23:36:26 -0400

    basic config loading

---
+++ great_expectations/zep/config.py
@@ -0,0 +1,28 @@
+"""POC for loading config."""
+from __future__ import annotations
+
+import logging
+import pathlib
+from pprint import pformat as pf
+from typing import Dict, Union
+
+from pydantic import BaseModel
+from ruamel.yaml import YAML
+
+from great_expectations.zep import interfaces
+
+yaml = YAML(typ="safe")
+
+Datasource = interfaces.Datasource.Datasource
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GxConfig(BaseModel):
+    datasources: Dict[str, Datasource]
+
+    @classmethod
+    def parse_yaml(cls, f: Union[pathlib.Path, str]) -> GxConfig:
+        loaded = yaml.load(f)
+        LOGGER.info(f"loaded from yaml ->\n{pf(loaded)}\n")
+        return cls(**loaded)
