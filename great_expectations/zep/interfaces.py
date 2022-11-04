4a0f2730f0faa2b32d4578198334ae127485103a
Gabriel Gore <gabriel59kg@gmail.com>
2022-11-03 23:21:27 -0400

    temp Datasource model

--- great_expectations/zep/interfaces.py
+++ great_expectations/zep/interfaces.py
@@ -1,21 +1,42 @@
 # import abc
 from typing import Dict, List, Type, Union
 
-from pydantic import BaseModel
-from typing_extensions import ClassVar
+from pydantic import BaseModel, constr, validator
+from typing_extensions import ClassVar, TypeAlias
 
 from great_expectations.core.batch import Batch, BatchRequest, RuntimeBatchRequest
 from great_expectations.execution_engine import ExecutionEngine
 from great_expectations.zep.metadatasource import MetaDatasource
 
+LowerStr: TypeAlias = constr(to_lower=True, strict=True)  # type: ignore[misc]
+
 
 class DataAsset(BaseModel):
     name: str
+    type: LowerStr
 
 
+# TODO: resolve metaclass conflict with pydantic BaseModel
 class Datasource(metaclass=MetaDatasource):
     # class attrs
     asset_types: ClassVar[List[Type[DataAsset]]] = []
+
+    class Datasource(BaseModel):
+        name: str
+        engine: str
+        execution_engine: ExecutionEngine
+        assets: Dict[str, DataAsset]
+
+        @validator("engine", pre=True)
+        def load_execution_engine(cls, v, values):
+            print("\n\nvalidating engine")
+            print(v)
+            print(values)
+            return v
+
+        class Config:
+            # TODO: revisit this
+            arbitrary_types_allowed = True
 
     # instance attrs
     name: str
