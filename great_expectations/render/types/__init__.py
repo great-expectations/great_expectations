9e77c94dca9697185d407c1233c7dbe7c9e49a7c
Nathan Farmer <nathan@greatexpectations.io>
2022-11-07 12:25:55 -0500

    Add deprecation warning if class is imported from types package

--- great_expectations/render/types/__init__.py
+++ great_expectations/render/types/__init__.py
@@ -1,7 +1,21 @@
+import warnings
 from copy import deepcopy
 from string import Template as pTemplate
 
+from great_expectations.render import (
+    RenderedAtomicContent as rendered_atomic_content_render,
+)
 from great_expectations.render.exceptions import InvalidRenderedContentError
+
+
+class RenderedAtomicContent(rendered_atomic_content_render):
+    # deprecated-v0.15.32
+    warnings.warn(
+        """Importing the class RenderedAtomicContent from great_expectations.render.types is deprecated as of v0.15.32\
+in v0.18. Please import class RenderedAtomicContent from great_expectations.render.
+""",
+        DeprecationWarning,
+    )
 
 
 class RenderedContent:
