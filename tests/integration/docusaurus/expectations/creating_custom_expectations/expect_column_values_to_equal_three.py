b93b85074c29ee589dacb959fc3fdf45c07cccec
Nathan Farmer <nathan@greatexpectations.io>
2022-11-08 13:08:45 -0500

    Deal with merge conflict

--- tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_equal_three.py
+++ tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_equal_three.py
@@ -28,9 +28,6 @@ from great_expectations.render.util import (
     handle_strict_min_max,
     parse_row_condition_string_pandas_engine,
     substitute_none_for_missing,
-from great_expectations.render import (
-    CollapseContent,
-    RenderedStringTemplateContent,
 )
 from great_expectations.validator.metric_configuration import MetricConfiguration
 
