c2dda3f6cdb68b8d243e58b36bfd44eb8d28616a
Anthony Burdi <anthony@greatexpectations.io>
2022-11-08 15:05:54 -0500

    Make assertions about result_format and add types to RunIdentifier init.

--- great_expectations/checkpoint/checkpoint.py
+++ great_expectations/checkpoint/checkpoint.py
@@ -5,6 +5,7 @@ import datetime
 import json
 import logging
 import os
+from types import NoneType
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, cast
 from uuid import UUID
 
@@ -126,6 +127,7 @@ class BaseCheckpoint(ConfigPeer):
         run_time = run_time or datetime.datetime.now()
         runtime_configuration = runtime_configuration or {}
         result_format = result_format or runtime_configuration.get("result_format")
+        assert isinstance(result_format, (NoneType, str, dict))
 
         batch_request = get_batch_request_as_dict(batch_request=batch_request)
         validations = get_validations_with_batch_request_as_dict(
