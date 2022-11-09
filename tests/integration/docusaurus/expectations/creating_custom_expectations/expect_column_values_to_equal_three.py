c3c9f2fa65c9a8c958c2897449044eff56cdbb42
Nathan Farmer <nathan@greatexpectations.io>
2022-11-08 19:16:39 -0500

    Unused imports

--- tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_equal_three.py
+++ tests/integration/docusaurus/expectations/creating_custom_expectations/expect_column_values_to_equal_three.py
@@ -21,9 +21,14 @@ from great_expectations.expectations.metrics import (
     column_condition_partial,
     metric_partial,
 )
-from great_expectations.expectations.metrics.import_manager import F, sa
-from great_expectations.render import CollapseContent, RenderedStringTemplateContent
+from great_expectations.expectations.metrics.import_manager import F
+from great_expectations.render import (
+    CollapseContent,
+    RenderedStringTemplateContent,
+    RenderedTableContent,
+)
 from great_expectations.render.renderer.renderer import renderer
+from great_expectations.render.util import num_to_str
 from great_expectations.validator.metric_configuration import MetricConfiguration
 
 
