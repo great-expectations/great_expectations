c8cddfe0cace83a787257ff3f7827fc4f38219f7
Alex Sherstinsky <alex@greatexpectations.io>
2022-11-14 12:32:25 -0800

    Use MetricValue instead of Any for metric computation value type hint.

--- great_expectations/rule_based_profiler/helpers/util.py
+++ great_expectations/rule_based_profiler/helpers/util.py
@@ -48,6 +48,7 @@ from great_expectations.util import (
     numpy_quantile,
 )
 from great_expectations.validator.metric_configuration import MetricConfiguration
+from great_expectations.validator.resolved_metric import MetricValue
 
 if TYPE_CHECKING:
     from great_expectations.data_context.data_context.abstract_data_context import (
