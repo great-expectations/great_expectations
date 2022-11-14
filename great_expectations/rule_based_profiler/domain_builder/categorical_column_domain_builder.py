4b9bc90a467705d690630a5f0f58c1ee32b54226
Alex Sherstinsky <alex@greatexpectations.io>
2022-11-14 12:28:34 -0800

    Use MetricValue instead of Any for metric computation value type hint.

--- great_expectations/rule_based_profiler/domain_builder/categorical_column_domain_builder.py
+++ great_expectations/rule_based_profiler/domain_builder/categorical_column_domain_builder.py
@@ -21,6 +21,7 @@ from great_expectations.rule_based_profiler.parameter_container import (
     ParameterContainer,
 )
 from great_expectations.validator.metric_configuration import MetricConfiguration
+from great_expectations.validator.resolved_metric import MetricValue
 
 if TYPE_CHECKING:
     from great_expectations.data_context.data_context.abstract_data_context import (
