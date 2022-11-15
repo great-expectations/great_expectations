68b04e081b1b2553771c70e9526491172193d653
Rachel-Reverie <rachel@superconductive.com>
2022-11-15 09:20:45 -0600

    DOC-379 - import assets script snippets

--- tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
+++ tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
@@ -15,6 +15,11 @@ To run this code as a local test, use the following console command:
 ```
 pytest -v --docs-tests -m integration -k "how_to_configure_a_spark_datasource" tests/integration/test_script_runner.py
 ```
+
+To validate the snippets in this file, use the following console command:
+```
+yarn snippet-check ./tests/integration/docusaurus/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource.py
+```
 """
 
 # The following imports are used as part of verifying that all example snippets are consistent.
@@ -138,7 +143,7 @@ def section_7_configure_your_individual_data_connectors__inferred():
     Spark Datasource configuration guide.
 
     """
-    # <script name="spark Datasource configuration up to adding an empty inferred data connector">
+    # <snippet name="spark Datasource configuration up to adding an empty inferred data connector">
     datasource_config: dict = {
         "name": "my_datasource_name",
         "class_name": "Datasource",
@@ -254,7 +259,7 @@ def section_7_configure_your_individual_data_connectors__configured():
     Spark Datasource configuration guide.
 
     """
-    # <script name="spark Datasource configuration up to adding an empty configured data connector">
+    # <snippet name="spark Datasource configuration up to adding an empty configured data connector">
     datasource_config: dict = {
         "name": "my_datasource_name",
         "class_name": "Datasource",
@@ -328,7 +333,7 @@ def section_7_configure_your_individual_data_connectors__runtime():
     Spark Datasource configuration guide.
 
     """
-    # <script name="spark Datasource configuration up to adding an empty runtime data connector">
+    # <snippet name="spark Datasource configuration up to adding an empty runtime data connector">
     datasource_config: dict = {
         "name": "my_datasource_name",
         "class_name": "Datasource",
@@ -678,71 +683,6 @@ def section_9_configure_your_data_connectors_data_assets__inferred__multi_batch(
         },
     }
     # </snippet>
-
-    #     dict = {
-    #     "data_connectors": {
-    #         "name_of_my_inferred_data_connector": {
-    #             "default_regex": {
-    #                 # <snippet name="inferred spark multi batch pattern">
-    #                 "pattern": "(yellow_tripdata_sample_2020)-(\\d.*)\\.csv",
-    #                 # </snippet>
-    #                 # <snippet name="inferred spark multi batch group_names">
-    #                 "group_names": ["data_asset_name", "month"]
-    #                 # </snippet>
-    #             },
-    #         },
-    #     },
-    # }
-    # is_subset(
-    #     single_line_snippets, get_full_config_spark_inferred_datasource_multi_batch()
-    # )
-
-    # data_connector_snippet: dict = {
-    #     "data_connectors": {
-    #
-    #         "name_of_my_inferred_data_connector": {
-    #             "class_name": "InferredAssetFilesystemDataConnector",
-    #             "base_directory": "../data",
-    #             "default_regex": {
-    #                 "pattern": "(yellow_tripdata_sample_2020)-(\\d.*)\\.csv",
-    #                 "group_names": ["data_asset_name", "month"],
-    #             },
-    #         },
-    #         # </snippet>
-    #     },
-    # }
-    # is_subset(
-    #     data_connector_snippet, get_full_config_spark_inferred_datasource_multi_batch()
-    # )
-
-    # # <snippet name="full datasource_config for inferred spark multi batch datasource">
-    # datasource_config: dict = {
-    #     "name": "my_datasource_name",  # Preferably name it something relevant
-    #     "class_name": "Datasource",
-    #     "module_name": "great_expectations.datasource",
-    #     "execution_engine": {
-    #         "class_name": "SparkDFExecutionEngine",
-    #         "module_name": "great_expectations.execution_engine",
-    #     },
-    #     "data_connectors": {
-    #         "name_of_my_inferred_data_connector": {
-    #             "class_name": "InferredAssetFilesystemDataConnector",
-    #             "base_directory": "../data",
-    #             "default_regex": {
-    #                 "pattern": "(yellow_tripdata_sample_2020)-(\\d.*)\\.csv",
-    #                 "group_names": ["data_asset_name", "month"],
-    #             },
-    #             "batch_spec_passthrough": {
-    #                 "reader_method": "csv",
-    #                 "reader_options": {
-    #                     "header": True,
-    #                     "inferSchema": True,
-    #                 },
-    #             },
-    #         }
-    #     },
-    # }
-    # # </snippet>
     is_subset(
         datasource_config, get_full_config_spark_inferred_datasource_multi_batch()
     )
