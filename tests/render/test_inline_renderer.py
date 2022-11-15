a1e3bc3281063b60a188de8c2fbead37c175c55a
Nathan Farmer <nathan@greatexpectations.io>
2022-11-15 12:45:29 -0500

    Add test for expectation confgiuration prescriptive rendered content

--- tests/render/test_inline_renderer.py
+++ tests/render/test_inline_renderer.py
@@ -586,3 +586,352 @@ def test_inline_renderer_expectation_validation_result_serialization(
         actual_serialized_expectation_validation_result_rendered_atomic_content
         == expected_serialized_expectation_validation_result_diagnostic_rendered_atomic_content
     )
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "expectation_configuration,expected_serialized_expectation_configuration_prescriptive_rendered_atomic_content",
+    [
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_table_row_count_to_equal",
+                kwargs={"value": 3},
+            ),
+            [
+                {
+                    "value_type": "StringValueType",
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "header": None,
+                        "template": "Must have exactly $value rows.",
+                        "schema": {"type": "com.superconductive.rendered.string"},
+                        "params": {
+                            "value": {"schema": {"type": "number"}, "value": 3},
+                        },
+                    },
+                }
+            ],
+            id="expect_table_row_count_to_equal",
+        ),
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_column_min_to_be_between",
+                kwargs={"column": "event_type", "min_value": 3, "max_value": 20},
+            ),
+            [
+                {
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "header": None,
+                        "params": {
+                            "column": {
+                                "schema": {"type": "string"},
+                                "value": "event_type",
+                            },
+                            "condition_parser": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "max_value": {"schema": {"type": "number"}, "value": 20},
+                            "min_value": {"schema": {"type": "number"}, "value": 3},
+                            "parse_strings_as_datetimes": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                            "row_condition": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "strict_max": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                            "strict_min": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                        },
+                        "schema": {"type": "com.superconductive.rendered.string"},
+                        "template": "$column minimum value must be greater than or equal "
+                        "to $min_value and less than or equal to $max_value.",
+                    },
+                    "value_type": "StringValueType",
+                }
+            ],
+            id="expect_column_min_to_be_between",
+        ),
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_column_quantile_values_to_be_between",
+                kwargs={
+                    "column": "user_id",
+                    "quantile_ranges": {
+                        "quantiles": [0.0, 0.5, 1.0],
+                        "value_ranges": [
+                            [300000, 400000],
+                            [2000000, 4000000],
+                            [4000000, 10000000],
+                        ],
+                    },
+                },
+            ),
+            [
+                {
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "header": {
+                            "schema": {"type": "StringValueType"},
+                            "value": {
+                                "params": {
+                                    "column": {
+                                        "schema": {"type": "string"},
+                                        "value": "user_id",
+                                    },
+                                    "condition_parser": {
+                                        "schema": {"type": "string"},
+                                        "value": None,
+                                    },
+                                    "mostly": {
+                                        "schema": {"type": "number"},
+                                        "value": None,
+                                    },
+                                    "row_condition": {
+                                        "schema": {"type": "string"},
+                                        "value": None,
+                                    },
+                                },
+                                "template": "$column quantiles must be within "
+                                "the following value ranges.",
+                            },
+                        },
+                        "header_row": [
+                            {"schema": {"type": "string"}, "value": "Quantile"},
+                            {"schema": {"type": "string"}, "value": "Min Value"},
+                            {"schema": {"type": "string"}, "value": "Max Value"},
+                        ],
+                        "schema": {"type": "TableType"},
+                        "table": [
+                            [
+                                {"schema": {"type": "string"}, "value": "0.00"},
+                                {"schema": {"type": "number"}, "value": 300000},
+                                {"schema": {"type": "number"}, "value": 400000},
+                            ],
+                            [
+                                {"schema": {"type": "string"}, "value": "Median"},
+                                {"schema": {"type": "number"}, "value": 2000000},
+                                {"schema": {"type": "number"}, "value": 4000000},
+                            ],
+                            [
+                                {"schema": {"type": "string"}, "value": "1.00"},
+                                {"schema": {"type": "number"}, "value": 4000000},
+                                {"schema": {"type": "number"}, "value": 10000000},
+                            ],
+                        ],
+                    },
+                    "value_type": "TableType",
+                }
+            ],
+            id="expect_column_quantile_values_to_be_between",
+        ),
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_column_values_to_be_in_set",
+                kwargs={"column": "event_type", "value_set": [19, 22, 73]},
+            ),
+            [
+                {
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "header": None,
+                        "params": {
+                            "column": {
+                                "schema": {"type": "string"},
+                                "value": "event_type",
+                            },
+                            "condition_parser": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "mostly": {"schema": {"type": "number"}, "value": None},
+                            "mostly_pct": {"schema": {"type": "string"}, "value": None},
+                            "parse_strings_as_datetimes": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                            "row_condition": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "v__0": {"schema": {"type": "string"}, "value": 19},
+                            "v__1": {"schema": {"type": "string"}, "value": 22},
+                            "v__2": {"schema": {"type": "string"}, "value": 73},
+                            "value_set": {
+                                "schema": {"type": "array"},
+                                "value": [19, 22, 73],
+                            },
+                        },
+                        "schema": {"type": "com.superconductive.rendered.string"},
+                        "template": "$column values must belong to this set: $v__0 $v__1 "
+                        "$v__2.",
+                    },
+                    "value_type": "StringValueType",
+                }
+            ],
+            id="expect_column_values_to_be_in_set",
+        ),
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_column_kl_divergence_to_be_less_than",
+                kwargs={
+                    "column": "user_id",
+                    "partition_object": {
+                        "values": [2000000, 6000000],
+                        "weights": [0.3, 0.7],
+                    },
+                },
+            ),
+            [
+                {
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "graph": {
+                            "autosize": "fit",
+                            "config": {
+                                "view": {
+                                    "continuousHeight": 300,
+                                    "continuousWidth": 400,
+                                }
+                            },
+                            "encoding": {
+                                "tooltip": [
+                                    {"field": "values", "type": "quantitative"},
+                                    {"field": "fraction", "type": "quantitative"},
+                                ],
+                                "x": {"field": "values", "type": "nominal"},
+                                "y": {"field": "fraction", "type": "quantitative"},
+                            },
+                            "height": 400,
+                            "mark": "bar",
+                            "width": 250,
+                        },
+                        "header": {
+                            "schema": {"type": "StringValueType"},
+                            "value": {
+                                "params": {
+                                    "column": {
+                                        "schema": {"type": "string"},
+                                        "value": "user_id",
+                                    },
+                                    "condition_parser": {
+                                        "schema": {"type": "string"},
+                                        "value": None,
+                                    },
+                                    "mostly": {
+                                        "schema": {"type": "number"},
+                                        "value": None,
+                                    },
+                                    "row_condition": {
+                                        "schema": {"type": "string"},
+                                        "value": None,
+                                    },
+                                    "threshold": {
+                                        "schema": {"type": "number"},
+                                        "value": None,
+                                    },
+                                },
+                                "template": "$column Kullback-Leibler (KL) "
+                                "divergence with respect to the "
+                                "following distribution must be "
+                                "lower than $threshold.",
+                            },
+                        },
+                        "schema": {"type": "GraphType"},
+                    },
+                    "value_type": "GraphType",
+                }
+            ],
+            id="expect_column_kl_divergence_to_be_less_than",
+        ),
+        pytest.param(
+            ExpectationConfiguration(
+                expectation_type="expect_column_values_to_be_between",
+                kwargs={
+                    "column": "event_datetime",
+                    "min_value": {"$PARAMETER": "now() - timedelta(weeks=208)"},
+                    "max_value": {"$PARAMETER": "now() - timedelta(weeks=1)"},
+                },
+            ),
+            [
+                {
+                    "name": AtomicPrescriptiveRendererType.SUMMARY,
+                    "value": {
+                        "header": None,
+                        "params": {
+                            "column": {
+                                "schema": {"type": "string"},
+                                "value": "event_datetime",
+                            },
+                            "condition_parser": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "max_value": {
+                                "schema": {"type": "number"},
+                                "value": {"$PARAMETER": "now() - timedelta(weeks=1)"},
+                            },
+                            "min_value": {
+                                "schema": {"type": "number"},
+                                "value": {"$PARAMETER": "now() - timedelta(weeks=208)"},
+                            },
+                            "mostly": {"schema": {"type": "number"}, "value": None},
+                            "mostly_pct": {"schema": {"type": "string"}, "value": None},
+                            "row_condition": {
+                                "schema": {"type": "string"},
+                                "value": None,
+                            },
+                            "strict_max": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                            "strict_min": {
+                                "schema": {"type": "boolean"},
+                                "value": None,
+                            },
+                        },
+                        "schema": {"type": "com.superconductive.rendered.string"},
+                        "template": "$column values must be greater than or equal to "
+                        "$min_value and less than or equal to $max_value.",
+                    },
+                    "value_type": "StringValueType",
+                }
+            ],
+            id="expect_column_values_to_be_between_evaluation_parameters",
+        ),
+    ],
+)
+def test_inline_renderer_expectation_configuration_serialization(
+    expectation_configuration: ExpectationConfiguration,
+    expected_serialized_expectation_configuration_prescriptive_rendered_atomic_content: dict,
+):
+
+    inline_renderer: InlineRenderer = InlineRenderer(
+        render_object=expectation_configuration
+    )
+
+    expectation_configuration_rendered_atomic_content: List[
+        RenderedAtomicContent
+    ] = inline_renderer.get_rendered_content()
+
+    actual_serialized_expectation_configuration_rendered_atomic_content: List[
+        dict
+    ] = clean_serialized_rendered_atomic_content_graphs(
+        serialized_rendered_atomic_content=[
+            rendered_atomic_content.to_json_dict()
+            for rendered_atomic_content in expectation_configuration_rendered_atomic_content
+        ]
+    )
+
+    assert (
+        actual_serialized_expectation_configuration_rendered_atomic_content
+        == expected_serialized_expectation_configuration_prescriptive_rendered_atomic_content
+    )
