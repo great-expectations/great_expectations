9ddd784c5f9abb0d1e8981cb532d2bbe19ee2f96
Gabriel Gore <gabriel59kg@gmail.com>
2022-11-16 13:14:58 -0500

    use basicConfig defaults

--- great_expectations/experimental/logger.py
+++ great_expectations/experimental/logger.py
@@ -2,4 +2,4 @@ import logging
 
 
 def init_logger(level: int = logging.WARNING):
-    logging.basicConfig(level=level, format="%(levelname)s:%(name)s | %(message)s")
+    logging.basicConfig(level=level)
