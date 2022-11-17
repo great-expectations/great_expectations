cd5cc026046ad9484463c7b1f8cf9af35436abb6
Will Shin <will@superconductive.com>
2022-11-17 10:33:35 -0800

    import

--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -62,6 +62,7 @@ from great_expectations.execution_engine import (
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
 )
+from great_expectations.execution_engine.sparkdf_batch_data import SparkDFBatchData
 from great_expectations.execution_engine.sqlalchemy_batch_data import (
     SqlAlchemyBatchData,
 )
