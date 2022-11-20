25a8f831fc20cfbaef20684d84f57227c5722ca3
Chetan Kini <chetan@superconductive.com>
2022-11-20 10:44:42 -0500

    chore: revert typevar usage

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -155,7 +155,7 @@ logger = logging.getLogger(__name__)
 yaml = YAMLHandler()
 
 
-T = TypeVar("T", bound=Union[dict, list, str])
+T = TypeVar("T", dict, list, str)
 
 
 class AbstractDataContext(ABC):
@@ -3702,30 +3702,23 @@ Generated, evaluated, and stored {total_expectations} Expectations during profil
             input value with all `$` characters replaced with the escape string
         """
         if isinstance(value, dict) or isinstance(value, OrderedDict):
-            return cast(
-                T,
-                {
-                    k: self.escape_all_config_variables(
-                        value=v,
-                        dollar_sign_escape_string=dollar_sign_escape_string,
-                        skip_if_substitution_variable=skip_if_substitution_variable,
-                    )
-                    for k, v in value.items()
-                },
-            )
-
+            return {
+                k: self.escape_all_config_variables(
+                    value=v,
+                    dollar_sign_escape_string=dollar_sign_escape_string,
+                    skip_if_substitution_variable=skip_if_substitution_variable,
+                )
+                for k, v in value.items()
+            }
         elif isinstance(value, list):
-            return cast(
-                T,
-                [
-                    self.escape_all_config_variables(
-                        value=v,
-                        dollar_sign_escape_string=dollar_sign_escape_string,
-                        skip_if_substitution_variable=skip_if_substitution_variable,
-                    )
-                    for v in value
-                ],
-            )
+            return [
+                self.escape_all_config_variables(
+                    value=v,
+                    dollar_sign_escape_string=dollar_sign_escape_string,
+                    skip_if_substitution_variable=skip_if_substitution_variable,
+                )
+                for v in value
+            ]
         if skip_if_substitution_variable:
             if parse_substitution_variable(value) is None:
                 return value.replace("$", dollar_sign_escape_string)
