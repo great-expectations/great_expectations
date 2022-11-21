4ceed3ea42ba685f7f7523eba1128979d531fe72
Nathan Farmer <nathan@greatexpectations.io>
2022-11-21 11:21:00 -0500

    Move everything out of if TYPE_CHECKING block

--- great_expectations/render/renderer_configuration.py
+++ great_expectations/render/renderer_configuration.py
@@ -1,14 +1,10 @@
-from typing import TYPE_CHECKING, Optional
+from typing import List, Optional, Union
 
+from great_expectations.core import (
+    ExpectationConfiguration,
+    ExpectationValidationResult,
+)
 from great_expectations.render.util import substitute_none_for_missing
-
-if TYPE_CHECKING:
-    from typing import List, Union
-
-    from great_expectations.core import (
-        ExpectationConfiguration,
-        ExpectationValidationResult,
-    )
 
 
 class RendererConfiguration:
