f21c92f76c0df7aa6f8c56b61073e7779d9b4375
itaise <itaise@wix.com>
2022-11-21 21:07:46 +0200

    reformat

--- great_expectations/expectations/metrics/query_metrics/query_template_values.py
+++ great_expectations/expectations/metrics/query_metrics/query_template_values.py
@@ -81,9 +81,9 @@ class QueryTemplateValues(QueryMetricProvider):
         metrics: Dict[str, Any],
         runtime_configuration: dict,
     ) -> List[pyspark_sql_Row]:
-        query = metric_value_kwargs.get(
+        query = metric_value_kwargs.get("query") or cls.default_kwarg_values.get(
             "query"
-        ) or cls.default_kwarg_values.get("query")
+        )
 
         df: pyspark_sql_DataFrame
         df, _, _ = execution_engine.get_compute_domain(
