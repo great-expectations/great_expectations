d517cfeaabe7d8ef32f35eec50b39a3d9f74e9d8
Nathan Farmer <nathan@greatexpectations.io>
2022-11-21 11:29:16 -0500

    Put GX objects back into if TYPE_CHECKING

--- great_expectations/render/renderer_configuration.py
+++ great_expectations/render/renderer_configuration.py
@@ -1,10 +1,12 @@
-from typing import List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional, Union
 
-from great_expectations.core import (
-    ExpectationConfiguration,
-    ExpectationValidationResult,
-)
 from great_expectations.render.util import substitute_none_for_missing
+
+if TYPE_CHECKING:
+    from great_expectations.core import (
+        ExpectationConfiguration,
+        ExpectationValidationResult,
+    )
 
 
 class RendererConfiguration:
