6c9161e41d51cd856f60949436a3867120187875
Nathan Farmer <nathan@greatexpectations.io>
2022-11-23 09:22:29 -0500

    Test update one signature

--- great_expectations/expectations/core/expect_column_proportion_of_unique_values_to_be_between.py
+++ great_expectations/expectations/core/expect_column_proportion_of_unique_values_to_be_between.py
@@ -228,7 +228,6 @@ class ExpectColumnProportionOfUniqueValuesToBeBetween(ColumnExpectation):
         cls,
         configuration: Optional[ExpectationConfiguration] = None,
         result: Optional[ExpectationValidationResult] = None,
-        language: Optional[str] = None,
         runtime_configuration: Optional[dict] = None,
         **kwargs,
     ):
