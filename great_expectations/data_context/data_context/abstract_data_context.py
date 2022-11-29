7081172f6771595a214613810095a450e1743d98
Anthony Burdi <anthony@greatexpectations.io>
2022-11-29 18:36:39 -0500

    Documentation link formatting

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -3172,10 +3172,8 @@ Generated, evaluated, and stored {total_expectations} Expectations during profil
         --Public API--
 
         --Documentation--
-
-            https://docs.greatexpectations.io/docs/terms/data_context
-
-            https://docs.greatexpectations.io/docs/guides/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config
+            - https://docs.greatexpectations.io/docs/terms/data_context
+            - https://docs.greatexpectations.io/docs/guides/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config
 
         Args:
             yaml_config: A string containing the yaml config to be tested
