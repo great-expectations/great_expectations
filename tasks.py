a6a4ce8146fb81228101d6fa3ceafc1c81b5e514
Anthony Burdi <anthony@greatexpectations.io>
2022-11-29 19:02:03 -0500

    Use blank index.md

--- tasks.py
+++ tasks.py
@@ -417,8 +417,7 @@ def docs(
 
     static_html_file_path = pathlib.Path(sphinx_api_docs_build_dir) / "html"
     paths = static_html_file_path.glob('**/*.html')
-    files = [p for p in paths if p.is_file() and p.name not in ("genindex.html", "search.html", "taxi.html") and "_static" not in str(p)]
-    print([f.relative_to(static_html_file_path) for f in files])
+    files = [p for p in paths if p.is_file() and p.name not in ("genindex.html", "search.html", "index.html") and "_static" not in str(p)]
 
     # Read with beautiful soup
     # Pull out content between <section> tag
