e8bd35a7bbda70a6905e765a88688bbd49161785
Nathan Farmer <nathan@greatexpectations.io>
2022-12-01 00:30:01 -0500

    Update expect_table_row_count_to_equal

--- great_expectations/expectations/core/expect_column_values_to_be_in_set.py
+++ great_expectations/expectations/core/expect_column_values_to_be_in_set.py
@@ -195,15 +195,8 @@ class ExpectColumnValuesToBeInSet(ColumnMapExpectation):
     @classmethod
     def _atomic_prescriptive_template(
         cls,
-        configuration: Optional[ExpectationConfiguration] = None,
-        result: Optional[ExpectationValidationResult] = None,
-        runtime_configuration: Optional[dict] = None,
+        renderer_configuration: RendererConfiguration,
     ) -> Tuple[str, dict, Union[dict, None]]:
-        renderer_configuration = RendererConfiguration(
-            configuration=configuration,
-            result=result,
-            runtime_configuration=runtime_configuration,
-        )
         renderer_configuration.add_param(
             name="column",
             schema_type=ParamSchemaType.STRING,
