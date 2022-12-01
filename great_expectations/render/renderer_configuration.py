e727b5a2e5a5e7c5b82dd56e2a2334a26154fc29
Nathan Farmer <nathan@greatexpectations.io>
2022-11-30 19:55:07 -0500

    Unused import

--- great_expectations/render/renderer_configuration.py
+++ great_expectations/render/renderer_configuration.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict, Optional, Type, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Dict, Optional, Type, Union
 
 from pydantic import BaseModel, Field, create_model
 
