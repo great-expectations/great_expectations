40cf6c4bec0e1453bb8ba878ffdce675a685b83c
Ken Wade <ken@superconductive.com>
2022-12-02 12:00:35 -0600

    Add copy_s3_old_prod_to_prod.sh and add another stage to manual-staging pipeline

--- assets/scripts/copy_s3_staging_to_prod.sh
+++ assets/scripts/copy_s3_staging_to_prod.sh
@@ -3,5 +3,8 @@
 aws --version
 aws s3 ls s3://superconductive-public/static/gallery/ | grep staging
 
+aws s3 cp s3://superconductive-public/static/gallery/expectation_library_v2.json s3://superconductive-public/static/gallery/expectation_library_v2--old-prod.json
+aws s3 cp s3://superconductive-public/static/gallery/package_manifests.json s3://superconductive-public/static/gallery/package_manifests--old-prod.json
+
 aws s3 cp s3://superconductive-public/static/gallery/expectation_library_v2--staging.json s3://superconductive-public/static/gallery/expectation_library_v2--tmp.json
 aws s3 cp s3://superconductive-public/static/gallery/package_manifests--staging.json s3://superconductive-public/static/gallery/package_manifests--tmp.json
