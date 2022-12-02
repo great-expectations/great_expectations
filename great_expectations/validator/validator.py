7d954499561bba96a218d0aa7918f23cf4fd8f51
Alex Sherstinsky <alex@greatexpectations.io>
2022-12-02 09:58:09 -0800

    clean up

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -1151,9 +1151,6 @@ class Validator:
         expectation_validation_graphs: List[ExpectationValidationGraph],
     ) -> ValidationGraph:
         # Collect edges from all expectation-level sub-graphs and incorporate them under common suite-level graph.
-        if expectation_validation_graphs is None:
-            expectation_validation_graphs = []
-
         expectation_validation_graph: ExpectationValidationGraph
         edges: List[MetricEdge] = list(
             itertools.chain.from_iterable(
@@ -1193,9 +1190,6 @@ class Validator:
         )
 
         # Trace MetricResolutionError occurrences to expectations relying on corresponding malfunctioning metrics.
-        if expectation_validation_graphs is None:
-            expectation_validation_graphs = []
-
         rejected_configurations: List[ExpectationConfiguration] = []
         for expectation_validation_graph in expectation_validation_graphs:
             metric_exception_info: Set[
