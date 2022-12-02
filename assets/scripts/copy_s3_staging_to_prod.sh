7b293591d9710fd0cde5bd00dae80b59304fc38a
Ken Wade <ken@superconductive.com>
2022-12-02 10:52:30 -0600

    Check count of files on s3

--- assets/scripts/copy_s3_staging_to_prod.sh
+++ assets/scripts/copy_s3_staging_to_prod.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 aws --version
+aws s3 ls s3://superconductive-public | wc -l
 aws s3 ls s3://superconductive-public | grep staging
 
 aws s3 cp s3://superconductive-public/expectation_library_v2--staging.json s3://superconductive-public/expectation_library_v2--tmp.json
