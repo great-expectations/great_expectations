2076afc95f02b08fa4fe19d34c3e17c92ba55fb1
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2022-12-05 08:56:26 -0800

    [MAINTENANCE] Move "Domain" to "great_expectations/core" to avoid circular imports; also add MetricConfiguration tests; and other clean up. (#6484)

--- tests/rule_based_profiler/parameter_builder/test_parameter_computations.py
+++ tests/rule_based_profiler/parameter_builder/test_parameter_computations.py
@@ -22,6 +22,7 @@ from great_expectations.rule_based_profiler.helpers.util import (
 # Allowable tolerance for how closely a bootstrap method approximates the sample
 from great_expectations.rule_based_profiler.parameter_builder import (
     MetricMultiBatchParameterBuilder,
+    ParameterBuilder,
 )
 from great_expectations.rule_based_profiler.parameter_container import (
     DOMAIN_KWARGS_PARAMETER_FULLY_QUALIFIED_NAME,
@@ -211,17 +212,19 @@ def test_sanitize_metric_computation(metric_name: str, metric_values_by_batch_id
     replace_nan_with_zero: bool = True
     reduce_scalar_metric: bool = True
 
-    metric_multi_batch_parameter_builder = MetricMultiBatchParameterBuilder(
-        name="my_parameter_builder",
-        metric_name=metric_name,
-        metric_domain_kwargs=DOMAIN_KWARGS_PARAMETER_FULLY_QUALIFIED_NAME,
-        metric_value_kwargs=None,
-        single_batch_mode=False,
-        enforce_numeric_metric=enforce_numeric_metric,
-        replace_nan_with_zero=replace_nan_with_zero,
-        reduce_scalar_metric=reduce_scalar_metric,
-        evaluation_parameter_builder_configs=None,
-        data_context=None,
+    metric_multi_batch_parameter_builder: ParameterBuilder = (
+        MetricMultiBatchParameterBuilder(
+            name="my_parameter_builder",
+            metric_name=metric_name,
+            metric_domain_kwargs=DOMAIN_KWARGS_PARAMETER_FULLY_QUALIFIED_NAME,
+            metric_value_kwargs=None,
+            single_batch_mode=False,
+            enforce_numeric_metric=enforce_numeric_metric,
+            replace_nan_with_zero=replace_nan_with_zero,
+            reduce_scalar_metric=reduce_scalar_metric,
+            evaluation_parameter_builder_configs=None,
+            data_context=None,
+        )
     )
     if metric_name == "my_metric_6":
         with pytest.raises(ge_exceptions.ProfilerExecutionError) as excinfo:
--- tests/rule_based_profiler/parameter_builder/test_value_counts_single_batch_parameter_builder.py
+++ tests/rule_based_profiler/parameter_builder/test_value_counts_single_batch_parameter_builder.py
@@ -3,8 +3,8 @@ from typing import Dict, Optional
 import pytest
 
 from great_expectations import DataContext
+from great_expectations.core.domain import Domain
 from great_expectations.core.metric_domain_types import MetricDomainTypes
-from great_expectations.rule_based_profiler.domain import Domain
 from great_expectations.rule_based_profiler.helpers.util import (
     get_parameter_value_and_validate_return_type,
 )
