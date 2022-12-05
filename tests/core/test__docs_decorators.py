a82b50629b893c8e8889628df784e63f49263a6d
Anthony Burdi <anthony@greatexpectations.io>
2022-12-05 16:33:55 -0500

    Test with no message

--- tests/core/test__docs_decorators.py
+++ tests/core/test__docs_decorators.py
@@ -65,6 +65,19 @@ def _func_full_docstring_deprecated(some_arg, other_arg):
     pass
 
 
+@deprecated(version="1.2.3")
+def _func_full_docstring_deprecated_no_message(some_arg, other_arg):
+    """My docstring.
+
+    Longer description.
+
+    Args:
+        some_arg: describe some_arg
+        other_arg: describe other_arg
+    """
+    pass
+
+
 @deprecated(version="1.2.3", message="This is deprecated!!")
 def _func_only_summary_deprecated(some_arg, other_arg):
     """My docstring."""
@@ -83,6 +96,23 @@ def test_deprecated_decorator_full_docstring():
         "\n"
         ".. deprecated:: 1.2.3\n"
         "    This is deprecated!!\n"
+        "\n"
+        "\n"
+        "Longer description.\n"
+        "\n"
+        "Args:\n"
+        "    some_arg: describe some_arg\n"
+        "    other_arg: describe other_arg\n"
+    )
+
+
+def test_deprecated_decorator_full_docstring_no_message():
+
+    assert _func_full_docstring_deprecated_no_message.__doc__ == (
+        "My docstring.\n"
+        "\n"
+        ".. deprecated:: 1.2.3\n"
+        "    \n"
         "\n"
         "\n"
         "Longer description.\n"
