ba1621e712ccde08c6405fe4fefe062529547b80
Nathan Farmer <nathan@greatexpectations.io>
2022-12-09 10:37:30 -0500

    Add logic for include_column_name and runtime_configuration in instantiation test

--- tests/render/test_renderer_configuration.py
+++ tests/render/test_renderer_configuration.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 from great_expectations.core import (
@@ -24,21 +26,38 @@ def mock_expectation_validation_result_from_expectation_configuration(
 
 
 @pytest.mark.parametrize(
-    "expectation_type,kwargs,fake_result",
-    [("expect_table_row_count_to_equal", {"value": 3}, {"observed_value": 3})],
+    "expectation_type,kwargs,runtime_configuration,fake_result,include_column_name",
+    [
+        (
+            "expect_table_row_count_to_equal",
+            {"value": 3},
+            {},
+            {"observed_value": 3},
+            True,
+        ),
+        (
+            "expect_table_row_count_to_equal",
+            {"value": 3},
+            {"include_column_name": False},
+            {"observed_value": 3},
+            False,
+        ),
+    ],
 )
-def test_renderer_configuration_instantiation(expectation_type, kwargs, fake_result):
+def test_renderer_configuration_instantiation(
+    expectation_type, kwargs, runtime_configuration, fake_result, include_column_name
+):
     expectation_configuration = ExpectationConfiguration(
         expectation_type=expectation_type,
         kwargs=kwargs,
     )
     renderer_configuration = RendererConfiguration(
         configuration=expectation_configuration,
-        runtime_configuration={},
+        runtime_configuration=runtime_configuration,
     )
     assert renderer_configuration.expectation_type == expectation_type
     assert renderer_configuration.kwargs == kwargs
-    assert renderer_configuration.include_column_name is True
+    assert renderer_configuration.include_column_name is include_column_name
 
     expectation_validation_result = (
         mock_expectation_validation_result_from_expectation_configuration(
@@ -48,8 +67,8 @@ def test_renderer_configuration_instantiation(expectation_type, kwargs, fake_res
     )
     renderer_configuration = RendererConfiguration(
         result=expectation_validation_result,
-        runtime_configuration={},
+        runtime_configuration=runtime_configuration,
     )
     assert renderer_configuration.expectation_type == expectation_type
     assert renderer_configuration.kwargs == kwargs
-    assert renderer_configuration.include_column_name is True
+    assert renderer_configuration.include_column_name is include_column_name
