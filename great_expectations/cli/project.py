46ec8b1fae12bafe989afb4134fce4da587f42e1
Gabriel <gabriel59kg@gmail.com>
2022-12-19 21:36:06 -0500

    [MAINTENANCE] partial `cli` + `usage_stats` typing (#6335)

--- great_expectations/cli/project.py
+++ great_expectations/cli/project.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 import sys
-from typing import Optional, Tuple
+from typing import TYPE_CHECKING, Optional, Tuple
 
 import click
 
@@ -13,6 +15,9 @@ from great_expectations.cli.upgrade_helpers import GE_UPGRADE_HELPER_VERSION_MAP
 from great_expectations.core.usage_statistics.events import UsageStatsEvents
 from great_expectations.core.usage_statistics.util import send_usage_message
 from great_expectations.data_context.types.base import CURRENT_GX_CONFIG_VERSION
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import AbstractDataContext
 
 
 @click.group()
@@ -66,13 +71,15 @@ def project_upgrade(ctx: click.Context) -> None:
         sys.exit(1)
 
 
-def do_config_check(target_directory: str) -> Tuple[bool, str, Optional[DataContext]]:
+def do_config_check(
+    target_directory: Optional[str],
+) -> Tuple[bool, str, Optional[AbstractDataContext]]:
     is_config_ok: bool = True
     upgrade_message: str = ""
-    context: Optional[DataContext]
+    context: Optional[AbstractDataContext]
     try:
         context = DataContext(context_root_dir=target_directory)
-        ge_config_version: int = context.get_config().config_version
+        ge_config_version: int = context.get_config().config_version  # type: ignore[union-attr] # could be dict, str
         if int(ge_config_version) < CURRENT_GX_CONFIG_VERSION:
             is_config_ok = False
             upgrade_message = f"""The config_version of your great_expectations.yml -- {float(ge_config_version)} -- is outdated.
