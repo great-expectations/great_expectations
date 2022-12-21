715818f9af34faeabbf67f833c70e83a92afd6b2
Gabriel Gore <gabriel59kg@gmail.com>
2022-12-21 17:12:12 -0500

    remove `os.PathLike` to help with type-checking

--- great_expectations/alias_types.py
+++ great_expectations/alias_types.py
@@ -1,9 +1,8 @@
 """This module contains shared TypeAliases"""
 
-import os
 import pathlib
 from typing import Union
 
 from typing_extensions import TypeAlias
 
-PathStr: TypeAlias = Union[str, pathlib.Path, os.PathLike]
+PathStr: TypeAlias = Union[str, pathlib.Path]
