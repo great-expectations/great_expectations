21047443c1704a8e36362480068e13bc362becaa
Gabriel Gore <gabriel59kg@gmail.com>
2022-12-22 18:23:36 -0500

    add back funtools caching

--- tests/experimental/datasources/test_viral_snippets.py
+++ tests/experimental/datasources/test_viral_snippets.py
@@ -1,4 +1,4 @@
-# import functools
+import functools
 import logging
 import pathlib
 from pprint import pformat as pf
@@ -113,7 +113,7 @@ def test_load_an_existing_config(
 
 
 @pytest.fixture
-# @functools.lru_cache(maxsize=1)
+@functools.lru_cache(maxsize=1)
 def zep_file_context(zep_yaml_config_file: pathlib.Path) -> FileDataContext:
     context = get_context(
         context_root_dir=zep_yaml_config_file.parent, cloud_mode=False
