0e92efb9d323196e8ec697c8e5df2c043ca69674
Alex Sherstinsky <alex@greatexpectations.io>
2022-12-22 16:21:36 -0800

    merge

--- great_expectations/expectations/metrics/query_metrics/query_column.py
+++ great_expectations/expectations/metrics/query_metrics/query_column.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, List, Optional, Union
 
 from great_expectations.core.metric_domain_types import MetricDomainTypes
-from great_expectations.core.util import convert_to_json_serializable
 from great_expectations.execution_engine import (
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
