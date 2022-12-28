b2010df43371e859e4d5259dd92f29b9361ec84f
Gabriel Gore <gabriel59kg@gmail.com>
2022-12-28 14:55:26 -0500

    revert `pathlib.Path` change

--- great_expectations/data_context/data_context/serializable_data_context.py
+++ great_expectations/data_context/data_context/serializable_data_context.py
@@ -50,8 +50,8 @@ class SerializableDataContext(AbstractDataContext):
         DataContextConfigDefaults.PROFILERS_BASE_DIRECTORY.value,
         GX_UNCOMMITTED_DIR,
     ]
-    GX_DIR: ClassVar[pathlib.Path] = pathlib.Path("great_expectations")
-    GX_YML: ClassVar[pathlib.Path] = pathlib.Path("great_expectations.yml")
+    GX_DIR: ClassVar[str] = "great_expectations"
+    GX_YML: ClassVar[str] = "great_expectations.yml"
     GX_EDIT_NOTEBOOK_DIR = GX_UNCOMMITTED_DIR
     DOLLAR_SIGN_ESCAPE_STRING = r"\$"
 
