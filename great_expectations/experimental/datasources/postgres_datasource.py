b1f46338fd46222df3c11cd90a96e2239e79d005
Bill Dirks <bill@greatexpectations.io>
2022-12-29 14:55:15 -0800

    Add postgres datasource connection string constraint.

--- great_expectations/experimental/datasources/postgres_datasource.py
+++ great_expectations/experimental/datasources/postgres_datasource.py
@@ -1,7 +1,16 @@
+from typing import TYPE_CHECKING
+
+from pydantic import constr
 from typing_extensions import Literal
 
 from great_expectations.experimental.datasources.sql_datasource import SQLDatasource
 
+if TYPE_CHECKING:
+    PostgresConnectionString = str
+else:
+    PostgresConnectionString = constr(regex=r"^postgresql")
+
 
 class PostgresDatasource(SQLDatasource):
     type: Literal["postgres"] = "postgres"  # type: ignore[assignment]
+    connection_string: PostgresConnectionString
