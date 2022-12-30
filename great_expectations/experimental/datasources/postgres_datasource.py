c29eccc30f58741af8e7befd3c8f14c48f7b6fc8
Bill Dirks <bill@greatexpectations.io>
2022-12-30 10:10:08 -0800

    Use PostgresDsn instead of constrained string

--- great_expectations/experimental/datasources/postgres_datasource.py
+++ great_expectations/experimental/datasources/postgres_datasource.py
@@ -1,16 +1,9 @@
-from typing import TYPE_CHECKING
-
-from pydantic import constr
+from pydantic import PostgresDsn
 from typing_extensions import Literal
 
 from great_expectations.experimental.datasources.sql_datasource import SQLDatasource
 
-if TYPE_CHECKING:
-    PostgresConnectionString = str
-else:
-    PostgresConnectionString = constr(regex=r"^postgresql")
-
 
 class PostgresDatasource(SQLDatasource):
     type: Literal["postgres"] = "postgres"  # type: ignore[assignment]
-    connection_string: PostgresConnectionString
+    connection_string: PostgresDsn
