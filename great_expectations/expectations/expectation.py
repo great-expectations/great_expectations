cb6fa36b12d6ac845bab65dc49aa32f12a1f81a0
Alex Sherstinsky <alex@greatexpectations.io>
2023-01-03 18:18:32 -0800

    JSON Serialize Metrics Results

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -2272,6 +2272,9 @@ class TableExpectation(Expectation, ABC):
         runtime_configuration: Optional[dict] = None,
         execution_engine: Optional[ExecutionEngine] = None,
     ) -> Dict[str, Union[bool, Dict[str, Any]]]:
+        # TODO: <Alex>ALEX</Alex>
+        metrics = convert_to_json_serializable(data=metrics)
+        # TODO: <Alex>ALEX</Alex>
         metric_value: Optional[Any] = metrics.get(metric_name)
 
         if metric_value is None:
