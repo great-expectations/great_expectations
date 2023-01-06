55769eea9a314b3eed7ca6663240474a929c3f3a
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-05 22:01:45 -0500

    `._json_dict()` method

--- great_expectations/experimental/datasources/experimental_base_model.py
+++ great_expectations/experimental/datasources/experimental_base_model.py
@@ -5,10 +5,24 @@ import logging
 import pathlib
 from io import StringIO
 from pprint import pformat as pf
-from typing import Type, TypeVar, Union, overload
+from typing import (
+    TYPE_CHECKING,
+    AbstractSet,
+    Any,
+    Callable,
+    Mapping,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
 
 import pydantic
 from ruamel.yaml import YAML
+
+if TYPE_CHECKING:
+    MappingIntStrAny = Mapping[Union[int, str], Any]
+    AbstractSetIntStr = AbstractSet[Union[int, str]]
 
 LOGGER = logging.getLogger(__name__)
 
@@ -61,6 +75,39 @@ class ExperimentalBaseModel(pydantic.BaseModel):
         if isinstance(stream_or_path, pathlib.Path):
             return stream_or_path
         return stream_or_path.getvalue()
+
+    def _json_dict(
+        self,
+        *,
+        include: Union[AbstractSetIntStr, MappingIntStrAny, None] = None,
+        exclude: Union[AbstractSetIntStr, MappingIntStrAny, None] = None,
+        by_alias: bool = False,
+        skip_defaults: Union[bool, None] = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+        encoder: Union[Callable[[Any], Any], None] = None,
+        models_as_dict: bool = True,
+        **dumps_kwargs,
+    ) -> dict:
+        """
+        JSON compatible dictionary. All complex types removed.
+        Prefer `.dict()` or `.json()`
+        """
+        return json.loads(
+            self.json(
+                include=include,
+                exclude=exclude,
+                by_alias=by_alias,
+                skip_defaults=skip_defaults,
+                exclude_unset=exclude_unset,
+                exclude_defaults=exclude_defaults,
+                exclude_none=exclude_none,
+                encoder=encoder,
+                models_as_dict=models_as_dict,
+                **dumps_kwargs,
+            )
+        )
 
     def __str__(self):
         return self.yaml()
