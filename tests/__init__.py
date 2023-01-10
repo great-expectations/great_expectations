7e84c3bd7a4e6678a48a99f68b037d022176b775
Ken Wade <ken@superconductive.com>
2023-01-09 22:05:53 -0600

    Just check to see if on a mac or not

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,10 @@
 import os
 import sys
 
-# Maybe just check to see `if sys.platform == "darwin"` instead
 
-if not os.path.isdir("/var/lib/cloud/instance"):
-    print("Not using a cloud instance, so unsetting any GE_CLOUD_* vars")
+if sys.platform == "darwin":
+    print("Running tests from a dev machine, so unsetting any GE_CLOUD_* environment vars")
     cloud_vars = [k for k in os.environ if k.startswith("GE_CLOUD")]
     for var_name in cloud_vars:
         print(f"Unsetting environment variable {var_name}")
         del os.environ[var_name]
-
-sys.exit(1)
