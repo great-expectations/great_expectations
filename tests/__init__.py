ec74fa9ba749ac164cc0ac801083e3629c7bd904
Ken Wade <ken@superconductive.com>
2023-01-09 22:11:14 -0600

    Lint

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 import os
 import sys
 
-
 if sys.platform == "darwin":
-    print("Running tests from a dev machine, so unsetting any GE_CLOUD_* environment vars")
+    print(
+        "Running tests from a dev machine, so unsetting any GE_CLOUD_* environment vars"
+    )
     cloud_vars = [k for k in os.environ if k.startswith("GE_CLOUD")]
     for var_name in cloud_vars:
         print(f"Unsetting environment variable {var_name}")
