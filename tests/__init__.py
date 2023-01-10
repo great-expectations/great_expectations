dddb50a4047d8782715af094ccb0824662e2db4d
Ken Wade <ken@superconductive.com>
2023-01-10 10:41:30 -0600

    Unset GE_CLOUD_xx and GX_CLOUD_xx vars

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,11 @@ import os
 import sys
 
 if sys.platform == "darwin":
-    print(
-        "Running tests from a dev machine, so unsetting any GE_CLOUD_* environment vars"
-    )
-    cloud_vars = [k for k in os.environ if k.startswith("GE_CLOUD")]
-    for var_name in cloud_vars:
-        print(f"Unsetting environment variable {var_name}")
-        del os.environ[var_name]
+    cloud_vars = [k for k in os.environ if k[:8] in ("GX_CLOUD", "GE_CLOUD")]
+    if cloud_vars:
+        print(
+            "Running tests from a dev machine, so unsetting any CLOUD environment vars"
+        )
+        for var_name in cloud_vars:
+            print(f"Unsetting environment variable {var_name}")
+            del os.environ[var_name]
