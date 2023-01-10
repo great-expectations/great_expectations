59b0558dec6c8888f1392a14054ccd9d01989e00
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-10 17:02:56 -0500

    suppress flake8 warning

--- great_expectations/expectations/core/expect_select_column_values_to_be_unique_within_record.py
+++ great_expectations/expectations/core/expect_select_column_values_to_be_unique_within_record.py
@@ -175,9 +175,9 @@ class ExpectSelectColumnValuesToBeUniqueWithinRecord(MulticolumnMapExpectation):
         )
 
         if params["mostly"] is not None and params["mostly"] < 1.0:
-            params_with_json_schema["mostly_pct"]["value"] = num_to_str(  # FIXME
-                params["mostly"] * 100, precision=15, no_scientific=True
-            )
+            params_with_json_schema["mostly_pct"][  # noqa: F821 # FIXME: this breaks
+                "value"
+            ] = num_to_str(params["mostly"] * 100, precision=15, no_scientific=True)
             template_str = "Values must be unique across columns, at least $mostly_pct % of the time: "
         else:
             template_str = "Values must always be unique across columns: "
