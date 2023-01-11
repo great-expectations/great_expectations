a1f2951ae8eec4a7232eed4bbc8f2c5f21bf5232
Chetan Kini <chetan@superconductive.com>
2023-01-11 12:45:22 -0500

    chore: update all refs

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ config = {
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 }
 
