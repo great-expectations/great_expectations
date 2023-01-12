630b3653f5916dcebfed06e8afece4a8ae9bab88
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-12 11:09:33 -0500

    `DataFrameFactoryFn` alias

--- great_expectations/experimental/datasources/dynamic_pandas.py
+++ great_expectations/experimental/datasources/dynamic_pandas.py
@@ -3,14 +3,17 @@ from pprint import pformat as pf
 from typing import Callable, List, Tuple
 
 import pandas as pd
-from typing_extensions import reveal_type
+from typing_extensions import TypeAlias, reveal_type
+
+DataFrameFactoryFn: TypeAlias = Callable[..., pd.DataFrame]
 
 
 def _public_dir(obj: object) -> List[str]:
     return [x for x in dir(obj) if not x.startswith("_")]
 
 
-def _extract_io_methods() -> List[Tuple[str, Callable[..., pd.DataFrame]]]:
+def _extract_io_methods() -> List[Tuple[str, DataFrameFactoryFn]]:
+    # TODO: use blacklist/whitelist?
     member_functions = inspect.getmembers(pd.io.api, predicate=inspect.isfunction)
     return [t for t in member_functions if t[0].startswith("read_")]
 
