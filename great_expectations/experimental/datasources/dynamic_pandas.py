9fffe7ba798a0e48abba959b2ebe9ae94e41dd03
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-13 10:58:37 -0500

    explore param types

--- great_expectations/experimental/datasources/dynamic_pandas.py
+++ great_expectations/experimental/datasources/dynamic_pandas.py
@@ -1,6 +1,6 @@
 import inspect
 from pprint import pformat as pf
-from typing import Callable, List, Tuple
+from typing import Callable, List, Sequence, Tuple
 
 import pandas as pd
 from typing_extensions import TypeAlias, reveal_type
@@ -10,6 +10,9 @@ DataFrameFactoryFn: TypeAlias = Callable[..., pd.DataFrame]
 
 def _public_dir(obj: object) -> List[str]:
     return [x for x in dir(obj) if not x.startswith("_")]
+
+
+# TODO: make these a generator pipeline
 
 
 def _extract_io_methods() -> List[Tuple[str, DataFrameFactoryFn]]:
@@ -24,14 +27,22 @@ def _extract_io_signatures(
     signatures = []
     for name, method in io_methods:
         sig = inspect.signature(method)
-        print(f"  {name}\n{sig}\n")
+        print(f"  {name} -> {sig.return_annotation}\n{sig}\n")
         signatures.append(sig)
     return signatures
 
 
+def _to_pydantic_fields(signatures: Sequence[inspect.Signature]):
+    for sig in signatures:
+        for param_name, param in sig.parameters.items():
+            print(type(param), param)
+
+
 if __name__ == "__main__":
-    io_methods = _extract_io_methods()[:]
+    io_methods = _extract_io_methods()[1:2]
     print(f"  IO Methods\n{pf(io_methods)}\n")
 
     io_method_sigs = _extract_io_signatures(io_methods)
     # print(f"  IO Method Signatures\n{pf(io_method_sigs)}")
+
+    fields = _to_pydantic_fields(io_method_sigs)
