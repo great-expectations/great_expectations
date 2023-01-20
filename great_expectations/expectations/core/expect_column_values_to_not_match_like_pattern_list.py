6bb676dc1c2b1c3ce0f315c879ff09b35513fa00
Robby Reinold <robby@superconductive.com>
2023-01-20 16:42:36 -0600

    add docstring, updated assert message

--- great_expectations/expectations/core/expect_column_values_to_not_match_like_pattern_list.py
+++ great_expectations/expectations/core/expect_column_values_to_not_match_like_pattern_list.py
@@ -92,6 +92,22 @@ class ExpectColumnValuesToNotMatchLikePatternList(ColumnMapExpectation):
     def validate_configuration(
         self, configuration: Optional[ExpectationConfiguration] = None
     ) -> None:
+        """
+        "Validates the configuration of an Expectation.
+
+        For `expect_column_values_to_not_match_like_pattern_list` it is required that:
+            - 'like_pattern_list' is present in configuration's kwarg
+            - if 'like_pattern_list' is list, assert non-empty
+            - if 'like_pattern_list' is dict, assert a key "$PARAMETER" is present
+
+        Args:
+            configuration: An `ExpectationConfiguration` to validate. If no configuration is provided, it will be pulled
+                                  from the configuration attribute of the Expectation instance.
+
+        Raises:
+            `InvalidExpectationConfigurationError`: The configuration does not contain the values required by the
+                                                                           Expectation."
+        """
         super().validate_configuration(configuration)
         configuration = configuration or self.configuration
         try:
@@ -100,7 +116,7 @@ class ExpectColumnValuesToNotMatchLikePatternList(ColumnMapExpectation):
             ), "Must provide like_pattern_list"
             assert isinstance(
                 configuration.kwargs.get("like_pattern_list"), (list, dict)
-            ), "like_pattern_list must be a list"
+            ), "like_pattern_list must be a list or dict"
             assert isinstance(configuration.kwargs.get("like_pattern_list"), dict) or (
                 len(configuration.kwargs.get("like_pattern_list")) > 0
             ), "At least one like_pattern must be supplied in the like_pattern_list."
