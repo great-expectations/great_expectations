8bcec67e442c4ab32c85d39da31f405e3d410b92
Alex Sherstinsky <alex@greatexpectations.io>
2023-01-20 14:54:05 -0800

    docs for metric domain types and metric function types

--- great_expectations/core/metric_function_types.py
+++ great_expectations/core/metric_function_types.py
@@ -8,17 +8,16 @@ logger = logging.getLogger(__name__)
 
 @public_api
 class MetricFunctionTypes(enum.Enum):
-    """Enum type, whose members depict the nature of return value of a metric implementation function (defined for
-    a specified "ExecutionEngine" subclass) that is the final result (rather than a Callable for deferred execution).
+    """Enum type, whose members depict the nature of return value of a metric implementation function (defined for a specified "ExecutionEngine" subclass) that is the final result (rather than a Callable for deferred execution).
 
     The available types are:
-    - VALUE -- metric implementation function returns a value computed over a dataset represented by "Domain" (e.g., a
+    - `VALUE` -- metric implementation function returns a value computed over a dataset represented by "Domain" (e.g., a
       statistic on row values of a column).
-    - MAP_VALUES (unused in current version) -- metric implementation function returns a mapping between every "Domain"
-      value and the result of a transformation of the corresponding "Domain" value.
-    - WINDOW_VALUES (unused in current version) -- metric implementation function returns the result of applying a
+    - `MAP_VALUES` (unused in current version) -- metric implementation function returns a mapping between every
+      "Domain" value and the result of a transformation of the corresponding "Domain" value.
+    - `WINDOW_VALUES` (unused in current version) -- metric implementation function returns the result of applying a
       specified windowing operation over "Domain" values.
-    - AGGREGATE_VALUE (unused in current version) -- metric implementation function returns the result of applying a
+    - `AGGREGATE_VALUE` (unused in current version) -- metric implementation function returns the result of applying a
       specified aggregation operation to every "Domain" value.
     """
 
@@ -30,26 +29,25 @@ class MetricFunctionTypes(enum.Enum):
 
 @public_api
 class MetricPartialFunctionTypes(enum.Enum):
-    """Enum type, whose members depict the nature of return value of a metric implementation function (defined for
-    a specified "ExecutionEngine" subclass) that is a (partial) Callable to be executed once execution plan is complete.
+    """Enum type, whose members depict the nature of return value of a metric implementation function (defined for a specified "ExecutionEngine" subclass) that is a (partial) Callable to be executed once execution plan is complete.
 
     The available types are:
-    - MAP_FN -- metric implementation function returns a mapping transformation for "Domain" values that evaluates to a
-      quantity (rather than a condition statement, or a series, etc.).
-    - MAP_SERIES -- metric implementation function returns a mapping transformation for "Domain" values that evaluates
+    - `MAP_FN` -- metric implementation function returns a mapping transformation for "Domain" values that evaluates to
+      a quantity (rather than a condition statement, or a series, etc.).
+    - `MAP_SERIES` -- metric implementation function returns a mapping transformation for "Domain" values that evaluates
       to a series-valued (e.g., Pandas.Series) result (rather than a Callable for deferred execution).
-    - WINDOW_FN -- metric implementation function returns specified windowing operation over "Domain" values (currently
-      applicable only to "SparkDFExecutionEngine").
-    - MAP_CONDITION_FN -- metric implementation function returns a mapping transformation for "Domain" values that
+    - `WINDOW_FN` -- metric implementation function returns specified windowing operation over "Domain" values
+      (currently applicable only to "SparkDFExecutionEngine").
+    - `MAP_CONDITION_FN` -- metric implementation function returns a mapping transformation for "Domain" values that
       evaluates to a Callable (partial) computational component (as part of deferred execution plan) that expresses the
       specified condition (i.e., a logical operation).
-    - MAP_CONDITION_SERIES -- metric implementation function returns a mapping transformation for "Domain" values that
+    - `MAP_CONDITION_SERIES` -- metric implementation function returns a mapping transformation for "Domain" values that
       evaluates to a Callable (partial) computational component (as part of deferred execution plan) that expresses the
       specified condition (i.e., a logical operation) as a series-valued (e.g., Pandas.Series) result.
-    - WINDOW_CONDITION_FN -- metric implementation function returns a windowing operation over "Domain" values that
+    - `WINDOW_CONDITION_FN` -- metric implementation function returns a windowing operation over "Domain" values that
       evaluates to a Callable (partial) computational component (as part of deferred execution plan) that expresses the
       specified condition (i.e., a logical operation).
-    - AGGREGATE_FN -- metric implementation function returns an aggregation transformation over "Domain" values that
+    - `AGGREGATE_FN` -- metric implementation function returns an aggregation transformation over "Domain" values that
       evaluates to a Callable (partial) computational component (as part of deferred execution plan) that expresses the
       specified aggregated quantity.
 
