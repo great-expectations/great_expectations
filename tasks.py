984fbbef6f3076798c8f69349035c47567fbac71
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-21 16:56:35 -0500

    sort usage

--- tasks.py
+++ tasks.py
@@ -52,9 +52,12 @@ def sort(
     ctx: Context, path: str = ".", check: bool = False, exclude: str | None = None
 ):
     """Sort module imports."""
-    cmds = ["ruff", path, "--select", "I"]
-    if check:
-        cmds.append("--diff")  # TODO: test this
+    cmds = [
+        "ruff",
+        path,
+        "--select I",
+        "--diff" if check else "--fix",
+    ]
     if exclude:
         cmds.extend(["--skip", exclude])  # TODO: check & test this
     ctx.run(" ".join(cmds), echo=True)
