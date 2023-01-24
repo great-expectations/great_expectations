d18ae0c5893f984861bea92f43a172630afef95c
Chetan Kini <chetan@superconductive.com>
2023-01-24 14:54:33 -0500

    test: clean up test

--- tests/core/usage_statistics/test_usage_statistics_data_context.py
+++ tests/core/usage_statistics/test_usage_statistics_data_context.py
@@ -1,7 +1,9 @@
 """
 TODO
 """
+from __future__ import annotations
 
+import ast
 import logging
 from unittest import mock
 
@@ -17,6 +19,7 @@ from great_expectations.data_context.data_context.ephemeral_data_context import 
 from great_expectations.data_context.data_context.file_data_context import (
     FileDataContext,
 )
+from great_expectations.data_context.util import file_relative_path
 
 logger = logging.getLogger(__name__)
 
@@ -26,50 +29,92 @@ def enable_usage_stats(monkeypatch):
     monkeypatch.delenv("GE_USAGE_STATS")
 
 
+@pytest.fixture
+def usage_stats_decorated_methods_on_abstract_data_context() -> list[str]:
+    """
+    Uses AST parsing to collect the names of all methods that are decorated with
+    `usage_statistics_enabled_method`.
+
+    This is used to ensure that any changes made to `AbstractDataContext` are automatically
+    accounted for in our tests.
+    """
+    path_to_abstract_context = file_relative_path(
+        __file__,
+        "../../../great_expectations/data_context/data_context/abstract_data_context.py",
+    )
+    with open(path_to_abstract_context) as f:
+        root = ast.parse(f.read())
+
+    relevant_methods = []
+    for node in ast.walk(root):
+        if not isinstance(node, ast.FunctionDef):
+            continue
+
+        name = node.name
+        # Ignore dunders like __init__
+        if name.startswith("__") and name.endswith("__"):
+            continue
+
+        decorators = node.decorator_list
+        for decorator in decorators:
+            if isinstance(decorator, ast.Call):
+                if decorator.func.id == "usage_statistics_enabled_method":
+                    relevant_methods.append(name)
+                    break
+
+    return relevant_methods
+
+
 @mock.patch(
     "great_expectations.core.usage_statistics.usage_statistics.UsageStatisticsHandler.emit"
 )
 def test_all_relevant_context_methods_emit_usage_stats(
     mock_emit: mock.MagicMock,
+    usage_stats_decorated_methods_on_abstract_data_context: list[str],
     enable_usage_stats,  # Needs to be before context fixtures to ensure usage stats handlers are attached
     in_memory_runtime_context: EphemeralDataContext,
     empty_data_context: FileDataContext,
     empty_data_context_in_cloud_mode: CloudDataContext,
 ):
+    """
+    What does this test and why?
+
+    Ensures that all primary context types (EphemeralDataContext, FileDataContext, and CloudDataContext)
+    emit the same usage stats events.
+
+    This guards against the case where a child class overrides a method defined by AbstractDataContext
+    but forgets to add the decorator, resulting in us losing event data.
+    """
+
+    relevant_methods = usage_stats_decorated_methods_on_abstract_data_context
+    expected_events = (
+        UsageStatsEvents.DATA_CONTEXT_SAVE_EXPECTATION_SUITE,
+        UsageStatsEvents.DATA_CONTEXT_ADD_DATASOURCE,
+        UsageStatsEvents.DATA_CONTEXT_RUN_CHECKPOINT,
+        UsageStatsEvents.DATA_CONTEXT_GET_BATCH_LIST,
+        UsageStatsEvents.DATA_CONTEXT_RUN_RULE_BASED_PROFILER_WITH_DYNAMIC_ARGUMENTS,
+        UsageStatsEvents.DATA_CONTEXT_RUN_RULE_BASED_PROFILER_ON_DATA,
+        UsageStatsEvents.DATA_CONTEXT_RUN_VALIDATION_OPERATOR,
+        UsageStatsEvents.DATA_CONTEXT_OPEN_DATA_DOCS,
+        UsageStatsEvents.DATA_CONTEXT_BUILD_DATA_DOCS,
+    )
+    assert len(relevant_methods) == len(
+        expected_events
+    ), "Please update the `expected_events` list to account for all usage stats decorated methods in AbstractDataContext"
+
     contexts = (
         in_memory_runtime_context,
         empty_data_context,
         empty_data_context_in_cloud_mode,
     )
 
-    methods_and_events = (
-        (
-            "save_expectation_suite",
-            UsageStatsEvents.DATA_CONTEXT_SAVE_EXPECTATION_SUITE,
-        ),
-        ("add_datasource", UsageStatsEvents.DATA_CONTEXT_ADD_DATASOURCE),
-        ("run_checkpoint", UsageStatsEvents.DATA_CONTEXT_RUN_CHECKPOINT),
-        ("get_batch_list", UsageStatsEvents.DATA_CONTEXT_GET_BATCH_LIST),
-        (
-            "run_profiler_with_dynamic_arguments",
-            UsageStatsEvents.DATA_CONTEXT_RUN_RULE_BASED_PROFILER_WITH_DYNAMIC_ARGUMENTS,
-        ),
-        (
-            "run_profiler_on_data",
-            UsageStatsEvents.DATA_CONTEXT_RUN_RULE_BASED_PROFILER_ON_DATA,
-        ),
-        (
-            "run_validation_operator",
-            UsageStatsEvents.DATA_CONTEXT_RUN_VALIDATION_OPERATOR,
-        ),
-        ("open_data_docs", UsageStatsEvents.DATA_CONTEXT_OPEN_DATA_DOCS),
-        ("build_data_docs", UsageStatsEvents.DATA_CONTEXT_BUILD_DATA_DOCS),
-    )
-
     for context in contexts:
-        for (method_name, expected_event) in methods_and_events:
+        for method_name, expected_event in zip(relevant_methods, expected_events):
             logger.info(f"Testing {context.__class__}.{method_name}")
 
+            # As we only care about the decorator and not the underlying method being decorated,
+            # we use the following try/except pattern. All method calls will generally fail due
+            # to having no input args but we still manage to trigger our target decorator.
             try:
                 method = getattr(context, method_name)
                 method()
