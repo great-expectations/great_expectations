4648eb5b2a12cc4d4cba08ba832425611969b042
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-24 23:07:30 -0500

    datasources tasks

---
+++ tests/experimental/datasources/tasks.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import pathlib
+
+import invoke
+import pandas as pd
+
+TEST_ROOT = pathlib.Path(__file__).parent.parent.parent.resolve(strict=True)
+
+
+@invoke.task(aliases=["gen-assets"])
+def generate_asset_files(
+    ctx: invoke.Context, source_csv_dir: str | pathlib.Path = None, limit: int = 5
+):
+    """Generate or re-generate various test asset files."""
+    if not source_csv_dir:
+        source_csv_dir = TEST_ROOT / "test_sets" / "taxi_yellow_tripdata_samples"
+    source_csv_dir = pathlib.Path(source_csv_dir).resolve(strict=True)
+
+    for i, csv_file in enumerate(source_csv_dir.glob("*.csv"), start=1):
+        json_output_file = pathlib.Path.cwd().joinpath(
+            csv_file.with_suffix(".json").name
+        )
+        print(i, json_output_file.name)
+        df = pd.read_csv(csv_file)
+        df.to_json(json_output_file)
+        if i == limit:
+            break
