ee6e7003159f3f6a4ab40330f69dbb1a628fffd7
Gabriel Gore <gabriel59kg@gmail.com>
2023-01-24 23:25:35 -0500

    asset json dir

--- tests/experimental/datasources/tasks.py
+++ tests/experimental/datasources/tasks.py
@@ -10,17 +10,17 @@ TEST_ROOT = pathlib.Path(__file__).parent.parent.parent.resolve(strict=True)
 
 @invoke.task(aliases=["gen-assets"])
 def generate_asset_files(
-    ctx: invoke.Context, source_csv_dir: str | pathlib.Path = None, limit: int = 5
+    ctx: invoke.Context, source_csv_dir: str | pathlib.Path = None, limit: int = 3
 ):
     """Generate or re-generate various test asset files."""
     if not source_csv_dir:
         source_csv_dir = TEST_ROOT / "test_sets" / "taxi_yellow_tripdata_samples"
     source_csv_dir = pathlib.Path(source_csv_dir).resolve(strict=True)
 
+    json_asset_dir = pathlib.Path.cwd() / "json_assets"
+    json_asset_dir.mkdir(exist_ok=True)
     for i, csv_file in enumerate(source_csv_dir.glob("*.csv"), start=1):
-        json_output_file = pathlib.Path.cwd().joinpath(
-            csv_file.with_suffix(".json").name
-        )
+        json_output_file = json_asset_dir / csv_file.with_suffix(".json").name
         print(i, json_output_file.name)
         df = pd.read_csv(csv_file)
         df.to_json(json_output_file)
