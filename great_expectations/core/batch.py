cbcb77a9e9d2f2dce7d1646dfb2e91fc3f32576b
James Campbell <james@greatexpectations.io>
2023-01-26 17:20:14 -0500

    batch initial commit

--- great_expectations/core/batch.py
+++ great_expectations/core/batch.py
@@ -38,6 +38,25 @@ except ImportError:
 
 
 class BatchDefinition(SerializableDictDot):
+    """A BatchDefinition includes all the information required to precisely
+    identify a set of data from the external data source that should be
+    translated into a Batch. One or more BatchDefinitions should always be
+    *returned* from the Datasource, as a result of processing the Batch Request.
+
+    ---Documentation---
+            - https://docs.greatexpectations.io/docs/terms/batch/#batches-and-batch-requests-design-motivation
+
+    Args:
+        datasource_name: name of the Datasource used to connect to the data
+        data_connector_name: name of the DataConnector used to connect to the data
+        data_asset_name: name of the DataAsset used to connect to the data
+        batch_identifiers: key-value pairs that the DataConnector
+            will use to obtain a specific set of data
+        batch_spec_passthrough: a dictionary of additional parameters that
+            the ExecutionEngine will use to obtain a specific set of data
+    Returns:
+        BatchDefinition
+    """
     def __init__(
         self,
         datasource_name: str,
