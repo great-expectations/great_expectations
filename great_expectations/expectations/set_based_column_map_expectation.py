468d7f6f7d1cdb7de9047112e140da7587578937
Anthony Burdi <anthony@greatexpectations.io>
2023-01-26 16:07:57 -0500

    Update great_expectations/expectations/set_based_column_map_expectation.py

--- great_expectations/expectations/set_based_column_map_expectation.py
+++ great_expectations/expectations/set_based_column_map_expectation.py
@@ -103,7 +103,7 @@ class SetBasedColumnMapExpectation(ColumnMapExpectation, ABC):
     )
     ```
 
-    Attributes:
+    Args:
         set_camel_name (str): A name describing a set of values, in camel case.
         set_ (str): A value set.
         set_semantic_name (optional[str]): A name for the semantic type representing the set being validated..
