4677e6b71eb891e12d5da7fd0590cb914ed5a13d
Anthony Burdi <anthony@greatexpectations.io>
2023-01-27 13:48:08 -0500

    Update configured_asset_filesystem_data_connector.py

--- great_expectations/datasource/data_connector/configured_asset_filesystem_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_filesystem_data_connector.py
@@ -18,8 +18,7 @@ logger = logging.getLogger(__name__)
 
 @public_api
 class ConfiguredAssetFilesystemDataConnector(ConfiguredAssetFilePathDataConnector):
-    """
-    Extension of ConfiguredAssetFilePathDataConnector used to connect to Filesystem
+    """Extension of ConfiguredAssetFilePathDataConnector used to connect to Filesystem.
 
     Being a Configured Asset Data Connector, it requires an explicit list of each Data Asset it can
     connect to. While this allows for fine-grained control over which Data Assets may be accessed,
