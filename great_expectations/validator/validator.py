67484721645fb5604a6008b09dfd01f3c72ff46f
Nathan Farmer <nathan@greatexpectations.io>
2023-01-26 20:16:05 -0500

    Reformat

--- great_expectations/validator/validator.py
+++ great_expectations/validator/validator.py
@@ -1557,7 +1557,6 @@ class Validator:
         message="Only the str version of this argument is deprecated. run_id should be a RunIdentifier or dict. Support will be removed in 0.16.0.",
         version="0.13.0",
     )
-
     def validate(  # noqa: C901 - Complexity 31
         self,
         expectation_suite: str | ExpectationSuite | None = None,
