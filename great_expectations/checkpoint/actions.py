0919a5ce51f829e9bc6f8ce0944c3fcbf0731db4
Josh Stauffer <josh@greatexpectations.io>
2023-01-30 11:42:06 -0500

    update for ruff

--- great_expectations/checkpoint/actions.py
+++ great_expectations/checkpoint/actions.py
@@ -12,7 +12,7 @@ from typing import TYPE_CHECKING, Dict, List, Optional, Union
 from typing_extensions import Final
 
 from great_expectations.core import ExpectationSuiteValidationResult
-from great_expectations.core._docs_decorators import deprecated_argument, public_api
+from great_expectations.core._docs_decorators import deprecated_argument
 from great_expectations.data_context.cloud_constants import CLOUD_APP_DEFAULT_BASE_URL
 from great_expectations.data_context.types.refs import GXCloudResourceRef
 
