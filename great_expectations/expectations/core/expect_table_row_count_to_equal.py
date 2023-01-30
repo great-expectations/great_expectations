2753bb8f54e3ec28748deebcd9ec942fc53bbe2e
Kim Mathieu <kim@superconductive.com>
2023-01-30 17:17:38 -0500

    Fix import

--- great_expectations/expectations/core/expect_table_row_count_to_equal.py
+++ great_expectations/expectations/core/expect_table_row_count_to_equal.py
@@ -4,6 +4,7 @@ from great_expectations.core import (
     ExpectationConfiguration,
     ExpectationValidationResult,
 )
+from great_expectations.core._docs_decorators import public_api
 from great_expectations.execution_engine import ExecutionEngine
 from great_expectations.expectations.expectation import (
     InvalidExpectationConfigurationError,
