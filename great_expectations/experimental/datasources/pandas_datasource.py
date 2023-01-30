10ec504af22365bd7522ca99b8f37be372a1ab37
Nathan Farmer <nathan@greatexpectations.io>
2023-01-30 07:38:56 -0500

    Make type checker happy

--- great_expectations/experimental/datasources/pandas_datasource.py
+++ great_expectations/experimental/datasources/pandas_datasource.py
@@ -260,7 +260,7 @@ class PandasDatasource(Datasource):
         # Only self.assets can be tested for PandasDatasource
         if self.assets and test_assets:
             for asset in self.assets.values():
-                asset.test_connection()
+                asset.test_connection()  # type: ignore[union-attr]
 
     def add_csv_asset(
         self,
