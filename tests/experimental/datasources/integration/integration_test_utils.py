2c9a5eab1164f416ef40eda9fcd9935a6ffe9582
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-01 10:34:15 -0800

    tests and docstring

--- tests/experimental/datasources/integration/integration_test_utils.py
+++ tests/experimental/datasources/integration/integration_test_utils.py
@@ -196,7 +196,6 @@ def run_multibatch_data_assistant_and_checkpoint(
     ]
 ):
     context, ds, _, batch_request = multibatch_datasource_test_data
-    execution_engine = ds.get_execution_engine()
     data_assistant_result, checkpoint_result = _configure_and_run_data_assistant(
         context, batch_request
     )
