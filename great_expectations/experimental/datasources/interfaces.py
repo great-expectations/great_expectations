ba007c35200cf634772df0341d8765428bbadb94
Gabriel Gore <gabriel59kg@gmail.com>
2023-02-04 22:02:12 -0500

    re-fix

--- great_expectations/experimental/datasources/interfaces.py
+++ great_expectations/experimental/datasources/interfaces.py
@@ -22,7 +22,7 @@ import pandas as pd
 import pydantic
 from pydantic import Field, StrictBool, StrictInt, root_validator, validate_arguments
 from pydantic import dataclasses as pydantic_dc
-from typing_extensions import ClassVar, TypeAlias, TypeGuard
+from typing_extensions import TypeAlias, TypeGuard
 
 import great_expectations.exceptions as ge_exceptions
 from great_expectations.core.id_dict import BatchSpec
