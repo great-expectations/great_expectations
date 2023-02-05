2354a12b8189ac3df08901099f5110e38811bd76
Nathan Farmer <nathan@greatexpectations.io>
2023-02-04 22:43:37 -0500

    Unused imports

--- tests/experimental/datasources/test_postgres_datasource.py
+++ tests/experimental/datasources/test_postgres_datasource.py
@@ -21,8 +21,6 @@ from great_expectations.experimental.datasources.postgres_datasource import (
     PostgresDatasource,
 )
 from great_expectations.experimental.datasources.sql_datasource import (
-    ColumnSplitter,
-    SQLAsset,
     SqlYearMonthSplitter,
     TableAsset,
 )
