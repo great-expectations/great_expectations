e4e6a3326a403f1cd4f94082767f2fbd874a0d7e
Gabriel Gore <gabriel59kg@gmail.com>
2023-02-05 20:58:23 -0500

    formatting

--- great_expectations/experimental/datasources/constants.py
+++ great_expectations/experimental/datasources/constants.py
@@ -5,4 +5,7 @@ from typing_extensions import Final
 # these fields must be added to `__fields_set__` before pydantic model serialization
 # methods are called. Otherwise it could be excluded.
 # https://docs.pydantic.dev/usage/exporting_models/#modeldict
-_FIELDS_ALWAYS_SET: Final[Set[str]] = {"assets", "type"}
+_FIELDS_ALWAYS_SET: Final[Set[str]] = {
+    "assets",
+    "type",
+}
