2e0d07467b2b38d97a656eb08fc4e440bba7b60e
Gabriel <gabriel59kg@gmail.com>
2023-02-06 11:48:56 -0500

    [MAINTENANCE] fix - `FutureWarning: pandas.Float64Index is deprecated` when importing `great_expectations` (#7055)

--- great_expectations/experimental/datasources/dynamic_pandas.py
+++ great_expectations/experimental/datasources/dynamic_pandas.py
@@ -6,6 +6,7 @@ import inspect
 import logging
 import pathlib
 import re
+import warnings
 from collections import defaultdict
 from pprint import pformat as pf
 from typing import (
@@ -168,7 +169,16 @@ _TYPE_REF_LOCALS: Final[Dict[str, Type]] = {
 def _extract_io_methods(
     whitelist: Optional[Sequence[str]] = None,
 ) -> List[Tuple[str, DataFrameFactoryFn]]:
-    member_functions = inspect.getmembers(pd, predicate=inspect.isfunction)
+    # suppress pandas future warnings that may be emitted by collecting
+    # pandas io methods
+    # Once the context manager exits, the warning filter is removed.
+    # Do not remove this context-manager.
+    # https://docs.python.org/3/library/warnings.html#temporarily-suppressing-warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter(action="ignore", category=FutureWarning)
+
+        member_functions = inspect.getmembers(pd, predicate=inspect.isfunction)
+    # filter removed
     if whitelist:
         return [t for t in member_functions if t[0] in whitelist]
     return [t for t in member_functions if t[0].startswith("read_")]
