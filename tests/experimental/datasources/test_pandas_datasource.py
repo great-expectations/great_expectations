ca04c836bb7635eaa495785a23b901c189e873d3
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-06 22:51:36 -0800

    type hints

--- tests/experimental/datasources/test_pandas_datasource.py
+++ tests/experimental/datasources/test_pandas_datasource.py
@@ -155,7 +155,7 @@ class TestDynamicPandasAssets:
         This is also a proxy for testing that the dynamic pydantic model creation was successful.
         """
         with pytest.raises(pydantic.ValidationError) as exc_info:
-            asset_class(  # type: ignore[call-arg] # type has a default
+            asset_class(
                 name="test",
                 base_directory=pathlib.Path(__file__),
                 regex=re.compile(r"yellow_tripdata_sample_(\d{4})-(\d{2})"),
@@ -257,10 +257,10 @@ def test_add_csv_asset_to_datasource(
 @pytest.mark.unit
 def test_construct_csv_asset_directly(csv_path: pathlib.Path):
     # noinspection PyTypeChecker
-    asset = CSVAsset(  # type: ignore[call-arg] # no type
+    asset = CSVAsset(
         name="csv_asset",
         base_directory=csv_path,
-        regex=r"yellow_tripdata_sample_(\d{4})-(\d{2}).csv",  # type: ignore[arg-type]
+        regex=r"yellow_tripdata_sample_(\d{4})-(\d{2}).csv",
     )
     assert asset.name == "csv_asset"
     assert asset.base_directory == csv_path
@@ -503,7 +503,7 @@ def datasource_test_connection_error_messages(
     pandas_datasource: PandasDatasource, request
 ) -> tuple[PandasDatasource, TestConnectionError]:
     base_directory, regex, test_connection_error = request.param()
-    csv_asset = CSVAsset(  # type: ignore[call-arg]
+    csv_asset = CSVAsset(
         name="csv_asset",
         base_directory=base_directory,
         regex=regex,
