2827820001c7749e2a542e982afd34ca7743f373
Chetan Kini <chetan@superconductive.com>
2023-02-07 10:35:07 -0500

    [FEATURE] Add `expectation_suite` arg to `DataContext` `ExpectationSuite` CRUD (#7059)

--- great_expectations/core/expectation_suite.py
+++ great_expectations/core/expectation_suite.py
@@ -70,7 +70,9 @@ logger = logging.getLogger(__name__)
 @public_api
 @deprecated_argument(argument_name="data_asset_type", version="0.14.0")
 @new_argument(
-    argument_name="ge_cloud_id", version="0.13.33", message="Used in cloud deployments."
+    argument_name="ge_cloud_id",
+    version="0.13.33",
+    message="Used in GX Cloud deployments.",
 )
 class ExpectationSuite(SerializableDictDot):
     """Suite of expectations plus create, read, update, and delete functionality.
