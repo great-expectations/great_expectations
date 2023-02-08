fac919ad370c806b6e411ace631a922e41bdc90f
Chetan Kini <chetan@superconductive.com>
2023-02-08 13:18:40 -0500

    docs: update table

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -195,18 +195,42 @@ class AbstractDataContext(ConfigPeer, ABC):
        :widths: 25 25 50
        :header-rows: 1
 
-       * -
-         - Stores
-         - Datasources
-         - ExpectationSuites
-         - Checkpoints
-         - Profilers
-       * - get
-         - add
-         - update
-         - add_or_update
-         - delete
-         -
+       * - Heading row 1, column 1
+         - Heading row 1, column 2
+         - Heading row 1, column 3
+         - Heading row 1, column 4
+         - Heading row 1, column 5
+         - Heading row 1, column 6
+       * - Row 1, column 1
+         - Row 1, column 2
+         - Row 1, column 3
+         - Row 1, column 4
+         - Row 1, column 5
+         - Row 1, column 6
+       * - Row 2, column 1
+         - Row 2, column 2
+         - Row 2, column 3
+         - Row 2, column 4
+         - Row 2, column 5
+         - Row 2, column 6
+       * - Row 3, column 1
+         - Row 3, column 2
+         - Row 3, column 3
+         - Row 3, column 4
+         - Row 3, column 5
+         - Row 3, column 6
+       * - Row 4, column 1
+         - Row 4, column 2
+         - Row 4, column 3
+         - Row 4, column 4
+         - Row 4, column 5
+         - Row 4, column 6
+       * - Row 5, column 1
+         - Row 5, column 2
+         - Row 5, column 3
+         - Row 5, column 4
+         - Row 5, column 5
+         - Row 5, column 6
     """
 
     # NOTE: <DataContextRefactor> These can become a property like ExpectationsStore.__name__ or placed in a separate
