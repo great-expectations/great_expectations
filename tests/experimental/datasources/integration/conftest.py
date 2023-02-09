2dada4f33f4946e3a9d0e0b85355ae8595f1982b
Nathan Farmer <nathan@greatexpectations.io>
2023-02-09 12:30:41 -0500

    Move base_directory in another fixture

--- tests/experimental/datasources/integration/conftest.py
+++ tests/experimental/datasources/integration/conftest.py
@@ -25,10 +25,12 @@ def pandas_data(
     csv_path = (
         pathlib.Path(__file__).parent.joinpath(relative_path).resolve(strict=True)
     )
-    pandas_ds = context.sources.add_pandas(name="my_pandas")
+    pandas_ds = context.sources.add_pandas_filesystem(
+        name="my_pandas",
+        base_directory=csv_path,
+    )
     asset = pandas_ds.add_csv_asset(
         name="csv_asset",
-        base_directory=csv_path,
         regex=r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})\.csv",
         order_by=["year", "month"],
     )
@@ -105,10 +107,12 @@ def multibatch_pandas_data(
     csv_path = (
         pathlib.Path(__file__).parent.joinpath(relative_path).resolve(strict=True)
     )
-    pandas_ds = context.sources.add_pandas(name="my_pandas")
+    pandas_ds = context.sources.add_pandas_filesystem(
+        name="my_pandas",
+        base_directory=csv_path,
+    )
     asset = pandas_ds.add_csv_asset(
         name="csv_asset",
-        base_directory=csv_path,
         regex=r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})\.csv",
         order_by=["year", "month"],
     )
