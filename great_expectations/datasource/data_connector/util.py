1c9609bcb57a135301e0ae766934fab78ec3ec03
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-09 08:41:47 -0800

    cleanup

--- great_expectations/datasource/data_connector/util.py
+++ great_expectations/datasource/data_connector/util.py
@@ -65,11 +65,13 @@ def batch_definition_matches_batch_request(
         and batch_request.datasource_name != batch_definition.datasource_name
     ):
         return False
+
     if (
         batch_request.data_connector_name
         and batch_request.data_connector_name != batch_definition.data_connector_name
     ):
         return False
+
     if (
         batch_request.data_asset_name
         and batch_request.data_asset_name != batch_definition.data_asset_name
@@ -272,14 +274,12 @@ def _invert_regex_to_data_reference_template(
         if token == sre_constants.LITERAL:
             # Transcribe the character directly into the template
             data_reference_template += chr(value)
-
         elif token == sre_constants.SUBPATTERN:
             if not (group_name_index < num_groups):
                 break
             # Replace the captured group with "{next_group_name}" in the template
             data_reference_template += f"{{{group_names[group_name_index]}}}"
             group_name_index += 1
-
         elif token in [
             sre_constants.MAX_REPEAT,
             sre_constants.IN,
@@ -288,7 +288,6 @@ def _invert_regex_to_data_reference_template(
         ]:
             # Replace the uncaptured group a wildcard in the template
             data_reference_template += "*"
-
         elif token in [
             sre_constants.AT,
             sre_constants.ASSERT_NOT,
@@ -334,10 +333,12 @@ def sanitize_prefix_for_s3(text: str) -> str:
     path_parts = text.split("/")
     if not path_parts:  # Empty prefix
         return text
-    elif "." in path_parts[-1]:  # File, not folder
+
+    if "." in path_parts[-1]:  # File, not folder
         return text
-    else:  # Folder, should have trailing /
-        return f"{text.rstrip('/')}/"
+
+    # Folder, should have trailing /
+    return f"{text.rstrip('/')}/"
 
 
 def normalize_directory_path(
@@ -466,6 +467,7 @@ def list_gcs_keys(
         name: str = blob.name
         if name.endswith("/"):  # GCS includes directories in blob output
             continue
+
         keys.append(name)
 
     return keys
@@ -505,6 +507,7 @@ def list_s3_keys(
             item["Key"] for item in s3_objects_info["Contents"] if item["Size"] > 0
         ]
         yield from keys
+
     if recursive and "CommonPrefixes" in s3_objects_info:
         common_prefixes: List[Dict[str, Any]] = s3_objects_info["CommonPrefixes"]
         for prefix_info in common_prefixes:
@@ -517,6 +520,7 @@ def list_s3_keys(
                 iterator_dict={},
                 recursive=recursive,
             )
+
     if s3_objects_info["IsTruncated"]:
         iterator_dict["continuation_token"] = s3_objects_info["NextContinuationToken"]
         # Recursively fetch more
@@ -543,8 +547,10 @@ def build_sorters_from_config(config_list: List[Dict[str, Any]]) -> Optional[dic
             # if sorters were not configured
             if sorter_config is None:
                 return None
+
             if "name" not in sorter_config:
                 raise ValueError("Sorter config should have a name")
+
             sorter_name: str = sorter_config["name"]
             new_sorter: Sorter = _build_sorter_from_config(sorter_config=sorter_config)
             sorter_dict[sorter_name] = new_sorter
