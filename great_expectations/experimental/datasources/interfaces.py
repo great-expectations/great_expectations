6d749ef734ccc37bdd71303fb8f269b72dc976c0
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-11 00:51:34 -0800

    use V3 DataConnector utilities wherever possible; also remove several circular dependencies and be able to use MetricsCalculator in new Datasource modules

--- great_expectations/experimental/datasources/interfaces.py
+++ great_expectations/experimental/datasources/interfaces.py
@@ -507,7 +507,7 @@ class Batch(ExperimentalBaseModel):
     @validate_arguments
     def head(
         self,
-        n_rows: StrictInt = None,
+        n_rows: StrictInt = 5,
         fetch_all: StrictBool = False,
     ) -> HeadData:
         """Return the first n rows of this Batch.
