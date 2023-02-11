026170ffdad198efb4ff1f95799b3796460d8562
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-10 18:10:08 -0800

    use V3 DataConnector utilities wherever possible; also remove several circular dependencies and be able to use MetricsCalculator in new Datasource modules

--- tests/experimental/datasources/data_asset/data_connector/test_s3_data_connector.py
+++ tests/experimental/datasources/data_asset/data_connector/test_s3_data_connector.py
@@ -10,8 +10,8 @@ from moto import mock_s3
 from great_expectations.core import IDDict
 from great_expectations.core.batch import BatchDefinition
 from great_expectations.datasource.data_connector.util import (
-    sanitize_prefix_for_s3,
     sanitize_prefix,
+    sanitize_prefix_for_s3,
 )
 from great_expectations.experimental.datasources.data_asset.data_connector.data_connector import (
     DataConnector,
