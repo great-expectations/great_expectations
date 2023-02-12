99adb4c8af232ba3eedc12d79062d2b29cbfd8ef
Gabriel Gore <gabriel59kg@gmail.com>
2023-02-11 22:19:25 -0500

    remove references to `base_directory` at data asset level

--- great_expectations/experimental/datasources/file_path_data_asset.py
+++ great_expectations/experimental/datasources/file_path_data_asset.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import dataclasses
 import logging
-import pathlib
 from pprint import pformat as pf
 from typing import TYPE_CHECKING, ClassVar, Dict, List, Optional, Pattern, Set, Union
 
@@ -78,10 +77,6 @@ class _FilePathDataAsset(DataAsset):
             self._regex_parser.get_all_group_index_to_group_name_mapping()
         )
         self._all_group_names = self._regex_parser.get_all_group_names()
-
-    @property
-    def _base_directory(self) -> pathlib.Path:
-        return self._datasource.base_directory
 
     def batch_request_options_template(
         self,
