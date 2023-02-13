5b5fd2e0991d867e61ca156a237230daf5246f6a
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-13 11:49:25 -0800

    mark data connector file path tests as @integration

--- great_expectations/datasource/data_connector/inferred_asset_filesystem_data_connector.py
+++ great_expectations/datasource/data_connector/inferred_asset_filesystem_data_connector.py
@@ -80,7 +80,7 @@ class InferredAssetFilesystemDataConnector(InferredAssetFilePathDataConnector):
         return str(Path(self.base_directory).joinpath(path))
 
     @property
-    def base_directory(self):
+    def base_directory(self) -> str:
         """
         Accessor method for base_directory. If directory is a relative path, interpret it as relative to the
         root directory. If it is absolute, then keep as-is.
