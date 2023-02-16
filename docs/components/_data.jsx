ea814fc75431b0b62d9ccf9527a4029832424b39
Bill Dirks <bill@greatexpectations.io>
2023-02-16 13:17:45 -0800

    Update _data.jsx

--- docs/components/_data.jsx
+++ docs/components/_data.jsx
@@ -1,9 +1,4 @@
 export default {
-  release_version: 'great_expectations, version 0.15.48',
-  min_python: 'Python 3.7',
-  max_python: 'Python 3.10'
-}
-export default {
   release_version: 'great_expectations, version 0.15.49',
   min_python: 'Python 3.7',
   max_python: 'Python 3.10'
