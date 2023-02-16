6b69ea82cee9863218df554bc78b6f9078da2085
Chetan Kini <chetan@superconductive.com>
2023-02-16 10:58:21 -0500

    chore: revert rbp change

--- tests/integration/profiling/rule_based_profiler/test_profiler_basic_workflows.py
+++ tests/integration/profiling/rule_based_profiler/test_profiler_basic_workflows.py
@@ -302,7 +302,7 @@ def test_profiler_save_and_load(data_context_with_taxi_data):
         "variables": {},
     }
     my_rbp.add_rule(rule=simple_variables_rule)
-    context.add_profiler(profiler=my_rbp)
+    context.save_profiler(profiler=my_rbp)
 
     # load profiler from store
     my_loaded_profiler: RuleBasedProfiler = context.get_profiler(name="my_rbp")
@@ -326,7 +326,7 @@ def test_profiler_save_and_load(data_context_with_taxi_data):
                         "_amount",
                     ],
                 },
-                "variables": None,
+                "variables": {},
                 "parameter_builders": [
                     {
                         "module_name": "great_expectations.rule_based_profiler.parameter_builder.metric_multi_batch_parameter_builder",
