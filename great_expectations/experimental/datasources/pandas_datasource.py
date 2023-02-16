67ee5ecd6402e2c1c7c4ed8846f0396b94cbec3e
Nathan Farmer <nathan@greatexpectations.io>
2023-02-16 12:56:45 -0500

    Use asset type_prefix solution over namespacing for now

--- great_expectations/experimental/datasources/pandas_datasource.py
+++ great_expectations/experimental/datasources/pandas_datasource.py
@@ -115,6 +115,7 @@ _PANDAS_ASSET_MODELS = _generate_pandas_data_asset_models(
     blacklist=_PANDAS_BLACK_LIST,
     use_docstring_from_method=True,
     skip_first_param=False,
+    type_prefix="pandas_",
 )
 
 try:
