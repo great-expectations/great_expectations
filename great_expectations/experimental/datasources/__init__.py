d4d77cad0533106fcada87b76dd0812f96bd9093
Alex Sherstinsky <alex@greatexpectations.io>
2023-02-21 07:31:37 -0800

    WIP

--- great_expectations/experimental/datasources/__init__.py
+++ great_expectations/experimental/datasources/__init__.py
@@ -1,12 +1,9 @@
 import pathlib
 
 from great_expectations.experimental.datasources.pandas_datasource import (
-    # TODO: <Alex>ALEX-THIS_IS_NOT_USED</Alex>
     PandasDatasource,
     _PandasDatasource,
 )
-
-# TODO: <Alex>ALEX</Alex>
 from great_expectations.experimental.datasources.pandas_filesystem_datasource import (
     PandasFilesystemDatasource,
 )
