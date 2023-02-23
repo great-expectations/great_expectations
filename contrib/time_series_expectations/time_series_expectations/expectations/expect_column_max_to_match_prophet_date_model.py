d0121be8643eec808df7621c0a625f5a93f827a8
Abe Gong <abegong@gmail.com>
2023-02-23 15:08:04 -0700

    Drop \ line connectors. Add docstring for ExpectColumnMaxToMatchProphetDateModel

--- contrib/time_series_expectations/time_series_expectations/expectations/expect_column_max_to_match_prophet_date_model.py
+++ contrib/time_series_expectations/time_series_expectations/expectations/expect_column_max_to_match_prophet_date_model.py
@@ -8,7 +8,44 @@ with open(file_relative_path(__file__, "example_prophet_date_model.json")) as f_
 
 
 class ExpectColumnMaxToMatchProphetDateModel(ColumnAggregateTimeSeriesExpectation):
-    """This Expectation checks to see if the max of a column matches the predictions of a prophet model for a given date."""
+    """This Expectation checks to see if the max of a column matches the predictions of a prophet model for a given date.
+
+    expect_column_max_to_match_prophet_date_model is a ColumnAggregateTimeSeriesExpectation.
+
+    Args:
+        column (str):
+            The name of the column to calculate the max of
+        date (str):
+            A string representing the date to compare the max to
+        model_json (str):
+            A string containing a JSON-serialized Prophet model
+
+    Keyword Args:
+
+    Other Parameters:
+        result_format (str or None):
+            Which output mode to use: BOOLEAN_ONLY, BASIC, COMPLETE, or SUMMARY.
+            For more detail, see [result_format](https://docs.greatexpectations.io/docs/reference/expectations/result_format).
+        include_config (boolean):
+            If True, then include the expectation config as part of the result object.
+        catch_exceptions (boolean or None):
+            If True, then catch exceptions and include them as part of the result object.
+            For more detail, see [catch_exceptions](https://docs.greatexpectations.io/docs/reference/expectations/standard_arguments/#catch_exceptions).
+        meta (dict or None):
+            A JSON-serializable dictionary (nesting allowed) that will be included in the output without
+            modification. For more detail, see [meta](https://docs.greatexpectations.io/docs/reference/expectations/standard_arguments/#meta).
+
+    Returns:
+        An [ExpectationSuiteValidationResult](https://docs.greatexpectations.io/docs/terms/validation_result)
+
+        Exact fields vary depending on the values passed to result_format, include_config, catch_exceptions, and meta.
+
+    Notes:
+        * Prophet is an open source forecasting library created at facebook. For more information, please see the [project github page](https://github.com/facebook/prophet).
+        * I apologize for the dangling prepositions in the Arg docstrings for this Expectation.
+        
+    
+    """
 
     examples = [
         {
