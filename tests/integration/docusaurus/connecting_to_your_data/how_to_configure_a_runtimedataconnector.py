1ac346bd747faec2a5446fff1996056fd610bacc
Will Shin <will@superconductive.com>
2023-02-22 17:08:44 -0800

    lets try underscores

--- tests/integration/docusaurus/connecting_to_your_data/how_to_configure_a_runtimedataconnector.py
+++ tests/integration/docusaurus/connecting_to_your_data/how_to_configure_a_runtimedataconnector.py
@@ -67,8 +67,8 @@ batch_request = RuntimeBatchRequest(
     datasource_name="taxi_datasource",
     data_connector_name="default_runtime_data_connector_name",
     data_asset_name="YOUR MEANINGFUL NAME",  # This can be anything that identifies this data_asset for you
-    runtime_parameters={"path": "PATH TO YOUR DATA HERE"},  # Add your path here.
-    batch_identifiers={"default_identifier_name": "YOUR MEANINGFUL IDENTIFIER"},
+    runtime_parameters={"path": "PATH_TO_YOUR_DATA_HERE"},  # Add your path here.
+    batch_identifiers={"default_identifier_name": "YOUR_MEANINGFUL_IDENTIFIER"},
 )
 # </snippet>
 
@@ -81,7 +81,7 @@ batch_request.runtime_parameters[
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_configure_a_runtimedataconnector.py get_validator 1">
 validator = context.get_validator(
     batch_request=batch_request,
-    create_expectation_suite_with_name="MY EXPECTATION SUITE NAME",
+    create_expectation_suite_with_name="MY_EXPECTATION_SUITE_NAME",
 )
 print(validator.head())
 # </snippet>
@@ -96,7 +96,7 @@ assert "YOUR MEANINGFUL NAME" in set(
 )
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_configure_a_runtimedataconnector.py path">
-path = "PATH TO YOUR DATA HERE"
+path = "PATH_TO_YOUR_DATA_HERE"
 # </snippet>
 
 # Please note this override is only to provide good UX for docs and tests.
@@ -107,16 +107,16 @@ df = pd.read_csv(path)
 batch_request = RuntimeBatchRequest(
     datasource_name="taxi_datasource",
     data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="YOUR MEANINGFUL NAME",  # This can be anything that identifies this data_asset for you
+    data_asset_name="YOUR_MEANINGFUL_NAME",  # This can be anything that identifies this data_asset for you
     runtime_parameters={"batch_data": df},  # Pass your DataFrame here.
-    batch_identifiers={"default_identifier_name": "YOUR MEANINGFUL IDENTIFIER"},
+    batch_identifiers={"default_identifier_name": "YOUR_MEANINGFUL_IDENTIFIER"},
 )
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_configure_a_runtimedataconnector.py get_validator 2">
 validator = context.get_validator(
     batch_request=batch_request,
-    expectation_suite_name="MY EXPECTATION SUITE NAME",
+    expectation_suite_name="MY_EXPECTATION_SUITE_NAME",
 )
 print(validator.head())
 # </snippet>
@@ -124,7 +124,7 @@ print(validator.head())
 # NOTE: The following code is only for testing and can be ignored by users.
 assert isinstance(validator, gx.validator.validator.Validator)
 assert [ds["name"] for ds in context.list_datasources()] == ["taxi_datasource"]
-assert "YOUR MEANINGFUL NAME" in set(
+assert "YOUR_MEANINGFUL_NAME" in set(
     context.get_available_data_asset_names()["taxi_datasource"][
         "default_runtime_data_connector_name"
     ]
