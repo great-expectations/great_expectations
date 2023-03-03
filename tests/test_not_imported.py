93d27a6b89d97293a8305f3335df2e217f635bde
Bill Dirks <bill@greatexpectations.io>
2023-03-02 16:26:47 -0800

    fix test names.

--- tests/test_not_imported.py
+++ tests/test_not_imported.py
@@ -8,16 +8,16 @@ def not_imported() -> NotImported:
     return NotImported("Please install foo")
 
 
-def test_get_attr_on_not_installed_module(not_imported):
+def test_get_attr_on_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, str(not_imported)):
         not_imported.thing
 
 
-def test_set_attr_on_installed_module(not_imported):
+def test_set_attr_on_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, str(not_imported)):
         not_imported.thing = 5
 
 
-def test_calling_not_installed_module(not_imported):
+def test_calling_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, str(not_imported)):
         not_imported()
