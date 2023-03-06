ca9ab8f32bb70cf217f458eb6a5ad261b950d43c
Anthony Burdi <anthony@greatexpectations.io>
2023-03-06 11:21:42 -0500

    [MAINTENANCE] Warn if sqlalchemy version is >= 2.0.0 (#7283)
    Co-authored-by: Gabriel <gabriel59kg@gmail.com>

---
+++ great_expectations/optional_imports.py
@@ -0,0 +1,30 @@
+"""Utilities to handle optional imports and related warnings e.g. sqlalchemy.
+
+Great Expectations contains support for datasources and data stores that are
+not included in the core package by default. Support requires install of
+additional packages. To ensure these code paths are not executed when supporting
+libraries are not installed, we check for existence of the associated library.
+
+We also consolidate logic for warning based on version number in this module.
+"""
+from __future__ import annotations
+
+import warnings
+
+from packaging.version import Version
+
+
+def sqlalchemy_version_check(version: str | Version) -> None:
+    """Check if the sqlalchemy version is supported or warn if not.
+
+    Args:
+        version: sqlalchemy version as a string or Version.
+    """
+    if isinstance(version, str):
+        version = Version(version)
+
+    if version >= Version("2.0.0"):
+        warnings.warn(
+            "SQLAlchemy v2.0.0 or later is not yet supported by Great Expectations.",
+            UserWarning,
+        )
