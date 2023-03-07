1223aad08699cac60d19c8af61f669218bfc7ca3
Anthony Burdi <anthony@greatexpectations.io>
2023-03-07 15:37:32 -0500

    Make executable

--- ci/checks/run_gx_airflow_operator_tests.sh
+++ ci/checks/run_gx_airflow_operator_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run unit tests defined in the Great Expectations Airflow Operator
+# Run tests defined in the Great Expectations Airflow Operator
 
 git clone git@github.com:astronomer/airflow-provider-great-expectations.git
 
