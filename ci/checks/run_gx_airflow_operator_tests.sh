c3a26c393f2cbb902a13990899ffc6337178d6b3
Anthony Burdi <anthony@greatexpectations.io>
2023-03-07 16:02:52 -0500

    pip install gx from repo, not pypi

--- ci/checks/run_gx_airflow_operator_tests.sh
+++ ci/checks/run_gx_airflow_operator_tests.sh
@@ -2,6 +2,16 @@
 
 # Run tests defined in the Great Expectations Airflow Operator
 
+# Install GX from it's repo
+cd ../../
+pip install -c constraints-dev.txt -e ".[test]"
+
+echo "Using great expectations version vvv"
+pip freeze | grep great-expectations
+ls
+echo "Using great expectations version ^^^"
+
+# Install the airflow provider from it's repo
 git clone https://github.com/astronomer/airflow-provider-great-expectations.git
 
 cd airflow-provider-great-expectations
