9d0c5d8c8c63ce148f852224bdb5aa8de3513912
Anthony Burdi <anthony@greatexpectations.io>
2023-03-07 11:53:28 -0500

    Add logging to see if we are using gx at the commit in the PR (and not pip installing the latest release)

--- ci/checks/run_gx_airflow_operator_tests.sh
+++ ci/checks/run_gx_airflow_operator_tests.sh
@@ -6,6 +6,12 @@ git clone git@github.com:astronomer/airflow-provider-great-expectations.git
 
 cd airflow-provider-great-expectations
 
+echo "Using great expectations version:"
+pip freeze | grep great-expectations
+
 pip install -e '.[tests]'
+
+echo "Using great expectations version:"
+pip freeze | grep great-expectations
 
 pytest
