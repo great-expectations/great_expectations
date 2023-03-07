1fb2acec0d82a421b27dfad4ebfeefc68bf0aeb8
Anthony Burdi <anthony@greatexpectations.io>
2023-03-07 15:50:03 -0500

    Use https instead of ssh

--- ci/checks/run_gx_airflow_operator_tests.sh
+++ ci/checks/run_gx_airflow_operator_tests.sh
@@ -2,16 +2,18 @@
 
 # Run tests defined in the Great Expectations Airflow Operator
 
-git clone git@github.com:astronomer/airflow-provider-great-expectations.git
+git clone https://github.com/astronomer/airflow-provider-great-expectations.git
 
 cd airflow-provider-great-expectations
 
-echo "Using great expectations version:"
+echo "Using great expectations version vvv"
 pip freeze | grep great-expectations
+echo "Using great expectations version ^^^"
 
 pip install -e '.[tests]'
 
-echo "Using great expectations version:"
+echo "Using great expectations version vvv"
 pip freeze | grep great-expectations
+echo "Using great expectations version ^^^"
 
 pytest
