dcabb2e67fca76e5458ae872cb5cf2eb2579f5ec
Abe Gong <abegong@users.noreply.github.com>
2023-03-09 07:14:24 -0700

    Fix a bug in the hourly generator. Add hourly functionality to genera… (#7305)

--- contrib/time_series_expectations/assets/generate_test_time_series_data.py
+++ contrib/time_series_expectations/assets/generate_test_time_series_data.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from great_expectations.data_context.util import file_relative_path
-from time_series_expectations.generator.daily_time_series_expectations import (
+from time_series_expectations.generator import (
     DailyTimeSeriesGenerator,
+    HourlyTimeSeriesGenerator,
 )
 
 plt.rcParams["figure.figsize"] = (20, 3)
@@ -19,6 +20,7 @@ def generate_time_series_data_and_plot(
     intercept: float,
     # noise,
     trend: float,
+    hourly_seasonality: float,
     weekly_seasonality: float,
     # annual_seasonality,
     outlier_alpha: float,
@@ -26,23 +28,47 @@ def generate_time_series_data_and_plot(
     filename = f"{grain}__size_{size}__trend_{trend}__weekly_seasonality_{weekly_seasonality}__outliers_{outlier_alpha}"
     print(filename)
 
-    generator = DailyTimeSeriesGenerator()
-    df = generator.generator_df(
-        size=size,
-        trend_params=[
-            {
-                "alpha": intercept,
-                "beta": trend,
-                "cutpoint": size,
-            }
-        ],
-        weekday_dummy_params=[
-            weekly_seasonality * x for x in [0, 0.5, 3.5, 4, 5, -3, -3.5]
-        ],
-        annual_seasonality_params=[],
-        holiday_alpha=1000,
-        outlier_alpha=outlier_alpha,
-    )
+    if grain == "daily":
+        generator = DailyTimeSeriesGenerator()
+        df = generator.generate_df(
+            size=size,
+            trend_params=[
+                {
+                    "alpha": intercept,
+                    "beta": trend,
+                    "cutpoint": size,
+                }
+            ],
+            weekday_dummy_params=[
+                weekly_seasonality * x for x in [0, 0.5, 3.5, 4, 5, -3, -3.5]
+            ],
+            annual_seasonality_params=[],
+            holiday_alpha=1000,
+            outlier_alpha=outlier_alpha,
+        )
+
+    elif grain == "hourly":
+        generator = HourlyTimeSeriesGenerator()
+        df = generator.generate_df(
+            size=size,
+            trend_params=[
+                {
+                    "alpha": intercept,
+                    "beta": trend,
+                    "cutpoint": size,
+                }
+            ],
+            hourly_seasonality=hourly_seasonality,
+            weekday_dummy_params=[
+                weekly_seasonality * x for x in [0, 0.5, 3.5, 4, 5, -3, -3.5]
+            ],
+            annual_seasonality_params=[],
+            holiday_alpha=1000,
+            outlier_alpha=outlier_alpha,
+        )
+
+    else:
+        raise ValueError(f"Invalid grain: {grain}")
 
     df.to_csv(
         os.path.join(  # noqa: PTH118
@@ -70,6 +96,7 @@ generate_time_series_data_and_plot(
     size=180,
     intercept=100,
     trend=0,
+    hourly_seasonality=0,
     weekly_seasonality=0,
     outlier_alpha=1000,
 )
@@ -79,6 +106,7 @@ generate_time_series_data_and_plot(
     size=180,
     intercept=100,
     trend=0.05,
+    hourly_seasonality=0,
     weekly_seasonality=0,
     outlier_alpha=1000,
 )
@@ -88,6 +116,7 @@ generate_time_series_data_and_plot(
     size=180,
     intercept=100,
     trend=0.05,
+    hourly_seasonality=0,
     weekly_seasonality=1,
     outlier_alpha=1000,
 )
@@ -99,6 +128,48 @@ generate_time_series_data_and_plot(
     size=180,
     intercept=100,
     trend=0.05,
+    hourly_seasonality=0,
+    weekly_seasonality=1,
+    outlier_alpha=2,
+)
+
+
+generate_time_series_data_and_plot(
+    grain="hourly",
+    size=24 * 180,
+    intercept=100,
+    trend=0.05,
+    hourly_seasonality=3,
+    weekly_seasonality=1,
+    outlier_alpha=1000,
+)
+
+generate_time_series_data_and_plot(
+    grain="hourly",
+    size=24 * 180,
+    intercept=100,
+    trend=0.05,
+    hourly_seasonality=3,
+    weekly_seasonality=1,
+    outlier_alpha=4,
+)
+
+generate_time_series_data_and_plot(
+    grain="hourly",
+    size=24 * 180,
+    intercept=100,
+    trend=0.05,
+    hourly_seasonality=3,
+    weekly_seasonality=1,
+    outlier_alpha=3,
+)
+
+generate_time_series_data_and_plot(
+    grain="hourly",
+    size=24 * 180,
+    intercept=100,
+    trend=0.05,
+    hourly_seasonality=3,
     weekly_seasonality=1,
     outlier_alpha=2,
 )
