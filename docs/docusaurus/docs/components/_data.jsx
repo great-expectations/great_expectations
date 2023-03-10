aef2e16d9214d5b1954f98410f088c40a6f8cd7f
Nathan Farmer <nathan@greatexpectations.io>
2023-03-10 09:30:23 -0500

    release prep

--- docs/docusaurus/docs/components/_data.jsx
+++ docs/docusaurus/docs/components/_data.jsx
@@ -3,3 +3,18 @@ export default {
   min_python: 'Python 3.7',
   max_python: 'Python 3.10'
 }
+export default {
+  release_version: 'great_expectations, version 0.16.0',
+  min_python: 'Python 3.7',
+  max_python: 'Python 3.10'
+}
+export default {
+  release_version: 'great_expectations, version 0.16.0',
+  min_python: 'Python 3.7',
+  max_python: 'Python 3.10'
+}
+export default {
+  release_version: 'great_expectations, version 0.16.0',
+  min_python: 'Python 3.7',
+  max_python: 'Python 3.10'
+}
