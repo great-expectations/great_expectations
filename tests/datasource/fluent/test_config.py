f1bb1b1e83612271145819522a7c877ccc56a169
Alex Sherstinsky <alex@greatexpectations.io>
2023-03-13 10:09:27 -0700

    clean up

--- tests/datasource/fluent/test_config.py
+++ tests/datasource/fluent/test_config.py
@@ -531,16 +531,16 @@ def test_dict_config_round_trip(
 
 
 def test_json_config_round_trip(
-    inject_engine_lookup_double, from_dict_gx_config: GxConfig
+    inject_engine_lookup_double, from_json_gx_config: GxConfig
 ):
-    dumped: str = from_dict_gx_config.json(indent=2)
+    dumped: str = from_json_gx_config.json(indent=2)
     print(f"  Dumped JSON ->\n\n{dumped}\n")
 
     re_loaded: GxConfig = GxConfig.parse_raw(dumped)
     pp(re_loaded)
     assert re_loaded
 
-    assert from_dict_gx_config.dict() == re_loaded.dict()
+    assert from_json_gx_config.dict() == re_loaded.dict()
 
 
 def test_yaml_config_round_trip(
@@ -577,9 +577,9 @@ def test_yaml_file_config_round_trip(
 
 
 def test_splitters_deserialization(
-    inject_engine_lookup_double, from_dict_gx_config: GxConfig
+    inject_engine_lookup_double, from_json_gx_config: GxConfig
 ):
-    table_asset: TableAsset = from_dict_gx_config.datasources["my_pg_ds"].assets[
+    table_asset: TableAsset = from_json_gx_config.datasources["my_pg_ds"].assets[
         "with_splitter"
     ]
     assert isinstance(table_asset.splitter, SplitterYearAndMonth)
@@ -600,9 +600,9 @@ def test_yaml_config_round_trip_ordering(
 
 @pytest.mark.xfail(reason="Custom Sorter serialization logic needs to be implemented")
 def test_custom_sorter_serialization(
-    inject_engine_lookup_double, from_dict_gx_config: GxConfig
+    inject_engine_lookup_double, from_json_gx_config: GxConfig
 ):
-    dumped: str = from_dict_gx_config.json(indent=2)
+    dumped: str = from_json_gx_config.json(indent=2)
     print(f"  Dumped JSON ->\n\n{dumped}\n")
 
     expected_sorter_strings: List[str] = COMPLEX_CONFIG_DICT["fluent_datasources"][  # type: ignore[index]
