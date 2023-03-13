4f10968b04d345ad0e5ef7f9f7aa9349c63e623c
Gabriel Gore <gabriel59kg@gmail.com>
2023-03-13 11:15:01 -0400

    remove uneeded logic from `add_asset`

--- great_expectations/datasource/fluent/interfaces.py
+++ great_expectations/datasource/fluent/interfaces.py
@@ -29,9 +29,6 @@ from pydantic import dataclasses as pydantic_dc
 from typing_extensions import TypeAlias, TypeGuard
 
 from great_expectations.core.id_dict import BatchSpec  # noqa: TCH001
-from great_expectations.datasource.fluent.constants import (
-    _ASSETS_KEY,
-)
 from great_expectations.datasource.fluent.fluent_base_model import (
     FluentBaseModel,
 )
@@ -464,10 +461,6 @@ class Datasource(
         asset.test_connection()
 
         self.assets[asset.name] = asset
-
-        # pydantic needs to know that an asset has been set so that it doesn't get excluded
-        # when dumping to dict, json, yaml etc.
-        self.__fields_set__.add(_ASSETS_KEY)
 
         return asset
 
