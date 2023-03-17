9b5348c6e7eb8d7cd236c098d7f4e6a0dcb4242f
Anthony Burdi <anthony@greatexpectations.io>
2023-03-17 11:54:15 -0400

    Fix tests

--- great_expectations/core/usage_statistics/package_dependencies.py
+++ great_expectations/core/usage_statistics/package_dependencies.py
@@ -241,21 +241,22 @@ class GXDependencies:
         return {name.lower() for name in self.GX_DEV_DEPENDENCIES}
 
     def get_required_dependency_names_from_requirements_file(self) -> List[str]:
-        """Get unique names of required dependencies.
+        """Get unique names of required dependencies. Lowercase names.
 
         Returns:
             List of string names of required dependencies.
         """
         return sorted(
-            set(
-                self._get_dependency_names_from_requirements_file(
+            {
+                name.lower()
+                for name in self._get_dependency_names_from_requirements_file(
                     self._requirements_paths[self.PRIMARY_REQUIREMENTS_FILE]
                 )
-            )
+            }
         )
 
     def get_dev_dependency_names_from_requirements_file(self) -> List[str]:
-        """Get unique names of dependencies from all dev requirements files.
+        """Get unique lowercase names of dependencies from all dev requirements files.
         Returns:
             List of string names of dev dependencies.
         """
@@ -272,7 +273,7 @@ class GXDependencies:
                 dev_dependency_path.absolute()
             )
             dev_dependency_names.update(dependency_names)
-        return sorted(dev_dependency_names)
+        return sorted(name.lower() for name in dev_dependency_names)
 
     def _get_dependency_names_from_requirements_file(
         self, filepath: pathlib.Path
