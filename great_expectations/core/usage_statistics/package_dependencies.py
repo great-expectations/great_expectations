ac0ce8799e569854b783b2e90ea7464b17c114c4
Anthony Burdi <anthony@greatexpectations.io>
2023-03-17 07:20:20 -0400

    First pass fix

--- great_expectations/core/usage_statistics/package_dependencies.py
+++ great_expectations/core/usage_statistics/package_dependencies.py
@@ -234,11 +234,11 @@ class GXDependencies:
 
     def get_required_dependency_names(self) -> List[str]:
         """Sorted list of required GX dependencies"""
-        return self.GX_REQUIRED_DEPENDENCIES
+        return [name.lower() for name in self.GX_REQUIRED_DEPENDENCIES]
 
     def get_dev_dependency_names(self) -> Set[str]:
         """Set of dev GX dependencies"""
-        return self.GX_DEV_DEPENDENCIES
+        return {name.lower() for name in self.GX_DEV_DEPENDENCIES}
 
     def get_required_dependency_names_from_requirements_file(self) -> List[str]:
         """Get unique names of required dependencies.
