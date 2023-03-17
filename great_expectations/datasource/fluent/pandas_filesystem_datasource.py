596089a4264e252d4290f1b7d0984e66a22c4804
Gabriel Gore <gabriel59kg@gmail.com>
2023-03-16 22:00:20 -0400

    remove `add_asset` methods from pandas fs

--- great_expectations/datasource/fluent/pandas_filesystem_datasource.py
+++ great_expectations/datasource/fluent/pandas_filesystem_datasource.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 import logging
 import pathlib
-import re
-from typing import ClassVar, Optional, Type, TypeVar, Union
+from typing import ClassVar, Optional, Type, TypeVar
 
 from typing_extensions import Literal
 
@@ -16,14 +15,8 @@ from great_expectations.datasource.fluent.file_path_data_asset import (
     _FilePathDataAsset,
 )
 from great_expectations.datasource.fluent.interfaces import (
-    Sorter,
-    SortersDefinition,
     TestConnectionError,
 )
-from great_expectations.datasource.fluent.pandas_file_path_datasource import (
-    ParquetAsset,
-)
-from great_expectations.datasource.fluent.signatures import _merge_signatures
 
 logger = logging.getLogger(__name__)
 
@@ -62,57 +55,6 @@ class PandasFilesystemDatasource(_PandasFilePathDatasource):
             for asset in self.assets.values():
                 asset.test_connection()
 
-    # TODO: remove this before merge. Using for reference
-    @public_api
-    def add_parquet_asset(
-        self,
-        name: str,
-        batching_regex: Optional[Union[str, re.Pattern]] = None,
-        glob_directive: str = "**/*",
-        order_by: Optional[SortersDefinition] = None,
-        **kwargs,
-    ) -> ParquetAsset:  # type: ignore[valid-type]
-        """Adds a Parquet DataAsst to the present "PandasFilesystemDatasource" object.
-        Args:
-            name: The name of the Parquet asset
-            batching_regex: regex pattern that matches Parquet filenames that is used to label the batches
-            glob_directive: glob for selecting files in directory (defaults to `**/*`) or nested directories (e.g. `*/*/*.csv`)
-            order_by: sorting directive via either list[Sorter] or "+/- key" syntax: +/- (a/de)scending; + default
-            kwargs: Extra keyword arguments should correspond to ``pandas.read_parquet`` keyword args
-        """
-        # TODO: this needs to work for dynamic methods
-        # TODO: example my making these validator methods on `DataAsset`
-        # or always doing this in the dynamic `add_asset` methods
-        batching_regex_pattern: re.Pattern = self.parse_batching_regex_string(
-            batching_regex=batching_regex
-        )
-        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
-
-        asset = ParquetAsset(
-            name=name,
-            batching_regex=batching_regex_pattern,
-            order_by=order_by_sorters,
-            **kwargs,
-        )
-
-        asset._data_connector = FilesystemDataConnector.build_data_connector(
-            datasource_name=self.name,
-            data_asset_name=name,
-            batching_regex=batching_regex_pattern,
-            base_directory=self.base_directory,
-            glob_directive=glob_directive,
-            data_context_root_directory=self.data_context_root_directory,
-        )
-        asset._test_connection_error_message = (
-            FilesystemDataConnector.build_test_connection_error_message(
-                data_asset_name=name,
-                batching_regex=batching_regex_pattern,
-                glob_directive=glob_directive,
-                base_directory=self.base_directory,
-            )
-        )
-        return self.add_asset(asset=asset)
-
     def add_asset(self, asset: _DataAssetT) -> _DataAssetT:
         """
         NOTE: Method 1: override `add_asset` if the Datasource needs to use a data_connector.
@@ -142,7 +84,3 @@ class PandasFilesystemDatasource(_PandasFilePathDatasource):
             )
         )
         return super().add_asset(asset)
-
-    # attr-defined issue
-    # https://github.com/python/mypy/issues/12472
-    add_parquet_asset.__signature__ = _merge_signatures(add_parquet_asset, ParquetAsset, exclude={"type"})  # type: ignore[attr-defined]
