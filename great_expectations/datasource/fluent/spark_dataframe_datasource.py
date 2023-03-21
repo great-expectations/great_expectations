7b40504d1c25e7cf5bf7128fb6fa9c9007b582c8
Alex Sherstinsky <alex@greatexpectations.io>
2023-03-21 15:24:43 -0700

    WIP

--- great_expectations/datasource/fluent/spark_dataframe_datasource.py
+++ great_expectations/datasource/fluent/spark_dataframe_datasource.py
@@ -1,36 +1,132 @@
 from __future__ import annotations
 
+import copy
 import logging
-from typing import TYPE_CHECKING, ClassVar, Dict, List, Type
+from typing import (
+    TYPE_CHECKING,
+    ClassVar,
+    Dict,
+    Generic,
+    List,
+    Type,
+    TypeVar,
+)
 
+import pydantic
 from typing_extensions import Literal
 
+from great_expectations.core.batch_spec import RuntimeDataBatchSpec
 from great_expectations.datasource.fluent import _SparkDatasource
-from great_expectations.datasource.fluent.file_path_data_asset import (
-    _FilePathDataAsset,
+from great_expectations.datasource.fluent.constants import (
+    _DATA_CONNECTOR_NAME,
+)
+from great_expectations.datasource.fluent.interfaces import (
+    Batch,
+    BatchRequest,
+    DataAsset,
 )
 
 if TYPE_CHECKING:
-    from great_expectations.datasource.fluent.interfaces import DataAsset
+    from great_expectations.execution_engine.sparkdf_execution_engine import (
+        SparkDFExecutionEngine,
+    )
 
 
 logger = logging.getLogger(__name__)
 
 
-class CSVAsset(_FilePathDataAsset):
-    # Overridden inherited instance fields
-    type: Literal["csv"] = "csv"
+try:
+    import pyspark
+except ImportError:
+    pyspark = None  # type: ignore[assignment]
+
+    logger.debug(
+        "Unable to load pyspark; install optional spark dependency for support."
+    )
+
+# this enables us to include dataframe in the json schema
+_SparkDataFrameT = TypeVar("_SparkDataFrameT")
+
+
+class DataFrameAsset(DataAsset, Generic[_SparkDataFrameT]):
+    # instance attributes
+    type: Literal["dataframe"] = "dataframe"
+    dataframe: _SparkDataFrameT = pydantic.Field(..., exclude=True, repr=False)
+
+    class Config:
+        extra = pydantic.Extra.forbid
+
+    @pydantic.validator("dataframe")
+    def _validate_dataframe(
+        cls, dataframe: pyspark.sql.DataFrame
+    ) -> pyspark.sql.DataFrame:
+        if not isinstance(dataframe, pyspark.sql.DataFrame):
+            raise ValueError("dataframe must be of type pyspark.sql.DataFrame")
+
+        return dataframe
 
     def _get_reader_method(self) -> str:
-        return self.type
+        raise NotImplementedError(
+            """Spark DataFrameAsset does not implement "_get_reader_method()" method, because DataFrame is already available."""
+        )
 
     def _get_reader_options_include(self) -> set[str] | None:
-        return {"header", "inferSchema"}
+        raise NotImplementedError(
+            """Spark DataFrameAsset does not implement "_get_reader_options_include()" method, because DataFrame is already available."""
+        )
+
+    def get_batch_list_from_batch_request(
+        self, batch_request: BatchRequest
+    ) -> list[Batch]:
+        self._validate_batch_request(batch_request)
+
+        batch_spec = RuntimeDataBatchSpec(batch_data=self.dataframe)
+        execution_engine: SparkDFExecutionEngine = (
+            self.datasource.get_execution_engine()
+        )
+        data, markers = execution_engine.get_batch_data_and_markers(
+            batch_spec=batch_spec
+        )
+
+        # batch_definition (along with batch_spec and markers) is only here to satisfy a
+        # legacy constraint when computing usage statistics in a validator. We hope to remove
+        # it in the future.
+        # imports are done inline to prevent a circular dependency with core/batch.py
+        from great_expectations.core import IDDict
+        from great_expectations.core.batch import BatchDefinition
+
+        batch_definition = BatchDefinition(
+            datasource_name=self.datasource.name,
+            data_connector_name=_DATA_CONNECTOR_NAME,
+            data_asset_name=self.name,
+            batch_identifiers=IDDict(batch_request.options),
+            batch_spec_passthrough=None,
+        )
+
+        batch_metadata = copy.deepcopy(batch_request.options)
+
+        # Some pydantic annotations are postponed due to circular imports.
+        # Batch.update_forward_refs() will set the annotations before we
+        # instantiate the Batch class since we can import them in this scope.
+        Batch.update_forward_refs()
+
+        return [
+            Batch(
+                datasource=self.datasource,
+                data_asset=self,
+                batch_request=batch_request,
+                data=data,
+                metadata=batch_metadata,
+                legacy_batch_markers=markers,
+                legacy_batch_spec=batch_spec,
+                legacy_batch_definition=batch_definition,
+            )
+        ]
 
 
-class _SparkFilePathDatasource(_SparkDatasource):
+class SparkDataframeDatasource(_SparkDatasource):
     # class attributes
-    asset_types: ClassVar[List[Type[DataAsset]]] = [CSVAsset]
+    asset_types: ClassVar[List[Type[DataAsset]]] = [DataFrameAsset]
 
     # instance attributes
-    assets: Dict[str, _FilePathDataAsset] = {}  # type: ignore[assignment]
+    assets: Dict[str, DataFrameAsset] = {}  # type: ignore[assignment]
