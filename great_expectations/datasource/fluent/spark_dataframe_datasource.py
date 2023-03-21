21e087e8ed0436b26b0c6f27b5257a5ef840d4f3
Alex Sherstinsky <alex@greatexpectations.io>
2023-03-21 16:26:35 -0700

    Spark DataFrame Datasource

--- great_expectations/datasource/fluent/spark_dataframe_datasource.py
+++ great_expectations/datasource/fluent/spark_dataframe_datasource.py
@@ -129,7 +129,7 @@ class SparkDataframeDatasource(_SparkDatasource):
     asset_types: ClassVar[List[Type[DataAsset]]] = [DataFrameAsset]
 
     # instance attributes
-    type: Literal["spark_filesystem"] = "spark"
+    type: Literal["spark"] = "spark"
 
     assets: Dict[str, DataFrameAsset] = {}  # type: ignore[assignment]
 
