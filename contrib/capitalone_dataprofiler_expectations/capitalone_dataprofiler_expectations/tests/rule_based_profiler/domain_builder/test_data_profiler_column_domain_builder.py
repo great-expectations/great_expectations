0b8afe69124dac6519ff7272fa2bb106cf625966
alexjeilert <alexjeilert@gmail.com>
2023-03-21 16:48:45 -0500

    Reformatted using 'black'

--- contrib/capitalone_dataprofiler_expectations/capitalone_dataprofiler_expectations/tests/rule_based_profiler/domain_builder/test_data_profiler_column_domain_builder.py
+++ contrib/capitalone_dataprofiler_expectations/capitalone_dataprofiler_expectations/tests/rule_based_profiler/domain_builder/test_data_profiler_column_domain_builder.py
@@ -5,7 +5,9 @@ import os
 import pytest
 from ruamel.yaml import YAML
 
-from contrib.capitalone_dataprofiler_expectations.capitalone_dataprofiler_expectations.rule_based_profiler.domain_builder.data_profiler_column_domain_builder import DataProfilerColumnDomainBuilder
+from contrib.capitalone_dataprofiler_expectations.capitalone_dataprofiler_expectations.rule_based_profiler.domain_builder.data_profiler_column_domain_builder import (
+    DataProfilerColumnDomainBuilder,
+)
 
 from great_expectations.rule_based_profiler.domain_builder import DomainBuilder
 
@@ -14,8 +16,7 @@ yaml = YAML(typ="safe")
 
 @pytest.mark.integration
 @pytest.mark.slow  # 1.21s
-def test_data_profiler_column_domain_builder(
-):
+def test_data_profiler_column_domain_builder():
 
     test_root_path = os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -35,7 +36,9 @@ def test_data_profiler_column_domain_builder(
 
     variables["parameter_nodes"] = a
 
-    variables["parameter_nodes"]["variables"]["variables"]["profile_path"] = profile_path
+    variables["parameter_nodes"]["variables"]["variables"][
+        "profile_path"
+    ] = profile_path
 
     batch_request: dict = {
         "datasource_name": "alice_columnar_table_single_batch_datasource",
@@ -50,9 +53,24 @@ def test_data_profiler_column_domain_builder(
     numeric_column_names = domain_builder.get_effective_column_names(
         rule_name="numeric_rule", variables=variables
     )
-    assert (text_column_names == ['store_and_fwd_flag'])
-    assert (numeric_column_names == ['VendorID', 'passenger_count', 'trip_distance', 'RatecodeID', 'PULocationID', 'DOLocationID', 'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount', 'congestion_surcharge']
-)
+    assert text_column_names == ["store_and_fwd_flag"]
+    assert numeric_column_names == [
+        "VendorID",
+        "passenger_count",
+        "trip_distance",
+        "RatecodeID",
+        "PULocationID",
+        "DOLocationID",
+        "payment_type",
+        "fare_amount",
+        "extra",
+        "mta_tax",
+        "tip_amount",
+        "tolls_amount",
+        "improvement_surcharge",
+        "total_amount",
+        "congestion_surcharge",
+    ]
 
     domain_builder: DomainBuilder = DataProfilerColumnDomainBuilder()
     text_domains = domain_builder._get_domains(
@@ -61,101 +79,87 @@ def test_data_profiler_column_domain_builder(
     numeric_domains = domain_builder._get_domains(
         rule_name="numeric_rule", variables=variables
     )
-    assert (text_domains == [{
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "store_and_fwd_flag"
+    assert text_domains == [
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "store_and_fwd_flag"},
+            "rule_name": "text_rule",
+        }
+    ]
+    assert numeric_domains == [
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "VendorID"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "text_rule"
-    }])
-    assert (numeric_domains == [{
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "VendorID"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "passenger_count"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "passenger_count"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "trip_distance"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "trip_distance"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "RatecodeID"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "RatecodeID"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "PULocationID"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "PULocationID"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "DOLocationID"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "DOLocationID"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "payment_type"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "payment_type"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "fare_amount"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "fare_amount"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "extra"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "extra"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "mta_tax"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "mta_tax"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "tip_amount"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "tip_amount"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "tolls_amount"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "tolls_amount"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "improvement_surcharge"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "improvement_surcharge"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "total_amount"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "total_amount"
+        {
+            "domain_type": "column",
+            "domain_kwargs": {"column": "congestion_surcharge"},
+            "rule_name": "numeric_rule",
         },
-        "rule_name": "numeric_rule"
-    }, {
-        "domain_type": "column",
-        "domain_kwargs": {
-            "column": "congestion_surcharge"
-        },
-        "rule_name": "numeric_rule"
-    }])
+    ]
