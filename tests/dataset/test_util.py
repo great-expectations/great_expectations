444b71ce433fe50b4adc4d1d124fd61e0aa013ba
Anthony Burdi <anthony@greatexpectations.io>
2023-03-25 07:35:45 -0400

    Remove tests

--- tests/dataset/test_util.py
+++ tests/dataset/test_util.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pytest
 
-from great_expectations.dataset import SqlAlchemyDataset
 from great_expectations.dataset.util import (
     build_continuous_partition_object,
     is_valid_continuous_partition_object,
@@ -14,15 +13,6 @@ def test_build_continuous_partition_object(
 ):
     # NOTE: this test will fail if sqlite is the driver because it lacks quantile support
     # skip in that case
-    try:
-        import sqlalchemy.dialects.sqlite as sqlite_dialect
-
-        if isinstance(numeric_high_card_dataset, SqlAlchemyDataset) and isinstance(
-            numeric_high_card_dataset.engine.dialect, sqlite_dialect.dialect
-        ):
-            pytest.skip()
-    except ImportError:
-        pytest.skip("sqlite is not available")
 
     n = len(numeric_high_card_dict["norm_0_1"])
     weights, bin_edges = np.histogram(numeric_high_card_dict["norm_0_1"], bins="auto")
