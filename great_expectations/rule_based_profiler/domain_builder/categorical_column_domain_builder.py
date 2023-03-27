5a30b20b78389cfe28c696918ccab244e4579eff
Alex Sherstinsky <alex@greatexpectations.io>
2023-03-27 11:23:33 -0700

    type hints

--- great_expectations/rule_based_profiler/domain_builder/categorical_column_domain_builder.py
+++ great_expectations/rule_based_profiler/domain_builder/categorical_column_domain_builder.py
@@ -29,16 +29,16 @@ from great_expectations.rule_based_profiler.helpers.util import (
     get_parameter_value_and_validate_return_type,
     get_resolved_metrics_by_key,
 )
-from great_expectations.rule_based_profiler.parameter_container import (
-    ParameterContainer,  # noqa: TCH001
-)
-from great_expectations.validator.computed_metric import MetricValue  # noqa: TCH001
 from great_expectations.validator.metric_configuration import MetricConfiguration
 
 if TYPE_CHECKING:
     from great_expectations.data_context.data_context.abstract_data_context import (
         AbstractDataContext,
     )
+    from great_expectations.rule_based_profiler.parameter_container import (
+        ParameterContainer,  # noqa: TCH001
+    )
+    from great_expectations.validator.computed_metric import MetricValue  # noqa: TCH001
     from great_expectations.validator.validator import Validator
 
 
