50815edc36e2ace876c3cf2c6230ef4361d229a0
Gabriel Gore <gabriel59kg@gmail.com>
2023-03-28 18:22:26 -0400

    lower min ariflow version

--- ci/checks/check_min_airflow_dependency_compatibility.sh
+++ ci/checks/check_min_airflow_dependency_compatibility.sh
@@ -5,7 +5,7 @@ set -e
 
 # Install great_expectations using the airflow constraints file for our minimum supported version
 
-AIRFLOW_VERSION=2.2.3
+AIRFLOW_VERSION=2.2.0  # TODO: this should be 2.0.2
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
@@ -15,7 +15,9 @@ CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${A
 echo "Install gx dependencies using airflow constraint file"
 echo "Python:${PYTHON_VERSION}\nAirflow:${AIRFLOW_VERSION}\n${CONSTRAINT_URL}\n"
 
-pip install -e . --constraint "${CONSTRAINT_URL}"
+# Some issues may require uninstalling existing dependencies
+# pip freeze | xargs pip uninstall -y
+pip install . --constraint "${CONSTRAINT_URL}"
 
 echo "\nCall 'get_context()' to check for show-stopping errors\n"
 
