5e20622c223cd4d92efb558a405aeb9c8b204bfb
Ken Wade <ken@superconductive.com>
2023-03-30 03:40:28 -0500

    Revert test_checkpoint_new_notebook_renderer.py change for now (comment out)

--- tests/render/renderer/test_checkpoint_new_notebook_renderer.py
+++ tests/render/renderer/test_checkpoint_new_notebook_renderer.py
@@ -174,7 +174,9 @@ def checkpoint_new_notebook_assets():
             "cell_type": "code",
             "metadata": {},
             "execution_count": None,
-            "source": "import great_expectations as gx\nfrom great_expectations.core.yaml_handler import YAMLHandler\nfrom pprint import pprint\n\nyaml = YAMLHandler()\ncontext = gx.get_context()",
+            "source": "from ruamel.yaml import YAML\nimport great_expectations as gx\nfrom pprint import pprint\n\nyaml = YAML()\ncontext = gx.get_context()",
+            # Use this commented version once YAMLHandler is in place everywhere (not just tests and docs)
+            # "source": "import great_expectations as gx\nfrom great_expectations.core.yaml_handler import YAMLHandler\nfrom pprint import pprint\n\nyaml = YAMLHandler()\ncontext = gx.get_context()",
             "outputs": [],
         },
     ]
