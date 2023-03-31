426e1e8643ea6f1c550e74621ae9fe1fee44f764
Gabriel Gore <gabriel59kg@gmail.com>
2023-03-31 10:10:41 -0400

    revert spark changes (save for followup)

--- great_expectations/datasource/fluent/spark_filesystem_datasource.py
+++ great_expectations/datasource/fluent/spark_filesystem_datasource.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import logging
 import pathlib
-from typing import TYPE_CHECKING, ClassVar, Optional, Type
+import re
+from typing import TYPE_CHECKING, Optional, Union
 
 from typing_extensions import Literal
 
@@ -13,22 +14,20 @@ from great_expectations.datasource.fluent.data_asset.data_connector import (
 from great_expectations.datasource.fluent.interfaces import (
     TestConnectionError,
 )
+from great_expectations.datasource.fluent.spark_file_path_datasource import (
+    CSVAsset,
+)
 
 if TYPE_CHECKING:
-
-    from great_expectations.datasource.fluent.file_path_data_asset import (
-        _FilePathDataAsset,
+    from great_expectations.datasource.fluent.interfaces import (
+        Sorter,
+        SortersDefinition,
     )
 
 logger = logging.getLogger(__name__)
 
 
 class SparkFilesystemDatasource(_SparkFilePathDatasource):
-    # class attributes
-    data_connector_type: ClassVar[
-        Type[FilesystemDataConnector]
-    ] = FilesystemDataConnector
-
     # instance attributes
     type: Literal["spark_filesystem"] = "spark_filesystem"
 
@@ -53,30 +52,50 @@ class SparkFilesystemDatasource(_SparkFilePathDatasource):
             for asset in self.assets.values():
                 asset.test_connection()
 
-    # TODO: this can probably be defined on the parent class
-    def _build_data_connector(
-        self, data_asset: _FilePathDataAsset, glob_directive: str = "**/*", **kwargs
-    ) -> None:
-        """Builds and attaches the `FilesystemDataConnector` to the asset."""
-        if kwargs:
-            raise TypeError(
-                f"_build_data_connector() got unexpected keyword arguments {list(kwargs.keys())}"
-            )
-        data_asset._data_connector = self.data_connector_type.build_data_connector(
+    def add_csv_asset(
+        self,
+        name: str,
+        batching_regex: Optional[Union[re.Pattern, str]] = None,
+        glob_directive: str = "**/*",
+        header: bool = False,
+        infer_schema: bool = False,
+        order_by: Optional[SortersDefinition] = None,
+    ) -> CSVAsset:
+        """Adds a CSV DataAsset to the present "SparkFilesystemDatasource" object.
+
+        Args:
+            name: The name of the CSV asset
+            batching_regex: regex pattern that matches csv filenames that is used to label the batches
+            glob_directive: glob for selecting files in directory (defaults to `**/*`) or nested directories (e.g. `*/*/*.csv`)
+            header: boolean (default False) indicating whether or not first line of CSV file is header line
+            infer_schema: boolean (default False) instructing Spark to attempt to infer schema of CSV file heuristically
+            order_by: sorting directive via either list[Sorter] or "+/- key" syntax: +/- (a/de)scending; + default
+        """
+        batching_regex_pattern: re.Pattern = self.parse_batching_regex_string(
+            batching_regex=batching_regex
+        )
+        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
+        asset = CSVAsset(
+            name=name,
+            batching_regex=batching_regex_pattern,
+            header=header,
+            inferSchema=infer_schema,
+            order_by=order_by_sorters,
+        )
+        asset._data_connector = FilesystemDataConnector.build_data_connector(
             datasource_name=self.name,
-            data_asset_name=data_asset.name,
-            batching_regex=data_asset.batching_regex,
+            data_asset_name=name,
+            batching_regex=batching_regex_pattern,
             base_directory=self.base_directory,
             glob_directive=glob_directive,
             data_context_root_directory=self.data_context_root_directory,
         )
-
-        # build a more specific `_test_connection_error_message`
-        data_asset._test_connection_error_message = (
-            self.data_connector_type.build_test_connection_error_message(
-                data_asset_name=data_asset.name,
-                batching_regex=data_asset.batching_regex,
+        asset._test_connection_error_message = (
+            FilesystemDataConnector.build_test_connection_error_message(
+                data_asset_name=name,
+                batching_regex=batching_regex_pattern,
                 glob_directive=glob_directive,
                 base_directory=self.base_directory,
             )
         )
+        return self._add_asset(asset=asset)
--- great_expectations/datasource/fluent/spark_google_cloud_storage_datasource.py
+++ great_expectations/datasource/fluent/spark_google_cloud_storage_datasource.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, Type, Union
+import re
+from typing import TYPE_CHECKING, Any, Dict, Optional, Union
 
 import pydantic
 from typing_extensions import Literal
 
 from great_expectations.core.util import GCSUrl
-from great_expectations.datasource.fluent import (
-    _SparkFilePathDatasource,
-)
+from great_expectations.datasource.fluent import _SparkFilePathDatasource
 from great_expectations.datasource.fluent.config_str import (
-    ConfigStr,  # noqa: TCH001 # needed at runtime  # noqa: TCH001 # needed at runtime
+    ConfigStr,  # noqa: TCH001 # needed at runtime
 )
 from great_expectations.datasource.fluent.data_asset.data_connector import (
     GoogleCloudStorageDataConnector,
@@ -20,6 +19,9 @@ from great_expectations.datasource.fluent.interfaces import TestConnectionError
 from great_expectations.datasource.fluent.spark_datasource import (
     SparkDatasourceError,
 )
+from great_expectations.datasource.fluent.spark_file_path_datasource import (
+    CSVAsset,
+)
 
 if TYPE_CHECKING:
     from google.cloud.storage.client import Client as GoogleCloudStorageClient
@@ -27,8 +29,9 @@ if TYPE_CHECKING:
         Credentials as GoogleServiceAccountCredentials,
     )
 
-    from great_expectations.datasource.fluent.file_path_data_asset import (
-        _FilePathDataAsset,
+    from great_expectations.datasource.fluent.interfaces import (
+        Sorter,
+        SortersDefinition,
     )
 
 
@@ -50,11 +53,6 @@ class SparkGoogleCloudStorageDatasourceError(SparkDatasourceError):
 
 
 class SparkGoogleCloudStorageDatasource(_SparkFilePathDatasource):
-    # class attributes
-    data_connector_type: ClassVar[
-        Type[GoogleCloudStorageDataConnector]
-    ] = GoogleCloudStorageDataConnector
-
     # instance attributes
     type: Literal["spark_gcs"] = "spark_gcs"
 
@@ -128,38 +126,58 @@ class SparkGoogleCloudStorageDatasource(_SparkFilePathDatasource):
             for asset in self.assets.values():
                 asset.test_connection()
 
-    def _build_data_connector(
+    def add_csv_asset(
         self,
-        data_asset: _FilePathDataAsset,
+        name: str,
+        batching_regex: Union[re.Pattern, str],
+        header: bool = False,
+        infer_schema: bool = False,
         prefix: str = "",
         delimiter: str = "/",
         max_results: int = 1000,
-        **kwargs,
-    ) -> None:
-        """Builds and attaches the `GoogleCloudStorageDataConnector` to the asset."""
-        if kwargs:
-            raise TypeError(
-                f"_build_data_connector() got unexpected keyword arguments {list(kwargs.keys())}"
-            )
-        data_asset._data_connector = self.data_connector_type.build_data_connector(
+        order_by: Optional[SortersDefinition] = None,
+    ) -> CSVAsset:
+        """Adds a CSV DataAsset to the present "SparkGoogleCloudStorageDatasource" object.
+
+        Args:
+            name: The name of the CSV asset
+            batching_regex: regex pattern that matches csv filenames that is used to label the batches
+            header: boolean (default False) indicating whether or not first line of CSV file is header line
+            infer_schema: boolean (default False) instructing Spark to attempt to infer schema of CSV file heuristically
+            prefix (str): Google Cloud Storage object name prefix
+            delimiter (str): Google Cloud Storage object name delimiter
+            max_results (int): Google Cloud Storage max_results (default is 1000)
+            order_by: sorting directive via either list[Sorter] or "+/- key" syntax: +/- (a/de)scending; + default
+        """
+        batching_regex_pattern: re.Pattern = self.parse_batching_regex_string(
+            batching_regex=batching_regex
+        )
+        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
+        asset = CSVAsset(
+            name=name,
+            batching_regex=batching_regex_pattern,
+            header=header,
+            inferSchema=infer_schema,
+            order_by=order_by_sorters,
+        )
+        asset._data_connector = GoogleCloudStorageDataConnector.build_data_connector(
             datasource_name=self.name,
-            data_asset_name=data_asset.name,
+            data_asset_name=name,
             gcs_client=self._get_gcs_client(),
-            batching_regex=data_asset.batching_regex,
+            batching_regex=batching_regex_pattern,
             bucket_or_name=self.bucket_or_name,
             prefix=prefix,
             delimiter=delimiter,
             max_results=max_results,
             file_path_template_map_fn=GCSUrl.OBJECT_URL_TEMPLATE.format,
         )
-
-        # build a more specific `_test_connection_error_message`
-        data_asset._test_connection_error_message = (
-            self.data_connector_type.build_test_connection_error_message(
-                data_asset_name=data_asset.name,
-                batching_regex=data_asset.batching_regex,
+        asset._test_connection_error_message = (
+            GoogleCloudStorageDataConnector.build_test_connection_error_message(
+                data_asset_name=name,
+                batching_regex=batching_regex_pattern,
                 bucket_or_name=self.bucket_or_name,
                 prefix=prefix,
                 delimiter=delimiter,
             )
         )
+        return self._add_asset(asset=asset)
