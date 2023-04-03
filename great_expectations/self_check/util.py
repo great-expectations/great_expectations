486414fa2d2214647217450ab7f2b2e4d8187905
Will Shin <will@superconductive.com>
2023-04-03 14:38:29 -0700

    Update util.py

--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -737,13 +737,13 @@ def _get_test_validator_with_data_pandas(
 
             # We will use timestamp for timezone-aware (UTC only) dates in our tests
             if value.lower() in ["timestamp", "datetime64[ns, tz]"]:
-                df[key] = pd.to_datetime(df[key], utc=True)
+                df[key] = pd.to_datetime(df[key], utc=True, format="ISO8601")
                 continue
             elif value.lower() in ["datetime", "datetime64", "datetime64[ns]"]:
-                df[key] = pd.to_datetime(df[key])
+                df[key] = pd.to_datetime(df[key], format="ISO8601")
                 continue
             elif value.lower() in ["date"]:
-                df[key] = pd.to_datetime(df[key]).dt.date
+                df[key] = pd.to_datetime(df[key], format="ISO8601").dt.date
                 value = "object"
             try:
                 type_ = np.dtype(value)
