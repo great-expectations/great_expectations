3ec143d11b33bc03984af2ac9c38f5a1f0d8a7eb
Anthony Burdi <anthony@greatexpectations.io>
2023-04-03 15:01:13 -0400

    Change file= style paths to include the versioned_code prefix

--- docs/prepare_prior_versions.py
+++ docs/prepare_prior_versions.py
@@ -14,21 +14,26 @@ def _repo_root() -> pathlib.Path:
 
 
 def change_paths_for_docs_file_references():
-    """Change file= style references, mainly in v0.14 docs like v0.14.13"""
+    """Change file= style references to use versioned_docs.
+
+    This is used in v0.14 docs like v0.14.13 since we moved to using named
+    snippets only for v0.15.50 and later.
+    """
     path = _repo_root() / "docs/docusaurus/versioned_docs/version-0.14.13/"
     files = glob.glob(f"{path}/**/*.md", recursive=True)
-    # pattern = re.compile()
+    pattern = re.compile(r"((.*)(file=)((../)*))(.*)(#.*)")
+    path_to_insert = "versioned_code/version-0.14.13/"
+
+    # TODO: Make this idempotent
+
     for file_path in files:
-        # TODO: Run on all files not just test one
-        if "how_to_use_great_expectations_in_databricks_copy.md" in str(file_path):
-            with open(file_path, "r+") as f:
-                contents = f.read()
-                # TODO: Use regex here instead
-                contents = contents.replace("file=", "test_change_paths=")
-                f.seek(0)
-                f.truncate()
-                f.write(contents)
-            print(f"processed {file_path}")
+        with open(file_path, "r+") as f:
+            contents = f.read()
+            contents = re.sub(pattern, rf"\1{path_to_insert}\6\7", contents)
+            f.seek(0)
+            f.truncate()
+            f.write(contents)
+        print(f"processed {file_path}")
 
 
 if __name__ == "__main__":
