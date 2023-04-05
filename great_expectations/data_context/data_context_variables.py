f9a67b560781b37ce61b9d13a962eb9d02fa7a88
Alex Sherstinsky <alex@greatexpectations.io>
2023-04-05 15:27:14 -0700

    Deduplicate Datasource and DataAsset name as key in object serialization.

--- great_expectations/data_context/data_context_variables.py
+++ great_expectations/data_context/data_context_variables.py
@@ -383,7 +383,7 @@ class FileDataContextVariables(DataContextVariables):
                 for fluent_datasource_name in config_fluent_datasources_stash.keys():
                     self.data_context.datasources.pop(fluent_datasource_name)
                 # this would be `deep_copy'ed in `instantiate_class_from_config` too
-                self.data_context.fluent_config.fluent_datasources = {}
+                self.data_context.fluent_config.fluent_datasources = []
             yield
         except Exception:
             raise
