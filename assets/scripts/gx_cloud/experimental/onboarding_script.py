c4a3f5924efd49f58e20f81b74d80321e6d2676c
Robert Moses Lim <robert.m.lim@gmail.com>
2023-04-06 00:52:35 -0700

    Lint

--- assets/scripts/gx_cloud/experimental/onboarding_script.py
+++ assets/scripts/gx_cloud/experimental/onboarding_script.py
@@ -21,7 +21,9 @@ context: AbstractDataContext = gx.get_context(
 
 # Set variables for creating a Datasource
 datasource_name = None
-data_connector_name = "default_runtime_data_connector_name"  # Optional: Set your own data_connector_name
+data_connector_name = (
+    "default_runtime_data_connector_name"  # Optional: Set your own data_connector_name
+)
 assert datasource_name, "Please set datasource_name."
 
 # Set variable for creating an Expectation Suite
@@ -32,7 +34,9 @@ assert expectation_suite_name, "Please set expectation_suite_name."
 data_asset_name = None
 assert data_asset_name, "Please set data_asset_name."
 path_to_validator_batch = None  # e.g. "https://raw.githubusercontent.com/great-expectations/gx_tutorials/main/data/yellow_tripdata_sample_2019-01.csv"
-assert path_to_validator_batch, "Please set path_to_validator_batch. This can be a local filepath or a remote URL."
+assert (
+    path_to_validator_batch
+), "Please set path_to_validator_batch. This can be a local filepath or a remote URL."
 
 # Set variable for creating a Checkpoint
 checkpoint_name = None
@@ -40,7 +44,9 @@ assert checkpoint_name, "Please set checkpoint_name."
 
 # Set variable to get a Batch of data to validate against the new Checkpoint
 path_to_batch_to_validate = None  # e.g. "https://raw.githubusercontent.com/great-expectations/gx_tutorials/main/data/yellow_tripdata_sample_2019-01.csv"
-assert path_to_batch_to_validate, "Please set path_to_batch_to_validate. This can be a local filepath or a remote URL."
+assert (
+    path_to_batch_to_validate
+), "Please set path_to_batch_to_validate. This can be a local filepath or a remote URL."
 
 # Create Datasource
 # For simplicity, this script creates a Datasource with a PandasExecutionEngine and a RuntimeDataConnector
@@ -69,7 +75,9 @@ pprint.pprint(datasource.config)
 
 # Create a new Expectation Suite
 try:
-    expectation_suite: ExpectationSuite = context.get_expectation_suite(expectation_suite_name=expectation_suite_name)
+    expectation_suite: ExpectationSuite = context.get_expectation_suite(
+        expectation_suite_name=expectation_suite_name
+    )
     expectation_suite_ge_cloud_id = expectation_suite.ge_cloud_id
 except StoreBackendError:
     expectation_suite: ExpectationSuite = context.add_or_update_expectation_suite(
@@ -81,19 +89,14 @@ except StoreBackendError:
 batch_df: pd.DataFrame = pd.read_csv(path_to_validator_batch)
 
 batch_request = RuntimeBatchRequest(
-    runtime_parameters={
-        "batch_data": batch_df
-    },
-    batch_identifiers={
-        "batch_id": path_to_validator_batch
-    },
+    runtime_parameters={"batch_data": batch_df},
+    batch_identifiers={"path": path_to_validator_batch},
     datasource_name=datasource_name,
     data_connector_name=data_connector_name,
-    data_asset_name=data_asset_name
+    data_asset_name=data_asset_name,
 )
 validator: Validator = context.get_validator(
-    expectation_suite_name=expectation_suite_name,
-    batch_request=batch_request
+    expectation_suite_name=expectation_suite_name, batch_request=batch_request
 )
 
 # Add Expectations interactively using tab completion
@@ -101,7 +104,9 @@ validator.expect_column_to_exist(column="")
 
 # Save Expectation Suite
 validator.save_expectation_suite(discard_failed_expectations=False)
-expectation_suite: ExpectationSuite = context.get_expectation_suite(expectation_suite_name=expectation_suite_name)
+expectation_suite: ExpectationSuite = context.get_expectation_suite(
+    expectation_suite_name=expectation_suite_name
+)
 print(f"\n{20*'='}\nExpectation Suite\n{20*'='}\n")
 pprint.pprint(expectation_suite)
 
@@ -112,17 +117,19 @@ try:
 except StoreBackendError:
     checkpoint_config = {
         "name": checkpoint_name,
-        "validations": [{
-            "expectation_suite_name": expectation_suite_name,
-            "expectation_suite_ge_cloud_id": expectation_suite_ge_cloud_id,
-            "batch_request": {
-                "datasource_name": datasource_name,
-                "data_connector_name": data_connector_name,
-                "data_asset_name": data_asset_name,
-            },
-        }],
+        "validations": [
+            {
+                "expectation_suite_name": expectation_suite_name,
+                "expectation_suite_ge_cloud_id": expectation_suite_ge_cloud_id,
+                "batch_request": {
+                    "datasource_name": datasource_name,
+                    "data_connector_name": data_connector_name,
+                    "data_asset_name": data_asset_name,
+                },
+            }
+        ],
         "config_version": 1,
-        "class_name": "Checkpoint"
+        "class_name": "Checkpoint",
     }
 
     context.add_or_update_checkpoint(**checkpoint_config)
@@ -166,13 +173,9 @@ validation_df: pd.DataFrame = pd.read_csv(path_to_batch_to_validate)
 result = context.run_checkpoint(
     ge_cloud_id=checkpoint_id,
     batch_request={
-        "runtime_parameters": {
-            "batch_data": validation_df
-        },
-        "batch_identifiers": {
-            "batch_id": path_to_batch_to_validate
-        },
-    }
+        "runtime_parameters": {"batch_data": validation_df},
+        "batch_identifiers": {"path": path_to_batch_to_validate},
+    },
 )
 
 print(f"\n{20*'='}\nValidation Result\n{20*'='}\n")
