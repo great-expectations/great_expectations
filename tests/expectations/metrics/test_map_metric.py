aa168096e59683a861d0f52a3dfa8dde8b00fef7
Anthony Burdi <anthony@greatexpectations.io>
2023-04-06 18:40:04 -0400

    Missed one

--- tests/expectations/metrics/test_map_metric.py
+++ tests/expectations/metrics/test_map_metric.py
@@ -70,6 +70,7 @@ def sqlite_table_for_unexpected_rows_with_index(
             # adapted from https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html
             try:
                 add_dataframe_to_db(
+                    df=df,
                     name="animal_names",
                     con=sqlite_engine,
                     index=False,
