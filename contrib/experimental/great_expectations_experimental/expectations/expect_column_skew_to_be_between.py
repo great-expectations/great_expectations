ce00b60b225275ae1c73e6359a8ecee224f215b5
Ken Wade <ken@superconductive.com>
2023-04-12 00:42:30 -0500

    Import from column_aggregate_metric_provider; see #3128

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_skew_to_be_between.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_skew_to_be_between.py
@@ -16,7 +16,7 @@ from great_expectations.execution_engine.sqlalchemy_execution_engine import (
     SqlAlchemyExecutionEngine,
 )
 from great_expectations.expectations.expectation import ColumnExpectation
-from great_expectations.expectations.metrics.column_aggregate_metric import (
+from great_expectations.expectations.metrics.column_aggregate_metric_provider import (
     ColumnAggregateMetricProvider,
 )
 from great_expectations.expectations.metrics.column_aggregate_metric_provider import (
