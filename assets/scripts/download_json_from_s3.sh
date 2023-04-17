3b8cc7fc4d5be947b3bf9b64e40794f76b662ddf
Ken Wade <ken@superconductive.com>
2023-04-17 15:04:18 -0500

    WIP

--- assets/scripts/download_json_from_s3.sh
+++ assets/scripts/download_json_from_s3.sh
@@ -3,4 +3,4 @@
 aws --version
 aws s3 ls s3://superconductive-public/static/gallery/ | grep "_full"
 
-aws s3 cp s3://superconductive-public/static/gallery/ . --exclude "*" --include "*_full.json"
+aws s3 cp s3://superconductive-public/static/gallery/ . --recursive --exclude "*" --include "*_full.json"
