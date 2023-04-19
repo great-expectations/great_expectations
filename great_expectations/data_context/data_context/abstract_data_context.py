03c64162a6ddf0ec15376e98cab903190ebe7a61
Nathan Farmer <nathan@greatexpectations.io>
2023-04-19 17:25:21 -0400

    Remove incorrect _add_fluent_datasource method

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -703,15 +703,6 @@ class AbstractDataContext(ConfigPeer, ABC):
     def sources(self) -> _SourceFactories:
         return self._sources
 
-    def _add_fluent_datasource(self, datasource: FluentDatasource) -> None:
-        # We currently don't allow one to overwrite a datasource with this internal method
-        if datasource.name in self.datasources:
-            raise gx_exceptions.DataContextError(
-                f"Can not write the fluent datasource {datasource.name} because a datasource of that "
-                "name already exists in the data context."
-            )
-        self.datasources[datasource.name] = datasource
-
     def _update_fluent_datasource(self, datasource: FluentDatasource) -> None:
         self.datasources[datasource.name] = datasource
 
