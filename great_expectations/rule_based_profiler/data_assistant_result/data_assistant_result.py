89160aa1b3fbeb0f4e2a060124ce53643f5d6bcd
Jose Tobar <jose@greatexpectations.io>
2023-04-20 13:55:12 -0700

    Make _plot and its dependent functions async

--- great_expectations/rule_based_profiler/data_assistant_result/data_assistant_result.py
+++ great_expectations/rule_based_profiler/data_assistant_result/data_assistant_result.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import asyncio
 import copy
 import datetime
 import json
@@ -21,6 +22,7 @@ from typing import (
 
 import altair as alt
 import ipywidgets as widgets
+import nest_asyncio
 import numpy as np
 import pandas as pd
 from IPython.display import HTML, display
@@ -83,6 +85,9 @@ if TYPE_CHECKING:
     from great_expectations.rule_based_profiler.config import RuleBasedProfilerConfig
 
 ColumnDataFrame = namedtuple("ColumnDataFrame", ["column", "df"])
+
+# This is needed to run the nested async functions using Async IO
+nest_asyncio.apply()
 
 
 @dataclass
@@ -540,12 +545,14 @@ class DataAssistantResult(SerializableDictDot):
         Returns:
             PlotResult wrapper object around Altair charts.
         """
-        return self._plot(
-            plot_mode=PlotMode.DESCRIPTIVE,
-            sequential=sequential,
-            theme=theme,
-            include_column_names=include_column_names,
-            exclude_column_names=exclude_column_names,
+        return asyncio.run(
+            self._plot(
+                plot_mode=PlotMode.DESCRIPTIVE,
+                sequential=sequential,
+                theme=theme,
+                include_column_names=include_column_names,
+                exclude_column_names=exclude_column_names,
+            )
         )
 
     @public_api
@@ -570,15 +577,17 @@ class DataAssistantResult(SerializableDictDot):
         Returns:
             PlotResult wrapper object around Altair charts.
         """
-        return self._plot(
-            plot_mode=PlotMode.DIAGNOSTIC,
-            sequential=sequential,
-            theme=theme,
-            include_column_names=include_column_names,
-            exclude_column_names=exclude_column_names,
+        return asyncio.run(
+            self._plot(
+                plot_mode=PlotMode.DIAGNOSTIC,
+                sequential=sequential,
+                theme=theme,
+                include_column_names=include_column_names,
+                exclude_column_names=exclude_column_names,
+            )
         )
 
-    def _plot(
+    async def _plot(
         self,
         plot_mode: PlotMode,
         sequential: bool,
@@ -643,12 +652,12 @@ class DataAssistantResult(SerializableDictDot):
         display_charts.extend(column_domain_display_charts)
         return_charts.extend(column_domain_return_charts)
 
-        self._display(charts=display_charts, plot_mode=plot_mode, theme=theme)
+        await self._display(charts=display_charts, plot_mode=plot_mode, theme=theme)
 
         return_charts = self._apply_theme(charts=return_charts, theme=theme)
         return PlotResult(charts=return_charts)
 
-    def _display(
+    async def _display(
         self,
         charts: Union[List[alt.Chart], List[alt.VConcatChart]],
         plot_mode: PlotMode,
