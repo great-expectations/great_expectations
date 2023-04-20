24482fba3ef716bd306d93ec18409b92fdecd6b6
Gabriel Gore <gabriel59kg@gmail.com>
2023-04-20 15:40:50 -0400

    call `self._datasource_store.delete` for cloud

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -721,10 +721,15 @@ class AbstractDataContext(ConfigPeer, ABC):
         self.datasources[datasource.name] = datasource
 
     def _update_fluent_datasource(self, datasource: FluentDatasource) -> None:
+        # NOTE: this is not called
         self.datasources[datasource.name] = datasource
 
     def _delete_fluent_datasource(self, datasource_name: str) -> None:
-        self.datasources.pop(datasource_name, None)
+        datasource = self.datasources.get(datasource_name)
+        if datasource:
+            if self._datasource_store.cloud_mode:
+                self._datasource_store.delete(datasource)  # type: ignore[arg-type] # Could be a LegacyDatasource
+            self.datasources.pop(datasource_name, None)
 
     def set_config(self, project_config: DataContextConfig) -> None:
         self._project_config = project_config
