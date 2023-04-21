f72ddb75f47deba07b5aa95b9360ba80cf2c1e40
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-04-21 14:04:04 -0400

    Minor edit

--- docs/docusaurus/docs/contributing/contributing_overview.md
+++ docs/docusaurus/docs/contributing/contributing_overview.md
@@ -1,5 +1,5 @@
 ---
-title: Contributing
+title: Contribute
 id: contributing
 ---
 
--- docs/docusaurus/sidebars.js
+++ docs/docusaurus/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   docs: [
     'intro',
     {
-      type: 'doc', id: 'docs/contributing/contributing_overview', label: 'Contribute'
+      type: 'doc', id: 'contributing/contributing_overview', label: 'Contribute'
     },
     {
       type: 'doc', id: 'tutorials/quickstart/quickstart', label: 'Quickstart'
