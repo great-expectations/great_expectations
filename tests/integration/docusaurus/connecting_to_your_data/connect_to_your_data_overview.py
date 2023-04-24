f918e31f18b003b63b6b0d6095e54786bb7c0ebb
Bill Dirks <bill@greatexpectations.io>
2023-04-24 16:22:03 -0700

    [DOCS] Update Batch Request glossary entry. (#7716)

--- tests/integration/docusaurus/connecting_to_your_data/connect_to_your_data_overview.py
+++ tests/integration/docusaurus/connecting_to_your_data/connect_to_your_data_overview.py
@@ -1,21 +1,9 @@
-import pathlib
-import great_expectations as gx
-
-data_directory = pathlib.Path(
-    gx.__file__,
-    "..",
-    "..",
-    "tests",
-    "test_sets",
-    "taxi_yellow_tripdata_samples",
-).resolve(strict=True)
-
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/connect_to_your_data_overview add_datasource">
 import great_expectations as gx
 
 context = gx.get_context()
 context.sources.add_pandas_filesystem(
-    name="my_pandas_datasource", base_directory=data_directory
+    name="my_pandas_datasource", base_directory="./data"
 )
 # </snippet>
 
