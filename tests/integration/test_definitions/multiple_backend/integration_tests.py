cc82be78dda9f5877a3e3be56a78f3b4758acc78
Austin Robinson <austin@greatexpectations.io>
2023-04-25 15:01:04 -0600

    tests

--- tests/integration/test_definitions/multiple_backend/integration_tests.py
+++ tests/integration/test_definitions/multiple_backend/integration_tests.py
@@ -5,7 +5,7 @@ multiple_backend = []
 
 cross_table_comparisons = [
     IntegrationTestFixture(
-        name="cross_table_comparisons",
+        name="RUNME_POSTGRES_MYSQL cross_table_comparisons",
         user_flow_script="tests/integration/docusaurus/expectations/advanced/data_assistant_cross_table_comparison.py",
         data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
         data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
@@ -36,20 +36,5 @@ creating_custom_expectations = [
     ),
 ]
 
-profiling = [
-    IntegrationTestFixture(
-        name="cross_table_comparisons",
-        user_flow_script="tests/integration/docusaurus/expectations/advanced/data_assistant_cross_table_comparison.py",
-        data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
-        util_script="tests/test_utils.py",
-        backend_dependencies=[
-            BackendDependencies.POSTGRESQL,
-            BackendDependencies.MYSQL,
-        ],
-    ),
-]
-
 multiple_backend += cross_table_comparisons
 multiple_backend += creating_custom_expectations
-multiple_backend += profiling
