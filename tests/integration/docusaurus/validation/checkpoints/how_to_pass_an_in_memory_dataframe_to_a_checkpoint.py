74efd11dd58ffebaf0b2d5f52fc9c46248fa7967
Nathan Farmer <nathan@greatexpectations.io>
2023-04-26 12:18:04 -0400

    Get script operational

--- tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py
+++ tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py
@@ -17,10 +17,10 @@ context.add_or_update_expectation_suite("my_expectation_suite")
 df = pandas.read_csv("./data/yellow_tripdata_sample_2019-01.csv")
 
 validator = context.sources.add_pandas("taxi_datasource").read_dataframe(
-    "taxi_frame", df, batch_metadata={"year": "2019", "month": "01"}
+    df, asset_name="taxi_frame", batch_metadata={"year": "2019", "month": "01"}
 )
 
-checkpoint: SimpleCheckpoint = SimpleCheckpoint(
+checkpoint = SimpleCheckpoint(
     name="my_taxi_validator_checkpoint",
     data_context=context,
     validator=validator,
@@ -30,21 +30,22 @@ checkpoint_result = checkpoint.run()
 # </snippet>
 
 # NOTE: The following code is only for testing and can be ignored by
-assert checkpoint_result["success"] == True
+assert checkpoint_result["success"]
 
 # clean-up between examples
 context.delete_datasource("taxi_datasource")
 
 # <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_pass_an_in_memory_dataframe_to_a_checkpoint.py simple_checkpoint_add_dataframe">
-context.sources.add_pandas("my_taxi_validator_checkpoint").add_dataframe_asset(
-    "taxi_frame", df, batch_metadata={"year": "2019", "month": "01"}
-)
+dataframe_asset = context.sources.add_pandas(
+    "my_taxi_validator_checkpoint"
+).add_dataframe_asset("taxi_frame", df, batch_metadata={"year": "2019", "month": "01"})
 
-checkpoint = context.add_checkpoint(
+batch_request = dataframe_asset.build_batch_request()
+
+checkpoint = SimpleCheckpoint(
     name="my_taxi_dataframe_checkpoint",
     data_context=context,
-    datasource_name="my_taxi_validator_checkpoint",
-    data_asset_name="taxi_frame",
+    batch_request=batch_request,
     expectation_suite_name="my_expectation_suite",
 )
 
@@ -52,4 +53,4 @@ checkpoint_result = checkpoint.run()
 # </snippet>
 
 # NOTE: The following code is only for testing and can be ignored by
-assert checkpoint_result["success"] == True
+assert checkpoint_result["success"]
