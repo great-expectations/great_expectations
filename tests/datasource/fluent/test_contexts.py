f5e10848bf965e810b87c0f8a2571a6a44be2c37
Gabriel Gore <gabriel59kg@gmail.com>
2023-04-27 11:26:15 -0400

    delete changes

--- tests/datasource/fluent/test_contexts.py
+++ tests/datasource/fluent/test_contexts.py
@@ -5,6 +5,7 @@ from pprint import pformat as pf
 from typing import TYPE_CHECKING
 
 import pytest
+import requests
 
 from great_expectations.core.yaml_handler import YAMLHandler
 from great_expectations.data_context import CloudDataContext, FileDataContext
@@ -17,13 +18,19 @@ if TYPE_CHECKING:
     from pytest_mock import MockerFixture
     from responses import RequestsMock
 
-    from great_expectations.datasource.fluent import SqliteDatasource
 
 # apply markers to entire test module
 pytestmark = [pytest.mark.integration]
 
 
 yaml = YAMLHandler()
+
+
+@pytest.fixture
+def taxi_data_samples_dir() -> pathlib.Path:
+    return pathlib.Path(
+        __file__, "..", "..", "..", "test_sets", "taxi_yellow_tripdata_samples"
+    ).resolve(strict=True)
 
 
 @pytest.mark.cloud
@@ -63,7 +70,9 @@ def test_add_fluent_datasource_are_persisted_without_duplicates(
 
     yaml_path = pathlib.Path(context.root_directory, context.GX_YML)
     assert yaml_path.exists()
+
     yaml_dict: dict = yaml.load(yaml_path.read_text())
+    print(pf(yaml_dict, depth=2, sort_dicts=True))
     assert datasource_name in yaml_dict["fluent_datasources"]
     assert datasource_name not in yaml_dict["datasources"]
 
@@ -100,24 +109,22 @@ def test_assets_are_persisted_on_creation_and_removed_on_deletion(
 
 
 @pytest.mark.cloud
-@pytest.mark.xfail(reason="Fluent logic attempts to delete before updating")
 def test_context_add_or_update_datasource(
     cloud_api_fake: RequestsMock,
     empty_contexts: CloudDataContext | FileDataContext,
-    db_file: pathlib.Path,
+    # db_file: pathlib.Path, TODO: sqlite deser broken
+    taxi_data_samples_dir: pathlib.Path,
 ):
     context = empty_contexts
 
-    datasource: SqliteDatasource = context.sources.add_sqlite(
-        name="save_ds_test", connection_string=f"sqlite:///{db_file}"
+    datasource = context.sources.add_pandas_filesystem(
+        name="save_ds_test", base_directory=taxi_data_samples_dir
+    )
+    datasource.add_csv_asset(
+        name="my_asset",
     )
 
-    assert datasource.connection_string == f"sqlite:///{db_file}"
-
-    # modify the datasource
-    datasource.connection_string = "sqlite:///"  # type: ignore[assignment]
-    context.sources.add_or_update_sqlite(datasource)
-
+    # TODO: spy the store.delete calls instead of ctx specific tests
     if isinstance(empty_contexts, CloudDataContext):
         # TODO: adjust call counts as needed
         cloud_api_fake.assert_call_count(
@@ -125,9 +132,57 @@ def test_context_add_or_update_datasource(
             1,
         )
         cloud_api_fake.assert_call_count(
-            f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources{datasource.id}",
+            f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources/{datasource.id}?name={datasource.name}",
             1,
         )
+
+        response = requests.get(
+            f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources/{datasource.id}"
+        )
+        response.raise_for_status()
+        print(pf(response.json(), depth=4))
+        pytest.xfail(  # TODO: remove this
+            "Immediate persistance for cloud not implemented"
+        )
+        assert response.json()["data"]["attributes"]["datasource_config"].get("assets")
+
+
+@pytest.mark.cloud
+def test_cloud_context_delete_datasource(
+    cloud_api_fake: RequestsMock,
+    empty_cloud_context_fluent: CloudDataContext,
+    taxi_data_samples_dir: pathlib.Path,
+):
+    context = empty_cloud_context_fluent
+
+    datasource = context.sources.add_pandas_filesystem(
+        name="delete_ds_test", base_directory=taxi_data_samples_dir
+    )
+
+    # check cloud_api_fake items
+    response1 = requests.get(
+        f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources/{datasource.id}",
+    )
+    print(f"Before Delete -> {response1}\n{pf(response1.json())}\n")
+    assert response1.status_code == 200
+
+    context.sources.delete_pandas_filesystem(datasource.name)
+    assert datasource.name not in context.fluent_datasources
+
+    cloud_api_fake.assert_call_count(
+        f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources",
+        1,
+    )
+    cloud_api_fake.assert_call_count(
+        f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources/{datasource.id}",
+        2,
+    )
+
+    response2 = requests.get(
+        f"{GX_CLOUD_MOCK_BASE_URL}/organizations/{FAKE_ORG_ID}/datasources/{datasource.id}",
+    )
+    print(f"After Delete -> {response2}\n{pf(response2.json())}")
+    assert response2.status_code == 404
 
 
 if __name__ == "__main__":
