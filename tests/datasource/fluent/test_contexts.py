80a63c8d754a518b040179b4df82f8b88fe0a5fd
Gabriel Gore <gabriel59kg@gmail.com>
2023-04-27 11:27:06 -0400

    remove TDD test xfail

--- tests/datasource/fluent/test_contexts.py
+++ tests/datasource/fluent/test_contexts.py
@@ -141,9 +141,6 @@ def test_context_add_or_update_datasource(
         )
         response.raise_for_status()
         print(pf(response.json(), depth=4))
-        pytest.xfail(  # TODO: remove this
-            "Immediate persistance for cloud not implemented"
-        )
         assert response.json()["data"]["attributes"]["datasource_config"].get("assets")
 
 
