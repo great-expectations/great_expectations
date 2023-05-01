030fa2f7c006164ed9229f613ca9f3564c88d098
Nathan Farmer <nathan@greatexpectations.io>
2023-05-01 09:17:56 -0400

    Add stub file for BatchRequest

--- great_expectations/datasource/fluent/batch_request.pyi
+++ great_expectations/datasource/fluent/batch_request.pyi
@@ -1,10 +1,11 @@
-from typing import Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, Dict, Optional
 
 import pydantic
 from pydantic import StrictStr
 from typing_extensions import TypeAlias
 
-from great_expectations.datasource.data_connector.batch_filter import BatchSlice
+if TYPE_CHECKING:
+    from great_expectations.datasource.data_connector.batch_filter import BatchSlice
 
 BatchRequestOptions: TypeAlias = Dict[StrictStr, Any]
 
