e5018b3552016994ffe087620326931a64cfe7d7
Will Shin <will@greatexpectations.io>
2023-05-02 12:58:05 -0700

    changes in a smaller commit

---
+++ great_expectations/compatibility/aws.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from great_expectations.compatibility.not_imported import NotImported
+
+AWS_NOT_IMPORTED = NotImported(
+    "Unable to load AWS connection object; please 'pip install boto3 botocore'"
+)
+
+try:
+    import boto3
+except ImportError:
+    boto3 = AWS_NOT_IMPORTED
+
+try:
+    import botocore
+except ImportError:
+    botocore = AWS_NOT_IMPORTED
+
+try:
+    from botocore.exceptions import ClientError
+
+except (ImportError, AttributeError):
+    ClientError = AWS_NOT_IMPORTED
+
+try:
+    pass
+
+except (ImportError, AttributeError):
+    ClientError = AWS_NOT_IMPORTED
