fd95a54085de8e01e352552ec5d354bfa6a557be
Anthony Burdi <anthony@greatexpectations.io>
2023-05-03 10:00:07 -0400

    [MAINTENANCE] Test DirectoryCSVAsset with both str and pathlib.Path (#7801)

--- great_expectations/datasource/fluent/data_asset/data_connector/file_path_data_connector.py
+++ great_expectations/datasource/fluent/data_asset/data_connector/file_path_data_connector.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import copy
 import logging
-import pathlib
 import re
 from abc import abstractmethod
 from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Set, Tuple, Union
@@ -31,8 +30,8 @@ from great_expectations.datasource.fluent.data_asset.data_connector.regex_parser
 # TODO: <Alex>ALEX</Alex>
 
 if TYPE_CHECKING:
+    from great_expectations.alias_types import PathStr
     from great_expectations.datasource.fluent import BatchRequest
-
 
 logger = logging.getLogger(__name__)
 
@@ -69,7 +68,7 @@ def file_get_unfiltered_batch_definition_list_fn(
 
 
 def make_directory_get_unfiltered_batch_definition_list_fn(
-    data_directory: str | pathlib.Path,
+    data_directory: PathStr,
 ) -> Callable[[FilePathDataConnector, BatchRequest], list[BatchDefinition]]:
     def directory_get_unfiltered_batch_definition_list_fn(
         data_connector: FilePathDataConnector,
