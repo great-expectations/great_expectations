55f26bb76c9f25521e142eb5b68db6814befadd0
Anthony Burdi <anthony@greatexpectations.io>
2023-05-03 11:22:58 -0400

    Add JSONAsset and generic file reader options in _SparkGenericFilePathAsset

--- great_expectations/datasource/fluent/spark_file_path_datasource.py
+++ great_expectations/datasource/fluent/spark_file_path_datasource.py
@@ -20,7 +20,26 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class CSVAsset(_FilePathDataAsset):
+class _SparkGenericFilePathAsset(_FilePathDataAsset):
+    ignore_corrupt_files: bool = Field(alias="ignoreCorruptFiles")
+    ignore_missing_files: bool = Field(alias="ignoreMissingFiles")
+    path_glob_filter: str = Field(alias="pathGlobFilter")
+    recursive_file_lookup: bool = Field(alias="recursiveFileLookup")
+    modified_before: str = Field(alias="modifiedBefore")
+    modified_after: str = Field(alias="modifiedAfter")
+
+    def _get_reader_options_include(self) -> set[str] | None:
+        return {
+            "ignoreCorruptFiles",
+            "ignoreMissingFiles",
+            "pathGlobFilter",
+            "recursiveFileLookup",
+            "modifiedBefore",
+            "modifiedAfter",
+        }
+
+
+class CSVAsset(_SparkGenericFilePathAsset):
     # Overridden inherited instance fields
     type: Literal["csv"] = "csv"
     header: bool = False
@@ -34,10 +53,10 @@ class CSVAsset(_FilePathDataAsset):
         return self.type
 
     def _get_reader_options_include(self) -> set[str] | None:
-        return {"header", "infer_schema"}
+        return super()._get_reader_options_include().union({"header", "infer_schema"})
 
 
-class DirectoryCSVAsset(_FilePathDataAsset):
+class DirectoryCSVAsset(_SparkGenericFilePathAsset):
     # Overridden inherited instance fields
     type: Literal["directory_csv"] = "directory_csv"
     data_directory: pathlib.Path
@@ -53,10 +72,14 @@ class DirectoryCSVAsset(_FilePathDataAsset):
         return self.type.replace("directory_", "")
 
     def _get_reader_options_include(self) -> set[str] | None:
-        return {"data_directory", "header", "infer_schema"}
+        return (
+            super()
+            ._get_reader_options_include()
+            .union({"data_directory", "header", "infer_schema"})
+        )
 
 
-class ParquetAsset(_FilePathDataAsset):
+class ParquetAsset(_SparkGenericFilePathAsset):
     type: Literal["parquet"] = "parquet"
     # The options below are available for parquet as of spark v3.4.0
     # See https://spark.apache.org/docs/latest/sql-data-sources-parquet.html for more info.
@@ -80,10 +103,14 @@ class ParquetAsset(_FilePathDataAsset):
 
         See https://spark.apache.org/docs/latest/sql-data-sources-parquet.html for more info.
         """
-        return {"datetimeRebaseMode", "int96RebaseMode", "mergeSchema"}
+        return (
+            super()
+            ._get_reader_options_include()
+            .union({"datetimeRebaseMode", "int96RebaseMode", "mergeSchema"})
+        )
 
 
-class ORCAsset(_FilePathDataAsset):
+class ORCAsset(_SparkGenericFilePathAsset):
     # Overridden inherited instance fields
     type: Literal["orc"] = "orc"
     merge_schema: bool = Field(False, alias="mergeSchema")
@@ -96,7 +123,88 @@ class ORCAsset(_FilePathDataAsset):
         return self.type
 
     def _get_reader_options_include(self) -> set[str] | None:
-        return {"merge_schema"}
+        """These options are available as of spark v3.4.0
+
+        See https://spark.apache.org/docs/latest/sql-data-sources-orc.html for more info.
+        """
+        return super()._get_reader_options_include().union({"mergeSchema"})
+
+
+class JSONAsset(_SparkGenericFilePathAsset):
+    # Overridden inherited instance fields
+    type: Literal["json"] = "json"
+    timezone: str = Field(alias="timeZone")
+    primitives_as_string: bool = Field(False, alias="primitivesAsString")
+    prefers_decimal: bool = Field(False, alias="prefersDecimal")
+    allow_comments: bool = Field(False, alias="allowComments")
+
+    allow_unquoted_field_names: bool = Field(False, alias="allowUnquotedFieldNames")
+    allow_single_quotes: bool = Field(True, alias="allowSingleQuotes")
+    allow_numeric_leading_zeros: bool = Field(False, alias="allowNumericLeadingZeros")
+    allow_backslash_escaping_any_character: bool = Field(
+        False, alias="allowBackslashEscapingAnyCharacter"
+    )
+    mode: Literal["PERMISSIVE", "DROPMALFORMED", "FAILFAST"] = Field("PERMISSIVE")
+    column_name_of_corrupt_record: str = Field(alias="columnNameOfCorruptRecord")
+    date_format: str = Field("yyyy-MM-dd", alias="dateFormat")
+    timestamp_format: str = Field(
+        "yyyy-MM-dd'T'HH:mm:ss[.SSS][XXX]", alias="timestampFormat"
+    )
+    timestamp_ntz_format: str = Field(
+        "yyyy-MM-dd'T'HH:mm:ss[.SSS]", alias="timestampNTZFormat"
+    )
+    enable_date_time_parsing_fallback: bool = Field(
+        alias="enableDateTimeParsingFallback"
+    )
+    multi_line: bool = Field(False, alias="multiLine")
+    allow_unquoted_control_chars: bool = Field(False, alias="allowUnquotedControlChars")
+    encoding: str
+    line_sep: str = Field(alias="lineSep")
+    sampling_ratio: float = Field(1.0, alias="samplingRatio")
+    drop_field_if_all_null: bool = Field(False, alias="dropFieldIfAllNull")
+    locale: str
+    allow_non_numeric_numbers: bool = Field(True, alias="allowNonNumericNumbers")
+    merge_schema: bool = Field(False, alias="mergeSchema")
+
+    class Config:
+        extra = pydantic.Extra.forbid
+        allow_population_by_field_name = True
+
+    def _get_reader_method(self) -> str:
+        return self.type
+
+    def _get_reader_options_include(self) -> set[str] | None:
+        return (
+            super()
+            ._get_reader_options_include()
+            .union(
+                {
+                    "timeZone",
+                    "primitivesAsString",
+                    "prefersDecimal",
+                    "allowComments",
+                    "allowUnquotedFieldNames",
+                    "allowSingleQuotes",
+                    "allowNumericLeadingZeros",
+                    "allowBackslashEscapingAnyCharacter",
+                    "mode",
+                    "columnNameOfCorruptRecord",
+                    "dateFormat",
+                    "timestampFormat",
+                    "timestampNTZFormat",
+                    "enableDateTimeParsingFallback",
+                    "multiLine",
+                    "allowUnquotedControlChars",
+                    "encoding",
+                    "lineSep",
+                    "samplingRatio",
+                    "dropFieldIfAllNull",
+                    "locale",
+                    "allowNonNumericNumbers",
+                    "mergeSchema",
+                }
+            )
+        )
 
 
 # New asset types should be added to the _SPARK_FILE_PATH_ASSET_TYPES tuple,
@@ -107,9 +215,10 @@ _SPARK_FILE_PATH_ASSET_TYPES = (
     DirectoryCSVAsset,
     ParquetAsset,
     ORCAsset,
+    JSONAsset,
 )
 _SPARK_FILE_PATH_ASSET_TYPES_UNION = Union[
-    CSVAsset, DirectoryCSVAsset, ParquetAsset, ORCAsset
+    CSVAsset, DirectoryCSVAsset, ParquetAsset, ORCAsset, JSONAsset
 ]
 # Directory asset classes should be added to the _SPARK_DIRECTORY_ASSET_CLASSES
 # tuple so that the appropriate directory related methods are called.
