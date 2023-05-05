993c8ef1702c85b9b91cd5019af3af932bc58e26
Will Shin <will@greatexpectations.io>
2023-05-05 15:52:10 -0700

    update the conftest.py too

--- tests/execution_engine/conftest.py
+++ tests/execution_engine/conftest.py
@@ -4,7 +4,7 @@ import random
 from pathlib import Path
 from typing import List
 
-import boto3
+from great_expectations.compatibility import aws
 import pandas as pd
 import pytest
 from moto import mock_s3
@@ -26,7 +26,7 @@ def aws_credentials():
 @pytest.fixture
 def s3(aws_credentials):
     with mock_s3():
-        yield boto3.client("s3", region_name="us-east-1")
+        yield aws.boto3.client("s3", region_name="us-east-1")
 
 
 @pytest.fixture
