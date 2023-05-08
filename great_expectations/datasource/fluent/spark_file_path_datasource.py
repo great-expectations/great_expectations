fcaf364f4deadb4335d22f47d9a2d4817df2e2fd
Anthony Burdi <anthony@greatexpectations.io>
2023-05-08 17:01:05 -0400

    DirectoryCSVAsset

--- great_expectations/datasource/fluent/spark_file_path_datasource.py
+++ great_expectations/datasource/fluent/spark_file_path_datasource.py
@@ -56,48 +56,6 @@ class _SparkGenericFilePathAsset(_FilePathDataAsset):
         }
 
 
-csv_def = """
-def csv(
-    self,
-    path: PathOrPaths,
-    schema: Optional[Union[StructType, str]] = None,
-    sep: Optional[str] = None,
-    encoding: Optional[str] = None,
-    quote: Optional[str] = None,
-    escape: Optional[str] = None,
-    comment: Optional[str] = None,
-    header: Optional[Union[bool, str]] = None,
-    inferSchema: Optional[Union[bool, str]] = None,
-    ignoreLeadingWhiteSpace: Optional[Union[bool, str]] = None,
-    ignoreTrailingWhiteSpace: Optional[Union[bool, str]] = None,
-    nullValue: Optional[str] = None,
-    nanValue: Optional[str] = None,
-    positiveInf: Optional[str] = None,
-    negativeInf: Optional[str] = None,
-    dateFormat: Optional[str] = None,
-    timestampFormat: Optional[str] = None,
-    maxColumns: Optional[Union[int, str]] = None,
-    maxCharsPerColumn: Optional[Union[int, str]] = None,
-    maxMalformedLogPerPartition: Optional[Union[int, str]] = None,
-    mode: Optional[str] = None,
-    columnNameOfCorruptRecord: Optional[str] = None,
-    multiLine: Optional[Union[bool, str]] = None,
-    charToEscapeQuoteEscaping: Optional[str] = None,
-    samplingRatio: Optional[Union[float, str]] = None,
-    enforceSchema: Optional[Union[bool, str]] = None,
-    emptyValue: Optional[str] = None,
-    locale: Optional[str] = None,
-    lineSep: Optional[str] = None,
-    pathGlobFilter: Optional[Union[bool, str]] = None,
-    recursiveFileLookup: Optional[Union[bool, str]] = None,
-    modifiedBefore: Optional[Union[bool, str]] = None,
-    modifiedAfter: Optional[Union[bool, str]] = None,
-    unescapedQuoteHandling: Optional[str] = None,
-) -> "DataFrame":
-    pass
-"""
-
-
 class CSVAsset(_SparkGenericFilePathAsset):
     type: Literal["csv"] = "csv"
 
@@ -274,11 +232,9 @@ class CSVAsset(_SparkGenericFilePathAsset):
         )
 
 
-class DirectoryCSVAsset(_DirectoryDataAsset):
+class DirectoryCSVAsset(_DirectoryDataAsset, CSVAsset):
     # Overridden inherited instance fields
     type: Literal["directory_csv"] = "directory_csv"
-    header: bool = False
-    infer_schema: bool = Field(False, alias="InferSchema")
 
     class Config:
         extra = pydantic.Extra.forbid
@@ -287,7 +243,6 @@ class DirectoryCSVAsset(_DirectoryDataAsset):
     @classmethod
     def _get_reader_method(cls) -> str:
         # Reader method is still "csv"
-        # return cls.type.replace("directory_", "")
         return "csv"
 
     def _get_reader_options_include(self) -> set[str] | None:
