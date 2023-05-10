9f013abd0ccb28753fab55bb0a79c131f845b561
Nathan Farmer <nathan@greatexpectations.io>
2023-05-10 14:53:49 -0400

    Try to make the directory with sudo

--- tests/integration/docusaurus/deployment_patterns/databricks_deployment_patterns_file_python_configs.py
+++ tests/integration/docusaurus/deployment_patterns/databricks_deployment_patterns_file_python_configs.py
@@ -19,11 +19,7 @@ base_directory = "/dbfs/example_data/nyctaxi/tripdata/yellow/"
 
 # For this test script, change base_directory to location where test runner data is located
 base_directory = "/dbfs/data/"
-original_umask = os.umask(0)
-try:
-    os.makedirs(base_directory, 0o777)
-finally:
-    os.umask(original_umask)
+os.system(f"sudo makedirs {base_directory}")
 shutil.copytree(str(pathlib.Path(pathlib.Path.cwd(), "data")), base_directory)
 
 # <snippet name="tests/integration/docusaurus/deployment_patterns/databricks_deployment_patterns_file_python_configs.py add datasource">
