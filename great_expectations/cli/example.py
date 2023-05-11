cc9483b246d0873723975bfc75369f3d951a6702
Anthony Burdi <anthony@greatexpectations.io>
2023-05-10 23:27:14 -0400

    Add pandas example cmd

--- great_expectations/cli/example.py
+++ great_expectations/cli/example.py
@@ -25,6 +25,17 @@ def example_airflow(ctx: click.Context) -> None:
     """Start an airflow example."""
     cli_message("<green>Setting up airflow example...</green>")
     repo_root = pathlib.Path(__file__).parents[2]
-    airflow_example_directory = repo_root / "examples" / "airflow"
-    airflow_example_setup_file = airflow_example_directory / "setup_airflow_2_6_0.sh"
-    subprocess.run(airflow_example_setup_file, cwd=airflow_example_directory)
+    example_directory = repo_root / "examples" / "airflow"
+    example_setup_file = example_directory / "setup_airflow_2_6_0.sh"
+    subprocess.run(example_setup_file, cwd=example_directory)
+
+
+@example.command(name="pandas")
+@click.pass_context
+def example_pandas(ctx: click.Context) -> None:
+    """Start a pandas filesystem example."""
+    cli_message("<green>Setting up pandas filesystem example...</green>")
+    repo_root = pathlib.Path(__file__).parents[2]
+    example_directory = repo_root / "examples" / "pandas_filesystem"
+    example_setup_commands = ["docker", "compose", "up"]
+    subprocess.run(example_setup_commands, cwd=example_directory)
