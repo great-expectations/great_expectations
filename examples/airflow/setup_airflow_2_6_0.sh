eb1380d6f5bfbf0cce5732a175f8c64701c370b2
Anthony Burdi <anthony@greatexpectations.io>
2023-05-10 22:44:46 -0400

    Logging

--- examples/airflow/setup_airflow_2_6_0.sh
+++ examples/airflow/setup_airflow_2_6_0.sh
@@ -6,17 +6,27 @@ print_orange () {
     echo -e "${ORANGE}$1${NC}"
 }
 
-print_orange "Downloading airflow base docker compose"
+print_orange_line () {
+    print_orange "================================================================================"
+}
+
+print_orange_header () {
+    print_orange_line
+    print_orange "$1"
+    print_orange_line
+}
+
+print_orange_header "Downloading airflow base docker compose"
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.6.0/docker-compose.yaml'
 
-print_orange "Making directories to mount to containers"
+print_orange_header "Making directories to mount to containers"
 mkdir ./dags ./plugins ./logs
 
-print_orange "Setting user/group for directories (for mac / linux)"
+print_orange_header "Setting user/group for directories (for mac / linux)"
 echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 
-print_orange "Initializing airflow"
+print_orange_header "Initializing airflow"
 docker compose up airflow-init
 
-print_orange "Starting airflow containers"
+print_orange_header "Starting airflow containers"
 docker compose up
