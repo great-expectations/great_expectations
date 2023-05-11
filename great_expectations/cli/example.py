98ec6b563bfd98be61ed9b11555fa1622634362e
Anthony Burdi <anthony@greatexpectations.io>
2023-05-10 22:56:00 -0400

    Change current working directory

--- great_expectations/cli/example.py
+++ great_expectations/cli/example.py
@@ -25,7 +25,6 @@ def example_airflow(ctx: click.Context) -> None:
     """Start an airflow example."""
     cli_message("<green>Setting up airflow example...</green>")
     repo_root = pathlib.Path(__file__).parents[2]
-    airflow_example_setup_file = (
-        repo_root / "examples" / "airflow" / "setup_airflow_2_6_0.sh"
-    )
-    subprocess.call(airflow_example_setup_file)
+    airflow_example_directory = repo_root / "examples" / "airflow"
+    airflow_example_setup_file = airflow_example_directory / "setup_airflow_2_6_0.sh"
+    subprocess.run(airflow_example_setup_file, cwd=airflow_example_directory)
