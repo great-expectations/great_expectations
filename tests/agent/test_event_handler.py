1938f6520abb33e6630c17d82c33abbaf72b8e35
Josh Stauffer <josh@greatexpectations.io>
2023-05-12 21:45:31 -0400

    remove shutdown test

--- tests/agent/test_event_handler.py
+++ tests/agent/test_event_handler.py
@@ -3,11 +3,10 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from great_expectations.agent.event_handler import EventHandler, ShutdownRequest
+from great_expectations.agent.event_handler import EventHandler
 from great_expectations.agent.models import (
     EventBase,
     RunDataAssistantEvent,
-    ShutdownEvent,
 )
 from great_expectations.data_context import CloudDataContext
 
@@ -35,15 +34,4 @@ def test_event_handler_handles_run_data_assistant_event():
     handler = EventHandler(context=context)
 
     with pytest.raises(NotImplementedError):
-        handler.handle_event(event=event, correlation_id=correlation_id)
-
-
-def test_event_handler_handles_shutdown_event():
-    event = ShutdownEvent()
-    correlation_id = "74842258-803a-48ca-8921-eaf2802c14e2"
-    context = MagicMock(autospec=CloudDataContext)
-
-    handler = EventHandler(context=context)
-
-    with pytest.raises(ShutdownRequest):
         handler.handle_event(event=event, correlation_id=correlation_id)
