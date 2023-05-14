bfc87deef80f7ac6f4e3d9aa14e466913ddf1b7c
Anthony Burdi <anthony@greatexpectations.io>
2023-05-14 15:59:36 -0400

    Postgres example WIP

--- examples/postgres/load_data.py
+++ examples/postgres/load_data.py
@@ -4,6 +4,11 @@ import pathlib
 import pandas as pd
 import sqlalchemy as sa
 
+# TODO: Better waiting for db (polling):
+print("Waiting for db to be ready")
+import time
+time.sleep(5)
+
 print("Loading data into dataframe...")
 csv_path = pathlib.Path("yellow_tripdata_sample_2019-01.csv")
 df = pd.read_csv(csv_path)
