20713b2c5c837ee5548367ae24d74217981d3e73
Josh Stauffer <josh@greatexpectations.io>
2023-05-15 09:17:58 -0400

    update test

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -68,7 +68,7 @@ def test_gx_agent_run_invokes_consume(get_context, subscriber, client, gx_agent_
     agent.run()
 
     subscriber().consume.assert_called_with(
-        queue=gx_agent_config.organization_id, on_message=agent._handle_event
+        queue=gx_agent_config.organization_id, on_message=agent._handle_event_as_thread
     )
 
 
