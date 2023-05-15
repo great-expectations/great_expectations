b5f1a3071d9c5f81e4ab14e553c199dfe538a31b
Anthony Burdi <anthony@greatexpectations.io>
2023-05-15 17:54:48 -0400

    Lint

--- tests/compatibility/test_compatibility.py
+++ tests/compatibility/test_compatibility.py
@@ -5,11 +5,13 @@ from great_expectations.compatibility.not_imported import NotImported
 # TODO: parametrize test_error_raised_when_optional_import_not_installed over all optional dependencies
 # TODO: Test to make sure we don't have optional dependencies that are not in our set of tests in test_error_raised_when_optional_import_not_installed
 
+
 @pytest.mark.compatibility
 def test_error_raised_when_optional_import_not_installed():
     from great_expectations.compatibility.pyspark import pyspark
+
     breakpoint()
     assert isinstance(pyspark, NotImported)
     assert not pyspark
     with pytest.raises(ModuleNotFoundError):
-        pyspark.some_param
+        _ = pyspark.some_param
