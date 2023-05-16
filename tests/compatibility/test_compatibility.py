a0240e811ecdcee10f74626ebb8f3ec1d42a96af
Anthony Burdi <anthony@greatexpectations.io>
2023-05-16 14:55:10 -0400

    WIP test_optional_import_fixture_completeness

--- tests/compatibility/test_compatibility.py
+++ tests/compatibility/test_compatibility.py
@@ -1,18 +1,61 @@
+import pathlib
 
 import pytest
 
+from great_expectations.compatibility.pyarrow import pyarrow
 from great_expectations.compatibility.google import google
 from great_expectations.compatibility.docstring_parser import docstring_parser
 from great_expectations.compatibility.azure import azure
 from great_expectations.compatibility.not_imported import NotImported
-from great_expectations.compatibility.pyspark import pyspark
 from great_expectations.compatibility.aws import boto3
 from great_expectations.compatibility.aws import botocore
+from great_expectations.compatibility.pyspark import pyspark
+from great_expectations.compatibility.sqlalchemy import sqlalchemy
 
-# TODO: parametrize test_error_raised_when_optional_import_not_installed over all optional dependencies
-# TODO: Test to make sure we don't have optional dependencies that are not in our set of tests in test_error_raised_when_optional_import_not_installed
 
-libraries = [pyspark, boto3, botocore, azure, docstring_parser, google]
+libraries = [
+    pyspark,
+    boto3,
+    botocore,
+    azure,
+    docstring_parser,
+    google,
+    pyarrow,
+    sqlalchemy,
+]
+
+expected_files = [
+    "pyarrow",
+    "azure",
+    "sqlalchemy_compatibility_wrappers",
+    "__init__",
+    "google",
+    "numpy",
+    "__pycache__",
+    "pandas_compatibility",
+    "aws",
+    "README",
+    "sqlalchemy",
+    "sqlalchemy_and_pandas",
+    "not_imported",
+    "pyspark",
+    "docstring_parser",
+]
+
+
+@pytest.mark.compatibility
+def test_optional_import_fixture_completeness():
+    """What does this test and why?
+
+    Make sure we don't have optional dependencies that are not in our set of tests in test_error_raised_when_optional_import_not_installed
+    """
+    repo_root = pathlib.Path(__file__).parents[2]
+    compatibility_dir = repo_root / "great_expectations" / "compatibility"
+    for filename in compatibility_dir.iterdir():
+        assert (
+            filename.stem in expected_files
+        ), "Please add representative import to libraries and the filename to expected_files"
+
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
