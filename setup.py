c476f76c390422e24d6bc9db26cadb0773041f98
Will Shin <will@greatexpectations.io>
2023-05-18 16:03:16 -0700

    pushing requirement

--- setup.py
+++ setup.py
@@ -110,7 +110,6 @@ config = {
         "Topic :: Software Development :: Testing",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
