88bb13c655808264de7f38a7300f175ec45c80e4
Thu Pham <thu.pham@greatexpectations.io>
2023-05-18 17:50:14 -0700

    Fix the test failure

--- tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data.py
+++ tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data.py
@@ -1,13 +1,44 @@
+import pathlib
+import sqlite3
+
+
+def get_sqlite_database_path() -> pathlib.Path:
+    relative_path = pathlib.Path(
+        "..",
+        "..",
+        "..",
+        "test_sets",
+        "taxi_yellow_tripdata_samples",
+        "sqlite",
+        "yellow_tripdata.db",
+    )
+    return pathlib.Path(__file__).parent.joinpath(relative_path).resolve(strict=True)
+
+
+sqlite_database_path = get_sqlite_database_path()
+assert sqlite_database_path.is_file()
+
+
+def get_table_name(sqlite_database_path: pathlib.Path) -> list:
+    conn = sqlite3.connect(sqlite_database_path)
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name")
+    return c.fetchone()[0]
+
+
+my_table_name = get_table_name(sqlite_database_path)
+assert my_table_name is not None
+
+
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data import">
 import great_expectations as gx
 
 context = gx.get_context()
 # </snippet>
-
-assert context is not None 
+assert context is not None
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data connection_string">
-my_connection_string = "sqlite:///<PATH_TO_DB_FILE>"
+my_connection_string = f"sqlite:///{sqlite_database_path}"
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data datasource_name">
@@ -15,17 +46,19 @@ datasource_name = "my_datasource"
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data datasource">
-datasource = context.sources.add_sqlite(name=datasource_name, connection_string=my_connection_string)
+datasource = context.sources.add_sqlite(
+    name=datasource_name, connection_string=my_connection_string
+)
 # </snippet>
 
-assert "base_directory:" in str(datasource)
+assert f"connection_string: sqlite:///{sqlite_database_path}" in str(datasource)
 assert "name: my_datasource" in str(datasource)
 assert "type: sqlite" in str(datasource)
 
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data assest_name">
 asset_name = "my_asset"
-asset_table_name = "yellow_tripdata_sample"
+asset_table_name = my_table_name
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data table_asset">
@@ -36,8 +69,8 @@ assert table_asset is not None
 
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data asset_query">
-asset_name = "my_asset"
-asset_query = "SELECT * from yellow_tripdata_sample"
+asset_name = "my_query_asset"
+asset_query = "SELECT * from yellow_tripdata_sample_2019_01"
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/how_to_connect_to_sqlite_data query_table_asset">
