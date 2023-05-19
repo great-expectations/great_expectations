b93ccbb81c2ebd5c76702370ee5381bada604763
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2023-05-19 16:57:57 -0700

    [FEATURE] Implementing Python code snippets under test for "https://docs.greatexpectations.io/docs/guides/connecting_to_your_data/fluent/cloud/how_to_connect_to_data_on_s3_using_spark" (#7943)

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_quickly_connect_to_a_single_file_with_pandas.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_quickly_connect_to_a_single_file_with_pandas.py
@@ -4,8 +4,6 @@ To run this code as a local test, use the following console command:
 pytest -v --docs-tests -m integration -k "how_to_quickly_connect_to_a_single_file_with_pandas" tests/integration/test_script_runner.py
 ```
 """
-import pathlib
-
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_quickly_connect_to_a_single_file_with_pandas.py get_context">
