4652cbe9a43492c2aae731b71b126c7e032a2a26
Alex Sherstinsky <alex@greatexpectations.io>
2023-05-19 07:24:22 -0700

    Implementing Python code snippets under test for "https://docs.greatexpectations.io/docs/guides/connecting_to_your_data/fluent/cloud/how_to_connect_to_data_on_s3_using_pandas"

--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -363,6 +363,7 @@ fluent_datasources = [
         name="how_to_connect_to_data_on_s3_using_pandas",
         user_flow_script="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_data_on_s3_using_pandas.py",
         data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
+        backend_dependencies=[BackendDependencies.AWS],
     ),
 ]
 
