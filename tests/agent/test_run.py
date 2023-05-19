c6c941995142adc29eaf52ed7bcd3e3d32fb4278
Josh Stauffer <josh@greatexpectations.io>
2023-05-18 23:10:16 -0400

    rename

--- tests/agent/test_run.py
+++ tests/agent/test_run.py
@@ -1,8 +1,8 @@
-from great_expectations.agent import run_agent
+from great_expectations.agent import run
 
 
 def test_run_calls_gx_agent(mocker):
     agent = mocker.patch("great_expectations.agent.run.GXAgent")
-    run_agent()
+    run()
     agent.assert_called_with()
     agent().run.assert_called_with()
