c8a9bc393aa5164665c55ea032883ca2884c769b
Thu Pham <thu.pham@greatexpectations.io>
2023-05-22 12:34:33 -0700

    fix typo

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py
@@ -10,12 +10,12 @@ import great_expectations as gx
 context = gx.get_context()
 
 # Python
-# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py.py sql_connection_string">
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py sql_connection_string">
 sql_connection_string = "postgresql+psycopg2://username:my_password@localhost/test"
 # </snippet>
 
 # Python
-# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py.py connection_string">
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py connection_string">
 connection_string = (
     "postgresql+psycopg2://<USERNAME>:${MY_PASSWORD}@<HOST>:<PORT>/<DATABASE>"
 )
@@ -24,7 +24,7 @@ connection_string = (
 sql_connection_string = test_utils.get_default_postgres_url()
 
 # Python
-# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py.py add_sql">
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py add_sql">
 datasource = context.sources.add_sql(
     name="my_datasource", connection_string=sql_connection_string
 )
