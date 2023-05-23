8dd9cb9dcff8e3461b83c0c9aabb68d9c9f7efe0
Anthony Burdi <anthony@greatexpectations.io>
2023-05-23 12:28:01 -0400

    [DOCS] Fix issues with technical tags links in earlier versions (#7961)

--- tests/docs/test_prepare_prior_versions.py
+++ tests/docs/test_prepare_prior_versions.py
@@ -1,7 +1,10 @@
+import pathlib
+
 import pytest
 
 from docs.prepare_prior_versions import (
     _update_tag_references_for_correct_version_substitution,
+    _use_relative_imports_for_tag_references_substitution,
 )
 
 
@@ -24,4 +27,32 @@ def test__update_tag_references_for_correct_version_substitution():
     <a href={'/docs/0.15.50/' + data[props.tag].url}>{props.text}</a>
     <span class="tooltiptext">{data[props.tag].definition}</span>
 </span>"""
+    assert updated_contents == expected_contents
+
+
+@pytest.mark.unit
+def test__use_relative_imports_for_tag_references_substitution():
+    contents = """import TabItem from '@theme/TabItem';
+import TechnicalTag from '@site/docs/term_tags/_tag.mdx';
+
+This guide will help you connect to your data stored on GCS using Pandas.
+"""
+
+    path_to_versioned_docs = pathlib.Path(
+        "docs/docusaurus/versioned_docs/version-0.14.13/"
+    )
+    file_path = pathlib.Path(
+        "docs/docusaurus/versioned_docs/version-0.14.13/guides/connecting_to_your_data/cloud/gcs/pandas.md"
+    )
+
+    updated_contents = _use_relative_imports_for_tag_references_substitution(
+        contents, path_to_versioned_docs, file_path
+    )
+
+    expected_contents = """import TabItem from '@theme/TabItem';
+import TechnicalTag from '../../../../term_tags/_tag.mdx';
+
+This guide will help you connect to your data stored on GCS using Pandas.
+"""
+
     assert updated_contents == expected_contents
