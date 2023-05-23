fefd167d7af9057a4d9c5c531bbbd63a2eba8a3d
Thu Pham <thu.pham@greatexpectations.io>
2023-05-23 11:28:49 -0700

    Using sqlite instead of postgres

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py
@@ -1,13 +1,28 @@
 """
 To run this code as a local test, use the following console command:
 ```
-pytest -v --docs-tests -m integration -k "how_to_connect_to_sql_data" tests/integration/test_script_runner.py --postgresql
+pytest -v --docs-tests -m integration -k "how_to_connect_to_sql_data" tests/integration/test_script_runner.py
 ```
 """
-import tests.test_utils as test_utils
+import pathlib
 import great_expectations as gx
 
 context = gx.get_context()
+
+sqlite_database_path = str(
+    pathlib.Path(
+        gx.__file__,
+        "..",
+        "..",
+        "tests",
+        "test_sets",
+        "taxi_yellow_tripdata_samples",
+        "sqlite",
+        "yellow_tripdata.db",
+    ).resolve(strict=True)
+)
+
+my_table_name = "yellow_tripdata_sample_2019_01"
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py sql_connection_string">
@@ -21,7 +36,7 @@ connection_string = (
 )
 # </snippet>
 
-connection_string = test_utils.get_default_postgres_url()
+connection_string = f"sqlite:///{sqlite_database_path}"
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py add_sql">
@@ -32,3 +47,35 @@ datasource = context.sources.add_sql(
 
 assert datasource
 assert datasource.name == "my_datasource"
+
+asset_name = "my_asset"
+datasource.add_table_asset(name=asset_name, table_name=my_table_name)
+
+assert datasource.get_asset_names() == {asset_name}
+
+my_asset = datasource.get_asset(asset_name)
+assert my_asset
+
+my_batch_request = my_asset.build_batch_request()
+batches = my_asset.get_batch_list_from_batch_request(my_batch_request)
+assert len(batches) == 1
+assert set(batches[0].columns()) == {
+    "vendor_id",
+    "pickup_datetime",
+    "dropoff_datetime",
+    "passenger_count",
+    "trip_distance",
+    "rate_code_id",
+    "store_and_fwd_flag",
+    "pickup_location_id",
+    "dropoff_location_id",
+    "payment_type",
+    "fare_amount",
+    "extra",
+    "mta_tax",
+    "tip_amount",
+    "tolls_amount",
+    "improvement_surcharge",
+    "total_amount",
+    "congestion_surcharge",
+}
--- tests/integration/test_script_runner.py
+++ tests/integration/test_script_runner.py
@@ -368,7 +368,6 @@ fluent_datasources = [
         name="how_to_connect_to_sql_data",
         user_flow_script="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_sql_data.py",
         data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
-        backend_dependencies=[BackendDependencies.POSTGRESQL],
     ),
     IntegrationTestFixture(
         name="how_to_connect_to_data_on_s3_using_pandas",
