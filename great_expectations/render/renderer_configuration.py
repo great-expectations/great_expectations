b57a02d5fa31428a90f8e2da31a0c3e88ae313b6
Nathan Farmer <nathan@greatexpectations.io>
2023-05-24 23:13:25 -0400

    Update meta_notes validator on RendererConfiguration

--- great_expectations/render/renderer_configuration.py
+++ great_expectations/render/renderer_configuration.py
@@ -370,7 +370,9 @@ class RendererConfiguration(GenericModel, Generic[RendererParams]):
 
     @root_validator()
     def _validate_for_meta_notes(cls, values: dict) -> dict:
-        meta_notes: Optional[list[str] | tuple[str] | str]
+        meta_notes: Optional[
+            dict[str, Optional[dict[str, list[str] | tuple[str] | str]]]
+        ]
         if (
             "result" in values
             and values["result"] is not None
@@ -380,18 +382,14 @@ class RendererConfiguration(GenericModel, Generic[RendererParams]):
         else:
             meta_notes = values["configuration"].meta.get("notes")
 
-        if isinstance(meta_notes["content"], (list, tuple)):
-            meta_notes["content"] = list(meta_notes["content"])
-            values["meta_notes"] = meta_notes
-        elif isinstance(meta_notes["content"], str):
-            meta_notes["content"] = [meta_notes["content"]]
-            values["meta_notes"] = meta_notes
-        else:
-            raise ValueError(
-                "Meta Notes Content must be either a list, tuple, or string "
-                "but content of type {type(meta_notes['content'])} was passed."
-            )
+        meta_notes_content = meta_notes.get("content") if meta_notes else None
 
+        if isinstance(meta_notes_content, (list, tuple)):
+            meta_notes["content"] = list(meta_notes_content)
+        elif isinstance(meta_notes_content, str):
+            meta_notes["content"] = [meta_notes_content]
+
+        values["meta_notes"] = meta_notes
         return values
 
     @root_validator()
