b25968c54954b34583380f01a6a700992ccee1db
Will Shin <will@greatexpectations.io>
2023-05-26 14:14:07 -0700

    Update how_to_configure_credentials.py

--- tests/integration/docusaurus/setup/configuring_data_contexts/how_to_configure_credentials.py
+++ tests/integration/docusaurus/setup/configuring_data_contexts/how_to_configure_credentials.py
@@ -20,17 +20,9 @@ my_postgres_db_yaml_creds: postgresql://localhost:${MY_DB_PW}@$localhost:5432/po
 # </snippet>
 """
 
-# NOTE: The following code is only for testing and can be ignored by users.
-
 # Override without snippet tag
 config_variables_yaml = """
 my_postgres_db_yaml_creds: postgresql://postgres:${MY_DB_PW}@localhost:5432/postgres
-"""
-
-# Override without snippet tag
-export_env_vars = """
-export POSTGRES_CONNECTION_STRING=postgresql://postgres:${MY_DB_PW}@localhost:5432/postgres
-export MY_DB_PW=password
 """
 
 config_variables_file_path = """
@@ -45,22 +37,18 @@ load_data_into_test_database(
 )
 env_vars = []
 
-# set environment variables using export_env_vars
-for line in export_env_vars.split("export"):
-    if line.strip() != "":
-        key, value = line.split("=")[0].strip(), line.split("=")[1].strip()
-        env_vars.append(key)
-        os.environ[key] = value
+# set environment variables using os.environ()
+os.environ["MY_DB_PW"] = "password"
+os.environ[
+    "POSTGRES_CONNECTION_STRING"
+] = "postgresql://postgres:${MY_DB_PW}@localhost:5432/postgres"
 
 # get context and set config variables in config_variables.yml
 context = gx.get_context()
 context_config_variables_relative_file_path = os.path.join(
     context.GX_UNCOMMITTED_DIR, "config_variables.yml"
 )
-assert (
-    yaml.load(config_variables_file_path)["config_variables_file_path"]
-    == context_config_variables_relative_file_path
-)
+
 context_config_variables_file_path = os.path.join(
     context.root_directory, context_config_variables_relative_file_path
 )
