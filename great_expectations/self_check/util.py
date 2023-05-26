977a705b5cc8ecb714a3633fec908850fe4d7b26
Anthony Burdi <anthony@greatexpectations.io>
2023-05-26 16:25:26 -0400

    Use connection instead of engine

--- great_expectations/self_check/util.py
+++ great_expectations/self_check/util.py
@@ -1033,22 +1033,21 @@ def build_sa_validator_with_data(  # noqa: C901 - 39
         execution_engine=execution_engine, table_name=table_name
     )
     with execution_engine.get_connection() as connection:
-        with connection.begin():
-            _debug("Calling df.to_sql")
-            _start = time.time()
-            add_dataframe_to_db(
-                df=df,
-                name=table_name,
-                con=engine,
-                index=False,
-                dtype=sql_dtypes,
-                if_exists="replace",
-                method=sql_insert_method,
-            )
-            _end = time.time()
-            _debug(
-                f"Took {_end - _start} seconds to df.to_sql for {sa_engine_name} {extra_debug_info}"
-            )
+        _debug("Calling df.to_sql")
+        _start = time.time()
+        add_dataframe_to_db(
+            df=df,
+            name=table_name,
+            con=connection,
+            index=False,
+            dtype=sql_dtypes,
+            if_exists="replace",
+            method=sql_insert_method,
+        )
+        _end = time.time()
+        _debug(
+            f"Took {_end - _start} seconds to df.to_sql for {sa_engine_name} {extra_debug_info}"
+        )
 
     if context is None:
         context = build_in_memory_runtime_context()
