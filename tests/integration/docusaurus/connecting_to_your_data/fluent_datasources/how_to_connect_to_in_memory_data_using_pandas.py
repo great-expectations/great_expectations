8e8a59b8d775d8ee53334f57952f9a7e89991b55
Thu Pham <thu.pham@greatexpectations.io>
2023-06-02 11:03:33 -0700

    [FEATURE] DX-441 put how_to_connect_to_in_memory_data_using_pandas under test

---
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py
@@ -0,0 +1,41 @@
+"""
+To run this code as a local test, use the following console command:
+```
+pytest -v --docs-tests -m integration -k "how_to_connect_to_in_memory_data_using_pandas" tests/integration/test_script_runner.py
+```
+"""
+
+import pathlib
+import great_expectations as gx
+
+context = gx.get_context()
+
+# Python
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py datasource">
+datasource = context.sources.add_pandas(name="my_pandas_datasource")
+# </snippet>
+
+
+# Python
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py dataframe">
+import pandas as pd
+
+dataframe = pd.read_parquet(
+    "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-11.parquet"
+)
+# </snippet>
+
+# Python
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py name">
+name = "taxi_dataframe"
+# </snippet>
+
+# Python
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py data_asset">
+data_asset = datasource.add_dataframe_asset(name=name, dataframe=dataframe)
+# </snippet>
+
+my_batch_request = data_asset.build_batch_request()
+assert my_batch_request.datasource_name == "my_pandas_datasource"
+assert my_batch_request.data_asset_name == "taxi_dataframe"
+assert my_batch_request.options == {}
