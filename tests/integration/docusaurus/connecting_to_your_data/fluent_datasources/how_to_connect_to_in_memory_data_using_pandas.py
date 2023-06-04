18bff4833ee3295665c73bdb8f1c26db4dab2b26
Thu Pham <thu.pham@greatexpectations.io>
2023-06-04 07:36:07 -0700

    update function args

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py
@@ -32,10 +32,10 @@ name = "taxi_dataframe"
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py data_asset">
-data_asset = datasource.add_dataframe_asset(name=name, dataframe=dataframe)
+data_asset = datasource.add_dataframe_asset(name=name)
 # </snippet>
 
-my_batch_request = data_asset.build_batch_request()
+my_batch_request = data_asset.build_batch_request(dataframe=dataframe)
 assert my_batch_request.datasource_name == "my_pandas_datasource"
 assert my_batch_request.data_asset_name == "taxi_dataframe"
 assert my_batch_request.options == {}
