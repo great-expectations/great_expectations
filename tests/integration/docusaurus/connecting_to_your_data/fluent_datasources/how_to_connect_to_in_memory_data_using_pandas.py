5f85aa4bbfc2034ddcd15255a39398ea5a32d44f
Alex Sherstinsky <alex@greatexpectations.io>
2023-06-05 16:11:50 -0700

    Add guidance for "DataFrameAsset.build_batch_request()" in "Connecting to Ephemeral Data" documentation.

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py
@@ -35,7 +35,11 @@ name = "taxi_dataframe"
 data_asset = datasource.add_dataframe_asset(name=name)
 # </snippet>
 
+# Python
+# <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_pandas.py build_batch_request_with_dataframe">
 my_batch_request = data_asset.build_batch_request(dataframe=dataframe)
+# </snippet>
+
 assert my_batch_request.datasource_name == "my_pandas_datasource"
 assert my_batch_request.data_asset_name == "taxi_dataframe"
 assert my_batch_request.options == {}
