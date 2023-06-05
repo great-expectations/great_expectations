3e2f5c1f8ac40f3c825707eff63fc065ccbfbe02
Josh Stauffer <josh@greatexpectations.io>
2023-06-05 00:10:44 -0700

    update ruff ignore

--- great_expectations/agent/actions/run_onboarding_data_assistant.py
+++ great_expectations/agent/actions/run_onboarding_data_assistant.py
@@ -22,7 +22,7 @@ class RunOnboardingDataAssistantAction(AgentAction[RunOnboardingDataAssistantEve
         checkpoint_name = f"{event.data_asset_name} onboarding assistant checkpoint"
 
         # build tz aware timestamp
-        tz = datetime.now().astimezone().tzinfo  # noqa: DTZ003
+        tz = datetime.now().astimezone().tzinfo  # noqa: DTZ005
         timestamp = datetime.now(tz=tz)
 
         # ensure we have unique names for created resources
