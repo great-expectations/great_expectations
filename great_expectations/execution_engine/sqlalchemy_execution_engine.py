02b080a8c6cf2b955e7a18c605d0fc345655f3f3
William Shin <will@greatexpectations.io>
2023-06-07 11:28:13 -0700

    let's gooooo

--- great_expectations/execution_engine/sqlalchemy_execution_engine.py
+++ great_expectations/execution_engine/sqlalchemy_execution_engine.py
@@ -1459,7 +1459,7 @@ class SqlAlchemyExecutionEngine(ExecutionEngine):
         with self.get_connection() as connection:
             if not connection.closed:
                 result = connection.execute(query)
-                result = connection.commit()
+                connection.commit()
             else:
                 with connection.begin():
                     result = connection.execute(query)
