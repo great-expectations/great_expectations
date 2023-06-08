fe5cebbdab68c4f0aeebe4f19baf1d0e8a07b8d9
Robert Moses Lim <robert.m.lim@gmail.com>
2023-06-07 22:54:53 -0700

    Nix redundant cast

--- great_expectations/data_context/data_context/cloud_data_context.py
+++ great_expectations/data_context/data_context/cloud_data_context.py
@@ -778,7 +778,7 @@ class CloudDataContext(SerializableDataContext):
                 data_context=self,
                 name=name,
             )
-        return cast(Checkpoint, result)
+        return result
 
     def _determine_default_action_list(self) -> Sequence[ActionDict]:
         default_actions = super()._determine_default_action_list()
