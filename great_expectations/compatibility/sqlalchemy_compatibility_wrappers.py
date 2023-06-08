cd0542316ff2b7f1c52ca6a3e9b98a9935feda29
William Shin <will@greatexpectations.io>
2023-06-08 16:06:05 -0700

    lint

--- great_expectations/compatibility/sqlalchemy_compatibility_wrappers.py
+++ great_expectations/compatibility/sqlalchemy_compatibility_wrappers.py
@@ -120,7 +120,7 @@ def add_dataframe_to_db(  # noqa: PLR0913
         # Note that RemovedIn20Warning is the warning class that we see from sqlalchemy
         # but using the base class here since sqlalchemy is an optional dependency and this
         # warning type only exists in sqlalchemy < 2.0.
-        # while loop 
+        # while loop
         warnings.filterwarnings(action="ignore", category=DeprecationWarning)
         for i in range(5):
             try:
