2a11f0d1fcad49a4ec4389f732871e5725fe5a1e
Anthony Burdi <anthony@greatexpectations.io>
2023-06-13 14:22:46 -0400

    Ignore mypy error

--- great_expectations/core/batch.py
+++ great_expectations/core/batch.py
@@ -690,7 +690,7 @@ class BatchData:
         return pd.DataFrame({})
 
 
-if pyspark_DataFrame:
+if pyspark_DataFrame:  # type: ignore[truthy-function]
     BatchDataType: TypeAlias = Union[
         Type[BatchData], Type[pd.DataFrame], Type[pyspark_DataFrame]
     ]
