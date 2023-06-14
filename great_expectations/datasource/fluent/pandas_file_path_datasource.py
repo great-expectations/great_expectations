79c33b539f2c61bf8a70642c6de24781f941cdc2
Gabriel Gore <gabriel59kg@gmail.com>
2023-06-14 11:58:14 -0400

    don't ignore

--- great_expectations/datasource/fluent/pandas_file_path_datasource.py
+++ great_expectations/datasource/fluent/pandas_file_path_datasource.py
@@ -32,8 +32,8 @@ _PANDAS_FILE_TYPE_READER_METHOD_UNSUPPORTED_LIST = (
     # "read_parquet",
     "read_clipboard",  # not path based
     # "read_feather",
-    "read_fwf",  # unhandled type
-    "read_gbq",  # not path based
+    # "read_fwf",
+    # "read_gbq",
     # "read_hdf",
     # "read_html",
     # "read_orc",
@@ -59,6 +59,9 @@ CSVAsset: Type[_FilePathDataAsset] = _FILE_PATH_ASSET_MODELS.get(
 )
 ExcelAsset: Type[_FilePathDataAsset] = _FILE_PATH_ASSET_MODELS.get(
     "excel", _FilePathDataAsset
+)
+FixedWidthFileAsset: Type[_FilePathDataAsset] = _FILE_PATH_ASSET_MODELS.get(
+    "fwf", _FilePathDataAsset
 )
 JSONAsset: Type[_FilePathDataAsset] = _FILE_PATH_ASSET_MODELS.get(
     "json", _FilePathDataAsset
