0929f8bf3093c7cb927ef559851ffc009c33cfbf
Chetan Kini <chetan@superconductive.com>
2023-06-14 13:07:58 +0000

    test: patch test

--- tests/integration/usage_statistics/test_usage_statistics_messages.py
+++ tests/integration/usage_statistics/test_usage_statistics_messages.py
@@ -371,7 +371,7 @@ valid_usage_statistics_messages = {
             "event_payload": {
                 "platform.system": "Darwin",
                 "platform.release": "19.3.0",
-                "version_info": "sys.version_info(major=3, minor=9, micro=2, releaselevel='final', serial=0)",
+                "version_info": "sys.version_info(major=3, minor=8, micro=4, releaselevel='final', serial=0)",
                 "anonymized_datasources": [
                     {
                         "anonymized_name": "f57d8a6edae4f321b833384801847498",
@@ -387,12 +387,70 @@ valid_usage_statistics_messages = {
                             "parent_class": "TupleFilesystemStoreBackend"
                         },
                     },
+                    {
+                        "anonymized_name": "313cbd9858dd92f3fc2ef1c10ab9c7c8",
+                        "parent_class": "ValidationsStore",
+                        "anonymized_store_backend": {
+                            "parent_class": "TupleFilesystemStoreBackend"
+                        },
+                    },
+                    {
+                        "anonymized_name": "2d487386aa7b39e00ed672739421473f",
+                        "parent_class": "EvaluationParameterStore",
+                        "anonymized_store_backend": {
+                            "parent_class": "InMemoryStoreBackend"
+                        },
+                    },
+                ],
+                "anonymized_validation_operators": [
+                    {
+                        "anonymized_name": "99d14cc00b69317551690fb8a61aca94",
+                        "parent_class": "ActionListValidationOperator",
+                        "anonymized_action_list": [
+                            {
+                                "anonymized_name": "5a170e5b77c092cc6c9f5cf2b639459a",
+                                "parent_class": "StoreValidationResultAction",
+                            },
+                            {
+                                "anonymized_name": "0fffe1906a8f2a5625a5659a848c25a3",
+                                "parent_class": "StoreEvaluationParametersAction",
+                            },
+                            {
+                                "anonymized_name": "101c746ab7597e22b94d6e5f10b75916",
+                                "parent_class": "UpdateDataDocsAction",
+                            },
+                        ],
+                    },
+                ],
+                "anonymized_data_docs_sites": [
+                    {
+                        "parent_class": "SiteBuilder",
+                        "anonymized_name": "eaf0cf17ad63abf1477f7c37ad192700",
+                        "anonymized_store_backend": {
+                            "parent_class": "TupleFilesystemStoreBackend"
+                        },
+                        "anonymized_site_index_builder": {
+                            "parent_class": "DefaultSiteIndexBuilder",
+                        },
+                    },
                 ],
                 "anonymized_expectation_suites": [
                     {
                         "anonymized_name": "238e99998c7674e4ff26a9c529d43da4",
-                        "expectation_count": 4,
+                        "expectation_count": 8,
                         "anonymized_expectation_counts": [
+                            {
+                                "expectation_type": "expect_column_value_lengths_to_be_between",
+                                "count": 1,
+                            },
+                            {
+                                "expectation_type": "expect_table_row_count_to_be_between",
+                                "count": 1,
+                            },
+                            {
+                                "expectation_type": "expect_column_values_to_not_be_null",
+                                "count": 2,
+                            },
                             {
                                 "expectation_type": "expect_column_distinct_values_to_be_in_set",
                                 "count": 1,
@@ -416,7 +474,7 @@ valid_usage_statistics_messages = {
             "event": "data_context.__init__",
             "success": True,
             "version": "2",
-            "event_time": "2023-06-13T01:14:21.155Z",
+            "event_time": "2020-06-08T01:14:21.155Z",
             "data_context_id": "96c547fe-e809-4f2e-b122-0dc91bb7b3ad",
             "data_context_instance_id": "445a8ad1-2bd0-45ce-bb6b-d066afe996dd",
             "mac_address": TEST_MAC_ADDRESS,
