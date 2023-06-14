dd947505d98c36475011ae48443d913e8ccb7646
Alex Sherstinsky <alex@greatexpectations.io>
2023-06-14 12:18:09 -0700

    types

--- great_expectations/rule_based_profiler/data_assistant/onboarding_data_assistant.py
+++ great_expectations/rule_based_profiler/data_assistant/onboarding_data_assistant.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 from great_expectations.core.domain import SemanticDomainTypes
--- great_expectations/rule_based_profiler/data_assistant/volume_data_assistant.py
+++ great_expectations/rule_based_profiler/data_assistant/volume_data_assistant.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 from great_expectations.rule_based_profiler.config import ParameterBuilderConfig
