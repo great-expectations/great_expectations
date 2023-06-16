ae281218ee391720b5687ba2894a603102502b2b
Gabriel Gore <gabriel59kg@gmail.com>
2023-06-16 11:58:40 -0400

    "remove" env var deletion logic

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,12 @@
-import os
-import sys
+# import os
+# import sys
 
-from great_expectations.data_context.cloud_constants import GXCloudEnvironmentVariable
+# from great_expectations.data_context.cloud_constants import GXCloudEnvironmentVariable
 
-if sys.platform == "darwin":
-    for var in GXCloudEnvironmentVariable:
-        if var in os.environ:
-            print(f"Unsetting environment variable {var}")
-            os.environ.pop(var)
+# if sys.platform == "darwin":
+#     for var in GXCloudEnvironmentVariable:
+#         if var in os.environ:
+#             print(f"Unsetting environment variable {var}")
+#             os.environ.pop(var)
+
+# TODO: delete this
