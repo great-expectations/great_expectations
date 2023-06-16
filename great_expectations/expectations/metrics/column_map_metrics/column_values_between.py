cd60af2d82791c08efe4b0e1a49758f73d3ec398
Thu Pham <thu.pham@greatexpectations.io>
2023-06-16 13:23:36 -0700

    ruff

--- great_expectations/expectations/metrics/column_map_metrics/column_values_between.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_between.py
@@ -30,7 +30,7 @@ class ColumnValuesBetween(ColumnMapMetricProvider):
     )
 
     @column_condition_partial(engine=PandasExecutionEngine)
-    def _pandas(  # noqa: C901, PLR0912, PLR0913, PLR0915
+    def _pandas(  # noqa: C901, PLR0912, PLR0913
         cls,
         column,
         min_value=None,
@@ -91,7 +91,7 @@ class ColumnValuesBetween(ColumnMapMetricProvider):
                 temp_column, min_value, max_value, strict_min, strict_max
             )
 
-        def is_between(val):  # noqa: C901, PLR0911, PLR0912
+        def is_between(val):  # noqa: PLR0911, PLR0912
             # TODO Might be worth explicitly defining comparisons between types (for example, between strings and ints).
             # Ensure types can be compared since some types in Python 3 cannot be logically compared.
             # print type(val), type(min_value), type(max_value), val, min_value, max_value
