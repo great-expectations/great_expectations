b346ee8f2d50a5fc0dae088d1bf569ab59f19cca
Chetan Kini <chetan@superconductive.com>
2023-06-21 19:37:30 +0000

    feat: add class

---
+++ great_expectations/datasource/fluent/snowflake_datasource.py
@@ -0,0 +1,23 @@
+from typing import Literal, Union
+
+from great_expectations.core._docs_decorators import public_api
+from great_expectations.datasource.fluent.config_str import ConfigStr
+from great_expectations.datasource.fluent.sql_datasource import SQLDatasource
+
+class SnowflakeDsn:
+    pass
+
+@public_api
+class SnowflakeDatasource(SQLDatasource):
+    """Adds a Snowflake datasource to the data context.
+
+    Args:
+        name: The name of this Snowflake datasource.
+        connection_string: The SQLAlchemy connection string used to connect to the Snowflake database.
+            For example: "postgresql+psycopg2://postgres:@localhost/test_database"
+        assets: An optional dictionary whose keys are TableAsset or QueryAsset names and whose values
+            are TableAsset or QueryAsset objects.
+    """
+
+    type: Literal["snowflake"] = "snowlfake"  # type: ignore[assignment]
+    connection_string: Union[ConfigStr, SnowflakeDsn]
