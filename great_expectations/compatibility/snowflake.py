51af2685b36383b4240cc415d363056bfeeb2a93
Alex Sherstinsky <alex@greatexpectations.io>
2023-06-28 14:12:10 -0700

    AWS Redshift Compatibility

---
+++ great_expectations/compatibility/snowflake.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from great_expectations.compatibility.not_imported import NotImported
+
+SNOWFLAKE_NOT_IMPORTED = NotImported(
+    "snowflake connection components are not installed, please 'pip install snowflake-sqlalchemy snowflake-connector-python'"
+)
+
+"""
+import snowflake.sqlalchemy.snowdialect
+import snowflake.sqlalchemy.snowdialect
+import snowflake.sqlalchemy.snowdialect as snowflakeDialect
+import snowflake.sqlalchemy.custom_types as snowflaketypes
+"""
+
+try:
+    import snowflake
+except ImportError:
+    snowflake = SNOWFLAKE_NOT_IMPORTED
+
+try:
+    import snowflake.sqlalchemy as snowflakesqlalchemy
+except (ImportError, AttributeError):
+    snowflakesqlalchemy = SNOWFLAKE_NOT_IMPORTED
+
+try:
+    import snowflake.sqlalchemy.snowdialect as snowflakedialect
+except (ImportError, AttributeError):
+    snowflakedialect = SNOWFLAKE_NOT_IMPORTED
+
+try:
+    import snowflake.sqlalchemy.custom_types as snowflaketypes
+except (ImportError, AttributeError):
+    snowflaketypes = SNOWFLAKE_NOT_IMPORTED
