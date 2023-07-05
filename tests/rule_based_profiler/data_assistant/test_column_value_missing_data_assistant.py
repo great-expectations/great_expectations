93b0ba587ed2fd05c4811022aead039eafad6d17
Alex Sherstinsky <alex@greatexpectations.io>
2023-07-05 14:14:58 -0700

    tests

---
+++ tests/rule_based_profiler/data_assistant/test_column_value_missing_data_assistant.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import pytest
+
+from great_expectations import DataContext
+from great_expectations.rule_based_profiler.data_assistant_result import (
+    DataAssistantResult,
+)
+from great_expectations.rule_based_profiler.data_assistant_result.plot_result import (
+    PlotResult,
+)
+
+
+@pytest.mark.integration
+@pytest.mark.slow  # 1.67s
+def test_column_value_missing_data_assistant_plot_expectations_and_metrics_correctly_handle_empty_plot_data(
+    bobby_columnar_table_multi_batch_probabilistic_data_context: DataContext,
+) -> None:
+    context: DataContext = bobby_columnar_table_multi_batch_probabilistic_data_context
+
+    batch_request: dict = {
+        "datasource_name": "taxi_pandas",
+        "data_connector_name": "monthly",
+        "data_asset_name": "my_reports",
+    }
+
+    include_column_names: List[str] = [
+        "congestion_surcharge",
+    ]
+
+    data_assistant_result: DataAssistantResult = (
+        context.assistants.column_value_missing.run(
+            batch_request=batch_request,
+            include_column_names=include_column_names,
+        )
+    )
+    plot_result: PlotResult = data_assistant_result.plot_expectations_and_metrics(
+        include_column_names=include_column_names
+    )
+
+    # This test passes only if absense of any metrics and expectations to plot does not cause exceptions to be raised.
+    column_domain_charts: List[dict] = [p.to_dict() for p in plot_result.charts[2:]]
+    assert len(column_domain_charts) == 0
