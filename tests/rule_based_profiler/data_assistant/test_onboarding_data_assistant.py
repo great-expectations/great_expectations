8b369140a3ed103461b8864f10aca4398c65f5e2
Alex Sherstinsky <alex@greatexpectations.io>
2023-07-05 16:05:22 -0700

    unit tests

--- tests/rule_based_profiler/data_assistant/test_onboarding_data_assistant.py
+++ tests/rule_based_profiler/data_assistant/test_onboarding_data_assistant.py
@@ -15,7 +15,6 @@ from great_expectations.core import ExpectationSuite
 from great_expectations.core.domain import Domain
 from great_expectations.core.metric_domain_types import MetricDomainTypes
 from great_expectations.core.usage_statistics.events import UsageStatsEvents
-from great_expectations.data_context import AbstractDataContext
 from great_expectations.rule_based_profiler.altair import AltairDataTypes
 from great_expectations.rule_based_profiler.data_assistant_result import (
     DataAssistantResult,
