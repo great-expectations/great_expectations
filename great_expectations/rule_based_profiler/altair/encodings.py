94b0049f761c469eb24b84c889045f25dce94614
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-07-06 10:32:30 -0400

    Revert "catch warnings on altair types"
    This reverts commit ac24c9eb910d781294b30512fb756c9f62165989.

--- great_expectations/rule_based_profiler/altair/encodings.py
+++ great_expectations/rule_based_profiler/altair/encodings.py
@@ -1,22 +1,21 @@
-import warnings
 from enum import Enum
 
 import altair as alt
 
-with warnings.catch_warnings():
 
-    class AltairDataTypes(Enum):
-        # available data types: https://altair-viz.github.io/user_guide/encoding.html#encoding-data-types
-        QUANTITATIVE = alt.StandardType("quantitative")
-        ORDINAL = alt.StandardType("ordinal")
-        NOMINAL = alt.StandardType("nominal")
-        TEMPORAL = alt.StandardType("temporal")
+class AltairDataTypes(Enum):
+    # available data types: https://altair-viz.github.io/user_guide/encoding.html#encoding-data-types
+    QUANTITATIVE = alt.StandardType("quantitative")
+    ORDINAL = alt.StandardType("ordinal")
+    NOMINAL = alt.StandardType("nominal")
+    TEMPORAL = alt.StandardType("temporal")
 
-    class AltairAggregates(Enum):
-        # available aggregates: https://altair-viz.github.io/user_guide/encoding.html#encoding-channel-options
-        MEAN = alt.Aggregate("mean")
-        MEDIAN = alt.Aggregate("median")
-        MIN = alt.Aggregate("min")
-        MAX = alt.Aggregate("max")
-        COUNT = alt.Aggregate("count")
-        SUM = alt.Aggregate("sum")
+
+class AltairAggregates(Enum):
+    # available aggregates: https://altair-viz.github.io/user_guide/encoding.html#encoding-channel-options
+    MEAN = alt.Aggregate("mean")
+    MEDIAN = alt.Aggregate("median")
+    MIN = alt.Aggregate("min")
+    MAX = alt.Aggregate("max")
+    COUNT = alt.Aggregate("count")
+    SUM = alt.Aggregate("sum")
