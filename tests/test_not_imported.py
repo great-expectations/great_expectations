151afcec24526792e849c1928a4669616d9adfaa
William Shin <will@greatexpectations.io>
2023-07-10 16:55:33 -0700

    not imported test

--- tests/test_not_imported.py
+++ tests/test_not_imported.py
@@ -7,17 +7,17 @@ from great_expectations.compatibility.not_imported import NotImported
 def not_imported() -> NotImported:
     return NotImported("Please install foo")
 
-
+@pytest.mark.unit
 def test_get_attr_on_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, match=str(not_imported)):
         _ = not_imported.thing
 
-
+@pytest.mark.unit
 def test_set_attr_on_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, match=str(not_imported)):
         not_imported.thing = 5
 
-
+@pytest.mark.unit
 def test_calling_not_installed(not_imported):
     with pytest.raises(ModuleNotFoundError, match=str(not_imported)):
         not_imported()
