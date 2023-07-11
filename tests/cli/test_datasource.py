d7d7b62846d5758172794a8f6105473d51a70f33
Bill Dirks <bill@greatexpectations.io>
2023-07-11 16:19:51 -0700

    Add marker cli to all cli tests.

--- tests/cli/test_datasource.py
+++ tests/cli/test_datasource.py
@@ -3,6 +3,9 @@ from great_expectations.cli import cli
 from great_expectations.cli.cli_messages import DATASOURCE_NEW_WARNING
 
 
+pytestmark = [pytest.mark.cli]
+
+
 def test_cli_datasource_new_warning(empty_data_context, monkeypatch):
     context = empty_data_context
     monkeypatch.chdir(context.root_directory)
