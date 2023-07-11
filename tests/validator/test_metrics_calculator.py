874d4a04901031ffb0919ac90c4bb711e5efa953
William Shin <will@greatexpectations.io>
2023-07-11 13:08:44 -0700

    format

--- tests/validator/test_metrics_calculator.py
+++ tests/validator/test_metrics_calculator.py
@@ -40,6 +40,7 @@ def integer_and_datetime_sample_dataset() -> dict:
         ],
     }
 
+
 # noinspection PyUnusedLocal
 @pytest.mark.parametrize(
     "backend,",
@@ -50,10 +51,7 @@ def integer_and_datetime_sample_dataset() -> dict:
         pytest.param(
             "sqlite",
         ),
-        pytest.param(
-            "spark",
-            marks=[pytest.mark.spark]
-        )
+        pytest.param("spark", marks=[pytest.mark.spark]),
     ],
 )
 def test_column_partition_metric(
