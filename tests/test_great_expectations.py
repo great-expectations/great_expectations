ce908b38abde96e988ad77e80d87f76bec30474e
William Shin <will@greatexpectations.io>
2023-07-11 22:11:09 -0700

    Update tests/test_great_expectations.py
    Co-authored-by: Gabriel <gabriel59kg@gmail.com>

--- tests/test_great_expectations.py
+++ tests/test_great_expectations.py
@@ -321,8 +321,8 @@ def test_stats_mixed_expectations():
     assert expected == actual
 
 
+@pytest.mark.filesystem
 class TestIO(unittest.TestCase):
-    @pytest.mark.filesystem
     def test_read_csv(self):
         script_path = os.path.dirname(os.path.realpath(__file__))  # noqa: PTH120
         _ = gx.read_csv(
