a156f62603b2197db2ced767d46706ddd137fd2f
Haebichan Jung <haebichan.jung@greatexpectations.io>
2023-07-12 12:34:53 -0400

    Run black on how_to_validate_multiple_batches_within_single_checkpoint
    Run black on how_to_validate_multiple_batches_within_single_checkpoint.py to reformat the code

--- tests/integration/docusaurus/validation/checkpoints/how_to_validate_multiple_batches_within_single_checkpoint.py
+++ tests/integration/docusaurus/validation/checkpoints/how_to_validate_multiple_batches_within_single_checkpoint.py
@@ -4,33 +4,38 @@ from great_expectations.core.expectation_configuration import ExpectationConfigu
 context = gx.get_context()
 
 datasource = context.sources.add_pandas_filesystem(
-    name = "example_datasource",
-    base_directory='/data'
+    name="example_datasource", base_directory="/data"
 )
 
 my_batching_regex = r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})\.csv"
 
-asset = datasource.add_csv_asset('asset', batching_regex =  my_batching_regex)
+asset = datasource.add_csv_asset("asset", batching_regex=my_batching_regex)
 
 # <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_validate_multiple_batches_within_single_checkpoint.py add_suite">
-ec = ExpectationConfiguration(expectation_type= 'expect_column_values_to_not_be_null', 
-                              kwargs={ 'column': 'passenger_count'}
-                             )
-suite = context.add_expectation_suite(expectation_suite_name = 'example_suite', expectations= [ec])
+ec = ExpectationConfiguration(
+    expectation_type="expect_column_values_to_not_be_null",
+    kwargs={"column": "passenger_count"},
+)
+suite = context.add_expectation_suite(
+    expectation_suite_name="example_suite", expectations=[ec]
+)
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_validate_multiple_batches_within_single_checkpoint.py add_batch_request_list">
 batch_request = asset.build_batch_request()
 batch_list = asset.get_batch_list_from_batch_request(batch_request)
-batch_request_list = [batch.batch_request for batch in batch_list] 
+batch_request_list = [batch.batch_request for batch in batch_list]
 # </snippet>
 
 # <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_validate_multiple_batches_within_single_checkpoint.py add_checkpoint">
-validations = [{"batch_request": batch.batch_request, "expectation_suite_name": "example_suite"} for batch in batch_list]
+validations = [
+    {"batch_request": batch.batch_request, "expectation_suite_name": "example_suite"}
+    for batch in batch_list
+]
 
-checkpoint = gx.checkpoint.SimpleCheckpoint(name = 'example_checkpoint', data_context=context, 
-                                           validations = validations
-                                           )
+checkpoint = gx.checkpoint.SimpleCheckpoint(
+    name="example_checkpoint", data_context=context, validations=validations
+)
 
 checkpoint_result = checkpoint.run()
 
