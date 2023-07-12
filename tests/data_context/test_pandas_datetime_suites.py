a6eaedb7b245fc74c6f38fddd59f48fb4d73494d
Gabriel Gore <gabriel59kg@gmail.com>
2023-07-12 18:17:05 -0400

    import pytest

--- tests/data_context/test_pandas_datetime_suites.py
+++ tests/data_context/test_pandas_datetime_suites.py
@@ -2,6 +2,7 @@ import datetime
 from tempfile import TemporaryDirectory
 
 import pandas as pd
+import pytest
 
 from great_expectations.core.expectation_suite import ExpectationSuite
 from great_expectations.dataset import PandasDataset
