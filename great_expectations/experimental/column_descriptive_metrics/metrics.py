60ec4ab8b1ef96926af66f96f76a5186309f1591
Anthony Burdi <anthony@greatexpectations.io>
2023-07-13 17:51:53 -0400

    Draft metrics models

---
+++ great_expectations/experimental/column_descriptive_metrics/metrics.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import uuid
+from typing import Any, Sequence
+
+import pydantic
+from pydantic import BaseModel, Field
+
+
+class CDMRBaseModel(BaseModel):  # TODO: Better name
+    """Base class for all ColumnDescriptiveMetricRepository related models."""
+
+    class Config:
+        extra = pydantic.Extra.forbid
+
+
+class RunId(CDMRBaseModel):
+    id: uuid.UUID = Field(description="Run id")
+    organization_id: uuid.UUID = Field(
+        description="Organization id"
+    )  # TODO: Is this filled in by the backend?
+    # created_at, created_by filled in by the backend.
+
+
+class Value(CDMRBaseModel):
+    value: Any  # TODO: Better than Any
+
+
+class BatchPointer(CDMRBaseModel):  # TODO: Better name
+    datasource_name: str = Field(description="Datasource name")
+    data_asset_name: str = Field(description="Data asset name")
+    batch_name: str = Field(description="Batch name")
+
+
+class Metric(CDMRBaseModel):
+    id: uuid.UUID = Field(description="Metric id")
+    organization_id: uuid.UUID = Field(
+        description="Organization id"
+    )  # TODO: Is this filled in by the backend?
+    run_id: uuid.UUID = Field(description="Run id")
+    batch_pointer: BatchPointer = Field(description="Batch pointer")
+    metric_name: str = Field(description="Metric name")
+    metric_domain_kwargs: dict = Field(description="Metric domain kwargs")
+    metric_value_kwargs: dict = Field(description="Metric value kwargs")
+    column: str = Field(description="Column name")
+    value: Value = Field(description="Metric value")
+    details: dict = Field(description="Metric details")
+
+
+class Metrics(CDMRBaseModel):
+    """Collection of Metric objects."""
+
+    metrics: Sequence[Metric]
