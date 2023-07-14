406dbd5607262f1a26188ae63eea21173d19b03b
Gabriel Gore <gabriel59kg@gmail.com>
2023-07-14 14:09:40 -0400

    test_markers

---
+++ tests/test_markers.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import logging
+import pathlib
+import pytest
+import tomli
+from typing import Final
+from pprint import pformat as pf
+
+
+from tasks import MARKER_MAPPINGS
+
+
+pytestmarks = [pytest.mark.project]
+
+LOGGER: Final = logging.getLogger(__name__)
+PYPROJECT_TOML: Final = pathlib.Path(__file__).parent.parent / "pyproject.toml"
+
+
+@pytest.fixture(scope="module")
+def pyproject_toml_dict() -> dict:
+    """Parse pyporject.toml and return as dict"""
+    return tomli.loads(PYPROJECT_TOML.read_text())
+
+
+@pytest.fixture(scope="module")
+def pytest_markers(pyproject_toml_dict: dict) -> list[str]:
+    """Return pytest markers"""
+    LOGGER.debug(
+        f"pytest config ->\n{pf(pyproject_toml_dict['tool']['pytest'], depth=2)}"
+    )
+    marker_details = pyproject_toml_dict["tool"]["pytest"]["ini_options"]["markers"]
+    LOGGER.debug(f"marker_details ->\n{pf(marker_details)}")
+    return [m.split(":")[0] for m in marker_details]
+
+
+def test_marker_mappings_are_registered(pytest_markers: list[str]):
+    """
+    Check that all pytest marker mappings are acutallly valid, and have been registered
+    with pytest.
+    """
+    print(f"pytest_markers:\n----------\n{pf(pytest_markers)}")
+
+    for marker in MARKER_MAPPINGS:
+        assert marker in pytest_markers
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-vv"])
