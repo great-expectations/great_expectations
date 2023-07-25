beba3aaa6e6d9d4aa29095c27e7ee96bf1c9822b
Anthony Burdi <anthony@greatexpectations.io>
2023-07-24 21:17:13 -0400

    [MAINTENANCE] Experimental column descriptive metrics repository (#8335)

--- great_expectations/agent/actions/__init__.py
+++ great_expectations/agent/actions/__init__.py
@@ -7,3 +7,6 @@ from great_expectations.agent.actions.data_assistants import (
     RunMissingnessDataAssistantAction,
     RunOnboardingDataAssistantAction,
 )
+from great_expectations.agent.actions.run_column_descriptive_metrics_action import (
+    ColumnDescriptiveMetricsAction,
+)
---
+++ great_expectations/experimental/metric_repository/metric_retriever.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import abc
+import uuid
+from typing import TYPE_CHECKING, Sequence
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import AbstractDataContext
+    from great_expectations.datasource.fluent import BatchRequest
+    from great_expectations.experimental.metric_repository.metrics import Metric
+
+
+class MetricRetriever(abc.ABC):
+    # TODO: Docstrings
+    def __init__(self, context: AbstractDataContext):
+        self._context = context
+
+    @abc.abstractmethod
+    def get_metrics(self, batch_request: BatchRequest) -> Sequence[Metric]:
+        raise NotImplementedError
+
+    def _generate_metric_id(self) -> uuid.UUID:
+        return uuid.uuid4()
