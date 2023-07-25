6a742fa7da6316d3c2397cdb0a9d64997f5f540b
Josh Stauffer <josh@greatexpectations.io>
2023-07-25 12:39:03 -0400

    implement action

--- tests/agent/actions/test_test_draft_datasource_config.py
+++ tests/agent/actions/test_test_draft_datasource_config.py
@@ -1,17 +1,23 @@
+from unittest.mock import MagicMock
 from uuid import UUID
 
-from great_expectations import get_context
-from great_expectations.agent.actions.test_draft_datasource_config import TestDraftDatasourceConfigAction
+import pytest
+
+from great_expectations.agent.actions.test_draft_datasource_config import (
+    TestDraftDatasourceConfigAction,
+)
 from great_expectations.agent.models import TestDatasourceConfig
+from great_expectations.data_context import CloudDataContext
 
 
-def test_test_draft_datasource_config_success():
-    context = get_context(cloud_mode=True)
+@pytest.fixture(scope="function")
+def context():
+    return MagicMock(autospec=CloudDataContext)
+
+
+def test_test_draft_datasource_config_success(context):
+
     action = TestDraftDatasourceConfigAction(context=context)
-    event = TestDatasourceConfig(
-        config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
-    )
+    event = TestDatasourceConfig(config_id=UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5"))
     id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
     action.run(event=event, id=str(id))
-    print()
-
