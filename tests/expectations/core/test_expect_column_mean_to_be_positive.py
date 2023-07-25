84288e42c88c16ebf41229b6d2ba0e99fd36650f
kenwade4 <95714847+kenwade4@users.noreply.github.com>
2023-07-25 15:50:41 -0500

    [DOCS] Update docs on expectation docstrings (#8405)

--- tests/expectations/core/test_expect_column_mean_to_be_positive.py
+++ tests/expectations/core/test_expect_column_mean_to_be_positive.py
@@ -9,7 +9,7 @@ from great_expectations.expectations.core.expect_column_mean_to_be_between impor
 
 # <snippet name="tests/expectations/core/test_expect_column_mean_to_be_positive.py ExpectColumnMeanToBePositive_class_def">
 class ExpectColumnMeanToBePositive(ExpectColumnMeanToBeBetween):
-    """Expects the mean of values in this column to be positive"""
+    """Expect the mean of values in this column to be positive."""
 
     default_kwarg_values = {
         "min_value": 0,
