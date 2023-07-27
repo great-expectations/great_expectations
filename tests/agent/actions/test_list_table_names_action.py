a79ca7f3ca96f0cc17f3d662209aee21ba703988
Gabriel Gore <gabriel59kg@gmail.com>
2023-07-27 15:42:44 -0400

    use `responses`

--- tests/agent/actions/test_list_table_names_action.py
+++ tests/agent/actions/test_list_table_names_action.py
@@ -1,8 +1,10 @@
+import re
 import uuid
 from unittest import mock
 from unittest.mock import MagicMock
 
 import pytest
+import responses
 from sqlalchemy.engine import Inspector
 
 from great_expectations.agent.actions import (
@@ -19,9 +21,22 @@ from great_expectations.exceptions import StoreBackendError
 pytestmark = pytest.mark.unit
 
 
+@pytest.fixture
+def dummy_base_url() -> str:
+    return "https://test-base-url"
+
+
+@pytest.fixture
+def dummy_org_id() -> str:
+    return "94af8c91-6e56-4f2a-9b1f-04868321c5f5"
+
+
 @pytest.fixture(scope="function")
-def context():
-    return MagicMock(autospec=CloudDataContext)
+def context(dummy_base_url, dummy_org_id):
+    mock_context = MagicMock(autospec=CloudDataContext)
+    mock_context._cloud_config.base_url = dummy_base_url
+    mock_context._cloud_config.organization_id = dummy_org_id
+    return mock_context
 
 
 @pytest.fixture
@@ -46,24 +61,27 @@ def test_list_table_names_event_raises_for_non_sql_datasource(context, event):
         action.run(event=event, id=id)
 
 
-def test_run_list_table_names_action_returns_action_result(context, event):
+@responses.activate
+def test_run_list_table_names_action_returns_action_result(
+    context, event, dummy_base_url, dummy_org_id
+):
     action = ListTableNamesAction(context=context)
     id = "096ce840-7aa8-45d1-9e64-2833948f4ae8"
 
     with mock.patch(
         "great_expectations.agent.actions.list_table_names.inspect"
-    ) as mock_inspect, mock.patch(
-        "requests.Session.patch", autospec=True
-    ) as mock_patch:
+    ) as mock_inspect:
         datasource = MagicMock(spec=SQLDatasource)
         datasource_id = str(uuid.uuid4())
         datasource.id = datasource_id
         context.get_datasource.return_value = datasource
 
-        mock_resp = MagicMock()
-        mock_resp.json.return_value = ""
-        mock_resp.status_code = 204
-        mock_patch.return_value = mock_resp
+        responses.patch(
+            re.compile(
+                rf"{dummy_base_url}/organizations/{dummy_org_id}/datasources/.*"
+            ),
+            status=204,
+        )
 
         table_names = ["table_1", "table_2", "table_3"]
         inspector = MagicMock(spec=Inspector)
