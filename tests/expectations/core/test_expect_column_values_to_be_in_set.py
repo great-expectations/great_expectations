14a4dd7a82ef66c36cf11317e00265b0d4787035
Gabriel Gore <gabriel59kg@gmail.com>
2023-07-31 08:54:42 -0400

    keep `result`

--- tests/expectations/core/test_expect_column_values_to_be_in_set.py
+++ tests/expectations/core/test_expect_column_values_to_be_in_set.py
@@ -198,4 +198,4 @@ def test_expect_column_values_to_be_in_set_no_set(
     with pytest.raises(
         great_expectations.exceptions.exceptions.InvalidExpectationConfigurationError
     ):
-        validator.expect_column_values_to_be_in_set(column="a")
+        result = validator.expect_column_values_to_be_in_set(column="a")  # noqa: F841
