c173db53e27647bde9b73b79e54b67a48625f5d2
Bill Dirks <bill@greatexpectations.io>
2023-07-31 10:38:25 -0700

    Start testing clickhouse in github actions.

--- tasks.py
+++ tasks.py
@@ -797,6 +797,7 @@ class TestDependencies(NamedTuple):
 
 MARKER_DEPENDENDENCY_MAP: Final[Mapping[str, TestDependencies]] = {
     "athena": TestDependencies(("reqs/requirements-dev-athena.txt",)),
+    "clickhouse": TestDependencies("reqs/requirements-dev-clickhouse.txt"),
     "cloud": TestDependencies(
         ("reqs/requirements-dev-cloud.txt",), exta_pytest_args=("--cloud",)
     ),
@@ -835,7 +836,8 @@ def _tokenize_marker_string(marker_string: str) -> Generator[str, None, None]:
         yield tokens[0]
     elif marker_string == "cloud and not e2e":
         yield "cloud"
-    elif marker_string == "openpyxl or pyarrow or project or sqlite":
+    elif marker_string == "clickhouse or openpyxl or pyarrow or project or sqlite":
+        yield "clickhouse"
         yield "openpyxl"
         yield "pyarrow"
         yield "project"
