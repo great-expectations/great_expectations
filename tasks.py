ea9bc61e0fc91b879c2991bdec6500167942f3bf
Bill Dirks <bill@greatexpectations.io>
2023-07-31 22:07:20 -0700

    Re-add in the test that gates releases. Will remove in follow-up.

--- tasks.py
+++ tasks.py
@@ -857,6 +857,12 @@ def _tokenize_marker_string(marker_string: str) -> Generator[str, None, None]:
         yield "pyarrow"
         yield "project"
         yield "sqlite"
+    # Remove this elif when this marker test is removed from ci.yml
+    elif marker_string == "openpyxl or pyarrow or project or sqlite":
+        yield "openpyxl"
+        yield "pyarrow"
+        yield "project"
+        yield "sqlite"
     else:
         raise ValueError(f"Unable to tokenize marker string: {marker_string}")
 
