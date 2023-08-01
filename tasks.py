d14e51448561ba030587936624e6ce8bcb841c32
Gabriel <gabriel59kg@gmail.com>
2023-08-01 11:36:47 -0400

    [MAINTENANCE] Github Actions - Spark tests (#8460)

--- tasks.py
+++ tasks.py
@@ -826,7 +826,8 @@ MARKER_DEPENDENDENCY_MAP: Final[Mapping[str, TestDependencies]] = {
         exta_pytest_args=("--postgresql",),
     ),
     "spark": TestDependencies(
-        ("reqs/requirements-dev-spark.txt",),
+        requirement_files=("reqs/requirements-dev-spark.txt",),
+        services=("spark",),
         exta_pytest_args=("--spark",),
     ),
     "trino": TestDependencies(
@@ -949,7 +950,8 @@ def ci_tests(  # noqa: PLR0913
     reports: bool = False,
     slowest: int = 5,
     timeout: float = 0.0,  # 0 indicates no timeout
-    xdist: bool = True,
+    xdist: bool = False,
+    pty: bool = True,
 ):
     """
     Run tests in CI.
@@ -971,8 +973,8 @@ def ci_tests(  # noqa: PLR0913
         "-rEf",
     ]
 
-    # if xdist:
-    #     pytest_cmds.append("-n auto")
+    if xdist:
+        pytest_cmds.append("-n auto")
 
     if timeout != 0:
         pytest_cmds.append(f"--timeout={timeout}")
@@ -985,7 +987,7 @@ def ci_tests(  # noqa: PLR0913
 
     for test_deps in _get_marker_dependencies(marker):
         if up_services:
-            service(ctx, names=test_deps.services, markers=test_deps.services)
+            service(ctx, names=test_deps.services, markers=test_deps.services, pty=pty)
 
         for extra_pytest_arg in test_deps.exta_pytest_args:
             pytest_cmds.append(extra_pytest_arg)
@@ -993,13 +995,15 @@ def ci_tests(  # noqa: PLR0913
     if marker in ["postgresql", "mssql", "mysql", "trino"]:
         pytest_cmds[3] = "all_backends"
 
-    ctx.run(" ".join(pytest_cmds), echo=True, pty=True)
+    ctx.run(" ".join(pytest_cmds), echo=True, pty=pty)
 
 
 @invoke.task(
     iterable=["names", "markers"],
 )
-def service(ctx: Context, names: Sequence[str], markers: Sequence[str]):
+def service(
+    ctx: Context, names: Sequence[str], markers: Sequence[str], pty: bool = True
+):
     """
     Startup a service, by referencing its name directly or by looking up a pytest marker.
 
@@ -1027,7 +1031,7 @@ def service(ctx: Context, names: Sequence[str], markers: Sequence[str]):
                 "-d",
                 "--quiet-pull",
             ]
-            ctx.run(" ".join(cmds), echo=True, pty=True)
+            ctx.run(" ".join(cmds), echo=True, pty=pty)
         # TODO: remove this sleep. This is a temporary hack to give services enough
         #       time to come up to get ci merging again.
         ctx.run("sleep 15")
