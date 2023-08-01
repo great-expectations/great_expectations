518713fc4779b7f500907c20297996c1d4e827dd
Gabriel Gore <gabriel59kg@gmail.com>
2023-08-01 11:08:26 -0400

    add `test_run_checkpoint`

--- test_indentifiers.py
+++ test_indentifiers.py
@@ -4,6 +4,9 @@ import pytest
 from pytest import param as p
 from great_expectations.datasource.fluent import SQLDatasource
 from great_expectations import get_context
+from great_expectations.expectations.expectation import (
+    ExpectationConfiguration,
+)
 from great_expectations.data_context import EphemeralDataContext
 
 
@@ -63,6 +66,46 @@ class TestIndentifiers:
         asset = datasources.add_table_asset(**asset_config)
         print(asset)
 
+    def test_run_checkpoint(
+        self,
+        context: EphemeralDataContext,
+        datasources: SQLDatasource,
+        asset_config: dict,
+    ):
+        asset = datasources.add_table_asset(**asset_config)
+        suite = context.add_expectation_suite(
+            expectation_suite_name=f"{datasources.name}-{asset.name}"
+        )
+        suite.add_expectation(
+            expectation_configuration=ExpectationConfiguration(
+                expectation_type="expect_column_values_to_not_be_null",
+                kwargs={
+                    "column": "val",
+                    "mostly": 1,
+                },
+            )
+        )
+
+        checkpoint_name = f"{datasources.name}-{asset_config['name']}"
+        print(f"WTF: {asset.name}")
+
+        checkpoint_config = {
+            "name": checkpoint_name,
+            "validations": [
+                {
+                    "expectation_suite_name": suite.expectation_suite_name,
+                    "expectation_suite_ge_cloud_id": suite.ge_cloud_id,
+                    "batch_request": {
+                        "datasource_name": datasources.name,
+                        "data_asset_name": asset.name,
+                    },
+                }
+            ],
+        }
+        checkpoint = context.add_checkpoint(**checkpoint_config)
+        result = checkpoint.run()
+        assert result.success is True
+
 
 if __name__ == "__main__":
-    pytest.main([__file__, "-vv"])
+    pytest.main([__file__, "-vv", "-rxpEf"])
