7c3d698cb62d5c719d1e594abdf769eb0a4cd60b
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-08-09 09:28:03 -0400

    Update how_to_connect_to_in_memory_data_using_spark.py

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
@@ -7,6 +7,7 @@ pytest -v --docs-tests -k "how_to_connect_to_in_memory_data_using_pandas" tests/
 
 import pathlib
 import great_expectations as gx
+import pyspark.pandas as ps
 
 context = gx.get_context()
 
@@ -20,13 +21,11 @@ datasource = context.sources.add_spark("my_spark_datasource")
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py dataframe">
-data = [
-    {"a": 1, "b": 2, "c": 3},
-    {"a": 4, "b": 5, "c": 6},
-    {"a": 7, "b": 8, "c": 9},
-]
-
-dataframe = spark.createDataFrame(data)
+df = ps.DataFrame(
+    {'a': [1, 2, 3, 4, 5, 6],
+     'b': [100, 200, 300, 400, 500, 600],
+     'c': ["one", "two", "three", "four", "five", "six"]},
+    index=[10, 20, 30, 40, 50, 60])
 # </snippet>
 
 # Python
@@ -36,7 +35,7 @@ name = "example_dataframe"
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py data_asset">
-data_asset = datasource.add_dataframe_asset(name=name)
+data_asset = datasource.add_dataframe_asset(name="my_df_asset")
 # </snippet>
 
 # Python
