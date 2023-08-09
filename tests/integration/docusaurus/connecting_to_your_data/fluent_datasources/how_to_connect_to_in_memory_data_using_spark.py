89a2b69b1d3c0d05243d0f12d3a059a83459869c
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-08-09 10:19:28 -0400

    Apply suggestions from code review
    Co-authored-by: Chetan Kini <chetan@superconductive.com>

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
@@ -7,7 +7,11 @@ pytest -v --docs-tests -k "how_to_connect_to_in_memory_data_using_pandas" tests/
 
 import pathlib
 import great_expectations as gx
+import os
 import pyspark.pandas as ps
+
+# Required by pyarrow>=2.0.0 within Spark to suppress UserWarning
+os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
 
 context = gx.get_context()
 
