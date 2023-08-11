dfef3feb396e155ab6033342b6c25b10ebb4eb03
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-08-11 07:37:07 -0400

    Removed unused topics and updated links.

--- docs/docusaurus/docs/components/_prerequisites.jsx
+++ docs/docusaurus/docs/components/_prerequisites.jsx
@@ -56,7 +56,7 @@ export default class Prerequisites extends React.Component {
       returnItems.push(<li>An installation of Python {GxData.min_python} to {GxData.max_python}. To download and install Python, see <a href='https://www.python.org/downloads/'>Python downloads.</a></li>)
     }
     if (this.props.requireInstallation === true) {
-      returnItems.push(<li>A Great Expectations instance. See <a href='/docs/guides/setup/installation/local'>Install Great Expectations locally</a>.</li>)
+      returnItems.push(<li>A Great Expectations instance. See <a href='/docs/guides/setup/installation/install_gx'>Install Great Expectations with source data system dependencies</a>.</li>)
     }
     if (this.props.requireDataContext === true) {
       returnItems.push(<li><a href='/docs/guides/setup/configuring_data_contexts/instantiating_data_contexts/how_to_quickly_instantiate_a_data_context'>A Data Context.</a></li>)
