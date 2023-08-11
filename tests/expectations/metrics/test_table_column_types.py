64e07a2c99dc14787b0cc29af2045c4c568c5387
Thu Pham <thu.pham@greatexpectations.io>
2023-08-11 14:32:23 -0700

    Update test cases

--- tests/expectations/metrics/test_table_column_types.py
+++ tests/expectations/metrics/test_table_column_types.py
@@ -1,6 +1,7 @@
 import pytest
 
 from great_expectations.data_context.util import file_relative_path
+from great_expectations.exceptions.exceptions import MetricResolutionError
 from great_expectations.execution_engine import SqlAlchemyExecutionEngine
 from great_expectations.execution_engine.sqlalchemy_batch_data import (
     SqlAlchemyBatchData,
@@ -97,11 +98,15 @@ def test_table_column_type__sqlalchemy_happy_path(sa, capfd):
 
     # Add another batch
     batch_data = SqlAlchemyBatchData(
-        execution_engine=engine, table_name="table_partitioned_by_date_column__B"
+        execution_engine=engine,
+        table_name="table_that_should_be_partitioned_by_random_hash__H",
     )
     engine.load_batch_data("__1", batch_data)
     assert isinstance(batch_data.selectable, sa.Table)
-    assert batch_data.selectable.name == "table_partitioned_by_date_column__B"
+    assert (
+        batch_data.selectable.name
+        == "table_that_should_be_partitioned_by_random_hash__H"
+    )
     assert batch_data.selectable.schema is None
 
     table_columns_metric, results = get_table_columns_metric(execution_engine=engine)
@@ -113,10 +118,34 @@ def test_table_column_type__sqlalchemy_happy_path(sa, capfd):
     assert results[("table.columns", (), ())] == [
         "index",
         "id",
-        "date",
         "event_type",
         "favorite_color",
     ]
 
 
-def test_table_column_types__sqlalchemy_
+@pytest.mark.sqlite
+def test_table_column_types__sqlalchemy_table_not_found(sa, capfd):
+    db_file = file_relative_path(
+        __file__,
+        "../../test_sets/test_cases_for_sql_data_connector.db",
+    )
+    eng = sa.create_engine(f"sqlite:///{db_file}", echo=True)
+    engine = SqlAlchemyExecutionEngine(engine=eng)
+
+    # Table `table_partitioned_by_date_column__B` does not exist in database
+    batch_data = SqlAlchemyBatchData(
+        execution_engine=engine, table_name="table_partitioned_by_date_column__B"
+    )
+    engine.load_batch_data("__1", batch_data)
+    assert isinstance(batch_data.selectable, sa.Table)
+    assert batch_data.selectable.name == "table_partitioned_by_date_column__B"
+    assert batch_data.selectable.schema is None
+
+    with pytest.raises(MetricResolutionError) as exc:
+        table_columns_metric, results = get_table_columns_metric(
+            execution_engine=engine
+        )
+    assert (
+        "(sqlite3.OperationalError) no such table: table_partitioned_by_date_column__B"
+        in str(exc.value)
+    )
