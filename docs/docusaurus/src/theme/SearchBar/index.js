03c026355a1e60443dcd11299e1428ef86ccd163
William Shin <will@greatexpectations.io>
2023-08-14 17:41:13 -0700

    change location of the searchPage

--- docs/docusaurus/src/theme/SearchBar/index.js
+++ docs/docusaurus/src/theme/SearchBar/index.js
@@ -6,7 +6,7 @@ import { useBaseUrlUtils } from '@docusaurus/useBaseUrl'
 import Link from '@docusaurus/Link'
 import Head from '@docusaurus/Head'
 import { isRegexpStringMatch } from '@docusaurus/theme-common'
-import { useSearchPage } from '@docusaurus/theme-common/internal'
+import { useSearchPage } from '@docusaurus/theme-common/hooks'
 import { DocSearchButton, useDocSearchKeyboardEvents } from '@docsearch/react'
 import { useAlgoliaContextualFacetFilters } from '@docusaurus/theme-search-algolia/client'
 import Translate from '@docusaurus/Translate'
@@ -23,6 +23,7 @@ function Hit ({ hit, children }) {
   return <Link to={hit.url}>{children}</Link>
 }
 function ResultsFooter ({ state, onClose }) {
+  // how do you generate a searchPagelink?
   const { generateSearchPageLink } = useSearchPage()
   return (
     <Link to={generateSearchPageLink(state.query)} onClick={onClose}>
