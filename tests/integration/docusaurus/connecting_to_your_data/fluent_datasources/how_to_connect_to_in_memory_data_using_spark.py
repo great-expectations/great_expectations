388897c92873413d13096a98aab129fa5675b035
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-08-15 17:59:25 -0400

    [DOCS] Update Spark Code Examples in Connect to In-memory Source Data (#8520)
    Co-authored-by: Chetan Kini <chetan@superconductive.com>
    Co-authored-by: William Shin <will@greatexpectations.io>

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
@@ -1,12 +1,20 @@
 """
 To run this code as a local test, use the following console command:
 ```
-pytest -v --docs-tests -k "how_to_connect_to_in_memory_data_using_pandas" tests/integration/test_script_runner.py
+pytest -vv --docs-tests -k "how_to_connect_to_in_memory_data_using_spark" tests/integration/test_script_runner.py
 ```
 """
-
-import pathlib
+import os
+import pandas as pd
 import great_expectations as gx
+from great_expectations.compatibility.not_imported import is_version_greater_or_equal
+
+# Required by pyarrow>=2.0.0 within Spark to suppress UserWarning
+os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
+
+# Required to make following test compatible with both Pandas > 2.0.0 and Pandas < 2.0.0
+if is_version_greater_or_equal(pd.__version__, "2.0.0"):
+    pd.DataFrame.iteritems = pd.DataFrame.items
 
 context = gx.get_context()
 
@@ -20,18 +28,21 @@ datasource = context.sources.add_spark("my_spark_datasource")
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py dataframe">
-data = [
-    {"a": 1, "b": 2, "c": 3},
-    {"a": 4, "b": 5, "c": 6},
-    {"a": 7, "b": 8, "c": 9},
-]
+df = pd.DataFrame(
+    {
+        "a": [1, 2, 3, 4, 5, 6],
+        "b": [100, 200, 300, 400, 500, 600],
+        "c": ["one", "two", "three", "four", "five", "six"],
+    },
+    index=[10, 20, 30, 40, 50, 60],
+)
 
-dataframe = spark.createDataFrame(data)
+dataframe = spark.createDataFrame(data=df)
 # </snippet>
 
 # Python
 # <snippet name="tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py name">
-name = "example_dataframe"
+name = "my_df_asset"
 # </snippet>
 
 # Python
@@ -45,7 +56,7 @@ my_batch_request = data_asset.build_batch_request(dataframe=dataframe)
 # </snippet>
 
 assert my_batch_request.datasource_name == "my_spark_datasource"
-assert my_batch_request.data_asset_name == "example_dataframe"
+assert my_batch_request.data_asset_name == "my_df_asset"
 assert my_batch_request.options == {}
 
 batches = data_asset.get_batch_list_from_batch_request(my_batch_request)
