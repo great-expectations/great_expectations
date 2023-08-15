608f49ce940d9c6fef5ea4c69beefcb44392d9e2
James Campbell <james@greatexpectations.io>
2023-08-15 10:14:42 -0400

    Allow duplicate BatchRequest information with the same values.

--- great_expectations/checkpoint/util.py
+++ great_expectations/checkpoint/util.py
@@ -251,7 +251,11 @@ def get_substituted_batch_request(
 
     for key, value in validation_batch_request.items():
         substituted_value = substituted_runtime_batch_request.get(key)
-        if value is not None and substituted_value is not None:
+        if (
+            value is not None
+            and substituted_value is not None
+            and value != substituted_value
+        ):
             raise gx_exceptions.CheckpointError(
                 f'BatchRequest attribute "{key}" was specified in both validation and top-level CheckpointConfig.'
             )
