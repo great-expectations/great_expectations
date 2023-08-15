f80f917ea119b19877194dcb99747bb09eeea43b
William Shin <will@greatexpectations.io>
2023-08-15 14:08:07 -0700

    kick

--- tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
+++ tests/integration/docusaurus/connecting_to_your_data/fluent_datasources/how_to_connect_to_in_memory_data_using_spark.py
@@ -1,7 +1,7 @@
 """
 To run this code as a local test, use the following console command:
 ```
-pytest -v --docs-tests -k "how_to_connect_to_in_memory_data_using_spark" tests/integration/test_script_runner.py
+pytest -vv --docs-tests -k "how_to_connect_to_in_memory_data_using_spark" tests/integration/test_script_runner.py
 ```
 """
 import os
