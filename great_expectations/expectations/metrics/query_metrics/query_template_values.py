4454bc91800e97657cdd9d6cfe487c3724f9610f
William Shin <will@greatexpectations.io>
2023-08-16 22:19:56 -0700

    better format

--- great_expectations/expectations/metrics/query_metrics/query_template_values.py
+++ great_expectations/expectations/metrics/query_metrics/query_template_values.py
@@ -28,7 +28,7 @@ class QueryTemplateValues(QueryMetricProvider):
     @classmethod
     def get_query(cls, query, template_dict, selectable):
         template_dict_reformatted = {
-            k: str(v).format(active_batch=selectable)
+            k: sa.text(v).format(active_batch=selectable)
             if isinstance(v, int)
             else v.format(active_batch=selectable)
             for k, v in template_dict.items()
