16939a9fcd1e80c15133011fcafa927269d688b9
Gabriel <gabriel59kg@gmail.com>
2023-08-21 09:56:02 -0400

    fix bad find and replace

--- great_expectations/compatibility/typing_extensions.py
+++ great_expectations/compatibility/typing_extensions.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable, TypeVar
 
 try:
-    from great_expectations.compatibility.typing_extensions import override
+    from typing_extensions import override
 except ImportError:
     F = TypeVar("F", bound=Callable[..., Any])
 
