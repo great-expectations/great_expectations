b122831ba8a9f5f04391ede424a0f6fcc449621f
Chetan Kini <chetan@superconductive.com>
2023-08-24 13:34:21 -0400

    revert mark

--- tests/data_context/abstract_data_context/test_datasource_delete.py
+++ tests/data_context/abstract_data_context/test_datasource_delete.py
@@ -38,6 +38,7 @@ def test_datasource_delete_removes_from_cache_and_config_data_context(
         )
 
 
+@pytest.mark.cloud
 def test_datasource_delete_removes_from_cache_and_config_cloud_data_context(
     empty_cloud_data_context: CloudDataContext,
     block_config_datasource_config: DatasourceConfig,
