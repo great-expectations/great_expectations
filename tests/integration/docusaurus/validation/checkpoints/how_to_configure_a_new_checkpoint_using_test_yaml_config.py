ae962f7565f2bc4d39845504acd4ddae5401dfda
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-08-30 10:54:25 -0400

    Update how_to_configure_a_new_checkpoint_using_test_yaml_config.py

--- tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py
+++ tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py
@@ -1,14 +1,12 @@
 import pathlib
 from pprint import pprint
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py setup">
 from ruamel.yaml import YAML
 
 import great_expectations as gx
 
 yaml = YAML()
 context = gx.get_context()
-# </snippet>
 
 data_directory = pathlib.Path(
     gx.__file__,
@@ -21,15 +19,11 @@ data_directory = pathlib.Path(
 
 context.add_expectation_suite(expectation_suite_name="taxi_data")
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py asset_names">
 pprint(context.get_available_data_asset_names())
-# </snippet>
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py suite_names">
+
 context.list_expectation_suite_names()
-# </snippet>
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py create_checkpoint">
 my_checkpoint_name = "my_checkpoint"
 
 yaml_config = f"""
@@ -43,14 +37,9 @@ validations:
       data_asset_name: yellow_tripdata
     expectation_suite_name: taxi_data
 """
-# </snippet>
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py test_checkpoint">
 my_checkpoint = context.test_yaml_config(yaml_config=yaml_config)
-# </snippet>
 
-# <snippet name="tests/integration/docusaurus/validation/checkpoints/how_to_configure_a_new_checkpoint_using_test_yaml_config.py save_checkpoint">
 context.add_checkpoint(checkpoint=my_checkpoint)
-# </snippet>
 
 assert context.get_checkpoint("my_checkpoint")
