404c63312b6950afb158136ea6d179993dcd1817
Anthony Burdi <anthony@greatexpectations.io>
2023-08-31 19:38:25 -0400

    Add test_add_metric_run_generic_metric_type_with_exception

--- tests/experimental/metric_repository/test_cloud_data_store.py
+++ tests/experimental/metric_repository/test_cloud_data_store.py
@@ -8,6 +8,7 @@ from great_expectations.experimental.metric_repository.cloud_data_store import (
 from great_expectations.experimental.metric_repository.metrics import (
     ColumnMetric,
     ColumnQuantileValuesMetric,
+    MetricException,
     MetricRun,
 )
 
@@ -110,5 +111,50 @@ class TestCloudDataStoreMetricRun:
             },
         )
 
-    def test_add_metric_run_generic_metric_type_with_exception(self):
-        raise NotImplementedError
+    def test_add_metric_run_generic_metric_type_with_exception(
+        self,
+        empty_cloud_context_fluent: CloudDataContext,  # noqa: F811  # used as a fixture
+    ):
+        cloud_data_store = CloudDataStore(context=empty_cloud_context_fluent)
+        data_asset_id = UUID("4469ed3b-61d4-421f-9635-8339d2558b0f")
+        metric_run = MetricRun(
+            data_asset_id=data_asset_id,
+            metrics=[
+                ColumnMetric[int](
+                    batch_id="batch_id",
+                    metric_name="metric_name",
+                    value=1,
+                    exception=MetricException(
+                        type="exception type", message="exception message"
+                    ),
+                    column="column",
+                )
+            ],
+        )
+        cloud_data_store._session = Mock()
+        cloud_data_store.add(metric_run)
+        cloud_data_store._session.post.assert_called_once_with(
+            url="https://app.greatexpectations.fake.io/organizations/12345678-1234-5678-1234-567812345678/metric-runs",
+            data={
+                "data": {
+                    "type": "metric-run",
+                    "attributes": {
+                        "data_asset_id": data_asset_id,
+                        "metrics": [
+                            {
+                                "metric_type": "ColumnMetric",
+                                "value_type": "int",
+                                "batch_id": "batch_id",
+                                "column": "column",
+                                "exception": {
+                                    "message": "exception message",
+                                    "type": "exception type",
+                                },
+                                "metric_name": "metric_name",
+                                "value": 1,
+                            }
+                        ],
+                    },
+                }
+            },
+        )
