cf31b9ecb42ef677425a2dd325744d4f65b8312b
Anthony Burdi <anthony@greatexpectations.io>
2023-08-31 19:44:08 -0400

    Remove separate serializer

--- great_expectations/experimental/metric_repository/metric_run_serializer.py
+++
@@ -1,7 +0,0 @@
-from great_expectations.experimental.metric_repository.metrics import MetricRun
-from great_expectations.experimental.metric_repository.serializer import Serializer
-
-
-class MetricRunSerializer(Serializer[MetricRun]):
-    def serialize(self, value: MetricRun) -> str:
-        return value.json()
