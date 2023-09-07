e50d0b45925a497b9dab24b11bd2f1626d6b88a9
William Shin <will@greatexpectations.io>
2023-09-06 20:58:17 -0700

    pushing for changes

--- great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
+++ great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from itertools import chain
 from typing import TYPE_CHECKING, List, Sequence
 
 from great_expectations.compatibility.typing_extensions import override
@@ -8,6 +9,7 @@ from great_expectations.experimental.metric_repository.metric_retriever import (
     MetricRetriever,
 )
 from great_expectations.experimental.metric_repository.metrics import (
+    ColumnMetric,
     Metric,
     TableMetric,
 )
@@ -24,8 +26,11 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
     @override
     def get_metrics(self, batch_request: BatchRequest) -> Sequence[Metric]:
         table_metrics_list = self._get_table_metrics(batch_request)
-
-        return table_metrics_list
+        column_list: List[str] = table_metrics_list[1].value
+        column_metrics_list = self._get_column_metrics(
+            batch_request=batch_request, column_list=column_list
+        )
+        return list(chain(table_metrics_list, column_metrics_list))
 
     def _get_table_metrics(self, batch_request: BatchRequest) -> Sequence[Metric]:
         table_metric_names = ["table.row_count", "table.columns", "table.column_types"]
@@ -77,9 +82,16 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
                 exception=None,  # TODO: Pass through a MetricException() if an exception is thrown
             )
         )
-
         metric_name = "table.column_types"
-        metric_lookup_key = (metric_name, tuple(), tuple())
+        # TODO: fix this jankiness
+        # (metric_name, tuple(), tuple())
+        # or
+        # (metric_name, tuple(), 'include_nested=True')
+        if (metric_name, tuple(), "include_nested=True") in computed_metrics.keys():
+            metric_lookup_key = (metric_name, tuple(), "include_nested=True")
+        else:
+            metric_lookup_key = (metric_name, tuple(), tuple())
+
         metrics.append(
             TableMetric[List[str]](
                 batch_id=validator.active_batch.id,
@@ -90,5 +102,51 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
         )
         return metrics
 
-    def _get_column_metrics(self, batch_request: BatchRequest) -> Sequence[Metric]:
-        pass
+    def _get_column_metrics(
+        self, batch_request: BatchRequest, column_list: List[str]
+    ) -> Sequence[Metric]:
+        column_metric_names = ["column.min", "column.max"]
+
+        column_metric_configs: List[MetricConfiguration] = list()
+
+        # TODO: nested for-loop can be better
+        for metric_name in column_metric_names:
+            for column in column_list:
+                column_metric_configs.append(
+                    MetricConfiguration(
+                        metric_name=metric_name,
+                        metric_domain_kwargs={"column": column},
+                        metric_value_kwargs={},
+                    )
+                )
+        validator = self._context.get_validator(batch_request=batch_request)
+        computed_metrics = validator.compute_metrics(
+            metric_configurations=column_metric_configs,
+            runtime_configuration={"catch_exceptions": True},  # TODO: Is this needed?
+        )
+        # Convert computed_metrics
+        metrics: list[Metric] = []
+        ColumnMetric.update_forward_refs()
+
+        assert isinstance(validator.active_batch, Batch)
+        if not isinstance(validator.active_batch, Batch):
+            raise TypeError(
+                f"validator.active_batch is type {type(validator.active_batch).__name__} instead of type {Batch.__name__}"
+            )
+        metric_lookup_key: _MetricKey
+
+        # TODO: nested for-loop can be better
+        for metric_name in column_metric_names:
+            for column in column_list:
+                metric_lookup_key = (metric_name, f"column={column}", tuple())
+                metrics.append(
+                    ColumnMetric[float](
+                        batch_id=validator.active_batch.id,
+                        metric_name=metric_name,
+                        column=column,
+                        value=computed_metrics[metric_lookup_key],  # type: ignore[arg-type] # Pydantic verifies the value type
+                        exception=None,  # TODO: Pass through a MetricException() if an exception is thrown
+                    )
+                )
+
+        return metrics
