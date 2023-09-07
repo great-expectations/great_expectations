5670e6aa3b1c799d21adf8e811d3253069f7f841
Anthony Burdi <anthony@greatexpectations.io>
2023-09-07 13:35:14 -0400

    [MAINTENANCE] Column Descriptive Metrics: Additional metrics mean median (#8680)
    Co-authored-by: William Shin <will@greatexpectations.io>

--- great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
+++ great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
@@ -30,7 +30,8 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
         column_metrics_list = self._get_column_metrics(
             batch_request=batch_request, column_list=column_list
         )
-        return list(chain(table_metrics_list, column_metrics_list))
+        bundled_list = list(chain(table_metrics_list, column_metrics_list))
+        return bundled_list
 
     def _get_column_list(self, metrics: Sequence[Metric]) -> List[str]:
         column_list: List[str] = []
@@ -97,7 +98,12 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
     def _get_column_metrics(
         self, batch_request: BatchRequest, column_list: List[str]
     ) -> Sequence[Metric]:
-        column_metric_names = ["column.min", "column.max"]
+        column_metric_names = [
+            "column.min",
+            "column.max",
+            "column.mean",
+            "column.median",
+        ]
 
         column_metric_configs: List[MetricConfiguration] = list()
 
