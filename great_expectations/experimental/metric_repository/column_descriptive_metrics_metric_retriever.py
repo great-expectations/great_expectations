f127ad9a890c7aa6be364ac0d0b10e573f6ab7dc
Anthony Burdi <anthony@greatexpectations.io>
2023-09-08 14:29:37 -0400

    Add table.column_types

--- great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
+++ great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
@@ -41,10 +41,7 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
         return column_list
 
     def _get_table_metrics(self, batch_request: BatchRequest) -> Sequence[Metric]:
-        table_metric_names = [
-            "table.row_count",
-            "table.columns",
-        ]  # , "table.column_types"]
+        table_metric_names = ["table.row_count", "table.columns", "table.column_types"]
         table_metric_configs = [
             MetricConfiguration(
                 metric_name=metric_name, metric_domain_kwargs={}, metric_value_kwargs={}
@@ -90,6 +87,26 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
                 batch_id=validator.active_batch.id,
                 metric_name=metric_name,
                 value=computed_metrics[metric_lookup_key],
+                exception=None,  # TODO: Pass through a MetricException() if an exception is thrown
+            )
+        )
+
+        metric_name = "table.column_types"
+        if (metric_name, tuple(), "include_nested=True") in computed_metrics.keys():
+            metric_lookup_key = (metric_name, tuple(), "include_nested=True")
+        else:
+            metric_lookup_key = (metric_name, tuple(), tuple())
+
+        raw_column_types = computed_metrics[metric_lookup_key]
+        column_types_converted_to_str = [
+            {"name": raw_column_type["name"], "type": str(raw_column_type["type"])}
+            for raw_column_type in raw_column_types
+        ]
+        metrics.append(
+            TableMetric[List[str]](
+                batch_id=validator.active_batch.id,
+                metric_name=metric_name,
+                value=column_types_converted_to_str,
                 exception=None,  # TODO: Pass through a MetricException() if an exception is thrown
             )
         )
