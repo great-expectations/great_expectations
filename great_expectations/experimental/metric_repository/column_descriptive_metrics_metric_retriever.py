6d3c5a3d4fa6bc76eefc86135cfb3c2f1531b8b7
Anthony Burdi <anthony@greatexpectations.io>
2023-09-08 16:29:16 -0400

    Add type ignore

--- great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
+++ great_expectations/experimental/metric_repository/column_descriptive_metrics_metric_retriever.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from itertools import chain
-from typing import TYPE_CHECKING, List, Sequence
+from typing import TYPE_CHECKING, Any, List, Sequence
 
 from great_expectations.compatibility.typing_extensions import override
 from great_expectations.datasource.fluent.interfaces import Batch
@@ -94,8 +94,9 @@ class ColumnDescriptiveMetricsMetricRetriever(MetricRetriever):
         metric_name = "table.column_types"
         metric_lookup_key = (metric_name, tuple(), "include_nested=True")
 
-        raw_column_types = computed_metrics[metric_lookup_key]
-        column_types_converted_to_str = [
+        raw_column_types: list[dict[str, Any]] = computed_metrics[metric_lookup_key]  # type: ignore[assignment] # Metric results from computed_metrics are not typed
+
+        column_types_converted_to_str: list[dict[str, str]] = [
             {"name": raw_column_type["name"], "type": str(raw_column_type["type"])}
             for raw_column_type in raw_column_types
         ]
