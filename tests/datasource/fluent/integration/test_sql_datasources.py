7672f6ba11c0e8beb124258e2643c09ac9478a94
Gabriel Gore <gabriel59kg@gmail.com>
2023-09-12 16:04:09 -0400

    remove new params

--- tests/datasource/fluent/integration/test_sql_datasources.py
+++ tests/datasource/fluent/integration/test_sql_datasources.py
@@ -614,8 +614,6 @@ def _is_quote_char_dialect_mismatch(
 @pytest.mark.parametrize(
     "column_name",
     [
-        param("name", id="str name"),
-        param("NAME", id="str NAME"),
         param("lower", id="str lower"),
         param("LOWER", id="str LOWER"),
         param('"lower"', id='str "lower"'),
