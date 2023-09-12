d82bc7b17f77be35514db3f34895c199d22ee4b9
Thu Pham <thu.pham@greatexpectations.io>
2023-09-12 11:38:27 -0700

    Remove unused code and update tests

--- tests/expectations/metrics/test_table_column_types.py
+++ tests/expectations/metrics/test_table_column_types.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 
 from great_expectations.data_context.util import file_relative_path
@@ -42,49 +44,13 @@ def test_table_column_introspection(sa, capsys):
     columns = insp.get_columns(
         batch_data.selectable.name, schema=batch_data.selectable.schema
     )
-
-    assert not capsys.readouterr().out
-
-
-@pytest.mark.sqlite
-def test_table_column_type__sqlalchemy_happy_path(sa, capsys):
-    db_file = file_relative_path(
-        __file__,
-        "../../test_sets/test_cases_for_sql_data_connector.db",
-    )
-    eng = sa.create_engine(f"sqlite:///{db_file}", echo=True)
-    engine = SqlAlchemyExecutionEngine(engine=eng)
-    batch_data = SqlAlchemyBatchData(
-        execution_engine=engine, table_name="table_partitioned_by_date_column__A"
-    )
-    engine.load_batch_data("__", batch_data)
-    assert isinstance(batch_data.selectable, sa.Table)
-    assert batch_data.selectable.name == "table_partitioned_by_date_column__A"
-    assert batch_data.selectable.schema is None
-
-    table_columns_metric, results = get_table_columns_metric(execution_engine=engine)
-
-    assert results[("table.columns", (), ())] == [
+    assert [x["name"] for x in columns] == [
         "index",
         "id",
         "date",
         "event_type",
         "favorite_color",
     ]
-
-    assert capsys.readouterr().out
-
-    table_columns_metric, results = get_table_columns_metric(execution_engine=engine)
-
-    assert results[("table.columns", (), ())] == [
-        "index",
-        "id",
-        "date",
-        "event_type",
-        "favorite_color",
-    ]
-
-    # Should not have to re-inspect
     assert not capsys.readouterr().out
 
     # Add another batch
@@ -100,9 +66,10 @@ def test_table_column_type__sqlalchemy_happy_path(sa, capsys):
     )
     assert batch_data.selectable.schema is None
 
-    table_columns_metric, results = get_table_columns_metric(execution_engine=engine)
-
-    assert results[("table.columns", (), ())] == [
+    columns = insp.get_columns(
+        batch_data.selectable.name, schema=batch_data.selectable.schema
+    )
+    assert [x["name"] for x in columns] == [
         "index",
         "id",
         "event_type",
@@ -111,6 +78,84 @@ def test_table_column_type__sqlalchemy_happy_path(sa, capsys):
 
     # Should have to re-inspect due to new batch
     assert capsys.readouterr().out
+
+
+@pytest.mark.sqlite
+def test_table_column_type__sqlalchemy_happy_path(sa, capsys):
+    db_file = file_relative_path(
+        __file__,
+        "../../test_sets/test_cases_for_sql_data_connector.db",
+    )
+    eng = sa.create_engine(f"sqlite:///{db_file}", echo=True)
+    engine = SqlAlchemyExecutionEngine(engine=eng)
+    with mock.patch(
+        "great_expectations.execution_engine.sqlalchemy_execution_engine.sa.inspect",
+        wraps=sa.inspect,
+    ) as mock_inspect:
+        batch_data = SqlAlchemyBatchData(
+            execution_engine=engine, table_name="table_partitioned_by_date_column__A"
+        )
+        engine.load_batch_data("__", batch_data)
+        assert isinstance(batch_data.selectable, sa.Table)
+        assert batch_data.selectable.name == "table_partitioned_by_date_column__A"
+        assert batch_data.selectable.schema is None
+
+        table_columns_metric, results = get_table_columns_metric(
+            execution_engine=engine
+        )
+
+        assert results[("table.columns", (), ())] == [
+            "index",
+            "id",
+            "date",
+            "event_type",
+            "favorite_color",
+        ]
+
+        assert mock_inspect.call_count == 1
+
+        table_columns_metric, results = get_table_columns_metric(
+            execution_engine=engine
+        )
+
+        assert results[("table.columns", (), ())] == [
+            "index",
+            "id",
+            "date",
+            "event_type",
+            "favorite_color",
+        ]
+
+        # Should not have to re-inspect
+        assert mock_inspect.call_count == 1
+
+        # Add another batch
+        batch_data = SqlAlchemyBatchData(
+            execution_engine=engine,
+            table_name="table_that_should_be_partitioned_by_random_hash__H",
+        )
+        engine.load_batch_data("__1", batch_data)
+        assert isinstance(batch_data.selectable, sa.Table)
+        assert (
+            batch_data.selectable.name
+            == "table_that_should_be_partitioned_by_random_hash__H"
+        )
+        assert batch_data.selectable.schema is None
+
+        table_columns_metric, results = get_table_columns_metric(
+            execution_engine=engine
+        )
+
+        assert results[("table.columns", (), ())] == [
+            "index",
+            "id",
+            "event_type",
+            "favorite_color",
+        ]
+
+        # Should have to re-inspect due to new batch
+        # But should use same inspector
+        assert mock_inspect.call_count == 1
 
 
 @pytest.mark.sqlite
