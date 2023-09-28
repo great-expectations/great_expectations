030cdf7cbe45af577dc2ccc65acb92f0ca21da7e
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-09-28 14:46:13 -0400

    Minor edit.

--- docs/docusaurus/sidebars.js
+++ docs/docusaurus/sidebars.js
@@ -12,7 +12,7 @@ module.exports = {
           items: [
             {
               type: 'link',
-              label: 'Sign up for a GX Cloud Beta account',
+              label: 'Request a GX Cloud Beta account',
               href: '/docs/cloud/set_up_gx_cloud#srequest-a-gx-cloud-beta-account',
             },
             {
