e45e6db82c00c4fcafa4a79f0b0b5e845142e84f
Robert Moses Lim <robert.m.lim@gmail.com>
2023-09-29 04:45:52 -0700

    Revert to no-op since logic no longer used

--- great_expectations/data_context/data_context/cloud_data_context.py
+++ great_expectations/data_context/data_context/cloud_data_context.py
@@ -943,26 +943,13 @@ class CloudDataContext(SerializableDataContext):
     @override
     def _save_project_config(
         self, _fds_datasource: FluentDatasource | None = None
-    ) -> FluentDatasource | None:
+    ) -> None:
         """
         See parent 'AbstractDataContext._save_project_config()` for more information.
 
         Explicitly override base class implementation to retain legacy behavior.
         """
-        # 042723 kilo59
-        # Currently CloudDataContext and FileDataContext diverge in how FDS are persisted.
-        # FileDataContexts don't use the DatasourceStore at all to save or hydrate FDS configs.
-        # CloudDataContext does use DatasourceStore in order to make use of the Cloud http clients.
-        # The intended future state is for a new FluentDatasourceStore that can fully encapsulate
-        # the different requirements for FDS vs BDS.
-        # At which time `_save_project_config` will revert to being a no-op operation on the CloudDataContext.
-        if _fds_datasource:
-            return self._datasource_store.set(key=None, value=_fds_datasource)
-        else:
-            logger.debug(
-                "CloudDataContext._save_project_config() has no `fds_datasource` to update"
-            )
-            return None
+        return None
 
     @override
     def _view_validation_result(self, result: CheckpointResult) -> None:
