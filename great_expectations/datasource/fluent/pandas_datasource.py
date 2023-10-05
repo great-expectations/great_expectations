c6f9db6d1746f36f2a60935292140dae2bf71da3
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-05 15:42:06 -0400

    fix source file type issue

--- great_expectations/datasource/fluent/pandas_datasource.py
+++ great_expectations/datasource/fluent/pandas_datasource.py
@@ -377,8 +377,8 @@ class DataFrameAsset(_PandasDataAsset, Generic[_PandasDataFrameT]):
         version="0.16.15",
     )
     @override
-    def build_batch_request(
-        self, dataframe: Optional[pd.DataFrame] = None  # type: ignore[override]
+    def build_batch_request(  # type: ignore[override]
+        self, dataframe: Optional[pd.DataFrame] = None
     ) -> BatchRequest:
         """A batch request that can be used to obtain batches for this DataAsset.
 
