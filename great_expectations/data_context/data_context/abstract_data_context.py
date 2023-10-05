1eaefd6fb01eaa73be9637e6d55c0d816fac7fbb
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-05 11:46:23 -0400

    update `add_or_update_profiler` overloads

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -3353,9 +3353,10 @@ class AbstractDataContext(ConfigPeer, ABC):
     @overload
     def add_or_update_profiler(  # noqa: PLR0913
         self,
-        name: str,
-        config_version: float,
-        rules: dict[str, dict],
+        name: str = ...,
+        id: str | None = ...,
+        config_version: float = ...,
+        rules: dict[str, dict] = ...,
         variables: dict | None = ...,
         profiler: None = ...,
     ) -> RuleBasedProfiler:
@@ -3369,6 +3370,7 @@ class AbstractDataContext(ConfigPeer, ABC):
     def add_or_update_profiler(  # noqa: PLR0913
         self,
         name: None = ...,
+        id: None = ...,
         config_version: None = ...,
         rules: None = ...,
         variables: None = ...,
