f1864a8851caa2740d3a4e61142d4d5111fc494f
Nathan Farmer <nathan@greatexpectations.io>
2023-10-08 20:42:16 -0400

    Make type checker happy

--- tests/datasource/fluent/integration/test_sql_datasources.py
+++ tests/datasource/fluent/integration/test_sql_datasources.py
@@ -27,6 +27,7 @@ from great_expectations.compatibility.sqlalchemy import (
 )
 from great_expectations.compatibility.sqlalchemy import (
     TextClause,
+    engine,
     inspect,
     quoted_name,
 )
