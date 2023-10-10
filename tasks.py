2b188d6919f246c3dd8c0ac145771f7d94919f39
Nathan Farmer <nathan@greatexpectations.io>
2023-10-10 16:24:06 -0400

    Revert "Reduce --wait-timeout now that correct healthcheck is in place"
    This reverts commit f1aacb11731164b28aa0b606ee7a62d5b53beb9f.

--- tasks.py
+++ tasks.py
@@ -1133,7 +1133,7 @@ def service(
                 "-d",
                 "--quiet-pull",
                 "--wait",
-                "--wait-timeout 60",
+                "--wait-timeout 90",
             ]
             ctx.run(" ".join(cmds), echo=True, pty=pty)
     else:
