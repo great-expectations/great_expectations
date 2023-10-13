0d64b0c35886ef0b528251582c66835094702788
Nathan Farmer <nathan@greatexpectations.io>
2023-10-13 09:40:35 -0400

    Refactor/cleanup

--- tests/integration/cloud/rest_contracts/conftest.py
+++ tests/integration/cloud/rest_contracts/conftest.py
@@ -1,9 +1,16 @@
+from __future__ import annotations
+
 import os
 import pathlib
-from typing import Final
+from typing import TYPE_CHECKING, Final
 
 import pytest
 from pact import Consumer, Pact, Provider
+
+from great_expectations.core.http import create_session
+
+if TYPE_CHECKING:
+    import requests
 
 CONSUMER: Final[str] = "great_expectations"
 PROVIDER: Final[str] = "mercury"
@@ -12,7 +19,9 @@ PACT_BROKER_BASE_URL: Final[str] = "https://greatexpectations.pactflow.io"
 PACT_BROKER_TOKEN: Final[str] = os.environ.get("PACT_BROKER_READ_ONLY_TOKEN")
 PACT_MOCK_HOST: Final[str] = "localhost"
 PACT_MOCK_PORT: Final[int] = 9292
-PACT_DIR = str((pathlib.Path(__file__).parent / pathlib.Path("pacts")).resolve())
+PACT_DIR: Final[str] = str(pathlib.Path(__file__).parent.resolve())
+
+GX_CLOUD_ACCESS_TOKEN = os.environ.get("GX_CLOUD_ACCESS_TOKEN")
 
 
 @pytest.fixture
@@ -24,7 +33,6 @@ def pact(request) -> Pact:
         host_name=PACT_MOCK_HOST,
         port=PACT_MOCK_PORT,
         pact_dir=PACT_DIR,
-        publish_to_broker=False,
     )
     pact.start_service()
     yield pact
@@ -34,3 +42,16 @@ def pact(request) -> Pact:
 @pytest.fixture
 def pact_mock_mercury_url() -> str:
     return f"http://{PACT_MOCK_HOST}:{PACT_MOCK_PORT}"
+
+
+@pytest.fixture
+def session() -> requests.Session:
+    return create_session(access_token=GX_CLOUD_ACCESS_TOKEN)
+
+
+@pytest.fixture
+def headers(session: requests.Session) -> dict:
+    headers = dict(session.headers)
+    headers.pop("Authorization")
+    headers.pop("Gx-Version")
+    return headers
--- tests/integration/cloud/rest_contracts/test_data_context.py
+++ tests/integration/cloud/rest_contracts/test_data_context.py
@@ -1,21 +1,27 @@
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING
 
 import pytest
 from pact import Like, Pact
 
-from great_expectations.core.http import create_session
+if TYPE_CHECKING:
+    import requests
 
-ORGANIZATION_ID = os.environ.get("MERCURY_ORGANIZATION_ID")
-ACCESS_TOKEN = os.environ.get("MERCURY_ACCESS_TOKEN")
+ORGANIZATION_ID = os.environ.get("GX_CLOUD_ORGANIZATION_ID")
 
 DATASOURCE_ID = "15da041b-328e-44f7-892e-2bfd1a887ef8"
 
 
-@pytest.mark.mercury
-def test_get_data_context_given_exists(pact: Pact, pact_mock_mercury_url: str):
+@pytest.mark.cloud
+def test_get_data_context_given_exists(
+    session: requests.Session,
+    pact: Pact,
+    pact_mock_mercury_url: str,
+):
     path = f"/organizations/{ORGANIZATION_ID}/data-context-configuration"
     request_url = f"{pact_mock_mercury_url}{path}"
-    session = create_session(access_token=ACCESS_TOKEN)
 
     expected_data_context = {
         "anonymous_usage_statistics": {
@@ -38,9 +44,11 @@ def test_get_data_context_given_exists(pact: Pact, pact_mock_mercury_url: str):
         .with_request(
             method="GET",
             path=path,
-            headers=dict(session.headers),
         )
-        .will_respond_with(200, body=Like(expected_data_context))
+        .will_respond_with(
+            status=200,
+            body=Like(expected_data_context),
+        )
     )
 
     with pact:
