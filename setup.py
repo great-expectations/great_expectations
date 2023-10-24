ee836b47f68c1a2db4558729173151403481961e
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-24 11:27:13 -0400

    add `3.12` classifier

--- setup.py
+++ setup.py
@@ -127,6 +127,7 @@ config = {
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 }
 
