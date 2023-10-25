65201c01bb727a086b470f5c270c30fdb1a03eff
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-25 13:53:47 -0400

    log retries and change scope

--- tests/integration/usage_statistics/conftest.py
+++ tests/integration/usage_statistics/conftest.py
@@ -1,13 +1,30 @@
+from __future__ import annotations
+
+import logging
+from typing import Final
+
 import pytest
 import requests
-from requests.adapters import HTTPAdapter, Retry
+from requests.adapters import HTTPAdapter, Request, Response, Retry
+
+LOGGER: Final[logging.Logger] = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def requests_session_with_retries() -> requests.Session:
     # https://stackoverflow.com/a/35636367
     session = requests.Session()
-    # backoff factor above 1.0 means increasing sleep time between retries
-    retries = Retry(total=5, backoff_factor=1.25, status_forcelist=[502, 503, 504])
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
     session.mount("https://", HTTPAdapter(max_retries=retries))
+
+    previous_requests: list[Request] = []
+
+    def _log_retries(r: Response, *args, **kwargs) -> None:
+        if not previous_requests:
+            return
+        LOGGER.info(f"{r.request.method} was retried - {len(previous_requests)} times")
+        previous_requests.append(r.request)
+
+    session.hooks["response"].append(_log_retries)
+
     return session
