dce819f09e86c9411d2fd6fc77b65db2b4560c1e
Robert Moses Lim <robert.m.lim@gmail.com>
2023-10-25 19:24:46 -0700

    Only update self.assets if cloud deletion successful

--- great_expectations/datasource/fluent/interfaces.py
+++ great_expectations/datasource/fluent/interfaces.py
@@ -520,12 +520,12 @@ class Datasource(
 
         asset: _DataAssetT
         asset = self.get_asset(asset_name=asset_name)
-        self.assets = list(filter(lambda asset: asset.name != asset_name, self.assets))
 
         if self._data_context and isinstance(self._data_context, CloudDataContext):
             self._data_context._delete_asset(id=str(asset.id))
-        else:
-            self._save_context_project_config()
+
+        self.assets = list(filter(lambda asset: asset.name != asset_name, self.assets))
+        self._save_context_project_config()
 
     def _add_asset(
         self, asset: _DataAssetT, connect_options: dict | None = None
