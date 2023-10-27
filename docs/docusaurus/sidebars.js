2abb22007e8a28d1bbda5e8b8228c100362a9408
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-10-27 08:32:33 -0400

    Minor edits.

--- docs/docusaurus/sidebars.js
+++ docs/docusaurus/sidebars.js
@@ -36,11 +36,6 @@ module.exports = {
               label: 'Supported browsers',
               href: '/docs/cloud/about_gx#supported-browsers',
             },
-            {
-              type: 'link',
-              label: 'Get support',
-              href: '/docs/cloud/about_gx#get-support',
-            },
           ]
         },
         {
