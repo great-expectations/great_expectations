22d293b771a19aa156f8c40a6671f721d7abb29d
Jose Tobar <jose@greatexpectations.io>
2023-10-27 17:15:16 -0700

    Get Checkpoint after it's been created

--- tests/integration/cloud/rest_contracts/test_add_checkpoint.py
+++ tests/integration/cloud/rest_contracts/test_add_checkpoint.py
@@ -15,6 +15,7 @@ if TYPE_CHECKING:
     from tests.integration.cloud.rest_contracts.conftest import PactBody
 
 NON_EXISTING_CHECKPOINT_ID: Final[str] = "68058949-f9c2-47b1-922a-a89c23ffad99"
+EXISTING_CHECKPOINT_ID: Final[str] = "68058949-f9c2-47b1-922a-a89c23ffad99"
 PUT_CHECKPOINT_MIN_RESPONSE_BODY: Final[PactBody] = pact.Like("204 string")
 PUT_CHECKPOINTS_MIN_REQUEST_BODY = {
     "data":
@@ -28,24 +29,29 @@ PUT_CHECKPOINTS_MIN_REQUEST_BODY = {
 
 }
 
+GET_CHECKPOINTS_MIN_RESPONSE_BODY: Final[dict] = {
+    "data":
+        {
+            "id": pact.Format().uuid,
+            "type": "checkpoint",
+            "attributes": {
+                "checkpoint_config": {
+                    "validations": [
+                        {
+                            "id": pact.Format().uuid,
+                        }
+                    ]
+                },
+            },
+        },
+
+}
+
 
 @pytest.mark.cloud
 @pytest.mark.parametrize(
     "contract_interaction",
     [
-        # ContractInteraction(
-        #     method="POST",
-        #     request_path=pathlib.Path(
-        #         "/",
-        #         "organizations",
-        #         EXISTING_ORGANIZATION_ID,
-        #         "datasources",
-        #     ),
-        #     upon_receiving="a request to add a Data Source",
-        #     given="the Data Source does not exist",
-        #     response_status=200,
-        #     response_body=POST_DATASOURCE_MIN_RESPONSE_BODY,
-        # ),
         ContractInteraction(
             method="PUT",
             request_path=pathlib.Path(
@@ -60,6 +66,20 @@ PUT_CHECKPOINTS_MIN_REQUEST_BODY = {
             given="the Checkpoint is created",
             response_status=204,
             response_body="",
+        ),
+        ContractInteraction(
+            method="GET",
+            request_path=pathlib.Path(
+                "/",
+                "organizations",
+                EXISTING_ORGANIZATION_ID,
+                "checkpoints",
+                EXISTING_CHECKPOINT_ID,
+            ),
+            upon_receiving="a request to get a Checkpoint",
+            given="the Checkpoint exists",
+            response_status=200,
+            response_body=GET_CHECKPOINTS_MIN_RESPONSE_BODY,
         ),
     ],
 )
