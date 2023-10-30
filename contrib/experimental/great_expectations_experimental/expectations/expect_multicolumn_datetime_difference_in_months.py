8b83878d63c99c63c64ecc642870069657828a1e
Tobias Bruckert <62531735+tb102122@users.noreply.github.com>
2023-10-30 16:55:04 +1100

    add logic

--- contrib/experimental/great_expectations_experimental/expectations/expect_multicolumn_datetime_difference_in_months.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_multicolumn_datetime_difference_in_months.py
@@ -26,14 +26,13 @@ class MulticolumnDatetimeDifferenceInMonths(MulticolumnMapMetricProvider):
         "condition_parser",
         "ignore_row_if",
     )
-    condition_value_keys = (
-        "start_datetime",
-        "end_datetime",
-        "gap",
-    )
+    condition_value_keys = ("start_datetime", "end_datetime", "gap", "threshold")
 
     @multicolumn_condition_partial(engine=PandasExecutionEngine)
-    def _pandas(cls, dataframe, start_datetime, end_datetime, gap, **kwargs):
+    def _pandas(cls, dataframe, start_datetime, end_datetime, gap, threshold, **kwargs):
+        print(f"threshold: {threshold}")
+        print(kwargs)
+
         def date_diff_in_months(row):
             col_start = to_datetime(row[start_datetime])
             col_end = to_datetime(row[end_datetime])
@@ -44,7 +43,12 @@ class MulticolumnDatetimeDifferenceInMonths(MulticolumnMapMetricProvider):
             diff_months = (col_end.year - col_start.year) * 12 + (
                 col_end.month - col_start.month
             )
-            return col_gap == diff_months
+            print(row)
+            print(diff_months)
+            return col_gap == diff_months or abs(col_gap - diff_months) <= threshold
+
+        if threshold is None:
+            threshold = 0
 
         return dataframe.apply(lambda row: date_diff_in_months(row), axis=1)
 
@@ -61,6 +65,7 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
         start_datetime (datetime): The first datetime column to compare.
         end_datetime (datetime): The second datetime column to compare.
         gap (int): The number of months that the difference between start_datetime and end_datetime should be.
+        threshold (int): (Optional) to adjust sensitivity of check e.g. dates are at the beginning and end of the months.
 
     """
 
@@ -71,14 +76,18 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
                     "2022-03-22 10:00:00",
                     "2022-03-22 10:00:00",
                     "2022-03-22 10:00:00",
+                    "2022-04-02 10:00:00",
                 ],
                 "end_datetime": [
                     "2022-04-22 11:00:00",
                     "2022-04-22 11:00:00",
                     "2022-04-22 11:00:00",
+                    "2022-05-28 10:00:00",
                 ],
-                "gap_pass": [1, 1, 1],
-                "gap_fail": [0, 1, 1],
+                "gap_pass": [1, 1, 1, 1],
+                "gap_fail": [0, 1, 1, 1],
+                "gap_pass_threshold": [2, 1, 1, 1],
+                "gap_fail_threshold": [3, 1, 1, 1],
             },
             "tests": [
                 {
@@ -109,6 +118,44 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
                         "success": False,
                     },
                 },
+                {
+                    "title": "passed test cases with threshold",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column_list": [
+                            "start_datetime",
+                            "end_datetime",
+                            "gap_pass_threshold",
+                        ],
+                        "start_datetime": "start_datetime",
+                        "end_datetime": "end_datetime",
+                        "gap": "gap_pass_threshold",
+                        "threshold": 1,
+                    },
+                    "out": {
+                        "success": True,
+                    },
+                },
+                {
+                    "title": "failed test cases threshold",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column_list": [
+                            "start_datetime",
+                            "end_datetime",
+                            "gap_fail_threshold",
+                        ],
+                        "start_datetime": "start_datetime",
+                        "end_datetime": "end_datetime",
+                        "gap": "gap_fail_threshold",
+                        "threshold": 1,
+                    },
+                    "out": {
+                        "success": False,
+                    },
+                },
             ],
             "test_backends": [
                 {
@@ -123,13 +170,14 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
     map_metric = "multicolumn_values.column_datetime_difference_in_months"
 
     # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("column_list", "start_datetime", "end_datetime", "gap")
+    success_keys = ("column_list", "start_datetime", "end_datetime", "gap", "threshold")
     # This dictionary contains default values for any parameters that should have default values
     default_kwarg_values = {
         "result_format": "BASIC",
         "include_config": True,
         "catch_exceptions": False,
         "base": 2,
+        "threshold": 0,
     }
 
     def validate_configuration(
@@ -151,6 +199,7 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
         start_datetime = configuration.kwargs["start_datetime"]
         end_datetime = configuration.kwargs["end_datetime"]
         gap = configuration.kwargs["gap"]
+        threshold = configuration.kwargs.get("threshold", 0)
         column_list = configuration.kwargs["column_list"]
         # # Check other things in configuration.kwargs and raise Exceptions if needed
         try:
@@ -161,6 +210,7 @@ class ExpectMulticolumnDatetimeDifferenceInMonths(MulticolumnMapExpectation):
             assert gap in column_list
             assert start_datetime in column_list
             assert end_datetime in column_list
+            assert threshold is None or isinstance(threshold, int)
 
         except AssertionError as e:
             raise InvalidExpectationConfigurationError(str(e))
