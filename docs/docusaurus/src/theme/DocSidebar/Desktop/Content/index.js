c8c432f14e01435b27e113893177bc388eeb1f5e
Rob Gray <104205257+kwcanuck@users.noreply.github.com>
2023-10-31 15:20:04 -0400

    Minor edits.

--- docs/docusaurus/src/theme/DocSidebar/Desktop/Content/index.js
+++ docs/docusaurus/src/theme/DocSidebar/Desktop/Content/index.js
@@ -1,45 +1,12 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
 import { ThemeClassNames, useThemeConfig } from '@docusaurus/theme-common';
-import {
-  useAnnouncementBar,
-  useScrollPosition,
-} from '@docusaurus/theme-common/internal';
 import DocSidebarItems from '@theme/DocSidebarItems';
-import SelectNav from '@site/src/components/SelectNav';
 import styles from './styles.module.css';
-function useShowAnnouncementBar() {
-  const { isActive } = useAnnouncementBar();
-  const [showAnnouncementBar, setShowAnnouncementBar] = useState(isActive);
-  useScrollPosition(
-    ({ scrollY }) => {
-      if (isActive) {
-        setShowAnnouncementBar(scrollY === 0);
-      }
-    },
-    [isActive],
-  );
-  return isActive && showAnnouncementBar;
-}
-function useSoftwareNavItems() {
-  // TODO temporary casting until ThemeConfig type is improved
-  return useThemeConfig().softwareNav.items;
-}
 export default function DocSidebarDesktopContent({ path, sidebar, className }) {
-  const showAnnouncementBar = useShowAnnouncementBar();
-  const softwareNavItems = useSoftwareNavItems();
-  return (
+return (
     <>
-      {path.indexOf('/software') > -1 && (
-        <SelectNav items={softwareNavItems} label="Select Software Version" />
-      )}
-      <nav
-        className={clsx(
-          'menu thin-scrollbar',
-          styles.menu,
-          showAnnouncementBar && styles.menuWithAnnouncementBar,
-          className,
-        )}>
+      <nav>
         <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, 'menu__list')}>
           <DocSidebarItems items={sidebar} activePath={path} level={1} />
         </ul>
