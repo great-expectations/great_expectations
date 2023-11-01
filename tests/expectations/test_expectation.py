e2437e1778879f81c8ba1720635967661afdab58
William Shin <will@greatexpectations.io>
2023-10-31 17:12:06 -0700

    adding check

--- tests/expectations/test_expectation.py
+++ tests/expectations/test_expectation.py
@@ -3,9 +3,10 @@ from typing import Any, Dict
 
 import pytest
 
-from great_expectations.core import expectation_configuration
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
 from great_expectations.exceptions import InvalidExpectationConfigurationError
 from great_expectations.expectations import expectation
+from great_expectations.validator.metric_configuration import MetricConfiguration
 
 
 class FakeMulticolumnExpectation(expectation.MulticolumnMapExpectation):
@@ -20,10 +21,48 @@ class FakeColumnPairMapExpectation(expectation.ColumnPairMapExpectation):
     map_metric = "fake_pair_metric"
 
 
+@pytest.fixture
+def metric_config_exists():
+    return [
+        MetricConfiguration(
+            metric_name="table.row_count",
+            metric_domain_kwargs={"batch_id": "projects-projects", "table": None},
+            metric_value_kwargs={},
+            id=("table.row_count", "56f4c287ce8deb0e68b08d220f1c452d", []),
+        )
+    ]
+
+
+@pytest.fixture
+def metric_config_does_not_exist():
+    return [
+        MetricConfiguration(
+            metric_name="table.row_count",
+            metric_domain_kwargs={"batch_id": "projects-projects", "table": None},
+            metric_value_kwargs={},
+            id=("table.row_count", "11111111111111111111111111111111", []),
+        )
+    ]
+
+
+@pytest.fixture
+def metrics_dict():
+    return {("table.row_count", "56f4c287ce8deb0e68b08d220f1c452d", []): ["i_exist"]}
+
+
+@pytest.fixture
+def fake_expectation_configuration():
+    return ExpectationConfiguration(
+        expectation_type="expect_column_values_to_not_be_null",
+        kwargs={"column": "I_dont_exist_either", "batch_id": "projects-projects"},
+        meta={},
+    )
+
+
 def fake_config(
     expectation_type: str, config_kwargs: Dict[str, Any]
-) -> expectation_configuration.ExpectationConfiguration:
-    return expectation_configuration.ExpectationConfiguration(
+) -> ExpectationConfiguration:
+    return ExpectationConfiguration(
         expectation_type=expectation_type,
         kwargs=config_kwargs,
     )
@@ -140,3 +179,24 @@ def test_multicolumn_expectation_validation_errors_with_bad_mostly(
 ):
     with pytest.raises(InvalidExpectationConfigurationError):
         fake_expectation_cls(config)
+
+
+@pytest.mark.unit
+def test_validate_dependencies_against_available_metrics(
+    metric_config_exists,
+    metrics_config_does_not_exist,
+    metrics_dict,
+    fake_config,
+):
+    expectation._validate_dependencies_against_available_metrics(
+        validation_dependencies=metric_config_exists,
+        metrics=metrics_dict,
+        configuration=fake_config,
+    )
+
+    with pytest.raises(InvalidExpectationConfigurationError):
+        expectation._validate_dependencies_against_available_metrics(
+            validation_dependencies=metrics_config_does_not_exist,
+            metrics=metrics_dict,
+            configuration=fake_config,
+        )
