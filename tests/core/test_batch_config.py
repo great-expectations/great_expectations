82f66195612898f979fb4a5f56af438408b9d4f5
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-11-17 10:56:47 -0500

    Backfill test

---
+++ tests/core/test_batch_config.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Optional
+from unittest.mock import Mock
+
+import pytest
+
+from great_expectations.core.batch_config import BatchConfig
+from great_expectations.datasource.fluent.batch_request import BatchRequestOptions
+from great_expectations.datasource.fluent.interfaces import DataAsset
+
+
+@pytest.mark.parametrize(
+    "batch_request_options",
+    [
+        (None,),
+        ({"foo": "bar"},),
+    ],
+)
+@pytest.mark.unit
+def test_build_batch_request(batch_request_options: Optional[BatchRequestOptions]):
+    batch_config = BatchConfig(data_asset=Mock(spec=DataAsset))
+
+    batch_config.build_batch_request(batch_request_options=batch_request_options)
+
+    batch_config.data_asset.build_batch_request.assert_called_once_with(
+        options=batch_request_options
+    )
