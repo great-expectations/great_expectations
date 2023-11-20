fcdc5b63041b79ef68fe63617a765954bc5b5905
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-11-20 10:44:17 -0500

    Update batch config to take in a context and fetch data asset by name

--- tests/core/test_batch_config.py
+++ tests/core/test_batch_config.py
@@ -6,9 +6,53 @@ from unittest.mock import Mock
 import pytest
 
 from great_expectations.core.batch_config import BatchConfig
+from great_expectations.data_context.data_context.abstract_data_context import (
+    AbstractDataContext,
+)
 from great_expectations.datasource.fluent.batch_request import BatchRequestOptions
 from great_expectations.datasource.fluent.interfaces import DataAsset
+from great_expectations.datasource.fluent.sql_datasource import TableAsset
+from great_expectations.datasource.fluent.sqlite_datasource import SqliteDatasource
 
+
+@pytest.fixture
+def mock_asset_name() -> str:
+    return "foo"
+
+
+@pytest.fixture
+def mock_asset(
+    mock_asset_name: str,
+) -> TableAsset:
+    mock_data_asset = Mock(spec=TableAsset)
+    mock_data_asset.name = mock_asset_name
+    return mock_data_asset
+
+
+@pytest.fixture
+def data_context(
+    fds_data_context: AbstractDataContext,
+    fds_data_context_datasource_name: str,
+    mock_asset: TableAsset,
+) -> AbstractDataContext:
+    datasource = fds_data_context.get_datasource(fds_data_context_datasource_name)
+    assert isinstance(datasource, SqliteDatasource)
+    datasource.assets.append(mock_asset)
+    return fds_data_context
+
+@pytest.mark.unit
+def test_data_asset(
+    data_context: AbstractDataContext,
+    fds_data_context_datasource_name: str,
+    mock_asset_name: str,
+    mock_asset: DataAsset,
+):
+    batch_config = BatchConfig(
+        context=data_context,
+        datasource_name=fds_data_context_datasource_name,
+        data_asset_name=mock_asset_name,
+    )
+    assert batch_config.data_asset == mock_asset
 
 @pytest.mark.parametrize(
     "batch_request_options",
@@ -18,12 +62,20 @@ from great_expectations.datasource.fluent.interfaces import DataAsset
     ],
 )
 @pytest.mark.unit
-def test_build_batch_request(batch_request_options: Optional[BatchRequestOptions]):
-    batch_config = BatchConfig(data_asset=Mock(spec=DataAsset))
+def test_build_batch_request(
+    batch_request_options: Optional[BatchRequestOptions],
+    data_context: AbstractDataContext,
+    fds_data_context_datasource_name: str,
+    mock_asset_name: str,
+):
+    batch_config = BatchConfig(
+        context=data_context,
+        datasource_name=fds_data_context_datasource_name,
+        data_asset_name=mock_asset_name,
+    )
 
     batch_config.build_batch_request(batch_request_options=batch_request_options)
 
     mock_build_batch_request = batch_config.data_asset.build_batch_request
     assert isinstance(mock_build_batch_request, Mock)
-
     mock_build_batch_request.assert_called_once_with(options=batch_request_options)
