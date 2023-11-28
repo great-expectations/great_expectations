45dbd434b6e3ff6387e077a242c7c199d74fec25
Chetan Kini <chetan@superconductive.com>
2023-11-28 13:24:00 -0500

    unit tests

--- great_expectations/expectations/core/validators.py
+++ great_expectations/expectations/core/validators.py
@@ -9,12 +9,6 @@ if TYPE_CHECKING:
 def validate_min_value(
     min_val: float | dict | datetime.datetime | None,
 ) -> float | dict | datetime.datetime | None:
-    if not (
-        min_val is None or isinstance(min_val, (float, int, dict, datetime.datetime))
-    ):
-        raise ValueError(
-            "Provided min threshold must be a datetime (for datetime columns) or number"
-        )
     if isinstance(min_val, dict) and "$PARAMETER" not in min_val:
         raise ValueError(
             'Evaluation Parameter dict for min_value kwarg must have "$PARAMETER" key'
@@ -26,12 +20,6 @@ def validate_min_value(
 def validate_max_value(
     max_val: float | dict | datetime.datetime | None,
 ) -> float | dict | datetime.datetime | None:
-    if not (
-        max_val is None or isinstance(max_val, (float, int, dict, datetime.datetime))
-    ):
-        raise ValueError(
-            "Provided max threshold must be a datetime (for datetime columns) or number"
-        )
     if isinstance(max_val, dict) and "$PARAMETER" not in max_val:
         raise ValueError(
             'Evaluation Parameter dict for max_value kwarg must have "$PARAMETER" key'
--- tests/expectations/core/test_validators.py
+++ tests/expectations/core/test_validators.py
@@ -1,42 +1,32 @@
-import datetime
-
 import pytest
 
 from great_expectations.expectations.core.validators import (
+    validate_max_value,
     validate_min_value,
 )
 
-# def validate_min_value(
-#     min_val: float | dict | datetime.datetime | None,
-# ) -> float | dict | datetime.datetime | None:
-#     if not (
-#         min_val is None or isinstance(min_val, (float, int, dict, datetime.datetime))
-#     ):
-#         raise ValueError(
-#             "Provided min threshold must be a datetime (for datetime columns) or number"
-#         )
-#     if isinstance(min_val, dict) and "$PARAMETER" not in min_val:
-#         raise ValueError(
-#             'Evaluation Parameter dict for min_value kwarg must have "$PARAMETER" key'
-#         )
-
-#     return min_val
-
 
 @pytest.mark.unit
-@pytest.mark.parametrize(
-    "min_val",
-    [
-        pytest.param(1.5, id="float"),
-        pytest.param({}, id="dict"),
-        pytest.param(datetime.datetime(1996, 6, 1), id="datetime"),
-        pytest.param(None, id="None"),
-    ],
-)
-def test_validate_min_value_success(min_val):
+def test_validate_min_value_success():
+    min_val = {"$PARAMETER": "foo"}
     validate_min_value(min_val)
 
 
 @pytest.mark.unit
-def test_validate_max_value():
-    pass
+def test_validate_min_value_failure():
+    min_val = {}
+    with pytest.raises(ValueError):
+        validate_min_value(min_val)
+
+
+@pytest.mark.unit
+def test_validate_max_value_success():
+    max_val = {"$PARAMETER": "foo"}
+    validate_max_value(max_val)
+
+
+@pytest.mark.unit
+def test_validate_max_value_failure():
+    max_val = {}
+    with pytest.raises(ValueError):
+        validate_max_value(max_val)
