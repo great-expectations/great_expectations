a296ae76c83f1e38c47c3fa2c135c10e0c7ea457
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-11-28 13:23:34 -0500

    Revert "[MAINTENANCE] Add context to `BatchConfig`, and fetch assets by name (#8982)"
    This reverts commit 6bd8a6336bc9f221ae0bda43ff147abfce4b5746.

--- great_expectations/core/batch_config.py
+++ great_expectations/core/batch_config.py
@@ -1,14 +1,11 @@
 from typing import Optional
 
 from great_expectations.compatibility import pydantic
-from great_expectations.data_context.data_context.abstract_data_context import (
-    AbstractDataContext,
-)
 from great_expectations.datasource.fluent.batch_request import (
     BatchRequest,
     BatchRequestOptions,
 )
-from great_expectations.datasource.fluent.interfaces import DataAsset, Datasource
+from great_expectations.datasource.fluent.interfaces import DataAsset
 
 
 class BatchConfig(pydantic.BaseModel):
@@ -18,20 +15,7 @@ class BatchConfig(pydantic.BaseModel):
     TODO: Add splitters and sorters?
     """
 
-    class Config:
-        arbitrary_types_allowed = True
-
-    context: AbstractDataContext
-    datasource_name: str
-    data_asset_name: str
-
-    @property
-    def data_asset(self) -> DataAsset:
-        """Get the DataAsset referenced by this BatchConfig."""
-        datasource = self.context.get_datasource(self.datasource_name)
-        assert isinstance(datasource, Datasource)
-
-        return datasource.get_asset(self.data_asset_name)
+    data_asset: DataAsset
 
     def build_batch_request(
         self, batch_request_options: Optional[BatchRequestOptions] = None
--- tests/validator/test_v1_validator.py
+++ tests/validator/test_v1_validator.py
@@ -5,6 +5,7 @@ from great_expectations.core.expectation_suite import ExpectationSuite
 from great_expectations.data_context.data_context.abstract_data_context import (
     AbstractDataContext,
 )
+from great_expectations.datasource.fluent.sqlite_datasource import SqliteDatasource
 from great_expectations.expectations.core.expect_column_values_to_be_between import (
     ExpectColumnValuesToBeBetween,
 )
@@ -43,26 +44,22 @@ def expectation_suite(
 
 
 @pytest.fixture
-def batch_config(
-    fds_data_context: AbstractDataContext,
-    fds_data_context_datasource_name: str,
-) -> BatchConfig:
+def batch_config(fds_data_context: AbstractDataContext) -> BatchConfig:
+    datasource = fds_data_context.get_datasource("sqlite_datasource")
+    assert isinstance(datasource, SqliteDatasource)
     return BatchConfig(
-        context=fds_data_context,
-        datasource_name=fds_data_context_datasource_name,
-        data_asset_name="trip_asset",
+        data_asset=datasource.get_asset("trip_asset"),
     )
 
 
 @pytest.fixture
 def batch_config_with_event_type_splitter(
     fds_data_context: AbstractDataContext,
-    fds_data_context_datasource_name: str,
 ) -> BatchConfig:
+    datasource = fds_data_context.get_datasource("sqlite_datasource")
+    assert isinstance(datasource, SqliteDatasource)
     return BatchConfig(
-        context=fds_data_context,
-        datasource_name=fds_data_context_datasource_name,
-        data_asset_name="trip_asset_split_by_event_type",
+        data_asset=datasource.get_asset("trip_asset_split_by_event_type"),
     )
 
 
