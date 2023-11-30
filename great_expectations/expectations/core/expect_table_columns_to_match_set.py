231e3ca934af65d319cd3105ca024273e89c71ec
Chetan Kini <chetan@superconductive.com>
2023-11-30 11:41:07 -0500

    use Union

--- great_expectations/expectations/core/expect_table_columns_to_match_set.py
+++ great_expectations/expectations/core/expect_table_columns_to_match_set.py
@@ -68,7 +68,7 @@ class ExpectTableColumnsToMatchSet(BatchExpectation):
     """
 
     column_set: Union[list, set, dict, None]
-    exact_match: bool | None
+    exact_match: Union[bool, None]
 
     _column_set = validator("column_set", allow_reuse=True)(
         validate_eval_parameter_dict
