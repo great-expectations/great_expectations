6fcb3d7a7a8832caa166be745f3d3077d719eaa7
Chetan Kini <chetan@superconductive.com>
2023-11-30 09:27:19 -0500

    update more

--- great_expectations/expectations/core/expect_column_sum_to_be_between.py
+++ great_expectations/expectations/core/expect_column_sum_to_be_between.py
@@ -91,6 +91,8 @@ class ExpectColumnSumToBeBetween(ColumnAggregateExpectation):
 
     min_value: Union[float, dict, datetime, None] = None
     max_value: Union[float, dict, datetime, None] = None
+    strict_min: bool = False
+    strict_max: bool = False
 
     _min_val = validator("min_value", allow_reuse=True)(validate_eval_parameter_dict)
     _max_val = validator("max_value", allow_reuse=True)(validate_eval_parameter_dict)
