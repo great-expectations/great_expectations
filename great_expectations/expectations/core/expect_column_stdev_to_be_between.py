44d307c8a64ed74216882040f1033f1b06d2acbd
Chetan Kini <chetan@superconductive.com>
2023-12-06 10:53:05 -0500

    [MAINTENANCE] Remove more instances of `include_config` (#9046)

--- great_expectations/expectations/core/expect_column_stdev_to_be_between.py
+++ great_expectations/expectations/core/expect_column_stdev_to_be_between.py
@@ -53,8 +53,6 @@ class ExpectColumnStdevToBeBetween(ColumnAggregateExpectation):
         result_format (str or None): \
             Which output mode to use: BOOLEAN_ONLY, BASIC, COMPLETE, or SUMMARY. \
             For more detail, see [result_format](https://docs.greatexpectations.io/docs/reference/expectations/result_format).
-        include_config (boolean): \
-            If True, then include the expectation config as part of the result object.
         catch_exceptions (boolean or None): \
             If True, then catch exceptions and include them as part of the result object. \
             For more detail, see [catch_exceptions](https://docs.greatexpectations.io/docs/reference/expectations/standard_arguments/#catch_exceptions).
@@ -65,7 +63,7 @@ class ExpectColumnStdevToBeBetween(ColumnAggregateExpectation):
     Returns:
         An [ExpectationSuiteValidationResult](https://docs.greatexpectations.io/docs/terms/validation_result)
 
-        Exact fields vary depending on the values passed to result_format, include_config, catch_exceptions, and meta.
+        Exact fields vary depending on the values passed to result_format, catch_exceptions, and meta.
 
     Notes:
         * min_value and max_value are both inclusive unless strict_min or strict_max are set to True.
--- great_expectations/expectations/core/expect_table_column_count_to_be_between.py
+++ great_expectations/expectations/core/expect_table_column_count_to_be_between.py
@@ -47,8 +47,6 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
         result_format (str or None): \
             Which output mode to use: BOOLEAN_ONLY, BASIC, COMPLETE, or SUMMARY. \
             For more detail, see [result_format](https://docs.greatexpectations.io/docs/reference/expectations/result_format).
-        include_config (boolean): \
-            If True, then include the expectation config as part of the result object.
         catch_exceptions (boolean or None): \
             If True, then catch exceptions and include them as part of the result object. \
             For more detail, see [catch_exceptions](https://docs.greatexpectations.io/docs/reference/expectations/standard_arguments/#catch_exceptions).
@@ -59,7 +57,7 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
     Returns:
         An [ExpectationSuiteValidationResult](https://docs.greatexpectations.io/docs/terms/validation_result)
 
-        Exact fields vary depending on the values passed to result_format, include_config, catch_exceptions, and meta.
+        Exact fields vary depending on the values passed to result_format, catch_exceptions, and meta.
 
     Notes:
         * min_value and max_value are both inclusive.
@@ -95,7 +93,6 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
         "min_value": None,
         "max_value": None,
         "result_format": "BASIC",
-        "include_config": True,
         "catch_exceptions": False,
         "meta": None,
     }
