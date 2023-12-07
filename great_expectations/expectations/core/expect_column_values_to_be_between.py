4221cc7d8245e0886cf8faffaba33793bb80e1b8
Chetan Kini <chetan@superconductive.com>
2023-12-07 13:26:18 -0500

    add back ints

--- great_expectations/expectations/core/expect_column_values_to_be_between.py
+++ great_expectations/expectations/core/expect_column_values_to_be_between.py
@@ -78,8 +78,8 @@ class ExpectColumnValuesToBeBetween(ColumnMapExpectation):
         [expect_column_value_lengths_to_be_between](https://greatexpectations.io/expectations/expect_column_value_lengths_to_be_between)
     """
 
-    min_value: Union[float, EvaluationParameterDict, datetime, None] = None
-    max_value: Union[float, EvaluationParameterDict, datetime, None] = None
+    min_value: Union[int, float, EvaluationParameterDict, datetime, None] = None
+    max_value: Union[int, float, EvaluationParameterDict, datetime, None] = None
     strict_min: bool = False
     strict_max: bool = False
 
