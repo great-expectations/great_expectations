d25fcee213a4da2beaa5a2b5e7a717f61186021e
Chetan Kini <chetan@superconductive.com>
2023-12-07 15:20:33 -0500

    fix eval param test

--- tests/integration/docusaurus/expectations/advanced/how_to_create_expectations_that_span_multiple_batches_using_evaluation_parameters.py
+++ tests/integration/docusaurus/expectations/advanced/how_to_create_expectations_that_span_multiple_batches_using_evaluation_parameters.py
@@ -2,7 +2,6 @@ import pathlib
 
 # <snippet name="tests/integration/docusaurus/expectations/advanced/how_to_create_expectations_that_span_multiple_batches_using_evaluation_parameters.py get_context">
 import great_expectations as gx
-from great_expectations.core.result_format import ResultFormat
 
 context = gx.get_context()
 # </snippet>
@@ -67,7 +66,6 @@ expected_validation_result = {
             "value": {
                 "$PARAMETER": "urn:great_expectations:validations:upstream_expectation_suite:expect_table_row_count_to_be_between.result.observed_value"
             },
-            "result_format": ResultFormat.BASIC,
         },
         "expectation_type": "expect_table_row_count_to_equal",
         "meta": {},
