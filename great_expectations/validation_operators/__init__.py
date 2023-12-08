43d2364dc17a83d507981fa65902a14764274dd2
Josh Stauffer <josh@greatexpectations.io>
2023-12-08 09:29:56 -0500

    remove WarningAndFailureExpectationSuitesValidationOperator

--- great_expectations/validation_operators/__init__.py
+++ great_expectations/validation_operators/__init__.py
@@ -11,15 +11,3 @@ from great_expectations.checkpoint.actions import (
     UpdateDataDocsAction,
     ValidationAction,
 )
-from great_expectations.util import verify_dynamic_loading_support
-
-from .validation_operators import (
-    ActionListValidationOperator,
-    ValidationOperator,
-    WarningAndFailureExpectationSuitesValidationOperator,
-)
-
-for module_name, package_name in [
-    (".validation_operators", "great_expectations.validation_operators"),
-]:
-    verify_dynamic_loading_support(module_name=module_name, package_name=package_name)
