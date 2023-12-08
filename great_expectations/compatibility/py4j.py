0287a6ec0c8c2185c252215ab0f20a30f4c7a4f1
Nathan Farmer <nathan@greatexpectations.io>
2023-12-08 10:23:30 -0500

    Bad compatibility import

--- great_expectations/compatibility/py4j.py
+++ great_expectations/compatibility/py4j.py
@@ -3,6 +3,6 @@ from great_expectations.compatibility.not_imported import NotImported
 PY4J_NOT_IMPORTED = NotImported("py4j is not installed, please 'pip install py4j'")
 
 try:
-    import py4j
+    from py4j import protocol
 except ImportError:
-    py4j = PY4J_NOT_IMPORTED
+    protocol = PY4J_NOT_IMPORTED
