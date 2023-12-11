d85e4fd103635eb0dd8890277d2f49213f9ecb69
Nathan Farmer <nathan@greatexpectations.io>
2023-12-11 16:55:21 -0500

    Remove spark config options that generate warnings from tests

--- tests/datasource/test_new_datasource.py
+++ tests/datasource/test_new_datasource.py
@@ -910,8 +910,6 @@ def test_spark_with_batch_spec_passthrough(tmp_path_factory, spark_session):
             class_name: SparkDFExecutionEngine
             spark_config:
                 spark.master: local[*]
-                spark.executor.memory: 450m
-                spark.driver.memory: 6g
                 spark.sql.shuffle.partitions: 2
                 spark.default.parallelism: 4
         data_connectors:
