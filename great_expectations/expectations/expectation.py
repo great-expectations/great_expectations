b55803a50c1b16a3fdeb56f379c670d42810b6b6
Anthony Burdi <anthony@greatexpectations.io>
2023-12-12 15:47:48 -0500

    WIP expect_column_values_to_be_of_type

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -41,9 +41,7 @@ from great_expectations import __version__ as ge_version
 from great_expectations.compatibility import pydantic
 from great_expectations.compatibility.pydantic import Field, ModelMetaclass
 from great_expectations.compatibility.typing_extensions import override
-from great_expectations.core._docs_decorators import (
-    public_api,
-)
+from great_expectations.core._docs_decorators import public_api
 from great_expectations.core.expectation_configuration import (
     ExpectationConfiguration,
     parse_result_format,
@@ -2808,7 +2806,7 @@ class ColumnPairMapExpectation(BatchExpectation, ABC):
         "condition_parser",
     )
     domain_type = MetricDomainTypes.COLUMN_PAIR
-    success_keys = ("mostly",)
+    success_keys: ClassVar[Tuple[str, ...]] = ("mostly",)
 
     @classmethod
     @override
