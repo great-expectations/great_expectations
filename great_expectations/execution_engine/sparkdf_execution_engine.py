842806b4eb1e32e55e917441483ddbb256ac5d19
Nathan Farmer <nathan@greatexpectations.io>
2023-12-12 10:28:48 -0500

    Handle py4j error

--- great_expectations/execution_engine/sparkdf_execution_engine.py
+++ great_expectations/execution_engine/sparkdf_execution_engine.py
@@ -369,7 +369,7 @@ class SparkDFExecutionEngine(ExecutionEngine):
                     and spark_session.sparkContext.appName != value
                 ):
                     spark_session.sparkContext.appName = value
-            # attribute error can be raised for connect sessions that haven't implemented a method
+            # attribute error can be raised for connect sessions that haven't implemented a conf.get
             # analysis exception can be raises in environments that don't allow updating config of that option
             except (
                 pyspark.PySparkAttributeError,
