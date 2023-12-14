09e8235ebc902c18112a46b5c9e1c7e53bdd1607
Chetan Kini <chetan@superconductive.com>
2023-12-14 14:37:22 -0500

    [MAINTENANCE] Update type-checking specific Expectation imports (#9103)

--- great_expectations/expectations/core/expect_column_values_to_be_of_type.py
+++ great_expectations/expectations/core/expect_column_values_to_be_of_type.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import inspect
 import logging
 from typing import TYPE_CHECKING, ClassVar, Dict, Optional, Tuple
@@ -8,10 +10,6 @@ import pandas as pd
 from great_expectations.compatibility import aws, pyspark, trino
 from great_expectations.compatibility.sqlalchemy import sqlalchemy as sa
 from great_expectations.compatibility.typing_extensions import override
-from great_expectations.core import (
-    ExpectationConfiguration,
-    ExpectationValidationResult,
-)
 from great_expectations.execution_engine import (
     ExecutionEngine,
     PandasExecutionEngine,
@@ -40,10 +38,14 @@ from great_expectations.util import (
     get_pyathena_potential_type,
 )
 from great_expectations.validator.metric_configuration import MetricConfiguration
-from great_expectations.validator.validator import ValidationDependencies
 
 if TYPE_CHECKING:
+    from great_expectations.core import (
+        ExpectationConfiguration,
+        ExpectationValidationResult,
+    )
     from great_expectations.render.renderer_configuration import AddParamArgs
+    from great_expectations.validator.validator import ValidationDependencies
 
 logger = logging.getLogger(__name__)
 
