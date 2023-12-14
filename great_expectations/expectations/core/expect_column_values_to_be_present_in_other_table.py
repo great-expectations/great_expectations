6e87b98fef13004b8892db6f2b7395db2b658ba8
William Shin <will@greatexpectations.io>
2023-12-14 11:34:46 -0800

    make Expectation

---
+++ great_expectations/expectations/core/expect_column_values_to_be_present_in_other_table.py
@@ -0,0 +1,11 @@
+from great_expectations.expectations.expectation import Expectation
+
+
+class ExpectColumnValuesToBePresentInAnotherTable(Expectation):
+    """Expect Column values to be in other table
+
+    Args:
+        BatchExpectation (_type_): _description_
+    """
+
+    pass
