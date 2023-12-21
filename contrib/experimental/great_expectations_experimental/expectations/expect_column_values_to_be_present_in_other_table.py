774faafb9af4876e39a73d3b7d36b69b3d35bd28
William Shin <will@greatexpectations.io>
2023-12-20 16:16:42 -0800

    pushing small change to simplify

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_present_in_other_table.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_present_in_other_table.py
@@ -111,7 +111,7 @@ class ExpectColumnValuesToBePresentInAnotherTable(QueryExpectation):
 
         template_dict = configuration.kwargs.get("template_dict")
 
-        template_str = "All values in column $foreign_key_column are present in column $primary_key_column_in_foreign_table of  table $foreign_table."
+        template_str = "All values in column $foreign_key_column are present in column $primary_key_column_in_foreign_table of table $foreign_table."
 
         params = {
             "foreign_key_column": template_dict["foreign_key_column"],
