449b6083f28539883e5ab2ba08aaf9aae6254543
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-01-08 16:25:01 -0500

    Add fallback for typing extensions

--- great_expectations/compatibility/typing_extensions.py
+++ great_expectations/compatibility/typing_extensions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, Tuple, TypeVar, Union
 
 try:
     from typing_extensions import override
@@ -11,4 +11,23 @@ except ImportError:
         return __arg
 
 
-__all__ = ["override"]
+try:
+    from typing_extensions import dataclass_transform
+except ImportError:
+    # borrowed from pydantic 1.9
+    _T = TypeVar("_T")
+
+    def __dataclass_transform__(
+        *,
+        eq_default: bool = True,
+        order_default: bool = False,
+        kw_only_default: bool = False,
+        field_descriptors: Tuple[Union[type, Callable[..., Any]], ...] = (()),
+    ) -> Callable[[_T], _T]:
+        return lambda a: a
+
+
+__all__ = [
+    "dataclass_transform",
+    "override",
+]
