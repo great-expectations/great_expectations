018c3a343dbbefc7dbc5c2359be124e73e82bc84
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-01-10 15:01:05 -0500

    update per review

--- tests/integration/docusaurus/expectations/expectation_management.py
+++ tests/integration/docusaurus/expectations/expectation_management.py
@@ -1,56 +1,41 @@
 # setup
 import great_expectations as gx
+import great_expectations.expectations as gxe
 from great_expectations.core.expectation_suite import ExpectationSuite
-from great_expectations.expectations.core import (
-    ExpectColumnValuesToBeInSet,
-    ExpectColumnValuesToNotBeNull,
-)
 
 context = gx.get_context()
 suite_name = "test-suite"
-column_a = "a"
-column_b = "b"
-column_c = "c"
 
 # create a suite
-suite = ExpectationSuite(name=suite_name)
-suite = context.suites.add(suite=suite)
-
-
-# add expectations
-expectation_a = ExpectColumnValuesToBeInSet(
-    column=column_a, value_set=["foo", "bar", "baz"]
-)
-suite.add_expectation(expectation=expectation_a)
-expectation_b = ExpectColumnValuesToNotBeNull(column=column_a)
-suite.add_expectation(expectation=expectation_b)
-
-# find and update a single expectation
-expectation_type = ExpectColumnValuesToNotBeNull
-expectation = next(
-    expectation
-    for expectation in suite.expectations
-    if isinstance(expectation, expectation_type)
-)
-expectation.column = column_b
-expectation.save()
-
-# batch update expectations
-for expectation in suite.expectations:
-    expectation.column = column_c
-suite.save()
-
-# delete an expectation
-expectation_type = ExpectColumnValuesToNotBeNull
-expectation = next(
-    expectation
-    for expectation in suite.expectations
-    if isinstance(expectation, expectation_type)
-)
-suite.delete_expectation(expectation=expectation)
+suite = context.suites.add(suite=ExpectationSuite(name=suite_name))
 
 # get a suite
 suite = context.suites.get(name=suite_name)
+
+# add expectations
+suite.add_expectation(
+    gxe.ExpectColumnValuesToBeInSet(column="passenger_count", value_set=[1, 2, 3, 4, 5])
+)
+suite.add_expectation(gxe.ExpectColumnValuesToNotBeNull(column="pickup_datetime"))
+
+# find a single expectation
+expectation = next(
+    expectation
+    for expectation in suite.expectations
+    if isinstance(expectation, gxe.ExpectColumnValuesToNotBeNull)
+)
+
+# update a single expectation
+expectation.column = "pickup_location_id"
+expectation.save()
+
+# update multiple expectations as a batch
+for expectation in suite.expectations:
+    expectation.notes = "This Expectation was generated as part of GX Documentation."
+suite.save()
+
+# delete an expectation
+suite.delete_expectation(expectation=expectation)
 
 # delete a suite
 context.suites.delete(suite=suite)
