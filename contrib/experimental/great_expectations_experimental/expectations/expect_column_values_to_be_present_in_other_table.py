3035a07a27bb6ea01346a03a503175b5c88b1ddf
William Shin <will@greatexpectations.io>
2024-01-10 15:19:41 -0800

    [CONTRIB] Referential Integrity - Make building of `unexpected_value` table non-case sensitive (#9224)

--- contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_present_in_other_table.py
+++ contrib/experimental/great_expectations_experimental/expectations/expect_column_values_to_be_present_in_other_table.py
@@ -308,7 +308,10 @@ class ExpectColumnValuesToBePresentInOtherTable(QueryExpectation):
 
         unexpected_list: List[Any] = []
         for values in unexpected_values:
-            unexpected_list.append(values[foreign_key_column_name])
+            # we don't use `foreign_key_column_name` but do an explicit listing of values() here because
+            # the returned unexpected_values are not always case-sensitive.
+            value = list(values.values())[0]
+            unexpected_list.append(value)
 
         partial_unexpected_counts = self._generate_partial_unexpected_counts(
             unexpected_list=unexpected_list,
