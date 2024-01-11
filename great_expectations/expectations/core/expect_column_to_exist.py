605af00e86909dc30dda3e4b52b427b71061878a
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-01-11 17:05:31 -0500

    Revert "More stuff to delete"
    This reverts commit f62b3bb1087fc66c193bc9614f40e62c3aba9703.

--- great_expectations/expectations/core/expect_column_to_exist.py
+++ great_expectations/expectations/core/expect_column_to_exist.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Dict, Optional, Union
 
-from great_expectations._docs_decorators import public_api
 from great_expectations.compatibility.typing_extensions import override
 from great_expectations.core.evaluation_parameters import (
     EvaluationParameterDict,  # noqa: TCH001
@@ -162,10 +161,6 @@ class ExpectColumnToExist(BatchExpectation):
                 },
             )
         ]
-
-    @public_api
-    def delete_me_plq(self) -> None:
-        pass
 
     @override
     def _validate(
