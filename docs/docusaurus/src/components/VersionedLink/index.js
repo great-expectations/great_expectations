36f1a56e1140c94c9c5f858027e613ed7a830b70
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-01-29 11:53:41 -0500

    Fix export

--- docs/docusaurus/src/components/VersionedLink/index.js
+++ docs/docusaurus/src/components/VersionedLink/index.js
@@ -9,9 +9,11 @@ import { useVersionedPath } from '@site/src/hooks/use_versioned_path';
  * e.g. <VersionedLink to="/intro" /> used in a page at `/docs/1.0.0/foo/bar` links to `docs/1.0.0/intro
  * e.g. <VersionedLink to="/intro" /> used in a page at `/docs/foo/bar` links to `docs/intro
  */
-export default VersionedLink = ({to, ...rest}) => {
+const VersionedLink = ({to, ...rest}) => {
     const versionedPath = useVersionedPath(to);
     return (
         <Link to={versionedPath} {...rest} />
     )
 }
+
+export default VersionedLink;
