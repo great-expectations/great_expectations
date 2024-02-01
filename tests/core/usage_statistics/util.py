73be5e6df7d3fd0238d001e4f71fd2b691ae33b9
Chetan Kini <chetan@superconductive.com>
2024-02-01 12:58:41 -0500

    bring back util

---
+++ tests/core/usage_statistics/util.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from great_expectations.core.usage_statistics.usage_statistics import (
+    UsageStatsExceptionPrefix,
+)
+
+
+def usage_stats_exceptions_exist(messages: List[str]) -> bool:
+    """
+    Since the usage stats functionality does not raise exceptions but merely logs them, we need to check the logs for errors.
+    """
+
+    return any(
+        UsageStatsExceptionPrefix.EMIT_EXCEPTION.value in message
+        for message in messages
+    )
+
+
+def usage_stats_invalid_messages_exist(messages: List[str]) -> bool:
+    """
+    Since the usage stats functionality does not raise exceptions but merely logs them, we need to check the logs for errors.
+    """
+
+    return any(
+        UsageStatsExceptionPrefix.INVALID_MESSAGE.value in message
+        for message in messages
+    )
