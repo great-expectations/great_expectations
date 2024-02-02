f8b4b3caffd7376b5ebf9240739c36ed0a8e5a5d
Deborah Niesz <deborah.niesz@10pines.com>
2024-02-02 15:15:35 -0300

    Creating Feedback Modal

--- docs/docusaurus/src/components/FeedbackModal/index.js
+++ docs/docusaurus/src/components/FeedbackModal/index.js
@@ -15,17 +15,15 @@ export default function FeedbackModal(){
 
     const handleChange = (e) => {
         const { name, value, files } = e.target;
-        // If the input is a file input, use files[0] to get the selected file
-        const file = name === 'attachment' ? files[0] : null;
         setFormData((prevData) => ({
             ...prevData,
-            [name]: name === 'attachment' ? file : value,
+            [name]: name === 'attachment' ? files[0] : value,
         }));
     };
 
-    let thumbsUpImg = useBaseUrl(`img/thumbs_up_icon.svg`);
-    let thumbsDownImg = useBaseUrl(`img/thumbs_down_icon.svg`);
-    let closeImg = useBaseUrl(`img/close_icon.svg`);
+    const thumbsUpImg = useBaseUrl(`img/thumbs_up_icon.svg`);
+    const thumbsDownImg = useBaseUrl(`img/thumbs_down_icon.svg`);
+    const closeImg = useBaseUrl(`img/close_icon.svg`);
 
     return <>
         <div className={styles.feedbackCard}>
@@ -41,68 +39,64 @@ export default function FeedbackModal(){
         </div>
 
         {isOpen && <>
-            <div className={styles.darkBackground} onClick={() => setIsOpen(false)}/>
-            <div className={styles.centered}>
-                <div className={styles.modal}>
-                    <div className={styles.modalHeader}>
-                        <h5 className={styles.modalHeaderTitle}>Tell Us More</h5>
-                        <img src={closeImg} className={styles.modalHeaderCloseButton}
-                             alt="Close icon" onClick={() => setIsOpen(false)}/>
-                    </div>
+            <div className={styles.overlay} onClick={() => setIsOpen(false)}/>
+            <div className={styles.modal}>
+                <div className={styles.modalHeader}>
+                    <h5 className={styles.modalHeaderTitle}>Tell Us More</h5>
+                    <img src={closeImg} className={styles.modalHeaderCloseButton}
+                         alt="Close icon" onClick={() => setIsOpen(false)}/>
+                </div>
 
-                    <div className={styles.modalContent}>
+                <div className={styles.modalContent}>
 
-                        Your opinion matters. Share your feedback here to help us improve
-                        the quality of our documentation and ensure a better user experience.
-                        Thank you for taking the time to share your experience.
+                    Your opinion matters. Share your feedback here to help us improve
+                    the quality of our documentation and ensure a better user experience.
+                    Thank you for taking the time to share your experience.
 
-                        <div className={styles.textInputs}>
-                            <div className={styles.modalTextContainer}>
-                                <label className={styles.modalTextLabel}>Name</label>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    className={styles.modalTextInput}
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                    placeholder="Phillip"
-                                />
-                            </div>
-                            <div className={styles.modalTextContainer}>
-                                <label className={styles.modalTextLabel}>Work Email*</label>
-                                <input
-                                    type="email"
-                                    name="workEmail"
-                                    className={styles.modalTextInput}
-                                    value={formData.workEmail}
-                                    onChange={handleChange}
-                                    placeholder="your_email@domain.com"
-                                />
-                            </div>
-                        </div>
+                    <div className={styles.textInputs}>
                         <div className={styles.modalTextContainer}>
-                            <label className={styles.modalTextLabel}>Tell Us More</label>
-                            <textarea
-                                name="description"
-                                value={formData.description}
-                                className={styles.modalTextInput + ' ' +styles.modalTextareaInput}
+                            <label className={styles.modalTextLabel}>Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                className={styles.modalTextInput}
+                                value={formData.name}
                                 onChange={handleChange}
-                                placeholder="Provide as much detail as possible about the issue you
-                                experienced or where improvement is needed. Detailed feedback helps
-                                us better identify the problem and determine a solution."
+                                placeholder="Phillip"
                             />
                         </div>
-                        <input
-                            type="file"
-                            name="attachment"
+                        <div className={styles.modalTextContainer}>
+                            <label className={styles.modalTextLabel}>Work Email*</label>
+                            <input
+                                type="email"
+                                name="workEmail"
+                                className={styles.modalTextInput}
+                                value={formData.workEmail}
+                                onChange={handleChange}
+                                placeholder="your_email@domain.com"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.modalTextContainer}>
+                        <label className={styles.modalTextLabel}>Tell Us More</label>
+                        <textarea
+                            name="description"
+                            value={formData.description}
+                            className={styles.modalTextInput + ' ' + styles.modalTextareaInput}
                             onChange={handleChange}
+                            placeholder="Provide as much detail as possible about the issue you
+                            experienced or where improvement is needed. Detailed feedback helps
+                            us better identify the problem and determine a solution."
                         />
                     </div>
-                    <div className={styles.modalActions}>
-                    <button className={styles.submitButton} onClick={() => setIsOpen(false)}> Submit </button>
+                    <input
+                        type="file"
+                        name="attachment"
+                        onChange={handleChange}
+                    />
+                    <button className={styles.submitButton} onClick={() => setIsOpen(false)}> Submit</button>
                 </div>
             </div>
-        </div>
         </>}
     </>
 }
