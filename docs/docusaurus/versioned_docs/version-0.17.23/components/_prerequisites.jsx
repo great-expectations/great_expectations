fa48c814ca94939faaf7631e55e7e4af526fd71a
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-02-02 12:41:10 -0500

    Revert "Revert "Update a few more links to point to the redirect links""
    This reverts commit 0fd3a94781062baafb180ecc14b6b5a521348830.

--- docs/docusaurus/versioned_docs/version-0.17.23/components/_prerequisites.jsx
+++ docs/docusaurus/versioned_docs/version-0.17.23/components/_prerequisites.jsx
@@ -68,11 +68,11 @@ export default class Prerequisites extends React.Component {
       returnItems.push(<li>Access to data stored in a SQL database.</li>)
     }
     if (this.props.requireDatasource === 'Pandas') {
-      returnItems.push(<li><a href='/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_pandas_datasource'>A Datasource configured to access your source data.</a></li>)
+      returnItems.push(<li><a href='/docs/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_pandas_datasource'>A Datasource configured to access your source data.</a></li>)
     } else if (this.props.requireDatasource === 'Spark') {
-      returnItems.push(<li><a href='/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource'>A Datasource configured to access your source data.</a></li>)
+      returnItems.push(<li><a href='/docs/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_spark_datasource'>A Datasource configured to access your source data.</a></li>)
     } else if (this.props.requireDatasource === 'SQL') {
-      returnItems.push(<li><a href='/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_sql_datasource'>A Datasource configured to access your source data.</a></li>)
+      returnItems.push(<li><a href='/docs/guides/connecting_to_your_data/datasource_configuration/how_to_configure_a_sql_datasource'>A Datasource configured to access your source data.</a></li>)
     } else if (this.props.requireDatasource === true) {
       returnItems.push(<li><VersionedLink to='/guides/connecting_to_your_data/connect_to_data_lp'>A Datasource configured to access your source data</VersionedLink></li>)
     }
