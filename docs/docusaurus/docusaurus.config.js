f6c3deefeaa6ab650d86ddc7c64fa410e30b87bb
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-02-05 14:28:47 -0500

    Update docusaurus config to exclude the current version

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -224,16 +224,11 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          includeCurrentVersion: false,
           sidebarPath: require.resolve('./sidebars.js'),
           // Note: remarkCodeImport is included to handle earlier versions with line number references (e.g. v0.14.13)
           remarkPlugins: [remarkNamedSnippets, remarkCodeImport],
           lastVersion: 'current',
-          versions: {
-            current: {
-              label: '0.18.8',
-              path: ''
-            }
-          }
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
