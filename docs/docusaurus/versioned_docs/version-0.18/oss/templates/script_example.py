49ac859b25de6023e500c1ce89ae3848376bcf13
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2024-02-07 15:50:44 -0500

    [DOCS] Cut v 0.18 docs (#9395)

---
+++ docs/docusaurus/versioned_docs/version-0.18/oss/templates/script_example.py
@@ -0,0 +1,18 @@
+# <snippet name="version-0.18 docs/docusaurus/docs/oss/templates/script_example.py full">
+# <snippet name="version-0.18 docs/docusaurus/docs/oss/templates/script_example.py imports">
+# <snippet name="version-0.18 docs/docusaurus/docs/oss/templates/script_example.py first import">
+
+# </snippet>
+
+import great_expectations as gx
+from great_expectations.core.yaml_handler import YAMLHandler
+
+# </snippet>
+
+yaml = YAMLHandler()
+context = gx.get_context()
+
+# <snippet name="version-0.18 docs/docusaurus/docs/oss/templates/script_example.py assert">
+assert context
+# </snippet>
+# </snippet>
