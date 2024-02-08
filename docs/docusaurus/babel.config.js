640096232e45198f6e2bd7801a6543dc8ef7e1c1
Jose Tobar <jose@greatexpectations.io>
2024-02-07 17:22:52 -0700

    Use swc compiler

--- docs/docusaurus/babel.config.js
+++ docs/docusaurus/babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [
-      require.resolve('@docusaurus/core/lib/babel/preset'),
-      ['@babel/preset-env', {targets: {node: 'current'}}]
-  ]
-}
+  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+};
--- docs/docusaurus/jest.config.js
+++ docs/docusaurus/jest.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-        "\\.css$": "identity-obj-proxy",
-        "@docusaurus/(BrowserOnly|ComponentCreator|constants|ExecutionEnvironment|Head|Interpolate|isInternalUrl|Link|Noop|renderRoutes|router|Translate|use.*)":
-            "identity-obj-proxy",
-        "@theme/(.*)": "@docusaurus/theme-classic/src/theme/$1",
-        "@site/(.*)": "website/$1",
-    },
-    transform: {
-        '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './babel.config.js' }],
-    },
-    globals: {
-        fetch: global.fetch
-    }
-}
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.css$": "identity-obj-proxy",
+    "@docusaurus/(BrowserOnly|ComponentCreator|constants|ExecutionEnvironment|Head|Interpolate|isInternalUrl|Link|Noop|renderRoutes|router|Translate|use.*)":
+      "identity-obj-proxy",
+    "@theme/(.*)": "@docusaurus/theme-classic/src/theme/$1",
+    "@site/(.*)": "website/$1",
+  },
+  transform: {
+    "^.+\\.[jt]sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "ecmascript",
+            jsx: true,
+          },
+        },
+      },
+    ],
+  },
+};
