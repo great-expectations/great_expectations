171a5552e4a8565499d34bf20f598a83c7425067
Jose Tobar <jose@greatexpectations.io>
2024-02-07 18:15:59 -0700

    fix test

--- docs/docusaurus/___test___/githubBadge.test.js
+++ docs/docusaurus/___test___/githubBadge.test.js
@@ -1,38 +1,25 @@
-import * as React from 'react';
-import {expect, jest, test} from '@jest/globals';
+import * as React from "react";
+import { jest, test } from "@jest/globals";
 
-import { waitFor, render, screen, act } from '@testing-library/react';
-import GithubBadge from '../src/components/NavbarItems/GithubNavbarItem';
+import { render, screen } from "@testing-library/react";
+import GithubBadge from "../src/components/NavbarItems/GithubNavbarItem";
 
+// Todo: replace this with a mock server if we need to do more fetch requests: https://testing-library.com/docs/react-testing-library/example-intro/#full-example
 global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve( { stargazers_count: 9100, forks_count: 20 }),
-    })
+  Promise.resolve({
+    json: () => Promise.resolve({ stargazers_count: 9100, forks_count: 20000 }),
+  })
 );
 
-jest.mock("@docusaurus/useBaseUrl", () => ({
-    __esModule: true,
-    default: jest.fn((url) => url),
-}));
-
-// describe('github badge in the header', () => {
-//     beforeEach(() => {
-
-    // });
-
-    afterEach(() => {
-        jest.restoreAllMocks();
-    });
-
-
-    test('should show stargazers count', async () => {
-        // await Promise.resolve()
-
-        // act(() => {
-            render(<GithubBadge owner="great-expectations" repository="great_expectations"/>);
-        // })
-
-        // screen.getByAltText("Github Forks Count");
-        // await screen.getByText("9.1k");
-    });
-// });
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+describe("github badge in the header", () => {
+  test("should show stargazers count", async () => {
+    render(
+      <GithubBadge owner="great-expectations" repository="great_expectations" />
+    );
+    await screen.findByText("9.1k");
+    screen.getByText("20k");
+  });
+});
