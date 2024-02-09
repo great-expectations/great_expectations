4f1213f20ac99faeac1b32b7a6a119e5a97e42d4
JessSaavedra <jessicasaavedraval@gmail.com>
2024-02-09 00:59:38 -0300

    Highlight section docs in sidebar

---
+++ docs/docusaurus/src/theme/DocSidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DocSidebar from '@theme-original/DocSidebar';
+import {useLocation} from '@docusaurus/router';
+
+export default function DocSidebarWrapper(props) {
+  const {pathname, hash} = useLocation();
+
+  let path = pathname;
+  if (hash) {
+      path = path + hash;
+  }
+
+  return (
+    <DocSidebar {...props} path={path}/>
+  );
+}
