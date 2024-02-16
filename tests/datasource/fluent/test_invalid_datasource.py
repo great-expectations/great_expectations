032fbf1a78bc75cf81197b7622c9e85ec8ac922f
Gabriel Gore <gabriel59kg@gmail.com>
2024-02-16 15:24:12 -0500

    more tests

--- tests/datasource/fluent/test_invalid_datasource.py
+++ tests/datasource/fluent/test_invalid_datasource.py
@@ -2,29 +2,17 @@ from __future__ import annotations
 
 import inspect
 from pprint import pformat as pf
-from typing import Any, Callable, Final, Literal
+from typing import Any, Callable, Final, Literal, Protocol
 
 import pytest
 
 from great_expectations.compatibility import pydantic
-from great_expectations.datasource.fluent import Datasource, InvalidDatasource
+from great_expectations.datasource.fluent import (
+    Datasource,
+    InvalidDatasource,
+    TestConnectionError,
+)
 from great_expectations.datasource.fluent.sources import _SourceFactories
-
-
-@pytest.fixture
-def invalid_datasource_factory() -> (
-    Callable[[dict[Literal["name", "type", "assets"] | Any, Any]], InvalidDatasource]
-):
-    def _invalid_ds_fct(config: dict) -> InvalidDatasource:
-        ds_type: type[Datasource] = _SourceFactories.type_lookup[config["type"]]
-        try:
-            ds_type(**config)
-        except pydantic.ValidationError as config_error:
-            return InvalidDatasource(**config, config_error=config_error)
-        raise ValueError("The Datasource was valid")
-
-    return _invalid_ds_fct
-
 
 _EXCLUDE_METHODS: Final[set[str]] = {
     # we don't care about overriding these methods for InvalidDatasource
@@ -61,6 +49,96 @@ def test_public_methods_are_overridden(base_ds_method_name: str):
         m for m in InvalidDatasource.__dict__.keys() if not m.startswith("_")
     ]
     assert base_ds_method_name in invalid_ds_public_attributes
+
+
+class InvalidDSFn(Protocol):
+    """
+    Accept a datasource config and return an InvalidDatasource instance.
+    Raises an error if the config was valid.
+    """
+
+    def __call__(
+        self, config: dict[Literal["name", "type", "assets"] | Any, Any]
+    ) -> InvalidDatasource:
+        ...
+
+
+@pytest.fixture
+def invalid_datasource_factory() -> InvalidDSFn:
+    def _invalid_ds_fct(config: dict) -> InvalidDatasource:
+        ds_type: type[Datasource] = _SourceFactories.type_lookup[config["type"]]
+        try:
+            ds_type(**config)
+        except pydantic.ValidationError as config_error:
+            return InvalidDatasource(**config, config_error=config_error)
+        raise ValueError("The Datasource was valid")
+
+    return _invalid_ds_fct
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "invalid_ds_cfg",
+    [
+        pytest.param(
+            {
+                "name": "my pg",
+                "type": "postgres",
+                "connection_string": "postmalone+psycopg2://postgres:@localhost/test_database",
+            },
+            id="invalid conn str",
+        ),
+        pytest.param(
+            {
+                "name": "my pg + asset",
+                "type": "postgres",
+                "connection_string": "postgresql+psycopg2://postgres:@localhost/test_database",
+                "assets": [
+                    {
+                        "name": "my_table",
+                        "type": "table",
+                        "query": "table assets don't have a query",
+                    }
+                ],
+            },
+            id="invalid asset",
+        ),
+        pytest.param(
+            {
+                "name": "my snowflake",
+                "type": "snowflake",
+                "connection_string": "${MY_CONN_STR}",
+                "user": "invalid_extra_field",
+            },
+            id="extra field",
+        ),
+    ],
+)
+class TestInvalidDatasource:
+    def test_connection_raises_informative_error(
+        self,
+        invalid_ds_cfg: dict,
+        invalid_datasource_factory: InvalidDSFn,
+    ):
+        invalid_ds = invalid_datasource_factory(invalid_ds_cfg)
+        print(invalid_ds)
+
+        with pytest.raises(TestConnectionError) as conn_err:
+            invalid_ds.test_connection()
+        print(f"{conn_err.value!r}\n >-- caused by -->\n{conn_err.value.__cause__!r}")
+        assert invalid_ds.config_error == conn_err.value.__cause__
+
+    def test_get_batch_list_raises_informative_error(
+        self,
+        invalid_ds_cfg: dict,
+        invalid_datasource_factory: Callable[
+            [dict[Literal["name", "type", "assets"] | Any, Any]], InvalidDatasource
+        ],
+    ):
+        invalid_ds = invalid_datasource_factory(invalid_ds_cfg)
+        with pytest.raises(TypeError) as err:
+            invalid_ds.get_batch_list_from_batch_request({})
+        assert invalid_ds.config_error == err.value.__cause__
 
 
 if __name__ == "__main__":
