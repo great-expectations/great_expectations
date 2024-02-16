12650731c0fa0e782d15a3d07dcc001a073f31a1
Nathan Farmer <nathan@greatexpectations.io>
2024-02-16 15:41:59 -0500

    Make type checker happy

--- great_expectations/compatibility/py4j.py
+++ great_expectations/compatibility/py4j.py
@@ -3,6 +3,6 @@ from great_expectations.compatibility.not_imported import NotImported
 PY4J_NOT_IMPORTED = NotImported("py4j is not installed, please 'pip install py4j'")
 
 try:
-    from py4j import protocol
+    from py4j import protocol  # type: ignore[import-untyped]
 except ImportError:
     protocol = PY4J_NOT_IMPORTED
