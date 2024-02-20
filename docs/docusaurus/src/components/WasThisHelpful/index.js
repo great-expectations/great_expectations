c1d6d543040ac91792f57a1bedef3abd24b0044f
Deborah Niesz <deborah.niesz@10pines.com>
2024-02-20 11:17:56 -0300

    Changes in reaction buttons

--- docs/docusaurus/src/components/WasThisHelpful/index.js
+++ docs/docusaurus/src/components/WasThisHelpful/index.js
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './styles.module.css';
-import useBaseUrl from "@docusaurus/useBaseUrl";
 import {useLocation} from "@docusaurus/router";
 
 export default function WasThisHelpful(){
 
-    const thumbsUpImg = useBaseUrl(`img/thumbs_up_icon.svg`);
-    const thumbsDownImg = useBaseUrl(`img/thumbs_down_icon.svg`);
-
     const { pathname } = useLocation();
+    const [buttonsClicked, setButtonsClicked] = useState(false)
 
     const handleThumbsUp = () => {
+        setButtonsClicked(true)
         posthog.capture('test_docs.thumbs_up', { doc_url: pathname })
     };
 
     const handleThumbsDown = () => {
+        setButtonsClicked(true)
         posthog.capture('test_docs.thumbs_down', { doc_url: pathname })
     };
 
     return <div className={styles.feedbackCard}>
             <h3 className={styles.feedbackCardTitle}>Was this helpful?</h3>
-            <div className={styles.feedbackCardBody}>
-                <img src={thumbsUpImg} className={styles.feedbackIcon}
-                     alt="Thumbs up icon" onClick={handleThumbsUp}/>
-                <img src={thumbsDownImg} className={styles.feedbackIcon}
-                     alt="Thumbs down icon" onClick={handleThumbsDown}/>
+            <div className={styles.feedbackCardActions}>
+                <button className={buttonsClicked ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={handleThumbsUp}>Yes</button>
+                <button className={buttonsClicked ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={handleThumbsDown}>No</button>
             </div>
         </div>
 }
