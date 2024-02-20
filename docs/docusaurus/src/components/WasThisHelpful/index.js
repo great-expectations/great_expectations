6ebf888b6f522415fabca27ac3f07e3213214ab7
Deborah Niesz <deborah.niesz@10pines.com>
2024-02-20 12:09:55 -0300

    Disabling buttons

--- docs/docusaurus/src/components/WasThisHelpful/index.js
+++ docs/docusaurus/src/components/WasThisHelpful/index.js
@@ -5,23 +5,20 @@ import {useLocation} from "@docusaurus/router";
 export default function WasThisHelpful(){
 
     const { pathname } = useLocation();
-    const [buttonsClicked, setButtonsClicked] = useState(false)
+    const [feedbackSent, setFeedbackSent] = useState(false)
 
-    const handleThumbsUp = () => {
-        setButtonsClicked(true)
-        posthog.capture('test_docs.thumbs_up', { doc_url: pathname })
-    };
-
-    const handleThumbsDown = () => {
-        setButtonsClicked(true)
-        posthog.capture('test_docs.thumbs_down', { doc_url: pathname })
+    const handleFeedbackReaction = (eventName) => {
+        if(!feedbackSent){
+            setFeedbackSent(true)
+            posthog.capture(eventName, { doc_url: pathname })
+        }
     };
 
     return <div className={styles.feedbackCard}>
             <h3 className={styles.feedbackCardTitle}>Was this helpful?</h3>
             <div className={styles.feedbackCardActions}>
-                <button className={buttonsClicked ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={handleThumbsUp}>Yes</button>
-                <button className={buttonsClicked ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={handleThumbsDown}>No</button>
+                <button className={feedbackSent ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={() => handleFeedbackReaction('test_docs.thumbs_up')}>Yes</button>
+                <button className={feedbackSent ? styles.inactiveFeedbackButton : styles.feedbackButton} onClick={() => handleFeedbackReaction('test_docs.thumbs_down')}>No</button>
             </div>
         </div>
 }
