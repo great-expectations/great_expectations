7accde9acf7c3ad7efd1176da2fb3aa129e0114b
Kyle Eaton <kyle@superconductivehealth.com>
2024-02-21 11:45:02 -0500

    Update docs/docusaurus/docusaurus.config.js
    Co-authored-by: Rob Gray <104205257+kwcanuck@users.noreply.github.com>

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -37,7 +37,7 @@ module.exports = {
     announcementBar: {
       id: 'cta_bar',
       content:
-        '<a style="font-weight:600" href="https://hubs.li/Q02lHZ_j0">Sign up for our hands-on workshop for Snowflake users or joing the waitlist for future workshops.</a>',
+        '<a style="font-weight:600" href="https://hubs.li/Q02lHZ_j0">Sign up for our Snowflake workshop, or join the waitlist for future workshops.</a>',
       backgroundColor: '#141432',
       textColor: '#fff',
       isCloseable: true,
