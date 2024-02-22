ccfc32e21592690eae708fc8b1f44ed586b4adc1
Chetan Kini <chetan@superconductive.com>
2024-02-22 12:25:45 -0500

    use pydantic model

--- great_expectations/core/validation.py
+++ great_expectations/core/validation.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from great_expectations.compatibility.pydantic import BaseModel
+
 if TYPE_CHECKING:
     from great_expectations.core.batch_config import BatchConfig
     from great_expectations.core.expectation_suite import ExpectationSuite
     from great_expectations.datasource.fluent.interfaces import DataAsset
 
 
-class Validation:
+class Validation(BaseModel):
     """
     Responsible for running a suite against data and returning a validation result.
 
@@ -19,27 +21,9 @@ class Validation:
 
     """
 
-    def __init__(
-        self,
-        name: str,
-        data: DataAsset | BatchConfig,
-        suite: ExpectationSuite,
-    ) -> None:
-        self._name = name
-        self._data = data
-        self._suite = suite
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def data(self) -> DataAsset | BatchConfig:
-        return self._data
-
-    @property
-    def suite(self) -> ExpectationSuite:
-        return self._suite
+    name: str
+    data: DataAsset | BatchConfig
+    suite: ExpectationSuite
 
     def run(self):
         raise NotImplementedError
