e6e777c4be1005761548c26e1c98278301ab6e90
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-02-22 23:39:46 -0500

    add partitioners to public api

--- great_expectations/core/partitioners.py
+++ great_expectations/core/partitioners.py
@@ -1,18 +1,22 @@
 from typing import List, Literal, Union
 
+from great_expectations._docs_decorators import public_api
 from great_expectations.compatibility import pydantic
 
 
+@public_api
 class PartitionerYear(pydantic.BaseModel):
     column_name: str
     method_name: Literal["partition_on_year"] = "partition_on_year"
 
 
+@public_api
 class PartitionerYearAndMonth(pydantic.BaseModel):
     column_name: str
     method_name: Literal["partition_on_year_and_month"] = "partition_on_year_and_month"
 
 
+@public_api
 class PartitionerYearAndMonthAndDay(pydantic.BaseModel):
     column_name: str
     method_name: Literal[
@@ -20,12 +24,14 @@ class PartitionerYearAndMonthAndDay(pydantic.BaseModel):
     ] = "partition_on_year_and_month_and_day"
 
 
+@public_api
 class PartitionerDatetimePart(pydantic.BaseModel):
     datetime_parts: List[str]
     column_name: str
     method_name: Literal["partition_on_date_parts"] = "partition_on_date_parts"
 
 
+@public_api
 class PartitionerDividedInteger(pydantic.BaseModel):
     divisor: int
     column_name: str
@@ -34,17 +40,20 @@ class PartitionerDividedInteger(pydantic.BaseModel):
     ] = "partition_on_divided_integer"
 
 
+@public_api
 class PartitionerModInteger(pydantic.BaseModel):
     mod: int
     column_name: str
     method_name: Literal["partition_on_mod_integer"] = "partition_on_mod_integer"
 
 
+@public_api
 class PartitionerColumnValue(pydantic.BaseModel):
     column_name: str
     method_name: Literal["partition_on_column_value"] = "partition_on_column_value"
 
 
+@public_api
 class PartitionerMultiColumnValue(pydantic.BaseModel):
     column_names: List[str]
     method_name: Literal[
@@ -52,6 +61,7 @@ class PartitionerMultiColumnValue(pydantic.BaseModel):
     ] = "partition_on_multi_column_values"
 
 
+@public_api
 class PartitionerConvertedDatetime(pydantic.BaseModel):
     column_name: str
     method_name: Literal[
