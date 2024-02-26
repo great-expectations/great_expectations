ce48110f2df1f574626ecf2d5ad66ca589ed4fd3
Gabriel <gabriel59kg@gmail.com>
2024-02-26 16:11:51 -0500

    [MAINTENANCE] `ruff` `0.2.2` (#9538)

--- great_expectations/expectations/core/expect_column_values_to_not_match_regex_list.py
+++ great_expectations/expectations/core/expect_column_values_to_not_match_regex_list.py
@@ -133,7 +133,7 @@ class ExpectColumnValuesToNotMatchRegexList(ColumnMapExpectation):
             + values_string
         )
 
-        if params.mostly and params.mostly.value < 1.0:  # noqa: PLR2004
+        if params.mostly and params.mostly.value < 1.0:
             renderer_configuration = cls._add_mostly_pct_param(
                 renderer_configuration=renderer_configuration
             )
@@ -182,7 +182,7 @@ class ExpectColumnValuesToNotMatchRegexList(ColumnMapExpectation):
             + values_string
         )
 
-        if params["mostly"] is not None and params["mostly"] < 1.0:  # noqa: PLR2004
+        if params["mostly"] is not None and params["mostly"] < 1.0:
             params["mostly_pct"] = num_to_str(
                 params["mostly"] * 100, no_scientific=True
             )
