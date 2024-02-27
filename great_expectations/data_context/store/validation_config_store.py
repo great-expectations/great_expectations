ddddc8d77f35d6d400360fa053e7700a012fda00
Chetan Kini <chetan@superconductive.com>
2024-02-27 11:50:08 -0500

    simplify store

--- great_expectations/data_context/store/validation_config_store.py
+++ great_expectations/data_context/store/validation_config_store.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from great_expectations.core.data_context_key import DataContextKey, StringKey
+from great_expectations.compatibility.typing_extensions import override
+from great_expectations.core.data_context_key import StringKey
 from great_expectations.core.validation_config import ValidationConfig
 from great_expectations.data_context.cloud_constants import GXCloudRESTResource
 from great_expectations.data_context.store.store import Store
@@ -29,18 +30,17 @@ class ValidationConfigStore(Store):
     ) -> GXCloudIdentifier | StringKey:
         """Given a name and optional ID, build the correct key for use in the ValidationConfigStore."""
         if self.cloud_mode:
-            key = GXCloudIdentifier(
+            return GXCloudIdentifier(
                 resource_type=GXCloudRESTResource.VALIDATION_CONFIG,
                 id=id,
                 resource_name=name,
             )
-        else:
-            key = StringKey(key=name)
-        return key
+        return StringKey(key=name)
 
-    def _add(self, key: DataContextKey, value: ValidationConfig, **kwargs) -> None:
-        return super()._add(key=key, value=value.json(), **kwargs)
+    @override
+    def serialize(self, value):
+        return value.json()
 
-    def get(self, key: DataContextKey) -> ValidationConfig:
-        validation_json_str = super().get(key)
-        return ValidationConfig.parse_raw(validation_json_str)
+    @override
+    def deserialize(self, value):
+        return ValidationConfig.parse_raw(value)
