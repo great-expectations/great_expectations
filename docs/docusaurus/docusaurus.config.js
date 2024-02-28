b4686a6fb0a4dcad64e40bb3d2c1b794c13bffe2
JessSaavedra <jessicasaavedraval@gmail.com>
2024-02-28 18:30:18 -0300

    Change implementation of code snippets icons to be purely CSS

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -2,7 +2,6 @@
 
 const remarkNamedSnippets = require('./scripts/remark-named-snippets/index');
 const remarkCodeImport = require('remark-code-import');
-const titleBasedIcon = require('./scripts/title-based-icon/index');
 
 require('dotenv').config()
 
@@ -272,7 +271,7 @@ module.exports = {
           includeCurrentVersion: true,
           sidebarPath: require.resolve('./sidebars.js'),
           // Note: remarkCodeImport is included to handle earlier versions with line number references (e.g. v0.14.13)
-          remarkPlugins: [remarkNamedSnippets, remarkCodeImport, titleBasedIcon],
+          remarkPlugins: [remarkNamedSnippets, remarkCodeImport],
           // versions needs to map major.minor -> major.minor.patch for display purposes. Update the patch as needed.
           versions: {
             current: {
