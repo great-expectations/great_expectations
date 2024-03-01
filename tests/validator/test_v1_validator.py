3c08170b8571b7e41967f742764096443e2adda8
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-03-01 14:33:12 -0500

    [MAINTENANCE] Remove context from v1 Validator and add helper to project manager

--- tests/validator/test_v1_validator.py
+++ tests/validator/test_v1_validator.py
@@ -87,7 +87,6 @@ def validator(
     fds_data_context: AbstractDataContext, batch_config: BatchConfig
 ) -> Validator:
     return Validator(
-        context=fds_data_context,
         batch_config=batch_config,
         batch_request_options=None,
         result_format=ResultFormat.SUMMARY,
@@ -166,7 +165,6 @@ def test_validate_expectation_with_batch_asset_options(
 ):
     desired_event_type = "start"
     validator = Validator(
-        context=fds_data_context,
         batch_config=batch_config_with_event_type_partitioner,
         batch_request_options={"event_type": desired_event_type},
     )
