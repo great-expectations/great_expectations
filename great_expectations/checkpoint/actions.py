cb036c4b05b12a10acedb16d29676e0c6f531e94
Chetan Kini <chetan@superconductive.com>
2024-03-14 11:47:48 -0400

    update docstring

--- great_expectations/checkpoint/actions.py
+++ great_expectations/checkpoint/actions.py
@@ -197,7 +197,7 @@ class DataDocsAction(ValidationAction):
 
 @public_api
 class SlackNotificationAction(DataDocsAction):
-    """Sends a Slack notification to a given webhook.
+    r"""Sends a Slack notification to a given webhook.
 
     ```yaml
     - name: send_slack_notification_on_validation_result
@@ -223,8 +223,10 @@ class SlackNotificationAction(DataDocsAction):
     Args:
         renderer: Specifies the Renderer used to generate a query consumable by Slack API, e.g.:
            ```python
+           \{
             "module_name": "great_expectations.render.renderer.slack_renderer",
             "class_name": "SlackRenderer",
+           \}
            ```
         slack_webhook: The incoming Slack webhook to which to send notification.
         slack_token: Token from Slack app. Used when not using slack_webhook.
