80268b21ade2633b601b2ca32a87b6c6f39fdd25
JessSaavedra <jessicasaavedraval@gmail.com>
2024-03-20 16:30:50 -0300

    Add tests for structural markdown syntax fixes

--- docs/sphinx_api_docs_source/build_sphinx_api_docs.py
+++ docs/sphinx_api_docs_source/build_sphinx_api_docs.py
@@ -30,6 +30,7 @@ from typing import Dict
 from urllib.parse import urlparse
 
 import invoke
+from docs.sphinx_api_docs_source.utils import apply_markdown_adjustments
 
 from docs.sphinx_api_docs_source.check_public_api_docstrings import (
     get_public_api_definitions,
@@ -218,45 +219,7 @@ class SphinxInvokeDocsBuilder:
         title_str = title.get_text(strip=True)
         title_str = title_str.replace("#", "")
 
-        # Add newline before closing dt tags when they have more than one child
-        for item in soup.find_all("dt"):
-            lengthChildren = len(item.findChildren())
-            if (
-                "expectation" in str(html_file_path)
-                and "Relevant Documentation Links" in item.get_text()
-            ):
-                item.string.replaceWith(item.get_text(strip=True))
-            if lengthChildren > 1:
-                item.append("\r\n")
-
-        # Add newline before closing th, td and li tags
-        for item in soup.find_all(["th", "td", "li"]):
-            item.append("\r\n")
-
-        # Add newline after opening pre, dd and li tags
-        for item in soup.find_all(["pre", "dd", "li"]):
-            item.insert(0, "\r\n")
-
-        # Add newline before opening dd and p tags
-        for item in soup.find_all(["dd", "p"]):
-            item.insert_before("\r\n")
-
-        # Add newline before closing cite tag
-        if "ConfiguredAssetFilesystemDataConnector" in str(html_file_path):
-            for item in soup.find_all("cite"):
-                item.append("\r\n")
-
-        # Replace asterisk character with corresponding HTML entity
-        for item in soup.find_all("span"):
-            if item and item.string and "*" in item.get_text():
-                item.string.replaceWith(item.get_text().replace("*", "&#42;"))
-
-        # Add newline after opening p tag and before closing p tags when they have children
-        for item in soup.find_all("p"):
-            lengthChildren = len(item.findChildren())
-            if lengthChildren > 0:
-                item.insert(0, "\r\n")
-                item.append("\r\n")
+        apply_markdown_adjustments(soup, html_file_path, html_file_contents)
 
         sidebar_entry = self._get_sidebar_entry(html_file_path=html_file_path)
 
