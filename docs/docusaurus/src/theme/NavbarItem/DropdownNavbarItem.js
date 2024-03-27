f3cc19da92fda998c255a7576d26c89665d15231
Deborah Niesz <deborah.niesz@10pines.com>
2024-03-27 16:54:52 -0300

    Rollback DropdownNavbarItem.js

--- docs/docusaurus/src/theme/NavbarItem/DropdownNavbarItem.js
+++ docs/docusaurus/src/theme/NavbarItem/DropdownNavbarItem.js
@@ -3,8 +3,8 @@ import DropdownNavbarItem from '@theme-original/NavbarItem/DropdownNavbarItem';
 
 export default function DropdownNavbarItemWrapper(props) {
   return (
-     // The href is overwritten to prevent default redirect when clicking on the label of the dropdown
-     // With the new href the user will stay in the current page even if they click it
-     <DropdownNavbarItem {...props} href={'#'} />
+    // The href is overwritten to prevent default redirect when clicking on the label of the dropdown
+    // With the new href the user will stay in the current page even if they click it
+    <DropdownNavbarItem {...props} href={'#'} />
   );
 }
