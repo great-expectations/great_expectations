614f77c4a570ba6f7abc17a64a856f748266d7ad
Deborah <38264052+deborahniesz@users.noreply.github.com>
2024-03-27 18:13:19 -0300

    Refactor of handleMobileDocSidebarItemClick
    Co-authored-by: Jose Tobar <jose@greatexpectations.io>

--- docs/docusaurus/src/theme/DocSidebarItems/index.js
+++ docs/docusaurus/src/theme/DocSidebarItems/index.js
@@ -5,11 +5,12 @@ import {useNavbarMobileSidebar} from "@docusaurus/theme-common/internal";
 export default function DocSidebarItemsWrapper(props) {
     const mobileSidebar = useNavbarMobileSidebar();
     const handleMobileDocSidebarItemClick = (item) => {
+        if (!props.onItemClick) return
         if (item.type === 'link') {
             mobileSidebar.toggle();
         }
     }
   return (
-      <DocSidebarItems {...props} onItemClick={props.onItemClick ? handleMobileDocSidebarItemClick : null} />
+      <DocSidebarItems {...props} onItemClick={handleMobileDocSidebarItemClick} />
   );
 }
