765c86b1aae87083df6943fe9ef307b56891aa90
Rachel-Reverie <94694058+Rachel-Reverie@users.noreply.github.com>
2024-03-28 12:19:25 -0500

    Update docs/docusaurus/docs/core/validate_data/checkpoints/_examples/get_checkpoint_by_name.py
    Co-authored-by: Rob Gray <104205257+kwcanuck@users.noreply.github.com>

--- docs/docusaurus/docs/core/validate_data/checkpoints/_examples/get_checkpoint_by_name.py
+++ docs/docusaurus/docs/core/validate_data/checkpoints/_examples/get_checkpoint_by_name.py
@@ -3,7 +3,7 @@ This example script demonstrates how to retrieve an existing Checkpoint
  from a Data Context by name.
 
 The <snippet> tags are used to insert the corresponding code into the
- Great Expectations documentation.  They can be disregarded by anyone
+  GX documentation, and you can disregard them.
  reviewing this script.
 """
 
