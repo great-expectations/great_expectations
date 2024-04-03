45b4a09c41e0af2e01440f7d3f2fd846f68008d2
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-04-03 08:24:42 -0400

    type ignore

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -61,7 +61,7 @@ def parse_requirements_files_to_specs(
         abs_path = req_file.absolute().as_posix()
         key = abs_path.rsplit(os.path.sep, 1)[-1]
         with open(req_file) as f:
-            req_set_dict[key] = {line.name: line.specs for line in rp.parse(f) if line.specs}
+            req_set_dict[key] = {line.name: line.specs for line in rp.parse(f) if line.specs}  # type: ignore[misc]
 
     return req_set_dict
 
