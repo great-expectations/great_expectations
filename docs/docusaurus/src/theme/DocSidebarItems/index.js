39759cc6679fba8726a0a222dec8943b7af49674
Deborah Niesz <deborah.niesz@10pines.com>
2024-04-11 15:43:39 -0300

    Fix overlay bug on desktop

--- docs/docusaurus/src/theme/DocSidebarItems/index.js
+++ docs/docusaurus/src/theme/DocSidebarItems/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import DocSidebarItems from '@theme-original/DocSidebarItems';
 import {useNavbarMobileSidebar} from "@docusaurus/theme-common/internal";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function DocSidebarItemsWrapper(props) {
     const mobileSidebar = useNavbarMobileSidebar();
     const handleMobileDocSidebarItemClick = (item) => {
-        if (!props.onItemClick) return
+        if(window.innerWidth > 996) return;
+        if (!props.onItemClick) return;
         if (item.type === 'link') {
             mobileSidebar.toggle();
         }
     }
   return (
-      <DocSidebarItems {...props} onItemClick={handleMobileDocSidebarItemClick} />
+      <BrowserOnly>{() => <DocSidebarItems {...props} onItemClick={handleMobileDocSidebarItemClick} />}</BrowserOnly>
   );
 }
