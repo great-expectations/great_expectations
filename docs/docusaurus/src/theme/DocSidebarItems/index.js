03e907cc8f66ce72eabfd4731d0f0dcccbda2a41
Deborah Niesz <deborah.niesz@10pines.com>
2024-04-11 16:15:19 -0300

    add mobile breakpoint constant

--- docs/docusaurus/src/theme/DocSidebarItems/index.js
+++ docs/docusaurus/src/theme/DocSidebarItems/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import DocSidebarItems from '@theme-original/DocSidebarItems';
 import {useNavbarMobileSidebar} from "@docusaurus/theme-common/internal";
 import BrowserOnly from "@docusaurus/BrowserOnly";
+const MOBILE_BREAKPOINT = 996;
 
 export default function DocSidebarItemsWrapper(props) {
     const mobileSidebar = useNavbarMobileSidebar();
+
     const handleMobileDocSidebarItemClick = (item) => {
-        if(window.innerWidth > 996) return;
+        if(window.innerWidth > MOBILE_BREAKPOINT) return;
         if (!props.onItemClick) return;
         if (item.type === 'link') {
             mobileSidebar.toggle();
