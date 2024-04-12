aced2789ad0a4452365c764257facaa450c18412
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-04-12 11:11:24 -0400

    add fixtures

--- tests/datasource/fluent/data_asset/test_sql_asset.py
+++ tests/datasource/fluent/data_asset/test_sql_asset.py
@@ -1,4 +1,4 @@
-from unittest.mock import create_autospec
+from unittest.mock import Mock, create_autospec  # noqa: TID251
 
 import pytest
 
@@ -12,80 +12,74 @@ from great_expectations.datasource.fluent import SQLDatasource
 from great_expectations.datasource.fluent.sql_datasource import _SQLAsset
 
 
-@pytest.mark.unit
-def test_add_batch_definition_fluent_sql__add_batch_definition_whole_table():
-    # arrange
-    mock_datasource = create_autospec(SQLDatasource)
+@pytest.fixture
+def datasource() -> Mock:
+    return create_autospec(SQLDatasource)
+
+
+@pytest.fixture
+def asset(datasource) -> _SQLAsset:
     asset = _SQLAsset(
         name="test_asset",
     )
-    asset._datasource = mock_datasource  # same pattern Datasource uses to init Asset
+    asset._datasource = datasource  # same pattern Datasource uses to init Asset
+    return asset
+
+
+@pytest.mark.unit
+def test_add_batch_definition_fluent_sql__add_batch_definition_whole_table(datasource: Mock, asset):
+    # arrange
     name = "batch_def_name"
     expected_batch_definition = BatchDefinition(name=name, partitioner=None, batching_regex=None)
-    mock_datasource.add_batch_definition.return_value = expected_batch_definition
+    datasource.add_batch_definition.return_value = expected_batch_definition
 
     # act
     batch_definition = asset.add_batch_definition_whole_table(name=name)
 
     # assert
     assert batch_definition == expected_batch_definition
-    mock_datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
+    datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
 
 
 @pytest.mark.unit
-def test_add_batch_definition_fluent_sql__add_batch_definition_yearly():
+def test_add_batch_definition_fluent_sql__add_batch_definition_yearly(datasource: Mock, asset):
     # arrange
-    mock_datasource = create_autospec(SQLDatasource)
-    asset = _SQLAsset(
-        name="test_asset",
-    )
-    asset._datasource = mock_datasource  # same pattern Datasource uses to init Asset
     name = "batch_def_name"
     column = "test_column"
     expected_batch_definition = BatchDefinition(
         name=name, partitioner=PartitionerYear(column_name=column), batching_regex=None
     )
-    mock_datasource.add_batch_definition.return_value = expected_batch_definition
+    datasource.add_batch_definition.return_value = expected_batch_definition
 
     # act
     batch_definition = asset.add_batch_definition_yearly(name=name, column=column)
 
     # assert
     assert batch_definition == expected_batch_definition
-    mock_datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
+    datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
 
 
 @pytest.mark.unit
-def test_add_batch_definition_fluent_sql__add_batch_definition_monthly():
+def test_add_batch_definition_fluent_sql__add_batch_definition_monthly(datasource: Mock, asset):
     # arrange
-    mock_datasource = create_autospec(SQLDatasource)
-    asset = _SQLAsset(
-        name="test_asset",
-    )
-    asset._datasource = mock_datasource  # same pattern Datasource uses to init Asset
     name = "batch_def_name"
     column = "test_column"
     expected_batch_definition = BatchDefinition(
         name=name, partitioner=PartitionerYearAndMonth(column_name=column), batching_regex=None
     )
-    mock_datasource.add_batch_definition.return_value = expected_batch_definition
+    datasource.add_batch_definition.return_value = expected_batch_definition
 
     # act
     batch_definition = asset.add_batch_definition_monthly(name=name, column=column)
 
     # assert
     assert batch_definition == expected_batch_definition
-    mock_datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
+    datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
 
 
 @pytest.mark.unit
-def test_add_batch_definition_fluent_sql__add_batch_definition_daily():
+def test_add_batch_definition_fluent_sql__add_batch_definition_daily(datasource: Mock, asset):
     # arrange
-    mock_datasource = create_autospec(SQLDatasource)
-    asset = _SQLAsset(
-        name="test_asset",
-    )
-    asset._datasource = mock_datasource  # same pattern Datasource uses to init Asset
     name = "batch_def_name"
     column = "test_column"
     expected_batch_definition = BatchDefinition(
@@ -93,11 +87,11 @@ def test_add_batch_definition_fluent_sql__add_batch_definition_daily():
         partitioner=PartitionerYearAndMonthAndDay(column_name=column),
         batching_regex=None,
     )
-    mock_datasource.add_batch_definition.return_value = expected_batch_definition
+    datasource.add_batch_definition.return_value = expected_batch_definition
 
     # act
     batch_definition = asset.add_batch_definition_daily(name=name, column=column)
 
     # assert
     assert batch_definition == expected_batch_definition
-    mock_datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
+    datasource.add_batch_definition.assert_called_once_with(expected_batch_definition)
