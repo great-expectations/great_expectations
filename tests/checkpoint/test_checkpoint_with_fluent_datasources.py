a1c9d38038f7b1a38df68855a4da5dde115dbcb7
Chetan Kini <chetan@superconductive.com>
2024-04-18 12:29:11 -0400

    delete tests

--- tests/checkpoint/test_checkpoint_with_fluent_datasources.py
+++ tests/checkpoint/test_checkpoint_with_fluent_datasources.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 import copy
 import logging
-import pickle
-from typing import TYPE_CHECKING, Optional
 
 import pytest
 
@@ -12,7 +10,6 @@ from great_expectations.checkpoint import Checkpoint
 from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 from great_expectations.core import (
     ExpectationSuite,
-    ExpectationSuiteValidationResult,
 )
 from great_expectations.core.expectation_validation_result import (
     ExpectationValidationResult,
@@ -21,7 +18,6 @@ from great_expectations.core.yaml_handler import YAMLHandler
 from great_expectations.data_context import FileDataContext
 from great_expectations.data_context.types.base import (
     CheckpointConfig,
-    checkpointConfigSchema,
 )
 from great_expectations.data_context.types.resource_identifiers import (
     ConfigurationIdentifier,
@@ -31,11 +27,6 @@ from great_expectations.datasource.fluent.batch_request import (
     BatchRequest as FluentBatchRequest,
 )
 from great_expectations.render import RenderedAtomicContent
-from great_expectations.validator.validator import Validator
-
-if TYPE_CHECKING:
-    from great_expectations.core.data_context_key import DataContextKey
-
 
 yaml = YAMLHandler()
 
@@ -72,136 +63,6 @@ def test_newstyle_checkpoint_instantiates_and_produces_a_validation_result_when_
 
     assert len(data_context.validation_results_store.list_keys()) == 1
     assert result["success"]
-
-
-@pytest.mark.filesystem
-def test_newstyle_checkpoint_instantiates_and_produces_a_validation_result_with_checkpoint_name_in_meta_when_run(  # noqa: E501
-    titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled,
-    store_validation_result_action,
-):
-    data_context: FileDataContext = titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled  # noqa: E501
-    checkpoint_name: str = "test_checkpoint_name"
-    # add checkpoint config
-    checkpoint_config = CheckpointConfig(
-        name=checkpoint_name,
-        expectation_suite_name="my_expectation_suite",
-        action_list=[
-            store_validation_result_action,
-        ],
-        validations=[
-            {
-                "batch_request": {
-                    "datasource_name": "my_pandas_filesystem_datasource",
-                    "data_asset_name": "users",
-                },
-            },
-        ],
-    )
-    checkpoint_config_key = ConfigurationIdentifier(configuration_key=checkpoint_config.name)
-    data_context.checkpoint_store.set(key=checkpoint_config_key, value=checkpoint_config)
-    checkpoint: Checkpoint = data_context.get_legacy_checkpoint(checkpoint_config.name)
-
-    assert len(data_context.validation_results_store.list_keys()) == 0
-
-    data_context.suites.add(ExpectationSuite("my_expectation_suite"))
-    result: CheckpointResult = checkpoint.run()
-
-    assert len(data_context.validation_results_store.list_keys()) == 1
-    assert result["success"]
-
-    validation_result_identifier: DataContextKey = (
-        data_context.validation_results_store.list_keys()[0]
-    )
-    validation_result: ExpectationSuiteValidationResult = data_context.validation_results_store.get(
-        validation_result_identifier
-    )
-
-    assert "checkpoint_name" in validation_result.meta
-    assert validation_result.meta["checkpoint_name"] == checkpoint_name
-
-
-@pytest.mark.filesystem
-@pytest.mark.slow  # 1.15s
-def test_newstyle_checkpoint_instantiates_and_produces_a_validation_result_when_run_with_validator_specified_in_constructor(  # noqa: E501
-    titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled,
-    common_action_list,
-    fluent_batch_request,
-):
-    data_context: FileDataContext = titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled  # noqa: E501
-    batch_request: FluentBatchRequest = fluent_batch_request
-    data_context.suites.add(ExpectationSuite("my_expectation_suite"))
-    validator: Validator = data_context.get_validator(
-        batch_request=batch_request,
-        expectation_suite_name="my_expectation_suite",
-    )
-    checkpoint: Checkpoint = Checkpoint(
-        name="my_checkpoint",
-        data_context=data_context,
-        validator=validator,
-        action_list=common_action_list,
-    )
-
-    result = checkpoint.run()
-
-    assert len(data_context.validation_results_store.list_keys()) == 1
-    assert result["success"]
-
-
-@pytest.mark.filesystem
-@pytest.mark.slow  # 1.15s
-def test_newstyle_checkpoint_instantiates_and_produces_a_validation_result_when_run_with_validator_specified_in_run(  # noqa: E501
-    titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled,
-    common_action_list,
-    fluent_batch_request,
-):
-    data_context: FileDataContext = titanic_data_context_with_fluent_pandas_datasources_with_checkpoints_v1_with_empty_store_stats_enabled  # noqa: E501
-    batch_request: FluentBatchRequest = fluent_batch_request
-    data_context.suites.add(ExpectationSuite("my_expectation_suite"))
-    validator: Validator = data_context.get_validator(
-        batch_request=batch_request,
-        expectation_suite_name="my_expectation_suite",
-    )
-    checkpoint: Checkpoint = Checkpoint(
-        name="my_checkpoint",
-        data_context=data_context,
-        expectation_suite_name="my_expectation_suite",
-        action_list=common_action_list,
-    )
-
-    result = checkpoint.run(
-        validator=validator,
-    )
-
-    assert len(data_context.validation_results_store.list_keys()) == 1
-    assert result["success"]
-
-
-@pytest.mark.slow  # 1.19s
-@pytest.mark.filesystem
-def test_newstyle_checkpoint_result_can_be_pickled(
-    titanic_data_context_with_fluent_pandas_datasources_stats_enabled_and_expectation_suite_with_one_expectation,
-    common_action_list,
-):
-    data_context: FileDataContext = titanic_data_context_with_fluent_pandas_datasources_stats_enabled_and_expectation_suite_with_one_expectation  # noqa: E501
-
-    batch_request: dict = {
-        "datasource_name": "my_pandas_filesystem_datasource",
-        "data_asset_name": "Titanic_1911",
-    }
-
-    # add checkpoint config
-    checkpoint_config: dict = {
-        "name": "my_checkpoint",
-        "expectation_suite_name": "my_expectation_suite",
-        "action_list": common_action_list,
-        "batch_request": batch_request,
-    }
-
-    data_context.add_checkpoint(**checkpoint_config)
-    checkpoint: Checkpoint = data_context.get_legacy_checkpoint(name="my_checkpoint")
-
-    result: CheckpointResult = checkpoint.run()
-    assert isinstance(pickle.dumps(result), bytes)
 
 
 @pytest.mark.slow  # 1.19s
@@ -287,131 +148,6 @@ def test_newstyle_checkpoint_result_validations_include_rendered_content_data_co
     for result in expectation_validation_result.results:
         for rendered_content in result.rendered_content:
             assert isinstance(rendered_content, RenderedAtomicContent)
-
-
-@pytest.mark.filesystem
-@pytest.mark.parametrize(
-    "checkpoint_config,expected_validation_id",
-    [
-        pytest.param(
-            CheckpointConfig(
-                name="my_checkpoint",
-                expectation_suite_name="my_expectation_suite",
-                action_list=[
-                    {
-                        "name": "store_validation_result",
-                        "action": {
-                            "class_name": "StoreValidationResultAction",
-                        },
-                    },
-                ],
-                validations=[
-                    {  # type: ignore[list-item]
-                        "batch_request": {
-                            "datasource_name": "my_pandas_filesystem_datasource",
-                            "data_asset_name": "Titanic_1911",
-                        },
-                    },
-                ],
-            ),
-            None,
-            id="no ids",
-        ),
-        pytest.param(
-            CheckpointConfig(
-                name="my_checkpoint",
-                default_validation_id="7e2bb5c9-cdbe-4c7a-9b2b-97192c55c95b",
-                expectation_suite_name="my_expectation_suite",
-                batch_request={
-                    "datasource_name": "my_pandas_filesystem_datasource",
-                    "data_asset_name": "Titanic_1911",
-                },
-                action_list=[
-                    {
-                        "name": "store_validation_result",
-                        "action": {
-                            "class_name": "StoreValidationResultAction",
-                        },
-                    },
-                ],
-                validations=[],
-            ),
-            "7e2bb5c9-cdbe-4c7a-9b2b-97192c55c95b",
-            id="default validation id",
-        ),
-        pytest.param(
-            CheckpointConfig(
-                name="my_checkpoint",
-                expectation_suite_name="my_expectation_suite",
-                action_list=[
-                    {
-                        "name": "store_validation_result",
-                        "action": {
-                            "class_name": "StoreValidationResultAction",
-                        },
-                    },
-                ],
-                validations=[
-                    {  # type: ignore[list-item]
-                        "id": "f22601d9-00b7-4d54-beb6-605d87a74e40",
-                        "batch_request": {
-                            "datasource_name": "my_pandas_filesystem_datasource",
-                            "data_asset_name": "Titanic_1911",
-                        },
-                    },
-                ],
-            ),
-            "f22601d9-00b7-4d54-beb6-605d87a74e40",
-            id="nested validation id",
-        ),
-        pytest.param(
-            CheckpointConfig(
-                name="my_checkpoint",
-                default_validation_id="7e2bb5c9-cdbe-4c7a-9b2b-97192c55c95b",
-                expectation_suite_name="my_expectation_suite",
-                action_list=[
-                    {
-                        "name": "store_validation_result",
-                        "action": {
-                            "class_name": "StoreValidationResultAction",
-                        },
-                    },
-                ],
-                validations=[
-                    {  # type: ignore[list-item]
-                        "id": "f22601d9-00b7-4d54-beb6-605d87a74e40",
-                        "batch_request": {
-                            "datasource_name": "my_pandas_filesystem_datasource",
-                            "data_asset_name": "Titanic_1911",
-                        },
-                    },
-                ],
-            ),
-            "f22601d9-00b7-4d54-beb6-605d87a74e40",
-            id="both default and nested validation id",
-        ),
-    ],
-)
-def test_checkpoint_run_adds_validation_ids_to_expectation_suite_validation_result_meta(
-    titanic_data_context_with_fluent_pandas_datasources_stats_enabled_and_expectation_suite_with_one_expectation: FileDataContext,  # noqa: E501
-    checkpoint_config: CheckpointConfig,
-    expected_validation_id: str,
-) -> None:
-    data_context: FileDataContext = titanic_data_context_with_fluent_pandas_datasources_stats_enabled_and_expectation_suite_with_one_expectation  # noqa: E501
-
-    checkpoint_config_dict: dict = checkpointConfigSchema.dump(checkpoint_config)
-    data_context.add_checkpoint(**checkpoint_config_dict)
-    checkpoint: Checkpoint = data_context.get_legacy_checkpoint(name="my_checkpoint")
-
-    result: CheckpointResult = checkpoint.run()
-
-    # Always have a single validation result based on the test's parametrization
-    validation_result: ExpectationValidationResult | dict = tuple(result.run_results.values())[0][  # type: ignore[assignment]
-        "validation_result"
-    ]
-
-    actual_validation_id: Optional[str] = validation_result.meta["validation_id"]  # type: ignore[union-attr]
-    assert expected_validation_id == actual_validation_id
 
 
 @pytest.mark.filesystem
