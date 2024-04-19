93121c8816a58e874db4f289d5f9047d8f249fcf
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-04-19 15:33:18 -0400

    Use none type

--- tests/validator/test_v1_validator.py
+++ tests/validator/test_v1_validator.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pprint import pformat as pf
-from typing import Any
+from typing import Type
 
 import pytest
 
@@ -69,7 +69,7 @@ def fds_data_asset_with_event_type_partitioner(
 def batch_definition(
     fds_data_asset: DataAsset,
 ) -> BatchDefinition:
-    batch_definition = BatchDefinition[Any](name="test_batch_definition")
+    batch_definition = BatchDefinition[Type[None]](name="test_batch_definition")
     batch_definition.set_data_asset(fds_data_asset)
     return batch_definition
 
