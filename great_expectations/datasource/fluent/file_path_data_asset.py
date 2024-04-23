dedc7a756fa32110161294a12e2085a4d41430db
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-04-23 15:10:48 -0400

    [MAINTENANCE] Remove Spark Partitioners (#9796)

--- great_expectations/datasource/fluent/file_path_data_asset.py
+++ great_expectations/datasource/fluent/file_path_data_asset.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import copy
 import logging
 import re
-from collections import Counter
 from pprint import pformat as pf
 from typing import (
     TYPE_CHECKING,
@@ -16,7 +15,6 @@ from typing import (
     Optional,
     Pattern,
     Set,
-    Type,
 )
 
 import great_expectations.exceptions as gx_exceptions
@@ -25,14 +23,6 @@ from great_expectations.compatibility import pydantic
 from great_expectations.compatibility.typing_extensions import override
 from great_expectations.core.partitioners import (
     Partitioner,
-    PartitionerColumnValue,
-    PartitionerDatetimePart,
-    PartitionerDividedInteger,
-    PartitionerModInteger,
-    PartitionerMultiColumnValue,
-    PartitionerYear,
-    PartitionerYearAndMonth,
-    PartitionerYearAndMonthAndDay,
 )
 from great_expectations.datasource.fluent.batch_request import (
     BatchParameters,
@@ -50,17 +40,6 @@ from great_expectations.datasource.fluent.interfaces import (
     DataAsset,
     DatasourceT,
     TestConnectionError,
-)
-from great_expectations.datasource.fluent.spark_generic_partitioners import (
-    SparkPartitioner,
-    SparkPartitionerColumnValue,
-    SparkPartitionerDatetimePart,
-    SparkPartitionerDividedInteger,
-    SparkPartitionerModInteger,
-    SparkPartitionerMultiColumnValue,
-    SparkPartitionerYear,
-    SparkPartitionerYearAndMonth,
-    SparkPartitionerYearAndMonthAndDay,
 )
 
 if TYPE_CHECKING:
@@ -134,20 +113,6 @@ class _FilePathDataAsset(DataAsset[DatasourceT, RegexPartitioner], Generic[Datas
     _data_connector: DataConnector = pydantic.PrivateAttr()
     # more specific `_test_connection_error_message` can be set inside `_build_data_connector()`
     _test_connection_error_message: str = pydantic.PrivateAttr("Could not connect to your asset")
-    _partitioner_implementation_map: Dict[Type[Partitioner], Type[SparkPartitioner]] = (
-        pydantic.PrivateAttr(
-            default={
-                PartitionerYear: SparkPartitionerYear,
-                PartitionerYearAndMonth: SparkPartitionerYearAndMonth,
-                PartitionerYearAndMonthAndDay: SparkPartitionerYearAndMonthAndDay,
-                PartitionerColumnValue: SparkPartitionerColumnValue,
-                PartitionerDatetimePart: SparkPartitionerDatetimePart,
-                PartitionerDividedInteger: SparkPartitionerDividedInteger,
-                PartitionerModInteger: SparkPartitionerModInteger,
-                PartitionerMultiColumnValue: SparkPartitionerMultiColumnValue,
-            }
-        )
-    )
 
     class Config:
         """
@@ -174,14 +139,6 @@ class _FilePathDataAsset(DataAsset[DatasourceT, RegexPartitioner], Generic[Datas
             self._regex_parser.get_all_group_index_to_group_name_mapping()
         )
         self._all_group_names = self._regex_parser.get_all_group_names()
-
-    def get_partitioner_implementation(self, abstract_partitioner: Partitioner) -> SparkPartitioner:
-        PartitionerClass = self._partitioner_implementation_map.get(type(abstract_partitioner))
-        if PartitionerClass is None:
-            raise ValueError(  # noqa: TRY003
-                f"Requested Partitioner `{abstract_partitioner.method_name}` is not implemented for this DataAsset. "  # noqa: E501
-            )
-        return PartitionerClass(**abstract_partitioner.dict())
 
     @override
     def get_batch_parameters_keys(
@@ -342,26 +299,9 @@ class _FilePathDataAsset(DataAsset[DatasourceT, RegexPartitioner], Generic[Datas
         Returns:
             List of batch definitions.
         """  # noqa: E501
-        if batch_request.partitioner:
-            spark_partitioner = self.get_partitioner_implementation(batch_request.partitioner)
-            # Remove the partitioner kwargs from the batch_request to retrieve the batch and add them back later to the batch_spec.options  # noqa: E501
-            valid_options = self.get_batch_parameters_keys(partitioner=batch_request.partitioner)
-            batch_parameters_counts = Counter(valid_options)
-            batch_request_copy_without_partitioner_kwargs = copy.deepcopy(batch_request)
-            for param_name in spark_partitioner.param_names:
-                # If the option appears twice (e.g. from asset regex and from partitioner) then don't remove.  # noqa: E501
-                if batch_parameters_counts[param_name] == 1:
-                    batch_request_copy_without_partitioner_kwargs.options.pop(param_name)
-                else:
-                    # TODO: figure out what to do here!
-                    ...
-            batch_definition_list = self._data_connector.get_batch_definition_list(
-                batch_request=batch_request_copy_without_partitioner_kwargs
-            )
-        else:
-            batch_definition_list = self._data_connector.get_batch_definition_list(
-                batch_request=batch_request
-            )
+        batch_definition_list = self._data_connector.get_batch_definition_list(
+            batch_request=batch_request
+        )
         return batch_definition_list
 
     def _batch_spec_options_from_batch_request(self, batch_request: BatchRequest) -> dict:
@@ -387,17 +327,6 @@ class _FilePathDataAsset(DataAsset[DatasourceT, RegexPartitioner], Generic[Datas
                 config_provider=self._datasource._config_provider,
             ),
         }
-
-        if batch_request.partitioner:
-            spark_partitioner = self.get_partitioner_implementation(batch_request.partitioner)
-            batch_spec_options["partitioner_method"] = spark_partitioner.method_name
-            partitioner_kwargs = spark_partitioner.partitioner_method_kwargs()
-            partitioner_kwargs["batch_identifiers"] = (
-                spark_partitioner.batch_parameters_to_batch_spec_kwarg_identifiers(
-                    batch_request.options
-                )
-            )
-            batch_spec_options["partitioner_kwargs"] = partitioner_kwargs
 
         return batch_spec_options
 
