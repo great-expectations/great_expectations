e33e46a844faa714484bb8a699f4797000e90b6a
JennyTee <jenny.terlinden@greatexpectations.io>
2024-04-25 15:14:20 -0500

    re-add requirements-dev-cloud.txt file

--- tasks.py
+++ tasks.py
@@ -763,6 +763,7 @@ MARKER_DEPENDENCY_MAP: Final[Mapping[str, TestDependencies]] = {
     "clickhouse": TestDependencies(("reqs/requirements-dev-clickhouse.txt",)),
     "cloud": TestDependencies(
         (
+            "reqs/requirements-dev-cloud.txt",
             "reqs/requirements-dev-snowflake.txt",
             "reqs/requirements-dev-spark.txt",
         ),
