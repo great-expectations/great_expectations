8fc4fd07a907af7a8d3503891bf8ad99c584ed63
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-04-25 09:23:57 -0400

    Remove second table_factory

--- tests/integration/cloud/end_to_end/conftest.py
+++ tests/integration/cloud/end_to_end/conftest.py
@@ -1,33 +1,27 @@
 from __future__ import annotations
 
-import logging
 import os
 import pathlib
 import uuid
-from pprint import pformat as pf
-from typing import TYPE_CHECKING, Final, Iterator, Literal, Protocol
+from typing import TYPE_CHECKING, Iterator
 
 import numpy as np
 import pytest
 
 import great_expectations as gx
 import great_expectations.exceptions as gx_exceptions
-from great_expectations.compatibility.sqlalchemy import TextClause
 from great_expectations.core.validation_definition import ValidationDefinition
 from great_expectations.data_context import CloudDataContext
 from great_expectations.execution_engine import (
     SparkDFExecutionEngine,
-    SqlAlchemyExecutionEngine,
 )
 
 if TYPE_CHECKING:
     from great_expectations.checkpoint import Checkpoint
-    from great_expectations.compatibility import pyspark, sqlalchemy
+    from great_expectations.compatibility import pyspark
     from great_expectations.core import ExpectationSuite
     from great_expectations.datasource.fluent import BatchRequest
     from great_expectations.validator.validator import Validator
-
-LOGGER: Final = logging.getLogger("tests")
 
 
 @pytest.fixture(scope="package")
@@ -128,70 +122,6 @@ def get_missing_data_asset_error_type() -> type[Exception]:
 @pytest.fixture(scope="package")
 def in_memory_batch_request_missing_dataframe_error_type() -> type[Exception]:
     return ValueError
-
-
-class TableFactory(Protocol):
-    def __call__(
-        self,
-        gx_engine: SqlAlchemyExecutionEngine,
-        table_names: set[str],
-        schema_name: str | None = None,
-    ) -> None: ...
-
-
-@pytest.fixture(scope="module")
-def table_factory() -> Iterator[TableFactory]:
-    """
-    Given a SQLAlchemy engine, table_name and schema,
-    create the table if it does not exist and drop it after the test class.
-    """
-    all_created_tables: dict[str, list[dict[Literal["table_name", "schema_name"], str | None]]] = {}
-    engines: dict[str, sqlalchemy.engine.Engine] = {}
-
-    def _table_factory(
-        gx_engine: SqlAlchemyExecutionEngine,
-        table_names: set[str],
-        schema_name: str | None = None,
-    ) -> None:
-        sa_engine = gx_engine.engine
-        LOGGER.info(
-            f"Creating `{sa_engine.dialect.name}` table for {table_names} if it does not exist"
-        )
-        created_tables: list[dict[Literal["table_name", "schema_name"], str | None]] = []
-
-        with gx_engine.get_connection() as conn:
-            transaction = conn.begin()
-            if schema_name:
-                conn.execute(TextClause(f"CREATE SCHEMA IF NOT EXISTS {schema_name}"))
-            for name in table_names:
-                qualified_table_name = f"{schema_name}.{name}" if schema_name else name
-                create_tables: str = (
-                    f"CREATE TABLE IF NOT EXISTS {qualified_table_name}"
-                    f" (id INTEGER, name VARCHAR(255), value INTEGER)"
-                )
-                conn.execute(TextClause(create_tables))
-
-                created_tables.append(dict(table_name=name, schema_name=schema_name))
-            transaction.commit()
-        all_created_tables[sa_engine.dialect.name] = created_tables
-        engines[sa_engine.dialect.name] = sa_engine
-
-    yield _table_factory
-
-    # teardown
-    print(f"dropping tables\n{pf(all_created_tables)}")
-    for dialect, tables in all_created_tables.items():
-        engine = engines[dialect]
-        with engine.connect() as conn:
-            transaction = conn.begin()
-            for table in tables:
-                name = table["table_name"]
-                schema = table["schema_name"]
-                qualified_table_name = f"{schema}.{name}" if schema else name
-                conn.execute(TextClause(f"DROP TABLE IF EXISTS {qualified_table_name}"))
-            if schema:
-                conn.execute(TextClause(f"DROP SCHEMA IF EXISTS {schema}"))
-            transaction.commit()
 
 
 @pytest.fixture(scope="module")
