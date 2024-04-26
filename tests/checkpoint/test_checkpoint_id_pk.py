bdf4c5d0423d557293c90fe061ad7b584a2a1375
Chetan Kini <chetan@superconductive.com>
2024-04-26 10:40:44 -0400

    more tests

--- tests/checkpoint/test_checkpoint_id_pk.py
+++ tests/checkpoint/test_checkpoint_id_pk.py
@@ -76,8 +76,15 @@ WHERE animals IS NOT NULL AND (animals NOT IN ('cat', 'fish', 'dog'));"
 
 
 @pytest.fixture
-def expect_column_values_to_be_in_set():
+def expect_column_values_to_be_in_set() -> gxe.ExpectColumnValuesToBeInSet:
     return gxe.ExpectColumnValuesToBeInSet(column="animals", value_set=["cat", "fish", "dog"])
+
+
+@pytest.fixture
+def expect_column_values_to_not_be_in_set() -> gxe.ExpectColumnValuesToNotBeInSet:
+    return gxe.ExpectColumnValuesToNotBeInSet(
+        column="animals", value_set=["giraffe", "lion", "zebra"]
+    )
 
 
 @pytest.fixture
@@ -95,7 +102,7 @@ def _build_checkpoint(
     expectations: list[gxe.Expectation],
     asset_name: str,
     result_format: dict,
-):
+) -> Checkpoint:
     suite = context.suites.add(
         suite=ExpectationSuite(name="metrics_exp", expectations=expectations)
     )
@@ -323,3 +330,132 @@ def test_sql_result_format_in_checkpoint_pk_defined_multi_column_sum_expectation
         unexpected_index_query
         == "SELECT pk_1, a, b, c \nFROM multi_column_sums \nWHERE 0 + a + b + c != 30.0;"
     )
+
+
+@pytest.mark.filesystem
+def test_sql_result_format_in_checkpoint_pk_defined_multi_column_sum_expectation_summary_output(
+    data_context_with_connection_to_metrics_db: FileDataContext,
+    expect_multicolumn_sum_to_equal: gxe.ExpectMulticolumnSumToEqual,
+):
+    checkpoint = _build_checkpoint(
+        context=data_context_with_connection_to_metrics_db,
+        expectations=[expect_multicolumn_sum_to_equal],
+        asset_name=MULTI_COLUMN_SUM_ASSET,
+        result_format={
+            "result_format": "SUMMARY",
+            "unexpected_index_column_names": ["pk_1"],
+        },
+    )
+
+    result = checkpoint.run()
+    evrs = list(result.run_results.values())
+
+    index_column_names = evrs[0]["results"][0]["result"]["unexpected_index_column_names"]
+    assert index_column_names == ["pk_1"]
+
+    first_result_full_list = evrs[0]["results"][0]["result"].get("unexpected_index_list")
+    assert not first_result_full_list
+
+    first_result_partial_list = evrs[0]["results"][0]["result"]["partial_unexpected_index_list"]
+    assert first_result_partial_list == [
+        {"pk_1": 1, "a": 20, "b": 20, "c": 20},
+        {"pk_1": 2, "a": 30, "b": 30, "c": 30},
+        {"pk_1": 3, "a": 40, "b": 40, "c": 40},
+        {"pk_1": 4, "a": 50, "b": 50, "c": 50},
+        {"pk_1": 5, "a": 60, "b": 60, "c": 60},
+    ]
+    unexpected_index_query = evrs[0]["results"][0]["result"].get("unexpected_index_query")
+    assert not unexpected_index_query
+
+
+@pytest.mark.filesystem
+def test_sql_result_format_in_checkpoint_pk_defined_one_expectation_complete_output_no_query(
+    data_context_with_connection_to_metrics_db: FileDataContext,
+    expect_column_values_to_be_in_set: gxe.ExpectColumnValuesToBeInSet,
+    expected_unexpected_indices_output: list[dict[str, str | int]],
+):
+    """
+    What does this test?
+        - unexpected_index_column defined in Checkpoint only.
+        - COMPLETE output, which means we have `unexpected_index_list` and
+          `partial_unexpected_index_list`
+        - 1 Expectations added to suite
+        - return_unexpected_index_query flag set to False
+    """
+    checkpoint = _build_checkpoint(
+        context=data_context_with_connection_to_metrics_db,
+        expectations=[expect_column_values_to_be_in_set],
+        asset_name=ANIMAL_ASSET,
+        result_format={
+            "result_format": "COMPLETE",
+            "unexpected_index_column_names": ["pk_1"],
+            "return_unexpected_index_query": False,
+        },
+    )
+
+    result = checkpoint.run()
+    evrs = list(result.run_results.values())
+
+    index_column_names = evrs[0]["results"][0]["result"]["unexpected_index_column_names"]
+    assert index_column_names == ["pk_1"]
+
+    first_result_full_list = evrs[0]["results"][0]["result"]["unexpected_index_list"]
+    assert first_result_full_list == expected_unexpected_indices_output
+
+    first_result_partial_list = evrs[0]["results"][0]["result"]["partial_unexpected_index_list"]
+    assert first_result_partial_list == expected_unexpected_indices_output
+
+    assert evrs[0]["results"][0]["result"].get("unexpected_index_query") is None
+
+
+@pytest.mark.filesystem
+def test_sql_result_format_in_checkpoint_pk_defined_two_expectation_complete_output(
+    data_context_with_connection_to_metrics_db: FileDataContext,
+    expect_column_values_to_be_in_set: gxe.ExpectColumnValuesToBeInSet,
+    expect_column_values_to_not_be_in_set: gxe.ExpectColumnValuesToNotBeInSet,
+    expected_unexpected_indices_output: list[dict[str, str | int]],
+    expected_sql_query_output: str,
+):
+    """
+    What does this test?
+        - unexpected_index_column not defined in Checkpoint config, but passed in at
+          run_checkpoint.
+        - COMPLETE output, which means we have `unexpected_index_list` and
+          `partial_unexpected_index_list`
+        - 2 Expectations added to suite
+    """
+    checkpoint = _build_checkpoint(
+        context=data_context_with_connection_to_metrics_db,
+        expectations=[
+            expect_column_values_to_be_in_set,
+            expect_column_values_to_not_be_in_set,
+        ],
+        asset_name=ANIMAL_ASSET,
+        result_format={
+            "result_format": "COMPLETE",
+            "unexpected_index_column_names": ["pk_1"],
+        },
+    )
+
+    result = checkpoint.run()
+    evrs = list(result.run_results.values())
+
+    index_column_names = evrs[0]["results"][0]["result"]["unexpected_index_column_names"]
+    assert index_column_names == ["pk_1"]
+
+    # first and second expectations have same results. Although one is "expect_to_be"
+    # and the other is "expect_to_not_be", they have opposite value_sets
+    first_result_full_list = evrs[0]["results"][0]["result"]["unexpected_index_list"]
+    assert first_result_full_list == expected_unexpected_indices_output
+
+    first_result_partial_list = evrs[0]["results"][0]["result"]["partial_unexpected_index_list"]
+    assert first_result_partial_list == expected_unexpected_indices_output
+
+    second_result_full_list = evrs[0]["results"][1]["result"]["unexpected_index_list"]
+    assert second_result_full_list == expected_unexpected_indices_output
+
+    second_result_partial_list = evrs[0]["results"][1]["result"]["partial_unexpected_index_list"]
+    assert second_result_partial_list == expected_unexpected_indices_output
+
+    unexpected_index_query = evrs[0]["results"][0]["result"]["unexpected_index_query"]
+    assert unexpected_index_query == expected_sql_query_output
