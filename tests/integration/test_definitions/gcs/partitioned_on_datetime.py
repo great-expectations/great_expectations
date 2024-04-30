c55d34e7470a27af6787ab4e3f67c3bebe7c34e3
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-04-30 10:55:15 -0400

    Print batch len

--- tests/integration/test_definitions/gcs/partitioned_on_datetime.py
+++ tests/integration/test_definitions/gcs/partitioned_on_datetime.py
@@ -15,7 +15,6 @@ assert datasource_name in context.datasources
 
 asset_name = "my_taxi_data_asset"
 gcs_prefix = "data/taxi_yellow_tripdata_samples/"
-batching_regex = r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})\.csv"
 data_asset = datasource.add_csv_asset(
     name=asset_name,
     gcs_prefix=gcs_prefix,
@@ -23,17 +22,18 @@ data_asset = datasource.add_csv_asset(
 
 batch_definition = data_asset.add_batch_definition_monthly(
     "monthly",
-    regex=re.compile(batching_regex),
+    regex=re.compile(r"yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})\.csv"),
 )
 
 # not passing in batch parameters
 my_batch_request = batch_definition.build_batch_request()
 batches = data_asset.get_batch_list_from_batch_request(my_batch_request)
+print(len(batches))
 assert len(batches) == 3
 
 # passing in batch parameters
 my_batch_request = batch_definition.build_batch_request(
-    batch_parameters={"year": "2019", "month": "03"}
+    batch_parameters={"year": "2019", "month": "02"}
 )
 batches = data_asset.get_batch_list_from_batch_request(my_batch_request)
 assert len(batches) == 1
