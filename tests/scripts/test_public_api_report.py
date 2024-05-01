d1c39ffd7fa8205c7962447f0b8e83c8cd70a1b4
Chetan Kini <chetan@superconductive.com>
2024-05-01 12:54:30 -0400

    [MAINTENANCE] Make `ExpectationSuite` importable from the top level GX namespace (#9854)

--- tests/scripts/test_public_api_report.py
+++ tests/scripts/test_public_api_report.py
@@ -350,7 +350,7 @@ def test_get_shortest_dotted_path(monkeypatch):
     # This is the shortest path
     assert (
         get_shortest_dotted_path(definition=definition, repo_root_path=repo_root)
-        == "great_expectations.core.ExpectationSuite"
+        == "great_expectations.ExpectationSuite"
     )
 
 
