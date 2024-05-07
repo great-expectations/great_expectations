3f3dd3c4885bec2d3f2b8b55b4619129dc66b346
Robert Moses Lim <robert.m.lim@gmail.com>
2024-05-06 17:27:58 -0700

    Add constants for snowflake partner application

--- great_expectations/datasource/fluent/constants.py
+++ great_expectations/datasource/fluent/constants.py
@@ -24,3 +24,7 @@ DEFAULT_PANDAS_DATASOURCE_NAME: Final[str] = "default_pandas_datasource"
 DEFAULT_PANDAS_DATA_ASSET_NAME: Final[str] = "#ephemeral_pandas_asset"
 
 _IN_MEMORY_DATA_ASSET_TYPE: Final[str] = "dataframe"
+
+SNOWFLAKE_PARTNER_APPLICATION_OSS: Final[str] = "great_expectations_oss"
+# TODO: Change this value to "great_expectations_cloud" once configured in Snowflake partner account
+SNOWFLAKE_PARTNER_APPLICATION_CLOUD: Final[str] = "great_expectations_oss"
