a1c0c5a46dea8c1b39e80ef35dbd963728028233
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2024-05-08 13:49:49 -0400

    [MAINTENANCE] Remove remaining references to block style datasources (#9881)

--- tests/integration/test_definitions/snowflake/integration_tests.py
+++ tests/integration/test_definitions/snowflake/integration_tests.py
@@ -38,22 +38,7 @@ partition_data = [
     ),
 ]
 
-sample_data = [
-    IntegrationTestFixture(
-        name="sample_data_using_limit_snowflake",
-        user_flow_script="tests/integration/db/test_sql_data_sampling.py",
-        data_context_dir="tests/integration/fixtures/no_datasources/great_expectations",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
-        util_script="tests/test_utils.py",
-        other_files=(
-            (
-                "tests/integration/fixtures/partition_and_sample_data/snowflake_connection_string.yml",
-                "connection_string.yml",
-            ),
-        ),
-        backend_dependencies=[BackendDependencies.SNOWFLAKE],
-    ),
-]
+sample_data: List[IntegrationTestFixture] = []
 
 snowflake_integration_tests += connecting_to_your_data
 snowflake_integration_tests += partition_data
