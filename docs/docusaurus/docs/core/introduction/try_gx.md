b8e4c8a8c998cd74e1b4606ad19aadc06d47b7b7
Rachel-Reverie <rachel@superconductive.com>
2024-05-13 14:20:29 -0500

    Tweaks to the sample script, updates some broken table formatting that was causing errors; draft of the "Try GX" procedure.

--- docs/docusaurus/docs/core/introduction/try_gx.md
+++ docs/docusaurus/docs/core/introduction/try_gx.md
@@ -1,7 +1,72 @@
 ---
 title: Try Great Expectations
 ---
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
-import InProgress from '../_core_components/_in_progress.md'
+import GxData from '../_core_components/_data.jsx';
+import PrereqPythonInstalled from '../_core_components/prerequisites/_python_installation.md';
 
-<InProgress/>
+
+
+Follow this guide to install {GxData.product_name}, connect to sample data, build your first Expectation, validate the sample data, and review the Validation Results.  This is an ideal place to start if you're new to GX and would like to experiment with some basic features of GX to see what it has to offer.
+
+## Prerequisites
+
+- <PrereqPythonInstalled/>
+
+## Try GX
+
+<Tabs>
+
+<TabItem value="procedure" label="Procedure">
+
+1. Import the required modules from the {GxData.product_name} library.
+
+   ```python title="Python input" name="docs/docusaurus/docs/core/introduction/try_gx.py imports"
+   ```
+
+2. Create a temporary Data Context and connect to the provided sample data.
+
+   A Data Context can be thought of as a GX project.  In the following code, a Data Context is initialized and then used to read the contents of a `.csv` file into a Batch of sample data:
+
+   ```python title="Python input" name="docs/docusaurus/docs/core/introduction/try_gx.py set up"
+   ```
+
+   The sample data that has been retrieved as a Batch will be used to test the Expectations that are created later in this guide.
+
+3. Create an Expectation.
+
+   Expectations are a fundamental feature of GX.  They allow you to explicitly define the state that your data should conform to.
+
+   Since the provided sample data corresponds to purchased taxicab trips, some assumptions can be made about the content of that data.  For example, it wouldn't make sense for the passenger count to ever be `0`, since at least one passanger needs to be present to purchase the ride.  Additionally, the taxicabs in question should not be capable of seating more than `6` individuals.
+
+   The following code defines an Expectation that the contents of the column `passenger_count` in the provided data consist of a value ranging from `1` to `6`: 
+
+   ```python title="Python input" name="docs/docusaurus/docs/core/introduction/try_gx.py create an expectation"
+   ```
+
+4. Validate the sample data against your Expectation and view the results.
+ 
+   Once an Expectation has been defined it can be applied to a Batch of data to see how well that data conforms to the Expectation.
+
+   In the following code, the previously defined Expectation is validated against the Batch of sample data and the Validation Results are then printed for evaluation:
+
+   ```python title="Python input" name="docs/docusaurus/docs/core/introduction/try_gx.py validate and view results"
+   ```
+
+   In this example, the sample data does conform to the Expectation that was defined.  The validation results returned will look like:
+
+   ```python title="Python output" name="docs/docusaurus/docs/core/introduction/try_gx.py output1"
+   ```
+
+</TabItem>
+
+<TabItem value="sample_code" label="Sample code">
+
+```python title="Full example script" name="docs/docusaurus/docs/core/introduction/try_gx.py full example script"
+```
+
+</TabItem>
+
+</Tabs>
--- docs/docusaurus/docs/core/introduction/try_gx.py
+++ docs/docusaurus/docs/core/introduction/try_gx.py
@@ -2,18 +2,20 @@
 This example script allows the user to try out GX by validating Expectations
  against provided sample data.
 
-The <snippet> tags are used to insert the corresponding code into the
+The snippet tags are used to insert the corresponding code into the
  Great Expectations documentation.  They can be disregarded by anyone
  reviewing this script.
 """
 
 # <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py full example script">
+# Import required modules from the GX library
 # <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py imports">
 import great_expectations as gx
 import great_expectations.expectations as gxe
 
 # </snippet>
 
+# Create a temporary Data Context and connect to provided sample data.
 # <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py set up">
 context = gx.get_context()
 batch = context.data_sources.pandas_default.read_csv(
@@ -21,38 +23,96 @@ batch = context.data_sources.pandas_default.read_csv(
 )
 # </snippet>
 
-# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py create an expectation>
+# Create an Expectation
+# highlight-start
+# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py create an expectation">
 expectation = gxe.ExpectColumnValuesToBeBetween(
     column="passenger_count", min_value=1, max_value=6
 )
 # </snippet>
+# highlight-end
 
-# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py validate and view results>
+# Validate the sample data against your Expectation and view the results
+# highlight-start
+# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py validate and view results">
 validation_result = batch.validate(expectation)
 print(validation_result.describe())
 # </snippet>
+# highlight-end
 # </snippet>
 
-# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py try another expectation 1>
-expectation = gxe.ExpectColumnValuesToBeBetween(
-    column="passenger_count", min_value=1, max_value=6
-)
-validation_result = batch.validate(expectation)
-print(validation_result.describe())
+output1 = """
+# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py output1">
+{
+    "expectation_type": "expect_column_values_to_be_between",
+    "success": false,
+    "kwargs": {
+        "batch_id": "default_pandas_datasource-#ephemeral_pandas_asset",
+        "column": "passenger_count",
+        "min_value": 1.0,
+        "max_value": 3.0
+    },
+    "result": {
+        "element_count": 10000,
+        "unexpected_count": 853,
+        "unexpected_percent": 8.53,
+        "partial_unexpected_list": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ],
+        "missing_count": 0,
+        "missing_percent": 0.0,
+        "unexpected_percent_total": 8.53,
+        "unexpected_percent_nonmissing": 8.53,
+        "partial_unexpected_counts": [
+            {
+                "value": 4,
+                "count": 20
+            }
+        ],
+        "partial_unexpected_index_list": [
+            9147,
+            9148,
+            9149,
+            9150,
+            9151,
+            9152,
+            9153,
+            9154,
+            9155,
+            9156,
+            9157,
+            9158,
+            9159,
+            9160,
+            9161,
+            9162,
+            9163,
+            9164,
+            9165,
+            9166
+        ]
+    }
+}
 # </snippet>
+"""
 
-# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py try another expectation 2>
-expectation = gxe.ExpectColumnValuesToBeBetween(
-    column="passenger_count", min_value=1, max_value=6
-)
-validation_result = batch.validate(expectation)
-print(validation_result.describe())
-# </snippet>
-
-# <snippet name="docs/docusaurus/docs/core/introduction/try_gx.py try another expectation 3>
-expectation = gxe.ExpectColumnValuesToBeBetween(
-    column="passenger_count", min_value=1, max_value=6
-)
-validation_result = batch.validate(expectation)
-print(validation_result.describe())
-# </snippet>
+assert validation_result.describe() == output1.strip()
