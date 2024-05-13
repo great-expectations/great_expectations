6a2c3759522389fc6d5b11993b1060fc5a5272de
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-05-13 13:46:06 -0400

    remove file path/ filesystem

--- great_expectations/datasource/data_connector/__init__.py
+++ great_expectations/datasource/data_connector/__init__.py
@@ -2,18 +2,5 @@
 
 from .data_connector import DataConnector
 from .runtime_data_connector import RuntimeDataConnector
-from .file_path_data_connector import FilePathDataConnector
-from .configured_asset_file_path_data_connector import (
-    ConfiguredAssetFilePathDataConnector,
-)
-from .inferred_asset_file_path_data_connector import (
-    InferredAssetFilePathDataConnector,
-)
-from .configured_asset_filesystem_data_connector import (
-    ConfiguredAssetFilesystemDataConnector,
-)
-from .inferred_asset_filesystem_data_connector import (
-    InferredAssetFilesystemDataConnector,
-)
 
 
