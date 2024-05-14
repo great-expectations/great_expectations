2dc81be5711e4c5a4efde06ba10d7e4b6edd0d74
Gabriel Gore <gabriel59kg@gmail.com>
2024-05-13 20:26:18 -0400

    better log

--- scripts/gen_gx_v0_package.py
+++ scripts/gen_gx_v0_package.py
@@ -6,6 +6,8 @@ Need to replace any instance of
 
 """
 
+from __future__ import annotations
+
 import pathlib
 import re
 from typing import Final, Pattern
@@ -17,6 +19,8 @@ CORE_DIRECTORY: Final[pathlib.Path] = pathlib.Path("great_expectations").resolve
     strict=True
 )
 
+UNTOUCED_FILES: Final[list[pathlib.Path]] = []
+
 
 def replace(file_path: pathlib.Path) -> None:
     with open(file_path, "r") as file:
@@ -24,8 +28,9 @@ def replace(file_path: pathlib.Path) -> None:
         new_contents = IMPORT_PATTERN.sub("import great_expectations_v0", contents)
         new_contents = FROM_PATTERN.sub("from great_expectations_v0", new_contents)
         if contents == new_contents:
+            UNTOUCED_FILES.append(file_path)
             return
-    print(f"{file_path} updated")
+    print(f"{file_path.relative_to(CORE_DIRECTORY.parent)} updated")
     with open(file_path, "w") as file:
         file.write(new_contents)
 
@@ -35,9 +40,9 @@ def iterate_files(file_dir: pathlib.Path) -> None:
         if file.is_dir():
             iterate_files(file)
         elif file.is_file() and file.suffix in (".py", ".pyi"):
-            print(f"Replacing in file: {file}")
             replace(file)
 
 
 if __name__ == "__main__":
     iterate_files(CORE_DIRECTORY)
+    print(f"\n Untouched files: {len(UNTOUCED_FILES)}")
