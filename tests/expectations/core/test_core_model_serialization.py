a4532f629a9603dbac18afc422ef14a37fa729f0
Nathan Farmer <nathan@greatexpectations.io>
2024-05-21 08:58:22 -0400

    Rename test

--- tests/expectations/core/test_core_model_serialization.py
+++ tests/expectations/core/test_core_model_serialization.py
@@ -18,7 +18,7 @@ def test_all_core_model_schemas_are_serializable():
 
 
 @pytest.mark.unit
-def test_title_serialization():
+def test_schema_title():
     expectation = core.ExpectColumnValuesToNotBeNull
     title = expectation.schema()["title"]
     assert title == "Expect Column Values To Not Be Null"
