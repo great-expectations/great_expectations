69fe9d7baaaa2eb687e561fad057684cd31643c1
Farmer <nathan@greatexpectations.io>
2024-05-30 10:12:15 -0400

    ExpectTableColumnCountToBeBetween

--- great_expectations/expectations/core/expect_table_column_count_to_be_between.py
+++ great_expectations/expectations/core/expect_table_column_count_to_be_between.py
@@ -32,9 +32,15 @@ if TYPE_CHECKING:
     )
     from great_expectations.render.renderer_configuration import AddParamArgs
 
+EXPECTATION_SHORT_DESCRIPTION = "Expect the number of columns to be between two values."
+MIN_VALUE_DESCRIPTION = "The minimum number of columns, inclusive."
+MAX_VALUE_DESCRIPTION = "The maximum number of columns, inclusive."
+SUPPORTED_DATA_SOURCES = ["Snowflake", "PostgreSQL"]
+DATA_QUALITY_ISSUES = ["Schema"]
+
 
 class ExpectTableColumnCountToBeBetween(BatchExpectation):
-    """Expect the number of columns to be between two values.
+    __doc__ = f"""{EXPECTATION_SHORT_DESCRIPTION}
 
     expect_table_column_count_to_be_between is a \
     [Batch Expectation](https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_batch_expectations).
@@ -43,10 +49,8 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
     They are evaluated for an entire Batch, and answer a semantic question about the Batch itself.
 
     Args:
-        min_value (int or None): \
-            The minimum number of columns, inclusive.
-        max_value (int or None): \
-            The maximum number of columns, inclusive.
+        min_value (int or None): {MIN_VALUE_DESCRIPTION}
+        max_value (int or None): {MAX_VALUE_DESCRIPTION}
 
     Other Parameters:
         result_format (str or None): \
@@ -74,12 +78,12 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
     See Also:
         [expect_table_column_count_to_equal](https://greatexpectations.io/expectations/expect_table_column_count_to_equal)
 
-    Supported Datasources:
-        [Snowflake](https://docs.greatexpectations.io/docs/application_integration_support/)
-        [PostgreSQL](https://docs.greatexpectations.io/docs/application_integration_support/)
+    Supported Data Sources:
+        [{SUPPORTED_DATA_SOURCES[0]}](https://docs.greatexpectations.io/docs/application_integration_support/)
+        [{SUPPORTED_DATA_SOURCES[1]}](https://docs.greatexpectations.io/docs/application_integration_support/)
 
-    Data Quality Category:
-        Schema
+    Data Quality Issues:
+        {DATA_QUALITY_ISSUES[0]}
 
     Example Data:
                 test 	test2
@@ -96,18 +100,18 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
             )
 
             Output:
-                {
-                  "exception_info": {
+                {{
+                  "exception_info": {{
                     "raised_exception": false,
                     "exception_traceback": null,
                     "exception_message": null
-                  },
-                  "result": {
+                  }},
+                  "result": {{
                     "observed_value": 2
-                  },
-                  "meta": {},
+                  }},
+                  "meta": {{}},
                   "success": true
-                }
+                }}
 
         Failing Case:
             Input:
@@ -116,18 +120,18 @@ class ExpectTableColumnCountToBeBetween(BatchExpectation):
             )
 
             Output:
-                {
-                  "exception_info": {
+                {{
+                  "exception_info": {{
                     "raised_exception": false,
                     "exception_traceback": null,
                     "exception_message": null
-                  },
-                  "result": {
+                  }},
+                  "result": {{
                     "observed_value": 2
-                  },
-                  "meta": {},
+                  }},
+                  "meta": {{}},
                   "success": false
-                }
+                }}
     """  # noqa: E501
 
     min_value: Union[float, SuiteParameterDict, datetime, None]
