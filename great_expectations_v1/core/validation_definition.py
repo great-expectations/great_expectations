07718fe067bc47d78b95bec0b2472a5a5cf44293
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2024-05-31 15:15:16 -0400

    [MAINTENANCE] Merge `develop` into `gx-v1-mirror` (#9996)

--- great_expectations_v1/core/validation_definition.py
+++ great_expectations_v1/core/validation_definition.py
@@ -280,7 +280,7 @@ class ValidationDefinition(BaseModel):
 
     @public_api
     def save(self) -> None:
-        from great_expectations import project_manager
+        from great_expectations_v1 import project_manager
 
         store = project_manager.get_validation_definition_store()
         key = store.get_key(name=self.name, id=self.id)
@@ -293,7 +293,7 @@ class ValidationDefinition(BaseModel):
         We need to persist a validation_definition before it can be run. If user calls runs but
         hasn't persisted it we add it for them."""
 
-        from great_expectations import project_manager
+        from great_expectations_v1 import project_manager
 
         store = project_manager.get_validation_definition_store()
         key = store.get_key(name=self.name, id=self.id)
