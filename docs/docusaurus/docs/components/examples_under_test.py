e3eec0031f5b777e4cc96147fdb38651c5391336
Rachel-Reverie <rachel@superconductive.com>
2024-05-31 07:05:10 -0500

    Batch Definition code going under test

--- docs/docusaurus/docs/components/examples_under_test.py
+++ docs/docusaurus/docs/components/examples_under_test.py
@@ -34,6 +34,21 @@ connecting_to_a_datasource = [
         ),
         backend_dependencies=[BackendDependencies.POSTGRESQL],
     ),
+    # Create a Batch Definition
+    IntegrationTestFixture(
+        name="create_a_batch_definition_postgres",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_batch_definition/create_a_batch_definition.py",
+        data_context_dir="docs/docusaurus/docs/components/_testing/create_datasource/great_expectations/",
+        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+        util_script="tests/test_utils.py",
+        other_files=(
+            (
+                "tests/integration/fixtures/partition_and_sample_data/postgres_connection_string.yml",
+                "connection_string.yml",
+            ),
+        ),
+        backend_dependencies=[BackendDependencies.POSTGRESQL],
+    ),
 ]
 
 
--- docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_batch_definition/create_a_batch_definition.py
+++ docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_batch_definition/create_a_batch_definition.py
@@ -3,10 +3,11 @@ To run this test locally, run:
 1. Activate docker.
 2. From the repo root dir, activate the postgresql database docker container:
 
-pytest  --postgresql --docs-tests -k "create_a_datasource_postgres" tests/integration/test_script_runner.py
+pytest  --postgresql --docs-tests -k "create_a_batch_definition_postgres" tests/integration/test_script_runner.py
 """
 
 # This section is setup for the environment used in the example script.
+import great_expectations as gx
 from tests.integration.db.taxi_data_utils import load_data_into_test_database
 
 # add test_data to database for testing
@@ -16,19 +17,15 @@ load_data_into_test_database(
     connection_string="postgresql+psycopg2://postgres:@localhost/test_ci",
 )
 
-# The example starts here
-import great_expectations as gx
-
+# Set up the Data Source and a Data Asset
 context = gx.get_context()
 
-datasource_name = "my_new_datasource"
+datasource_name = "my_datasource"
 my_connection_string = "${POSTGRESQL_CONNECTION_STRING}"
 
 data_source = context.data_sources.add_postgres(
     name=datasource_name, connection_string=my_connection_string
 )
-
-print(context.get_datasource(datasource_name))
 
 asset_name = "MY_TABLE_ASSET"
 database_table_name = "postgres_taxi_data"
@@ -36,14 +33,33 @@ table_data_asset = data_source.add_table_asset(
     table_name=database_table_name, name=asset_name
 )
 
-asset_name = "MY_QUERY_ASSET"
-asset_query = "SELECT * from postgres_taxi_data"
-query_data_asset = data_source.add_query_asset(query=asset_query, name=asset_name)
+# The example starts here
+import great_expectations as gx
 
+context = gx.get_context()
+
+# Retrieve a Data Source
+datasource_name = "my_datasource"
+data_source = context.get_datasource(datasource_name)
+
+# Get the Data Asset from the Data Source
+asset_name = "MY_TABLE_ASSET"
+data_asset = data_source.get_asset(asset_name)
+
+# Example of a full table Batch Definition
 bd_name = "FULL_TABLE"
-batch_definition = table_data_asset.add_batch_definition_whole_table(name=bd_name)
-batch_definition = query_data_asset.add_batch_definition_whole_table(name=bd_name)
+full_table_batch_definition = data_asset.add_batch_definition_whole_table(name=bd_name)
+
+# Verify that the Batch Definition is valid
+full_table_batch = full_table_batch_definition.get_batch()
+full_table_batch.head()
 
 
-batch = batch_definition.get_batch()
-batch.head()
+bd_name = "DAILY"
+date_column = "pickup_datetime"
+daily_table_batch_definition = data_asset.add_batch_definition_daily(
+    name=bd_name, column=date_column
+)
+
+daily_table_batch = daily_table_batch_definition.get_batch()
+daily_table_batch.head()
