3431d94ff11778ee1d696cdf8098933ad4634253
Farmer <nathan@greatexpectations.io>
2024-06-04 01:44:54 -0400

    Clean up

--- great_expectations/compatibility/typing_extensions.py
+++ great_expectations/compatibility/typing_extensions.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 from typing import Any, Callable, TypeVar
 
 try:
-    from typing import (
-        Annotated,  # type: ignore[attr-defined]  # only exists in some python versions
+    from typing import (  # type: ignore[attr-defined]  # only exists in some python versions
+        Annotated,
     )
 except ImportError:
     from typing_extensions import Annotated
