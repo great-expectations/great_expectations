8e20d59cad25589de5592ada4fbfafc24012a614
Farmer <nathan@greatexpectations.io>
2024-06-04 00:40:25 -0400

    Fix typing errors

--- great_expectations/compatibility/typing_extensions.py
+++ great_expectations/compatibility/typing_extensions.py
@@ -3,10 +3,13 @@ from __future__ import annotations
 from typing import Any, Callable, TypeVar
 
 try:
-    from typing_extensions import Annotated, override
+    from typing_extensions import Annotated
 except ImportError:
     from typing import Annotated
 
+try:
+    from typing_extensions import override
+except ImportError:
     F = TypeVar("F", bound=Callable[..., Any])
 
     def override(__arg: F, /) -> F:
