8c2f03a1029526e5b017c114067ac121f3a8dc78
Gabriel <gabriel59kg@gmail.com>
2024-06-06 09:34:20 -0400

    revert

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -247,7 +247,7 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev-test.txt", "adr-tools-python", (("==", "1.0.3"),)),
         ("requirements-dev-test.txt", "docstring-parser", (("==", "0.15"),)),
         ("requirements-dev-test.txt", "ipykernel", (("<=", "6.17.1"),)),
-        ("requirements-dev-test.txt", "moto", (("<", "3.0.0"), (">=", "2.1.0"))),
+        ("requirements-dev-test.txt", "moto", (("<", "3.0.0"), (">=", "2.0.0"))),
         ("requirements-dev-test.txt", "snapshottest", (("==", "0.6.0"),)),
         ("requirements-dev.txt", "PyMySQL", (("<", "0.10"), (">=", "0.9.3"))),
         ("requirements-dev.txt", "adr-tools-python", (("==", "1.0.3"),)),
