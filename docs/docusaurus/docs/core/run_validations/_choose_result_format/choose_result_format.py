75131ac538153da330242f7065b1dc868d970555
Rachel-Reverie <rachel@superconductive.com>
2024-06-11 20:21:08 -0500

    Adds the "Choose result format" and "Run a Validation Definition" content.

---
+++ docs/docusaurus/docs/core/run_validations/_choose_result_format/choose_result_format.py
@@ -0,0 +1,11 @@
+basic_rf_dict = {}
+basic_rf_dict["result_format"] = "BASIC"
+
+boolean_only_rf_dict = {}
+boolean_only_rf_dict["result_format"] = "BOOLEAN_ONLY"
+
+complete_rf_dict = {}
+complete_rf_dict["result_format"] = "COMPLETE"
+
+summary_rf_dict = {}
+summary_rf_dict["result_format"] = "SUMMARY"
