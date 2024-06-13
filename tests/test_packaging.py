e53c48f3d7a16fc6ae9f3e48c9354fc730b26825
Gabriel Gore <gabriel59kg@gmail.com>
2024-06-13 19:36:36 -0400

    cherrypick

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -202,14 +202,8 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev-lite.txt", "ipykernel", (("<=", "6.17.1"),)),
         ("requirements-dev-lite.txt", "moto", (("<", "3.0.0"), (">=", "2.0.0"))),
         ("requirements-dev-lite.txt", "snapshottest", (("==", "0.6.0"),)),
-        ("requirements-dev-mysql.txt", "PyMySQL", (("<", "0.10"), (">=", "0.9.3"))),
         ("requirements-dev-pagerduty.txt", "pypd", (("==", "1.1.0"),)),
         ("requirements-dev-snowflake.txt", "pandas", (("<", "2.2.0"),)),
-        (
-            "requirements-dev-sqlalchemy.txt",
-            "PyMySQL",
-            (("<", "0.10"), (">=", "0.9.3")),
-        ),
         ("requirements-dev-sqlalchemy.txt", "ipykernel", (("<=", "6.17.1"),)),
         (
             "requirements-dev-sqlalchemy.txt",
@@ -248,7 +242,6 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev-test.txt", "mypy", (("==", "1.10.0"),)),
         ("requirements-dev-test.txt", "ruff", (("==", "0.4.5"),)),
         ("requirements-dev-test.txt", "snapshottest", (("==", "0.6.0"),)),
-        ("requirements-dev.txt", "PyMySQL", (("<", "0.10"), (">=", "0.9.3"))),
         ("requirements-dev.txt", "adr-tools-python", (("==", "1.0.3"),)),
         ("requirements-dev.txt", "altair", (("<", "5.0.0"), (">=", "4.2.1"))),
         ("requirements-dev.txt", "black", (("==", "23.10.1"),)),
