02435e48a127d37c6d09007bfeddd1b2eb4517da
Gabriel <gabriel59kg@gmail.com>
2024-06-17 15:48:12 -0400

    Apply suggestions from code review

--- setup.py
+++ setup.py
@@ -140,7 +140,6 @@ config = {
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
 }
 
