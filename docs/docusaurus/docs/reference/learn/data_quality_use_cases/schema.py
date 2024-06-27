85fd5c54e5f69cba89e4133a00d3932d7ae4f4f9
Rachel House <rachel.house@greatexpectations.io>
2024-06-27 16:19:47 -0400

    add code snippet under test example

---
+++ docs/docusaurus/docs/reference/learn/data_quality_use_cases/schema.py
@@ -0,0 +1,42 @@
+# This section loads sample data to use for CI testing of the script.
+import pathlib
+
+from tests.test_utils import load_data_into_test_database
+
+CONNECTION_STRING = "postgresql+psycopg2://postgres:@localhost/test_ci"
+
+test_data_1_path = pathlib.Path(gx.__file__).parent.parent / "tests/test_sets/learn_data_quality_use_cases/schema_financial_transfers_1.csv"
+
+# Add test_data to database for testing.
+load_data_into_test_database(
+    table_name="transfers_1",
+    csv_path=str(test_data_1_path),
+    connection_string=CONNECTION_STRING,
+)
+
+# <snippet name="docs/docusaurus/docs/reference/learn/data_quality_use_cases/schema.py full sample code">
+import great_expectations as gx
+import great_expectations.expectations as gxe
+
+context = gx.get_context()
+
+# Create the Data Source and Data Assets.
+# CONNECTION_STRING contains the connection string to connect to the postgres database.
+datasource = context.data_sources.add_postgres(
+    "postgres database",
+    connection_string=CONNECTION_STRING
+)
+
+data_asset_1 = datasource.add_table_asset(
+    name="data asset 1",
+    table_name="transfers_1"
+)
+
+# Create the Expectation Suite and add an Expectation.
+expectation = gxe.ExpectColumnToExist(column="recipient_fullname")
+
+batch_definition_1 = data_asset_1.add_batch_definition_whole_table("batch definition 1")
+
+batch_1 = batch_definition_1.get_batch()
+batch_1.validate(expectation)
+# </snippet>
