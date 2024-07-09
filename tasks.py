e2277414c44567964777548f979cac7a8ae75887
Gabriel Gore <gabriel59kg@gmail.com>
2024-07-09 18:30:15 -0400

    don't error on warnings for marker coverage

--- tasks.py
+++ tasks.py
@@ -215,7 +215,7 @@ def docstrings(ctx: Context, paths: list[str] | None = None):
 def marker_coverage(
     ctx: Context,
 ):
-    pytest_cmds = ["pytest", "--verify-marker-coverage-and-exit"]
+    pytest_cmds = ["pytest", "--verify-marker-coverage-and-exit", "-W=once"]
     ctx.run(" ".join(pytest_cmds), echo=True, pty=True)
 
 
