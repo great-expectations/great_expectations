f6b1e83289507ed7ab31255cf7aecb5d75c8b43d
Gabriel Gore <gabriel59kg@gmail.com>
2024-07-15 10:45:25 -0400

    narrow patches

--- tests/datasource/fluent/test_sql_datasources.py
+++ tests/datasource/fluent/test_sql_datasources.py
@@ -74,20 +74,19 @@ def sql_datasource(
 
 
 @pytest.fixture
-def sql_datasource_test_connection_noop(
+def sql_datasource_table_asset_test_connection_noop(
     monkeypatch: pytest.MonkeyPatch, sql_datasource: SQLDatasource
 ) -> SQLDatasource:
     """
-    Patch and return the sql_datasource fixture `SQLDatasource.test_connection()` and `TableAsset.test_connection()`
-    to be noops and always pass.
+    Patch and return the sql_datasource fixture `TableAsset.test_connection()`
+    to be a noop and always pass.
     """
 
     LOGGER.warning(f"Patching {sql_datasource.name} `.test_connection()` to a noop")
 
     def noop(self: SQLDatasource | TableAsset):
-        LOGGER.warning(f"{self.__class__.__name__}.test_connection noop")
+        LOGGER.warning(f"{self.__class__.__name__}.test_connection noop called")
 
-    # monkeypatch.setattr(SQLDatasource, "test_connection", noop, raising=True)
     monkeypatch.setattr(TableAsset, "test_connection", noop, raising=True)
     return sql_datasource
 
@@ -421,10 +420,12 @@ class TestTableAsset:
     @pytest.mark.parametrize("schema_name", ["my_schema", "MY_SCHEMA", "My_Schema"])
     def test_unquoted_schema_names_are_added_as_lowercase(
         self,
-        sql_datasource_noop_test_connection: SQLDatasource,
+        sql_datasource_table_asset_test_connection_noop: SQLDatasource,
         schema_name: str,
     ):
-        table_asset = sql_datasource_noop_test_connection.add_table_asset(
+        my_datasource: SQLDatasource = sql_datasource_table_asset_test_connection_noop
+
+        table_asset = my_datasource.add_table_asset(
             name="my_table_asset",
             table_name="my_table",
             schema_name=schema_name,
@@ -444,10 +445,12 @@ class TestTableAsset:
     )
     def test_quoted_schema_names_are_not_modified(
         self,
-        sql_datasource_noop_test_connection: SQLDatasource,
+        sql_datasource_table_asset_test_connection_noop: SQLDatasource,
         schema_name: str,
     ):
-        table_asset = sql_datasource_noop_test_connection.add_table_asset(
+        my_datasource: SQLDatasource = sql_datasource_table_asset_test_connection_noop
+
+        table_asset = my_datasource.add_table_asset(
             name="my_table_asset",
             table_name="my_table",
             schema_name=schema_name,
