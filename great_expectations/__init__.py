4638748568546935b5a158bdc516b480c4df08d4
Bill Dirks <bill@greatexpectations.io>
2024-07-24 13:23:16 -0500

    Add top level symbols to top level __init__.py file.

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -1,36 +1,37 @@
 # isort:skip_file
 
 # Set up version information immediately
-from ._version import get_versions  # isort:skip
+from ._version import get_versions as _get_versions
 
-__version__ = get_versions()["version"]  # isort:skip
+__version__ = _get_versions()["version"]
+del _get_versions
 
-# This brings in compatibility, data_context, exceptions, get_context
-from great_expectations.data_context.data_context.context_factory import get_context
+# great_expectations.data_context must be imported first or we will have circular dependency issues
+# I believe this is a bad import interplay between of Checkpoints and Renderers.
+import great_expectations.data_context  # isort:skip
 import great_expectations.core
 
-# Checkpoint must be imported after get_context and great_expectations.core
-from great_expectations.checkpoint.checkpoint import Checkpoint
+from great_expectations.data_context.data_context.context_factory import get_context
 
-# from great_expectations.data_context.migrator.cloud_migrator import CloudMigrator
-# from great_expectations.expectations.registry import (
-#     register_core_expectations,
-#     register_core_metrics,
-# )
-
-del get_versions  # isort:skip
-
-# from great_expectations.core.expectation_suite import ExpectationSuite
-# from great_expectations import get_context, project_manager, set_context
-#
 # # By placing this registry function in our top-level __init__,  we ensure that all
 # # GX workflows have populated expectation registries before they are used.
-# #
-# # Both of the following import paths will trigger this file, causing the registration to occur:
-# #   import great_expectations as gx
-# #   from great_expectations.core.expectation_suite import ExpectationSuite
-# #   from great_expectations.expectations.expectation_configuration import ExpectationConfiguration
-# register_core_metrics()
-# register_core_expectations()
+from great_expectations.expectations.registry import (
+    register_core_expectations as _register_core_expectations,
+    register_core_metrics as _register_core_metrics,
+)
 
-# rtd_url_ge_version = __version__.replace(".", "_")
+_register_core_metrics()
+_register_core_expectations()
+
+del _register_core_metrics
+del _register_core_expectations
+
+from great_expectations.checkpoint import actions
+from great_expectations import exceptions
+from great_expectations import expectations
+
+from great_expectations.checkpoint.checkpoint import Checkpoint
+from great_expectations.core.expectation_suite import ExpectationSuite
+from great_expectations.core.result_format import ResultFormat
+from great_expectations.core.run_identifier import RunIdentifier
+from great_expectations.core.validation_definition import ValidationDefinition
