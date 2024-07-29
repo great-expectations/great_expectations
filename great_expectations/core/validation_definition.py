da63ef4f98525c95890107ea410be68934ee8770
Bill Dirks <bill@greatexpectations.io>
2024-07-29 15:24:12 -0500

    [FEATURE] Clean up import structure for gx (#10146)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- great_expectations/core/validation_definition.py
+++ great_expectations/core/validation_definition.py
@@ -281,7 +281,7 @@ class ValidationDefinition(BaseModel):
 
     @public_api
     def save(self) -> None:
-        from great_expectations import project_manager
+        from great_expectations.data_context.data_context.context_factory import project_manager
 
         store = project_manager.get_validation_definition_store()
         key = store.get_key(name=self.name, id=self.id)
@@ -294,7 +294,7 @@ class ValidationDefinition(BaseModel):
         We need to persist a validation_definition before it can be run. If user calls runs but
         hasn't persisted it we add it for them."""
 
-        from great_expectations import project_manager
+        from great_expectations.data_context.data_context.context_factory import project_manager
 
         store = project_manager.get_validation_definition_store()
         key = store.get_key(name=self.name, id=self.id)
