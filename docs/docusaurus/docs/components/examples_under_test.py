213346c9639cc391569aec4d3d85674d65da9a3e
Rachel-Reverie <rachel@superconductive.com>
2024-08-08 14:54:17 -0500

    Puts connect to filesystem data > local or networked > spark/pandas docs scripts under test.

--- docs/docusaurus/docs/components/examples_under_test.py
+++ docs/docusaurus/docs/components/examples_under_test.py
@@ -8,12 +8,65 @@ from tests.integration.integration_test_fixture import IntegrationTestFixture
 
 docs_tests = []
 
+connect_to_filesystem_data_create_a_data_source = [
+    # Local, pandas/spark
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_datasource_filesystem_local_pandas" tests/integration/test_script_runner.py
+        name="create_a_datasource_filesystem_local_pandas",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_pandas.py",
+        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
+        backend_dependencies=[],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_datasource_filesystem_local_spark" tests/integration/test_script_runner.py
+        name="create_a_datasource_filesystem_local_spark",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_spark.py",
+        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+        backend_dependencies=[],
+    ),
+    # # ABS, pandas/spark
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_abs_pandas",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_abs/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_local_spark",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_abs/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+    # # GCS, pandas/spark
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_gcs_pandas",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_gcs/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_gcs_spark",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_gcs/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+    # # S3, pandas/spark
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_s3_pandas",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_s3/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+    # IntegrationTestFixture(
+    #     name="create_a_datasource_filesystem_s3_spark",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_s3/_spark.py",
+    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+    # ),
+]
+
 # TODO: As we get these example tests working, uncomment/update to add them to CI.
 connecting_to_a_datasource = [
     # # Create a Data Source
     # IntegrationTestFixture(
     #     name="create_a_datasource_postgres",
-    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_data_source/postgres.py",
+    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_spark.py",
     #     data_context_dir="docs/docusaurus/docs/components/_testing/create_datasource/great_expectations/",
     #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
     #     util_script="tests/test_utils.py",
@@ -141,6 +194,7 @@ learn_data_quality_use_cases = [
 
 # Extend the docs_tests list with the above sublists (only the docs_tests list is imported
 # into `test_script_runner.py` and actually used in CI checks).
+docs_tests.extend(connect_to_filesystem_data_create_a_data_source)
 docs_tests.extend(connecting_to_a_datasource)
 docs_tests.extend(learn_data_quality_use_cases)
 docs_tests.extend(expectation_tests)
