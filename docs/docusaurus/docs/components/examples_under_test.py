8207b17dc58554f21897b1c21bd7fcaf1c13d96f
Rachel-Reverie <rachel@superconductive.com>
2024-08-09 08:55:05 -0500

    Puts "connect to data asset" code for Local and Azure blob storage under test. Puts "create a batch definition" code for file and directory Batch Definitions (standing in for all filesystem environment data assets) under test.

--- docs/docusaurus/docs/components/examples_under_test.py
+++ docs/docusaurus/docs/components/examples_under_test.py
@@ -12,27 +12,26 @@ connect_to_filesystem_data_create_a_data_source = [
     # Local, pandas/spark
     IntegrationTestFixture(
         # To test, run:
-        # pytest --docs-tests -k "create_a_datasource_filesystem_local_pandas" tests/integration/test_script_runner.py
-        name="create_a_datasource_filesystem_local_pandas",
+        # pytest --docs-tests -k "create_a_data_source_filesystem_local_pandas" tests/integration/test_script_runner.py
+        name="create_a_data_source_filesystem_local_pandas",
         user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_pandas.py",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/first_3_files",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/empty_data_folder",
         backend_dependencies=[],
     ),
     IntegrationTestFixture(
         # To test, run:
-        # pytest --docs-tests --spark -k "create_a_datasource_filesystem_local_spark" tests/integration/test_script_runner.py
-        name="create_a_datasource_filesystem_local_spark",
+        # pytest --docs-tests --spark -k "create_a_data_source_filesystem_local_spark" tests/integration/test_script_runner.py
+        name="create_a_data_source_filesystem_local_spark",
         user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_spark.py",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/empty_data_folder",
         backend_dependencies=[BackendDependencies.SPARK],
     ),
     # ABS, pandas/spark
     IntegrationTestFixture(
         # To test, run:
-        # pytest --docs-tests --azure -k "create_a_datasource_filesystem_abs_pandas" tests/integration/test_script_runner.py
-        name="create_a_datasource_filesystem_abs_pandas",
+        # pytest --docs-tests --azure -k "create_a_data_source_filesystem_abs_pandas" tests/integration/test_script_runner.py
+        name="create_a_data_source_filesystem_abs_pandas",
         user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_abs/_pandas.py",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
         backend_dependencies=[BackendDependencies.AZURE],
     ),
     IntegrationTestFixture(
@@ -40,7 +39,6 @@ connect_to_filesystem_data_create_a_data_source = [
         # pytest --docs-tests --azure --spark -k "create_a_datasource_filesystem_abs_spark" tests/integration/test_script_runner.py
         name="create_a_datasource_filesystem_abs_spark",
         user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_abs/_spark.py",
-        data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
         backend_dependencies=[BackendDependencies.AZURE, BackendDependencies.SPARK],
     ),
     # # GCS, pandas/spark
@@ -67,111 +65,111 @@ connect_to_filesystem_data_create_a_data_source = [
     # ),
 ]
 
-# TODO: As we get these example tests working, uncomment/update to add them to CI.
-connecting_to_a_datasource = [
-    # # Create a Data Source
-    # IntegrationTestFixture(
-    #     name="create_a_datasource_postgres",
-    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_source/_local_or_networked/_spark.py",
-    #     data_context_dir="docs/docusaurus/docs/components/_testing/create_datasource/great_expectations/",
-    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
-    #     util_script="tests/test_utils.py",
-    #     other_files=(
-    #         (
-    #             "tests/integration/fixtures/partition_and_sample_data/postgres_connection_string.yml",
-    #             "connection_string.yml",
-    #         ),
-    #     ),
-    #     backend_dependencies=[BackendDependencies.POSTGRESQL],
-    # ),
-    # # Create a Data Asset
-    # IntegrationTestFixture(
-    #     name="create_a_data_asset_postgres",
-    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_data_asset/create_a_data_asset.py",
-    #     data_context_dir="docs/docusaurus/docs/components/_testing/create_datasource/great_expectations/",
-    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/",
-    #     util_script="tests/test_utils.py",
-    #     other_files=(
-    #         (
-    #             "tests/integration/fixtures/partition_and_sample_data/postgres_connection_string.yml",
-    #             "connection_string.yml",
-    #         ),
-    #     ),
-    #     backend_dependencies=[BackendDependencies.POSTGRESQL],
-    # ),
-    # # Create a Batch Definition
-    # IntegrationTestFixture(
-    #     name="create_a_batch_definition_postgres",
-    #     user_flow_script="docs/docusaurus/docs/core/connect_to_data/sql_data/_create_a_batch_definition/create_a_batch_definition.py",
-    #     data_context_dir="docs/docusaurus/docs/components/_testing/create_datasource/great_expectations/",
-    #     data_dir="tests/test_sets/taxi_yellow_tripdata_samples/samples_2020",
-    #     util_script="tests/test_utils.py",
-    #     other_files=(
-    #         (
-    #             "tests/integration/fixtures/partition_and_sample_data/postgres_connection_string.yml",
-    #             "connection_string.yml",
-    #         ),
-    #     ),
-    #     backend_dependencies=[BackendDependencies.POSTGRESQL],
-    # ),
+connect_to_filesystem_data_create_a_data_asset = [
+    # local, directory asset/file asset
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_data_asset_filesystem_local_file_asset" tests/integration/test_script_runner.py
+        name="create_a_data_asset_filesystem_local_file_asset",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_asset/_file_asset.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/single_test_file",
+        backend_dependencies=[],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --spark -k "create_a_data_asset_filesystem_local_directory_asset" tests/integration/test_script_runner.py
+        name="create_a_data_asset_filesystem_local_directory_asset",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_asset/_local_or_networked/_directory_asset.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/three_test_files",
+        backend_dependencies=[BackendDependencies.SPARK],
+    ),
+    # ABS, directory asset/file asset
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --azure -k "create_a_data_asset_filesystem_abs_file_asset" tests/integration/test_script_runner.py
+        name="create_a_data_asset_filesystem_abs_file_asset",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_asset/_abs/_file_asset.py",
+        backend_dependencies=[BackendDependencies.AZURE],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --azure --spark -k "create_a_data_asset_filesystem_abs_directory_asset" tests/integration/test_script_runner.py
+        name="create_a_data_asset_filesystem_abs_directory_asset",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_data_asset/_abs/_directory_asset.py",
+        backend_dependencies=[BackendDependencies.AZURE, BackendDependencies.SPARK],
+    ),
+    # GCS, directory asset/file asset
+    # S3, directory asset/file asset
 ]
 
-# TODO: As we get these example tests working, uncomment/update to add them to CI.
-expectation_tests = [
-    # # Expectation example scripts
-    # IntegrationTestFixture(
-    #     name="create_an_expectation.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectations/_examples/create_an_expectation.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="edit_an_expectation.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectations/_examples/edit_an_expectation.py",
-    #     backend_dependencies=[],
-    # ),
-    # # Expectation Suite example scripts
-    # IntegrationTestFixture(
-    #     name="add_expectations_to_an_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/add_expectations_to_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="create_an_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/create_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="delete_an_expectation_in_an_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/delete_an_expectation_in_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="delete_an_expectation_suite",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/delete_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="edit_a_single_expectation.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/edit_a_single_expectation.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="edit_all_expectations_in_an_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/edit_all_expectations_in_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="get_a_specific_expectation_from_an_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/get_a_specific_expectation_from_an_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
-    # IntegrationTestFixture(
-    #     name="get_an_existing_expectation_suite.py",
-    #     user_flow_script="docs/docusaurus/docs/core/_create_expectations/expectation_suites/_examples/get_an_existing_expectation_suite.py",
-    #     backend_dependencies=[],
-    # ),
+connect_to_filesystem_data_create_a_batch_definition = [
+    # directory, whole/daily/monthly/yearly
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --spark -k "create_a_batch_definition_filesystem_directory_whole" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_directory_whole",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_whole_directory.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/three_test_files",
+        backend_dependencies=[BackendDependencies.SPARK],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --spark -k "create_a_batch_definition_filesystem_directory_daily" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_directory_daily",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_partitioned_daily.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/three_test_files",
+        backend_dependencies=[BackendDependencies.SPARK],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --spark -k "create_a_batch_definition_filesystem_directory_monthly" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_directory_monthly",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_partitioned_monthly.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/three_test_files",
+        backend_dependencies=[BackendDependencies.SPARK],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests --spark -k "create_a_batch_definition_filesystem_directory_yearly" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_directory_yearly",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_partitioned_yearly.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/three_test_files",
+        backend_dependencies=[BackendDependencies.SPARK],
+    ),
+    # file, path/daily/monthly/yearly
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_batch_definition_filesystem_file_path" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_file_path",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_path.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/single_test_file",
+        backend_dependencies=[],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_batch_definition_filesystem_file_daily" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_file_daily",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/single_test_file",
+        backend_dependencies=[],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_batch_definition_filesystem_file_monthly" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_file_monthly",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_monthly.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/single_test_file",
+        backend_dependencies=[],
+    ),
+    IntegrationTestFixture(
+        # To test, run:
+        # pytest --docs-tests -k "create_a_batch_definition_filesystem_file_yearly" tests/integration/test_script_runner.py
+        name="create_a_batch_definition_filesystem_file_yearly",
+        user_flow_script="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_yearly.py",
+        data_dir="docs/docusaurus/docs/components/_testing/test_data_sets/single_test_file",
+        backend_dependencies=[],
+    ),
 ]
-
 
 learn_data_quality_use_cases = [
     # Schema.
@@ -201,6 +199,7 @@ learn_data_quality_use_cases = [
 # Extend the docs_tests list with the above sublists (only the docs_tests list is imported
 # into `test_script_runner.py` and actually used in CI checks).
 docs_tests.extend(connect_to_filesystem_data_create_a_data_source)
-docs_tests.extend(connecting_to_a_datasource)
+docs_tests.extend(connect_to_filesystem_data_create_a_data_asset)
+docs_tests.extend(connect_to_filesystem_data_create_a_batch_definition)
+
 docs_tests.extend(learn_data_quality_use_cases)
-docs_tests.extend(expectation_tests)
