37dc07012b9d706e4dfa62d1dd06373443397556
Anthony Burdi <anthony@greatexpectations.io>
2024-08-13 17:53:13 -0400

    Add test

--- tests/experimental/metric_repository/test_cloud_data_store.py
+++ tests/experimental/metric_repository/test_cloud_data_store.py
@@ -1,4 +1,5 @@
 import uuid
+from unittest import mock
 from unittest.mock import Mock  # noqa: TID251
 from uuid import UUID
 
@@ -39,10 +40,10 @@ class TestCloudDataStoreMetricRun:
                 )
             ],
         )
-        cloud_data_store._session = Mock()
-        cloud_data_store._session.post = Mock()
+        cloud_data_store.session = Mock()
+        cloud_data_store.session.post = Mock()
         response_mock = Mock()
-        cloud_data_store._session.post.return_value = response_mock
+        cloud_data_store.session.post.return_value = response_mock
 
         response_metric_run_id = uuid.uuid4()
         response_metric_id = uuid.uuid4()
@@ -69,7 +70,7 @@ class TestCloudDataStoreMetricRun:
 
         expected_data = '{"data":{"type":"metric-run","attributes":{"data_asset_id":"4469ed3b-61d4-421f-9635-8339d2558b0f","metrics":[{"batch_id":"batch_id","metric_name":"metric_name","value":[0.25,0.5,0.75],"exception":null,"column":"column","quantiles":[0.25,0.5,0.75],"allow_relative_error":0.001,"value_type":"list[float]","metric_type":"ColumnQuantileValuesMetric"}]}}}'  # noqa: E501
 
-        cloud_data_store._session.post.assert_called_once_with(
+        cloud_data_store.session.post.assert_called_once_with(
             url="https://app.greatexpectations.fake.io/organizations/12345678-1234-5678-1234-567812345678/metric-runs",
             data=expected_data,
         )
@@ -93,10 +94,10 @@ class TestCloudDataStoreMetricRun:
                 )
             ],
         )
-        cloud_data_store._session = Mock()
-        cloud_data_store._session.post = Mock()
+        cloud_data_store.session = Mock()
+        cloud_data_store.session.post = Mock()
         response_mock = Mock()
-        cloud_data_store._session.post.return_value = response_mock
+        cloud_data_store.session.post.return_value = response_mock
 
         response_metric_run_id = uuid.uuid4()
         response_metric_id = uuid.uuid4()
@@ -121,7 +122,7 @@ class TestCloudDataStoreMetricRun:
 
         expected_data = '{"data":{"type":"metric-run","attributes":{"data_asset_id":"4469ed3b-61d4-421f-9635-8339d2558b0f","metrics":[{"batch_id":"batch_id","metric_name":"metric_name","value":1,"exception":null,"column":"column","value_type":"int","metric_type":"ColumnMetric"}]}}}'  # noqa: E501
 
-        cloud_data_store._session.post.assert_called_once_with(
+        cloud_data_store.session.post.assert_called_once_with(
             url="https://app.greatexpectations.fake.io/organizations/12345678-1234-5678-1234-567812345678/metric-runs",
             data=expected_data,
         )
@@ -145,10 +146,10 @@ class TestCloudDataStoreMetricRun:
                 )
             ],
         )
-        cloud_data_store._session = Mock()
-        cloud_data_store._session.post = Mock()
+        cloud_data_store.session = Mock()
+        cloud_data_store.session.post = Mock()
         response_mock = Mock()
-        cloud_data_store._session.post.return_value = response_mock
+        cloud_data_store.session.post.return_value = response_mock
 
         response_metric_run_id = uuid.uuid4()
         response_metric_id = uuid.uuid4()
@@ -176,7 +177,7 @@ class TestCloudDataStoreMetricRun:
 
         expected_data = '{"data":{"type":"metric-run","attributes":{"data_asset_id":"4469ed3b-61d4-421f-9635-8339d2558b0f","metrics":[{"batch_id":"batch_id","metric_name":"metric_name","value":1,"exception":{"type":"exception type","message":"exception message"},"column":"column","value_type":"int","metric_type":"ColumnMetric"}]}}}'  # noqa: E501
 
-        cloud_data_store._session.post.assert_called_once_with(
+        cloud_data_store.session.post.assert_called_once_with(
             url="https://app.greatexpectations.fake.io/organizations/12345678-1234-5678-1234-567812345678/metric-runs",
             data=expected_data,
         )
@@ -200,10 +201,10 @@ class TestCloudDataStoreMetricRun:
                 )
             ],
         )
-        cloud_data_store._session = Mock()
-        cloud_data_store._session.post = Mock()
+        cloud_data_store.session = Mock()
+        cloud_data_store.session.post = Mock()
         response_mock = Mock()
-        cloud_data_store._session.post.return_value = response_mock
+        cloud_data_store.session.post.return_value = response_mock
 
         response_metric_run_id = uuid.uuid4()
         response_metric_id = uuid.uuid4()
@@ -228,8 +229,21 @@ class TestCloudDataStoreMetricRun:
 
         expected_data = '{"data":{"type":"metric-run","attributes":{"data_asset_id":"4469ed3b-61d4-421f-9635-8339d2558b0f","metrics":[{"batch_id":"batch_id","metric_name":"metric_name","value":2.5,"exception":null,"column":"column","value_type":"float64","metric_type":"ColumnMetric"}]}}}'  # noqa: E501
 
-        cloud_data_store._session.post.assert_called_once_with(
+        cloud_data_store.session.post.assert_called_once_with(
             url="https://app.greatexpectations.fake.io/organizations/12345678-1234-5678-1234-567812345678/metric-runs",
             data=expected_data,
         )
         assert uuid_from_add == response_metric_run_id
+
+
+def test_closes_session(
+    empty_cloud_context_fluent: CloudDataContext,  # used as a fixture
+):
+    """Make sure that when the CloudDataStore object is garbage collected,
+    the session is closed."""
+    cloud_data_store = CloudDataStore(context=empty_cloud_context_fluent)
+    with mock.patch("requests.Session.close", autospec=True) as mock_close:
+        # Use the finalizer to remove the object from memory.
+        # Using del or __del__ directly does not work in the test environment.
+        cloud_data_store._finalizer()
+        mock_close.assert_called_once()
