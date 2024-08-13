6c5028a3f3a9bd3f3ff86cd2c903fe4ae89c57b9
Anthony Burdi <anthony@greatexpectations.io>
2024-08-13 13:59:50 -0400

    Use weakref.finalize

--- great_expectations/data_context/store/gx_cloud_store_backend.py
+++ great_expectations/data_context/store/gx_cloud_store_backend.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import logging
+import weakref
 from abc import ABCMeta
 from enum import Enum
 from typing import Any, Dict, List, Optional, Set, Tuple, Union
@@ -161,6 +162,12 @@ class GXCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
             _ = self.store_backend_id
 
         self._session = create_session(access_token=self._ge_cloud_credentials["access_token"])
+        # Finalizer to close the session when the object is garbage collected.
+        # https://docs.python.org/3.11/library/weakref.html#weakref.finalize
+        self._finalizer = weakref.finalize(
+            self,
+            self._close_session,
+        )
 
         # Gather the call arguments of the present function (include the "module_name" and add the "class_name"), filter  # noqa: E501
         # out the Falsy values, and set the instance "_config" variable equal to the resulting dictionary.  # noqa: E501
@@ -334,7 +341,8 @@ class GXCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
             return True
         if not (kwarg_names <= self.allowed_set_kwargs):
             extra_kwargs = kwarg_names - self.allowed_set_kwargs
-            raise ValueError(f'Invalid kwargs: {(", ").join(extra_kwargs)}')  # noqa: TRY003
+            error_text = f'Invalid kwargs: {(", ").join(extra_kwargs)}'
+            raise ValueError(error_text)
         return None
 
     @override
@@ -407,7 +415,8 @@ class GXCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
             ) from timeout_exc
         except Exception as e:
             logger.debug(str(e))
-            raise StoreBackendError(f"Unable to set object in GX Cloud Store Backend: {e}") from e  # noqa: TRY003
+            error_text = f"Unable to set object in GX Cloud Store Backend: {e}"
+            raise StoreBackendError(error_text) from e
 
     @property
     def ge_cloud_base_url(self) -> str:
@@ -454,7 +463,8 @@ class GXCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
             return keys
         except Exception as e:
             logger.debug(str(e))
-            raise StoreBackendError(f"Unable to list keys in GX Cloud Store Backend: {e}") from e  # noqa: TRY003
+            error_text = f"Unable to list keys in GX Cloud Store Backend: {e}"
+            raise StoreBackendError(error_text) from e
 
     @override
     def get_url_for_key(  # type: ignore[override]
@@ -727,3 +737,6 @@ class GXCloudStoreBackend(StoreBackend, metaclass=ABCMeta):
     @property
     def _is_v1_resource(self) -> bool:
         return self._ENDPOINT_VERSION_LOOKUP.get(self.ge_cloud_resource_type) == EndpointVersion.V1
+
+    def _close_session(self):
+        self._session.close()
