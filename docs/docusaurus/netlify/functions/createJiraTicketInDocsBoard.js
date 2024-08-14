2425bd0428ef389325bd18ba47e86564a76c8fdc
JessSaavedra <jessicasaavedraval@gmail.com>
2024-08-14 13:01:45 -0300

    Transition feedback Jira tickets to kanban board

--- docs/docusaurus/netlify/functions/createJiraTicketInDocsBoard.js
+++ docs/docusaurus/netlify/functions/createJiraTicketInDocsBoard.js
@@ -1,7 +1,8 @@
 const TITLE_MAX_CHARACTERS = 120;
 const DOCUMENTATION_BOARD_ID = 10019;
 const STORY_ISSUETYPE_ID = 10011;
-const CREATE_JIRA_ISSUE_ENDPOINT_URL = "https://greatexpectations.atlassian.net/rest/api/2/issue"
+const JIRA_ISSUE_ENDPOINT_URL = "https://greatexpectations.atlassian.net/rest/api/2/issue"
+const TO_DO_STATE_ID = 291;
 
 const truncateDescription = (description) => {
     return description.length > TITLE_MAX_CHARACTERS ? description.substring(0, TITLE_MAX_CHARACTERS) + "..." : description;
@@ -14,33 +15,43 @@ const fullDescription = (description, name, email, selectedValue) => {
     return `*Name:* ${formatOptionalValue(name)}\n*Email:* ${formatOptionalValue(email)}\n*Selected feedback type:* ${formattedSelectedValue}\n\n*Description:* ${description}`;
 }
 
+const httpPostRequest = async (url, body) => {
+    return await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + Buffer.from(process.env.JIRA_API_USER + ":" + process.env.JIRA_API_TOKEN).toString('base64')
+        },
+        body: JSON.stringify(body)
+    })
+}
+
 exports.handler = async (req, context) => {
     const { description, name, email, selectedValue } = JSON.parse(req.body);
     try {
-        const response = await fetch(CREATE_JIRA_ISSUE_ENDPOINT_URL, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + Buffer.from(process.env.JIRA_API_USER + ":" + process.env.JIRA_API_TOKEN).toString('base64')
-            },
-            body: JSON.stringify({
-                "fields": {
-                    "project": {
-                        "id": DOCUMENTATION_BOARD_ID
-                    },
-                    "summary": truncateDescription(description),
-                    "description": fullDescription(description, name, email, selectedValue),
-                    "issuetype": {
-                        "id": STORY_ISSUETYPE_ID
-                    },
-                    "labels": [
-                        "feedback_modal"
-                    ],
-                }
-            })
+        const response = await httpPostRequest(JIRA_ISSUE_ENDPOINT_URL, {
+            "fields": {
+                "project": {
+                    "id": DOCUMENTATION_BOARD_ID
+                },
+                "summary": truncateDescription(description),
+                "description": fullDescription(description, name, email, selectedValue),
+                "issuetype": {
+                    "id": STORY_ISSUETYPE_ID
+                },
+                "labels": [
+                    "feedback_modal"
+                ],
+            }
         })
 
         const result = await response.json()
+
+        await httpPostRequest(`${JIRA_ISSUE_ENDPOINT_URL}/${result.id}/transitions`, {
+            "transition": {
+                "id": TO_DO_STATE_ID
+            }
+        })
 
         return {
             statusCode: response.status,
