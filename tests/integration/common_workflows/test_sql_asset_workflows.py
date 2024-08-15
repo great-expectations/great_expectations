c14813d5ab013532ad391920b2b128212a0b8056
Chetan Kini <chetan@superconductive.com>
2024-08-15 18:15:54 -0400

    one more

--- tests/integration/common_workflows/test_sql_asset_workflows.py
+++ tests/integration/common_workflows/test_sql_asset_workflows.py
@@ -161,9 +161,9 @@ def test_validation_definition_run(
     """Ensure ValidationDefinition::run works"""
 
     batch_definition = request.getfixturevalue(batch_definition_fixture_name)
-    suite = ExpectationSuite("my_suite", expectations=[expectation])
-    validation_definition = ValidationDefinition(
-        name="whatever", data=batch_definition, suite=suite
+    suite = context.suites.add(ExpectationSuite("my_suite", expectations=[expectation]))
+    validation_definition = context.validation_definitions.add(
+        ValidationDefinition(name="whatever", data=batch_definition, suite=suite)
     )
     result = validation_definition.run(batch_parameters=batch_parameters)
 
