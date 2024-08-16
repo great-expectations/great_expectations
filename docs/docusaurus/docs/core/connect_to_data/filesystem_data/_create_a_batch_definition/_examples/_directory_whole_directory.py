511a5998b3237241e7a66a741ab49639af6e660c
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-08-16 12:28:54 -0400

    try ephemeral context

--- docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_whole_directory.py
+++ docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_whole_directory.py
@@ -4,20 +4,14 @@ from pathlib import Path
 
 import great_expectations as gx
 
-context = gx.get_context()
+context = gx.get_context(mode="ephemeral")
 
 data_source_name = "my_filesystem_data_source"
 data_asset_name = "my_directory_data_asset"
-file_data_asset = context.data_sources.get(data_source_name).get_asset(data_asset_name)
+# file_data_asset = context.data_sources.get(data_source_name).get_asset(data_asset_name)
+data_source = context.data_sources.add_spark_filesystem(data_source_name, base_directory=Path("data"))
+file_data_asset = data_source.add_directory_csv_asset(data_asset_name, data_directory=Path("folder_with_data"), header=True)
 # </snippet>
-
-print("--- ci debug ----")
-print(file_data_asset)
-ds = context.data_sources.get(data_source_name)
-print(ds)
-expected_data_dir = ds.base_directory / file_data_asset.data_directory
-print(f"does data source data_directory exist? {expected_data_dir}")
-print(Path.exists(expected_data_dir))
 
 # <snippet name="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_directory_whole_directory.py - add Batch Definition">
 batch_definition_name = "yellow_tripdata"
--- docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py
+++ docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py
@@ -1,12 +1,16 @@
 # <snippet name="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py - full_example">
+from pathlib import Path
+
 import great_expectations as gx
 
-context = gx.get_context()
+context = gx.get_context(mode="ephemeral")
 
 # <snippet name="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py - retrieve Data Asset">
 data_source_name = "my_filesystem_data_source"
 data_asset_name = "my_file_data_asset"
-file_data_asset = context.data_sources.get(data_source_name).get_asset(data_asset_name)
+# file_data_asset = context.data_sources.get(data_source_name).get_asset(data_asset_name)
+data_source = context.data_sources.add_spark_filesystem(data_source_name, base_directory=Path("data"))
+file_data_asset = data_source.add_directory_csv_asset(data_asset_name, data_directory=Path("folder_with_data"))
 # </snippet>
 
 # <snippet name="docs/docusaurus/docs/core/connect_to_data/filesystem_data/_create_a_batch_definition/_examples/_file_partitioned_daily.py - add Batch Definition">
@@ -14,7 +18,7 @@ batch_definition_name = "daily_yellow_tripdata_sample"
 batch_definition_regex = r"folder_with_data/yellow_tripdata_sample_(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})\.csv"
 
 batch_definition = file_data_asset.add_batch_definition_daily(
-    name=batch_definition_name, regex=batch_definition_regex
+    name=batch_definition_name, column="pickup_datetime"
 )
 # </snippet>
 
