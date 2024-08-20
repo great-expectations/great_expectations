95d96129205b6e74ac3f9300456341d97ebf0373
Deborah Niesz <deborah.niesz@10pines.com>
2024-08-20 18:07:47 -0300

    fix bug in github component

--- docs/docusaurus/src/components/NavbarItems/GithubNavbarItem/index.js
+++ docs/docusaurus/src/components/NavbarItems/GithubNavbarItem/index.js
@@ -10,21 +10,6 @@ export default function GithubNavbarItem({ owner, repository, className }) {
     const [forksCount, setForksCount] = useState('0');
     const [showGithubBadgeInfo, setShowGithubBadgeInfo] = useState(true);
 
-    const [colorCode, setColorCode] = useState('');
-    const [githubMarkImg, setGithubMarkImg] = useState(useBaseUrl(`img/github-mark.svg`));
-    const [githubLogoImg, setGithubLogoImg] = useState(useBaseUrl(`img/github.svg`));
-    const [starIcon, setStarIcon] = useState(useBaseUrl(`img/star.svg`));
-    const [forkIcon, setForkIcon] = useState(useBaseUrl(`img/code-branch.svg`));
-
-    useEffect(() => {
-        setColorCode(colorMode === 'dark' ? '-dark' : '');
-        setGithubMarkImg(useBaseUrl(`img/github-mark${colorCode}.svg`));
-        setGithubLogoImg(useBaseUrl(`img/github${colorCode}.svg`));
-        setStarIcon(useBaseUrl(`img/star${colorCode}.svg`));
-        setForkIcon(useBaseUrl(`img/code-branch${colorCode}.svg`));
-    }, [colorMode]);
-
-
     useEffect(() => {
 
         fetch(`https://api.github.com/repos/${owner}/${repository}`)
@@ -43,23 +28,30 @@ export default function GithubNavbarItem({ owner, repository, className }) {
         return formatter.format(number).toLowerCase();
     }
 
-
+    const githubMarkImg = useBaseUrl(`img/github-mark.svg`);
+    const githubLogoImg = useBaseUrl(`img/github.svg`);
+    const starIcon = useBaseUrl(`img/star.svg`);
+    const forkIcon = useBaseUrl(`img/code-branch.svg`);
+    const githubMarkDarkImg = useBaseUrl(`img/github-mark-dark.svg`);
+    const githubLogoDarkImg = useBaseUrl(`img/github-dark.svg`);
+    const starDarkIcon = useBaseUrl(`img/star-dark.svg`);
+    const forkDarkIcon = useBaseUrl(`img/code-branch-dark.svg`);
 
     return repository && (
         <a href={`https://github.com/${owner}/${repository}`} target="_blank"
            className={ className + ' ' + styles.githubBadge + ' ' + (showGithubBadgeInfo ? styles.githubBadgeNoErrors : '')}>
-            <img src={githubMarkImg} className={styles.githubMark}
+            <img src={colorMode === 'dark' ? githubMarkDarkImg : githubMarkImg} className={styles.githubMark}
                  alt="Github Invertocat Logo"/>
             { showGithubBadgeInfo && (<div className={styles.githubBadgeInfo}>
-                <img src={githubLogoImg} className={styles.githubLogo}
+                <img src={colorMode === 'dark' ? githubLogoDarkImg : githubLogoImg} className={styles.githubLogo}
                      alt="Github Logo"/>
                 <div className={styles.githubStats}>
                     <span>
-                        <img src={starIcon} alt="Github Stargazers Count"/>
+                        <img src={colorMode === 'dark' ? starDarkIcon : starIcon} alt="Github Stargazers Count"/>
                         {starsCount}
                     </span>
                     <span>
-                        <img src={forkIcon} alt="Github Forks Count"/>
+                        <img src={colorMode === 'dark' ? forkDarkIcon : forkIcon} alt="Github Forks Count"/>
                         {forksCount}
                     </span>
                 </div>
