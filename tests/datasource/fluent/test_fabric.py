c14457d2ce6458c0ea5d98789e4bdeaf03db701f
Gabriel Gore <gabriel59kg@gmail.com>
2024-08-30 15:20:10 -0400

    minimal tests

--- tests/datasource/fluent/test_fabric.py
+++ tests/datasource/fluent/test_fabric.py
@@ -10,6 +10,8 @@ import pytest
 from packaging.version import Version
 
 import great_expectations.core.batch_spec
+from great_expectations.datasource.fluent import BatchRequest
+from great_expectations.exceptions.exceptions import BuildBatchRequestError
 from great_expectations.experimental.datasource.fabric import (
     FabricPowerBIDatasource,
     _PowerBIAsset,
@@ -136,6 +138,64 @@ class TestFabricPowerBI:
         print(f"keyword args:\n{pf(captured_kwargs[-1])}")
         for asset_kwarg in asset_kwargs:
             assert asset_kwarg in captured_kwargs[-1]
+
+    @pytest.mark.parametrize(
+        ["asset_type", "asset_kwargs"],
+        [
+            param("powerbi_dax", {"dax_string": "my_dax_string"}, id="dax min_args"),
+            param(
+                "powerbi_measure",
+                {
+                    "measure": "my_measure",
+                },
+                id="measure min_args",
+            ),
+            param(
+                "powerbi_measure",
+                {
+                    "measure": "my_measure",
+                    "groupby_columns": ["foo[Bar]", "'fizz with space'[Buzz]"],
+                    "filters": {
+                        "State[Region]": ["East", "Central"],
+                        "State[State]": ["WA", "CA"],
+                    },
+                    "fully_qualified_columns": True,
+                    "num_rows": 100,
+                    "use_xmla": True,
+                },
+                id="measure all_args",
+            ),
+            param("powerbi_table", {"table": "my_table_name"}, id="table min_args"),
+        ],
+    )
+    def test_valid_build_batch_request_params(
+        self,
+        power_bi_datasource: FabricPowerBIDatasource,
+        asset_type: Literal["powerbi_dax", "powerbi_measure", "powerbi_table"],
+        asset_kwargs: dict,
+    ):
+        """
+        Test that passing valid keyword parameters to `build_batch_request` does not error.
+
+        This test is a bit redundant since mypy should enforce that the method signature is correct,
+        but we experienced an error here as part of the initial 1.0 release.
+        """
+        add_asset_fn: Callable[[str], _PowerBIAsset] = getattr(
+            power_bi_datasource, f"add_{asset_type}_asset"
+        )
+        my_asset = add_asset_fn(f"my_{asset_type}_asset", **asset_kwargs)
+
+        batch_request = my_asset.build_batch_request(
+            options=None, batch_slice=None, partitioner=None
+        )
+        assert isinstance(batch_request, BatchRequest)
+
+    def test_invalid_build_batch_request_params(self, power_bi_datasource: FabricPowerBIDatasource):
+        """Test that passing invalid parameters to `build_batch_request` raises an error"""
+        with pytest.raises(BuildBatchRequestError):
+            power_bi_datasource.add_powerbi_dax_asset(
+                "my_dax_asset", dax_string="my_dax_string"
+            ).build_batch_request(options="not valid")
 
 
 if __name__ == "__main__":
