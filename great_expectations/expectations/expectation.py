52f122e85c2d5562ce8ece394a846ee8ed8d80dc
Farmer <nathan@greatexpectations.io>
2024-09-03 13:37:03 -0600

    Make description an instance var

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -313,7 +313,7 @@ class Expectation(pydantic.BaseModel, metaclass=MetaExpectation):
     meta: Union[dict, None] = None
     notes: Union[str, List[str], None] = None
     result_format: Union[ResultFormat, dict] = ResultFormat.BASIC
-    description: ClassVar[Union[str, None]] = None
+    description: Union[str, None] = None
 
     catch_exceptions: bool = False
     rendered_content: Optional[List[RenderedAtomicContent]] = None
