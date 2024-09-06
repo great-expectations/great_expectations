a0f502a51ada5ff1174004ea82cce3dda78a3e24
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2024-09-06 09:54:08 -0400

    [MAINTENANCE] Add `description` field to `ExpectationConfigurationSchema` and render always `description` over `template_str` (#10347)

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -474,7 +474,7 @@ class Expectation(metaclass=MetaExpectation):
         configuration: Optional[ExpectationConfiguration] = None,
         result: Optional[ExpectationValidationResult] = None,
         runtime_configuration: Optional[dict] = None,
-    ) -> Tuple[str, dict, MetaNotes, Optional[dict]]:
+    ) -> Tuple[Optional[str], dict, MetaNotes, Optional[dict]]:
         """
         Template function that contains the logic that is shared by AtomicPrescriptiveRendererType.SUMMARY and
         LegacyRendererType.PRESCRIPTIVE.
