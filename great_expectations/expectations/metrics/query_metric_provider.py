97678222204788697bb0d4be02e39a4b34b7d1fa
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2024-09-17 13:08:34 -0600

    [BUGFIX] Using `{batch}` keyword in `UnexpectedRowsQuery` (#10392) (#10411)

--- great_expectations/expectations/metrics/query_metric_provider.py
+++ great_expectations/expectations/metrics/query_metric_provider.py
@@ -1,12 +1,22 @@
 import logging
+from typing import ClassVar, Set, Union
 
 from great_expectations.core._docs_decorators import public_api
+from great_expectations.execution_engine.sqlalchemy_dialect import GXSqlDialect
 from great_expectations.expectations.metrics.metric_provider import MetricProvider
 
 logger = logging.getLogger(__name__)
 
 
 @public_api
+class MissingElementError(TypeError):
+    def __init__(self):
+        super().__init__(
+            "The batch_subquery selectable does not contain an "
+            "element from which query parameters can be extracted."
+        )
+
+
 class QueryMetricProvider(MetricProvider):
     """Base class for all Query Metrics, which define metrics to construct SQL queries.
 
@@ -28,3 +38,52 @@ class QueryMetricProvider(MetricProvider):
     """
 
     domain_keys = ("batch_id", "row_condition", "condition_parser")
+
+    dialect_columns_require_subquery_aliases: ClassVar[Set[GXSqlDialect]] = {
+        GXSqlDialect.POSTGRESQL
+    }
+
+    @classmethod
+    def _get_query_string_with_substituted_batch_parameters(
+        cls,
+        query: str,
+        batch_subquery: Union[  # type: ignore[name-defined]  # fixed in 1.0
+            "sa.sql.Subquery", "sa.sql.Alias"  # noqa: F821  # fixed in 1.0
+        ],
+    ) -> str:
+        """Specifying a runtime query string returns the active batch as a Subquery or Alias type
+        There is no object-based way to apply the subquery alias to columns in the SELECT and
+        WHERE clauses. Instead, we extract the subquery parameters from the batch selectable
+        and inject them into the SQL string.
+
+        Raises:
+            MissingElementError if the batch_subquery.selectable does not have an element
+            for which to extract query parameters.
+        """
+
+        try:
+            froms = batch_subquery.selectable.element.get_final_froms()
+            try:
+                batch_table = froms[0].name
+            except AttributeError:
+                batch_table = str(froms[0])
+            batch_filter = str(batch_subquery.selectable.element.whereclause)
+        except (AttributeError, IndexError) as e:
+            raise MissingElementError() from e
+
+        unfiltered_query = query.format(batch=batch_table)
+
+        if "WHERE" in query.upper():
+            query = unfiltered_query.replace("WHERE", f"WHERE {batch_filter} AND")
+        elif "GROUP BY" in query.upper():
+            query = unfiltered_query.replace(
+                "GROUP BY", f"WHERE {batch_filter} GROUP BY"
+            )
+        elif "ORDER BY" in query.upper():
+            query = unfiltered_query.replace(
+                "ORDER BY", f"WHERE {batch_filter} ORDER BY"
+            )
+        else:
+            query = unfiltered_query + f" WHERE {batch_filter}"
+
+        return query
