42d4338622073464a1af512c2c03025dce18cc48
Robby Reinold <rreinold@gmail.com>
2024-09-18 20:54:50 -0400

    update default

--- tests/expectations/core/test_expectation_serialization.py
+++ tests/expectations/core/test_expectation_serialization.py
@@ -19,7 +19,7 @@ from great_expectations.expectations import (
             (
                 '{"id": null, "meta": null, "notes": null, "result_format": "BASIC", '
                 '"description": null, "catch_exceptions": false, "rendered_content": null, '
-                '"windows": [], "batch_id": null, "row_condition": null, "condition_parser": '
+                '"windows": null, "batch_id": null, "row_condition": null, "condition_parser": '
                 'null, "column": "test_column", "min_value": 1.0, "max_value": null, '
                 '"strict_min": false, "strict_max": false}'
             ),
@@ -32,7 +32,7 @@ from great_expectations.expectations import (
             (
                 '{"id": null, "meta": null, "notes": null, "result_format": "BASIC", '
                 '"description": null, "catch_exceptions": true, "rendered_content": null, '
-                '"windows": [], "batch_id": null, "row_condition": null, "condition_parser": '
+                '"windows": null, "batch_id": null, "row_condition": null, "condition_parser": '
                 'null, "column": "test_column", "mostly": 0.82}'
             ),
         ),
@@ -44,7 +44,7 @@ from great_expectations.expectations import (
             (
                 '{"id": null, "meta": null, "notes": null, "result_format": "BASIC", '
                 '"description": "Data shouldn\'t be bad.", "catch_exceptions": false, '
-                '"rendered_content": null, "windows": [], "batch_id": null, "row_condition": '
+                '"rendered_content": null, "windows": null, "batch_id": null, "row_condition": '
                 'null, "condition_parser": null, "unexpected_rows_query": "SELECT * FROM '
                 "my_table WHERE data='bad'\"}"
             ),
