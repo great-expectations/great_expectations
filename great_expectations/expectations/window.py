42fd47762271f8011d123267754058bc9c09f324
Robby Reinold <rreinold@gmail.com>
2024-09-19 18:06:08 -0400

    add model definitions

--- great_expectations/expectations/window.py
+++ great_expectations/expectations/window.py
@@ -2,11 +2,19 @@ from great_expectations.compatibility import pydantic
 
 
 class Offset(pydantic.BaseModel):
+    """
+    A threshold in which a metric will be considered passable
+    """
+
     positive: float
     negative: float
 
 
 class Window(pydantic.BaseModel):
+    """
+    A definition for a temporal window across <`range`> number of previous invocations
+    """
+
     constraint_fn: str
     parameter_name: str
     range: int
