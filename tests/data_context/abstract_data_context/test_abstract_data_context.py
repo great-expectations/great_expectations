70a0d8a168744fb53c891fb24824373c0c80be45
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-09-30 20:24:38 -0400

    Clean up logic around fixtures

--- tests/data_context/abstract_data_context/test_abstract_data_context.py
+++ tests/data_context/abstract_data_context/test_abstract_data_context.py
@@ -1,46 +1,24 @@
-from typing import Type
-
 import pytest
-
-from great_expectations.data_context.data_context.abstract_data_context import AbstractDataContext
-from great_expectations.data_context.data_context.cloud_data_context import CloudDataContext
-from great_expectations.data_context.data_context.ephemeral_data_context import EphemeralDataContext
-from great_expectations.data_context.data_context.file_data_context import FileDataContext
 
 
 @pytest.mark.parametrize(
-    ("context_fixture_name", "concrete_class"),
+    "context_fixture_name",
     [
-        pytest.param(
-            "ephemeral_context_with_defaults",
-            EphemeralDataContext,
-            marks=[pytest.mark.unit],
-        ),
-        pytest.param(
-            "empty_data_context",
-            FileDataContext,
-            marks=[pytest.mark.filesystem],
-        ),
-        pytest.param(
-            "empty_cloud_context_fluent",
-            CloudDataContext,
-            marks=[pytest.mark.cloud],
-        ),
+        pytest.param("ephemeral_context_with_defaults", marks=[pytest.mark.unit]),
+        pytest.param("empty_data_context", marks=[pytest.mark.filesystem]),
+        pytest.param("empty_cloud_context_fluent", marks=[pytest.mark.cloud]),
     ],
 )
 @pytest.mark.unit
 def test_fluent_datasources_show_when_printed(
     context_fixture_name: str,
-    concrete_class: Type[AbstractDataContext],
     request,
 ) -> None:
     """Verify that fluent datasources show up when printed.
     This ends up testing both add and update (since adding assets and batch defs
     are implemented as updates on the datasource)
     """
-    # just double check that the context is what we think it is :)
     context = request.getfixturevalue(context_fixture_name)
-    assert isinstance(context, concrete_class)
 
     ds_name = "my-datasource"
     asset_name = "my-asset"
@@ -61,34 +39,19 @@ def test_fluent_datasources_show_when_printed(
 
 
 @pytest.mark.parametrize(
-    ("context_fixture_name", "concrete_class"),
+    "context_fixture_name",
     [
-        pytest.param(
-            "ephemeral_context_with_defaults",
-            EphemeralDataContext,
-            marks=[pytest.mark.unit],
-        ),
-        pytest.param(
-            "empty_data_context",
-            FileDataContext,
-            marks=[pytest.mark.filesystem],
-        ),
-        pytest.param(
-            "empty_cloud_context_fluent",
-            CloudDataContext,
-            marks=[pytest.mark.cloud],
-        ),
+        pytest.param("ephemeral_context_with_defaults", marks=[pytest.mark.unit]),
+        pytest.param("empty_data_context", marks=[pytest.mark.filesystem]),
+        pytest.param("empty_cloud_context_fluent", marks=[pytest.mark.cloud]),
     ],
 )
 @pytest.mark.unit
 def test_deleted_fluent_datasources_do_not_show_when_printed(
     context_fixture_name: str,
-    concrete_class: Type[AbstractDataContext],
     request,
 ) -> None:
-    # just double check that the context is what we think it is :)
     context = request.getfixturevalue(context_fixture_name)
-    assert isinstance(context, concrete_class)
 
     ds_name = "my-datasource"
     asset_name = "my-asset"
