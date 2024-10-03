5a71fbc3cd6edd82523116ca184b9a1e2631affc
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-10-03 09:43:05 +0200

    [MAINTENANCE] Mark GX as stable on PyPi (#10457)

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ config = {
     "include_package_data": True,
     "python_requires": get_python_requires(),
     "classifiers": [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Other Audience",
