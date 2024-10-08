30c6b49e994cbf2cbcb4b8b1127d171e347c0cc2
Chetan Kini <chetan@superconductive.com>
2024-10-08 12:16:35 -0400

    type ignore

--- great_expectations/core/util.py
+++ great_expectations/core/util.py
@@ -99,7 +99,7 @@ def nested_update(
 
 def in_jupyter_notebook():
     try:
-        from IPython import get_ipython
+        from IPython import get_ipython  # type: ignore[import-not-found]
 
         shell = get_ipython().__class__.__name__
         if shell == "ZMQInteractiveShell":
