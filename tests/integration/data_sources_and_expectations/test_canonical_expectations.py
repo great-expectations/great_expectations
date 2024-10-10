74d4942797856e23812edd5a183c12a9138fdb24
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-10-10 11:46:57 -0400

    Cleanup

--- tests/integration/data_sources_and_expectations/test_canonical_expectations.py
+++ tests/integration/data_sources_and_expectations/test_canonical_expectations.py
@@ -1,16 +1,10 @@
-import pytest
-
 from tests.integration.conftest import DataSourceType, parameterize_batch_for_data_sources
 
 
-@pytest.mark.parametrize("even_more", [pytest.param("a", id="a"), pytest.param("b", id="b")])
 @parameterize_batch_for_data_sources(
     types=[DataSourceType.FOO, DataSourceType.BAR],
     data=[1, 2],
     # description="test_stuff",
 )
-@pytest.mark.parametrize("more", [pytest.param(1, id="one"), pytest.param(2, id="two")])
-def test_stuff(batch_for_datasource, more, even_more) -> None:
-    assert batch_for_datasource == [1, 2, 3]
-    assert more in (1, 2)
-    assert even_more in ("a, b")
+def test_stuff(batch_for_datasource) -> None:
+    assert batch_for_datasource == [1, 2, 3]  # this would actually be a Batch object
