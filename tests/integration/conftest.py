267f195c2a5daae0a8351cee86c2d6add98f8308
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-10-11 15:31:55 -0400

    WiP

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,11 +1,18 @@
 import enum
+import random
+import string
 from abc import ABC, abstractmethod
 from collections.abc import Generator, Mapping
-from typing import Any, Callable, Optional, TypeVar
+from functools import cached_property
+from typing import Callable, Optional, TypeVar
 
+import pandas as pd
 import pytest
 
+import great_expectations as gx
 from great_expectations.compatibility.typing_extensions import override
+from great_expectations.data_context.data_context.abstract_data_context import AbstractDataContext
+from great_expectations.datasource.fluent.interfaces import Batch
 
 F = TypeVar("F", bound=Callable)
 
@@ -16,7 +23,7 @@ class DataSourceType(str, enum.Enum):
 
 
 def parameterize_batch_for_data_sources(
-    types: list[DataSourceType], data: list[int], description: Optional[str] = None
+    types: list[DataSourceType], data: pd.DataFrame, description: Optional[str] = None
 ) -> Callable[[F], F]:
     """Test decorator that parametrizes a test function with batches for various data sources.
 
@@ -53,7 +60,7 @@ def parameterize_batch_for_data_sources(
 
 
 @pytest.fixture
-def batch_for_datasource(request: pytest.FixtureRequest) -> Generator[Any, None, None]:
+def batch_for_datasource(request: pytest.FixtureRequest) -> Generator[Batch, None, None]:
     """Fixture that yields a batch for a specific data source type.
 
     This must be used in conjunction with `indirect=True` to defer execution
@@ -82,8 +89,7 @@ class BatchSetup(ABC):
         self.data = data
 
     @abstractmethod
-    def make_batch(self) -> list:  # TODO: return Batch
-        ...
+    def make_batch(self) -> Batch: ...
 
     @abstractmethod
     def setup(self) -> None: ...
@@ -93,10 +99,19 @@ class BatchSetup(ABC):
         pass
 
 
+_foo_cache: dict[int, pd.DataFrame] = {}
+
+
 class FooBatchSetup(BatchSetup):
     @override
-    def make_batch(self) -> list:
-        return [*self.data, 3]
+    def make_batch(self) -> Batch:
+        name = _random_resource_name()
+        return (
+            self._context.data_sources.add_pandas(name)
+            .add_dataframe_asset(name)
+            .add_batch_definition_whole_dataframe(name)
+            .get_batch(batch_parameters={"dataframe": self.data})
+        )
 
     @override
     def setup(self) -> None: ...
@@ -104,11 +119,15 @@ class FooBatchSetup(BatchSetup):
     @override
     def teardown(self) -> None: ...
 
+    @cached_property
+    def _context(self) -> AbstractDataContext:
+        return gx.get_context(mode="ephemeral")
+
 
 class BarBatchSetup(BatchSetup):
     @override
-    def make_batch(self) -> list:
-        return [*self.data, 3, 4]
+    def make_batch(self) -> Batch:
+        raise NotImplementedError
 
     @override
     def setup(self) -> None: ...
@@ -121,3 +140,7 @@ data_source_to_batch_setup: Mapping[DataSourceType, type[BatchSetup]] = {
     DataSourceType.FOO: FooBatchSetup,
     DataSourceType.BAR: BarBatchSetup,
 }
+
+
+def _random_resource_name() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=10))
--- tests/integration/data_sources_and_expectations/test_canonical_expectations.py
+++ tests/integration/data_sources_and_expectations/test_canonical_expectations.py
@@ -1,10 +1,22 @@
+import pandas as pd
+
+from great_expectations.datasource.fluent.interfaces import Batch
 from tests.integration.conftest import DataSourceType, parameterize_batch_for_data_sources
 
 
 @parameterize_batch_for_data_sources(
     types=[DataSourceType.FOO, DataSourceType.BAR],
-    data=[1, 2],
+    data=pd.DataFrame([[1, 2]]),
     # description="test_stuff",
 )
 def test_stuff(batch_for_datasource) -> None:
-    assert batch_for_datasource == [1, 2, 3]  # this would actually be a Batch object
+    assert isinstance(batch_for_datasource, Batch)
+
+
+@parameterize_batch_for_data_sources(
+    types=[DataSourceType.FOO, DataSourceType.BAR],
+    data=pd.DataFrame([[1, 2]]),
+    # description="test_stuff",
+)
+def test_more_stuff(batch_for_datasource) -> None:
+    assert isinstance(batch_for_datasource, Batch)
