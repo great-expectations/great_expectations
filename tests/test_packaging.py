c6eb14631f381d35ec4d140f8fddf4d1de8d96ed
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-10-15 14:50:07 +0200

    ratchet

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -193,7 +193,7 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
     )
 
     # Polish and ratchet this number down as low as possible
-    assert len(sorted_packages_with_pins_or_upper_bounds) == 36
+    assert len(sorted_packages_with_pins_or_upper_bounds) == 32
     assert set(sorted_packages_with_pins_or_upper_bounds) == {
         (
             "requirements-dev-api-docs-test.txt",
