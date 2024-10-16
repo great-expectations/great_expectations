9f3d9a9bc9c76b4b11397d61e8bc82e69b874fd4
Jose Tobar <jose@greatexpectations.io>
2024-10-16 10:59:56 -0700

    Revert changes

--- tests/render/test_renderer_configuration.py
+++ tests/render/test_renderer_configuration.py
@@ -110,7 +110,7 @@ def test_successful_renderer_configuration_instantiation(
                 "condition_parser": "great_expectations",
                 "row_condition": 'PClass=="1st"',
             },
-            ['PClass is "1st"'],
+            ['PClass IS "1st"'],
         ),
         (
             "expect_column_values_to_be_in_set",
@@ -130,7 +130,7 @@ def test_successful_renderer_configuration_instantiation(
                 "condition_parser": "great_expectations",
                 "row_condition": "foo == 1 | foo == 2",
             },
-            ["foo is 1 or foo is 2"],
+            ["foo IS 1", "foo IS 2"],
         ),
         (
             "expect_column_values_to_be_in_set",
@@ -140,7 +140,7 @@ def test_successful_renderer_configuration_instantiation(
                 "condition_parser": "great_expectations",
                 "row_condition": "foo ~ 4",
             },
-            ["foo is not 4"],
+            ["foo", "4"],
         ),
         (
             "expect_column_values_to_be_in_set",
@@ -150,7 +150,7 @@ def test_successful_renderer_configuration_instantiation(
                 "condition_parser": "great_expectations",
                 "row_condition": "foo > 1 & foo < 3",
             },
-            ["foo > 1 and foo < 3"],
+            ["foo > 1", "foo < 3"],
         ),
     ],
 )
