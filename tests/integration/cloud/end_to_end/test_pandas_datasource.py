467bbff33b1371bb30ce690b4a32c1b88929abc2
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-10-18 20:55:09 +0200

    wip

--- tests/integration/cloud/end_to_end/test_pandas_datasource.py
+++ tests/integration/cloud/end_to_end/test_pandas_datasource.py
@@ -1,25 +1,27 @@
 from __future__ import annotations
 
 import uuid
-from typing import TYPE_CHECKING, Iterator
+import warnings
+from typing import TYPE_CHECKING, Iterator, Generator
 
 import pandas as pd
 import pytest
 
+from great_expectations import ValidationDefinition
+from great_expectations.core.batch_definition import BatchDefinition
 from great_expectations.datasource.fluent.pandas_datasource import DataFrameAsset
-from great_expectations.expectations.expectation_configuration import (
-    ExpectationConfiguration,
-)
+import great_expectations.expectations as gxe
+from great_expectations.exceptions import DataContextError
+from great_expectations.core import ExpectationSuite, ExpectationValidationResult
+from great_expectations.checkpoint.checkpoint import Checkpoint, CheckpointResult
 
 if TYPE_CHECKING:
-    from great_expectations.checkpoint.checkpoint import Checkpoint, CheckpointResult
-    from great_expectations.core import ExpectationSuite, ExpectationValidationResult
     from great_expectations.data_context import CloudDataContext
     from great_expectations.datasource.fluent import (
         BatchRequest,
         DataAsset,
-        PandasDatasource,
-    )
+        PandasDatasource, GxInvalidDatasourceWarning,
+)
     from great_expectations.validator.validator import Validator
 
 
@@ -49,7 +51,7 @@ def datasource(
         name=datasource_name,
     )
     assert datasource.name == datasource_name
-    new_datasource_name = f"ds{uuid.uuid4().hex}"
+    new_datasource_name = f"data_source_{uuid.uuid4().hex}"
     datasource.name = new_datasource_name
     datasource = context.data_sources.add_or_update_pandas(
         datasource=datasource,
@@ -67,26 +69,15 @@ def datasource(
     return datasource
 
 
-def dataframe_asset(
-    datasource: PandasDatasource,
-    asset_name: str,
-) -> DataFrameAsset:
-    return datasource.add_dataframe_asset(
-        name=asset_name,
-    )
-
-
-@pytest.fixture(scope="module", params=[dataframe_asset])
+@pytest.fixture(scope="module")
 def data_asset(
     datasource: PandasDatasource,
     get_missing_data_asset_error_type: type[Exception],
-    request,
-) -> Iterator[DataAsset]:
+) -> Generator[DataFrameAsset, None, None]:
     """Test the entire Data Asset CRUD lifecycle here and in Data Asset-specific fixtures."""
-    asset_name = f"da_{uuid.uuid4().hex}"
-    yield request.param(
-        datasource=datasource,
-        asset_name=asset_name,
+    asset_name = f"asset_{uuid.uuid4().hex}"
+    yield datasource.add_dataframe_asset(
+        name=asset_name,
     )
     datasource.delete_asset(name=asset_name)
     with pytest.raises(get_missing_data_asset_error_type):
@@ -110,24 +101,72 @@ def batch_request(
 
 
 @pytest.fixture(scope="module")
+def batch_definition(
+    context: CloudDataContext,
+    data_asset: DataFrameAsset,
+) -> Generator[BatchDefinition, None, None]:
+    batch_def_name = f"batch_def_{uuid.uuid4().hex}"
+    yield data_asset.add_batch_definition_whole_dataframe(
+        name=batch_def_name,
+    )
+    data_asset.delete_batch_definition(name=batch_def_name)
+    with pytest.raises(KeyError):
+        data_asset.get_batch_definition(name=batch_def_name)
+
+@pytest.fixture(scope="module")
 def expectation_suite(
     context: CloudDataContext,
-    expectation_suite: ExpectationSuite,
-) -> ExpectationSuite:
+) -> Generator[ExpectationSuite, None, None]:
     """Add Expectations for the Data Assets defined in this module.
     Note: There is no need to test Expectation Suite CRUD.
     Those assertions can be found in the expectation_suite fixture.
     """
-    expectation_suite.add_expectation_configuration(
-        expectation_configuration=ExpectationConfiguration(
-            type="expect_column_values_to_not_be_null",
-            kwargs={
-                "column": "string",
-                "mostly": 1,
-            },
+    expectation_suite_name = f"es_{uuid.uuid4().hex}"
+    yield context.suites.add(
+        ExpectationSuite(
+            name=expectation_suite_name,
+            expectations=[
+                gxe.ExpectColumnValuesToNotBeNull(
+                    column="string",
+                    mostly=1
+                )
+            ]
         )
     )
-    return expectation_suite
+    context.suites.delete(name=expectation_suite_name)
+
+
+@pytest.fixture(scope="module")
+def validation_definition(
+    context: CloudDataContext,
+    expectation_suite: ExpectationSuite,
+    batch_definition: BatchDefinition,
+) -> Generator[ValidationDefinition, None, None]:
+    validation_def_name = f"val_def_{uuid.uuid4().hex}"
+    yield context.validation_definitions.add(
+        ValidationDefinition(
+            name=validation_def_name,
+            data=batch_definition,
+            suite=expectation_suite,
+        )
+    )
+    context.validation_definitions.delete(name=validation_def_name)
+
+
+@pytest.fixture(scope="module")
+def checkpoint(
+    context: CloudDataContext,
+    validation_definition: ValidationDefinition,
+) -> Iterator[Checkpoint]:
+    checkpoint_name = f"{validation_definition.name} Checkpoint"
+
+    checkpoint = Checkpoint(name=checkpoint_name, validation_definitions=[validation_definition])
+    checkpoint = context.checkpoints.add(checkpoint=checkpoint)
+    yield checkpoint
+    context.checkpoints.delete(name=checkpoint_name)
+
+    with pytest.raises(DataContextError):
+        context.checkpoints.get(name=checkpoint_name)
 
 
 @pytest.mark.cloud
@@ -139,7 +178,7 @@ def test_interactive_validator(
     Note: There is no need to test getting a Validator or using Validator.head(). That is already
     tested in the validator fixture.
     """
-    expectation_validation_result: ExpectationValidationResult = (
+    expectation_validation_result = (
         validator.expect_column_values_to_not_be_null(
             column="datetime",
             mostly=1,
@@ -148,9 +187,10 @@ def test_interactive_validator(
     assert expectation_validation_result.success
 
 
-@pytest.mark.xfail(reason="Fails due to a V1 change in the Checkpoint shape.", strict=True)
 @pytest.mark.cloud
-def test_checkpoint_run(checkpoint: Checkpoint):
+def test_checkpoint_run(checkpoint: Checkpoint, pandas_test_df):
     """Test running a Checkpoint that was created using the entities defined in this module."""
-    checkpoint_result: CheckpointResult = checkpoint.run()
+    checkpoint_result = checkpoint.run(batch_parameters={
+        "dataframe": pandas_test_df
+    })
     assert checkpoint_result.success
