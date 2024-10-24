346dd6228bb5ff5b3c4c4bef1857c3a9ca77677a
Bill Dirks <bill@greatexpectations.io>
2024-10-24 13:25:15 -0700

    release_prep

--- docs/docusaurus/docs/components/_data.jsx
+++ docs/docusaurus/docs/components/_data.jsx
@@ -1,5 +1,5 @@
 export default {
-  release_version: 'great_expectations, version 1.1.3',
+  release_version: 'great_expectations, version 1.2.0',
   min_python: '3.9',
   max_python: '3.12'
 }
--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -298,7 +298,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.1.3',
+              label: '1.2.0',
             },
             ['0.18']: {
               label: '0.18.21',
